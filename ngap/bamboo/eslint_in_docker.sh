c75a6a6a4d5bf9bab400aaa217545c74263efa96
Marc Huffnagle <marc@element84.com>
2017-05-02 20:30:05 -0400

    Add mocha_in_docker.sh

--- ngap/bamboo/eslint_in_docker.sh
+++ ngap/bamboo/eslint_in_docker.sh
@@ -8,7 +8,7 @@ set -e
   ./node_modules/.bin/eslint \
     --ext .js\
     --format junit \
-    --output-file results.xml \
+    --output-file test-results.xml \
     ./app/ ./test/
 )
-chown -R "${RELEASE_UID}:${RELEASE_GID}" /source/results.xml
+chown -R "${RELEASE_UID}:${RELEASE_GID}" /source/test-results.xml
