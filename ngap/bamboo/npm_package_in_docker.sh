ca46f2ac1712bb9011b3528d7e066cb301cceec4
Marc Huffnagle <marc@element84.com>
2017-05-04 17:25:43 -0400

    Add npm_package_in_docker.sh

---
+++ ngap/bamboo/npm_package_in_docker.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+###
+# This script is intended to be run inside of docker.  It will build the app
+# and create a package.tar file.
+#
+# It should be invoked with:
+#
+# docker run \
+#   -e RELEASE_UID=$(id -u) \
+#   -e RELEASE_GID=$(id -g) \
+#   --rm \
+#   -v "$(pwd):/source" \
+#   -w /source \
+#   node \
+#   /source/ngap/bamboo/npm_package_in_docker.sh
+#
+###
+
+set -e
+
+npm package
+rm -rf dist
+chown "${RELEASE_UID}:${RELEASE_GID}" /source/package.tar
