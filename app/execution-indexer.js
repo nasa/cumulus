8e74af37fcd0005cfd835134440349c233e0269b
Jason Gilman <jason@element84.com>
2017-05-09 14:17:55 -0400

    GITC-165: Added index for tracking the last time executions were indexed.

--- app/execution-indexer.js
+++ app/execution-indexer.js
@@ -1,5 +1,8 @@
 'use strict';
 
+/* eslint-disable no-await-in-loop */
+/* eslint-disable no-console */
+
 // Implements a local helper namespace to bulk index executions
 
 
@@ -81,59 +84,142 @@ const docsToBulk = (docs) => {
   return bulkArgs;
 };
 
+
+/**
+ * TODO
+ */
+const getLastIndexedDate = async () => {
+  const resp = await es().search({
+    index: 'executions-meta',
+    body: {
+      query: { match_all: {} },
+      stored_fields: ['last_indexed_date']
+    }
+  });
+  if (resp.hits.total > 0) {
+    return Date.parse(resp.hits.hits[0].fields.last_indexed_date[0]);
+  }
+  return null;
+};
+
+
+/**
+ * TODO
+ */
+const saveIndexedDate = async date =>
+  es().index({
+    index: 'executions-meta',
+    type: 'executionMeta',
+    id: 'executionMeta-id',
+    body: {
+      last_indexed_date: date
+    }
+  });
+
+
+/**
+ * The number of milliseconds before the last time we indexed that we'll continue to find and index
+ * executions. Executions prior to that will be skipped.
+ */
+const LAST_INDEXED_THRESHOLD = 5 * 60 * 1000;
+
+/**
+ * TODO
+ */
+const executionBeforeLastIndexed = (lastIndexedDate, e) =>
+  Date.parse(e.stopDate) < lastIndexedDate - LAST_INDEXED_THRESHOLD;
+
+
+/**
+ * TODO
+ */
+const indexExecutions = async (workflowId, executions) => {
+  // Convert non-running executions to docs
+  const docs = executions.filter(e => e.status !== 'RUNNING')
+  .map(e => executionToDoc(workflowId, e));
+
+  const numIndexed = docs.length;
+  if (numIndexed > 0) {
+    // Save them to elastic search
+    const bulkArgs = docsToBulk(docs);
+    const esResp = await es().bulk({ body: bulkArgs });
+    // Handle errors
+    if (esResp.errors !== false) {
+      console.error(`esResp failed ${JSON.stringify(esResp, null, 2)}`);
+      throw new Error('Failed saving to elasticsearch');
+    }
+  }
+  return numIndexed;
+};
+
 /**
  *
  */
-const indexRecentExecutions = async (stackName, numExecutions) => {
+const indexRecentExecutions = async (stackName, maxIndexExecutions) => {
+  const lastIndexedDate = await getLastIndexedDate();
+  // Capture the time that we start indexing before we start fetching executions.
+  const indexingStartTime = Date.now();
   const workflows = await ws.getWorkflowStatuses(stackName, 0);
+
   const promises = workflows.map(async (workflow) => {
     const arn = await ws.getStateMachineArn(stackName, workflow);
     const workflowId = workflow.get('id');
 
-    // eslint-disable-next-line no-console
-    console.log(`Indexing executions for workflow ${workflowId}`);
-    let numIndexed = 0;
-    let moreExecutions = true;
+    console.info(`Indexing executions for workflow ${workflowId}`);
+    let totalIndexed = 0;
+    let done = false;
     let nextToken = null;
 
-    while (numIndexed < numExecutions && moreExecutions) {
-      // eslint-disable-next-line no-await-in-loop
+    while (totalIndexed < maxIndexExecutions && !done) {
       const resp = await stepFunctions()
       .listExecutions({ stateMachineArn: arn, nextToken })
       .promise();
 
-      const docs = resp.executions.filter(e => e.status !== 'RUNNING')
-      .map(e => executionToDoc(workflowId, e));
+      // Did we find any executions before the lastIndexedDate?
+      const foundOlderExecutions = resp.executions.filter(e =>
+        executionBeforeLastIndexed(lastIndexedDate, e)
+      ).length > 0;
 
-      if (docs.length > 0) {
-        const bulkArgs = docsToBulk(docs);
-        // eslint-disable-next-line no-await-in-loop
-        const esResp = await es().bulk({ body: bulkArgs });
+      const numIndexed = await indexExecutions(workflowId, resp.executions);
 
-        if (esResp.errors !== false) {
-          // eslint-disable-next-line no-console
-          console.error(`esResp failed ${JSON.stringify(esResp, null, 2)}`);
-          throw new Error('Failed saving to elasticsearch');
-        }
-
-        numIndexed += docs.length;
-        // eslint-disable-next-line no-console
-        console.log(`Indexed total of ${numIndexed} docs for workflow ${workflowId}`);
+      if (numIndexed > 0) {
+        // Decide if we're continuing to search for executions
+        totalIndexed += numIndexed;
+        console.info(`Indexed total of ${totalIndexed} docs for workflow ${workflowId}`);
         nextToken = resp.nextToken;
-        moreExecutions = !!nextToken;
+
+        if (foundOlderExecutions) {
+          console.info('Stopping because executions were found older than last indexed date.');
+        }
+        if (!nextToken) {
+          console.info('Stopping because no more executions were found.');
+        }
+        // We're done if we found older executions that are before the last time we indexed or if
+        // AWS does not give us a next token meaning there's no more data to index.
+        done = foundOlderExecutions || !nextToken;
       }
       else {
-        moreExecutions = false;
+        console.info('Stopping because no executions were found.');
+        // Nothing was found to index
+        done = true;
+      }
+
+      if (totalIndexed >= maxIndexExecutions) {
+        console.info('We indexed up to max index executions.');
       }
     }
   });
+  // Wait until all of the workflows have been indexed
   await Promise.all(promises);
+  // Record that we've indexed at least everything up to the time we started.
+  await saveIndexedDate(indexingStartTime);
 };
 
-
 const stackName = 'gitc-pq-sfn';
-
-let p = indexRecentExecutions(stackName, 50000);
+let p;
+p = getLastIndexedDate();
+p = saveIndexedDate(Date.now() - (6 * 3600 * 1000));
+p = indexRecentExecutions(stackName, 50000);
 
 let data;
 p.then(d => data = d).catch(e => data = e);
