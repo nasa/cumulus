0e206f7b30ec769b1552e9244efe2a08a5029bf1
Scisco <scisco7@gmail.com>
2017-08-09 19:29:06 -0400

    move webpack to individual modules

--- cumulus/tasks/discover-pdrs/index.js
+++ cumulus/tasks/discover-pdrs/index.js
@@ -31,14 +31,13 @@ function handler(_event, context, cb) {
     event.payload.pdrs = pdrs;
     return cb(null, event);
   }).catch(e => {
-    if (e.details.status === 'timeout') {
+    if (e.details && e.details.status === 'timeout') {
       cb(new errors.ConnectionTimeout('connection Timed out'));
     }
-    else if (e.details.status === 'notfound') {
+    else if (e.details && e.details.status === 'notfound') {
       cb(new errors.HostNotFound(`${e.details.url} not found`));
     }
     else {
-      console.log(e);
       cb(e);
     }
   });
