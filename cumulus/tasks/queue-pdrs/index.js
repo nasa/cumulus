613ba11b7b4224fe92cc472ecce64136633134c0
Scisco <scisco7@gmail.com>
2017-08-10 08:49:01 -0400

    turn queueing a library

--- cumulus/tasks/queue-pdrs/index.js
+++ cumulus/tasks/queue-pdrs/index.js
@@ -1,7 +1,7 @@
 'use strict';
 
 import get from 'lodash.get';
-import { SQS, S3 } from '@cumulus/ingest/aws';
+import { queuePdr } from '@cumulus/ingest/queue';
 
 function handler(_event, context, cb) {
   // for each PDR, generate a new SF messages
@@ -9,31 +9,12 @@ function handler(_event, context, cb) {
 
   const event = _event;
   const pdrs = get(event, 'payload.pdrs', []);
-  const queueUrl = get(event, 'resources.queues.startSF');
-  const templates = get(event, 'resources.templates');
-  const next = get(event, 'ingest_meta.config.next', 'ParsePdr');
-  const messages = [];
 
-  // get message template
-  const parsed = S3.parseS3Uri(templates[next]);
-  S3.get(parsed.Bucket, parsed.Key).then((data) => {
-    const message = JSON.parse(data.Body);
-    message.provider = event.provider;
-    message.collection = event.collection;
-    message.meta = event.meta;
+  const queues = pdrs.map(pdr => queuePdr(event, pdr));
 
-    const queueMessages = pdrs.map((pdr) => {
-      message.payload = pdr;
-
-      message.ingest_meta.execution_name = `${pdr.pdrName}__PDR__${Date.now()}`;
-      messages.push(message.ingest_meta);
-      return SQS.sendMessage(queueUrl, message);
-    });
-
-    Promise.all(queueMessages).then(() => {
-      event.payload.pdrs_queued = messages.length;
-      return cb(null, event);
-    }).catch(e => cb(e));
+  Promise.all(queues).then(() => {
+    event.payload.pdrs_queued = queues.length;
+    return cb(null, event);
   }).catch(e => cb(e));
 }
 
