27b142ca10f5e094fe6da96f97665d8cf925eb8b
Scisco <scisco7@gmail.com>
2017-08-10 23:12:02 -0400

    add discover and discoverqueue

--- packages/ingest/granule.js
+++ packages/ingest/granule.js
@@ -1,12 +1,147 @@
 'use strict';
 
 const fs = require('fs');
+const join = require('path').join;
 const get = require('lodash.get');
 const urljoin = require('url-join');
 const uploadS3Files = require('@cumulus/common/aws').uploadS3Files;
 const errors = require('@cumulus/common/errors');
+const S3 = require('./aws').S3;
+const queue = require('./queue');
+const sftpMixin = require('./sftp');
 const ftpMixin = require('./ftp').ftpMixin;
 const httpMixin = require('./http').httpMixin;
+
+class Discover {
+  constructor(buckets, collections, provider) {
+    if (this.constructor === Discover) {
+      throw new TypeError('Can not construct abstract class.');
+    }
+
+    this.buckets = buckets;
+    this.port = get(provider, 'port', 21);
+    this.host = get(provider, 'host', null);
+    this.path = get(provider, 'path', '/');
+    this.provider = provider;
+    this.collections = collections;
+    this.endpoint = urljoin(this.host, this.path);
+    this.username = get(provider, 'username', null);
+    this.password = get(provider, 'password', null);
+
+    // create hash with file regex as key
+    this.regexes = {};
+    Object.keys(collections).forEach((name) => {
+      const c = collections[name];
+      Object.keys(c.files).forEach((key) => {
+        const f = c.files[key];
+        this.regexes[f.regex] = {
+          collection: name,
+          bucket: this.buckets[f.bucket],
+          definitionName: key
+        };
+      });
+    });
+  }
+
+  getCollection(file) {
+    for (const r of Object.keys(this.regexes)) {
+      if (file.name.match(r)) {
+        let granuleId;
+        const collection = this.collections[this.regexes[r].collection];
+        const test = new RegExp(collection.granuleIdExtraction);
+        const match = file.name.match(test);
+        if (match) {
+          granuleId = match[1];
+        }
+
+        return Object.assign({}, {
+          filename: file.name,
+          path: file.path,
+          filesize: file.size,
+          modifyTime: file.modifyTime,
+          granuleId
+        }, this.regexes[r]);
+      }
+    }
+
+    return false;
+  }
+
+  async discover() {
+    const list = await this._list();
+
+    // flatten list
+    const flatten = [];
+    Object.keys(list).forEach((p) => {
+      list[p].forEach(_item => {
+        const item = _item;
+        item.path = p;
+        flatten.push(item);
+      });
+    });
+
+    // pass list through regex list and get their
+    // location on cumulus to check if they exist
+    const files = flatten.map(item => this.getCollection(item)).filter(item => item);
+    return await this.findNewGranules(files);
+  }
+
+  async fileIsNew(file) {
+    const exists = await S3.fileExists(file.bucket, file.filename);
+    return exists ? false : file;
+  }
+
+  async findNewGranules(files) {
+    const checkFiles = files.map(f => this.fileIsNew(f));
+    const t = await Promise.all(checkFiles);
+    const newFiles = t.filter(f => f);
+
+    // reorganize by granule
+    const granules = {};
+    newFiles.forEach(_f => {
+      const f = _f;
+      const granuleId = f.granuleId;
+      const collection = f.collection;
+      delete f.granuleId;
+      delete f.collection;
+      if (granules[f.granuleId]) {
+        granules[granuleId].files.push(f);
+      }
+      else {
+        granules[granuleId] = {
+          collection,
+          granuleId,
+          files: [f]
+        };
+      }
+    });
+
+    return granules;
+  }
+}
+
+/**
+ * This is a base class for discovering PDRs
+ * It must be mixed with a FTP or HTTP mixing to work
+ *
+ * @class
+ * @abstract
+ */
+class DiscoverAndQueue extends Discover {
+  constructor(event) {
+    const buckets = get(event, 'resources.buckets');
+    const collections = get(event, 'meta.collections');
+    const provider = get(event, 'provider');
+
+    super(buckets, collections, provider);
+    this.event = event;
+  }
+
+  async findNewGranules(files) {
+    const granules = await super.findNewGranules(files);
+    return Promise.all(Object.values(granules).map(g => queue.queueGranule(this.event, g)));
+  }
+}
 
 
 /**
@@ -117,6 +252,23 @@ class Granule {
  * @class
  */
 
+class SftpDiscoverGranules extends sftpMixin(Discover) {}
+
+/**
+ * Ingest Granule from a FTP endpoint.
+ *
+ * @class
+ */
+
+class SftpDiscoverAndQueueGranules extends sftpMixin(DiscoverAndQueue) {}
+
+
+/**
+ * Ingest Granule from a FTP endpoint.
+ *
+ * @class
+ */
+
 class FtpGranule extends ftpMixin(Granule) {}
 
 /**
@@ -129,3 +281,5 @@ class HttpGranule extends httpMixin(Granule) {}
 
 module.exports.HttpGranule = HttpGranule;
 module.exports.FtpGranule = FtpGranule;
+module.exports.SftpDiscoverGranules = SftpDiscoverGranules;
+module.exports.SftpDiscoverAndQueueGranules = SftpDiscoverAndQueueGranules;
