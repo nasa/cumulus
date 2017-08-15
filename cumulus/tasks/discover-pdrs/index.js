770c9f607edf5bd55cb2d09bf8eae18a001a6c44
Scisco <scisco7@gmail.com>
2017-08-15 01:00:28 -0400

    add kms encryption/decryption support

--- cumulus/tasks/discover-pdrs/index.js
+++ cumulus/tasks/discover-pdrs/index.js
@@ -6,30 +6,30 @@ const pdr = require('@cumulus/ingest/pdr');
 const errors = require('@cumulus/common/errors');
 
 function handler(_event, context, cb) {
-  let discover;
   const event = Object.assign({}, _event);
-  const bucket = get(event, 'resources.buckets.internal');
+  const queue = get(event, 'meta.useQueue', true);
   const provider = get(event, 'provider', null);
+
+  // this is used to override where PDRs are saved
   const folder = get(event, 'meta.pdrsFolder', 'pdrs');
-  const discoverLimit = get(event, 'meta.discoverLimit', 100);
 
   if (!provider) {
     const err = new ProviderNotFound('Provider info not provided');
     return cb(err);
   }
 
-  // discover files
-  switch (provider.protocol) {
-    case 'ftp': {
-      discover = new pdr.FtpDiscoverAndQueue(event);
-      break;
-    }
-    default: {
-      discover = new pdr.HttpDiscoverAndQueue(event);
-    }
-  }
+  const Discover = granule.selector('discover', provider.protocol, queue);
+  const discover = new Discover(event);
 
   return discover.discover().then((pdrs) => {
+    if (queue) {
+      event.payload.granules_found = gs.length;
+    }
+    else {
+      event.payload.granules = gs;
+    }
+
+
     event.payload.pdrs = pdrs.slice(0, 10);
     return cb(null, event);
   }).catch(e => {
