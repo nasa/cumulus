d2f2fe8d8deb960751ba66b424473ced4c71d320
Scisco <scisco7@gmail.com>
2017-08-15 18:26:09 -0400

    add parse and queue option

--- cumulus/tasks/parse-pdr/index.js
+++ cumulus/tasks/parse-pdr/index.js
@@ -7,19 +7,33 @@ const pdr = require('@cumulus/ingest/pdr');
 module.exports.handler = function handler(_event, context, cb) {
   const event = Object.assign({}, _event);
   const provider = get(event, 'provider', null);
+  const queue = get(event, 'meta.useQueue', true);
 
   if (!provider) {
     const err = new errors.ProviderNotFound('Provider info not provided');
     return cb(err);
   }
 
-  const Parse = pdr.selector('parse', provider.protocol);
+  const Parse = pdr.selector('parse', provider.protocol, queue);
   const parse = new Parse(event);
 
-  return parse.ingest().then((granules) => {
-    event.payload = Object.assign({}, event.payload, granules);
+  return parse.ingest().then((payload) => {
+    if (parse.connected) {
+      parse.end();
+    }
+
+    if (queue) {
+      event.payload.granules_queued = payload.length
+    }
+    else {
+      event.payload = Object.assign({}, event.payload, payload);
+    }
     return cb(null, event);
   }).catch(e => {
+    if (parse.connected) {
+      parse.end();
+    }
+
     if (e.toString().includes('ECONNREFUSED')) {
       return cb(new errors.RemoteResourceError('Connection Refused'));
     }
