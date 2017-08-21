bac85089fa1d057a1149f71db33d5a7f39cf2180
Scisco <scisco7@gmail.com>
2017-08-21 09:49:59 -0400

    update encryption methods

--- daac-ops-api/models/providers.js
+++ daac-ops-api/models/providers.js
@@ -1,8 +1,7 @@
 'use strict';
 
-const forge = require('node-forge');
 const Manager = require('./base');
-const S3 = require('@cumulus/ingest/aws').S3;
+const { KMS } = require('@cumulus/ingest/aws');
 const providerSchema = require('./schemas').provider;
 
 class Provider extends Manager {
@@ -12,102 +11,32 @@ class Provider extends Manager {
   }
 
   async encryptPassword(password) {
-    // Download the publickey
-    const pki = forge.pki;
-    const pub = await S3.get(
-      process.env.internal,
-      `${process.env.StackName}-${process.env.Stage}/crypto/public.pub`
-    );
-
-    const publicKey = pki.publicKeyFromPem(pub.Body.toString());
-    return publicKey.encrypt(password);
+    const kmsId = process.env.KMS_ID;
+    return KMS.encrypt(password, kmsId);
   }
 
   async decryptPassword(password) {
-    const pki = forge.pki;
-    const priv = await S3.get(
-      process.env.internal,
-      `${process.env.StackName}-${process.env.Stage}/crypto/private.pem`
-    );
-
-    const privateKey = pki.privateKeyFromPem(priv.Body.toString());
-    return privateKey.decrypt(password);
+    return KMS.decrypt(password);
   }
 
   async update(key, _item, keysToDelete = []) {
     const item = _item;
     // encrypt the password
-    if (item.config && item.config.password) {
-      item.config.password = await this.encryptPassword(item.config.password);
+    if (item.password) {
+      item.password = await this.encryptPassword(item.password);
     }
 
     return super.update(key, item, keysToDelete);
   }
 
-  async create(_items) {
-    const items = _items;
-    if (items instanceof Array) {
-      for (const item of items) {
-        if (!item.regex) {
-          item.regex = {};
-        }
-      }
-    }
-    else {
-      items.regex = {};
-    }
-
+  async create(_item) {
+    const item = _item;
     // encrypt the password
-    if (items.config && items.config.password) {
-      items.config.password = await this.encryptPassword(items.config.password);
+    if (item.password) {
+      item.password = await this.encryptPassword(item.password);
     }
 
-    return super.create(items);
-  }
-
-  async addRegex(name, granuleIdExtraction, collectionName) {
-    const params = {
-      TableName: this.tableName,
-      Key: { name: name },
-      UpdateExpression: 'SET regex.#collectionName = :value',
-      ExpressionAttributeNames: {
-        '#collectionName': collectionName
-      },
-      ExpressionAttributeValues: {
-        ':value': granuleIdExtraction
-      },
-      ReturnValues: 'ALL_NEW'
-    };
-
-    const response = await this.dynamodb.update(params).promise();
-    return response.Attributes;
-  }
-
-  async removeRegex(name, collectionName) {
-    const params = {
-      TableName: this.tableName,
-      Key: { name: name },
-      UpdateExpression: 'REMOVE regex.#collectionName',
-      ExpressionAttributeNames: {
-        '#collectionName': collectionName
-      },
-      ReturnValues: 'ALL_NEW'
-    };
-
-    const response = await this.dynamodb.update(params).promise();
-    return response.Attributes;
-  }
-
-  /**
-   * Sets the PDR record to active and updates status to ingesting
-   *
-   */
-  async restart(name) {
-    return this.update(
-      { name: name },
-      { status: 'ingesting', isActive: true },
-      ['error'] // keys to delete
-    );
+    return super.create(item);
   }
 }
 
