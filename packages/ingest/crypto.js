6b2eceee0e4b6df348f223591e67b53f9d649fcd
Patrick Quinn <patrick@element84.com>
2017-08-21 16:14:52 -0400

    Add docs for crypto.js

--- packages/ingest/crypto.js
+++ packages/ingest/crypto.js
@@ -1,7 +1,21 @@
+/**
+ * Provides encryption and decryption methods with a consistent API but
+ * differing mechanisms for dealing with encryption keys.
+ */
+
 const forge = require('node-forge');
 const { S3, KMS } = require('./aws').S3;
 
+/**
+ * Provides encryption and decryption methods using a keypair stored in S3
+ */
 class S3KeyPairProvider {
+  /**
+   * Encrypt the given string using the given public key stored in the internal bucket
+   * @param {String} - The string to encrypt
+   * @param {String} - The name of the public key to use for encryption
+   * @return the decrypted string
+   */
   static async encrypt(str, keyId = 'public.pub') {
     // Download the publickey
     const pki = forge.pki;
@@ -14,6 +28,12 @@ class S3KeyPairProvider {
     return publicKey.encrypt(str);
   }
 
+  /**
+   * Decrypt the given string using the given private key stored in the internal bucket
+   * @param {String} - The encrypted string to decrypt
+   * @param {String} - The name of the private key to use for decryption
+   * @return the decrypted string
+   */
   static async decrypt(str, keyId = 'private.pem') {
     const pki = forge.pki;
     const priv = await S3.get(
@@ -29,5 +49,6 @@ class S3KeyPairProvider {
 module.exports = {
   S3KeyPairProvider: S3KeyPairProvider,
   KmsProvider: KMS,
+  // Use S3 by default. This will be the case until KMS is available in operations
   DefaultProvider: S3KeyPairProvider
 };
