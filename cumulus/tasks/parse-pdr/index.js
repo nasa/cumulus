80b716917151dd02e183ee108642b8b16f0d3d45
Scisco <scisco7@gmail.com>
2017-08-22 15:46:33 -0400

    check for payload size

--- cumulus/tasks/parse-pdr/index.js
+++ cumulus/tasks/parse-pdr/index.js
@@ -3,44 +3,45 @@
 const get = require('lodash.get');
 const errors = require('@cumulus/common/errors');
 const pdr = require('@cumulus/ingest/pdr');
+const { StepFunction } = require('@cumulus/ingest/aws');
 
 module.exports.handler = function handler(_event, context, cb) {
-  const event = Object.assign({}, _event);
-  const provider = get(event, 'provider', null);
-  const queue = get(event, 'meta.useQueue', true);
+  let event;
+  let parse;
+  StepFunction.pullEvent(_event).then((ev) => {
+    event = ev;
+    const provider = get(event, 'provider', null);
+    const queue = get(event, 'meta.useQueue', true);
 
-  if (!provider) {
-    const err = new errors.ProviderNotFound('Provider info not provided');
-    return cb(err);
-  }
+    if (!provider) {
+      const err = new errors.ProviderNotFound('Provider info not provided');
+      return cb(err);
+    }
 
-  const Parse = pdr.selector('parse', provider.protocol, queue);
-  const parse = new Parse(event);
+    const Parse = pdr.selector('parse', provider.protocol, queue);
+    parse = new Parse(event);
 
-  return parse.ingest().then((payload) => {
+    return parse.ingest();
+  }).then((payload) => {
     if (parse.connected) {
       parse.end();
     }
 
-    if (queue) {
-      event.payload.granules_queued = payload.length
-    }
-    else {
-      event.payload = Object.assign({}, event.payload, payload);
-    }
-    return cb(null, event);
-  }).catch(e => {
-    if (parse.connected) {
-      parse.end();
-    }
+    event.payload = Object.assign({}, event.payload, payload);
+    return StepFunction.pushEvent(event);
+  }).then(ev => cb(null, ev))
+    .catch(e => {
+      if (parse.connected) {
+        parse.end();
+      }
 
-    if (e.toString().includes('ECONNREFUSED')) {
-      return cb(new errors.RemoteResourceError('Connection Refused'));
-    }
-    else if (e.details && e.details.status === 'timeout') {
-      return cb(new errors.ConnectionTimeout('connection Timed out'));
-    }
+      if (e.toString().includes('ECONNREFUSED')) {
+        return cb(new errors.RemoteResourceError('Connection Refused'));
+      }
+      else if (e.details && e.details.status === 'timeout') {
+        return cb(new errors.ConnectionTimeout('connection Timed out'));
+      }
 
-    return cb(e);
-  });
+      return cb(e);
+    });
 };
