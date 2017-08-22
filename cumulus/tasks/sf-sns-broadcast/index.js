33544319fc4b9e149b3b1e51421d17a2b6d80442
Scisco <scisco7@gmail.com>
2017-08-22 07:01:50 -0400

    separate start and end

--- cumulus/tasks/sf-sns-broadcast/index.js
+++ cumulus/tasks/sf-sns-broadcast/index.js
@@ -1,23 +1,43 @@
+/* eslint-disable no-param-reassign */
 'use strict';
 
 const AWS = require('aws-sdk');
 const get = require('lodash.get');
 
-function handler(event, context, cb) {
+function publish(arn, message, cb) {
+  const sns = new AWS.SNS();
+  sns.publish({
+    TopicArn: arn,
+    Message: JSON.stringify(message)
+  }, (e) => {
+    if (e) return cb(e);
+    return cb(message.exception, message);
+  });
+}
+
+function start(event, context, cb) {
   const topicArn = get(event, 'ingest_meta.topic_arn', null);
 
   if (topicArn) {
-    const sns = new AWS.SNS();
-    return sns.publish({
-      TopicArn: topicArn,
-      Message: JSON.stringify(event)
-    }, (e) => {
-      if (e) return cb(e);
-
-      return cb(null, event);
-    });
+    return publish(topicArn, event, cb);
   }
   return event;
 }
 
-module.exports.handler = handler;
+function end(event, context, cb) {
+  const topicArn = get(event, 'ingest_meta.topic_arn', null);
+
+  if (topicArn) {
+    if (event.exception) {
+      event.ingest_meta.status = 'failed';
+    }
+    else {
+      event.ingest_meta.status = 'completed';
+    }
+    return publish(topicArn, event, cb);
+  }
+  return event;
+}
+
+module.exports.start = start;
+module.exports.end = end;
