ac42b5bf6d087510a9d60cb3fc928167b97789b7
Scisco <scisco7@gmail.com>
2017-08-24 00:48:39 -0400

    make sf-sns-broadcast fails after publishing the message with exception

--- cumulus/tasks/sf-sns-broadcast/index.js
+++ cumulus/tasks/sf-sns-broadcast/index.js
@@ -4,15 +4,18 @@
 const AWS = require('aws-sdk');
 const get = require('lodash.get');
 const { StepFunction } = require('@cumulus/ingest/aws');
+const errors = require('@cumulus/common/errors');
 
 async function publish(message, finish = false) {
   const event = await StepFunction.pullEvent(message);
   const topicArn = get(event, 'ingest_meta.topic_arn', null);
+  let failed = false;
 
   if (topicArn) {
     // if this is the sns call at the end of the execution
     if (finish) {
       if (event.exception || event.error) {
+        failed = true;
         event.ingest_meta.status = 'failed';
       }
       else {
@@ -30,6 +33,20 @@ async function publish(message, finish = false) {
       TopicArn: topicArn,
       Message: JSON.stringify(message)
     }).promise();
+  }
+
+  if (failed) {
+    const error = get(event, 'exception.Error');
+    const cause = get(event, 'exception.Cause');
+    if (error) {
+      if (errors[error]) {
+        throw new errors[error](cause);
+      }
+      else if (error === 'TypeError') {
+        throw new TypeError(cause);
+      }
+      throw new Error(cause);
+    }
   }
 
   return message;
