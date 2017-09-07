92a82f5881cde3f1f0f6a74119f49b0f00df3210
James Norton <jamesnorton@gmail.com>
2017-09-07 13:48:24 -0400

    Gitc 400 - Common SIPS handler (#37)
    * GITC-400: First cut at all the tasks for the SIPS handler step function.
    
    * GITC-400: Modified to use FTP/SFTP SIPS server instead of S3 for testing.
    
    * GITC-400: Corrected returned value from discover-pdrs.
    
    * GITC-400: Finished initial downloader mock implementation.
    
    * GITC-400: Added task to do archive validation
    
    * GITC-400: Added initial code for PDRD generation
    
    * GITC-400: Changed format to support download activity.
    
    * GITC-400: Added file list generation task
    
    * GITC-400: Added support for generating PAN files.
    
    * GITC-400: Added tee module to help demo task pipleline with Provider Gateway
    
    * GITC-400: Hcnaged tasks to use snake-case in message keys
    
    * Fixes broken GIBS tests.
    
    * GITC-400: Intermediate commit
    
    * GITC-400: Added more validations for PDR
    
    * CMR-400: Fixed bug in PDRD generation
    
    * GITC-400: Fixed bug that was causing pdr deletion to hang
    
    * GITC-400: Cleaned up requires
    
    * GITC-400: Adding PVL dependency. Fixing promise resolution in message source.
    
    * GITC-400: Added Choice to SIPS step function
    Needed to deal with times when no PDRs are available from the SIPS server.
    
    * GITC-400: Updated to support provider gateway config standard
    
    * GITC-400: Updated require paths to lerna style
    
    * GITC-400: Changed SIPS tasks to download PDRS to S3 first
    
    * GITC-400: Trigger process PDRs now working
    
    * GITC-400: Fixed execution name generation for trigger process pdr
    
    * GITC-400: Fixed output of archive validation task
    
    * GITC-400: Added support for deleting PDR from S3 when provider gateway fails
    This is needed to allow retries
    
    * GIT-400: Fixed requires for pvl package
    
    * GITC-400: Added some test files.
    
    * GITC-400: Changes to try to get tests running.
    
    * GITC-400: Removed some commented out code
    
    * GITC-400: Fixed broken test
    
    * GITC-400: More tests
    
    * GITC-400: More tests
    
    * GITC-400: Responding to PR comments
    
    * GITC-400: Updated test settings
    
    * GITC-400: Updated more tests to work with AVA
    
    * GITC-400: Updated more tests to work with AVA
    
    * GITC-400: Updated more tests to work with AVA
    
    * GITC-400: Updated package.json for tee task
    
    * GITC-400: Updated package.json for download-activity-mock task
    
    * GITC-400: Added place holder test for validate-pdr
    
    * GITC-400: Updated test for tirgger-process-pdrs
    
    * GITC-400: Added tests for pdrd generation
    
    * GITC-400: Added tests for PDR validation
    
    * GITC-400: Added test for trigger PDR processing
    
    * GITC-400: Fixed broken test
    
    * GITC-400: Fixed bug in PDR validation

---
+++ cumulus/tasks/delete-pdr-ftp/index.js
@@ -0,0 +1,75 @@
+'use strict';
+
+const log = require('@cumulus/common/log');
+const Task = require('@cumulus/common/task');
+const FtpClient = require('ftp');
+const SftpClient = require('sftpjs');
+const promisify = require('util.promisify');
+const ftp = require('./ftp_util');
+
+/**
+ * Task that deletes a PDR from a SIPS server
+ * Input payload: The path to the PDR on the server
+ * Output payload: none
+ */
+module.exports = class DeletePdrFtp extends Task {
+  /**
+   * Main task entry point
+   * @return An object referencing the oldest PDR on the server
+   */
+  async run() {
+    // Vars needed from config to connect to the SIPS server
+    const { conn_type, host, port, username, password } =
+     this.message.provider.config.gateway_config.conn_config;
+    const folder = this.config.folder;
+
+    // Message payload contains the name of the PDR to be deleted
+    const payload = await this.message.payload;
+    const pdrFileName = payload.pdr_file_name;
+
+    let client;
+    if (conn_type.toUpperCase() === 'FTP') {
+      client = new FtpClient();
+    }
+    else {
+      client = new SftpClient();
+    }
+
+    const clientReady = promisify(client.once).bind(client);
+
+    client.connect({
+      host: host,
+      port: port,
+      user: username,
+      password: password
+    });
+
+    await clientReady('ready');
+
+    try {
+      await ftp.deleteFile(client, folder, pdrFileName);
+    }
+    catch (e) {
+      log.error(e);
+      log.error(e.stack);
+      throw e;
+    }
+    finally {
+      // Close the connection to the SIPS server
+      client.end();
+    }
+
+    log.info(`PDR ${pdrFileName} DELETED`);
+
+    return { pdr_file_name: pdrFileName };
+  }
+
+  /**
+   * Entry point for Lambda
+   * @param {array} args The arguments passed by AWS Lambda
+   * @return The handler return value
+   */
+  static handler(...args) {
+    return DeletePdrFtp.handle(...args);
+  }
+};
