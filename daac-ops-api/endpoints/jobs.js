4d1c545526142a4c13d8f2e02b195326fe735e25
Scisco <scisco7@gmail.com>
2017-09-12 00:06:46 -0400

    improve background job logic

--- daac-ops-api/endpoints/jobs.js
+++ daac-ops-api/endpoints/jobs.js
@@ -1,14 +1,16 @@
 /* runs a bunch of periodic jobs to keep the database updateToDate */
 'use strict';
 
+const get = require('lodash.get');
 const log = require('@cumulus/ingest/log');
 const { StepFunction } = require('@cumulus/ingest/aws');
 const { Search } = require('../es/search');
+const { handlePayload } = require('../es/indexer');
 
-async function findStaleRecords(type, timeout, limit = 100, page = 1) {
+async function findStaleRecords(type, q, limit = 100, page = 1) {
   const search = new Search({
     queryStringParameters: {
-      q: `status:running AND timestamp:<${timeout}`,
+      q,
       page: page,
       limit: limit
     }
@@ -16,44 +18,21 @@ async function findStaleRecords(type, timeout, limit = 100, page = 1) {
   const response = await search.query();
 
   if (response.results.length >= limit) {
-    const more = await findStaleRecords(type, timeout, limit, page + 1);
+    const more = await findStaleRecords(type, q, limit, page + 1);
     return response.results.concat(more);
   }
   return response.results;
 }
 
-async function abortExecution(arn) {
-  let r;
-  try {
-    r = await StepFunction.stop(
-      arn,
-      'Execution is aborted because it did not finish in 5 hours',
-      'Timeout'
-    );
-  }
-  catch (e) {
-    if (e.message.includes('Execution Does Not Exist')) {
-      log.error(e.message);
-    }
-    else {
-      throw e;
-    }
-  }
-  return r;
-}
-
-async function markRecordAsFailed(esClient, id, type, p, index = 'cumulus') {
+async function markRecordAsFailed(esClient, id, type, status, error, p, index = 'cumulus') {
   const params = {
     index,
     type,
     id,
     body: {
       doc: {
-        status: 'failed',
-        error: {
-          Error: 'Timeout',
-          Cause: 'Task did not finish. Cause unknown'
-        },
+        status,
+        error,
         timestamp: Date.now()
       }
     }
@@ -66,6 +45,78 @@ async function markRecordAsFailed(esClient, id, type, p, index = 'cumulus') {
   return esClient.update(params);
 }
 
+async function checkExecution(arn, url, esClient) {
+  let error = {
+    Error: null,
+    Cause: null
+  };
+  const r = await StepFunction.getExecution(arn, true);
+  r.status = r.status.toLowerCase();
+  r.status = r.status === 'succeeded' ? 'completed' : r.status;
+
+  const input = JSON.parse(get(r, 'input', '{}'));
+  const output = JSON.parse(get(r, 'output', '{}'));
+  const type = get(output, 'ingest_meta.workflow_name');
+
+  if (!input) {
+    return;
+  }
+
+  if (r.status === 'not_found' || r.status === 'running') {
+    log.error(`Execution does not exist: ${arn}`);
+    error = {
+      Error: 'Timeout',
+      Cause: 'Execution is aborted because it did not finish in 5 hours'
+    };
+    await markRecordAsFailed(esClient, arn, 'execution', 'failed', error);
+
+    if (r.status === 'running') {
+      await StepFunction.stop(
+        arn,
+        error.Cause,
+        error.Error
+      );
+    }
+
+    // find related granule and update their status
+    if (type === 'IngestGranule') {
+      const searchTerm = `execution:"${url}"`;
+      const granules = await findStaleRecords('granule', searchTerm, 100);
+      await Promise.all(granules.map(g => markRecordAsFailed(
+        esClient, g.granuleId, 'granule', 'failed', error, g.collectionId
+      )));
+    }
+
+    // find related pdrs and update their status
+    if (type === 'ParsePdr') {
+      const searchTerm = `execution:"${url}"`;
+      const pdrs = await findStaleRecords('pdr', searchTerm, 100);
+      await Promise.all(pdrs.map(p => markRecordAsFailed(
+        esClient, p.pdrName, 'pdr', 'failed', error
+      )));
+    }
+  }
+  else {
+    if (output.error) {
+      input.exception = output.error;
+      input.ingest_meta.status = 'failed';
+      await handlePayload(output);
+      return;
+    }
+
+    if (!output.ingest_meta) {
+      output.ingest_meta = {
+        status: r.status
+      };
+    }
+    else {
+      output.ingest_meta.status = r.status;
+    }
+
+    await handlePayload(output);
+  }
+}
+
 function getHoursAgo(hours) {
   const now = Date.now();
   return now - (hours * 60 * 60 * 1000);
@@ -73,23 +124,14 @@ function getHoursAgo(hours) {
 
 async function cleanup() {
   const fiveHoursAgo = getHoursAgo(5);
-  const tenHoursAgo = getHoursAgo(10);
+  const searchTerm = `status:running AND timestamp:<${fiveHoursAgo}`;
 
   const esClient = await Search.es();
-  const executions = await findStaleRecords('execution', fiveHoursAgo, 100);
-  const granules = await findStaleRecords('granule', fiveHoursAgo, 100);
-  const pdrs = await findStaleRecords('pdr', tenHoursAgo, 100);
+  const executions = await findStaleRecords('execution', searchTerm, 100);
 
-  log.info(`Found ${executions.length} stale executions, ` +
-           `${granules.length} stale granules, ${pdrs.length} stale pdrs`);
+  log.info(`Found ${executions.length} stale executions`);
 
-  await Promise.all(executions.map(ex => abortExecution(ex.arn)));
-  await Promise.all(executions.map(ex => markRecordAsFailed(esClient, ex.arn, 'execution')));
-
-  await Promise.all(
-    granules.map(g => markRecordAsFailed(esClient, g.granuleId, 'granule', g.collectionId))
-  );
-  await Promise.all(pdrs.map(p => markRecordAsFailed(esClient, p.pdrName, 'pdr')));
+  await Promise.all(executions.map(ex => checkExecution(ex.arn, ex.execution, esClient)));
 }
 
 function handler(event, context, cb) {
