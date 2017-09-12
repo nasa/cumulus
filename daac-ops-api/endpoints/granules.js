a69b68c4ca43109c54084beb104a49d76cb75de6
Scisco <scisco7@gmail.com>
2017-09-12 11:56:25 -0400

    add reingest method for granule endpoint

--- daac-ops-api/endpoints/granules.js
+++ daac-ops-api/endpoints/granules.js
@@ -1,8 +1,11 @@
 'use strict';
 
 const _get = require('lodash.get');
+const { invoke } = require('@cumulus/ingest/aws');
 const handle = require('../lib/response').handle;
 const Search = require('../es/search').Search;
+const { partialRecordUpdate } = require('../es/indexer');
+const Rule = require('../models/rules');
 
 /**
  * List all granules for a given collection.
@@ -17,6 +20,67 @@ function list(event, cb) {
     cb(e);
   });
 }
+
+
+/**
+ * Update a single granule.
+ * Supported Actions: Reprocess, Remove From CMR.
+ *
+ * @param {object} event aws lambda event object.
+ * @return {Promise}
+ */
+async function put(event) {
+  const granuleId = _get(event.pathParameters, 'granuleName');
+  let body = _get(event, 'body', '{}');
+  body = JSON.parse(body);
+
+  const action = _get(body, 'action');
+
+  if (action) {
+    const search = new Search({}, 'granule');
+    const response = await search.get(granuleId);
+    if (action === 'reingest') {
+      const collection = response.collectionId.split('___');
+      const payload = await Rule.buildPayload({
+        workflow: 'IngestGranule',
+        provider: response.provider,
+        collection: {
+          name: collection[0],
+          version: collection[1]
+        },
+        meta: { granuleId: response.granuleId },
+        payload: {
+          granules: [{
+            granuleId: response.granuleId,
+            files: response.files
+          }]
+        }
+      });
+
+      await partialRecordUpdate(
+        null,
+        response.granuleId,
+        'granule',
+        { status: 'running' },
+        response.collectionId
+      );
+      await invoke(process.env.invoke, payload);
+      return {
+        granuleId: response.granuleId,
+        action,
+        status: 'SUCCESS'
+      };
+    }
+    else if (action === 'removeFromCmr') {
+
+    }
+
+    throw new Error('Action is not supported. Choices are: \'reprocess\' and \'removeFromCmr\'');
+  }
+
+  throw new Error('Action is missing');
+}
+
 
 /**
  * Query a single granule.
@@ -39,6 +103,9 @@ function handler(event, context) {
   handle(event, context, true, (cb) => {
     if (event.httpMethod === 'GET' && event.pathParameters) {
       get(event, cb);
+    }
+    else if (event.httpMethod === 'PUT' && event.pathParameters) {
+      put(event).then(r => cb(null, r)).catch(e => cb(e));
     }
     else {
       list(event, cb);
