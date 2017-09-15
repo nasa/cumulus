3b0e1ffe6446d7c16713fc3133f7b74018ff45cb
James Norton <james@element84.com>
2017-09-15 14:47:21 -0400

    Fixed broken test.

--- packages/task-debug/src/index.js
+++ packages/task-debug/src/index.js
@@ -17,7 +17,7 @@ const increaseVerbosity = (_v, total) => total + 1;
  * Run a task locally with given message and return ouptput message for next stage
  * @param {Function} handler Function that executes a task
  * @param {Function} invocation Function that returns the message for a task
- * @return {Object} The message for the next stage
+ * @return {*} The result from the execution
  */
 const runTask = (handler, invocation) => handler(invocation(), {}, result => result);
 
@@ -29,23 +29,34 @@ const doDebug = configFile => {
   log.info(`Verbosity: ${program.verbose}`);
 };
 
-const messageGen = (taskName, payload = null) =>
+/**
+ *
+ * @param {string} taskName
+ * @param {*} payload
+ */
+const genMessage = (taskName, resources = {}, payload = null) =>
   local.collectionMessageInput('VNGCR_LQD_C1_SIPS', taskName, o =>
     Object.assign({}, o, {
-      resources: {
-        buckets: {
-          private: 'gitc-jn-private'
-        }
-      },
+      resources: resources,
       payload: payload
     })
   );
 
-const rval = runTask(DiscoverPdrTask.handler, messageGen('DiscoverPdr'));
+const message = genMessage('DiscoverPdr');
+log.info(`MESSAGE: ${message}`);
+
+const rval = runTask(
+  DiscoverPdrTask.handler,
+  genMessage('DiscoverPdr', {
+    buckets: {
+      private: 'gitc-jn-private'
+    }
+  })
+);
 
 rval.then(results => {
   log.info(JSON.stringify(results));
-  const res = runTask(TriggerProcessPdrsTask.handler, messageGen('TriggerProcessPdrs', results));
+  const res = runTask(TriggerProcessPdrsTask.handler, genMessage('TriggerProcessPdrs', results));
   res.then(r => {
     log.info(JSON.stringify(r));
   });
