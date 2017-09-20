a965fc05f0fdafbf34929276c928b7fe626bbfae
James Norton <jamesnorton@gmail.com>
2017-09-20 16:01:18 -0400

    Debug tasks (#41)
    * Basic task chaining and debugging working.
    
    * Fixed broken test.
    
    * Workflows mostly working
    
    * Refactored to clean up debug harness a bit.
    
    * Cleaned up workflow execution
    
    * Removed some logging

---
+++ packages/task-debug/src/index.js
@@ -0,0 +1,46 @@
+'use strict';
+
+// Let everyone know that this is a debug session
+global.__isDebug = true;
+
+const log = require('@cumulus/common/log');
+const program = require('commander');
+const workflow = require('./workflow');
+const local = require('@cumulus/common/local-helpers');
+
+const increaseVerbosity = (_v, total) => total + 1;
+
+const doDebug = async () => {
+  const configFile = program.configFile;
+  const collectionId = program.collection;
+  const workflowName = program.workflow;
+  const bucket = program.bucket;
+
+  log.info(`Config file: ${configFile}`);
+  log.info(`Collection: ${collectionId}`);
+  log.info(`Workflow: ${workflowName}`);
+  log.info(`S3 Bucket: ${bucket}`);
+
+  const workflows = local.parseWorkflows(collectionId);
+  const wf = workflows[workflowName];
+  const resources = {
+    buckets: {
+      private: bucket
+    }
+  };
+
+  const result = await workflow.runWorkflow(collectionId, wf, resources);
+
+  log.info(`RESULT: ${JSON.stringify(result)}`);
+};
+
+program
+  .version('0.0.1')
+  .option('-v, --verbose', 'A value that can be increased', increaseVerbosity, 0)
+  .option('-c, --collection <value>', 'The ID of the collection to process')
+  .option('-w, --workflow <value>', 'The workflow to run')
+  .option('-b, --bucket [value]', 'The private S3 bucket to use')
+  .command('debugg <config-file>')
+  .action(doDebug);
+
+program.parse(process.argv);
