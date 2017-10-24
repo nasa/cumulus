9e9151558075f219949bbc463128bc3933ca0ca9
Katherine Kelly <katherine.a.kelly@nasa.gov>
2017-10-24 15:02:05 -0400

    TODO hello-world-task test

---
+++ cumulus/tasks/hello-world-task/tests/hello_world_test.js
@@ -0,0 +1,9 @@
+'use strict';
+const expect = require('expect.js');
+const test = require('ava');
+const helpers = require('@cumulus/common/test-helpers');
+import { handler } from '../index';
+
+test.cb('TODO - add test', t => {
+  t.is(1, 1);
+});
