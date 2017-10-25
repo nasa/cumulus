5bb9ea1ef33845a0049c895199b04e18cdae2123
Katherine Kelly <katherine.a.kelly@nasa.gov>
2017-10-25 16:37:23 -0400

    Updates

--- cumulus/tasks/hello-world-task/tests/hello_world_test.js
+++ cumulus/tasks/hello-world-task/tests/hello_world_test.js
@@ -1,9 +1,20 @@
 'use strict';
-const expect = require('expect.js');
 const test = require('ava');
 const helpers = require('@cumulus/common/test-helpers');
 import { handler } from '../index';
 
-test.cb('TODO - add test', t => {
-  t.is(1, 1);
+
+test('TODO - add test using Task class', t => {
+  t.is(1,1);
 });
+
+
+/*
+test.cb('Tests response from Handler', t => {
+  handler({}, {}, (err, data)=> {
+    t.is(err, null);
+    t.is(data.hello, "Hello World");
+    t.end();
+  });
+});
+*/
