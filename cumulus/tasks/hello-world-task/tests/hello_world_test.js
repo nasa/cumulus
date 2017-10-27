263904ad14434b532e1f3ceaf1d040ab04e02b52
Katherine Kelly <katherine.a.kelly@nasa.gov>
2017-10-27 13:47:54 -0400

    Added test using Task class

--- cumulus/tasks/hello-world-task/tests/hello_world_test.js
+++ cumulus/tasks/hello-world-task/tests/hello_world_test.js
@@ -1,20 +1,24 @@
 'use strict';
 const test = require('ava');
 const helpers = require('@cumulus/common/test-helpers');
-import { handler } from '../index';
+const HelloWorld = require('../index');
 
 
-test('TODO - add test using Task class', t => {
-  t.is(1,1);
+test('Test return value from Hello World Task', async t => {
+
+  /*
+  * This setup prepares a formatted message required For the Task class.
+  * The specific values of the message don't make a difference
+  * as the Hello World Task doesn't use any parameters.
+  */
+  const payload = [{ s3_key: '123' }];
+  let message = helpers.collectionMessageInput('VNGCR_LQD_C1_SIPS', 'HelloWorld');
+  message = Object.assign(message, { payload: payload });
+  const ingestMeta = Object.assign({}, message.ingest_meta, { message_source: 'stdin' });
+  message.ingest_meta = ingestMeta;
+
+  const [errors, data] = await helpers.run(HelloWorld, message);
+
+  t.is(errors, null);
+  t.is(data.hello, "Hello World");
 });
-
-
-/*
-test.cb('Tests response from Handler', t => {
-  handler({}, {}, (err, data)=> {
-    t.is(err, null);
-    t.is(data.hello, "Hello World");
-    t.end();
-  });
-});
-*/
