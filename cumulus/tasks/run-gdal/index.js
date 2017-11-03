b0ed43c3e7439180bfff65f7a36dd03829bfabcc
Patrick Quinn <patrick@element84.com>
2017-11-03 10:16:43 -0400

    GITC-484: Allow run-gdal to compress output and handle multiple inputs

--- cumulus/tasks/run-gdal/index.js
+++ cumulus/tasks/run-gdal/index.js
@@ -2,11 +2,13 @@
 
 const Task = require('@cumulus/common/task');
 const aws = require('@cumulus/common/aws');
+const log = require('@cumulus/common/log');
 const fs = require('fs');
+const path = require('path');
 const spawn = require('child_process').spawn;
 
 /**
- * 
+ *
  */
 module.exports = class RunGdalTask extends Task {
   /**
@@ -17,34 +19,67 @@ module.exports = class RunGdalTask extends Task {
     const config = this.config;
     const payload = this.message.payload;
 
-    const filename = this.config.input_filename;
-    const { bucket, key } = Array.isArray(payload) ? payload[0] : payload;
-    await aws.downloadS3File({ Bucket: bucket, Key: key }, filename);
+    let files = [];
+    if (Array.isArray(payload)) {
+      files = files.concat(payload);
+    }
+    else if (payload) {
+      files.push(payload);
+    }
+    if (config.additional_files) {
+      files = files.concat(config.additional_files);
+    }
+    if (files.length > config.input_filenames.length) {
+      throw new Error('input_filenames do not provide enough values for input files');
+    }
+    const downloads = files.map((s3file, i) =>
+      aws.downloadS3File(s3file, path.join('/tmp', config.input_filenames[i]))
+    );
+    await Promise.all(downloads);
 
     for (const command of config.commands) {
       await this.runGdalCommand(command.gdal_command, command.args);
     }
 
-    const outputs = config.outputs.map((out) => ({
-      filename: out.filename,
-      bucket: out.dest.bucket,
-      key: out.dest.key
-    }));
+    const outputPromises = config.outputs.map((output) => this.compressAndUploadOutput(output));
 
-    return await aws.uploadS3Files(outputs);
+    return await Promise.all(outputPromises);
+  }
+
+  async compressAndUploadOutput(output) {
+    let filename = output.filename;
+    if (output.compress) {
+      filename = `${filename}.tgz`;
+      await this.promiseSpawn('tar', ['cvpzfS', filename, output.filename]);
+    }
+
+    return aws.uploadS3Files([{
+      filename: path.join('/tmp', filename),
+      bucket: output.dest.Bucket,
+      key: output.dest.Key
+    }]);
   }
 
   /**
-   * Runs the given GDAL command and arguments (as defined in the task config), validating the command is valid first
+   * Runs the given GDAL command and arguments (as defined in the task config), validating the
+   * command is valid first
    * @param {String} command The name of the gdal command to run, e.g. "gdalinfo"
    * @param {Array<string>} args An array of string arguments to pass to the gdal command
    */
   runGdalCommand(command, args) {
-    const program = `bin/${command}`;
-    if (!program.match(/^bin\/[a-z0-9\-_]+$/g) || !fs.existsSync(program)) {
+    const program = path.resolve(process.cwd(), 'bin', command);
+    if (!command.match(/^[a-z0-9\-_]+$/g) || !fs.existsSync(program)) {
+      if (!fs.existsSync(program)) {
+        throw new Error(`this happened: ${__dirname}, ${program}`);
+      }
       throw new Error(`Invalid gdal_command: ${command}`);
     }
-    const process = spawn(program, args || [], { stdio: 'inherit' });
+    return this.promiseSpawn(program, args);
+  }
+
+  promiseSpawn(program, args) {
+    log.info(`Spawning: ${program} "${args.join('", "')}"`);
+    const process = spawn(program, args || [], { stdio: 'inherit', cwd: '/tmp' });
     return new Promise((resolve, reject) => {
       process.on('close', (code) => {
         if (code === 0) {
