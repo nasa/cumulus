88d269c0de9f01a349e7e5cc492df4490e6fd6f3
Marc Huffnagle <marc@element84.com>
2017-11-22 10:03:25 -0500

    Move the "uuid" function to common/util.js and use the already-installed "uuid" module.

--- packages/common/string.js
+++ packages/common/string.js
@@ -7,10 +7,6 @@
 const unicodeEscapeCharacter = (char) =>
   ['\\u', `0000${char.charCodeAt().toString(16)}`.slice(-4)].join('');
 
-// https://gist.github.com/jed/982883
-// eslint-disable-next-line
-function uuid(a){return a?(a^Math.random()*16>>a/4).toString(16):([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g,uuid)}
-
 module.exports = {
   /**
    * Given a string, replaces all characters matching the passed regex with their unicode
@@ -20,6 +16,5 @@ module.exports = {
    * @param{string} regex - The regex matching characters to replace (default: all chars)
    * @return{string} - The string with characters unicode-escaped
    */
-  unicodeEscape: (str, regex = /[\s\S]/g) => str.replace(regex, unicodeEscapeCharacter),
-  uuid: uuid
+  unicodeEscape: (str, regex = /[\s\S]/g) => str.replace(regex, unicodeEscapeCharacter)
 };
