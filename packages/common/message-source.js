e7364a2d1df627b3c06b5240405140ea74523e7c
Marc Huffnagle <marc@element84.com>
2017-11-22 10:03:25 -0500

    Move the "uuid" function to common/util.js and use the already-installed "uuid" module.

--- packages/common/message-source.js
+++ packages/common/message-source.js
@@ -3,7 +3,7 @@
 const readline = require('readline');
 const aws = require('./aws');
 const log = require('./log');
-const { uuid } = require('./string');
+const { uuid } = require('./util');
 
 // Maximum message payload size that will NOT be stored in S3. Anything bigger will be.
 const MAX_NON_S3_PAYLOAD_SIZE = 10000;
