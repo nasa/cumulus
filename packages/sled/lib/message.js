4fdae11947702b106dfa36ca61c53a72a6246aa9
Patrick Quinn <patrick@element84.com>
2017-11-22 10:52:31 -0500

    CUMULUS-153: Fix UUID implementation

--- packages/sled/lib/message.js
+++ packages/sled/lib/message.js
@@ -315,7 +315,7 @@ function assignOutputs(nestedResponse, event, messageConfig) {
 
 // https://gist.github.com/jed/982883
 // eslint-disable-next-line
-function uuid(a){return a?(a^Math.random()*16>>a/4).toString(16):([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g,b)}
+function uuid(a){return a?(a^Math.random()*16>>a/4).toString(16):([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g,uuid)}
 
 // Maximum message payload size that will NOT be stored in S3. Anything bigger will be.
 const MAX_NON_S3_PAYLOAD_SIZE = 10000;
