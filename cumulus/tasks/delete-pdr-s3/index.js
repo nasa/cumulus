20ef388fd1adfcc20c9a5e6973254da41419f62a
Marc Huffnagle <marc@element84.com>
2017-12-14 09:34:03 -0500

    Fix comment for delete-pdr-s3.handler

--- cumulus/tasks/delete-pdr-s3/index.js
+++ cumulus/tasks/delete-pdr-s3/index.js
@@ -6,16 +6,16 @@ const aws = require('@cumulus/common/aws');
  * Delete a PDR object from S3.
  *
  * For documentation about the format of the incoming event, see
- * https://github.com/cumulus-nasa/cumulus/blob/message-updates/packages/sled/README.md
- *
- * @todo Fix the preceding comment to point to master
+ * https://github.com/cumulus-nasa/cumulus/blob/master/packages/sled/README.md
  *
  * @param {Object} event - a cumulus sled event
  *
  * @example
- * handler({ input:
- *   bucket: 'my-bucket',
- *   key: 'my/key.pdr'
+ * handler({
+ *  input: {
+ *    bucket: 'my-bucket',
+ *    key: 'my/key.pdr'
+ *  }
  * }).then(() => console.log('PDR deleted'));
  */
 function handler(event, context, callback) {
