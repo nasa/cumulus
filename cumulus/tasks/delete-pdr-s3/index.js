8204395441e4853a777c34c68bed0bebae5a731b
Marc Huffnagle <marc@element84.com>
2017-12-15 17:41:57 -0500

    delete-pdr-s3 updates based on James's feedback

--- cumulus/tasks/delete-pdr-s3/index.js
+++ cumulus/tasks/delete-pdr-s3/index.js
@@ -5,26 +5,30 @@ const aws = require('@cumulus/common/aws');
 /**
  * Delete a PDR object from S3.
  *
- * For documentation about the format of the incoming event, see
- * https://github.com/cumulus-nasa/cumulus/blob/master/packages/sled/README.md
+ * @param {Object} s3Object - an Object with "Bucket" and "Key" properties
+ * @returns {Promise}
+ */
+function deletePdr(s3Object) {
+  return aws.deleteS3Files([s3Object]);
+}
+
+/**
+ * @param {Object} event - see https://github.com/cumulus-nasa/cumulus/blob/master/packages/sled/README.md
+ * @param {Object} context - see https://docs.aws.amazon.com/lambda/latest/dg/nodejs-prog-model-context.html
+ * @param {Function} callback - see https://docs.aws.amazon.com/lambda/latest/dg/nodejs-prog-model-handler.html#nodejs-prog-model-handler-callback
+ * @returns {Promise}
  *
- * @param {Object} event - a cumulus sled event
+ * Example event:
  *
- * @example
- * handler({
+ * {
  *  input: {
- *    bucket: 'my-bucket',
- *    key: 'my/key.pdr'
+ *    Bucket: 'my-bucket',
+ *    Key: 'my/key.pdr'
  *  }
- * }).then(() => console.log('PDR deleted'));
+ * }
  */
 function handler(event, context, callback) {
-  return aws.deleteS3Files([
-    {
-      Bucket: event.input.bucket,
-      Key: event.input.key
-    }
-  ])
+  return deletePdr(event.input)
     .then(() => callback())
     .catch(callback);
 }
