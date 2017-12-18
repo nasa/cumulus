2b4ad4c4529b5a5926aaa9c865ab663ad2bb8c00
Marc Huffnagle <marc@element84.com>
2017-12-18 14:07:41 -0500

    Restore accidentally removed file

--- cumulus/tasks/discover-granules/tests/test.js
+++ cumulus/tasks/discover-granules/tests/test.js
@@ -2,11 +2,13 @@
 
 import test from 'ava';
 import sinon from 'sinon';
+import { handler as sled } from '@cumulus/sled';
 import log from '@cumulus/common/log';
 import amsr2 from '@cumulus/test-data/payloads/amsr2/discover.json';
 import queue from '@cumulus/ingest/queue';
 import { S3 } from '@cumulus/ingest/aws';
 import mur from './fixtures/mur.json';
+import sledInput from './fixtures/sled-input.json';
 import { handler } from '../index';
 
 test.cb('test discovering mur granules', (t) => {
@@ -16,9 +18,6 @@ test.cb('test discovering mur granules', (t) => {
 
   // make sure queue is not used
   newMur.config.useQueue = false;
-  // update discovery rule
-  const rule = '/allData/ghrsst/data/GDS2/L4/GLOB/JPL/MUR/v4.1/2017/(20[1-3])';
-  newMur.config.collection.meta.provider_path = rule;
 
   handler(newMur, {}, (e, output) => {
     if (e && e.message.includes('getaddrinfo ENOTFOUND')) {
@@ -35,7 +34,7 @@ test.cb('test discovering mur granules', (t) => {
   });
 });
 
-test.skip('test discovering mur granules with queue', (t) => {
+test.cb('test discovering mur granules with queue', (t) => {
   const newMur = Object.assign({}, mur);
   sinon.stub(queue, 'queueGranule').callsFake(() => true);
   sinon.stub(S3, 'fileExists').callsFake(() => false);
@@ -45,7 +44,7 @@ test.skip('test discovering mur granules with queue', (t) => {
   newMur.config.useQueue = true;
   newMur.config.collection.meta.provider_path = rule;
 
-  handler(newMur, {}, (e, r) => {
+  handler(newMur, {}, (e, output) => {
     if (e && e.message.includes('getaddrinfo ENOTFOUND')) {
       log.info('ignoring this test. Test server seems to be down');
     }
@@ -66,22 +65,38 @@ test.cb('test discovering amsr2 granules using SFTP', (t) => {
   }
   else {
     const payload = Object.assign({}, amsr2);
-    payload.provider.host = process.env.JAXA_HOST;
-    payload.provider.port = process.env.JAXA_PORT;
-    payload.provider.username = process.env.JAXA_USER;
-    payload.provider.password = process.env.JAXA_PASS;
-    payload.provider.encrypted = true;
+    payload.config.provider.host = process.env.JAXA_HOST;
+    payload.config.provider.port = process.env.JAXA_PORT;
+    payload.config.provider.username = process.env.JAXA_USER;
+    payload.config.provider.password = process.env.JAXA_PASS;
+    payload.config.provider.encrypted = true;
 
     // update discovery rule
-    payload.meta.useQueue = false;
-    payload.payload = {};
+    payload.config.useQueue = false;
+    payload.input = {};
 
-    handler(payload, {}, (e, r) => {
-      const granules = r.payload.granules;
+    handler(payload, {}, (e, output) => {
+      const granules = output.granules;
       t.true(Object.keys(granules).length > 5);
       const g = Object.keys(granules)[0];
       t.is(granules[g].files.length, 1);
       t.end(e);
     });
   }
+});
+
+test.cb('test running in sled', (t) => {
+  sinon.stub(S3, 'fileExists').callsFake(() => false);
+
+  function callback (err, output) {
+    t.ifError(err);
+    const granules = output.payload.granules;
+    t.is(Object.keys(granules).length, 3);
+    const g = Object.keys(granules)[0];
+    t.is(granules[g].files.length, 2);
+    S3.fileExists.restore();
+    t.end();
+  }
+
+  sled(sledInput, {}, callback, handler)
 });
