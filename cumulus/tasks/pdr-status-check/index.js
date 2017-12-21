397eac98855b6944cc287b170d4f396d9f1b3206
sethvincent <sethvincent@gmail.com>
2017-12-21 14:10:57 -0500

    port pdr-status-check to new message schema

--- cumulus/tasks/pdr-status-check/index.js
+++ cumulus/tasks/pdr-status-check/index.js
@@ -8,102 +8,110 @@ let log = require('@cumulus/ingest/log');
 
 log = log.child({ file: 'pdr-status-check/index.js' });
 
-module.exports.handler = function handler(_event, context, cb) {
+function pullEvent(_event, cb) {
   let event;
   let counter;
   let limit;
   let isFinished;
-  try {
-    StepFunction.pullEvent(_event).then((ev) => {
-      event = ev;
-      const pdrName = get(event, 'payload.pdr.name');
-      log = log.child({ pdrName });
 
-      counter = get(event, 'payload.counter', 0);
-      limit = get(event, 'payload.limit', 30);
-      isFinished = get(event, 'payload.isFinished', false);
-      const running = get(event, 'payload.running', []);
+  function handleError(e) {
+    log.error(e);
+    cb(e);
+  }
 
-      // if finished, exit
-      if (isFinished) {
-        log.info('pdr is already finished. Exiting...');
-        return cb(null, event);
-      }
+  function checkStatus(ev) {
+    event = ev;
 
-      // if this is tried too many times, exit
-      if (counter > limit) {
-        const err = new IncompleteError(`PDR didn't complete after ${counter} checks`);
-        throw err;
-      }
+    const pdrName = get(event, 'payload.pdr.name');
+    log = log.child({ pdrName });
 
-      return Promise.all(running.map(arn => StepFunction.getExecution(arn, true)));
-    }).then((r) => {
-      const completed = get(event, 'payload.completed', []);
-      const failed = get(event, 'payload.failed', []);
-      const running = [];
+    counter = get(event, 'payload.counter', 0);
+    limit = get(event, 'payload.limit', 30);
+    isFinished = get(event, 'payload.isFinished', false);
+    const runningExecutions = get(event, 'payload.running', []);
 
-      r.forEach((sf) => {
-        if (sf.status === 'SUCCEEDED') {
-          completed.push(sf.executionArn);
-        }
-        else if (sf.status === 'FAILED') {
-          const output = get(sf, 'output', '{ "exception": "Workflow Failed" }');
-          failed.push({
-            arn: sf.executionArn,
-            reason: JSON.parse(output).exception
-          });
-        }
-        else if (sf.status === 'ABORTED') {
-          const output = get(sf, 'output', '{ "exception": "Workflow Aborted" }');
-          failed.push({
-            arn: sf.executionArn,
-            reason: JSON.parse(output).exception
-          });
+    // if finished, exit
+    if (isFinished) {
+      log.info('pdr is already finished. Exiting...');
+      return cb(null, event);
+    }
+
+    // if this is tried too many times, exit
+    if (counter >= limit) {
+      const err = new IncompleteError(`PDR didn't complete after ${counter} checks`);
+      handleError(err);
+    }
+
+    // update the status of each previously running execution
+    function updateStatus(arn) {
+      return StepFunction.getExecution(arn, true).then((r) => {
+        const completed = get(event, 'payload.completed', []);
+        const failed = get(event, 'payload.failed', []);
+        const running = [];
+
+        r.forEach((sf) => {
+          if (sf.status === 'SUCCEEDED') {
+            completed.push(sf.executionArn);
+          }
+          else if (sf.status === 'FAILED') {
+            const output = get(sf, 'output', '{ "exception": "Workflow Failed" }');
+            failed.push({
+              arn: sf.executionArn,
+              reason: JSON.parse(output).exception
+            });
+          }
+          else if (sf.status === 'ABORTED') {
+            const output = get(sf, 'output', '{ "exception": "Workflow Aborted" }');
+            failed.push({
+              arn: sf.executionArn,
+              reason: JSON.parse(output).exception
+            });
+          }
+          else {
+            running.push(sf.executionArn);
+          }
+        });
+
+        if (running.length === 0) {
+          isFinished = true;
+          event.payload.isFinished = isFinished;
+          event.payload.running = running.length;
+          event.payload.failed = failed.length;
+          event.payload.completed = completed.length;
         }
         else {
-          running.push(sf.executionArn);
+          isFinished = false;
+          event.payload.isFinished = isFinished;
+          log.info({
+            running: running.length,
+            completed: completed.length,
+            failed: failed.length,
+            counter,
+            limit
+          }, 'latest status');
+
+          event.payload.counter = counter + 1;
+          event.payload.limit = limit;
+          event.payload.running = running;
+          event.payload.completed = completed;
+          event.payload.failed = failed;
         }
+
+        return StepFunction.pushEvent(event)
+          .then(() => cb(null, event))
+          .catch(handleError);
       });
+    }
 
-      if (running.length === 0) {
-        isFinished = true;
-        event.payload.isFinished = isFinished;
-        event.payload.running = running.length;
-        event.payload.failed = failed.length;
-        event.payload.completed = completed.length;
-      }
-      else {
-        isFinished = false;
-        event.payload.isFinished = isFinished;
-        log.info({
-          running: running.length,
-          completed: completed.length,
-          failed: failed.length,
-          counter,
-          limit
-        }, 'latest status');
-
-        event.payload.counter = counter + 1;
-        event.payload.limit = limit;
-        event.payload.running = running;
-        event.payload.completed = completed;
-        event.payload.failed = failed;
-      }
-
-      return StepFunction.pushEvent(event);
-    }).then(ev => {
-      if (ev.s3_path) {
-        ev.payload = { isFinished };
-      }
-      cb(null, ev);
-    }).catch(e => { //eslint-disable-line newline-per-chained-call
-      log.error(e);
-      cb(e);
-    });
+    return Promise.all(runningExecutions.map(updateStatus))
+      .catch(handleError);
   }
-  catch (e) {
-    log.error(e);
-    throw e;
-  }
+
+  StepFunction.pullEvent(_event)
+    .then(checkStatus)
+    .catch(handleError);
+}
+
+module.exports.handler = function handler(_event, context, cb) {
+  return pullEvent(_event, cb);
 };
-
