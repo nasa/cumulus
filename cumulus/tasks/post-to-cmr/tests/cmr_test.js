d74f785684cf44a2f35730e262c31e5f5675a1ee
Scisco <scisco7@gmail.com>
2018-01-08 19:52:41 -0500

    make tests localstack comptabile

--- cumulus/tasks/post-to-cmr/tests/cmr_test.js
+++ cumulus/tasks/post-to-cmr/tests/cmr_test.js
@@ -1,8 +1,11 @@
 'use strict';
 
+import fs from 'fs';
 import test from 'ava';
 import sinon from 'sinon';
-import { S3 } from '@cumulus/ingest/aws';
+import aws from '@cumulus/common/aws';
+import testUtils from '@cumulus/common/test-utils';
+
 import cmrjs from '@cumulus/cmrjs';
 import payload from './data/payload.json';
 import { handler } from '../index';
@@ -11,17 +14,38 @@ const result = {
   'concept-id': 'testingtesging'
 };
 
-test.before(() => {
-  sinon.stub(S3, 'get').callsFake(() => ({ Body: '<xml></xml>' }));
+async function deleteBucket(bucket) {
+  const response = await aws.s3().listObjects({ Bucket: bucket }).promise();
+  const keys = response.Contents.map((o) => o.Key);
+  await Promise.all(keys.map((key) =>
+    aws.s3().deleteObject({ Bucket: bucket, Key: key }).promise()
+  ));
+}
+
+test.beforeEach((t) => {
+  t.context.bucket = testUtils.randomString();
+  return aws.s3().createBucket({ Bucket: t.context.bucket }).promise();
+});
+
+test.afterEach.always(async (t) => {
+  deleteBucket(t.context.bucket);
 });
 
 test.cb.serial('should succeed if cmr correctly identifies the xml as invalid', (t) => {
   sinon.stub(cmrjs.CMR.prototype, 'getToken');
-  const newPayload = JSON.parse(JSON.stringify(payload));
-  handler(newPayload, {}, (e) => {
-    cmrjs.CMR.prototype.getToken.restore();
-    t.true(e instanceof cmrjs.ValidationError);
-    t.end();
+
+  aws.promiseS3Upload({
+    Bucket: t.context.bucket,
+    Key: 'meta.cmr.xml',
+    Body: '<?xml version="1.0" encoding="UTF-8"?><results></results>'
+  }).then(() => {
+    const newPayload = JSON.parse(JSON.stringify(payload));
+    newPayload.input.granules[0].files.push({ filename: `s3://${t.context.bucket}/meta.cmr.xml` });
+    handler(newPayload, {}, (e) => {
+      cmrjs.CMR.prototype.getToken.restore();
+      t.true(e instanceof cmrjs.ValidationError);
+      t.end();
+    });
   });
 });
 
@@ -30,14 +54,22 @@ test.cb.serial('should succeed with correct payload', (t) => {
   sinon.stub(cmrjs.CMR.prototype, 'ingestGranule').callsFake(() => ({
     result
   }));
-  handler(newPayload, {}, (e, output) => {
-    cmrjs.CMR.prototype.ingestGranule.restore();
-    t.is(e, null);
-    t.is(
-      output.granules[0].cmr.link,
-      `https://cmr.uat.earthdata.nasa.gov/search/granules.json?concept_id=${result['concept-id']}`
-    );
-    t.end(e);
+
+  aws.promiseS3Upload({
+    Bucket: t.context.bucket,
+    Key: 'meta.cmr.xml',
+    Body: fs.createReadStream('tests/data/meta.xml')
+  }).then(() => {
+    newPayload.input.granules[0].files.push({ filename: `s3://${t.context.bucket}/meta.cmr.xml` });
+    handler(newPayload, {}, (e, output) => {
+      cmrjs.CMR.prototype.ingestGranule.restore();
+      t.is(e, null);
+      t.is(
+        output.granules[0].cmr.link,
+        `https://cmr.uat.earthdata.nasa.gov/search/granules.json?concept_id=${result['concept-id']}`
+      );
+      t.end(e);
+    });
   });
 });
 
@@ -46,7 +78,7 @@ test.cb.serial('Should skip cmr step if the metadata file uri is missing', (t) =
   newPayload.input.granules = [{
     granuleId: 'some granule',
     files: [{
-      filename: 's3://path/to/file.xml'
+      filename: `s3://${t.context.bucket}/to/file.xml`
     }]
   }];
 
@@ -56,6 +88,6 @@ test.cb.serial('Should skip cmr step if the metadata file uri is missing', (t) =
   });
 });
 
-test.after(() => {
-  S3.get.restore();
-});
+// test.after(() => {
+//   S3.get.restore();
+// });
