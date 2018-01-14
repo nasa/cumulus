60f9aabdcfc8125f47377cdbb7e084ebfc64477c
cappelaere <pat@cappelaere.com>
2018-01-14 10:16:14 -0500

    Added validateParameters, Updates tests and code cleanup (ESLINT/JSDocs)

---
+++ cumulus/tasks/discover-cmr-granules/webpack.config.js
@@ -0,0 +1,31 @@
+module.exports = {
+  entry: ['babel-polyfill', './index.js'],
+  output: {
+    libraryTarget: 'commonjs2',
+    filename: 'dist/index.js'
+  },
+  target: 'node',
+  devtool: 'sourcemap',
+  externals: [
+    'aws-sdk',
+    'electron'
+  ],
+  module: {
+    resolve: {
+      alias: {
+        'aws-sdk': 'aws-sdk/dist/aws-sdk'
+      }
+    },
+    noParse: [
+      /graceful-fs\/fs.js/
+    ],
+    loaders: [{
+      test: /\.js?$/,
+      exclude: /node_modules(?!\/@cumulus)/,
+      loader: 'babel'
+    }, {
+      test: /\.json$/,
+      loader: 'json'
+    }]
+  }
+};
