a8756085fe09e0840fffbe95b27baa179b18fa4a
cappelaere <pat@cappelaere.com>
2018-01-14 10:16:14 -0500

    Added validateParameters, Updates tests and code cleanup (ESLINT/JSDocs)

--- cumulus/tasks/discover-cmr-granules/test/discover-cmr-granules-spec.js
+++ cumulus/tasks/discover-cmr-granules/test/discover-cmr-granules-spec.js
@@ -1,21 +1,54 @@
 'use strict';
-const expect = require('expect.js');
-const helpers = require('@cumulus/common/test-helpers');
-const DiscoverCmrGranulesTask = require('../index');
 
-xdescribe('discover-cmr-granules.handler', () => {
-  let result;
-  beforeEach(async (done) => {
-    try {
-      result = null;
-      const message = helpers.collectionMessageInput('VNGCR_LQD_C1', 'DiscoverCmrGranules');
-      result = await helpers.run(DiscoverCmrGranulesTask, message);
-    }
-    finally {
-      done();
-    }
-  });
+import test from 'ava';
+//import { handler } from '../index';
 
-  it('TODO', () => {
-  });
+const localHelpers = require('@cumulus/common/local-helpers');
+const testHelpers = require('@cumulus/common/test-helpers');
+const _ = require('lodash');
+
+const DiscoverCmrGranules = require('../index');
+
+const localTaskName = 'DiscoverCmrGranules';
+const message = localHelpers.collectionMessageInput('MOPITT_DCOSMR_LL_D_STD', localTaskName)();
+
+test('check with valid parameters', async (t) => {
+  const [error] = await testHelpers.run(DiscoverCmrGranules, message);
+  t.is(error, null);
+});
+
+test('check with invalid query parameters', async (t) => {
+  // Remove query parameter for  DiscoverCmrGranules Task
+  const newPayload = _.cloneDeep(message); //Object assign will not work here
+  delete newPayload.workflow_config_template.DiscoverCmrGranules.query;
+
+  const [errors] = await testHelpers.run(DiscoverCmrGranules, newPayload);
+  t.is(errors, 'Undefined query parameters');
+});
+
+test('check with invalid root parameter', async (t) => {
+  // Remove root parameter for  DiscoverCmrGranules Task
+  const newPayload = _.cloneDeep(message); //Object assign will not work here
+  delete newPayload.workflow_config_template.DiscoverCmrGranules.root;
+
+  const [errors] = await testHelpers.run(DiscoverCmrGranules, newPayload);
+  t.is(errors, 'Undefined root parameter');
+});
+
+test('check with invalid event parameter', async (t) => {
+  // Remove event parameter for  DiscoverCmrGranules Task
+  const newPayload = _.cloneDeep(message); //Object assign will not work here
+  delete newPayload.workflow_config_template.DiscoverCmrGranules.event;
+
+  const [errors] = await testHelpers.run(DiscoverCmrGranules, newPayload);
+  t.is(errors, 'Undefined event parameter');
+});
+
+test('check with invalid granule_meta parameter', async (t) => {
+  // Remove granule_meta parameter for  DiscoverCmrGranules Task
+  const newPayload = _.cloneDeep(message); //Object assign will not work here
+  delete newPayload.workflow_config_template.DiscoverCmrGranules.granule_meta;
+
+  const [errors] = await testHelpers.run(DiscoverCmrGranules, newPayload);
+  t.is(errors, 'Undefined granule_meta parameters');
 });
