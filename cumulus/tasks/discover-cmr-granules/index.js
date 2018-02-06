22ff14014cd43ad69c84a02a9d50040b6d1cc0dc
Scisco <scisco7@gmail.com>
2018-02-06 13:07:13 -0500

    merge latest master changes to message-updates

--- cumulus/tasks/discover-cmr-granules/index.js
+++ cumulus/tasks/discover-cmr-granules/index.js
@@ -5,9 +5,34 @@ const fetch = require('node-fetch');
 const log = require('@cumulus/common/log');
 const Task = require('@cumulus/common/task');
 const FieldPattern = require('@cumulus/common/field-pattern');
+const docClient = require('@cumulus/common/aws').dynamodbDocClient();
 const querystring = require('querystring');
+const moment = require('moment');
 
-const PAGE_SIZE = 2000;
+/**
+ * Validate presence of parameter in config
+ *
+ * @param {hash} config - Configuration
+ * @param {string} param - Param to test
+ * @returns {bool} - true or will throw an exception if required
+ */
+function validateParameter(config, param) {
+  if (config[param]) return true;
+  throw new Error(`Undefined ${param} parameter`);
+}
+
+/**
+ * Validate required parameters in Config
+ *
+ * @param {hash} config - config to test
+ * @returns {bool} - true or exception
+ */
+function validateParameters(config) {
+  const params = ['root', 'event', 'granule_meta', 'query'];
+  for (const p of params) validateParameter(config, p);
+
+  return true;
+}
 
 /**
  * Task which discovers granules by querying the CMR
@@ -16,36 +41,71 @@ const PAGE_SIZE = 2000;
  *                 for each discovered granule
  */
 module.exports = class DiscoverCmrGranulesTask extends Task {
-  /**
+    /**
    * Main task entrypoint
-   * @return An array of CMR granules that need ingest
+   *
+   * @returns {Array} -- An array of CMR granules that need ingest
    */
   async run() {
-    const query = this.config.query || {};
+    validateParameters(this.config);
+    const query = this.config.query;
     if (query.updated_since) {
       query.updated_since = new Date(Date.now() - parseDuration(query.updated_since)).toISOString();
     }
-    const granules = await this.cmrGranules(this.config.root, query);
+    this.message.payload = this.message.payload || { scrollID: null };
+
+    const { scrollID, granules } = await this.cmrGranules(
+      this.config.root,
+      query,
+      this.message.payload.scrollID);
     const messages = this.buildMessages(granules, this.config.granule_meta, this.message.meta);
     const filtered = this.excludeFiltered(messages, this.config.filtered_granule_keys);
-    return filtered;
+
+    // Write the messages to a DynamoDB table so we can track ingest failures
+    const messagePromises = filtered.map(msg => {
+      const { granuleId, version, collection } = msg.meta;
+      const params = {
+        TableName: this.config.ingest_tracking_table,
+        Item: {
+          'granule-id': granuleId,
+          'version': version,
+          'collection': collection,
+          'ingest-start-datetime': moment().format(),
+          'message': JSON.stringify(msg)
+        }
+      };
+      return docClient.put(params).promise();
+    });
+
+    await Promise.all(messagePromises);
+
+    return { messages: filtered, scrollID: scrollID };
   }
 
   /**
    * excludeFiltered - Excludes messages that do not match one of the specified granuleFilter.
    * Allows all messages if matchingKeys is null.
+   *
+   * @param {Hash} messages - messages
+   * @param {Hash} granuleFilter - granuleFilter
+   * @returns {Hash} - Filtered messages
    */
   excludeFiltered(messages, granuleFilter) {
+    /**
+     * Filter Function to be used
+     *
+     * @returns {Hash} - Filtered messages
+     */
     let filterFn = () => true;
     if (granuleFilter) {
       if (granuleFilter.filtered_granule_keys) {
         const keySet = new Set(granuleFilter.filtered_granule_keys);
-        filterFn = msg => keySet.has(msg.meta.key);
+        filterFn = (msg) => keySet.has(msg.meta.key);
       }
       else if (granuleFilter.filtered_granule_key_start) {
         const start = granuleFilter.filtered_granule_key_start;
         const end = granuleFilter.filtered_granule_key_end;
-        filterFn = msg => msg.meta.key >= start && msg.meta.key <= end;
+        filterFn = (msg) => msg.meta.key >= start && msg.meta.key <= end;
       }
     }
     return messages.filter(filterFn);
@@ -53,44 +113,47 @@ module.exports = class DiscoverCmrGranulesTask extends Task {
 
   /**
    * Returns CMR granules updated after the specified date
+   *
    * @param {string} root - The CMR root url (protocol and domain without path)
-   * @param {object} query - The query parameters to serialize and send to a CMR granules search
-   * @return An array of all granules matching the given query
+   * @param {Object} query - The query parameters to serialize and send to a CMR granules search
+   * @returns {Array} An array of all granules matching the given query
    */
-  async cmrGranules(root, query) {
+  async cmrGranules(root, query, scrollID) {
     const granules = [];
-    const params = Object.assign({
-      page_size: PAGE_SIZE,
-      sort_key: 'revision_date'
-    }, query);
+    const params = Object.assign({}, query);
+    if (params.updated_since) params.sort_key = 'revision_date';
+    params.scroll = 'true';
     const baseUrl = `${root}/search/granules.json`;
     const opts = { headers: { 'Client-Id': 'GitC' } };
-    let done = false;
-    let page = 1;
-    while (!done) {
-      params.page_num = page;
-      const url = [baseUrl, querystring.stringify(params)].join('?');
-      log.info('Fetching:', url);
-      const response = await fetch(url, opts);
-      if (!response.ok) {
-        throw new Error(`CMR Error ${response.status} ${response.statusText}`);
-      }
-      const json = await response.json();
-      granules.push(...json.feed.entry);
-      const hits = parseInt(response.headers.get('CMR-Hits'), 10);
-      if (page === 1) log.info(`CMR Granule count: ${hits}`);
-      done = hits <= page * PAGE_SIZE;
-      page++;
+    const url = [baseUrl, querystring.stringify(params)].join('?');
+    if (scrollID) opts.headers['CMR-Scroll-Id'] = scrollID;
+    log.info('Fetching:', url);
+    const response = await fetch(url, opts);
+    if (!response.ok) {
+      throw new Error(`CMR Error ${response.status} ${response.statusText}`);
     }
-    return granules;
+    const json = await response.json();
+    log.info(json);
+    granules.push(...json.feed.entry);
+    log.info(`scrollID:${scrollID}`,
+             `cmr-scroll-id:${response.headers._headers['cmr-scroll-id']}`,
+             `json.feed.entry.length:${json.feed.entry.length}`);
+    const nextScrollID = (json.feed.entry.length === 0) ?
+                         false :
+                         response.headers._headers['cmr-scroll-id'];
+    log.info(`nextScrollID:${nextScrollID}`);
+    log.info('----TOTAL----: ', granules.length);
+    return { granules: granules, scrollID: nextScrollID };
   }
 
   /**
    * Builds the output array for the task
-   * @param {array} granules - The granules to output
-   * @param {object} opts - The granule_meta object passed to the task config
-   * @param {object} fieldValues - Field values to apply to the granule_meta (the incoming message)
-   * @return An array of meta objects for each granule created as specified in the task config
+   *
+   * @param {Array} granules - The granules to output
+   * @param {Object} opts - The granule_meta object passed to the task config
+   * @param {Object} fieldValues - Field values to apply to the granule_meta (the incoming message)
+   * @returns {Array} An array of meta objects for each granule created as specified
+   * in the task config
    */
   buildMessages(granules, opts, fieldValues) {
     if (!opts) return granules;
@@ -119,18 +182,21 @@ module.exports = class DiscoverCmrGranulesTask extends Task {
 
   /**
    * Entrypoint for Lambda
-   * @param {array} args The arguments passed by AWS Lambda
-   * @return The handler return value
+   *
+   * @param {Array} args - The arguments passed by AWS Lambda
+   * @returns {Hash} -
+   *
+   *  The handler return value
    */
   static handler(...args) {
     return DiscoverCmrGranulesTask.handle(...args);
   }
 };
 
-// To run a small test:
-// node discover-cmr-granules local
-
+// To use with Visual Studio Code Debugger, uncomment next block
+//
+global.__isDebug = true;
 const local = require('@cumulus/common/local-helpers');
 const localTaskName = 'DiscoverCmrGranules';
-local.setupLocalRun(module.exports.handler,
-                    local.collectionMessageInput('MOPITT_DCOSMR_LL_D_STD', localTaskName));
+local.setupLocalRun(module.exports.handler, local.collectionMessageInput(
+  'MOPITT_DCOSMR_LL_D_STD', localTaskName));
