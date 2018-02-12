b498355cfe15368a0051d3c0d9322cf076dcdcfc
Scisco <scisco7@gmail.com>
2018-02-12 15:44:13 -0500

    Convert workflows.yml from a list to an object

--- packages/deployment/test/test.js
+++ packages/deployment/test/test.js
@@ -21,3 +21,48 @@ test('test cumulus message syntax fix', (t) => {
   t.is(testObj.someKey, returnObj.someKey);
   t.is(testObj.obj.key1, returnObj.obj.key1);
 });
+
+test('handing CumulusConfig in workflows', (t) => {
+  const getConfig = kes.extractCumulusConfigFromSF;
+  const testObj = {
+    stepFunctions: {
+      DiscoverPdrs: {
+        States: {
+          MyTask1: {
+            CumulusConfig: {
+              useQueue: true,
+              collection: '{$.meta.collection}'
+            }
+          },
+          MyTask2: {}
+        }
+      },
+      ParsePdr: {
+        States: {
+          Mytask1: {}
+        }
+      }
+    }
+  };
+
+  const returnObj = getConfig(JSON.parse(JSON.stringify(testObj)));
+
+  // make sure there is workflowConfig key
+  t.truthy(returnObj.workflowConfigs);
+
+  // make sure CumulusConfig is removed from all states
+  Object.keys(returnObj.stepFunctions).forEach((name) => {
+    const sf = returnObj.stepFunctions[name];
+    Object.keys(sf.States).forEach((state) => t.falsy(sf.States[state].CumulusConfig));
+
+    // make sure there is a workflowConfig for each SF and its tasks
+    t.truthy(returnObj.workflowConfigs[name]);
+    Object.keys(sf.States).forEach((state) => t.truthy(returnObj.workflowConfigs[name][state]));
+  });
+
+  // test the value one of the workflowConfigs
+  t.is(
+    `{${testObj.stepFunctions.DiscoverPdrs.States.MyTask1.CumulusConfig.collection}}`,
+    returnObj.workflowConfigs.DiscoverPdrs.MyTask1.collection
+  );
+});
