e52ff3e64b1512f0930e3776e2b3627f40246bec
Katherine Kelly <katherine.a.kelly@nasa.gov>
2018-02-13 17:36:58 -0500

    addressing some comments

--- packages/common/log.js
+++ packages/common/log.js
@@ -5,7 +5,7 @@
  *
  * @param {string} level - type of log (info, error, debug, warn)
  * @param {string} args - Message to log and any other information
- * @returns {JSON} - the JSON to be logged
+ * @returns {undefined} - the JSON to be logged
  */
 function log(level, args) {
   const executions = process.env.EXECUTIONS; //set in handler from cumulus_meta
@@ -20,7 +20,7 @@ function log(level, args) {
     level: level
   };
   for (const arg of args) {
-    if ((typeof arg) === 'string') message += arg;
+    if ((typeof arg) === 'string') message = message + arg + ' ';
     else {
       for (const key in arg) {
         output[key] = arg[key];
@@ -29,15 +29,15 @@ function log(level, args) {
   }
 
   output.message = message;
-
-  console.log(output);
+  if (level === 'error') console.error(output);
+  else console.log(output);
 }
 
 /**
  * Logs the message
  *
  * @param {string} args - Includes message and any other information to log
- * @returns {JSON} - the JSON to be logged
+ * @returns {undefined} - the JSON to be logged
  */
 function info(...args) {
   return log('info', args);
@@ -47,7 +47,7 @@ function info(...args) {
  * Logs the error
  *
  * @param {Object} args - Includes error and any other information to log
- * @returns {JSON} - the JSON to be logged
+ * @returns {undefined} - the JSON to be logged
  */
 function error(...args) {
   log('error', args);
@@ -57,7 +57,7 @@ function error(...args) {
  * Logs the debugger messsages
  *
  * @param {Object} args - Includes debugger message and any other information to log
- * @returns {JSON} - the JSON to be logged
+ * @returns {undefined} - the JSON to be logged
  */
 function debug(...args) {
   log('debug', args);
@@ -67,13 +67,34 @@ function debug(...args) {
  * Logs the Warning messsage
  *
  * @param {Object} args - Includes Warn message and any other information to log
- * @returns {JSON} - the JSON to be logged
+ * @returns {undefined} - the JSON to be logged
  */
 function warn(...args) {
   log('warn', args);
+}
+
+/**
+ * Logs the Fatal messsage
+ *
+ * @param {Object} args - Includes Fatal message and any other information to log
+ * @returns {undefined} - the JSON to be logged
+ */
+function fatal(...args) {
+  log('fatal', args);
+}
+/**
+ * Logs the Trace messsage
+ *
+ * @param {Object} args - Includes Trace message and any other information to log
+ * @returns {undefined} - the JSON to be logged
+ */
+function trace(...args) {
+  log('trace', args);
 }
 
 module.exports.info = info;
 module.exports.error = error;
 module.exports.debug = debug;
 module.exports.warn = warn;
+module.exports.fatal = fatal;
+module.exports.trace = trace;
