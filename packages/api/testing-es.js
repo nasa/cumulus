a184b98dabc84d5bba9ddc5c3ca1b02d97c7fe69
Aimee Barciauskas <aimee@developmentseed.org>
2018-02-13 09:53:51 -0700

    Remove obsolete test file

--- packages/api/testing-es.js
+++
@@ -1,6 +0,0 @@
-// ✔ ~/DevSeed/cumulus-projects/cumulus/packages/api
-// $ nvm use 8.0
-// $ node
-
-var bootstrap = require('./lambdas/bootstrap')
-bootstrap.bootstrapElasticSearch('http://localhost:4571')
