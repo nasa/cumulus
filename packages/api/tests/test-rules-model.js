9db6d5ba275c28a0b0bb8ff540ad891d7d0d47df
Jenny Liu <jenny.h.liu@nasa.gov>
2018-02-20 10:42:56 -0500

    always verify workflow

--- packages/api/tests/test-rules-model.js
+++ packages/api/tests/test-rules-model.js
@@ -2,16 +2,20 @@
 
 const test = require('ava');
 const models = require('../models');
+const aws = require('@cumulus/common/aws');
 const { randomString } = require('@cumulus/common/test-utils');
 
 process.env.RulesTable = `RulesTable_${randomString()}`;
 process.env.stackName = 'my-stackName';
 process.env.kinesisConsumer = 'my-kinesisConsumer';
+process.env.bucket = 'my-bucket';
+const workflow = 'my-workflow';
+const workflowfile = `${process.env.stackName}/workflows/${workflow}.json`;
 
-const originalRule = {
+const kinesisRule = {
   name: 'my_kinesis_rule',
   workflow: 'my-workflow',
-  provider: 'my provider',
+  provider: 'my-provider',
   collection: {
     name: 'my-collection-name',
     version: 'my-collection-version'
@@ -23,24 +27,53 @@ const originalRule = {
   state: 'DISABLED'
 };
 
+const onetimeRule = {
+  name: 'my_one_time_rule',
+  workflow: 'my-workflow',
+  provider: 'my-provider',
+  collection: {
+    name: 'my-collection-name',
+    version: 'my-collection-version'
+  },
+  rule: {
+    type: 'onetime'
+  },
+  state: 'ENABLED'
+};
+
 test.before(async () => {
   const hash = { name: 'name', type: 'S' };
   // create Rules table
   await models.Manager.createTable(process.env.RulesTable, hash);
+  await aws.s3().createBucket({ Bucket: process.env.bucket }).promise();
+  await aws.s3().putObject({ Bucket: process.env.bucket, Key: workflowfile, Body: 'test data' })
+    .promise();
 });
 
 test.after.always(async () => {
   // cleanup table
   models.Manager.deleteTable(process.env.RulesTable);
+  await aws.recursivelyDeleteS3Bucket(process.env.bucket);
+});
+
+test('create and delete a onetime rule', async (t) => {
+  // create rule
+  const rules = new models.Rule();
+  return rules.create(onetimeRule)
+    .then(async (rule) => {
+      t.is(rule.name, onetimeRule.name);
+      // delete rule
+      await rules.delete(rule);
+    });
 });
 
 test('create and delete a kinesis type rule', async (t) => {
   // create rule
   const rules = new models.Rule();
-  return rules.create(originalRule)
+  return rules.create(kinesisRule)
     .then(async (rule) => {
-      t.is(rule.name, originalRule.name);
-      t.is(rule.rule.value, originalRule.rule.value);
+      t.is(rule.name, kinesisRule.name);
+      t.is(rule.rule.value, kinesisRule.rule.value);
       t.false(rule.rule.arn === undefined);
       // delete rule
       await rules.delete(rule);
@@ -50,8 +83,8 @@ test('create and delete a kinesis type rule', async (t) => {
 test('update a kinesis type rule state, arn does not change', async (t) => {
   // create rule
   const rules = new models.Rule();
-  await rules.create(originalRule);
-  const rule = await rules.get({ name: originalRule.name });
+  await rules.create(kinesisRule);
+  const rule = await rules.get({ name: kinesisRule.name });
 
   // update rule state
   const updated = { name: rule.name, state: 'ENABLED' };
@@ -68,8 +101,8 @@ test('update a kinesis type rule state, arn does not change', async (t) => {
 test('update a kinesis type rule value, resulting in new arn', async (t) => {
   // create rule
   const rules = new models.Rule();
-  await rules.create(originalRule);
-  const rule = await rules.get({ name: originalRule.name });
+  await rules.create(kinesisRule);
+  const rule = await rules.get({ name: kinesisRule.name });
 
   // update rule value
   const updated = {
