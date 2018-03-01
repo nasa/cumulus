dfd608591ddc06235cb23cab9a916c254699d1db
Scisco <scisco7@gmail.com>
2018-03-01 15:37:45 -0500

    add sftp support to circleci

--- cumulus/tasks/discover-granules/tests/index.js
+++ cumulus/tasks/discover-granules/tests/index.js
@@ -36,28 +36,12 @@ test('discover granules using FTP', async (t) => {
 
 test('discover granules using SFTP', async (t) => {
   const internalBucketName = randomString();
-  const providerPath = randomString();
-
-  // Figure out the directory paths that we're working with
-  const providerPathDirectory = path.join(await findTmpTestDataDirectory(), providerPath);
 
   // Create providerPathDirectory and internal bucket
-  await Promise.all([
-    fs.ensureDir(providerPathDirectory),
-    s3().createBucket({ Bucket: internalBucketName }).promise()
-  ]);
-
-  // State sample files
-  const files = [
-    'granule-1.nc', 'granule-1.nc.md5',
-    'granule-2.nc', 'granule-2.nc.md5',
-    'granule-3.nc', 'granule-3.nc.md5'
-  ];
-  await Promise.all(files.map((file) =>
-    fs.outputFile(path.join(providerPathDirectory, file), `This is ${file}`)));
+  await s3().createBucket({ Bucket: internalBucketName }).promise();
 
   const event = cloneDeep(mur);
-  event.config.collection.provider_path = providerPath;
+  event.config.collection.provider_path = 'granules/fake_granules';
   event.config.provider = {
     id: 'MODAPS',
     protocol: 'sftp',
@@ -83,10 +67,7 @@ test('discover granules using SFTP', async (t) => {
   }
   finally {
     // Clean up
-    await Promise.all([
-      recursivelyDeleteS3Bucket(internalBucketName),
-      fs.remove(providerPathDirectory)
-    ]);
+    await recursivelyDeleteS3Bucket(internalBucketName);
   }
 });
 
