b2dc9e14af82cfe767b6733ec096181a29525d09
Aimee Barciauskas <aimee@developmentseed.org>
2018-03-03 23:53:59 -0500

    Working test

--- packages/api/tests/test-db-indexer.js
+++ packages/api/tests/test-db-indexer.js
@@ -5,6 +5,7 @@ const archiver = require('archiver');
 const aws = require('@cumulus/common/aws');
 const fs = require('fs');
 const path = require('path');
+const { randomString } = require('@cumulus/common/test-utils');
 const test = require('ava');
 
 process.env.stackName = 'test-stack';
@@ -17,7 +18,7 @@ const collections = new models.Collection();
 const EsCollection = require('../es/collections');
 
 const testCollection = {
-  'name': 'collection-006',
+  'name': `collection-${randomString()}`,
   'version': '0.0.0',
   'provider_path': '/',
   'duplicateHandling': 'replace',
@@ -39,65 +40,65 @@ const dbIndexerFnName = 'test-dbIndexer';
 // records show up in elasticsearch 'hooked-up': the dynamo has a stream and the
 // lambda has an event source mapping to that dynamo stream.
 test.before(async () => {
-  // await aws.s3().createBucket({ Bucket: process.env.internal }).promise();
+  await aws.s3().createBucket({ Bucket: process.env.internal }).promise();
 
-  // const hash = { name: 'name', type: 'S' };
-  // // create collections table
-  // await models.Manager.createTable(process.env.CollectionsTable, hash);
-  // await bootstrap.bootstrapElasticSearch('http://localhost:4571');
+  const hash = { name: 'name', type: 'S' };
+  // create collections table
+  await models.Manager.createTable(process.env.CollectionsTable, hash);
+  await bootstrap.bootstrapElasticSearch('http://localhost:4571');
 
-  // // create the lambda function
-  // await new Promise((resolve) => {
-  //   output.on('close', () => {
-  //     const contents = fs.readFileSync(tmpZipFile)
+  // create the lambda function
+  await new Promise((resolve) => {
+    output.on('close', () => {
+      const contents = fs.readFileSync(tmpZipFile)
 
-  //     aws.lambda().createFunction({
-  //       FunctionName: dbIndexerFnName,
-  //       Runtime: 'nodejs6.10',
-  //       Handler: 'index.dbIndexer', // point to the db indexer
-  //       Role: 'testRole',
-  //       Code: {
-  //         ZipFile: contents
-  //       }
-  //     })
-  //     .promise()
-  //     .then(res => {
-  //       fs.unlinkSync(tmpZipFile);
-  //       resolve(res);
-  //     });
-  //   });
+      aws.lambda().createFunction({
+        FunctionName: dbIndexerFnName,
+        Runtime: 'nodejs6.10',
+        Handler: 'index.dbIndexer', // point to the db indexer
+        Role: 'testRole',
+        Code: {
+          ZipFile: contents
+        }
+      })
+      .promise()
+      .then(res => {
+        fs.unlinkSync(tmpZipFile);
+        resolve(res);
+      });
+    });
 
-  //   archive.pipe(output)
-  //   archive.directory(codeDirectory, false);
-  //   archive.finalize()
-  // })
-  // .catch(e => console.log(e));
+    archive.pipe(output)
+    archive.directory(codeDirectory, false);
+    archive.finalize()
+  })
+  .catch(e => console.log(e));
 
-  // //get the dynamo collections table stream arn and add it as an event source to the lambda
-  // await new Promise((resolve, reject) => {
-  //   aws.dynamodbstreams().listStreams({TableName: process.env.CollectionsTable}, (err, data) => {
-  //     if (err) reject(err);
-  //     const collectionsTableStreamArn = data.Streams.find(s => s.TableName === 'test-stack-CollectionsTable').StreamArn;
-  //     const eventSourceMappingParams = {
-  //       EventSourceArn: collectionsTableStreamArn,
-  //       FunctionName: dbIndexerFnName,
-  //       StartingPosition: 'TRIM_HORIZON',
-  //       BatchSize: 10
-  //     };
+  //get the dynamo collections table stream arn and add it as an event source to the lambda
+  await new Promise((resolve, reject) => {
+    aws.dynamodbstreams().listStreams({TableName: process.env.CollectionsTable}, (err, data) => {
+      if (err) reject(err);
+      const collectionsTableStreamArn = data.Streams.find(s => s.TableName === 'test-stack-CollectionsTable').StreamArn;
+      const eventSourceMappingParams = {
+        EventSourceArn: collectionsTableStreamArn,
+        FunctionName: dbIndexerFnName,
+        StartingPosition: 'TRIM_HORIZON',
+        BatchSize: 10
+      };
 
-  //     aws.lambda().createEventSourceMapping(eventSourceMappingParams, (err, data) => {
-  //       if (err) reject(err);
-  //       resolve(data);
-  //     });
-  //   });    
-  // })
-  // .catch(e => console.log(e));
+      aws.lambda().createEventSourceMapping(eventSourceMappingParams, (err, data) => {
+        if (err) reject(err);
+        resolve(data);
+      });
+    });
+  })
+  .catch(e => console.log(e));
 });
 
 test.after.always(async () => {
-  // await models.Manager.deleteTable(process.env.CollectionsTable);
-  // await aws.lambda().deleteFunction({FunctionName: dbIndexerFnName}).promise();
-  // await aws.recursivelyDeleteS3Bucket(process.env.internal);
+  await models.Manager.deleteTable(process.env.CollectionsTable);
+  await aws.lambda().deleteFunction({FunctionName: dbIndexerFnName}).promise();
+  await aws.recursivelyDeleteS3Bucket(process.env.internal);
 });
 
 test.only('creates a collection in dynamodb and es', async t => {
