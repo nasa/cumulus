3aa2669c6d06159c3176852f370e6ec77266eee8
Aimee Barciauskas <aimee@developmentseed.org>
2018-03-08 15:34:33 -0500

    CUMULUS-175 : Dashboard providers not in sync with AWS providers (#234)
    **Summary:** Hooks up db-indexer lambda function to dynamoDB streams which replicates updates to elasticsearch. Update API endpoints to just use elastic search.
    
    Closes #234 
    
    Addresses [CUMULUS-175: Dashboard providers not in sync with AWS providers](https://bugs.earthdata.nasa.gov/browse/CUMULUS-175)
    
    ## Changes
    
    * Update API endpoints for collections, providers and rules.
    * Add happy path tests for updated endpoints
    * Adds db-indexer lambda function and cloudformation configuration of event source of dynamo db streams
    * Adds test for db-indexer lambda function, currently skipped because it only works locally
    
    ## Test Plan
    - [x] Unit tests
    - [x] Adhoc testing of dashboard: list, get, create, update, delete of collections, providers and rules
    - [x] Testing of elasticsearch - checked instances exist in `<ELASTICSEARCH_DOMAIN>/_all/<collection|rule|provider>/_search?pretty=true`
    
    Reviewers: @scisco

---
+++ packages/api/tests/testUtils.js
@@ -0,0 +1,12 @@
+'use strict';
+
+function testEndpoint(endpoint, event, testCallback) {
+  return new Promise((resolve, reject) => {
+    endpoint(event, {
+      succeed: response => resolve(testCallback(response)),
+      fail: e => reject(e)
+    });
+  });
+}
+
+module.exports = { testEndpoint };
