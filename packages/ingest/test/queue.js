13c4ac25d5ec540f4ff3b4366200d11a7600b607
Aimee Barciauskas <aimee@developmentseed.org>
2018-03-15 16:32:06 -0400

    Add test for enqueueGranuleIngestMessage

--- packages/ingest/test/queue.js
+++ packages/ingest/test/queue.js
@@ -21,6 +21,7 @@ test.beforeEach(async (t) => {
   };
  
   const messageTemplateKey = `${randomString()}/template.json`;
+  t.context.messageTemplateKey = messageTemplateKey;
   await s3().putObject({
     Bucket: t.context.templateBucket,
     Key: messageTemplateKey,
@@ -35,4 +36,37 @@ test.afterEach(async (t) => {
     recursivelyDeleteS3Bucket(t.context.templateBucket),
     sqs().deleteQueue({ QueueUrl: t.context.queueUrl }).promise()
   ]);
+});
+
+test('the queue receives a correctly formatted workflow message', async (t) => {
+  const granule = { granuleId: '1', files: [] };
+  const queueUrl = t.context.queueUrl;
+  const templateUri = `s3://${t.context.templateBucket}/${t.context.messageTemplateKey}`;
+  const collection = { name: 'test-collection', version: '0.0.0' };
+  const provider = { id: 'test-provider' };
+
+  await queue.enqueueGranuleIngestMessage(granule, queueUrl, templateUri, provider, collection);
+  await sqs().receiveMessage({
+    QueueUrl: t.context.queueUrl,
+    MaxNumberOfMessages: 10,
+    WaitTimeSeconds: 1
+  }).promise()
+  .then((receiveMessageResponse) => {
+    t.is(receiveMessageResponse.Messages.length, 1);
+
+    const actualMessage = JSON.parse(receiveMessageResponse.Messages[0].Body);
+    const expectedMessage = {
+      cumulus_meta: {
+        state_machine: t.context.stateMachineArn
+      },
+      meta: {
+        queues: { startSF: t.context.queueUrl },
+        provider: provider,
+        collection: collection
+      },
+      payload: { granules: [ granule ] }
+    };
+
+    t.deepEqual(expectedMessage, actualMessage);
+  });
 });
