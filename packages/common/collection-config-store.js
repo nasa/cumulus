d49a7ad464b1de162be4a89e3df9b1ce64324dcb
Marc Huffnagle <marc@element84.com>
2018-04-04 17:47:12 -0400

    ParsePdr task uses correct granuleExtractionId if multiple dataTypes are present in PDR

---
+++ packages/common/collection-config-store.js
@@ -0,0 +1,84 @@
+'use strict';
+
+const { s3 } = require('./aws');
+
+/**
+ * Fetch collection configs from S3
+ */
+class CollectionConfigStore {
+  /**
+   * Initialize a CollectionConfigFetcher instance
+   *
+   * @param {string} bucket - the bucket where collection configs are stored
+   * @param {string} stackName - the Cumulus deployment stack name
+   */
+  constructor(bucket, stackName) {
+    this.bucket = bucket;
+    this.stackName = stackName;
+    this.cachedCollectionConfigs = {};
+  }
+
+  /**
+   * Fetch a collection config from S3 (or cache if available)
+   *
+   * @param {string} dataType - the name of the collection config to fetch
+   * @returns {Object} the fetched collection config
+   */
+  async get(dataType) {
+    if (!this.cachedCollectionConfigs[dataType]) {
+      const Bucket = this.bucket;
+      const Key = this.configKey(dataType);
+
+      let response;
+      try {
+        response = await s3().getObject({ Bucket, Key }).promise();
+      }
+      catch (err) {
+        if (err.code === 'NoSuchKey') {
+          throw new Error(`A collection config for data type ${dataType} was not found.`);
+        }
+
+        if (err.code === 'NoSuchBucket') {
+          throw new Error(`Collection config bucket does not exist: ${this.bucket}`);
+        }
+
+        throw err;
+      }
+
+      this.cachedCollectionConfigs[dataType] = JSON.parse(response.Body.toString());
+    }
+
+    return this.cachedCollectionConfigs[dataType];
+  }
+
+  /**
+   * Store a collection config to S3
+   *
+   * @param {string} dataType - the name of the collection config to store
+   * @param {Object} config - the collection config to store
+   * @returns {Promise} resolves when the collection config has been written
+   *   to S3
+   */
+  async put(dataType, config) {
+    this.cachedCollectionConfigs[dataType] = config;
+
+    return s3().putObject({
+      Bucket: this.bucket,
+      Key: `${this.stackName}/collections/${dataType}.json`,
+      Body: JSON.stringify(config)
+    }).promise();
+  }
+
+  /**
+   * Return the S3 key pointing to the collection config
+   *
+   * @param {string} dataType - the datatype
+   * @returns {string} the S3 key where the collection config is located
+   *
+   * @private
+   */
+  configKey(dataType) {
+    return `${this.stackName}/collections/${dataType}.json`
+  }
+}
+module.exports = CollectionConfigStore;
--- tasks/queue-granules/index.js
+++ tasks/queue-granules/index.js
@@ -2,42 +2,7 @@
 
 const cumulusMessageAdapter = require('@cumulus/cumulus-message-adapter-js');
 const { enqueueGranuleIngestMessage } = require('@cumulus/ingest/queue');
-const { s3 } = require('@cumulus/common/aws');
-
-/**
- * Fetch a collection config from S3
- *
- * @param {string} stackName - the name of the stack
- * @param {string} Bucket - the name of the bucket containing the collection configs
- * @param {string} dataType - the name of the collection
- * @returns {Array} the datatype and the collection config
- */
-async function fetchCollectionConfig(stackName, Bucket, dataType) {
-  const Key = `${stackName}/collections/${dataType}.json`;
-  const collectionConfig = (await s3().getObject({ Bucket, Key }).promise()).Body;
-  return [dataType, JSON.parse(collectionConfig)];
-}
-
-/**
- * For a list of data-types, fetch the corresponding collection configs from S3
- *
- * @param {string} stackName - the name of the stack
- * @param {string} Bucket - the name of the bucket containing the collection configs
- * @param {Array<string>} dataTypes - the name of the collection
- * @returns {Promise<Object>} a map of data-type to collection config
- */
-async function fetchCollectionConfigs(stackName, Bucket, dataTypes) {
-  const fetchConfig = (dataType) => fetchCollectionConfig(stackName, Bucket, dataType); // eslint-disable-line require-jsdoc, max-len
-
-  const fetchedCollectionConfigs = await Promise.all(dataTypes.map(fetchConfig));
-
-  const collectionConfigs = {};
-  fetchedCollectionConfigs.forEach(([dataType, collectionConfig]) => {
-    collectionConfigs[dataType] = collectionConfig;
-  });
-
-  return collectionConfigs;
-}
+const { CollectionConfigStore } = require('@cumulus/common');
 
 /**
 * See schemas/input.json and schemas/config.json for detailed event description
@@ -49,22 +14,23 @@ async function fetchCollectionConfigs(stackName, Bucket, dataTypes) {
 async function queueGranules(event) {
   const granules = event.input.granules || [];
 
-  const dataTypes = granules.map((granule) => granule.dataType);
-  const collectionConfigs = await fetchCollectionConfigs(
-    event.config.stackName,
-    event.config.internalBucket,
-    dataTypes
-  );
+  const collectionConfigStore =
+    new CollectionConfigStore(event.config.internalBucket, event.config.stackName);
 
   await Promise.all( // eslint-disable-line function-paren-newline
-    granules.map((granule) => enqueueGranuleIngestMessage(
-      granule,
-      event.config.queueUrl,
-      event.config.granuleIngestMessageTemplateUri,
-      event.config.provider,
-      collectionConfigs[granule.dataType],
-      event.input.pdr
-    )));
+    granules.map(async (granule) => {
+      const collectionConfig = await collectionConfigStore.get(granule.dataType);
+
+      return enqueueGranuleIngestMessage(
+        granule,
+        event.config.queueUrl,
+        event.config.granuleIngestMessageTemplateUri,
+        event.config.provider,
+        collectionConfig,
+        event.input.pdr
+      );
+    }));
+
 
   return { granules_queued: granules.length };
 }
