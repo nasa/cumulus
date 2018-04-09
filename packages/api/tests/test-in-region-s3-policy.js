ed4a5a7ac48062db6b76cfb3187e16ed5f3308fb
Lauren Frederick <lpfreder@mbp-lpfreder.edn.ecs.nasa.gov>
2018-04-09 11:23:26 -0400

    Try excluding entire file

--- packages/api/tests/test-in-region-s3-policy.js
+++ packages/api/tests/test-in-region-s3-policy.js
@@ -6,49 +6,49 @@ const get = require('lodash.get');
 
 const policy = require('../lambdas/in-region-s3-policy');
 
-// test.beforeEach(() => {
-//   const ipResponse = [
-//     {
-//       ip_prefix: '192.0.2.3',
-//       region: 'us-east-1',
-//       service: 'AMAZON'
-//     },
-//     {
-//       ip_prefix: '192.0.2.3',
-//       region: 'us-west-2',
-//       service: 'AMAZON'
-//     },
-//     {
-//       ip_prefix: '192.0.2.3',
-//       region: 'us-east-1',
-//       service: 'AMAZON'
-//     },
-//     {
-//       ip_prefix: '192.0.2.3',
-//       region: 'ap-northeast-1',
-//       service: 'AMAZON'
-//     },
-//     {
-//       ip_prefix: '192.0.2.32',
-//       region: 'us-east-1',
-//       service: 'AMAZON'
-//     },
-//     {
-//       ip_prefix: '192.0.2.3',
-//       region: 'us-west-2',
-//       service: 'AMAZON'
-//     }
-//   ];
+test.beforeEach(() => {
+  const ipResponse = [
+    {
+      ip_prefix: '192.0.2.3',
+      region: 'us-east-1',
+      service: 'AMAZON'
+    },
+    {
+      ip_prefix: '192.0.2.3',
+      region: 'us-west-2',
+      service: 'AMAZON'
+    },
+    {
+      ip_prefix: '192.0.2.3',
+      region: 'us-east-1',
+      service: 'AMAZON'
+    },
+    {
+      ip_prefix: '192.0.2.3',
+      region: 'ap-northeast-1',
+      service: 'AMAZON'
+    },
+    {
+      ip_prefix: '192.0.2.32',
+      region: 'us-east-1',
+      service: 'AMAZON'
+    },
+    {
+      ip_prefix: '192.0.2.3',
+      region: 'us-west-2',
+      service: 'AMAZON'
+    }
+  ];
 
-//   sinon.stub(
-//     policy,
-//     'getIpRanges'
-//   ).resolves(ipResponse);
-// });
+  sinon.stub(
+    policy,
+    'getIpRanges'
+  ).resolves(ipResponse);
+});
 
-// test.afterEach(() => {
-//   policy.getIpRanges.restore();
-// });
+test.afterEach(() => {
+  policy.getIpRanges.restore();
+});
 
 // test.serial('IP ranges filtered correctly', async (t) => {
 //   const bucketPolicy = await policy.generatePolicy('url', 'us-east-1');
