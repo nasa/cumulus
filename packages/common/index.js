4cc0ac2bf0db644fa991e0644dbfbf24e875c10b
Lauren Frederick <lpfreder@mbp-lpfreder.edn.ecs.nasa.gov>
2018-04-12 15:53:04 -0400

    CUMULUS-352 Fix bad merge

--- packages/common/index.js
+++ packages/common/index.js
@@ -3,8 +3,5 @@
 exports.log = require('./log');
 exports.aws = require('./aws');
 exports.task = require('./task');
-<<<<<<< HEAD
 exports.cliUtils = require('./cli-utils');
-=======
 exports.CollectionConfigStore = require('./collection-config-store');
->>>>>>> master
