c640e26323974b0dfede38a19d31e60bfdc3c1d2
Lauren Frederick <lpfreder@mbp-lpfreder.edn.ecs.nasa.gov>
2018-04-16 11:30:55 -0400

    Remove tests

--- example/spec/helloWorld/HelloWorldSuccessSpec.js
+++ example/spec/helloWorld/HelloWorldSuccessSpec.js
@@ -35,35 +35,3 @@ describe('The Hello World workflow', () => {
     });
   });
 });
-
-describe('The Hello World workflow using ECS', () => {
-  let workflowExecution = null;
-
-  beforeAll(async () => {
-    workflowExecution = await executeWorkflow(
-      awsConfig.stackName,
-      awsConfig.bucket,
-      'EcsHelloWorldWorkflow',
-      './spec/helloWorld/HelloWorld.input.template.json'
-    );
-  });
-
-  it('executes successfully', () => {
-    expect(workflowExecution.status).toEqual('SUCCEEDED');
-  });
-
-  describe('the HelloWorld ECS', () => {
-    let activityOutput = null;
-
-    beforeAll(async () => {
-      activityOutput = await activityStep.getStepOutput(
-        workflowExecution.executionArn,
-        'EcsTaskHelloWorld'
-      );
-    });
-
-    it('output is Hello World', () => {
-      expect(activityOutput.payload).toEqual({ hello: 'Hello World' });
-    });
-  });
-});
