78779a026a1a9f54f5028b299273271c6b44dbed
Lauren Frederick <lpfreder@mbp-lpfreder.edn.ecs.nasa.gov>
2018-04-23 15:42:12 -0400

    Centralize timeout

--- example/spec/ingestGranule/IngestGranuleSuccessSpec.js
+++ example/spec/ingestGranule/IngestGranuleSuccessSpec.js
@@ -13,7 +13,8 @@ const templatedOutputPayloadFilename = templateFile({
 });
 const expectedPayload = JSON.parse(fs.readFileSync(templatedOutputPayloadFilename));
 
-jasmine.DEFAULT_TIMEOUT_INTERVAL = 2000000;
+console.log(jasmine.getEnv().defaultTimeoutInterval);
+console.log(jasmine.DEFAULT_TIMEOUT_INTERVAL);
 
 describe('The S3 Ingest Granules workflow', () => {
   const inputPayloadFilename = './spec/ingestGranule/IngestGranule.input.payload.json';
