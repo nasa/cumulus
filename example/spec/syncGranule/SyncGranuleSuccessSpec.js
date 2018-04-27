85f98535e249a3dee3bd58130092be94e642a631
Katherine Kelly <katherine.a.kelly@nasa.gov>
2018-04-27 13:27:34 -0400

    fixed spec name

---
+++ example/spec/syncGranule/SyncGranuleSuccessSpec.js
@@ -0,0 +1,50 @@
+const fs = require('fs');
+const { buildAndExecuteWorkflow, LambdaStep } = require('@cumulus/integration-tests');
+
+const { loadConfig, templateFile } = require('../helpers/testUtils');
+const config = loadConfig();
+const lambdaStep = new LambdaStep();
+const taskName = 'SyncGranule';
+
+const outputPayloadTemplateFilename = './spec/syncGranule/SyncGranule.output.payload.template.json'; // eslint-disable-line max-len
+const templatedOutputPayloadFilename = templateFile({
+  inputTemplateFilename: outputPayloadTemplateFilename,
+  config: config.IngestGranule.SyncGranuleOutput
+});
+const expectedPayload = JSON.parse(fs.readFileSync(templatedOutputPayloadFilename));
+
+
+describe('The Sync Granules workflow', () => {
+  const inputPayloadFilename = './spec/syncGranule/SyncGranule.input.payload.json';
+  const inputPayload = JSON.parse(fs.readFileSync(inputPayloadFilename));
+  const collection = { name: 'MOD09GQ', version: '006' };
+  const provider = { id: 's3_provider' };
+  let workflowExecution = null;
+
+  beforeAll(async () => {
+    // eslint-disable-next-line function-paren-newline
+    workflowExecution = await buildAndExecuteWorkflow(
+      config.stackName, config.bucket, taskName, collection, provider, inputPayload
+    );
+  });
+
+  it('executes successfully', () => {
+    expect(workflowExecution.status).toEqual('SUCCEEDED');
+  });
+
+  describe('the SyncGranule Lambda function', () => {
+    let lambdaOutput = null;
+
+    beforeAll(async () => {
+      lambdaOutput = await lambdaStep.getStepOutput(workflowExecution.executionArn, 'SyncGranule');
+    });
+
+    it('has expected payload', () => {
+      expect(lambdaOutput.payload).toEqual(expectedPayload);
+    });
+
+    it('has expected updated meta', () => {
+      expect(lambdaOutput.meta.input_granules).toEqual(expectedPayload.granules);
+    });
+  });
+});
