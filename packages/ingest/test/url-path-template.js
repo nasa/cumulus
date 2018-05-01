64bbf111dd465301fa66c8281cfa19887cf26b27
Jenny Liu <jenny.h.liu@nasa.gov>
2018-05-01 15:30:08 -0400

    url-path-template supports metadata fields

--- packages/ingest/test/url-path-template.js
+++ packages/ingest/test/url-path-template.js
@@ -1,7 +1,25 @@
 'use strict';
 
 const test = require('ava');
+const fs = require('fs');
+const { parseString } = require('xml2js');
+const { xmlParseOptions } = require('@cumulus/cmrjs/utils');
 const urlPathTemplate = require('../url-path-template');
+
+// eslint-disable-next-line max-len
+const modisXmlFile = '../../packages/test-data/granules/MOD09GQ.A2016358.h13v04.006.2016360104606.cmr.xml';
+// eslint-disable-next-line max-len
+const measuresXmlFile = '../../packages/test-data/granules/antarctica_ice_velocity_450m.nc..cmr.xml';
+
+function getMetadata(xmlFile) {
+  const xmlString = fs.readFileSync(xmlFile, 'utf8');
+  return new Promise((resolve, reject) => {
+    parseString(xmlString, xmlParseOptions, (err, obj) => {
+      if (err) reject(err);
+      resolve(obj);
+    });
+  });
+}
 
 test('test basic usage', (t) => {
   const urlPath = '/{file.bucket}/{file.name}';
@@ -14,4 +32,37 @@ test('test basic usage', (t) => {
 
   const result = urlPathTemplate(urlPath, context);
   t.is(result, '/example/file.hdf');
+});
+
+test('url path has metadata fields', async (t) => {
+  const metadataObject = await getMetadata(modisXmlFile);
+  const urlPath = '{cmrMetadata.Granule.Collection.ShortName}.{cmrMetadata.Granule.Collection.VersionId}';
+  const result = urlPathTemplate(urlPath, { cmrMetadata: metadataObject });
+  t.is(result, 'MOD09GQ.006');
+});
+
+test('url path has operations on metadata date components', async (t) => {
+  const metadataObject = await getMetadata(modisXmlFile);
+  // build a long url path
+  const yearPart = '{extractYear(cmrMetadata.Granule.Temporal.RangeDateTime.BeginningDateTime)}/';
+  const monthPart = '{extractMonth(cmrMetadata.Granule.Temporal.RangeDateTime.BeginningDateTime)}/';
+  const datePart = '{extractDate(cmrMetadata.Granule.Temporal.RangeDateTime.BeginningDateTime)}/';
+  const hourPart = '{extractHour(cmrMetadata.Granule.Temporal.RangeDateTime.BeginningDateTime)}/';
+  const urlPath = yearPart.concat(monthPart, datePart, hourPart);
+  const result = urlPathTemplate(urlPath, { cmrMetadata: metadataObject });
+  t.is(result, '2016/12/23/13/');
+});
+
+test('url path has substring operation', async (t) => {
+  const metadataObject = await getMetadata(modisXmlFile);
+  const urlPath = '{substring(cmrMetadata.Granule.PGEVersionClass.PGEVersion, 0, 3)}';
+  const result = urlPathTemplate(urlPath, { cmrMetadata: metadataObject });
+  t.is(result, '6.0');
+});
+
+test('url path has metadata fields which has multiple values', async (t) => {
+  const metadataObject = await getMetadata(measuresXmlFile);
+  const urlPath = '{cmrMetadata.Granule.Platforms.Platform[0].ShortName}';
+  const result = urlPathTemplate(urlPath, { cmrMetadata: metadataObject });
+  t.is(result, 'ALOS');
 });
