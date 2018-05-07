faaec280e6aaa5dfce78176d2eabb9026df56a20
Katherine Kelly <katherine.a.kelly@nasa.gov>
2018-05-07 16:00:14 -0400

    Fixing post to cmr unit tests

--- tasks/post-to-cmr/index.js
+++ tasks/post-to-cmr/index.js
@@ -3,11 +3,98 @@
 'use strict';
 
 const get = require('lodash.get');
+const path = require('path');
 const cumulusMessageAdapter = require('@cumulus/cumulus-message-adapter-js');
 const { justLocalRun } = require('@cumulus/common/local-helpers');
 const { DefaultProvider } = require('@cumulus/ingest/crypto');
 const { CMR } = require('@cumulus/cmrjs');
+const { getS3Object } = require('@cumulus/common/aws');
+const { XmlMetaFileNotFound } = require('@cumulus/common/errors');
+const { xmlParseOptions } = require('@cumulus/cmrjs/utils');
+const xml2js = require('xml2js');
 const log = require('@cumulus/common/log');
+
+/**
+ * Parse an xml string
+ *
+ * @param {string} xml - xml to parse
+ * @returns {Promise<Object>} promise resolves to object version of the xml
+ */
+async function parseXmlString(xml) {
+  return new Promise((resolve, reject) => {
+    xml2js.parseString(xml, xmlParseOptions, (err, data) => {
+      if (err) return reject(err);
+      return resolve(data);
+    });
+  });
+}
+
+/**
+ * getMetadata
+ *
+ * @param {string} xmlFilePath - S3 URI to the xml metadata document
+ * @returns {string} returns stringified xml document downloaded from S3
+ */
+async function getMetadata(xmlFilePath) {
+  if (!xmlFilePath) {
+    throw new XmlMetaFileNotFound('XML Metadata file not provided');
+  }
+
+  // GET the metadata text
+  // Currently, only supports files that are stored on S3
+  const parts = xmlFilePath.match(/^s3:\/\/(.+?)\/(.+)$/);
+  const obj = await getS3Object(parts[1], parts[2]);
+  return obj.Body.toString();
+}
+
+/**
+ * Extract the granule ID from the a given s3 uri
+ *
+ * @param {string} uri - the s3 uri of the file
+ * @param {string} regex - the regex for extracting the ID
+ * @returns {string} the granule
+ */
+function getGranuleId(uri, regex) {
+  const filename = path.basename(uri);
+  const test = new RegExp(regex);
+  const match = filename.match(test);
+
+  if (match) {
+    return match[1];
+  }
+  return match;
+}
+
+/**
+ * returns a list of CMR xml files
+ *
+ * @param {Array} input - an array of s3 uris
+ * @param {string} granuleIdExtraction - a regex for extracting granule IDs
+ * @returns {Promise<Array>} promise resolves to an array of objects
+ * that includes CMR xmls uris and granuleIds
+ */
+async function getCmrFiles(input, granuleIdExtraction) {
+  const files = [];
+  const expectedFormat = /.*\.cmr\.xml$/;
+
+  for (const filename of input) {
+    if (filename && filename.match(expectedFormat)) {
+      const metadata = await getMetadata(filename);
+      const metadataObject = await parseXmlString(metadata);
+
+      const cmrFileObject = {
+        filename,
+        metadata,
+        metadataObject,
+        granuleId: getGranuleId(filename, granuleIdExtraction)
+      };
+
+      files.push(cmrFileObject);
+    }
+  }
+
+  return files;
+}
 
 /**
  * function for posting cmr xml files from S3 to CMR
@@ -95,9 +182,13 @@ async function postToCMR(event) {
   const bucket = get(config, 'bucket'); // the name of the bucket with private/public keys
   const stack = get(config, 'stack'); // the name of the deployment stack
   const input = get(event, 'input', []);
+  const regex = get(config, 'granuleIdExtraction', '(.*)');
   const allGranules = get(input, 'allGranules'); // Object of all Granules
-  const cmrFiles = get(input, 'cmrFiles'); // list of CMR files from input
+  const inputFiles = get(input, 'inputFiles'); // list of files from input
   const creds = get(config, 'cmr');
+
+  // get cmr files
+  const cmrFiles = await getCmrFiles(inputFiles, regex);
 
   // post all meta files to CMR
   const publishRquests = cmrFiles.map((cmrFile) => publish(cmrFile, creds, bucket, stack));
