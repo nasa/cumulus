7579e4a979758b5a831b7966c5445a154292ef1c
Pat Cappelaere <pat@cappelaere.com>
2018-05-10 13:40:53 -0400

    Updated based on Marc's comments

--- tasks/parse-pdr/tests/parse_modaps_pdrs.js
+++ tasks/parse-pdr/tests/parse_modaps_pdrs.js
@@ -1,11 +1,7 @@
 'use strict';
 
-const errors = require('@cumulus/common/errors');
-const fs = require('fs-extra');
 const test = require('ava');
-const os=require('os');
 const parsePdr = require('@cumulus/ingest/parse-pdr');
-
 
 test('parse MODAPS PDR', async (t) => {
   const pdrFilePath = './tests/fixtures/MODAPSops7.1234567.PDR'
@@ -19,8 +15,8 @@ test('parse MODAPS PDR', async (t) => {
   const collectionConfigStore = new CollectionConfigStore()
 
   // Note: This PDR contains a different type of checksum
-  const parsed = await parsePdr.parsePdr(pdrFilePath, collectionConfigStore, pdrName);
-  t.is(parsed.filesCount, 30);
-  t.is(parsed.granulesCount, 30);
-  t.is(parsed.granules.length, 30);
+  const parsedPdr = await parsePdr.parsePdr(pdrFilePath, collectionConfigStore, pdrName);
+  t.is(parsedPdr.filesCount, 30);
+  t.is(parsedPdr.granulesCount, 30);
+  t.is(parsedPdr.granules.length, 30);
 });
