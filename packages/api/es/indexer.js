a89fb4d68c769b4671e15da54cfb7a95abdfbdcb
Jenny Liu <jenny.h.liu@nasa.gov>
2018-05-15 10:17:10 -0400

    ems reports lib

--- packages/api/es/indexer.js
+++ packages/api/es/indexer.js
@@ -436,8 +436,8 @@ async function granule(esClient, payload, index = defaultIndexAlias, type = 'gra
         productVolume: getGranuleProductVolume(g.files),
         timeToPreprocess: get(payload, 'meta.sync_granule_duration'),
         timeToArchive: get(payload, 'meta.post_to_cmr_duration'),
-        processingStartTime: extractDate(payload, 'meta.sync_granule_end_time'),
-        processingEndTime: extractDate(payload, 'meta.post_to_cmr_start_time')
+        processingStartDateTime: extractDate(payload, 'meta.sync_granule_end_time'),
+        processingEndDateTime: extractDate(payload, 'meta.post_to_cmr_start_time')
       };
 
       doc.published = get(g, 'published', false);
