e46b6cf343a991a229e0ac9c9c9efc3079b821a2
Scisco <scisco7@gmail.com>
2018-05-16 09:52:00 -0400

    execution model

---
+++ packages/api/models/executions.js
@@ -0,0 +1,51 @@
+'use strict';
+
+const get = require('lodash.get');
+const aws = require('@cumulus/ingest/aws');
+const Manager = require('./base');
+const { constructCollectionId, parseException } = require('../lib/utils');
+const executionSchema = require('./schemas').execution;
+
+
+class Execution extends Manager {
+  constructor() {
+    super(process.env.ExecutionsTable, executionSchema);
+  }
+
+  /**
+   * Create a new execution record from incoming sns messages
+   *
+   * @param {Object} payload - sns message containing the output of a Cumulus Step Function
+   * @returns {Promise<Object>} an execution record
+   */
+  createExecutionFromSns(payload) {
+    const name = get(payload, 'cumulus_meta.execution_name');
+    const arn = aws.getExecutionArn(
+      get(payload, 'cumulus_meta.state_machine'),
+      name
+    );
+    if (!arn) {
+      const error = new Error('State Machine Arn is missing. Must be included in the cumulus_meta');
+      return Promise.reject(error);
+    }
+
+    const execution = aws.getExecutionUrl(arn);
+
+    const doc = {
+      name,
+      arn,
+      execution,
+      error: parseException(payload.exception),
+      type: get(payload, 'meta.workflow_name'),
+      collectionId: get(payload, 'meta.collection.name'),
+      status: get(payload, 'meta.status', 'unknown'),
+      createdAt: get(payload, 'cumulus_meta.workflow_start_time'),
+      timestamp: Date.now()
+    };
+
+    doc.duration = (doc.timestamp - doc.createdAt) / 1000;
+    return this.create(doc);
+  }
+}
+
+module.exports = Execution;
---
+++ packages/api/tests/test-endpoints-executions.js
@@ -0,0 +1,107 @@
+'use strict';
+
+const test = require('ava');
+const aws = require('@cumulus/common/aws');
+const { randomString } = require('@cumulus/common/test-utils');
+const models = require('../models');
+const bootstrap = require('../lambdas/bootstrap');
+const executionEndpoint = require('../endpoints/executions');
+const indexer = require('../es/indexer');
+const { testEndpoint } = require('./testUtils');
+const { Search } = require('../es/search');
+
+// create all the variables needed across this test
+let esClient;
+let fakeExecutions;
+const hash = { name: 'arn', type: 'S' };
+const esIndex = randomString();
+process.env.ExecutionsTable = randomString();
+process.env.stackName = randomString();
+process.env.internal = randomString();
+const ex = new models.Execution();
+
+/**
+ * creates fake execution records
+ *
+ * @param {string} status - pdr status (default to completed)
+ * @returns {Object} fake execution object
+ */
+function fakeExecutionFactory(status = 'completed') {
+  return {
+    arn: randomString(),
+    name: randomString(),
+    status,
+    createdAt: Date.now()
+  };
+}
+
+test.before(async () => {
+  // create esClient
+  esClient = await Search.es('fakehost');
+
+  // add fake elasticsearch index
+  await bootstrap.bootstrapElasticSearch('fakehost', esIndex);
+
+  // create a fake bucket
+  await aws.s3().createBucket({ Bucket: process.env.internal }).promise();
+
+  // create fake granule table
+  await models.Manager.createTable(process.env.ExecutionsTable, hash);
+
+  // create fake granule records
+  fakeExecutions = ['completed', 'failed'].map(fakeExecutionFactory);
+  await Promise.all(fakeExecutions.map((i) => ex.create(i)
+    .then((record) => indexer.indexStepFunction(esClient, record, esIndex))
+  ));
+});
+
+test.after.always(async () => {
+  await Promise.all([
+    models.Manager.deleteTable(process.env.ExecutionsTable),
+    esClient.indices.delete({ index: esIndex }),
+    aws.recursivelyDeleteS3Bucket(process.env.internal)
+  ]);
+});
+
+
+test('default returns list of executions', (t) => {
+  const listEvent = { httpMethod: 'list' };
+  return testEndpoint(executionEndpoint, listEvent, (response) => {
+    const { meta, results } = JSON.parse(response.body);
+    t.is(results.length, 2);
+    t.is(meta.stack, process.env.stackName);
+    t.is(meta.table, 'execution');
+    t.is(meta.count, 2);
+    const arns = fakeExecutions.map((i) => i.arn);
+    results.forEach((r) => {
+      t.true(arns.includes(r.arn));
+    });
+  });
+});
+
+test('GET returns an existing execution', (t) => {
+  const getEvent = {
+    httpMethod: 'GET',
+    pathParameters: {
+      arn: fakeExecutions[0].arn
+    }
+  };
+  return testEndpoint(executionEndpoint, getEvent, (response) => {
+    const { arn } = JSON.parse(response.body);
+    t.is(arn, fakeExecutions[0].arn);
+  });
+});
+
+test('GET fails if execution is not found', async (t) => {
+  const event = {
+    httpMethod: 'GET',
+    pathParameters: {
+      arn: 'unknown'
+    }
+  };
+
+  const response = await testEndpoint(executionEndpoint, event, (r) => r);
+  t.is(response.statusCode, 400);
+  const { message } = JSON.parse(response.body);
+  t.true(message.includes('No record found for'));
+});
