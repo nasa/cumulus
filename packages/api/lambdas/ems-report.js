15e6e73cfe4b2b55d80f1556d69f8d955d6e3e1f
Jenny Liu <jenny.h.liu@nasa.gov>
2018-05-17 13:35:27 -0400

    update based on comments

--- packages/api/lambdas/ems-report.js
+++ packages/api/lambdas/ems-report.js
@@ -8,15 +8,17 @@ const { generateReports } = require('../lib/ems');
  *
  * @param {Object} event - event passed to lambda
  * @param {Object} context - AWS Lambda context
- * @param {*} callback - callback function
- * @returns {undefined} - none
+ * @param {function} callback - callback function
+ * @returns {Promise} - list of report type and its file path {reportType, file}
  */
 function handler(event, context, callback) {
   // 24-hour period ending past midnight
   const endTime = moment.utc().startOf('day').toDate().toUTCString();
   const startTime = moment.utc().subtract(1, 'days').startOf('day').toDate()
     .toUTCString();
-  return generateReports(startTime, endTime).then((r) => callback(null, r)).catch(callback);
+  return generateReports(startTime, endTime)
+    .then((r) => callback(null, r))
+    .catch(callback);
 }
 
 module.exports = {
