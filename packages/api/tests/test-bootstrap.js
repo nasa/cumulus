e258cacc3e0a58dddd432463bfcb7a2256686e44
Lauren Frederick <lpfreder@mbp-lpfreder.edn.ecs.nasa.gov>
2018-05-31 12:01:28 -0400

    CUMULUS-556 Prototype running migration scripts

--- packages/api/tests/test-bootstrap.js
+++ packages/api/tests/test-bootstrap.js
@@ -7,6 +7,7 @@ const { Search } = require('../es/search');
 const mappings = require('../models/mappings.json');
 const testMappings = require('./data/testEsMappings.json');
 const mappingsSubset = require('./data/testEsMappingsSubset.json');
+const { s3 } = require('@cumulus/common/aws');
 
 let esClient;
 
@@ -72,4 +73,33 @@ test.serial('Missing types added to index', async (t) => {
   );
 
   await esClient.indices.delete({ index: indexName });
+});
+
+async function migrationScript(x) {
+  console.log('params: ' + x);
+}
+
+
+test.serial('', async (t) => {
+  process.env.internal = randomString();
+  process.env.stackName = 'test-stack';
+  const Key = `${process.env.stackName}/migrations.txt`;
+
+  await s3().createBucket({ Bucket: process.env.internal }).promise();
+
+  const scripts = [
+    { script: 'script1', timestamp: '6/1/18' },
+    { script: 'script2', timestamp: '6/1/18' }
+  ];
+
+  await s3().putObject({
+    Bucket: process.env.internal,
+    Key,
+    Body: JSON.stringify(scripts)
+  }).promise();
+
+  await bootstrap.runMigrations({ testMigration: { script: migrationScript, params: 'script' } });
+
+  await s3().deleteObject({ Bucket: process.env.internal, Key }).promise();
+  await s3().deleteBucket({ Bucket: process.env.internal }).promise();
 });
