4b9e720aaec8c960d76a87f793c6eb210a09d30e
Jacob Campbell <jacob.campbell.ctr@usgs.gov>
2018-06-06 14:21:30 -0500

    removing unused commands and organizing the test a bit better

--- tasks/sync-granule/tests/sync_granule_test.js
+++ tasks/sync-granule/tests/sync_granule_test.js
@@ -276,21 +276,19 @@ test('download granule with checksum in file from an HTTP endpoint', async (t) =
 test('download granule with bad checksum in file from HTTP endpoint throws', async(t) => {
   const granuleChecksumValue = 8675309;
 
+  // Give it a bogus checksumValue to prompt a failure in validateChecksumFile
+  t.context.event.input.granules[0].files[0].checksumValue = granuleChecksumValue;
   t.context.event.config.provider = {
     id: 'MODAPS',
     protocol: 'http',
     host: 'http://localhost:3030'
   };
 
-  // Give it a bogus checksumValue to prompt a failure in validateChecksumFile
-  t.context.event.input.granules[0].files[0].checksumValue = granuleChecksumValue;
-
   validateConfig(t, t.context.event.config);
   validateInput(t, t.context.event.input);
 
   try {
     // Stage the files to be downloaded
-    const sourceDir = path.join(await findTestDataDirectory(), 'granules');
     const granuleFilename = t.context.event.input.granules[0].files[0].name;
     const granuleChecksumType = t.context.event.input.granules[0].files[0].checksumType;
     const errorMessage = `Invalid checksum for ${granuleFilename} with type ${granuleChecksumType} and value ${granuleChecksumValue}`;
