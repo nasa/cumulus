a60039f8fcb81ad15d7bf3873199d5573e086048
Jacob <ifestus@protonmail.com>
2018-06-12 12:46:24 -0500

    [LPCUMULUS-220] (#382)
    * [LPCUMULUS-220] Add a test to sync-granules that ensures expected functionality when a granule with an incorrect checksumValue is encountered
    
    * removing unused commands and organizing the test a bit better
    
    * removing a logging statement and a try-catch statement to skip test in sync_granule_tests

--- tasks/sync-granule/tests/sync_granule_test.js
+++ tasks/sync-granule/tests/sync_granule_test.js
@@ -299,6 +299,28 @@ test('download granule with checksum in file from an HTTP endpoint', async (t) =
   }
 });
 
+test('download granule with bad checksum in file from HTTP endpoint throws', async(t) => {
+  const granuleChecksumValue = 8675309;
+
+  // Give it a bogus checksumValue to prompt a failure in validateChecksumFile
+  t.context.event.input.granules[0].files[0].checksumValue = granuleChecksumValue;
+  t.context.event.config.provider = {
+    id: 'MODAPS',
+    protocol: 'http',
+    host: 'http://localhost:3030'
+  };
+
+  validateConfig(t, t.context.event.config);
+  validateInput(t, t.context.event.input);
+
+  // Stage the files to be downloaded
+  const granuleFilename = t.context.event.input.granules[0].files[0].name;
+  const granuleChecksumType = t.context.event.input.granules[0].files[0].checksumType;
+  const errorMessage = `Invalid checksum for ${granuleFilename} with type ${granuleChecksumType} and value ${granuleChecksumValue}`;
+
+  await t.throws(syncGranule(t.context.event), errorMessage);
+});
+
 test('validate file properties', async (t) => {
   t.context.event.config.provider = {
     id: 'MODAPS',
