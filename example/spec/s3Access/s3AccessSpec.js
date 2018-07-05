b7e0521fd71ad217dc720c105981504e941c45f8
Scisco <scisco7@gmail.com>
2018-07-05 14:27:42 -0400

    move configs to app/config.yml

--- example/spec/s3Access/s3AccessSpec.js
+++ example/spec/s3Access/s3AccessSpec.js
@@ -4,7 +4,7 @@ const { s3 } = require('@cumulus/common/aws');
 
 const config = loadConfig();
 
-const testBucket = `${config.prefix}-s3directaccess`;
+const testBucket = `${config.stackName}-s3directaccess`;
 
 /**
  * Calls the s3AccessTest lambda in the given region, which returns
