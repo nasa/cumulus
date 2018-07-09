64d83a28662c995fc55623d22c2189c800f08502
Scisco <scisco7@gmail.com>
2018-07-09 09:53:09 -0400

    read account id from config

--- example/spec/helpers/testUtils.js
+++ example/spec/helpers/testUtils.js
@@ -47,7 +47,7 @@ function templateFile({ inputTemplateFilename, config }) {
   const inputTemplate = JSON.parse(fs.readFileSync(inputTemplateFilename));
   const templatedInput = lodash.merge(lodash.cloneDeep(inputTemplate), config);
   let jsonString = JSON.stringify(templatedInput, null, 2);
-  jsonString = jsonString.replace('{{AWS_ACCOUNT_ID}}', process.env.AWS_ACCOUNT_ID);
+  jsonString = jsonString.replace('{{AWS_ACCOUNT_ID}}', config.AWS_ACCOUNT_ID);
   const templatedInputFilename = inputTemplateFilename.replace('.template', '');
   fs.writeFileSync(templatedInputFilename, jsonString);
   return templatedInputFilename;
