a5aa73de89b63c1ef34e6b05df23f97d2484fa25
Matt Savoie <savoie@nsidc.org>
2018-07-12 11:00:23 -0600

    Creates a Stream before tests and Tears it down at the end.

--- example/spec/setup/CreatesTestResources.js
+++ example/spec/setup/CreatesTestResources.js
@@ -1,13 +1,13 @@
 
 const { loadConfig } = require('../helpers/testUtils');
-const { createNewTestStream, waitForActiveStream } = require('../helpers/kinesisHelpers');
+const { createOrUseTestStream, waitForActiveStream } = require('../helpers/kinesisHelpers');
 const config = loadConfig();
 
 jasmine.DEFAULT_TIMEOUT_INTERVAL = 550000;
 
 describe('Creates Necessary Test Resources', () => {
   it('Creates a kinesis test stream.', async () => {
-    await createNewTestStream(config.streamName);
+    await createOrUseTestStream(config.streamName);
   });
 
   it('Waits for the stream to be active', async () => {
