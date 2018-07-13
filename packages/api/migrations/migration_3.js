50e926551cda714baa5a0c9f608fd0a44cbd0c2c
Pat Cappelaere <pat@cappelaere.com>
2018-07-13 11:28:06 -0400

    Modified migration_3 per request

--- packages/api/migrations/migration_3.js
+++ packages/api/migrations/migration_3.js
@@ -5,6 +5,27 @@ const { CollectionConfigStore } = require('../../common');
 const { constructCollectionId } = require('../lib/utils');
 
 /**
+ * Create correct file in s3 using version number
+ *
+ * @param {Object} file - File Object from S3
+ * @param {string} prefix - S3 key prefix
+ * @param {string} bucket - S3 bucket name
+ * @param {Object} collectionConfigStore - CollectionConfigStore Object
+ * @returns {null} - N/A
+ */
+async function migrateCollection(file, prefix, bucket, collectionConfigStore) {
+  const coll = await s3.getObject(file.Bucket, file.Key);
+  const item = JSON.parse(coll.Body.toString());
+
+  const collectionId = constructCollectionId(item.dataType, item.dataVersion);
+  const key = `${prefix}/${collectionId}.json`;
+
+  if (!s3.fileExists(bucket, key)) {
+    await collectionConfigStore.put(item.dataType, item.version, item);
+  }
+}
+
+/**
  * Migrates exisiting collections in DynamoDB to versioned collections
  * i.e datatype___version.json in s3://internalBucket/stackName/collections/
  *
@@ -12,26 +33,14 @@ const { constructCollectionId } = require('../lib/utils');
  * @returns {Promise<string>} test message
  */
 async function run(options) {
-  const stackName = process.env.stackName;
-  const bucket = process.env.internal;
+  const stackName = options.stackName;
+  const bucket = options.internal;
   const prefix = `${stackName}/collections`;
 
   const collectionConfigStore = new CollectionConfigStore(bucket, stackName);
 
-  async function migrateCollection(file) {
-    const coll = await s3.getObject(file.Bucket, file.Key);
-    const item = JSON.parse(coll.Body.toString());
-
-    const collectionId = constructCollectionId(item.dataType, item.dataVersion);
-    const key = `${prefix}/${collectionId}.json`;
-
-    if (!s3.fileExists(bucket, key)) {
-      await collectionConfigStore.put(item.dataType, item.version, item);
-    }
-  }
-
   const collObjects = await s3.listS3Objects(bucket, prefix);
-  const promises = collObjects.map(migrateCollection);
+  const promises = collObjects.map(migrateCollection, prefix, bucket, collectionConfigStore);
   await Promise.all(promises);
 
   return options;
