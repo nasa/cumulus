3ad8b444ba26c40ee98b53b0cfd4a9d26c55ba04
Marc Huffnagle <marc@element84.com>
2018-07-24 15:37:42 -0400

    Refactor and add comments

--- packages/api/ecs/async-operation/index.js
+++ packages/api/ecs/async-operation/index.js
@@ -9,6 +9,11 @@ const fs = require('fs');
 const https = require('https');
 const url = require('url');
 
+/**
+ * Return a list of environment variables that should be set but aren't
+ *
+ * @returns {Array<string>} a list of missing environment variables
+ */
 function missingEnvironmentVariables() {
   return [
     'asyncOperationId',
@@ -18,52 +23,50 @@ function missingEnvironmentVariables() {
   ].filter((key) => process.env[key] === undefined);
 }
 
-async function fetchPayload() {
+/**
+ * Fetch and delete a lambda payload from S3
+ *
+ * @param {string} payloadUrl - the s3:// URL of the payload
+ * @returns {Promise<Object>} a payload that can be passed as the event of a lambda call
+ */
+async function fetchPayload(payloadUrl) {
   const s3 = new AWS.S3();
 
-  // Download and delete the payload
-  const parsedPayloadUrl = url.parse(process.env.payloadUrl);
-  const payloadBucket = parsedPayloadUrl.hostname;
-  const payloadKey = parsedPayloadUrl.path.substring(1);
+  const parsedPayloadUrl = url.parse(payloadUrl);
+  const Bucket = parsedPayloadUrl.hostname;
+  const Key = parsedPayloadUrl.path.substring(1);
 
-  console.log(`Fetching s3://${payloadBucket}/${payloadKey}`);
-
+  console.log(`Fetching ${payloadUrl}`);
   let payloadResponse;
   try {
-    payloadResponse = await s3.getObject({
-      Bucket: payloadBucket,
-      Key: payloadKey
-    }).promise();
+    payloadResponse = await s3.getObject({ Bucket, Key }).promise();
   }
   catch (err) {
-    console.error(`Failed to fetch s3://${payloadBucket}/${payloadKey}: ${err.message}`);
+    console.error(`Failed to fetch ${payloadUrl}: ${err.message}`);
     throw err;
   }
 
-  // console.log('stringified payloadResponse in fetchPayload:', JSON.stringify(payloadResponse, null, 2));
+  console.log(`Deleting ${payloadUrl}`);
+  await s3.deleteObject({ Bucket, Key }).promise();
 
-  const payload = payloadResponse.Body.toString();
-
-  console.log('payload in fetchPayload:', payload);
-
-  // await s3.deleteObject({
-  //   Bucket: payloadBucket,
-  //   Key: payloadKey
-  // }).promise();
-
-  return JSON.parse(payload);
+  return JSON.parse(payloadResponse.Body.toString());
 }
 
-async function getLambdaInfo() {
+/**
+ * Query AWS for the codeUrl, moduleFileName, and moduleFunctionName of a
+ *   Lambda function.
+ *
+ * @param {string} FunctionName - the name of the Lambda function
+ * @returns {Promise<Object>} an object with the codeUrl, moduleFileName, and
+ *   moduleFunctionName
+ */
+async function getLambdaInfo(FunctionName) {
   const lambda = new AWS.Lambda();
 
-  // Fetch info about the lambda function
-  const getFunctionResponse = await lambda.getFunction({
-    FunctionName: process.env.lambdaName
-  }).promise();
+  const getFunctionResponse = await lambda.getFunction({ FunctionName }).promise();
 
-  const handlerId = getFunctionResponse.Configuration.Handler;
-  const [moduleFileName, moduleFunctionName] = handlerId.split('.');
+  const handler = getFunctionResponse.Configuration.Handler;
+  const [moduleFileName, moduleFunctionName] = handler.split('.');
 
   return {
     moduleFileName,
@@ -72,8 +75,14 @@ async function getLambdaInfo() {
   };
 }
 
+/**
+ * Download a lambda function from AWS and extract it to the
+ *   /home/task/lambda-function directory
+ *
+ * @param {string} codeUrl - an https URL to download the lambda code from
+ * @returns {Promise<undefined>} resolves when the code has been downloaded
+ */
 async function fetchLambdaFunction(codeUrl) {
-  // Download the lambda function
   await new Promise((resolve, reject) => {
     const file = fs.createWriteStream('/home/task/fn.zip');
 
@@ -84,56 +93,96 @@ async function fetchLambdaFunction(codeUrl) {
     https.get(codeUrl, (res) => res.pipe(file));
   });
 
-  // Extract the lambda function
-  await exec('unzip -o /home/task/fn.zip -d /home/task/lambda-function');
+  return exec('unzip -o /home/task/fn.zip -d /home/task/lambda-function');
 }
-
-function updateAsyncOperation(names, values, expression) {
+/**
+ * Update an AsyncOperation item in DynamoDB
+ *
+ * For help with parameters, see:
+ * https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/DynamoDB.html#updateItem-property
+ *
+ * @param {string} TableName - the AsyncOperation DynamoDB table
+ * @param {string} id - the id of the AsyncOperation
+ * @param {Object} names - the ExpressionAttributeNames to update
+ * @param {Object} values - the ExpressionAttributeValues to update
+ * @param {string} expression - the UpdateExpression to update
+ * @returns {Promise} resolves when the item has been updated
+ */
+function updateAsyncOperation(TableName, id, names, values, expression) {
   const dynamodb = new AWS.DynamoDB();
 
   return dynamodb.updateItem({
-    TableName: process.env.asyncOperationsTable,
-    Key: {
-      id: { S: process.env.asyncOperationId }
-    },
+    TableName,
+    Key: { id: { S: id } },
     ExpressionAttributeNames: names,
     ExpressionAttributeValues: values,
     UpdateExpression: expression
   }).promise();
 }
 
-function storeOperationSuccess(result) {
-  console.log(`AsyncOperation ${process.env.asyncOperationId} succeeded with result: ${JSON.stringify(result)}`); // eslint-disable-line max-len
+/**
+ * Update DynamoDB with a successful result
+ *
+ * @param {string} TableName - the AsyncOperation DynamoDB table
+ * @param {string} id - the id of the AsyncOperation
+ * @param {Object} result - the result to store.  Will be converted to JSON.
+ * @returns {Promise} resolves when the item has been updated
+ */
+function storeOperationSuccess(TableName, id, result) {
   return updateAsyncOperation(
+    TableName,
+    id,
     { '#S': 'status', '#R': 'result' },
     { ':s': { S: 'SUCCEEDED' }, ':r': { S: JSON.stringify(result) } },
     'SET #S = :s, #R = :r'
   );
 }
 
-function storeOperationFailure(message) {
-  console.log(`AsyncOperation ${process.env.asyncOperationId} failed with error: ${message}`);
+/**
+ * Update DynamoDB with a failed result
+ *
+ * @param {string} TableName - the AsyncOperation DynamoDB table
+ * @param {string} id - the id of the AsyncOperation
+ * @param {string} message - the error message to store
+ * @returns {Promise} resolves when the item has been updated
+ */
+function storeOperationFailure(TableName, id, message) {
   return updateAsyncOperation(
+    TableName,
+    id,
     { '#S': 'status', '#E': 'error' },
     { ':s': { S: 'FAILED' }, ':e': { S: message } },
     'SET #S = :s, #E = :e'
   );
 }
 
+/**
+ * Download and run a Lambda task locally.  On completion, write the results out
+ *   to a DynamoDB table.
+ *
+ * @returns {Promise<undefined>} resolves when the task has completed
+ */
 async function runTask() {
   let lambdaInfo;
   let payload;
 
   try {
-    lambdaInfo = await getLambdaInfo();
+    // Get some information about the lambda function that we'll be calling
+    lambdaInfo = await getLambdaInfo(process.env.lambdaName);
 
+    // Download the task (to the /home/task/lambda-function directory)
     await fetchLambdaFunction(lambdaInfo.codeUrl);
 
-    payload = await fetchPayload();
+    // Fetch the event that will be passed to the lambda function from S3
+    payload = await fetchPayload(process.env.payloadUrl);
   }
   catch (err) {
     console.error(err);
-    await storeOperationFailure(`AsyncOperation failure: ${err.message}`);
+    await storeOperationFailure(
+      process.env.asyncOperationsTable,
+      process.env.asyncOperationId,
+      `AsyncOperation failure: ${err.message}`
+    );
     return;
   }
 
@@ -142,20 +191,27 @@ async function runTask() {
     const task = require(`/home/task/lambda-function/${lambdaInfo.moduleFileName}`); //eslint-disable-line global-require, import/no-dynamic-require, max-len
 
     // Run the lambda function
-    console.log('payload:', payload);
-    console.log('stringified payload:', JSON.stringify(payload, null, 2));
     const result = await task[lambdaInfo.moduleFunctionName](payload);
 
-    await storeOperationSuccess(result);
+    // Write the result out to DynamoDb
+    await storeOperationSuccess(
+      process.env.asyncOperationsTable,
+      process.env.asyncOperationId,
+      result
+    );
   }
   catch (err) {
-    await storeOperationFailure(err.message);
+    await storeOperationFailure(
+      process.env.asyncOperationsTable,
+      process.env.asyncOperationId,
+      err.message
+    );
   }
 }
+
+// Here's where the magic happens ...
 
 // Make sure that all of the required environment variables are set
 const missingVars = missingEnvironmentVariables();
-if (missingVars.length > 0) {
-  console.error('Missing environment variables:', missingVars.join(', '));
-}
-else runTask();
+if (missingVars.length === 0) runTask();
+else console.error('Missing environment variables:', missingVars.join(', '));
--- packages/api/endpoints/bulk-delete.js
+++ packages/api/endpoints/bulk-delete.js
@@ -1,9 +1,7 @@
 'use strict';
 
 const { log } = require('@cumulus/common');
-
 const { AsyncOperation } = require('../models');
-
 const {
   buildLambdaProxyResponse,
   getAuthorizationFailureResponse,
@@ -11,22 +9,28 @@ const {
   notFoundResponse
 } = require('../lib/response');
 
+/**
+ * Start an AsyncOperation that will perform a bulk delete
+ *
+ * @param {Object} params - params
+ * @param {Object} params.asyncOperationModel - an instance of an AsyncOperation
+ *   model
+ * @param {string} params.asyncOperationTaskDefinition - the name or ARN of the
+ *   async-operation ECS task definition
+ * @param {string} params.bulkDeleteLambdaName - the name of the Lambda function
+ *   to be run as an AsyncOperation
+ * @param {string} params.cluster - the name of the ECS cluster
+ * @param {string} params.granuleIds - the granuleIds to be deleted
+ * @returns {Promise<Object>} - a Lambda proxy response
+ */
 async function startBulkDeleteAsyncOperation(params) {
   const {
-    asyncOperationsTable,
+    asyncOperationModel,
     asyncOperationTaskDefinition,
     bulkDeleteLambdaName,
     cluster,
-    granuleIds,
-    stackName,
-    systemBucket
+    granuleIds
   } = params;
-
-  const asyncOperationModel = new AsyncOperation({
-    stackName,
-    systemBucket,
-    tableName: asyncOperationsTable
-  });
 
   const asyncOperation = await asyncOperationModel.start({
     asyncOperationTaskDefinition,
@@ -42,21 +46,34 @@ async function startBulkDeleteAsyncOperation(params) {
   });
 }
 
-async function handler(event, context, _callback) {
-  const getConfigValue = (key) => context[key] || process.env[key];
-
-  // In unit testing, we'll smuggle the table names in using the context.  When
+/**
+ * Handle an API Gateway Lambda Proxy request related to Bulk Deletes
+ *
+ * @param {Object} event - a Lambda Proxy request
+ * @param {Object} context - the Lambda context
+ * @returns {Promise<Object>} - returns a Lambda Proxy response
+ */
+async function handler(event, context) {
+  // In testing, we'll smuggle the table names in using the context.  When
   // this is run in API Gateway, the table names will be set using environment
   // variables
-  const asyncOperationsTable = getConfigValue('AsyncOperationsTable');
-  const asyncOperationTaskDefinition = getConfigValue('AsyncOperationTaskDefinition');
-  const bulkDeleteLambdaName = getConfigValue('BulkDeleteLambda');
-  const stackName = getConfigValue('stackName');
-  const systemBucket = getConfigValue('systemBucket');
-  const usersTable = getConfigValue('UsersTable');
-  const cluster = getConfigValue('EcsCluster');
+  const getConfigValue = (key) => {
+    const value = context[key] || process.env[key];
+    if (!value) throw new Error(`${key} must be set.`);
+    return value;
+  };
 
   try {
+    // If any of these are missing, an exception will be thrown and a 500
+    // response will be returned to the caller.
+    const asyncOperationsTable = getConfigValue('AsyncOperationsTable');
+    const asyncOperationTaskDefinition = getConfigValue('AsyncOperationTaskDefinition');
+    const bulkDeleteLambdaName = getConfigValue('BulkDeleteLambda');
+    const stackName = getConfigValue('stackName');
+    const systemBucket = getConfigValue('systemBucket');
+    const usersTable = getConfigValue('UsersTable');
+    const cluster = getConfigValue('EcsCluster');
+
     // Verify the user's credentials
     const authorizationFailureResponse = await getAuthorizationFailureResponse({
       usersTable,
@@ -64,21 +81,28 @@ async function handler(event, context, _callback) {
     });
     if (authorizationFailureResponse) return authorizationFailureResponse;
 
+    // Figure out where to route the request
     if (event.httpMethod === 'POST') {
+      const asyncOperationModel = new AsyncOperation({
+        stackName,
+        systemBucket,
+        tableName: asyncOperationsTable
+      });
+
       return await startBulkDeleteAsyncOperation({
-        asyncOperationsTable,
+        asyncOperationModel,
         asyncOperationTaskDefinition,
         bulkDeleteLambdaName,
         cluster,
-        stackName,
-        systemBucket,
         granuleIds: JSON.parse(event.body).granuleIds
       });
     }
 
+    // If nothing matched, return a 404 response
     return notFoundResponse;
   }
   catch (err) {
+    // If an exception was thrown, log it and return an Internal Server Error
     log.error(err);
     return internalServerErrorResponse;
   }
