d7510bb03f6ccab7c763ca4505befc56eff206f7
Marc Huffnagle <marc@element84.com>
2018-07-24 16:23:25 -0400

    Add tests for the AsyncOperation model

--- packages/api/tests/models/test-async-operation-model.js
+++ packages/api/tests/models/test-async-operation-model.js
@@ -15,64 +15,42 @@ test.before(async () => {
 
 test.after.always(() => asyncOperationModel.deleteTable());
 
-test.serial('The AsyncOperation create() method generates a UUID for the id field', async (t) => {
-  const asyncOperation = await asyncOperationModel.create({});
-
-  t.truthy(asyncOperation.id.match(/^[0-9a-z]{8}-[0-9a-z]{4}-[0-9a-z]{4}-[0-9a-z]{4}-[0-9a-z]{12}/));
+test('The AsyncOperation constructor requires that stackName be specified', (t) => {
+  try {
+    new AsyncOperation({ // eslint-disable-line no-new
+      systemBucket: 'asdf',
+      tableName: 'asdf'
+    });
+    t.fail('stackName should be required');
+  }
+  catch (err) {
+    t.is(err instanceof TypeError);
+    t.is(err.message, 'stackName is required');
+  }
 });
 
-test.serial('The AsyncOperation create() method writes the record to DynamoDB', async (t) => {
-  const asyncOperation = await asyncOperationModel.create({});
-
-  const getItemResponse = await dynamodb().getItem({
-    TableName: asyncOperationModel.tableName,
-    Key: {
-      id: {
-        S: asyncOperation.id
-      }
-    }
-  }).promise();
-
-  t.notDeepEqual(getItemResponse, {}); // Returned if the item was not found
-  t.is(getItemResponse.Item.id.S, asyncOperation.id);
+test('The AsyncOperation constructor requires that systemBucket be specified', (t) => {
+  try {
+    new AsyncOperation({ // eslint-disable-line no-new
+      stackName: 'asdf',
+      tableName: 'asdf'
+    });
+    t.fail('systemBucket should be required');
+  }
+  catch (err) {
+    t.is(err instanceof TypeError);
+    t.is(err.message, 'systemBucket is required');
+  }
 });
 
-test.serial('The AsyncOperation create() method sets an initial status to "CREATED"', async (t) => {
-  const { id: asyncOperationId } = await asyncOperationModel.create({});
+test.todo('The AsyncAdapter.start() method uploads the payload to S3');
 
-  const { status } = await asyncOperationModel.get(asyncOperationId);
+test.todo('The AsyncAdapter.start() method starts an ECS task with the correct parameters');
 
-  t.is(status, 'CREATED');
-});
+test.todo('The AsyncAdapter.start() method throws an exception if runTask() returned failures');
 
-test.serial('The AsyncOperation get() method returns the correct async operation', async (t) => {
-  const { id: asyncOperationId } = await asyncOperationModel.create({});
+test.todo('The AsyncAdapter.start() method writes a new record to DynamoDB');
 
-  const asyncOperation = await asyncOperationModel.get(asyncOperationId);
+test.todo('The AsyncAdapter.start() method sets the record status to "CREATED"');
 
-  t.is(asyncOperation.id, asyncOperationId);
-});
-
-test.serial('The AsyncOperation wrapTask() method updates the item in the DB when the task succeeds', async (t) => {
-  const { id: asyncOperationId } = await asyncOperationModel.create({});
-
-  await asyncOperationModel.wrapTask(asyncOperationId, () => ({ a: 1, b: 2 }));
-
-  const { status, result } = await asyncOperationModel.get(asyncOperationId);
-
-  t.is(status, 'SUCCEEDED');
-  t.deepEqual(result, { a: 1, b: 2 });
-});
-
-test.serial('The AsyncOperation wrapTask() method updates the item in the DB when the task fails', async (t) => {
-  const { id: asyncOperationId } = await asyncOperationModel.create({});
-
-  await asyncOperationModel.wrapTask(asyncOperationId, () => {
-    throw new Error('Complete failure');
-  });
-
-  const { status, errorMessage } = await asyncOperationModel.get(asyncOperationId);
-
-  t.is(status, 'FAILED');
-  t.is(errorMessage, 'Complete failure');
-});
+test.todo('The AsyncAdapter.start() method returns the newly-generated record');
