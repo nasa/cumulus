8ebd40960b512c062b65933bbca951d092facbce
Katherine Kelly <katherine.a.kelly@nasa.gov>
2018-07-26 11:47:54 -0400

    Adding executions endpoint

--- packages/api/endpoints/logs.js
+++ packages/api/endpoints/logs.js
@@ -1,7 +1,12 @@
 'use strict';
 
+const _get = require('lodash.get');
 const handle = require('../lib/response').handle;
 const { Search } = require('../es/search');
+const AWS = require('aws-sdk');
+const {
+  aws: { cloudwatchlogs }
+} = require('@cumulus/common');
 
 function count(event, cb) {
   return cb(null, {});
@@ -14,11 +19,34 @@ function list(event, cb) {
   });
 }
 
+/**
+ * Query logs from a single function.
+ *
+ * @param {Object} event - aws lambda event object.
+ * @param {callback} cb - aws lambda callback function
+ * @returns {undefined} undefined
+ */
+function get(event, cb) {
+  const executionName = event.pathParameters.executionName;
+
+  const search = new Search({
+    queryStringParameters: {
+      limit: 50,
+      executions: executionName
+    }
+  }, 'logs');
+  return search.query().then((response) => cb(null, response)).catch((e) => {
+    cb(e);
+  });
+}
 
 function handler(event, context) {
   return handle(event, context, true, (cb) => {
     if (event.httpMethod === 'GET' && event.resource === '/stats/logs') {
       return count(event, cb);
+    }
+    else if (event.httpMethod === 'GET' && event.pathParameters.executionName) {
+      return get(event, cb);
     }
 
     return list(event, cb);
--- packages/api/es/search.js
+++ packages/api/es/search.js
@@ -173,7 +173,7 @@ class BaseSearch {
           term: {
             _id: id
           }
-        }
+        } 
       };
 
       logDetails.granuleId = id;
@@ -284,6 +284,7 @@ class BaseSearch {
       if (!this.client) {
         this.client = await this.constructor.es();
       }
+      console.log('params', JSON.stringify(searchParams, null, 2));
       const result = await this.client.search(searchParams);
 
       const response = result.hits.hits.map((s) => s._source);
