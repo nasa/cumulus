99c4c377e77cdc53fe37dea727e3e3dae4317f0c
Lauren Frederick <lpfreder@mbp-lpfreder.edn.ecs.nasa.gov>
2018-07-26 16:53:28 -0400

    Workflow and lambda to wait for deployment

--- example/spec/helpers/workflowUtils.js
+++ example/spec/helpers/workflowUtils.js
@@ -3,9 +3,26 @@
 const fs = require('fs');
 const yaml = require('js-yaml');
 
-
-function loadWorkflowFile(workflowConfigFile = './workflows.yml') {
+/**
+ * Load workflows yml file
+ * 
+ * @param {string} workflowConfigFile - workflow yml file,defaults to './workflows.yml'
+ * @returns {Object} - JS Object representation of yml file
+ */
+function loadWorkflowConfigFile(workflowConfigFile = './workflows.yml') {
   return yaml.safeLoad(fs.readFileSync(workflowConfigFile, 'utf8'));
+}
+
+/**
+ * Convery workflow config JS to yml 
+ *
+ * @param {Object} configJs - configuration as a JS object
+ * @param {string} fileName - file name to save to
+ * @returns {undefined} None
+ */
+function saveWorkflowConfig(configJs, fileName) {
+  const configYaml = yaml.safeDump(configJs);
+  fs.writeFileSync(fileName, configYaml);
 }
 
 /**
@@ -16,7 +33,7 @@ function loadWorkflowFile(workflowConfigFile = './workflows.yml') {
  * @returns {Object} return the workflow configuration
  */
 function getWorkflowConfig(workflowConfigFile, workflowName) {
-  const config = loadWorkflowFile(workflowConfigFile);
+  const config = loadWorkflowConfigFile(workflowConfigFile);
   if (workflowName) return config[workflowName];
   return config;
 }
