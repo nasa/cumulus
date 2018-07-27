4f542f4fa57bde6dda84e65b75f02a3d4a3375cb
Matt Savoie <savoie@nsidc.org>
2018-07-27 08:47:56 -0600

    tests passing.
    mocks AWS.ecs service for testing.

--- packages/api/tests/models/test-async-operation-model.js
+++ packages/api/tests/models/test-async-operation-model.js
@@ -1,19 +1,67 @@
 'use strict';
 
+const sinon = require('sinon');
+const proxyquire = require('proxyquire');
 const test = require('ava');
+const { promisify } = require('util');
+
 const {
-  aws: { dynamodb },
+  aws: { dynamodb, s3, s3Join },
   testUtils: { randomString }
 } = require('@cumulus/common');
+const awsImport = require('@cumulus/common/aws');
 const { AsyncOperation } = require('../../models');
 
 let asyncOperationModel;
+let configParams;
+let awsStub = {};
+let ecsStub = {};
+let ProxyAsyncOperation;
+let runTaskReturn;
+
+const successfulRunTask = {
+  tasks: [{ taskArn: 'a-fake-arn' }], failures: []
+};
+const failureRunTask = {
+  tasks: [{ taskArn: 'another-fake-arn' }], failures: [{ error: 'failure', reason: 'meant to fail.' }]
+};
+
+const startParams = {
+  asyncOperationTaskDefinition: 'aTaskName',
+  cluster: 'aCluster',
+  lambdaName: 'ALambda',
+  payload: { a: 'payload to upload' }
+};
+
 test.before(async () => {
-  asyncOperationModel = new AsyncOperation({ tableName: randomString() });
+  runTaskReturn = successfulRunTask;
+  ecsStub = () => ({
+    runTask: () => ({
+      promise: () => Promise.resolve(runTaskReturn)
+    })
+  });
+
+  awsStub = { aws: awsImport };
+  awsStub.aws.ecs = ecsStub;
+
+  ProxyAsyncOperation = proxyquire('../../models/async-operation.js', { '@cumulus/common/aws': awsStub });
+
+  configParams = {
+    tableName: randomString(),
+    systemBucket: randomString(),
+    stackName: randomString()
+  };
+
+  asyncOperationModel = new ProxyAsyncOperation(configParams);
   await asyncOperationModel.createTable();
+  await s3().createBucket({ Bucket: configParams.systemBucket }).promise();
 });
 
-test.after.always(() => asyncOperationModel.deleteTable());
+test.after.always(() => {
+  asyncOperationModel.deleteTable();
+  s3().deleteBucket({ Bucket: configParams.systemBucket }).promise();
+  // do I need to retore test doubles?
+});
 
 test('The AsyncOperation constructor requires that stackName be specified', (t) => {
   try {
@@ -24,7 +72,7 @@ test('The AsyncOperation constructor requires that stackName be specified', (t) 
     t.fail('stackName should be required');
   }
   catch (err) {
-    t.is(err instanceof TypeError);
+    t.is(err instanceof TypeError, true);
     t.is(err.message, 'stackName is required');
   }
 });
@@ -38,19 +86,35 @@ test('The AsyncOperation constructor requires that systemBucket be specified', (
     t.fail('systemBucket should be required');
   }
   catch (err) {
-    t.is(err instanceof TypeError);
+    t.true(err instanceof TypeError);
     t.is(err.message, 'systemBucket is required');
   }
 });
 
-test.todo('The AsyncAdapter.start() method uploads the payload to S3');
+test('The AsyncOperation.start() method uploads the payload to S3', async (t) => {
+  const asyncOperationRecord = await asyncOperationModel.start(startParams);
 
-test.todo('The AsyncAdapter.start() method starts an ECS task with the correct parameters');
+  const payloadKey = s3Join(configParams.stackName, 'async-operation-payloads', `${asyncOperationRecord.id}.json`);
+  const s3Object = await s3().getObject({ Bucket: configParams.systemBucket, Key: payloadKey }).promise();
 
-test.todo('The AsyncAdapter.start() method throws an exception if runTask() returned failures');
+  t.deepEqual(JSON.parse(s3Object.Body.toString()), startParams.payload);
+});
 
-test.todo('The AsyncAdapter.start() method writes a new record to DynamoDB');
+test.todo('The AsyncOperation.start() method starts an ECS task with the correct parameters');
 
-test.todo('The AsyncAdapter.start() method sets the record status to "CREATED"');
+test('The AsyncOperation.start() method throws an exception if runTask() returned failures', async (t) => {
+  runTaskReturn = failureRunTask;
+  try {
+    await asyncOperationModel.start(startParams);
+    t.fail('AsyncOperation.start() with ECS failures should raise error');
+  }
+  catch (error) {
+    t.is(error.message, 'Failed to start AsyncOperation: meant to fail.');
+  }
+});
 
-test.todo('The AsyncAdapter.start() method returns the newly-generated record');
+test.todo('The AsyncOperation.start() method writes a new record to DynamoDB');
+
+test.todo('The AsyncOperation.start() method sets the record status to "CREATED"');
+
+test.todo('The AsyncOperation.start() method returns the newly-generated record');
