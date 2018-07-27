a7257ace321e13db8ec823ca0bc72ccd062f08a8
Matt Savoie <savoie@nsidc.org>
2018-07-27 13:29:22 -0600

    Blind luck I think.

--- packages/api/tests/models/test-async-operation-model.js
+++ packages/api/tests/models/test-async-operation-model.js
@@ -19,6 +19,8 @@ let awsStub = {};
 let ecsStub = {};
 let ProxyAsyncOperation;
 let runTaskReturn;
+let spyRunTaskFn;
+let runTaskFunction;
 
 const successfulRunTask = {
   tasks: [{ taskArn: 'a-fake-arn' }], failures: []
@@ -37,10 +39,14 @@ const startParams = {
 test.before(async () => {
   runTaskReturn = successfulRunTask;
 
+  runTaskFunction = (params) => ({ //eslint-disable-line no-unused-vars
+    promise: () => Promise.resolve(runTaskReturn)
+  });
+
+  spyRunTaskFn = sinon.spy(runTaskFunction);
+
   ecsStub = () => ({
-    runTask: () => ({
-      promise: () => Promise.resolve(runTaskReturn)
-    })
+    runTask: spyRunTaskFn
   });
 
   awsStub = { aws: awsImport };
@@ -101,8 +107,34 @@ test('The AsyncOperation.start() method uploads the payload to S3', async (t) =>
   t.deepEqual(JSON.parse(s3Object.Body.toString()), startParams.payload);
 });
 
-test.todo('The AsyncOperation.start() method starts an ECS task with the correct parameters');
-// This will need sinon spy for ecs(), and I could use a suggestion since it took me so long to just fake it.
+test('The AsyncOperation.start() method starts an ECS task with the correct parameters', async (t) => {
+  // This will need sinon spy for ecs(), and I could use a suggestion since it took me so long to just fake the current one.
+
+  const asyncOperationRecord = await asyncOperationModel.start(startParams);
+  const payloadKey = s3Join(configParams.stackName, 'async-operation-payloads', `${asyncOperationRecord.id}.json`);
+
+  const expectedCallingArguments = {
+    cluster: startParams.cluster,
+    taskDefinition: startParams.asyncOperationTaskDefinition,
+    launchType: 'EC2',
+    overrides: {
+      containerOverrides: [
+        {
+          name: 'AsyncOperation',
+          environment: [
+            { name: 'asyncOperationId', value: `${asyncOperationRecord.id}` },
+            { name: 'asyncOperationsTable', value: configParams.tableName },
+            { name: 'lambdaName', value: startParams.lambdaName },
+            { name: 'payloadUrl', value: `s3://${configParams.systemBucket}/${payloadKey}` }
+          ]
+        }
+      ]
+    }
+  };
+  t.true(spyRunTaskFn.called);
+  t.true(spyRunTaskFn.calledWith(expectedCallingArguments));
+});
+
 
 test('The AsyncOperation.start() method throws an exception if runTask() returned failures', async (t) => {
   runTaskReturn = failureRunTask;
