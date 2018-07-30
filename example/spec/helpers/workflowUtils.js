6cd4331eb9bd408a30b928459cdf4d0f6dfe48d9
Lauren Frederick <lpfreder@mbp-lpfreder.edn.ecs.nasa.gov>
2018-07-30 10:50:03 -0400

    Integration test for removing a workflow and redeploy [it]

--- example/spec/helpers/workflowUtils.js
+++ example/spec/helpers/workflowUtils.js
@@ -1,20 +1,44 @@
 'use strict';
 
-const fs = require('fs');
+const fs = require('fs-extra');
 const yaml = require('js-yaml');
+const omit = require('lodash.omit');
+
+const workflowsYmFile = './workflows.yml';
+const workflowsYmlCopyFile = './workflowsCopy.yml';
+
+/**
+ * Copy the workflows.yml file to a backup location
+ *
+ * @returns {undefined} none
+ */
+function backupWorkflowsYml() {
+  fs.copyFileSync(workflowsYmFile, workflowsYmlCopyFile);
+}
+
+/**
+ * Copy the workflows.yml back from the backup location. Delete 
+ * the backup workflows file
+ *
+ * @returns {undefined} none
+ */
+function restoreWorkflowsYml() {
+  fs.copyFileSync(workflowsYmlCopyFile, workflowsYmFile);
+  fs.unlinkSync(workflowsYmlCopyFile);
+}
 
 /**
  * Load workflows yml file
- * 
+ *
  * @param {string} workflowConfigFile - workflow yml file,defaults to './workflows.yml'
  * @returns {Object} - JS Object representation of yml file
  */
-function loadWorkflowConfigFile(workflowConfigFile = './workflows.yml') {
+function loadWorkflowConfigFile(workflowConfigFile = workflowsYmFile) {
   return yaml.safeLoad(fs.readFileSync(workflowConfigFile, 'utf8'));
 }
 
 /**
- * Convery workflow config JS to yml 
+ * Convery workflow config JS to yml
  *
  * @param {Object} configJs - configuration as a JS object
  * @param {string} fileName - file name to save to
@@ -38,6 +62,25 @@ function getWorkflowConfig(workflowConfigFile, workflowName) {
   return config;
 }
 
+/**
+ * Remove a workflow from the workflows config file and save the file
+ *
+ * @param {string} workflowName - workflow to remove
+ * @param {string} workflowConfigFile - workflow config file, defaults to 
+ * workflows.yml
+ * @returns {undefined} none
+ */
+function removeWorkflow(workflowName, workflowConfigFile = workflowsYmFile) {
+  let config = loadWorkflowConfigFile(workflowConfigFile);
+
+  config = omit(config, workflowName);
+
+  saveWorkflowConfig(config, workflowConfigFile);
+}
+
 module.exports = {
-  getWorkflowConfig
+  getWorkflowConfig,
+  backupWorkflowsYml,
+  restoreWorkflowsYml,
+  removeWorkflow
 };
