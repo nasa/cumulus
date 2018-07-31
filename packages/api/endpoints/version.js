e1220452c88eac3cc80a730946cf85ec390c4d0a
Aimee Barciauskas <aimee@developmentseed.org>
2018-07-31 11:35:09 -0400

    CCE google oauth (#408)
    **Summary:** Add Google OAuth Token Option
    
    This PR enables users to deploy Cumulus using a google oauth endpoint in place of the default `token` endpoint.
    
    Note: Using Google OAuth requires a project is set up in [Google APIs](https://console.developers.google.com/apis) which is configured with the API Gateway backend endpoint.
    
    ## Changes
    
    * Update token module to `@cumulus/api` to use either google or earth data oauth logic depending on `OAUTH_PROVIDER` environment variable flag.
    
    ## Test Plan
    See **Testing notes** below as well
    
    - [x] Unit tests
    - [x] Adhoc testing
    - [x] Update CHANGELOG

--- packages/api/endpoints/version.js
+++ packages/api/endpoints/version.js
@@ -1,24 +1,21 @@
 'use strict';
 
-const { resp } = require('../lib/response');
+const { buildLambdaProxyResponse } = require('../lib/response');
 const pckg = require('../package.json');
-
 
 /**
  * Returns the response and api versions.
  * This is intended as public endpoint that requires no authentication
  *
  * @function handler
- * @param  {type} event - aws lambda function event
- * @param  {type} context - aws lambda funciton context
  * @returns {type} Http response in json format
  */
-function handler(event, context) {
+function handler() {
   const response = {
     response_version: 'v1',
     api_version: pckg.version
   };
-  return resp(context, null, response);
+  return buildLambdaProxyResponse({ body: response });
 }
 
 module.exports = handler;
