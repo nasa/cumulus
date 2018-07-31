e3647cb7c60c54f634f67c3c20621bec188517c4
Lauren Frederick <lpfreder@mbp-lpfreder.edn.ecs.nasa.gov>
2018-07-31 07:59:01 -0400

    Test for removing task from a workflow [it]

--- example/spec/redeployment/WorkflowRedeploySpec.js
+++ example/spec/redeployment/WorkflowRedeploySpec.js
@@ -1,5 +1,5 @@
-const { 
-  buildAndStartWorkflow, 
+const {
+  buildAndStartWorkflow,
   waitForCompletedExecution,
   api: apiTestUtils
 } = require('@cumulus/integration-tests');
@@ -12,13 +12,64 @@ const {
 const {
   backupWorkflowsYml,
   restoreWorkflowsYml,
-  removeWorkflow
+  removeWorkflow,
+  removeTaskFromWorkflow
 } = require('../helpers/workflowUtils');
 
 const config = loadConfig();
 
-xdescribe('When a workflow is updated and deployed during a workflow execution', () => {
+describe('When a workflow is updated and deployed during a workflow execution', () => {
+  let workflowExecutionArn = null;
+  let workflowStatus = null;
 
+  beforeAll(async () => {
+    backupWorkflowsYml();
+
+    // Kick off the workflow, don't wait for completion
+    workflowExecutionArn = await buildAndStartWorkflow(
+      config.stackName,
+      config.bucket,
+      'WaitForDeployWorkflow'
+    );
+
+    removeTaskFromWorkflow('WaitForDeployWorkflow', 'HelloWorld');
+
+    await redeploy(config);
+
+    workflowStatus = await waitForCompletedExecution(workflowExecutionArn);
+  });
+
+  afterAll(() => {
+    restoreWorkflowsYml();
+  });
+
+  it('the workflow executes successfully', () => {
+    expect(workflowStatus).toEqual('SUCCEEDED');
+  });
+
+  describe('When querying the workflow via the API', () => {
+    let executionStatus;
+
+    beforeAll(async () => {
+      executionStatus = await apiTestUtils.getExecutionStatus({
+        prefix: config.stackName,
+        arn: workflowExecutionArn
+      });
+    });
+
+    it('the execution is returned', () => {
+      expect(executionStatus.execution).toBeTruthy();
+      expect(executionStatus.execution.executionArn).toEqual(workflowExecutionArn);
+    });
+
+    it('the execution steps show the original workflow steps', () => {
+      const helloWorldScheduledEvents = executionStatus.executionHistory.events.filter((event) =>
+        event.type === 'LambdaFunctionScheduled' &&
+        event.lambdaFunctionScheduledEventDetails.resource.includes('HelloWorld'));
+
+      expect(helloWorldScheduledEvents.length).toEqual(1);
+    });
+  });
 });
 
 describe('When a workflow is removed and deployed during a workflow execution', () => {
@@ -60,7 +111,7 @@ describe('When a workflow is removed and deployed during a workflow execution', 
       });
     });
 
-    it('the execution is returned', async () => {
+    it('the execution is returned', () => {
       expect(executionStatus.execution).toBeTruthy();
       expect(executionStatus.execution.executionArn).toEqual(workflowExecutionArn);
     });
