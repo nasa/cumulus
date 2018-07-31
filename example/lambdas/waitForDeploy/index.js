c5d71f60d639d78a1afa49005878c282170c9ef9
Lauren Frederick <lpfreder@mbp-lpfreder.edn.ecs.nasa.gov>
2018-07-31 09:13:41 -0400

    Change how the tests determine if a stack deployed [it]

--- example/lambdas/waitForDeploy/index.js
+++ example/lambdas/waitForDeploy/index.js
@@ -21,8 +21,7 @@ async function handler(event, context, callback) {
   const cloudformation = new CloudFormation();
   const stack = event.meta.stack;
   let retries = 0;
-
-  const lambdaStartTime = new Date();
+  let deployStarted = false;
 
   /* eslint-disable no-await-in-loop */
   while (retries < maxRetries) {
@@ -30,12 +29,12 @@ async function handler(event, context, callback) {
       .promise()
       .catch((err) => callback(err));
 
-    console.log(`stack status: ${JSON.stringify(stackDetails.Stacks[0].StackStatus)}`);
+      console.log(`stack status: ${JSON.stringify(stackDetails.Stacks[0].StackStatus)}`);
 
-    const updateDateTime = new Date(stackDetails.Stacks[0].LastUpdatedTime);
-
-    if (!stackDetails.Stacks[0].StackStatus.includes('IN_PROGRESS') &&
-        updateDateTime > lambdaStartTime) {
+    if (stackDetails.Stacks[0].StackStatus.includes('IN_PROGRESS')) {
+      deployStarted = true;
+    }
+    else if (deployStarted) { // state is not in progress and we know a deploy happened
       callback(null, event);
       return;
     }
