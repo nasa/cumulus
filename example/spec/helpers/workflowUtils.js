32d73e68c76bda399c91db2a3a2d902b017fc000
Lauren Frederick <lpfreder@mbp-lpfreder.edn.ecs.nasa.gov>
2018-07-31 07:59:01 -0400

    Test for removing task from a workflow [it]

--- example/spec/helpers/workflowUtils.js
+++ example/spec/helpers/workflowUtils.js
@@ -2,7 +2,12 @@
 
 const fs = require('fs-extra');
 const yaml = require('js-yaml');
-const omit = require('lodash.omit');
+
+const {
+  get,
+  omit,
+  unset
+} = require('lodash');
 
 const workflowsYmFile = './workflows.yml';
 const workflowsYmlCopyFile = './workflowsCopy.yml';
@@ -17,7 +22,7 @@ function backupWorkflowsYml() {
 }
 
 /**
- * Copy the workflows.yml back from the backup location. Delete 
+ * Copy the workflows.yml back from the backup location. Delete
  * the backup workflows file
  *
  * @returns {undefined} none
@@ -66,7 +71,7 @@ function getWorkflowConfig(workflowConfigFile, workflowName) {
  * Remove a workflow from the workflows config file and save the file
  *
  * @param {string} workflowName - workflow to remove
- * @param {string} workflowConfigFile - workflow config file, defaults to 
+ * @param {string} workflowConfigFile - workflow config file, defaults to
  * workflows.yml
  * @returns {undefined} none
  */
@@ -78,9 +83,44 @@ function removeWorkflow(workflowName, workflowConfigFile = workflowsYmFile) {
   saveWorkflowConfig(config, workflowConfigFile);
 }
 
+/**
+ * Remove a task from the workflow and save the workflow config file. Change any
+ * tasks pointing to the removed task as the 'Next' step to point to the 
+ * following step, or remove the 'Next' step if the removed step was the last step
+ *
+ * @param {*} workflowName - name of the workflow to remove the task from
+ * @param {*} taskName - task name to remove
+ * @param {*} workflowConfigFile - workflow config file, defaults to workflows.yml
+ * @returns {undefined} none
+ */
+function removeTaskFromWorkflow(workflowName, taskName, workflowConfigFile = workflowsYmFile) {
+  const config = loadWorkflowConfigFile(workflowConfigFile);
+
+  unset(config, `${workflowName}.States.${taskName}`);
+
+  const workflowConfig = get(config, workflowName);
+
+  const tasks = Object.keys(workflowConfig.States);
+
+  // Fix the 'Next' task configuration to skip the removed task
+  tasks.forEach((task, index) => {
+    if (workflowConfig.States[task].Next === taskName) {
+      if (index < tasks.length - 1) {
+        workflowConfig.States[task].Next = tasks[index + 1];
+      }
+      else {
+        delete workflowConfig.States[task].Next;
+      }
+    }
+  });
+
+  saveWorkflowConfig(config, workflowConfigFile);
+}
+
 module.exports = {
   getWorkflowConfig,
   backupWorkflowsYml,
   restoreWorkflowsYml,
-  removeWorkflow
+  removeWorkflow,
+  removeTaskFromWorkflow
 };
