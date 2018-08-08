8347678e09e92d1ba79fec6eabaedc803f7b8a47
Jenny Liu <jenny.h.liu@nasa.gov>
2018-08-08 08:55:34 -0400

    stream remote file to s3

--- packages/ingest/test/ftp.js
+++ packages/ingest/test/ftp.js
@@ -4,6 +4,11 @@ const sinon = require('sinon');
 const proxyquire = require('proxyquire');
 const test = require('ava');
 const JSFtp = require('jsftp');
+const { ftpMixin: TestFtpMixin } = require('../ftp');
+const {
+  checksumS3Objects, fileExists, recursivelyDeleteS3Bucket, s3
+} = require('@cumulus/common/aws');
+const { randomString } = require('@cumulus/common/test-utils');
 
 class MyTestDiscoveryClass {
   constructor(useList) {
@@ -47,3 +52,21 @@ test('useList defaults to false when not assigned', async (t) => {
   t.is(jsftpSpy.getCall(0).args[0].useList, false);
 });
 
+test('Download remote file to s3', async (t) => {
+  class MyTestFtpDiscoveryClass extends TestFtpMixin(MyTestDiscoveryClass) {}
+  const myTestFtpDiscoveryClass = new MyTestFtpDiscoveryClass();
+  const bucket = randomString();
+  const key = randomString();
+  try {
+    await s3().createBucket({ Bucket: bucket }).promise();
+    await myTestFtpDiscoveryClass.sync(
+      '/granules/MOD09GQ.A2017224.h27v08.006.2017227165029.hdf', bucket, key
+    );
+    t.truthy(fileExists(bucket, key));
+    const sum = await checksumS3Objects('CKSUM', bucket, key);
+    t.is(sum, 1435712144);
+  }
+  finally {
+    await recursivelyDeleteS3Bucket(bucket);
+  }
+});
