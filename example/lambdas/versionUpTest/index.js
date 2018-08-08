96e2a8613e5c3e30f26c6d1e807e73ecfe912edf
Jonathan Kovarik <jonathan.kovarik@gmail.com>
2018-08-08 13:19:41 -0600

    Update docstring/address PR comments

--- example/lambdas/versionUpTest/index.js
+++ example/lambdas/versionUpTest/index.js
@@ -1,6 +1,13 @@
 'use strict';
 
-// Function that returns constant payload object.  Used for testing lambda modification.
+/**
+ * calls callback with static key/text payload object.  Used for testing lambda modification.
+ * @param {Object} event - AWS event
+ * @param {Object} context - an AWS Lambda context
+ * @param {Function} callback - an AWS Lambda handler
+ * @returns {undefined} - does not return a value
+ */
+
 function handler(event, context, callback) {
   const eventCopy = event;
   eventCopy.payload = { output: 'Current Version' };
--- example/lambdas/versionUpTest/update.js
+++ example/lambdas/versionUpTest/update.js
@@ -1,6 +1,12 @@
 'use strict';
 
-// Function that returns constant key/text payload object.  Used for testing lambda modification.
+/**
+ * calls callback with static key/text payload object.  Used for testing lambda modification.
+ * @param {Object} event - AWS event
+ * @param {Object} context - an AWS Lambda context
+ * @param {Function} callback - an AWS Lambda handler
+ * @returns {undefined} - does not return a value
+ */
 
 function handler(event, context, callback) {
   const eventCopy = event;
