2fa35a69890c20591c7d08921d481bff41e5f86d
Marc Huffnagle <marc@element84.com>
2018-08-10 12:38:59 -0400

    Add TravisCI support

---
+++ travis-ci/build-cache.sh
@@ -0,0 +1,63 @@
+#!/bin/sh
+
+set -e
+
+MD5SUM=$(cat $(git ls-files | grep package.json | sort) | md5sum | awk '{print $1}')
+CACHE_FILENAME="${MD5SUM}.tar.gz"
+KEY="travis-ci-cache/${CACHE_FILENAME}"
+DATE=$(date -R)
+
+STRING_TO_SIGN_HEAD="HEAD
+
+
+${DATE}
+/${CACHE_BUCKET}/${KEY}"
+SIGNATURE=$(/bin/echo -n "$STRING_TO_SIGN_HEAD" | openssl sha1 -hmac ${CACHE_AWS_SECRET_ACCESS_KEY} -binary | base64)
+
+CACHE_EXISTS_STATUS_CODE=$(curl \
+  -s \
+  -o /dev/null \
+  -w '%{http_code}' \
+  --head \
+  -H "Host: ${CACHE_BUCKET}.s3.amazonaws.com" \
+  -H "Date: ${DATE}" \
+  -H "Authorization: AWS ${CACHE_AWS_ACCESS_KEY_ID}:${SIGNATURE}" \
+  https://${CACHE_BUCKET}.s3.amazonaws.com/${KEY}
+)
+
+if [ "$CACHE_EXISTS_STATUS_CODE" = "200" ]; then
+  echo "Cache already exists: s3://${CACHE_BUCKET}/${KEY}"
+else
+  echo "Creating cache"
+  npm install
+  npm run bootstrap-no-build
+
+  for p in $(git ls-files | grep package.json); do
+    NODE_MODULES_DIR="$(dirname $p)/node_modules"
+    if [ -d "$NODE_MODULES_DIR" ]; then
+      tar -rf "${MD5SUM}.tar" "$(dirname $p)/node_modules"
+    fi
+  done
+
+  gzip "${MD5SUM}.tar"
+
+  # pip install awscli --upgrade --user
+
+  echo "Uploading cache"
+  STRING_TO_SIGN_PUT="PUT
+
+
+${DATE}
+/${CACHE_BUCKET}/${KEY}"
+  SIGNATURE=$(/bin/echo -n "$STRING_TO_SIGN_PUT" | openssl sha1 -hmac ${CACHE_AWS_SECRET_ACCESS_KEY} -binary | base64)
+
+  curl \
+    -X PUT \
+    -T "$CACHE_FILENAME" \
+    -H "Host: ${CACHE_BUCKET}.s3.amazonaws.com" \
+    -H "Date: ${DATE}" \
+    -H "Authorization: AWS ${CACHE_AWS_ACCESS_KEY_ID}:${SIGNATURE}" \
+    https://${CACHE_BUCKET}.s3.amazonaws.com/${KEY}
+
+  # aws s3 cp --no-progress "$CACHE_FILENAME" "s3://${CACHE_BUCKET}/${KEY}"
+fi
---
+++ travis-ci/fetch-cache.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+
+set -e
+
+MD5SUM=$(cat $(git ls-files | grep package.json | sort) | md5sum | awk '{print $1}')
+CACHE_FILENAME="${MD5SUM}.tar.gz"
+KEY="travis-ci-cache/${CACHE_FILENAME}"
+DATE=$(date -R)
+
+STRING_TO_SIGN_HEAD="HEAD
+
+
+${DATE}
+/${CACHE_BUCKET}/${KEY}"
+SIGNATURE=$(/bin/echo -n "$STRING_TO_SIGN_HEAD" | openssl sha1 -hmac ${CACHE_AWS_SECRET_ACCESS_KEY} -binary | base64)
+
+CACHE_EXISTS_STATUS_CODE=$(curl \
+  -s \
+  -o /dev/null \
+  -w '%{http_code}' \
+  --head \
+  -H "Host: ${CACHE_BUCKET}.s3.amazonaws.com" \
+  -H "Date: ${DATE}" \
+  -H "Authorization: AWS ${CACHE_AWS_ACCESS_KEY_ID}:${SIGNATURE}" \
+  https://${CACHE_BUCKET}.s3.amazonaws.com/${KEY}
+)
+
+if [ "$CACHE_EXISTS_STATUS_CODE" = "200" ]; then
+  echo "Fetching cache"
+
+  STRING_TO_SIGN_GET="GET
+
+
+${DATE}
+/${CACHE_BUCKET}/${KEY}"
+  SIGNATURE=$(/bin/echo -n "$STRING_TO_SIGN_GET" | openssl sha1 -hmac ${CACHE_AWS_SECRET_ACCESS_KEY} -binary | base64)
+
+  curl \
+    -H "Host: ${CACHE_BUCKET}.s3.amazonaws.com" \
+    -H "Date: ${DATE}" \
+    -H "Authorization: AWS ${CACHE_AWS_ACCESS_KEY_ID}:${SIGNATURE}" \
+    https://${CACHE_BUCKET}.s3.amazonaws.com/${KEY} |\
+  tar -xz
+else
+  echo "No cache found"
+fi
