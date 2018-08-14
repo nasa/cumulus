cfbdadbf560813640c9d5c4cff80196383dfe4a9
Marc Huffnagle <marc@element84.com>
2018-08-14 11:40:11 -0400

    Add "release-" previs to NPM tags

--- travis-ci/npm-tag.js
+++ travis-ci/npm-tag.js
@@ -8,7 +8,9 @@ const semver = require('semver');
 
 // Given a git tag (process.env.TRAVIS_TAG), this function determines what NPM
 // tag to apply to a new release.  More simply, this determines if the NPM tag
-// should be "latest", or just the value of the git tag.
+// should be "latest", or just the value of the git tag.  Tags other than
+// "latest" will be prefixed with "release-", since NPM tags are not allowed to
+// be valid semantic versions.
 //
 // - If the git tag does not begin with the letter "v" followed by a number then
 //   this is not a new "latest" release and the git tag is returned.
@@ -27,20 +29,21 @@ async function getNpmTag() {
   const thisTag = process.env.TRAVIS_TAG;
   if (!thisTag) throw new Error('TRAVIS_TAG is not set');
 
-  // Official release tags start with "v" so, if this tag does not start with
-  // "v", then it is definitely not going to be "latest".
-  if (thisTag.match(/^v(\d.*)/)) {
-    const lernaConfig = JSON.parse(await fs.readFile('lerna.json', 'utf8'));
-    const thisVersion = lernaConfig.version;
+  const lernaConfig = JSON.parse(await fs.readFile('lerna.json', 'utf8'));
+  const thisVersion = lernaConfig.version;
 
-    // If this is a pre-release version, we'll use the given tag
-    if (semver.prerelease(thisVersion) !== null) return thisTag;
+  const latestVersion = await getLatestVersion('@cumulus/common', { version: 'latest' });
 
-    const latestVersion = await getLatestVersion('@cumulus/common', { version: 'latest' });
-    return (semver.gt(thisVersion, latestVersion)) ? 'latest' : thisTag;
-  }
+  if (
+    // tag starts with "v" and a digit
+    thisTag.match(/^v(\d.*)/)
+    // the version in lerna.json is not a pre-release version
+    && !semver.prerelease(thisVersion)
+    // the version in lerna.json is greater than the currently published "latest" version
+    && semver.gt(thisVersion, latestVersion)
+  ) return 'latest';
 
-  return thisTag;
+  return `release-${thisTag}`;
 }
 
 getNpmTag()
