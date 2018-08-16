a4d2703293a67be903e886cf9a81942755e49b67
Lauren Frederick <lpfreder@mbp-lpfreder.edn.ecs.nasa.gov>
2018-08-16 13:04:02 -0400

    Undo changes from previous commit

--- example/spec/setup/PopulateProvidersCollections.js
+++ example/spec/setup/PopulateProvidersCollections.js
@@ -15,7 +15,6 @@ const rulesDirectory = './data/rules';
 
 const s3data = [
   '@cumulus/test-data/pdrs/MOD09GQ_1granule_v3.PDR',
-  '@cumulus/test-data/pdrs/MOD09GQ_1granule_v3_2.PDR',
   '@cumulus/test-data/granules/MOD09GQ.A2016358.h13v04.006.2016360104606.hdf.met',
   '@cumulus/test-data/granules/MOD09GQ.A2016358.h13v04.006.2016360104606.hdf',
   '@cumulus/test-data/granules/MOD09GQ.A2016358.h13v04.006.2016360104606_ndvi.jpg',
