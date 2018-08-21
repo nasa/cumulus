4d5ce0468fcabb83c503b5d6f2b958155f267075
Jonathan Kovarik <jonathan.kovarik@gmail.com>
2018-08-21 13:24:17 -0600

    Refactor/lint

--- packages/deployment/test_resources.js
+++ packages/deployment/test_resources.js
@@ -2,84 +2,110 @@
 
 'use strict';
 
-const fs = require('fs-extra');
-const path = require('path');
-const util = require('util');
-const utils = require('kes').utils;
-const yauzl = require('yauzl');
 const AWS = require('aws-sdk');
 
 const { compact, uniq } = require('lodash');
-const { Lambda } = require('kes');
 
-
-async function getAllActiveLambdaResources() {
-  // TODO: Pull this from config
-  AWS.config.update({region: 'us-east-1'});
-  const sf = new AWS.StepFunctions();
-  let filteredExecutionObjects = [];
-  let executionArnList = [];
-  let lambdaResources;
-
-  try {
-    // TODO: Genercize getAllExecutions to get all state machines if needed.
-    let stateMachinesResults = await sf.listStateMachines({maxResults: 1000}).promise();
-    // TODO: use defined deployment name here, once we know where to integrate
-    let stateMachines = stateMachinesResults.stateMachines.filter(sm => sm.name.includes('jkCumulus'));
-    let stateMachineArns = stateMachines.map(x => x.stateMachineArn);
-    let listExecutionsPromises = stateMachineArns.map(x => sf.listExecutions({stateMachineArn: x, statusFilter: 'RUNNING'}).promise());
-    let executionObjects = await Promise.all(listExecutionsPromises);
-    // Filter objects with no executions.   We could -probably- do this in get all executions.
-    executionObjects.map(x => {if(x.executions.length > 0) filteredExecutionObjects.push(x);});
-    let feoPromiseList = filteredExecutionObjects.map(feo => getAllExecutions(feo, sf));
-    let allExecutionArns = await Promise.all(feoPromiseList);
-    allExecutionArns.map(x => {executionArnList = executionArnList.concat(x);});
-    lambdaResources = await getLambdaResourceArns(executionArnList, sf);
-  } catch (e) {
-    console.log(e);
-    throw(e);
-  }
-  return lambdaResources;
+// Given a stateMachineDefinitionObject, return a list of lambdas
+function parseStepFunctionDefinition(stateMachineDefinitionObject) {
+  const definition = JSON.parse(stateMachineDefinitionObject.definition);
+  return compact(Object.keys(definition.States).map((key) => definition.States[key].Resource));
 }
 
-// Given an execution list from above, returns unique lambda resources in use
+// Given a list of state machine execution ARNs,
+// returns qualifed ARN for all lambda resources in use
 async function getLambdaResourceArns(executionArnList, sf) {
-  let lambdaResourceArns = [];
-  let executionPromises = [];
-  console.log(`executionArnList is ${executionArnList}`);
+  const executionPromises = [];
   executionArnList.forEach((arn) => {
-    executionPromises.push(sf.describeStateMachineForExecution({executionArn: arn}).promise());
+    executionPromises.push(sf.describeStateMachineForExecution({ executionArn: arn }).promise());
   });
 
-  let stepFunctionDefinitions = await Promise.all(executionPromises);
+  const stepFunctionDefinitions = await Promise.all(executionPromises);
   console.log(`Step FunctionDefinitions are ${JSON.stringify(stepFunctionDefinitions)}`);
-  let stepFunctionResources = stepFunctionDefinitions.map(definitionObject => parseStepFunctionDefinition(definitionObject));
-  return uniq([].concat.apply([], stepFunctionResources));
-}
-
-// Given a stateMachineDefinitionObject, return a list of lambdas in use
-function parseStepFunctionDefinition(stateMachineDefinitionObject) {
-  let definition = JSON.parse(stateMachineDefinitionObject.definition);
-  return compact(Object.keys(definition.States).map(key => definition.States[key].Resource));
+  const stepFunctionResources = stepFunctionDefinitions.map(
+    (definitionObject) => parseStepFunctionDefinition(definitionObject)
+  );
+  // TODO: Check this works
+  return uniq([].concat(...stepFunctionResources));
 }
 
 // For a given executionObject(first page of executions), get all executions
-async function getAllExecutions(executionObject, sf2) {
+async function getAllExecutions(executionObject, sf2, statusFilter) {
   let executionsList = [];
-  let stateMachineArn = executionObject.executions[0].stateMachineArn;
-  executionsList = executionsList.concat(executionObject.executions.map(execution => execution.executionArn));
-  if(executionObject.nextToken) {
+  const stateMachineArn = executionObject.executions[0].stateMachineArn;
+  executionsList = executionsList.concat(
+    executionObject.executions.map((execution) => execution.executionArn)
+  );
+
+  if (executionObject.nextToken) {
     let nextObject = null;
     try {
-      nextObject = await sf2.listExecutions({stateMachineArn: stateMachineArn, statusFilter: 'RUNNING', nextToken: executionObject.nextToken}).promise();
-    } catch (e) {
-      console.log(`Error in listExecutionsQuery:  ${e}`);
-      throw(e);
+      nextObject = await sf2.listExecutions(
+        {
+          stateMachineArn: stateMachineArn,
+          statusFilter: statusFilter,
+          nextToken: executionObject.nextToken
+        }
+      ).promise();
     }
-    let executionsResult = await getAllExecutions(nextObject, sf2);
-    let executionList = executionsList.concat(executionsResult);
+    catch (e) {
+      console.log(`Error in listExecutionsQuery:  ${e}`);
+      throw (e);
+    }
+    const executionsResult = await getAllExecutions(nextObject, sf2, statusFilter);
+    executionsList = executionsList.concat(executionsResult);
   }
   return executionsList;
 }
 
-module.exports = getAllActiveLambdaResources;
+// TODO: Docstring
+async function getAllActiveLambdaArns(region, deploymentName) {
+  AWS.config.update({ region: region });
+  const sf = new AWS.StepFunctions();
+  const statusFilter = 'RUNNING';
+
+  let executionArnList = [];
+  let lambdaResources;
+
+  try {
+    // TODO: We should genercize getAllExecutions to get all state machines as well.
+    const stateMachinesResults = await sf.listStateMachines({ maxResults: 1000 }).promise();
+
+    // Get list of stateMachines with names matching the current deployment.
+    const stateMachines = stateMachinesResults.stateMachines.filter(
+      (sm) => sm.name.includes(deploymentName)
+    );
+    const stateMachineArns = stateMachines.map((x) => x.stateMachineArn);
+
+    // Get a list of 'execution objects', containing the first page of sf.ListExecutions objects
+    const listExecutionsPromises = stateMachineArns.map(
+      (x) => sf.listExecutions({ stateMachineArn: x, statusFilter: statusFilter }).promise()
+    );
+    const executionObjects = await Promise.all(listExecutionsPromises);
+    const filteredExecutionObjects = executionObjects.filter((x) => x.executions.length > 0);
+
+    // Get all execuions for each execution list header(execution object)
+    const feoPromiseList = filteredExecutionObjects.map(
+      (feo) => getAllExecutions(feo, sf, statusFilter)
+    );
+
+    const allExecutionArns = await Promise.all(feoPromiseList);
+
+    //  For all execution ARNs for all state machines, return a list of unique
+    // lambda resource ARNs
+
+    // eslint-disable-next-line array-callback-return
+    allExecutionArns.map((x) => {
+      executionArnList = executionArnList.concat(x);
+    });
+    lambdaResources = await getLambdaResourceArns(executionArnList, sf);
+  }
+  catch (e) {
+    // TODO: Add cumulus logging
+    console.log(e);
+    throw (e);
+  }
+  return lambdaResources;
+}
+
+module.exports = getAllActiveLambdaArns;
