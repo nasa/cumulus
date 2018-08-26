8ea8b22b2db17373e6fc4adf05cd72330a58e39d
Jonathan Kovarik <jonathan.kovarik@gmail.com>
2018-08-25 21:39:50 -0600

    Add 'original' lambda for redeployment test

---
+++ example/lambdas/versionUpTest/original.js
@@ -0,0 +1,18 @@
+'use strict';
+
+/**
+ * calls callback with static key/text payload object.  Used for testing lambda modification.
+ * @param {Object} event - AWS event
+ * @param {Object} context - an AWS Lambda context
+ * @param {Function} callback - an AWS Lambda handler
+ * @returns {undefined} - does not return a value
+ */
+
+function handler(event, context, callback) {
+  const eventCopy = event;
+  console.log('foo bar');
+  eventCopy.payload = { output: 'Current Version' };
+  callback(null, event);
+}
+
+exports.handler = handler;
