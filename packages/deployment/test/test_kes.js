5589a35317befd4c16388d8cc49ecb9e7a756a0e
Jonathan Kovarik <jonathan.kovarik@gmail.com>
2018-08-26 23:47:40 -0600

    Fix test_kes linting

--- packages/deployment/test/test_kes.js
+++ packages/deployment/test/test_kes.js
@@ -1,4 +1,4 @@
-/* eslint-disable no-console, no-param-reassign */
+/* eslint-disable no-console, no-param-reassign, no-template-curly-in-string, object-curly-spacing*/
 
 'use strict';
 
@@ -6,47 +6,45 @@ const sinon = require('sinon');
 const test = require('ava');
 
 const configFixture = require('./fixtures/config.json');
+const aliasFixture = require('./fixtures/aliases.json');
 const UpdatedKes = require('../lib/kes');
 
-
-test.beforeEach((test) => {
-  test.context.kes = new UpdatedKes(configFixture);
+test.beforeEach((t) => {
+  t.context.kes = new UpdatedKes(configFixture);
 });
 
 function setupKesForLookupLambdaReference(kes, functionName, hashValue) {
-  const  lambdaFixture = {};
+  const lambdaFixture = {};
   lambdaFixture[functionName] = { hash: hashValue };
   kes.config.lambdas = lambdaFixture;
   return kes;
 }
 
-test('lookupLambdaReference returns the expected alias resource string', (test) => {
+test('lookupLambdaReference returns the expected alias resource string', (t) => {
   const resourceString = '${FunctionNameLambdaFunction.Arn}';
-  const kes = setupKesForLookupLambdaReference(test.context.kes,
-                                           'FunctionName',
-                                           'notarealhash');
-  let result = kes.lookupLambdaReference(resourceString);
-  test.is(result, '${FunctionNameLambdaAliasnotarealhash}');
+  const kes = setupKesForLookupLambdaReference(t.context.kes, 'FunctionName', 'notarealhash');
+  const result = kes.lookupLambdaReference(resourceString);
+  t.is(result, '${FunctionNameLambdaAliasnotarealhash}');
 });
 
-test('lookupLambdaReference returns the original ', (test) => {
+test('lookupLambdaReference returns the original ', (t) => {
   const resourceString = '${FunctionNameLambdaFunction.Arn}';
-  const kes = setupKesForLookupLambdaReference(test.context.kes, 'FunctionName', null);
-  let result = kes.lookupLambdaReference(resourceString);
-  test.is(result, resourceString);
+  const kes = setupKesForLookupLambdaReference(t.context.kes, 'FunctionName', null);
+  const result = kes.lookupLambdaReference(resourceString);
+  t.is(result, resourceString);
 });
 
-test('lookupLambdaReference throws error on invalid configuration', (test) => {
+test('lookupLambdaReference throws error on invalid configuration', (t) => {
   const resourceString = '${SomeOtherConfigurationString.foobar}';
-  const kes = setupKesForLookupLambdaReference(test.context.kes, 'FunctionName', null);
-  const error = test.throws(() => {
+  const kes = setupKesForLookupLambdaReference(t.context.kes, 'FunctionName', null);
+  const error = t.throws(() => {
     kes.lookupLambdaReference(resourceString);
   }, Error);
-  test.is(error.message, `Invalid stateObjectResource: ${resourceString}`);
+  t.is(error.message, `Invalid stateObjectResource: ${resourceString}`);
 });
 
-test('injectWorkflowLambdaAliases updates the correct resources', (test) => {
-  let kes = test.context.kes;
+test('injectWorkflowLambdaAliases updates the correct resources', (t) => {
+  const kes = t.context.kes;
 
   kes.config.lambdas = {
     TestLambda: { hash: 'notarealhash' },
@@ -68,7 +66,7 @@ test('injectWorkflowLambdaAliases updates the correct resources', (test) => {
     }
   };
 
-  let expected = {
+  const expected = {
     TestStepFunction: {
       States: {
         1: { Type: 'Task', Resource: '${TestLambdaLambdaAliasnotarealhash}' },
@@ -84,83 +82,86 @@ test('injectWorkflowLambdaAliases updates the correct resources', (test) => {
   };
 
   kes.injectWorkflowLambdaAliases();
-  test.deepEqual(expected, kes.config.stepFunctions);
+  t.deepEqual(expected, kes.config.stepFunctions);
 });
 
 
+test('injectOldWorkflowLambdaAliases adds oldLambdas to configuration object', async (t) => {
+  const kes = t.context.kes;
+  const nameArray = ['LambdaName-12345', 'LambdaName-abcde', 'SecondLambdaName-67890'];
+  kes.getRetainedLambdaAliasNames = () => (Promise.resolve(nameArray));
 
-test('injectOldWorkflowLambdaAliases adds oldLambdas to configuration object', async (test) => {
-  const aliasFunc = () => {new Promise((resolve, reject) => resolve());};
-  let kes = test.context.kes;
-
-  kes.getRetainedLambdaAliasNames = () =>  Promise.resolve(['LambdaName-12345',
-                                                            'LambdaName-abcde',
-                                                            'SecondLambdaName-67890']);
-  const expected = { LambdaName: { hashes: ['12345', 'abcde'] },
-                     SecondLambdaName: { hashes: ['67890'] } };
+  const expected = {
+    LambdaName: { hashes: ['12345', 'abcde'] },
+    SecondLambdaName: { hashes: ['67890'] }
+  };
 
   await kes.injectOldWorkflowLambdaAliases();
-  test.deepEqual(expected, kes.config.oldLambdas);
+  t.deepEqual(expected, kes.config.oldLambdas);
 });
 
 
-test('parseAliasName parses the alias name', (test) => {
-  const expected = {name: 'functionName', hash: 'hashValue'};
-  const actual = test.context.kes.parseAliasName('functionName-hashValue');
-  test.deepEqual(expected, actual);
+test('parseAliasName parses the alias name', (t) => {
+  const expected = { name: 'functionName', hash: 'hashValue' };
+  const actual = t.context.kes.parseAliasName('functionName-hashValue');
+  t.deepEqual(expected, actual);
 });
 
-test('parseAliasName returns null hash if hash missing', (test) => {
-  const expected = {name: 'functionName', hash: null};
-  const actual = test.context.kes.parseAliasName('functionName-');
-  test.deepEqual(expected, actual);
+test('parseAliasName returns null hash if hash missing', (t) => {
+  const expected = { name: 'functionName', hash: null };
+  const actual = t.context.kes.parseAliasName('functionName-');
+  t.deepEqual(expected, actual);
 });
 
 
-test.serial('getAllLambdaAliases returns an unpaginated list of aliases', async (test) => {
-  let kes = test.context.kes;
+test.serial('getAllLambdaAliases returns an unpaginated list of aliases', async (t) => {
+  const kes = t.context.kes;
 
   // Mock out AWS calls
-  const aliasFixture = require('./fixtures/aliases.json');
   const versionUpAliases = aliasFixture.Aliases[0];
-  sinon.stub(kes.AWS, 'Lambda').callsFake(function () {
-    return { listAliases: function (config) {
-        return { promise: listAliasesStub };
-    }};
-  });
+
   const listAliasesStub = sinon.stub().callsFake(() => Promise.reject());
-  for(let i=0; i < versionUpAliases.length-1; i++) {
+
+  sinon.stub(kes.AWS, 'Lambda').callsFake(() => (
+    {
+      listAliases: (_config) => (
+        { promise: listAliasesStub }
+      )
+    }
+  ));
+
+  for (let i = 0; i < versionUpAliases.length - 1; i += 1) {
     listAliasesStub.onCall(i).returns(Promise.resolve({
       NextMarker: 'mocked out',
       Aliases: [versionUpAliases[i]]
     }));
   }
-  listAliasesStub.onCall(versionUpAliases.length-1).returns(
-    Promise.resolve({Aliases: [versionUpAliases[versionUpAliases.length-1]]}));
+  listAliasesStub.onCall(versionUpAliases.length - 1).returns(
+    Promise.resolve({ Aliases: [versionUpAliases[versionUpAliases.length - 1]] })
+  );
 
   const lambda = kes.AWS.Lambda();
   const config = { MaxItems: 1, FunctionName: 'Mocked' };
 
-  let actual = await kes.getAllLambdaAliases(lambda, config);
+  const actual = await kes.getAllLambdaAliases(lambda, config);
 
-  test.deepEqual(versionUpAliases, actual,
-                 `Expected:${JSON.stringify(versionUpAliases)}\n\n` +
-                 `Actual:${JSON.stringify(actual)}`);
+  t.deepEqual(versionUpAliases, actual,
+    `Expected:${JSON.stringify(versionUpAliases)}\n\n` +
+    `Actual:${JSON.stringify(actual)}`);
 });
 
-test.serial('getRetainedLambdaAliasNames returns filtered aliasNames', async (test) => {
-  const aliasFixture = require('./fixtures/aliases.json');
-  let kes = test.context.kes;
+test.serial('getRetainedLambdaAliasNames returns filtered aliasNames', async (t) => {
+  const kes = t.context.kes;
 
   kes.config.lambdas = aliasFixture.Lambdas;
-  const getAllLambdaAliasesStub  = sinon.stub(kes, 'getAllLambdaAliases');
-  for(let i=0; i<aliasFixture.Aliases.length; i++) {
+  const getAllLambdaAliasesStub = sinon.stub(kes, 'getAllLambdaAliases');
+  for (let i = 0; i < aliasFixture.Aliases.length; i += 1) {
     getAllLambdaAliasesStub.onCall(i).returns(aliasFixture.Aliases[i]);
   }
 
-  let expected = [ 'VersionUpTest-PreviousVersionHash',
-                   'VersionUpTest-SecondPreviousVersionHash',
-                   'HelloWorld-d49d272b8b1e8eb98a61affc34b1732c1032b1ca' ];
-  let actual = await kes.getRetainedLambdaAliasNames();
-  test.deepEqual(expected, actual);
+  const expected = ['VersionUpTest-PreviousVersionHash', 'VersionUpTest-SecondPreviousVersionHash',
+    'HelloWorld-d49d272b8b1e8eb98a61affc34b1732c1032b1ca'];
+
+  const actual = await kes.getRetainedLambdaAliasNames();
+  t.deepEqual(expected, actual);
 });
