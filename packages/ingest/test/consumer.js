d25d1fe9c43aa07bc369ef58907cca46f245db55
Aimee Barciauskas <aimee@developmentseed.org>
2018-08-28 09:58:54 +0300

    Fix tests

--- packages/ingest/test/consumer.js
+++ packages/ingest/test/consumer.js
@@ -17,7 +17,7 @@ async function sleep(milliseconds) {
 }
 
 const timeLimit = 10;
-const messageLimit = 2;
+let messageLimit = 2;
 class MyTestConsumerClass extends Consume {
   constructor() {
     super()
@@ -37,36 +37,36 @@ consumer.__set__('receiveSQSMessages', stubReceiveSQSMessages);
 function processFn(msg) {};
 
 const sandbox = sinon.sandbox.create();
-let receiveSpy;
+let processSpy;
 
 test.afterEach.always(() => sandbox.restore());
 
 test.serial('stops after timelimit', async (t) => {
   const myTestConsumerClass = new MyTestConsumerClass();
-  receiveSpy = sandbox.spy(myTestConsumerClass, 'processMessage');
+  processSpy = sandbox.spy(myTestConsumerClass, 'processMessage');
 
   const result = await myTestConsumerClass.processMessages(processFn, messageLimit);
   t.is(result, 1);
-  t.is(receiveSpy.calledOnce, true);
+  t.is(processSpy.calledOnce, true);
 });
 
 test.serial('it continues when timeLimit is is greater than time to receive', async (t) => {
   const myTestConsumerClass = new MyTestConsumerClass();
-  receiveSpy = sandbox.spy(myTestConsumerClass, 'processMessage');
+  processSpy = sandbox.spy(myTestConsumerClass, 'processMessage');
   myTestConsumerClass.timeLimit = timeLimit*3;
 
   const result = await myTestConsumerClass.processMessages(processFn, messageLimit);
   t.is(result, 2);
-  t.is(receiveSpy.calledTwice, true);
+  t.is(processSpy.calledTwice, true);
 });
 
 test.serial('it stops after messageLimit is reached', async (t) => {
   const myTestConsumerClass = new MyTestConsumerClass();
-  receiveSpy = sandbox.spy(myTestConsumerClass, 'processMessage');
+  processSpy = sandbox.spy(myTestConsumerClass, 'processMessage');
   myTestConsumerClass.timeLimit = timeLimit*3;
-  myTestConsumerClass.messageLimit = 1;
+  messageLimit = 1;
 
   const result = await myTestConsumerClass.processMessages(processFn, messageLimit);
   t.is(result, 1);
-  t.is(receiveSpy.calledOnce, true);  
+  t.is(processSpy.calledOnce, true);
 });
