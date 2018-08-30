008eeef2bcf077d7ee4986b21728653ad960ca08
Menno Van Diermen <mvandiermen@contractor.usgs.gov>
2018-08-30 16:06:38 -0500

    setup/teardown in kinesis tests

--- example/spec/setup/CleansUpTestResources.js
+++ example/spec/setup/CleansUpTestResources.js
@@ -1,18 +1,10 @@
 const { loadConfig } = require('../helpers/testUtils');
-const { aws: { deleteS3Files, listS3ObjectsV2, s3 } } = require('@cumulus/common');
-const { deleteRules, rulesList } = require('@cumulus/integration-tests');
+const { aws: { deleteS3Files, listS3ObjectsV2 } } = require('@cumulus/common');
 const testConfig = loadConfig();
-const ruleDirectory = './data/rules';
 
 jasmine.DEFAULT_TIMEOUT_INTERVAL = 550000;
 
 describe('Cleans up Test Resources', () => {
-  it('removes the PODAAC_SWOT rule', async () => {
-    const rules = await rulesList(testConfig.stackName, testConfig.bucket, ruleDirectory);
-    const deleted = await deleteRules(testConfig.stackName, testConfig.bucket, rules); //
-    expect(deleted).toBeGreaterThanOrEqual(1);
-  });
-
   it('removes the test output', async () => {
     const params = {
       Bucket: testConfig.bucket,
@@ -20,6 +12,7 @@ describe('Cleans up Test Resources', () => {
     };
     const s3list = await listS3ObjectsV2(params);
     const s3objects = s3list.map((obj) => ({ Bucket: testConfig.bucket, Key: obj.Key }));
+    console.log(`\nDeleting ${s3objects.length} objects`);
     await deleteS3Files(s3objects);
   });
 });
