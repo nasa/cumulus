fe43f7a13fbf134456b599b7c90e57e43054556e
Menno Van Diermen <mvandiermen@contractor.usgs.gov>
2018-08-31 09:18:16 -0500

    remove rule population from setup

--- example/spec/setup/PopulateProvidersCollections.js
+++ example/spec/setup/PopulateProvidersCollections.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const { addProviders, addCollections, addRules } = require('@cumulus/integration-tests');
+const { addProviders, addCollections } = require('@cumulus/integration-tests');
 const { loadConfig } = require('../helpers/testUtils');
 const config = loadConfig();
 
@@ -8,18 +8,15 @@ jasmine.DEFAULT_TIMEOUT_INTERVAL = 550000;
 
 const collectionsDirectory = './data/collections';
 const providersDirectory = './data/providers';
-const rulesDirectory = './data/rules';
 
 describe('Populating providers, collections and rules to database', () => {
   let collections;
   let providers;
-  let rules;
 
   beforeAll(async () => {
     try {
       collections = await addCollections(config.stackName, config.bucket, collectionsDirectory);
       providers = await addProviders(config.stackName, config.bucket, providersDirectory, config.bucket);
-      rules = await addRules(config, rulesDirectory);
     }
     catch (e) {
       console.log(JSON.stringify(e));
@@ -30,6 +27,5 @@ describe('Populating providers, collections and rules to database', () => {
   it('providers, collections and rules are added successfully', async () => {
     expect(providers).toBeGreaterThanOrEqual(1, 'Number of providers incorrect.');
     expect(collections).toBeGreaterThanOrEqual(1, 'Number of collections incorrect.');
-    expect(rules).toBeGreaterThanOrEqual(1, 'Number of rules incorrect.');
   });
 });
