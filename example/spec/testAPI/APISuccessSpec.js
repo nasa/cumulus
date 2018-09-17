2b873767108ceae4ab9d46bd0ee6035e5ca25277
mhuffnagle <marc@element84.com>
2018-09-17 10:14:11 -0400

    Increase timeout waiting for CMR granule to update in test

--- example/spec/testAPI/APISuccessSpec.js
+++ example/spec/testAPI/APISuccessSpec.js
@@ -183,7 +183,7 @@ describe('The Cumulus API', () => {
         workflow: 'PublishGranule'
       });
 
-      const granulePublished = await waitForExist(cmrLink, true, 6, 20000);
+      const granulePublished = await waitForExist(cmrLink, true, 10, 30000);
       expect(granulePublished).toEqual(true);
 
       // Cleanup: remove from CMR
