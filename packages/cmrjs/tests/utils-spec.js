e078ac2c3fddf298c1bd1ec4a05a51966c5e5623
Aimee Barciauskas <aimee@developmentseed.org>
2018-09-17 16:55:43 -0400

    Make CMR host configurable (#542)

--- packages/cmrjs/tests/utils-spec.js
+++ packages/cmrjs/tests/utils-spec.js
@@ -4,12 +4,12 @@ const sinon = require('sinon');
 const test = require('ava');
 const publicIp = require('public-ip');
 
-const { getIp } = require('../utils');
+const { getIp, getHost } = require('../utils');
 
 let stub;
 
 test.afterEach(t => {
-  stub.restore();
+  if (stub !== undefined) stub.restore();
 });
 
 test('getIp returns public IP when available', async (t) => {
@@ -30,4 +30,19 @@ test('getIp throws an error when the error is unexpected', async (t) => {
   await getIp().catch((e) => {
     t.is(e.message, errorMessage);
   });
+});
+
+test('getHost returns value according to process.env.CMR_ENVIRONMENT', (t) => {
+  process.env.CMR_ENVIRONMENT = 'OPS';
+  t.is(getHost(), 'cmr.earthdata.nasa.gov');
+  process.env.CMR_ENVIRONMENT = 'SIT';
+  t.is(getHost(), 'cmr.sit.earthdata.nasa.gov');
+  process.env.CMR_ENVIRONMENT = '';
+  t.is(getHost(), 'cmr.uat.earthdata.nasa.gov');
+});
+
+test('getHost returns CMR_HOST when defined', (t) => {
+  const anotherHost = 'cmr.com';
+  process.env.CMR_HOST = anotherHost;
+  t.is(getHost(), anotherHost);
 });
