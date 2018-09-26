b5f062755bea872b40ddc24e941a25fddad66d3c
Mark Boyd <markdavidboyd@gmail.com>
2018-09-26 14:09:43 -0400

    add assertion for failing SyncGranule lambda in duplicate handling of "error" spec

--- example/spec/syncGranule/SyncGranuleDuplicateHandlingErrorSpec.js
+++ example/spec/syncGranule/SyncGranuleDuplicateHandlingErrorSpec.js
@@ -1,9 +1,13 @@
 const fs = require('fs');
+const path = require('path');
 const { Collection } = require('@cumulus/api/models');
-const { addCollections, buildAndExecuteWorkflow } = require('@cumulus/integration-tests');
+const { constructCollectionId } = require('@cumulus/common');
+const { addCollections, buildAndExecuteWorkflow, LambdaStep } = require('@cumulus/integration-tests');
 
 const { loadConfig } = require('../helpers/testUtils');
 const config = loadConfig();
+
+const lambdaStep = new LambdaStep();
 
 describe('The Sync Granules workflow is configured to handle duplicates as an error', () => {
   const inputPayloadFilename = './spec/syncGranule/SyncGranule.input.payload.json';
@@ -14,9 +18,28 @@ describe('The Sync Granules workflow is configured to handle duplicates as an er
   process.env.CollectionsTable = `${config.stackName}-CollectionsTable`;
   const c = new Collection();
 
+  const catchTaskName = 'SyncGranuleCatchDuplicateErrorTest';
+  const taskName = 'SyncGranule';
+
+  const granuleFileName = inputPayload.granules[0].files[0].name;
+  let existingFileKey;
+
   beforeAll(async () => {
     const collectionsDirectory = './data/collections/syncGranule';
     await addCollections(config.stackName, config.bucket, collectionsDirectory);
+    await buildAndExecuteWorkflow(
+      config.stackName, config.bucket, catchTaskName, collection, provider, inputPayload
+    );
+    await buildAndExecuteWorkflow(
+      config.stackName, config.bucket, taskName, collection, provider, inputPayload
+    );
+    const collectionInfo = await c.get(collection);
+    existingFileKey = path.join(
+      'custom-staging-dir',
+      config.stackName,
+      constructCollectionId(collectionInfo.dataType, collectionInfo.version),
+      granuleFileName
+    );
   });
 
   it('configured collection to handle duplicates as error', () => {
@@ -26,7 +49,6 @@ describe('The Sync Granules workflow is configured to handle duplicates as an er
 
   describe('and it is configured to catch the duplicate error', () => {
     let catchWorkflowExecution;
-    const catchTaskName = 'SyncGranuleCatchDuplicateErrorTest';
 
     beforeAll(async () => {
       catchWorkflowExecution = await buildAndExecuteWorkflow(
@@ -34,18 +56,37 @@ describe('The Sync Granules workflow is configured to handle duplicates as an er
       );
     });
 
-    it('completes execution with success status', () => {
+    it('fails the SyncGranule Lambda function', async () => {
+      const lambdaOutput = await lambdaStep.getStepOutput(catchWorkflowExecution.executionArn, 'SyncGranuleNoVpc', 'failure');
+      const { error, cause } = lambdaOutput;
+      const errorCause = JSON.parse(cause);
+      expect(error).toEqual('DuplicateFile');
+      expect(errorCause.errorMessage).toEqual(
+        `${existingFileKey} already exists in ${config.bucket} bucket`
+      );
+    });
+
+    it('completes execution with success status', async () => {
       expect(catchWorkflowExecution.status).toEqual('SUCCEEDED');
     });
   });
 
   describe('and it is not configured to catch the duplicate error', () => {
-    const taskName = 'SyncGranule';
     let failWorkflowExecution;
 
     beforeAll(async () => {
       failWorkflowExecution = await buildAndExecuteWorkflow(
         config.stackName, config.bucket, taskName, collection, provider, inputPayload
+      );
+    });
+
+    it('fails the SyncGranule Lambda function', async () => {
+      const lambdaOutput = await lambdaStep.getStepOutput(failWorkflowExecution.executionArn, 'SyncGranuleNoVpc', 'failure');
+      const { error, cause } = lambdaOutput;
+      const errorCause = JSON.parse(cause);
+      expect(error).toEqual('DuplicateFile');
+      expect(errorCause.errorMessage).toEqual(
+        `${existingFileKey} already exists in ${config.bucket} bucket`
       );
     });
 
