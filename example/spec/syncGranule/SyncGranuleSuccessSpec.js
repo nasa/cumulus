13d04813732a6b6870ca85e6b28500dfeca1a8d1
Jenny Liu <jenny.h.liu@nasa.gov>
2018-09-26 09:52:51 -0400

    integration test for sync-granule with duplicateHandling replace

--- example/spec/syncGranule/SyncGranuleSuccessSpec.js
+++ example/spec/syncGranule/SyncGranuleSuccessSpec.js
@@ -1,36 +1,57 @@
 const fs = require('fs');
 const path = require('path');
 const { Execution } = require('@cumulus/api/models');
-const { s3, s3ObjectExists } = require('@cumulus/common/aws');
+const {
+  aws: {
+    headObject,
+    parseS3Uri,
+    s3,
+    s3ObjectExists
+  },
+  testUtils: {
+    randomString
+  }
+} = require('@cumulus/common');
 const { buildAndExecuteWorkflow, LambdaStep } = require('@cumulus/integration-tests');
 
 const { loadConfig, templateFile } = require('../helpers/testUtils');
+const {
+  setupTestGranuleForIngest,
+  loadFileWithUpdatedGranuleId
+} = require('../helpers/granuleUtils');
 const config = loadConfig();
 const lambdaStep = new LambdaStep();
-const taskName = 'SyncGranule';
+const workflowName = 'SyncGranule';
 
-const outputPayloadTemplateFilename = './spec/syncGranule/SyncGranule.output.payload.template.json'; // eslint-disable-line max-len
+const granuleRegex = '^MOD09GQ\\.A[\\d]{7}\\.[\\w]{6}\\.006\\.[\\d]{13}$';
+const testDataGranuleId = 'MOD09GQ.A2016358.h13v04.006.2016360104606';
+
+const outputPayloadTemplateFilename = './spec/syncGranule/SyncGranule.output.payload.template.json';
 const templatedOutputPayloadFilename = templateFile({
   inputTemplateFilename: outputPayloadTemplateFilename,
   config: config.SyncGranule
 });
-const expectedPayload = JSON.parse(fs.readFileSync(templatedOutputPayloadFilename));
 
-
-describe('The Sync Granules workflow', () => {
+describe('The Sync Granules workflow is configured to overwrite data with duplicate filenames', () => {
   const inputPayloadFilename = './spec/syncGranule/SyncGranule.input.payload.json';
-  const inputPayload = JSON.parse(fs.readFileSync(inputPayloadFilename));
+  let inputPayload;
+  let expectedPayload;
   const collection = { name: 'MOD09GQ', version: '006' };
   const provider = { id: 's3_provider' };
-  let workflowExecution = null;
+  let workflowExecution;
 
   process.env.ExecutionsTable = `${config.stackName}-ExecutionsTable`;
   const executionModel = new Execution();
 
   beforeAll(async () => {
-    // eslint-disable-next-line function-paren-newline
+    const inputPayloadJson = fs.readFileSync(inputPayloadFilename, 'utf8');
+    inputPayload = await setupTestGranuleForIngest(config.bucket, inputPayloadJson, testDataGranuleId, granuleRegex);
+
+    const granuleId = inputPayload.granules[0].granuleId;
+    expectedPayload = loadFileWithUpdatedGranuleId(templatedOutputPayloadFilename, testDataGranuleId, granuleId);
+
     workflowExecution = await buildAndExecuteWorkflow(
-      config.stackName, config.bucket, taskName, collection, provider, inputPayload
+      config.stackName, config.bucket, workflowName, collection, provider, inputPayload
     );
   });
 
@@ -52,11 +73,6 @@ describe('The Sync Granules workflow', () => {
       key2 = path.join(files[1].fileStagingDir, files[1].name);
       existCheck[0] = await s3ObjectExists({ Bucket: files[0].bucket, Key: key1 });
       existCheck[1] = await s3ObjectExists({ Bucket: files[1].bucket, Key: key2 });
-    });
-
-    afterAll(async () => {
-      await s3().deleteObject({ Bucket: files[0].bucket, Key: key1 }).promise();
-      await s3().deleteObject({ Bucket: files[1].bucket, Key: key2 }).promise();
     });
 
     it('receives payload with file objects updated to include file staging location', () => {
@@ -85,6 +101,60 @@ describe('The Sync Granules workflow', () => {
     it('the execution record is added to DynamoDB', async () => {
       const record = await executionModel.get({ arn: workflowExecution.executionArn });
       expect(record.status).toEqual('completed');
+    });
+  });
+
+  describe('and it encounters data with a duplicated filename', () => {
+    let lambdaOutput;
+    let existingfiles;
+    let fileUpdated;
+
+    beforeAll(async () => {
+      lambdaOutput = await lambdaStep.getStepOutput(workflowExecution.executionArn, 'SyncGranule');
+      const files = lambdaOutput.payload.granules[0].files;
+      existingfiles = await Promise.all(files.map(async (f) => {
+        const header = await headObject(f.bucket, parseS3Uri(f.filename).Key);
+        return { filename: f.filename, fileSize: header.ContentLength, LastModified: header.LastModified };
+      }));
+
+      // update one of the input files, so that the file has different checksum
+      const content = randomString();
+      const file = inputPayload.granules[0].files[0];
+      fileUpdated = file.name;
+      const updateParams = {
+        Bucket: config.bucket, Key: path.join(file.path, file.name), Body: content
+      };
+
+      await s3().putObject(updateParams).promise();
+      inputPayload.granules[0].files[0].fileSize = content.length;
+
+      workflowExecution = await buildAndExecuteWorkflow(
+        config.stackName, config.bucket, workflowName, collection, provider, inputPayload
+      );
+    });
+
+    it('does not raise a workflow error', () => {
+      expect(workflowExecution.status).toEqual('SUCCEEDED');
+    });
+
+    it('overwrites the existing file with the new data', async () => {
+      lambdaOutput = await lambdaStep.getStepOutput(workflowExecution.executionArn, 'SyncGranule');
+      const files = lambdaOutput.payload.granules[0].files;
+
+      const currentFiles = await Promise.all(files.map(async (f) => {
+        const header = await headObject(f.bucket, parseS3Uri(f.filename).Key);
+        return { filename: f.filename, fileSize: header.ContentLength, LastModified: header.LastModified };
+      }));
+
+      expect(currentFiles.length).toBe(existingfiles.length);
+
+      currentFiles.forEach((cf) => {
+        const existingfile = existingfiles.filter((ef) => ef.filename === cf.filename);
+        expect(cf.LastModified).toBeGreaterThan(existingfile[0].LastModified);
+        if (cf.filename.endsWith(fileUpdated)) {
+          expect(cf.fileSize).toBe(inputPayload.granules[0].files[0].fileSize);
+        }
+      });
     });
   });
 });
