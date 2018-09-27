36e861caa37daff313fd38561013702f286a5d29
mhuffnagle <marc@element84.com>
2018-09-27 15:19:38 -0400

    Add rules endpoint tests for missing Authorization header

--- packages/api/tests/endpoints/rules.js
+++ packages/api/tests/endpoints/rules.js
@@ -7,12 +7,17 @@ const { randomString } = require('@cumulus/common/test-utils');
 const bootstrap = require('../../lambdas/bootstrap');
 const models = require('../../models');
 const rulesEndpoint = require('../../endpoints/rules');
-const { testEndpoint } = require('../../lib/testUtils');
+const {
+  fakeUserFactory,
+  testEndpoint
+} = require('../../lib/testUtils');
 const { Search } = require('../../es/search');
+const assertions = require('../../lib/assertions');
 
 const esIndex = randomString();
 
 process.env.RulesTable = randomString();
+process.env.UsersTable = randomString();
 process.env.stackName = randomString();
 process.env.bucket = randomString();
 const workflowName = randomString();
@@ -32,7 +37,9 @@ const testRule = {
   state: 'DISABLED'
 };
 
+let authHeaders;
 let ruleModel;
+let userModel;
 test.before(async () => {
   await bootstrap.bootstrapElasticSearch('fakehost', esIndex);
 
@@ -47,19 +54,99 @@ test.before(async () => {
   await ruleModel.createTable();
 
   await ruleModel.create(testRule);
+
+  userModel = new models.User();
+  await userModel.createTable();
+
+  const authToken = (await userModel.create(fakeUserFactory())).password;
+  authHeaders = {
+    Authorization: `Bearer ${authToken}`
+  };
 });
 
 test.after.always(async () => {
   await ruleModel.deleteTable();
+  await userModel.deleteTable();
   await aws.recursivelyDeleteS3Bucket(process.env.bucket);
 
   const esClient = await Search.es('fakehost');
   await esClient.indices.delete({ index: esIndex });
 });
 
+test('GET without pathParameters and without an Authorization header returns an Authorization Missing response', async (t) => {
+  const request = {
+    httpMethod: 'GET',
+    headers: {}
+  };
+
+  return testEndpoint(rulesEndpoint, request, (response) => {
+    assertions.isAuthorizationMissingResponse(t, response);
+  });
+});
+
+test('GET with pathParameters and without an Authorization header returns an Authorization Missing response', async (t) => {
+  const request = {
+    httpMethod: 'GET',
+    pathParameters: {
+      name: 'asdf'
+    },
+    headers: {}
+  };
+
+  return testEndpoint(rulesEndpoint, request, (response) => {
+    assertions.isAuthorizationMissingResponse(t, response);
+  });
+});
+
+test('POST with pathParameters and without an Authorization header returns an Authorization Missing response', async (t) => {
+  const request = {
+    httpMethod: 'POST',
+    pathParameters: {
+      name: 'asdf'
+    },
+    headers: {}
+  };
+
+  return testEndpoint(rulesEndpoint, request, (response) => {
+    assertions.isAuthorizationMissingResponse(t, response);
+  });
+});
+
+test('PUT with pathParameters and without an Authorization header returns an Authorization Missing response', async (t) => {
+  const request = {
+    httpMethod: 'PUT',
+    pathParameters: {
+      name: 'asdf'
+    },
+    headers: {}
+  };
+
+  return testEndpoint(rulesEndpoint, request, (response) => {
+    assertions.isAuthorizationMissingResponse(t, response);
+  });
+});
+
+test('DELETE with pathParameters and without an Authorization header returns an Authorization Missing response', async (t) => {
+  const request = {
+    httpMethod: 'DELETE',
+    pathParameters: {
+      name: 'asdf'
+    },
+    headers: {}
+  };
+
+  return testEndpoint(rulesEndpoint, request, (response) => {
+    assertions.isAuthorizationMissingResponse(t, response);
+  });
+});
+
 // TODO(aimee): Add a rule to ES. List uses ES and we don't have any rules in ES.
 test('default returns list of rules', (t) => {
-  const listEvent = { httpMethod: 'list ' };
+  const listEvent = {
+    httpMethod: 'GET',
+    headers: authHeaders
+  };
+
   return testEndpoint(rulesEndpoint, listEvent, (response) => {
     const { results } = JSON.parse(response.body);
     t.is(results.length, 0);
@@ -71,8 +158,10 @@ test('GET gets a rule', (t) => {
     pathParameters: {
       name: testRule.name
     },
-    httpMethod: 'GET'
+    httpMethod: 'GET',
+    headers: authHeaders
   };
+
   return testEndpoint(rulesEndpoint, getEvent, (response) => {
     const { name } = JSON.parse(response.body);
     t.is(name, testRule.name);
@@ -83,7 +172,8 @@ test('POST creates a rule', (t) => {
   const newRule = Object.assign(cloneDeep(testRule), { name: 'make_waffles' });
   const postEvent = {
     httpMethod: 'POST',
-    body: JSON.stringify(newRule)
+    body: JSON.stringify(newRule),
+    headers: authHeaders
   };
 
   return testEndpoint(rulesEndpoint, postEvent, (response) => {
@@ -101,8 +191,10 @@ test('POST returns a record exists when one exists', (t) => {
   const newRule = Object.assign({}, testRule);
   const postEvent = {
     httpMethod: 'POST',
-    body: JSON.stringify(newRule)
+    body: JSON.stringify(newRule),
+    headers: authHeaders
   };
+
   return testEndpoint(rulesEndpoint, postEvent, (response) => {
     const { message, record } = JSON.parse(response.body);
     t.is(message, `A record already exists for ${newRule.name}`);
@@ -118,8 +210,10 @@ test('PUT updates a rule', (t) => {
     pathParameters: {
       name: testRule.name
     },
-    httpMethod: 'PUT'
+    httpMethod: 'PUT',
+    headers: authHeaders
   };
+
   return testEndpoint(rulesEndpoint, updateEvent, (response) => {
     const record = JSON.parse(response.body);
     newRule.createdAt = record.createdAt;
@@ -135,8 +229,10 @@ test('PUT returns "record does not exist"', (t) => {
     pathParameters: {
       name: 'new_make_coffee'
     },
-    httpMethod: 'PUT'
+    httpMethod: 'PUT',
+    headers: authHeaders
   };
+
   return testEndpoint(rulesEndpoint, updateEvent, (response) => {
     const { message, record } = JSON.parse(response.body);
     t.is(message, 'Record does not exist');
@@ -149,8 +245,10 @@ test('DELETE deletes a rule', (t) => {
     pathParameters: {
       name: testRule.name
     },
-    httpMethod: 'DELETE'
+    httpMethod: 'DELETE',
+    headers: authHeaders
   };
+
   return testEndpoint(rulesEndpoint, deleteEvent, (response) => {
     const { message } = JSON.parse(response.body);
     t.is(message, 'Record deleted');
