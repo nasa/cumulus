957bad7eff58528701bad8857bd4fabf9f55fc07
mhuffnagle <marc@element84.com>
2018-09-27 15:06:02 -0400

    Add logs tests

---
+++ packages/api/tests/endpoints/logs.js
@@ -0,0 +1,46 @@
+'use strict';
+
+const test = require('ava');
+
+const logsEndpoint = require('../../endpoints/logs');
+const {
+  testEndpoint
+} = require('../../lib/testUtils');
+const assertions = require('../../lib/assertions');
+
+test('GET without pathParameters and without an Authorization header returns an Authorization Missing response', async (t) => {
+  const request = {
+    httpMethod: 'GET',
+    headers: {}
+  };
+
+  return testEndpoint(logsEndpoint, request, (response) => {
+    assertions.isAuthorizationMissingResponse(t, response);
+  });
+});
+
+test('GET /stats/logs without an Authorization header returns an Authorization Missing response', async (t) => {
+  const request = {
+    httpMethod: 'GET',
+    resource: '/stats/logs',
+    headers: {}
+  };
+
+  return testEndpoint(logsEndpoint, request, (response) => {
+    assertions.isAuthorizationMissingResponse(t, response);
+  });
+});
+
+test('GET with pathParameters and without an Authorization header returns an Authorization Missing response', async (t) => {
+  const request = {
+    httpMethod: 'GET',
+    pathParameters: {
+      executionName: 'asdf'
+    },
+    headers: {}
+  };
+
+  return testEndpoint(logsEndpoint, request, (response) => {
+    assertions.isAuthorizationMissingResponse(t, response);
+  });
+});
