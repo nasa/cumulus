165812300b36960d7fc2b84a46533131738c8769
mhuffnagle <marc@element84.com>
2018-09-30 16:43:10 -0400

    EarthdataLogin refactoring

--- packages/api/index.js
+++ packages/api/index.js
@@ -1,6 +1,6 @@
 'use strict';
 
-exports.token = require('./endpoints/token').handler;
+exports.token = require('./endpoints/token').handleApiGatewayRequest;
 exports.collections = require('./endpoints/collections');
 exports.granules = require('./endpoints/granules');
 exports.logs = require('./endpoints/logs');
@@ -13,7 +13,7 @@ exports.executionStatus = require('./endpoints/execution-status');
 exports.schemas = require('./endpoints/schemas');
 exports.stats = require('./endpoints/stats');
 exports.version = require('./endpoints/version');
-exports.distribution = require('./endpoints/distribution').handler;
+exports.distribution = require('./endpoints/distribution').handleApiGatewayRequest;
 exports.dbIndexer = require('./lambdas/db-indexer');
 exports.reconciliationReports = require('./endpoints/reconciliation-reports');
 
@@ -40,5 +40,3 @@ exports.logHandler = indexer.logHandler;
 
 exports.models = require('./models');
 exports.testUtils = require('./lib/testUtils');
-
-exports.EarthdataLoginClient = require('./lib/EarthdataLoginClient');
