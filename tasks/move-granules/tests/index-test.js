64c3dbc355c52f9c3cb54857831b650087f8c17c
Mark Boyd <markdavidboyd@gmail.com>
2018-10-03 15:54:09 -0400

    update move-granules test to only import/use needed aws helper functions

--- tasks/move-granules/tests/index-test.js
+++ tasks/move-granules/tests/index-test.js
@@ -4,35 +4,40 @@
 
 const fs = require('fs');
 const test = require('ava');
-const aws = require('@cumulus/common/aws');
+const {
+  recursivelyDeleteS3Bucket,
+  s3ObjectExists,
+  s3,
+  promiseS3Upload
+} = require('@cumulus/common/aws');
 const payload = require('./data/payload.json');
 const { moveGranules } = require('../index');
 const { randomString, validateOutput } = require('@cumulus/common/test-utils');
 
 // eslint-disable-next-line require-jsdoc
-async function deleteBucket(bucket) {
-  const response = await aws.s3().listObjects({ Bucket: bucket }).promise();
-  const keys = response.Contents.map((o) => o.Key);
-  await Promise.all(keys.map(
-    (key) => aws.s3().deleteObject({ Bucket: bucket, Key: key }).promise()
-  ));
-}
+// async function deleteBucket(bucket) {
+//   const response = await s3().listObjects({ Bucket: bucket }).promise();
+//   const keys = response.Contents.map((o) => o.Key);
+//   await Promise.all(keys.map(
+//     (key) => s3().deleteObject({ Bucket: bucket, Key: key }).promise()
+//   ));
+// }
 
 test.beforeEach(async (t) => {
   t.context.stagingBucket = randomString();
   t.context.endBucket = randomString();
-  await aws.s3().createBucket({
+  await s3().createBucket({
     Bucket: t.context.endBucket
   }).promise();
 
-  await aws.s3().createBucket({
+  await s3().createBucket({
     Bucket: t.context.stagingBucket
   }).promise();
 });
 
 test.afterEach.always(async (t) => {
-  await deleteBucket(t.context.endBucket);
-  await deleteBucket(t.context.stagingBucket);
+  await recursivelyDeleteS3Bucket(t.context.endBucket);
+  await recursivelyDeleteS3Bucket(t.context.stagingBucket);
 });
 
 test.serial('should move files to final location', async (t) => {
@@ -52,13 +57,13 @@ test.serial('should move files to final location', async (t) => {
   newPayload.config.input_granules[0].files[1].filename =
   `s3://${t.context.stagingBucket}/file-staging/MOD11A1.A2017200.h19v04.006.2017201090724_2.jpg`;
 
-  await aws.promiseS3Upload({
+  await promiseS3Upload({
     Bucket: t.context.stagingBucket,
     Key: 'file-staging/MOD11A1.A2017200.h19v04.006.2017201090724_1.jpg',
     Body: 'Something'
   });
 
-  await aws.promiseS3Upload({
+  await promiseS3Upload({
     Bucket: t.context.stagingBucket,
     Key: 'file-staging/MOD11A1.A2017200.h19v04.006.2017201090724_2.jpg',
     Body: 'Something'
@@ -67,7 +72,7 @@ test.serial('should move files to final location', async (t) => {
   const output = await moveGranules(newPayload);
   await validateOutput(t, output);
 
-  const check = await aws.s3ObjectExists({
+  const check = await s3ObjectExists({
     Bucket: t.context.endBucket,
     Key: 'jpg/example/MOD11A1.A2017200.h19v04.006.2017201090724_1.jpg'
   });
@@ -96,13 +101,13 @@ test.serial('should update filenames with specific url_path', async (t) => {
   newPayload.config.input_granules[0].files[1].filename =
   `s3://${t.context.stagingBucket}/file-staging/MOD11A1.A2017200.h19v04.006.2017201090724_2.jpg`;
 
-  await aws.promiseS3Upload({
+  await promiseS3Upload({
     Bucket: t.context.stagingBucket,
     Key: 'file-staging/MOD11A1.A2017200.h19v04.006.2017201090724_1.jpg',
     Body: 'Something'
   });
 
-  await aws.promiseS3Upload({
+  await promiseS3Upload({
     Bucket: t.context.stagingBucket,
     Key: 'file-staging/MOD11A1.A2017200.h19v04.006.2017201090724_2.jpg',
     Body: 'Something'
@@ -138,19 +143,19 @@ test.serial('should update filenames with metadata fields', async (t) => {
     `s3://${t.context.endBucket}/example/2003/MOD11A1.A2017200.h19v04.006.2017201090724_2.jpg`,
     `s3://${t.context.endBucket}/example/2003/MOD11A1.A2017200.h19v04.006.2017201090724.cmr.xml`];
 
-  await aws.promiseS3Upload({
+  await promiseS3Upload({
     Bucket: t.context.stagingBucket,
     Key: 'file-staging/MOD11A1.A2017200.h19v04.006.2017201090724_1.jpg',
     Body: 'Something'
   });
 
-  await aws.promiseS3Upload({
+  await promiseS3Upload({
     Bucket: t.context.stagingBucket,
     Key: 'file-staging/MOD11A1.A2017200.h19v04.006.2017201090724_2.jpg',
     Body: 'Something'
   });
 
-  await aws.promiseS3Upload({
+  await promiseS3Upload({
     Bucket: t.context.stagingBucket,
     Key: 'file-staging/MOD11A1.A2017200.h19v04.006.2017201090724.cmr.xml',
     Body: fs.createReadStream('tests/data/meta.xml')
