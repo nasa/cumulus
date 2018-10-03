b42962075a112cac2532d8f26ac903c312a1c658
Matt Savoie <savoie@nsidc.org>
2018-10-03 12:03:54 -0600

    Increase wait for execution states.
    We see errors due to syncGranule taking too long, but that affect these tests.
    Increase the time we wait for a execution to complete beyond 5 min to capture
    any pauses that may occur between and durin lambda executions.  Also increase
    the duration of the test suite timeout.

--- example/spec/kinesisTests/KinesisTestTriggerSpec.js
+++ example/spec/kinesisTests/KinesisTestTriggerSpec.js
@@ -5,7 +5,7 @@ const {
   stringUtils: { globalReplace }
 } = require('@cumulus/common');
 
-jasmine.DEFAULT_TIMEOUT_INTERVAL = 9 * 60 * 1000;
+jasmine.DEFAULT_TIMEOUT_INTERVAL = 12 * 60 * 1000;
 
 const {
   addRules,
@@ -107,7 +107,7 @@ const s3data = ['@cumulus/test-data/granules/L2_HR_PIXC_product_0001-of-4154.h5'
 // triggers workflows associated with the kinesis-type rules.
 describe('The Cloud Notification Mechanism Kinesis workflow', () => {
   const maxWaitForSFExistSecs =  60 * 4;
-  const maxWaitForExecutionSecs = 60 * 5;
+  const maxWaitForExecutionSecs = 60 * 8;
   let executionStatus;
   let s3FileHead;
   let responseStreamShardIterator;
