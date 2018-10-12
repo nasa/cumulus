0f2ef7b679c250c7ab40129e0b3402b63032af22
Jacob Campbell <jacob.campbell.ctr@usgs.gov>
2018-10-12 15:36:35 -0500

    adding a / so I get the contents of a dir

--- travis-ci/deploy-to-github-pages.sh
+++ travis-ci/deploy-to-github-pages.sh
@@ -8,7 +8,7 @@ yarn docs-build
 git clone --depth=50 --branch=gh-pages "https://${GITHUB_USER}:${GITHUB_TOKEN}@github.com/nasa/cumulus.git" gh-pages
 (
   cd gh-pages
-  rsync -av ../website/build/Cumulus .
+  rsync -av ../website/build/Cumulus/ .
   git add .
   git commit -m "Automated build in Travis CI"
   git push
