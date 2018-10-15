dffa3ec340cdfa95f4967e7eb0cc68b0bf9ae3ae
Menno Van Diermen <mvandiermen@contractor.usgs.gov>
2018-10-15 15:07:56 -0500

    test with versioning

--- packages/deployment/test/test_lib.js
+++ packages/deployment/test/test_lib.js
@@ -11,19 +11,33 @@ const stack = randomString();
 const prefix = `${stack}/crypto`;
 const s3Client = s3();
 
-async function keyModifiedDates () {
+async function keyVersions () {
     const vals = await Promise.all([
         s3Client.headObject({ Bucket: bucket, Key: `${prefix}/public.pub` }).promise(),
         s3Client.headObject({ Bucket: bucket, Key: `${prefix}/private.pem` }).promise()
     ]);
-    return vals.map((v) => v.LastModified);
+    return vals.map((v) => v.VersionId);
 }
 
 test.before(async (t) => {
     await s3Client.createBucket({ Bucket: bucket }).promise();
+    // enable versioning to test whether new copies are created
+    await s3Client.putBucketVersioning({
+        Bucket: bucket, 
+        VersioningConfiguration: {
+            Status: "Enabled"
+        }
+    }).promise();
 });
 
 test.after(async (t) => {
+    // disable versioning to auto-delete old versions
+    await s3Client.putBucketVersioning({
+        Bucket: bucket, 
+        VersioningConfiguration: {
+            Status: "Disabled"
+        }
+    }).promise();
     await recursivelyDeleteS3Bucket(bucket);
 });
 
@@ -36,18 +50,18 @@ test.serial('crypto creates keys when they do not exist', async (t) => {
 });
 
 test.serial('crypto creates new key pair when either file does not exist', async (t) => {
-    const lastUpdatedTimes = await keyModifiedDates();
+    const oldVersions = await keyVersions();
     await deleteS3Object(bucket, `${prefix}/public.pub`);
     await crypto(stack, bucket, s3Client);
-    const newTimes = await keyModifiedDates();
-    t.notDeepEqual(lastUpdatedTimes, newTimes);
+    const newVersions = await keyVersions();
+    t.notDeepEqual(oldVersions, newVersions);
     await deleteS3Object(bucket, `${prefix}/private.pem`);
     await crypto(stack, bucket, s3Client);
-    t.notDeepEqual(newTimes, await keyModifiedDates());
+    t.notDeepEqual(newVersions, await keyVersions());
 });
 
 test.serial('crypto does not create new keys when they do exist', async (t) => {
-    const lastUpdatedTimes = await keyModifiedDates();
+    const oldVersions = await keyVersions();
     await crypto(stack, bucket, s3Client);
-    t.deepEqual(lastUpdatedTimes, await keyModifiedDates());
+    t.deepEqual(oldVersions, await keyVersions());
 });
