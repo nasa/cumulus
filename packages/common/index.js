550cc6d4ac46a999783f753e160f468ac9d9ba65
mhuffnagle <marc@element84.com>
2018-10-16 10:05:46 -0400

    Handle throttling when calling cf.describeStacks
    - Add a `@cumulus/common/CloudFormationGateway` class that will serve
      as a gateway to the AWS CloudFormation SDK.  See:
      https://www.martinfowler.com/eaaCatalog/gateway.html
    - Add a `@cumulus/common/CloudFormationGateway.getStackStatus()` method
      that will get the status of a CloudFormation stack, retrying if a
      ThrottlingException is encountered.
    - Add a `@cumulus/common/aws.isThrottlingException()` function that will
      test if an exception is a ThrottlingException.
    - Update the `@cumulus/common/step-functions` module to use
      `@cumulus/common/aws.isThrottlingException()`
    - Update the `example/lambdas/waitForDeploy` Lambda function to use
      `@cumulus/common/CloudFormationGateway.getStackStatus()`
    - Refactor the `example/lambdas/waitForDeploy` Lambda function to be
      easier to read.

--- packages/common/index.js
+++ packages/common/index.js
@@ -1,11 +1,12 @@
 'use strict';
 
-exports.log = require('./log');
 exports.aws = require('./aws');
 exports.cliUtils = require('./cli-utils');
-exports.constructCollectionId = require('./collection-config-store').constructCollectionId;
+exports.CloudFormationGateway = require('./CloudFormationGateway');
 exports.CollectionConfigStore = require('./collection-config-store').CollectionConfigStore;
-exports.testUtils = require('./test-utils');
+exports.constructCollectionId = require('./collection-config-store').constructCollectionId;
+exports.log = require('./log');
 exports.stepFunctions = require('./step-functions');
 exports.stringUtils = require('./string');
+exports.testUtils = require('./test-utils');
 exports.util = require('./util');
