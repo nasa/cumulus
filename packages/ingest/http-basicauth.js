9aeb9630b6c175e08483ba81b6f7f53c56ffed7c
Aimee Barciauskas <aimee@developmentseed.org>
2018-10-22 19:34:35 +0400

    Add doc string

--- packages/ingest/http-basicauth.js
+++ packages/ingest/http-basicauth.js
@@ -5,6 +5,13 @@ const https = require('https');
 const request = require('request'); // eslint-disable-line node/no-extraneous-require
 const { parse } = require('url');
 
+/**
+ * genUriOptions - default options for calling the `request` module, making HTTP[|S] requests
+ * @param  {string} uri     Uri to request
+ * @param  {Object} headers Object of key-value pairs representing headers
+ *                          sent to the `request` module
+ * @returns {Object}        Options to send to the `request` module
+ */
 function genUriOptions(uri, headers) {
   return {
     uri,
@@ -14,6 +21,12 @@ function genUriOptions(uri, headers) {
   };
 }
 
+/**
+ * authString base64 encoded string used to support a basic authentication request
+ * as the value for the `authorization` header
+ *
+ * @returns {string} Basic auth string
+ */
 function authString() {
   return Buffer.from(
     `${process.env.INGEST_USERNAME}:${
@@ -21,6 +34,18 @@ function authString() {
   ).toString('base64');
 }
 
+/**
+ * followRedirects - Repeatedly makes requests to the location of
+ * 302 responses from the server, using the basic auth header and
+ * appending cookies from each request, `numRedirects` times.
+ *
+ * @param  {Integer} options.currentRedirect Iterator for current redirect
+ * @param  {Integer} options.numRedirects    Number of redirects to follow in this
+ *                                           basic auth request cycle
+ * @param  {Object}  options.uriOptions      uri options to return or
+ *                                           use in the next `request` call.
+ * @returns {Object}                         uri options
+ */
 async function followRedirects({ currentRedirect, numRedirects, uriOptions }) {
   return new Promise((resolve, reject) => {
     if (currentRedirect === numRedirects) {
