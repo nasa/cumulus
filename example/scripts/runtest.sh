684ebda47390f24b2b198385fadf04e1dc308a5a
Lauren Frederick <lpfreder@mbp-lpfreder.edn.ecs.nasa.gov>
2018-10-23 12:12:10 -0400

    Parallel test fixes

--- example/scripts/runtest.sh
+++ example/scripts/runtest.sh
@@ -5,6 +5,11 @@ set -e
 specName=$(echo $2 | rev | cut -d'/' -f 1 | cut -d'.' -f 2 | rev)
 outputPath="$1/$specName.txt"
 
-echo run test $2 and output to $outputPath
+echo $(date "+%Y%m%d-%H:%M:%S") run test $2 and output to $outputPath
 
 jasmine $2 > $outputPath
+result=$?
+
+echo $(date "+%Y%m%d-%H:%M:%S") test $2 complete
+
+exit $result
