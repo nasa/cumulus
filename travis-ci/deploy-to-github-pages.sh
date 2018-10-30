9c55459bb4e8842fdd60f060d5608f9e7ea873da
Jacob Campbell <jmcampbell@contractor.usgs.gov>
2018-10-30 07:52:44 -0500

    added a cleanup statement into the deploy-to-gh-pages

--- travis-ci/deploy-to-github-pages.sh
+++ travis-ci/deploy-to-github-pages.sh
@@ -8,6 +8,7 @@ yarn docs-build
 git clone --depth=50 --branch=gh-pages "https://${GITHUB_USER}:${GITHUB_TOKEN}@github.com/nasa/cumulus.git" gh-pages
 (
   cd gh-pages
+  rm -rf *
   rsync -av ../website/build/Cumulus/ .
   git add .
   git commit -m "Automated build in Travis CI"
