4d6cb0c8113493054c67beb8aad97ff4d5640267
Marc Huffnagle <marc@element84.com>
2018-11-07 20:16:06 -0500

    Prevent deletion of collections and providers with associated rules

--- packages/api/lib/testUtils.js
+++ packages/api/lib/testUtils.js
@@ -95,13 +95,13 @@ function fakeGranuleFactoryV2(options = {}) {
 }
 
 /**
- * creates fake rule record
+ * Create a fake rule record
  *
- * @param {string} state - rule state (default to DISABLED)
+ * @param {Object} params - overrides
  * @returns {Object} fake rule object
  */
-function fakeRuleFactory(state = 'DISABLED') {
-  return {
+function fakeRuleFactoryV2(params = {}) {
+  const rule = {
     name: randomString(),
     workflow: randomString(),
     provider: randomString(),
@@ -112,8 +112,20 @@ function fakeRuleFactory(state = 'DISABLED') {
     rule: {
       type: 'onetime'
     },
-    state
+    state: 'DISABLED'
   };
+
+  return { ...rule, ...params };
+}
+
+/**
+ * creates fake rule record
+ *
+ * @param {string} state - rule state (default to DISABLED)
+ * @returns {Object} fake rule object
+ */
+function fakeRuleFactory(state = 'DISABLED') {
+  return fakeRuleFactoryV2({ state });
 }
 
 /**
@@ -225,6 +237,7 @@ module.exports = {
   fakeCollectionFactory,
   fakeExecutionFactory,
   fakeRuleFactory,
+  fakeRuleFactoryV2,
   fakeFilesFactory,
   fakeUserFactory,
   fakeProviderFactory,
