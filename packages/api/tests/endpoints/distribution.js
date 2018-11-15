5c5aabe16bf3affcdeb2bae086b9d9be56bbb6d0
Marc Huffnagle <marc@element84.com>
2018-11-15 13:48:55 -0500

    Add persistent auth to the distribution API

--- packages/api/tests/endpoints/distribution.js
+++ packages/api/tests/endpoints/distribution.js
@@ -1,221 +1,362 @@
 'use strict';
 
 const test = require('ava');
+const { Cookie } = require('tough-cookie');
 const { URL } = require('url');
-const {
-  aws: { s3 },
-  testUtils: { randomString }
-} = require('@cumulus/common');
+const { randomString } = require('@cumulus/common/test-utils');
 
 const distributionEndpoint = require('../../endpoints/distribution');
-const EarthdataLoginClient = require('../../lib/EarthdataLogin');
-const { OAuth2AuthenticationFailure } = require('../../lib/OAuth2');
+const { AccessToken } = require('../../models');
+const { getCookie, normalizeHeaders } = require('../../lib/api-gateway');
+const { fakeAccessTokenFactory } = require('../../lib/testUtils');
 
+function headerIs(headers, key, value) {
+  const lowerCaseKey = key.toLowerCase();
 
-test.beforeEach((t) => {
-  t.context.clientId = randomString();
-  t.context.clientPassword = randomString();
-  t.context.earthdataLoginUrl = `http://${randomString()}`;
-  t.context.redirectUri = `http://${randomString()}/cb`;
+  return headers[lowerCaseKey].length === 1
+    && headers[lowerCaseKey][0] === value;
+}
 
-  t.context.earthdataLoginClient = new EarthdataLoginClient({
-    clientId: t.context.clientId,
-    clientPassword: t.context.clientPassword,
-    earthdataLoginUrl: t.context.earthdataLoginUrl,
-    redirectUri: t.context.redirectUri
+function validateDefaultHeaders(t, response) {
+  const headers = normalizeHeaders(response);
+
+  t.true(headerIs(headers, 'Access-Control-Allow-Origin', '*'));
+  t.true(headerIs(headers, 'Strict-Transport-Security', 'max-age=31536000'));
+}
+
+function validateRedirectToGetAuthorizationCode(t, response) {
+  const { authorizationUrl } = t.context;
+
+  t.is(response.statusCode, 307);
+
+  validateDefaultHeaders(t, response);
+
+  t.is(response.headers.Location, authorizationUrl);
+}
+
+let suiteContext;
+
+test.before(async () => {
+  const accessTokenModel = new AccessToken({ tableName: randomString() });
+  await accessTokenModel.createTable();
+
+  suiteContext = {
+    accessTokenModel
+  };
+});
+
+test.beforeEach(async (t) => {
+  const { accessTokenModel } = suiteContext;
+
+  const accessTokenRecord = fakeAccessTokenFactory();
+  await accessTokenModel.create(accessTokenRecord);
+
+  const accessTokenCookie = new Cookie({
+    key: 'accessToken',
+    value: accessTokenRecord.accessToken
   });
 
-  t.context.s3Client = s3();
-});
+  const fileBucket = randomString();
+  const fileKey = randomString();
+  const fileLocation = `${fileBucket}/${fileKey}`;
 
-test('Using Earthdata Login, GET without a code in the queryParameters returns a correct redirect', async (t) => {
-  const granuleId = randomString();
+  const getAccessTokenResponse = {
+    accessToken: randomString(),
+    refreshToken: randomString(),
+    username: randomString(),
+    expirationTime: Date.now() + (60 * 60 * 1000)
+  };
 
-  const request = {
-    pathParameters: {
-      proxy: granuleId
+  const authClient = {
+    getAccessToken(authorizationCode) {
+      if (authorizationCode !== t.context.authorizationCode) {
+        throw new Error(`Unexpected authorizationCode: ${authorizationCode}`);
+      }
+
+      return t.context.getAccessTokenResponse;
     },
-    queryParameters: {}
-  };
+    getAuthorizationUrl(state) {
+      if (state !== t.context.fileLocation) {
+        throw new Error(`Unexpected state: ${state}`);
+      }
 
-  const response = await distributionEndpoint.handleRequest(
-    request,
-    t.context.earthdataLoginClient,
-    t.context.s3Client
-  );
-
-  const locationUrl = new URL(response.headers.Location);
-
-  t.is(response.statusCode, 302);
-  t.is(response.headers['Strict-Transport-Security'], 'max-age=31536000');
-
-  t.is(locationUrl.origin, t.context.earthdataLoginUrl);
-
-  t.is(locationUrl.pathname, '/oauth/authorize');
-
-  t.is(locationUrl.searchParams.get('client_id'), t.context.clientId);
-  t.is(locationUrl.searchParams.get('redirect_uri'), t.context.redirectUri);
-  t.is(locationUrl.searchParams.get('response_type'), 'code');
-  t.is(locationUrl.searchParams.get('state'), granuleId);
-});
-
-test('Using Earthdata Login, GET without a code and with state set in pathParameters.proxy sets the correct state in the redirect URL', async (t) => {
-  const state = randomString();
-
-  const request = {
-    pathParameters: {
-      proxy: state
-    },
-    queryParameters: {}
-  };
-
-  const response = await distributionEndpoint.handleRequest(
-    request,
-    t.context.earthdataLoginClient,
-    t.context.s3Client
-  );
-
-  const locationUrl = new URL(response.headers.Location);
-
-  t.is(locationUrl.searchParams.get('state'), state);
-});
-
-test('Using Earthdata Login, GET without a code and with state set in queryParameters.state sets the correct state in the redirect URL', async (t) => {
-  const state = randomString();
-
-  const request = {
-    queryStringParameters: {
-      state
+      return t.context.authorizationUrl;
     }
   };
-
-  const response = await distributionEndpoint.handleRequest(
-    request,
-    t.context.earthdataLoginClient,
-    t.context.s3Client
-  );
-
-  const locationUrl = new URL(response.headers.Location);
-
-  t.is(locationUrl.searchParams.get('state'), state);
-});
-
-test('Using Earthdata Login, GET with an invalid code returns a 400 response', async (t) => {
-  const earthdataLoginClient = {
-    getAccessToken: async () => {
-      throw new OAuth2AuthenticationFailure();
-    }
-  };
-
-  const request = {
-    queryStringParameters: {
-      code: randomString()
-    }
-  };
-
-  const response = await distributionEndpoint.handleRequest(
-    request,
-    earthdataLoginClient,
-    t.context.s3Client
-  );
-
-  t.is(response.statusCode, 400);
-});
-
-
-test("The S3 redirect includes the user's Earthdata Login username", async (t) => {
-  const myUsername = randomString();
-
-  const earthdataLoginClient = {
-    getAccessToken: async () => ({
-      accessToken: 'my-access-token',
-      refreshToken: 'my-refresh-token',
-      username: myUsername,
-      expirationTime: 12345
-    })
-  };
-
-  const request = {
-    queryStringParameters: {
-      code: randomString(),
-      state: `${randomString()}/${randomString()}`
-    }
-  };
-
-  const response = await distributionEndpoint.handleRequest(
-    request,
-    earthdataLoginClient,
-    t.context.s3Client
-  );
-
-  t.is(response.statusCode, 302);
-
-  const redirectLocation = new URL(response.headers.Location);
-  t.is(redirectLocation.searchParams.get('x-EarthdataLoginUsername'), myUsername);
-});
-
-test('The correct signed URL is requested', async (t) => {
-  const earthdataLoginClient = {
-    getAccessToken: async () => ({
-      accessToken: 'my-access-token',
-      refreshToken: 'my-refresh-token',
-      username: 'sidney',
-      expirationTime: 12345
-    })
-  };
-
-  const granuleBucket = randomString();
-  const granuleKey = `${randomString()}/${randomString()}`;
 
   const s3Client = {
-    getSignedUrl: (operation, params) => {
-      t.is(params.Bucket, granuleBucket);
-      t.is(params.Key, granuleKey);
+    getSignedUrl(operation, params) {
+      if (operation !== 'getObject') {
+        throw new Error(`Unexpected operation: ${operation}`);
+      }
 
-      return 'http://www.example.com';
+      if (params.Bucket !== fileBucket) {
+        throw new Error(`Unexpected params.Bucket: ${params.Bucket}`);
+      }
+
+      if (params.Key !== fileKey) {
+        throw new Error(`Unexpected params.Key: ${params.Key}`);
+      }
+
+      return t.context.signedFileUrl.toString();
     }
   };
+
+  t.context = {
+    accessTokenRecord,
+    accessTokenCookie,
+    authClient,
+    getAccessTokenResponse,
+    fileBucket,
+    fileKey,
+    fileLocation,
+    s3Client,
+    authorizationUrl: `https://${randomString()}.com/${randomString()}`,
+    signedFileUrl: new URL(`https://${randomString()}.com/${randomString()}`),
+    authorizationCode: randomString(),
+    distributionUrl: `https://${randomString()}/${randomString()}`
+  };
+});
+
+test.after.always(async () => {
+  const { accessTokenModel } = suiteContext;
+
+  await accessTokenModel.deleteTable();
+});
+
+test('A request for a file without an access token returns a redirect to an OAuth2 provider', async (t) => {
+  const { authClient, fileLocation } = t.context;
 
   const request = {
-    queryStringParameters: {
-      code: randomString(),
-      state: `${granuleBucket}/${granuleKey}`
+    httpMethod: 'GET',
+    resource: '/{proxy+}',
+    pathParameters: {
+      proxy: fileLocation
     }
   };
 
-  await distributionEndpoint.handleRequest(
+  const response = await distributionEndpoint.handleRequest({
+    authClient,
+    request
+  });
+
+  validateRedirectToGetAuthorizationCode(t, response);
+});
+
+test('A request for a file using a non-existent access token returns a redirect to an OAuth2 provider', async (t) => {
+  const { accessTokenModel } = suiteContext;
+  const { authClient, fileLocation } = t.context;
+
+  const accessTokenCookie = new Cookie({
+    key: 'accessToken',
+    value: randomString()
+  });
+
+  const request = {
+    httpMethod: 'GET',
+    resource: '/{proxy+}',
+    pathParameters: {
+      proxy: fileLocation
+    },
+    multiValueHeaders: {
+      cookie: [accessTokenCookie.toString()]
+    }
+  };
+
+  const response = await distributionEndpoint.handleRequest({
+    accessTokenModel,
+    authClient,
+    request
+  });
+
+  validateRedirectToGetAuthorizationCode(t, response);
+});
+
+test('A request for a file using an expired access token returns a redirect to an OAuth2 provider', async (t) => {
+  const { accessTokenModel } = suiteContext;
+  const { authClient, fileLocation } = t.context;
+
+  const accessTokenRecord = fakeAccessTokenFactory({
+    expirationTime: Date.now() - (5 * 1000)
+  });
+  await accessTokenModel.create(accessTokenRecord);
+
+  const accessTokenCookie = new Cookie({
+    key: 'accessToken',
+    value: accessTokenRecord.accessToken
+  });
+
+  const request = {
+    httpMethod: 'GET',
+    resource: '/{proxy+}',
+    pathParameters: {
+      proxy: fileLocation
+    },
+    multiValueHeaders: {
+      cookie: [accessTokenCookie.toString()]
+    }
+  };
+
+  const response = await distributionEndpoint.handleRequest({
+    accessTokenModel,
+    authClient,
+    request
+  });
+
+  validateRedirectToGetAuthorizationCode(t, response);
+});
+
+test('An authenticated request for a file that cannot be parsed returns a 404', async (t) => {
+  const { accessTokenModel } = suiteContext;
+  const { accessTokenCookie, authClient } = t.context;
+
+  t.context.fileLocation = 'invalid';
+
+  const request = {
+    httpMethod: 'GET',
+    resource: '/{proxy+}',
+    pathParameters: {
+      proxy: t.context.fileLocation
+    },
+    multiValueHeaders: {
+      cookie: [accessTokenCookie.toString()]
+    }
+  };
+
+  const response = await distributionEndpoint.handleRequest({
+    accessTokenModel,
+    authClient,
+    request
+  });
+
+  t.is(response.statusCode, 404);
+});
+
+test('An authenticated request for a file returns a redirect to S3', async (t) => {
+  const { accessTokenModel } = suiteContext;
+
+  const {
+    accessTokenCookie,
+    accessTokenRecord,
+    authClient,
+    fileLocation,
+    s3Client,
+    signedFileUrl
+  } = t.context;
+
+  const request = {
+    httpMethod: 'GET',
+    resource: '/{proxy+}',
+    pathParameters: {
+      proxy: fileLocation
+    },
+    multiValueHeaders: {
+      cookie: [accessTokenCookie.toString()]
+    }
+  };
+
+  const response = await distributionEndpoint.handleRequest({
+    accessTokenModel,
+    authClient,
     request,
-    earthdataLoginClient,
     s3Client
+  });
+
+  t.is(response.statusCode, 307);
+
+  validateDefaultHeaders(t, response);
+
+  const { location } = normalizeHeaders(response);
+  const redirectLocation = new URL(location);
+
+  t.is(redirectLocation.origin, signedFileUrl.origin);
+  t.is(redirectLocation.pathname, signedFileUrl.pathname);
+
+  t.is(redirectLocation.searchParams.get('x-EarthdataLoginUsername'), accessTokenRecord.username);
+});
+
+test('A /redirect request with a good authorization code returns a correct response', async (t) => {
+  const { accessTokenModel } = suiteContext;
+
+  const {
+    authClient,
+    authorizationCode,
+    getAccessTokenResponse,
+    distributionUrl,
+    fileLocation,
+    s3Client
+  } = t.context;
+
+  const request = {
+    httpMethod: 'GET',
+    resource: '/redirect',
+    pathParameters: {},
+    multiValueHeaders: {},
+    queryStringParameters: {
+      code: authorizationCode,
+      state: fileLocation
+    }
+  };
+
+  const response = await distributionEndpoint.handleRequest({
+    accessTokenModel,
+    authClient,
+    distributionUrl,
+    request,
+    s3Client
+  });
+
+  t.is(response.statusCode, 307);
+
+  validateDefaultHeaders(t, response);
+
+  t.is(response.headers.Location, `${distributionUrl}/${fileLocation}`);
+
+  const responseAccessTokenCookie = getCookie(response, 'accessToken');
+
+  t.truthy(responseAccessTokenCookie);
+  t.is(responseAccessTokenCookie.value, getAccessTokenResponse.accessToken);
+  t.is(responseAccessTokenCookie.httpOnly, true);
+  t.is(responseAccessTokenCookie.secure, true);
+
+  t.is(
+    responseAccessTokenCookie.expires.valueOf(),
+    // Cookie expirations only have per-second precision
+    getAccessTokenResponse.expirationTime - (getAccessTokenResponse.expirationTime % 1000)
   );
 });
 
-test('A correct error is returned if the granule bucket and key could not be extracted', async (t) => {
-  const earthdataLoginClient = {
-    getAccessToken: async () => ({
-      accessToken: 'my-access-token',
-      refreshToken: 'my-refresh-token',
-      username: 'my-username',
-      expirationTime: 12345
-    })
-  };
+test('A /redirect request with a good authorization code stores the access token', async (t) => {
+  const { accessTokenModel } = suiteContext;
+
+  const {
+    authClient,
+    authorizationCode,
+    distributionUrl,
+    fileLocation,
+    s3Client
+  } = t.context;
 
   const request = {
+    httpMethod: 'GET',
+    resource: '/redirect',
+    pathParameters: {},
+    multiValueHeaders: {},
     queryStringParameters: {
-      code: randomString(),
-      state: 'invalid'
+      code: authorizationCode,
+      state: fileLocation
     }
   };
 
-  const response = await distributionEndpoint.handleRequest(
+  const response = await distributionEndpoint.handleRequest({
+    accessTokenModel,
+    authClient,
+    distributionUrl,
     request,
-    earthdataLoginClient,
-    t.context.s3Client
-  );
+    s3Client
+  });
 
-  t.is(response.statusCode, 400);
+  const responseAccessTokenCookie = getCookie(response, 'accessToken');
 
-  const parsedBody = JSON.parse(response.body);
-
-  t.is(parsedBody.error, 'Granule location "invalid" could not be parsed');
+  t.true(await accessTokenModel.exists({ accessToken: responseAccessTokenCookie.value }));
 });
