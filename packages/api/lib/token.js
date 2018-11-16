264b93efe6b91761d563421349ba25d9418e27a9
Mark Boyd <markdavidboyd@gmail.com>
2018-11-16 11:33:44 -0500

    fix response lib unit tests

--- packages/api/lib/token.js
+++ packages/api/lib/token.js
@@ -1,6 +1,8 @@
 const { sign: jwtSign } = require('jsonwebtoken');
 
 const createJwtToken = ({ accessToken, expirationTime, username }) => {
+  // JWT expiration time is in seconds, not milliseconds
+  expirationTime = Math.floor(expirationTime / 1000);
   return jwtSign({
     exp: expirationTime,
     accessToken,
