c3ce697945d0aa3996db9d374e0e396a7075139f
scisco <me@scisco.io>
2018-11-19 14:37:11 -0500

    resolve PR comments

--- packages/api/lambdas/cleanExecutions.js
+++ packages/api/lambdas/cleanExecutions.js
@@ -2,13 +2,21 @@
 
 const { Execution } = require('../models');
 
+/**
+ * Scans the execution table and remove input and output fields from
+ * records that are older than the retention period
+ * 
+ * @param {Object} ExecutionModel - the execution model class
+ * @returns {Promise} the result of removing old payload records 
+ */
 async function cleanExecutionPayloads(ExecutionModel) {
-  const timeout = parseInt(process.env.executionPayloadTimeout, 10);
-  if (process.env.executionPayloadTimeout === 'disabled') {
+  const timeout = parseInt(process.env.executionPayloadRetentionPeriod, 10);
+  if (process.env.executionPayloadRetentionPeriod === 'disabled') {
     return [];
   }
   if (!Number.isInteger(timeout)) {
-    throw new TypeError(`Invalid number of days specified in configuration for payload_timout: ${process.env.executionPayloadTimeout}`);
+    throw new TypeError('Invalid number of days specified for ' +
+      `executionPayloadRetentionPeriod env variable. It must be a number: ${process.env.executionPayloadRetentionPeriod}`);
   }
   const execution = new ExecutionModel();
   return execution.removeOldPayloadRecords(timeout);
