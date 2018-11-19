9c48ce3fc5ef8a7fb14ce1d6198e4f3e6a8d589e
scisco <me@scisco.io>
2018-11-19 14:52:46 -0500

    fix linting issues

--- packages/api/lambdas/cleanExecutions.js
+++ packages/api/lambdas/cleanExecutions.js
@@ -5,9 +5,9 @@ const { Execution } = require('../models');
 /**
  * Scans the execution table and remove input and output fields from
  * records that are older than the retention period
- * 
+ *
  * @param {Object} ExecutionModel - the execution model class
- * @returns {Promise} the result of removing old payload records 
+ * @returns {Promise} the result of removing old payload records
  */
 async function cleanExecutionPayloads(ExecutionModel) {
   const timeout = parseInt(process.env.executionPayloadRetentionPeriod, 10);
@@ -15,8 +15,8 @@ async function cleanExecutionPayloads(ExecutionModel) {
     return [];
   }
   if (!Number.isInteger(timeout)) {
-    throw new TypeError('Invalid number of days specified for ' +
-      `executionPayloadRetentionPeriod env variable. It must be a number: ${process.env.executionPayloadRetentionPeriod}`);
+    throw new TypeError('Invalid number of days specified for '
+      + `executionPayloadRetentionPeriod env variable. It must be a number: ${process.env.executionPayloadRetentionPeriod}`);
   }
   const execution = new ExecutionModel();
   return execution.removeOldPayloadRecords(timeout);
