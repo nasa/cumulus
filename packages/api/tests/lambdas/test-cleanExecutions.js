73931edaed0fae0241de3f2634eef7cdf20f4e50
Jonathan Kovarik <kovarik@nsidc.org>
2018-11-26 23:55:23 -0700

    Update execution cleanup to take multiple configuration values
    Update execution cleanup to allow cleanup to evaluate completed status
    and 'other' status exeuction records seperately so that non-complete
    execution records may retain their payload history for a different
    amount of time.

--- packages/api/tests/lambdas/test-cleanExecutions.js
+++ packages/api/tests/lambdas/test-cleanExecutions.js
@@ -8,30 +8,51 @@ const { cleanExecutionPayloads } = require('../../lambdas/cleanExecutions');
 const removeRecordsStub = sinon.spy();
 
 class executionModel {
-  removeOldPayloadRecords(value) {
-    return removeRecordsStub(value);
+  removeOldPayloadRecords(...value) {
+    return removeRecordsStub(...value);
   }
 }
 
-test.serial('Function is called with passed in number of days', async (t) => {
-  process.env.executionPayloadRetentionPeriod = 100;
-  await cleanExecutionPayloads(executionModel);
-  t.truthy(removeRecordsStub.calledWith(100));
+test.beforeEach(() => {
+  process.env.completeExecutionPayloadTimeoutDisable = false;
+  process.env.nonCompleteExecutionPayloadTimeoutDisable = false;
 });
 
-test.serial('Function returns empty set if passed in value is disabled', async (t) => {
-  process.env.executionPayloadRetentionPeriod = 'disabled';
+test.serial('Function is called with correct timeout values', async (t) => {
+  process.env.completeExecutionPayloadTimeout = 100;
+  process.env.nonCompleteExecutionPayloadTimeout = 50;
+  await cleanExecutionPayloads(executionModel);
+  t.truthy(removeRecordsStub.calledWith(100, 50, false, false));
+});
+
+test.serial('Function returns empty array if passed in values are disabled', async (t) => {
+  process.env.completeExecutionPayloadTimeout = 100;
+  process.env.nonCompleteExecutionPayloadTimeout = 50;
+  process.env.completeExecutionPayloadTimeoutDisable = true;
+  process.env.nonCompleteExecutionPayloadTimeoutDisable = true;
   const actual = await cleanExecutionPayloads(executionModel);
   const expected = [];
   t.deepEqual(actual, expected);
 });
 
-test.serial('Function throws error if passed invalid RetentionPeriod', async (t) => {
-  process.env.executionPayloadRetentionPeriod = 'testValue';
+test.serial('Function throws error if passed invalid complete timeout', async (t) => {
+  process.env.nonCompleteExecutionPayloadTimeout = 100;
+  process.env.completeExecutionPayloadTimeout = 'notaninteger';
   let actual;
   await cleanExecutionPayloads(executionModel).catch((e) => {
     actual = e.message;
   });
-  const expected = 'Invalid number of days specified for executionPayloadRetentionPeriod env variable. It must be a number: testValue';
+  const expected = 'Invalid number of days specified in configuration for completeExecutionPayloadTimeout: notaninteger';
+  t.is(actual, expected);
+});
+
+test.serial('Function throws error if passed invalid non-complete timeout', async (t) => {
+  process.env.nonCompleteExecutionPayloadTimeout = 'notaninteger';
+  process.env.completeExecutionPayloadTimeout = 100;
+  let actual;
+  await cleanExecutionPayloads(executionModel).catch((e) => {
+    actual = e.message;
+  });
+  const expected = 'Invalid number of days specified in configuration for nonCompleteExecutionPayloadTimeout: notaninteger';
   t.is(actual, expected);
 });
