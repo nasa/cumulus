18dd7ab1caaa49062c9b78b7fcaa9daab549d14e
Marc Huffnagle <marc@element84.com>
2018-12-06 16:14:16 -0500

    Fix bug in providers migration script

--- packages/api/knex_migrations/20181130132407_create_providers_table.js
+++ packages/api/knex_migrations/20181130132407_create_providers_table.js
@@ -1,26 +1,6 @@
 'use strict';
 
-const { knex: { fn: { now: knexNow } } } = require('knex');
 const { DynamoDbScanQueue } = require('@cumulus/common/aws');
-
-function providersModelCallback(table) {
-  table.string('id').primary().notNull();
-
-  table.datetime('created_at', 3).defaultTo(knexNow(3));
-  table.datetime('updated_at', 3).defaultTo(knexNow(3));
-
-  table.integer('global_connection_limit').notNull();
-  table.text('host').notNull();
-  table.enu(
-    'protocol',
-    ['http', 'https', 'ftp', 'sftp', 's3']
-  ).notNull();
-  table.integer('port');
-  table.string('username', 1000);
-  table.string('password', 1000);
-  table.boolean('encrypted');
-  table.json('meta');
-}
 
 function dynamoRecordToPgRecord(dynamoRecord) {
   const fields = ['id', 'port', 'host', 'username', 'password', 'encrypted', 'protocol', 'globalConnectionLimit'];
@@ -36,7 +16,27 @@ function dynamoRecordToPgRecord(dynamoRecord) {
 }
 
 exports.up = async (knex) => {
-  await knex.schema.createTable('providers', providersModelCallback);
+  await knex.schema.createTable(
+    'providers',
+    (table) => {
+      table.string('id').primary().notNull();
+
+      table.datetime('created_at', 3).defaultTo(knex.fn.now(3));
+      table.datetime('updated_at', 3).defaultTo(knex.fn.now(3));
+
+      table.integer('global_connection_limit').notNull();
+      table.text('host').notNull();
+      table.enu(
+        'protocol',
+        ['http', 'https', 'ftp', 'sftp', 's3']
+      ).notNull();
+      table.integer('port');
+      table.string('username', 1000);
+      table.string('password', 1000);
+      table.boolean('encrypted');
+      table.json('meta');
+    }
+  );
 
   if (process.env.ProvidersTable) {
     const dynamoDbScanQueue = new DynamoDbScanQueue({
