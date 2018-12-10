d824d92ab90a2106608dd6cda1cfed861900df6f
Jonathan Kovarik <kovarik@nsidc.org>
2018-12-10 15:39:46 -0700

    Fix webpack quoted properties/errors

--- packages/integration-tests/webpack.config.js
+++ packages/integration-tests/webpack.config.js
@@ -3,9 +3,9 @@ const path = require('path');
 let mode = 'development';
 let devtool = 'inline-source-map';
 
-if(process.env.PRODUCTION) {
-  mode = 'production',
-  devtool = false
+if (process.env.PRODUCTION) {
+  mode = 'production';
+  devtool = false;
 }
 
 module.exports = {
@@ -20,19 +20,19 @@ module.exports = {
     'aws-sdk',
     'electron',
     {
-     'formidable': 'url',
-     'sqlite3': 'sqlite3',
-     'mariasql': 'mariasql',
-     'mssql': 'mssql',
-     'mssql/lib/base': 'mssql/lib/base',
-     'mssql/package.json': 'mssql/package/json',
-     'mysql2': 'mysql2',
-     'tedious': 'tedious',
-     'oracle': 'oracle',
-     'strong-oracle': 'strong-oracle',
-     'oracledb': 'oracledb',
-     'pg': 'pg',
-     'pg-query-stream': 'pg-query-stream'
+      formidable: 'url',
+      sqlite3: 'sqlite3',
+      mariasql: 'mariasql',
+      mssql: 'mssql',
+      'mssql/lib/base': 'mssql/lib/base',
+      'mssql/package.json': 'mssql/package/json',
+      mysql2: 'mysql2',
+      tedious: 'tedious',
+      oracle: 'oracle',
+      'strong-oracle': 'strong-oracle',
+      oracledb: 'oracledb',
+      pg: 'pg',
+      'pg-query-stream': 'pg-query-stream'
     }
   ],
   devtool,
