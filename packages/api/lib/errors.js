5c3e8735fe8d78ba2d6a9bb2a2bac669a09fc4b3
Matt Savoie <savoie@nsidc.org>
2018-12-12 13:42:03 -0700

    remove duplicate code use @cumulus/common/errors

--- packages/api/lib/errors.js
+++ packages/api/lib/errors.js
@@ -1,23 +1,6 @@
 'use strict';
 
-/**
- * Creates a new error type with the given name and parent class. Sets up
- * boilerplate necessary to successfully subclass Error and preserve stack trace
- * @param {string} name - The name of the error type
- * @param {Error} parentType - The error that serves as the parent
- * @return - The new type
- */
-
-const createErrorType = (name, ParentType = Error) => {
-  function E(message) {
-    Error.captureStackTrace(this, this.constructor);
-    this.message = message;
-  }
-  E.prototype = new ParentType();
-  E.prototype.name = name;
-  E.prototype.constructor = E;
-  return E;
-};
+const { createErrorType } = require('@cumulus/common/errors');
 
 module.exports.RecordDoesNotExist = createErrorType('RecordDoesNotExist');
 module.exports.TokenUnauthorizedUserError = createErrorType('TokenUnauthorizedUserError');
