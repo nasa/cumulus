439ee11babc6d22c3f0f6c95707b1b2cc1716252
Lauren Frederick <lpfreder@mbp-lpfreder.edn.ecs.nasa.gov>
2018-12-20 11:37:35 -0500

    Remove code to get stack name from lock script

--- example/scripts/lock-stack.js
+++ example/scripts/lock-stack.js
@@ -6,54 +6,34 @@ const {
   concurrency
 } = require('@cumulus/common');
 const aws = require('@cumulus/common/aws');
-const fs = require('fs-extra');
-const yaml = require('js-yaml');
-const yamlfiles = require('yaml-files');
 
 const STACK_EXPIRATION_MS = 120 * 1000;
 
-const workflowConfigFile = './app/config.yml';
 
-async function getStackName(deployment) {
-  const config = yaml.safeLoad(await fs.readFile(workflowConfigFile, 'utf8'), { schema: yamlfiles.YAML_FILES_SCHEMA });
-
-  console.log(config[deployment].stackName);
-
-  return config[deployment].stackName;
-}
-
-async function performLock(mutex, stackName, cb) {
+async function performLock(mutex, deployment, cb) {
   try {
-    await mutex.writeLock(stackName, STACK_EXPIRATION_MS);
+    await mutex.writeLock(deployment, STACK_EXPIRATION_MS);
 
     return cb(true);
   }
   catch (e) {
-    console.log(`Error locking stack ${stackName}: ${e}`);
-    return cb(false);
+    return cb(`Error locking stack ${deployment}: ${e}`);
   }
 }
 
-async function removeLock(mutex, stackName, cb) {
+async function removeLock(mutex, deployment, cb) {
   try {
-    await mutex.unlock(stackName);
+    await mutex.unlock(deployment);
 
     return cb(true);
   }
   catch (e) {
-    console.log(`Error unlocking stack ${stackName}: ${e}`);
-    return cb(false);
+    return cb(`Error unlocking stack ${deployment}: ${e}`);
   }
 }
 
 async function updateLock(lockFile, deployment, cb) {
   console.log(`deployment: ${deployment}`);
-
-  const stackName = await getStackName(deployment);
-
-  console.log(`stack name: ${stackName}`);
-
-  console.log(`lock: ${lockFile}`);
 
   const dynamodbDocClient = aws.dynamodbDocClient({
     convertEmptyValues: true
@@ -62,10 +42,10 @@ async function updateLock(lockFile, deployment, cb) {
   const mutex = new concurrency.Mutex(dynamodbDocClient, 'lf-test');
 
   if (lockFile === 'true') {
-    return performLock(mutex, stackName, cb);
+    return performLock(mutex, deployment, cb);
   }
 
-  return removeLock(mutex, stackName, cb);
+  return removeLock(mutex, deployment, cb);
 }
 
 updateLock(process.argv[2], process.argv[3], console.log);
