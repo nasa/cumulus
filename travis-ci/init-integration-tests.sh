48465897ee92239d6c6c131e493587bebe066615
Lauren Frederick <lpfreder@mbp-lpfreder.edn.ecs.nasa.gov>
2019-02-12 10:31:02 -0500

    Test to deploy to SIT from travis

--- travis-ci/init-integration-tests.sh
+++ travis-ci/init-integration-tests.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-. ./travis-ci/set-env-vars.sh
+. ./travis-ci/set-env-vars.sh SIT
 
 if [ "$USE_NPM_PACKAGES" = "true" ]; then
   cd example
@@ -12,19 +12,20 @@ else
   cd example
 fi
 
-echo "Locking stack for deployment $DEPLOYMENT"
+# echo "Locking stack for deployment $DEPLOYMENT"
 
-# Wait for the stack to be available
-LOCK_EXISTS_STATUS=$(node ./scripts/lock-stack.js true $DEPLOYMENT)
+# # Wait for the stack to be available
+# cd example
+# LOCK_EXISTS_STATUS=$(node ./scripts/lock-stack.js true $DEPLOYMENT)
 
-echo "Locking status $LOCK_EXISTS_STATUS"
+# echo "Locking status $LOCK_EXISTS_STATUS"
 
-while [ "$LOCK_EXISTS_STATUS" = 1 ]; do
-  echo "Another build is using the ${DEPLOYMENT} stack."
-  sleep 30
+# while [ "$LOCK_EXISTS_STATUS" = 1 ]; do
+#   echo "Another build is using the ${DEPLOYMENT} stack."
+#   sleep 30
 
-  LOCK_EXISTS_STATUS=$(node ./scripts/lock-stack.js true $DEPLOYMENT)
-done
+#   LOCK_EXISTS_STATUS=$(node ./scripts/lock-stack.js true $DEPLOYMENT)
+# done
 
 (
   ./node_modules/.bin/kes cf deploy \
