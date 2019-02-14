2414bf7bf8681856b42314ceebd7ee3baed2520e
Lauren Frederick <lpfreder@mbp-lpfreder.edn.ecs.nasa.gov>
2019-02-14 12:40:40 -0500

    Test to actually unlock stack

--- travis-ci/init-integration-tests.sh
+++ travis-ci/init-integration-tests.sh
@@ -4,9 +4,6 @@ set -e
 
 . ./travis-ci/set-env-vars.sh
 
-echo variables set
-exit 1
-
 if [ "$USE_NPM_PACKAGES" = "true" ]; then
   yarn
 else
@@ -28,8 +25,6 @@ while [ "$LOCK_EXISTS_STATUS" = 1 ]; do
 
   LOCK_EXISTS_STATUS=$(node ./scripts/lock-stack.js true $DEPLOYMENT)
 done
-
-
 
 (
   ./node_modules/.bin/kes cf deploy \
