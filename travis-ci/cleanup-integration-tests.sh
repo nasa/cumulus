1ad01d7391ad9e679aea9234bda7e31f49009bdd
Lauren Frederick <lpfreder@mbp-lpfreder.edn.ecs.nasa.gov>
2019-02-14 16:08:07 -0500

    Stop making builds fail

--- travis-ci/cleanup-integration-tests.sh
+++ travis-ci/cleanup-integration-tests.sh
@@ -13,4 +13,28 @@ fi
 
 cd example || exit 1
 
-./travis-ci/cleanup-stack.sh
+# Delete the stack if it's a nightly build
+if [ "$DEPLOYMENT" = "cumulus-nightly" ]; then
+  npm install
+
+  echo Delete app deployment
+
+  ./node_modules/.bin/kes cf delete \
+    --kes-folder app \
+    --region us-east-1 \
+    --deployment "$DEPLOYMENT" \
+    --yes
+
+  echo Delete iam deployment
+
+  ./node_modules/.bin/kes cf delete \
+    --kes-folder iam \
+    --region us-east-1 \
+    --deployment "$DEPLOYMENT" \
+    --yes
+
+  echo Delete app deployment
+fi
+
+echo Unlocking stack
+node ./scripts/lock-stack.js false $DEPLOYMENT
