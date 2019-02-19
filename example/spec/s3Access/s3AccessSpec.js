feec274d452e27c8a1c9c32063d48097207bd4ea
Lauren Frederick <lpfreder@mbp-lpfreder.edn.ecs.nasa.gov>
2019-02-19 18:30:29 -0500

    Gut unneeded functions from test

--- example/spec/s3Access/s3AccessSpec.js
+++ example/spec/s3Access/s3AccessSpec.js
@@ -26,47 +26,18 @@ async function canAccessObject(region) {
 }
 
 /**
- * Create a message formatted like an sns message to send to the lambda
- * The bucket would not normally be included in this message, but is used
- * for this test
- *
- * @returns {Object} SNS-type message
+ * TO DO:
+ * test bucket should be the protected bucket
+ * Put an object in the bucket
+ * Call the credential API endpoint
+ * Update canAccessObject to take in the keys returned and the lambda use them to initizialize the
+ *    S3 object to try to grab the object in the bucket
  */
-function createSnsMessage() {
-  const message = {
-    synctoken: '0123456789',
-    md5: '6a45316e8bc9463c9e926d5d37836d33',
-    url: 'https://ip-ranges.amazonaws.com/ip-ranges.json',
-    bucket: testBucket // only needed for testing purposes
-  };
-
-  message['create-time'] = '2018-04-24T10:00:s00+00:00';
-
-  const Records = [{
-    Sns: { Message: JSON.stringify(message) }
-  }];
-
-  return { Records };
-}
-
-// Removing these tests for now. With NGAP permissions boundaries, we do not have
-// permission to update a bucket policy
-// Will be resolved with NGAP-3647
 xdescribe('The S3 bucket', () => {
   beforeAll(async () => {
     await s3().createBucket({ Bucket: testBucket }).promise();
 
     await s3().putObject({ Bucket: testBucket, Key: 'test.txt', Body: 'test' }).promise();
-
-    const snsMessage = createSnsMessage();
-
-    const lambda = new Lambda();
-
-    // Invoke the lambda to set the bucket policy
-    await lambda.invoke({
-      FunctionName: `${config.stackName}-InRegionS3Policy`,
-      Payload: JSON.stringify(snsMessage)
-    }).promise();
   });
 
   afterAll(async () => {
