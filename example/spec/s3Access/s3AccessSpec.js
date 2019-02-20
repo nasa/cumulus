f7080b2558f3e2f5237f10d9a8379c089c58c7fe
Lauren Frederick <lpfreder@mbp-lpfreder.edn.ecs.nasa.gov>
2019-02-20 08:26:52 -0500

    Update test

--- example/spec/s3Access/s3AccessSpec.js
+++ example/spec/s3Access/s3AccessSpec.js
@@ -4,7 +4,8 @@ const { loadConfig } = require('../helpers/testUtils');
 
 const config = loadConfig();
 
-const testBucket = `${config.stackName}-s3directaccess-bucket`;
+const testFileKey = `${config.stackName}-s3AccessTest/test.txt`;
+const protectedBucket = config.buckets.protected.name;
 
 /**
  * Calls the s3AccessTest lambda in the given region, which returns
@@ -19,7 +20,7 @@ async function canAccessObject(region) {
 
   const data = await lambda.invoke({
     FunctionName: `${config.stackName}-S3AccessTest`,
-    Payload: JSON.stringify({ Bucket: testBucket, Key: 'test.txt' })
+    Payload: JSON.stringify({ Bucket: protectedBucket, Key: testFileKey })
   }).promise();
 
   return data.Payload;
@@ -27,22 +28,17 @@ async function canAccessObject(region) {
 
 /**
  * TO DO:
- * test bucket should be the protected bucket
- * Put an object in the bucket
  * Call the credential API endpoint
  * Update canAccessObject to take in the keys returned and the lambda use them to initizialize the
  *    S3 object to try to grab the object in the bucket
  */
-xdescribe('The S3 bucket', () => {
+describe('The S3 bucket', () => {
   beforeAll(async () => {
-    await s3().createBucket({ Bucket: testBucket }).promise();
-
-    await s3().putObject({ Bucket: testBucket, Key: 'test.txt', Body: 'test' }).promise();
+    await s3().putObject({ Bucket: protectedBucket, Key: testFileKey, Body: 'test' }).promise();
   });
 
   afterAll(async () => {
-    await s3().deleteObject({ Bucket: testBucket, Key: 'test.txt' }).promise();
-    await s3().deleteBucket({ Bucket: testBucket }).promise();
+    await s3().deleteObject({ Bucket: protectedBucket, Key: testFileKey }).promise();
   });
 
   it('is accessible from us-east-1', async () => {
