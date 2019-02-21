66f0da3b5a1ace018b0045ad2f7c8401e4d08a63
Mark Boyd <markdavidboyd@gmail.com>
2019-02-20 23:31:24 -0500

    update distributionSpec to use new distribution API helpers

--- example/spec/parallel/testAPI/distributionSpec.js
+++ example/spec/parallel/testAPI/distributionSpec.js
@@ -4,7 +4,6 @@ const fs = require('fs');
 const { URL } = require('url');
 const got = require('got');
 
-const { serveDistributionApi } = require('@cumulus/api/bin/serve');
 const {
   file: { getFileChecksumFromStream }
 } = require('@cumulus/common');
@@ -20,6 +19,11 @@ const {
   uploadTestDataToBucket,
   deleteFolder
 } = require('../../helpers/testUtils');
+const {
+  setDistributionApiEnvVars,
+  startDistributionApi,
+  stopDistributionApi
+} = require('../../helpers/apiUtils');
 
 const config = loadConfig();
 const s3Data = [
@@ -31,30 +35,20 @@ describe('Distribution API', () => {
   const testDataFolder = createTestDataPath(testId);
   const fileKey = `${testDataFolder}/MOD09GQ.A2016358.h13v04.006.2016360104606.hdf.met`;
 
-  let server;
-
   process.env.AccessTokensTable = `${config.stackName}-AccessTokensTable`;
-  // Port for distribution API.
-  process.env.PORT = 5002;
-  process.env.DISTRIBUTION_REDIRECT_ENDPOINT = `http://localhost:${process.env.PORT}/redirect`;
-  process.env.DISTRIBUTION_ENDPOINT = `http://localhost:${process.env.PORT}`;
-  // Ensure integration tests use Earthdata login UAT if not specified.
-  if (!process.env.EARTHDATA_BASE_URL) {
-    process.env.EARTHDATA_BASE_URL = 'https://uat.urs.earthdata.nasa.gov';
-  }
 
   beforeAll(async (done) => {
     await uploadTestDataToBucket(config.bucket, s3Data, testDataFolder);
+
     // Use done() callback to signal end of beforeAll() after the
     // distribution API has started up.
-    server = await serveDistributionApi(config.stackName, done);
+    setDistributionApiEnvVars();
+    startDistributionApi(testId, done);
   });
 
   afterAll(async (done) => {
     await deleteFolder(config.bucket, testDataFolder);
-    // Use done() callback to signal end of afterAll() after the
-    // distribution API has shutdown.
-    server.close(done);
+    stopDistributionApi(testId, done);
   });
 
   describe('handles requests for files over HTTPS', () => {
