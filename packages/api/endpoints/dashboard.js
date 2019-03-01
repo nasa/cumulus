e2c38ff775ff1ccd075ddb127e6552374ed9b452
Lauren Frederick <lpfreder@mbp-lpfreder.edn.ecs.nasa.gov>
2019-03-01 09:28:39 -0500

    Endpoint for getting dashboard from bucket

---
+++ packages/api/endpoints/dashboard.js
@@ -0,0 +1,42 @@
+'use strict';
+
+const router = require('express-promise-router')();
+const {
+  aws
+} = require('@cumulus/common');
+
+/**
+ * To Do: Took this from dist, move to common
+ */
+function getFileBucketAndKey(pathParams) {
+  const fields = pathParams.split('/');
+
+  const Bucket = fields.shift();
+  const Key = fields.join('/');
+
+  if (Bucket.length === 0 || Key.length === 0) {
+    throw new UnparsableFileLocationError(pathParams);
+  }
+
+  return [Bucket, Key];
+}
+
+
+/**
+ * TO DO
+ *
+ * @param {Object} req - express request object
+ * @param {Object} res - express response object
+ * @returns {Promise<Object>} the promise of express response object
+ */
+async function get(req, res) {
+  const params = getFileBucketAndKey(req.params[0]);
+
+  const getObjectResponse = await aws.getS3Object(params[0], params[1]);
+  res.set('Content-Type', getObjectResponse.ContentType);
+  return res.send(getObjectResponse.Body.toString());
+}
+
+router.get('/*', get);
+
+module.exports = router;
