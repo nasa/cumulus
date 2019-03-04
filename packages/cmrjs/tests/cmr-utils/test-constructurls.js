1d089100b35616649eebcc92f3934bd279c4b574
Jacob Campbell <jmcampbell@contractor.usgs.gov>
2019-03-04 21:17:39 +0000

    add tests for new url construction functions

--- packages/cmrjs/tests/cmr-utils/test-constructurls.js
+++ packages/cmrjs/tests/cmr-utils/test-constructurls.js
@@ -3,17 +3,20 @@ const rewire = require('rewire');
 
 const { randomId } = require('@cumulus/common/test-utils');
 const { BucketsConfig } = require('@cumulus/common');
+const { omit } = require('@cumulus/common/util');
 
 const cmrUtils = rewire('../../cmr-utils');
 
 const constructOnlineAccessUrls = cmrUtils.__get__('constructOnlineAccessUrls');
+const constructResourceUrls = cmrUtils.__get__('constructResourceUrls');
+const constructRelatedUrls = cmrUtils.__get__('constructRelatedUrls');
 const getS3CredentialsObject = cmrUtils.__get__('getS3CredentialsObject');
 
 
 const sortByURL = (a, b) => a.URL < b.URL;
 
 const endpoint = 'https://endpoint';
-// const s3CredentialsEndpointObject = getS3CredentialsObject(`${endpoint}/s3credentials`);
+const s3CredentialsEndpointObject = getS3CredentialsObject(`${endpoint}/s3credentials`);
 
 
 test.beforeEach((t) => {
@@ -132,4 +135,63 @@ test('returns an array of correct url objects given a list of moved files.', (t)
   });
 
   t.deepEqual(actual.sort(sortByURL), expected.sort(sortByURL));
+});
+
+test('constructResourceUrls returns expected array of echo10xml resource urls', (t) => {
+  const expected = [omit(s3CredentialsEndpointObject, 'Description')];
+  const actual = constructResourceUrls(endpoint);
+
+  t.deepEqual(expected, actual);
+});
+
+test('constructRelatedUrls returns expected array when called with file list', (t) => {
+  const movedFiles = [
+    {
+      key: 'hidden/secretfile.gpg',
+      bucket: t.context.bucketConfig.private.name
+    },
+    {
+      key: 'path/publicfile.jpg',
+      bucket: t.context.bucketConfig.public.name
+    },
+    {
+      key: 'another/path/protected.hdf',
+      bucket: t.context.bucketConfig.protected.name
+    }
+  ];
+
+  const expected = [
+    {
+      URL: `${endpoint}/${t.context.bucketConfig.protected.name}/another/path/protected.hdf`,
+      Description: 'File to download',
+      Type: 'GET DATA'
+    },
+    {
+      URL: `https://${t.context.bucketConfig.public.name}.s3.amazonaws.com/path/publicfile.jpg`,
+      Description: 'File to download',
+      Type: 'GET DATA'
+    },
+    omit(s3CredentialsEndpointObject, 'URLDescription')
+  ];
+
+  const actual = constructRelatedUrls({
+    files: movedFiles,
+    distEndpoint: endpoint,
+    buckets: t.context.buckets
+  });
+
+  t.deepEqual(actual.sort(sortByURL), expected.sort(sortByURL));
+});
+
+test('constructRelatedUrls returns expected array when called with an empty file list', (t) => {
+  const movedFiles = []
+  const expected = [omit(s3CredentialsEndpointObject, 'URLDescription')];
+
+  const actual = constructRelatedUrls({
+    files: movedFiles,
+    distEndpoint: endpoint,
+    buckets: t.context.buckets
+  });
+
+  t.deepEqual(actual, expected);
 });
