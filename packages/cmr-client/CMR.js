57c932380b1efc821c3d9b57c78ccb9d9226fa80
Marc <yjpa7145@users.noreply.github.com>
2019-03-06 16:35:33 -0500

    Update packages/cmr-client/CMR.js
    Co-Authored-By: kkelly51 <katiek515+github@gmail.com>

--- packages/cmr-client/CMR.js
+++ packages/cmr-client/CMR.js
@@ -122,7 +122,7 @@ async function updateToken(cmrProvider, clientId, username, password) {
  * @example
  * const { CMR } = require('@cumulus/cmr-client');
  *
- * const cmrClient = new CMR('my-provider', 'my-clientId', 'my-username', 'my-password');
+ * const cmrClient = new CMR({ provider: 'my-provider', clientId: 'my-clientId', username: 'my-username', password: 'my-password' });
  */
 class CMR {
   /**
