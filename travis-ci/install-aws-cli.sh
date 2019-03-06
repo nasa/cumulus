c620be0b77e1a4b85308e05e919807bb06e4d18b
Marc Huffnagle <marc@element84.com>
2019-03-06 16:58:17 -0500

    Experimenting with Travis

---
+++ travis-ci/install-aws-cli.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+set -e
+
+mkdir -p ~/bin
+curl "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o "awscli-bundle.zip"
+unzip awscli-bundle.zip
+./awscli-bundle/install -b ~/bin/awscli
