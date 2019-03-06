c51caf7959defd069862b53de378804e4d8a557c
Marc Huffnagle <marc@element84.com>
2019-03-06 16:48:17 -0500

    Experimenting with Travis

--- travis-ci/build-cache.sh
+++ travis-ci/build-cache.sh
@@ -2,41 +2,17 @@
 
 set -e
 
-mkdir -p bin
-curl "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o "awscli-bundle.zip"
-unzip awscli-bundle.zip
-./awscli-bundle/install -b ./bin/aws
+./travis-ci/install-aws-cli.sh
 
 # Determine what cache to use (based on all of the package.json files)
 MD5SUM=$(cat $(git ls-files | grep package-lock.json | sort) | md5sum | awk '{print $1}')
 CACHE_FILENAME="${MD5SUM}.tar.gz"
 KEY="travis-ci-cache/${CACHE_FILENAME}"
 
-# Determine if the cache already exists
-DATE=$(date -R)
-STRING_TO_SIGN_HEAD="HEAD
-
-
-${DATE}
-/${CACHE_BUCKET}/${KEY}"
-SIGNATURE=$(/bin/echo -n "$STRING_TO_SIGN_HEAD" | openssl sha1 -hmac ${CACHE_AWS_SECRET_ACCESS_KEY} -binary | base64)
-
-# CACHE_EXISTS_STATUS_CODE=$(curl \
-#   -sS \
-#   -o /dev/null \
-#   -w '%{http_code}' \
-#   --head \
-#   -H "Host: ${CACHE_BUCKET}.s3.amazonaws.com" \
-#   -H "Date: ${DATE}" \
-#   -H "Authorization: AWS ${CACHE_AWS_ACCESS_KEY_ID}:${SIGNATURE}" \
-#   https://${CACHE_BUCKET}.s3.amazonaws.com/${KEY}
-# )
-
-./bin/aws s3 ls "s3://${CACHE_BUCKET}/${KEY}" >/dev/null
+~/bin/aws s3 ls "s3://${CACHE_BUCKET}/${KEY}" >/dev/null
 CACHE_EXISTS_STATUS_CODE="$?"
 
 if [ "$CACHE_EXISTS_STATUS_CODE" -eq "0" ]; then
-# if [ "$CACHE_EXISTS_STATUS_CODE" = "200" ]; then
   # If the cache exists then do nothing
   echo "Cache already exists: s3://${CACHE_BUCKET}/${KEY}"
 else
@@ -58,19 +34,6 @@ else
   echo "Cache size: $CACHE_SIZE"
 
   echo "Uploading cache to s3://${CACHE_BUCKET}/${KEY}"
-  STRING_TO_SIGN_PUT="PUT
 
-
-${DATE}
-/${CACHE_BUCKET}/${KEY}"
-  SIGNATURE=$(/bin/echo -n "$STRING_TO_SIGN_PUT" | openssl sha1 -hmac ${CACHE_AWS_SECRET_ACCESS_KEY} -binary | base64)
-
-  curl \
-    -sS \
-    -X PUT \
-    -T "$CACHE_FILENAME" \
-    -H "Host: ${CACHE_BUCKET}.s3.amazonaws.com" \
-    -H "Date: ${DATE}" \
-    -H "Authorization: AWS ${CACHE_AWS_ACCESS_KEY_ID}:${SIGNATURE}" \
-    https://${CACHE_BUCKET}.s3.amazonaws.com/${KEY}
+  ~/bin/aws s3 cp "$CACHE_FILENAME" "s3://${CACHE_BUCKET}/${KEY}"
 fi
--- travis-ci/fetch-cache.sh
+++ travis-ci/fetch-cache.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+./travis-ci/install-aws-cli.sh
+
 # Determine what cache to use (based on all of the package.json files)
 MD5SUM=$(cat $(git ls-files | grep package-lock.json | sort) | md5sum | awk '{print $1}')
 CACHE_FILENAME="${MD5SUM}.tar.gz"
@@ -10,54 +12,14 @@ KEY="travis-ci-cache/${CACHE_FILENAME}"
 echo "Fetching cache from s3://${CACHE_BUCKET}/${KEY}"
 
 # Determine if the cache exists
-DATE=$(date -R)
-STRING_TO_SIGN_HEAD="HEAD
-
-${DATE}
-/${CACHE_BUCKET}/${KEY}"
-SIGNATURE=$(/bin/echo -n "$STRING_TO_SIGN_HEAD" | openssl sha1 -hmac ${CACHE_AWS_SECRET_ACCESS_KEY} -binary | base64)
-
-# CACHE_EXISTS_STATUS_CODE=$(curl \
-#   -sS \
-#   -o /dev/null \
-#   -w '%{http_code}' \
-#   --head \
-#   -H "Host: ${CACHE_BUCKET}.s3.amazonaws.com" \
-#   -H "Date: ${DATE}" \
-#   -H "Authorization: AWS ${CACHE_AWS_ACCESS_KEY_ID}:${SIGNATURE}" \
-#   https://${CACHE_BUCKET}.s3.amazonaws.com/${KEY}
-# )
-
-aws s3 ls "s3://${CACHE_BUCKET}/${KEY}" >/dev/null
+~/bin/aws s3 ls "s3://${CACHE_BUCKET}/${KEY}" >/dev/null
 CACHE_EXISTS_STATUS_CODE="$?"
 
 if [ "$CACHE_EXISTS_STATUS_CODE" -eq "0" ]; then
   # If the cache exists, download it from S3
   echo "Fetching cache"
 
-#   STRING_TO_SIGN_GET="GET
-
-
-# ${DATE}
-# /${CACHE_BUCKET}/${KEY}"
-#   SIGNATURE=$(/bin/echo -n "$STRING_TO_SIGN_GET" | openssl sha1 -hmac ${CACHE_AWS_SECRET_ACCESS_KEY} -binary | base64)
-
-#   curl \
-#     -sS \
-#     -O \
-#     -H "Host: ${CACHE_BUCKET}.s3.amazonaws.com" \
-#     -H "Date: ${DATE}" \
-#     -H "Authorization: AWS ${CACHE_AWS_ACCESS_KEY_ID}:${SIGNATURE}" \
-#     https://${CACHE_BUCKET}.s3.amazonaws.com/${KEY} || \
-#   curl \
-#     -sS \
-#     -O \
-#     -H "Host: ${CACHE_BUCKET}.s3.amazonaws.com" \
-#     -H "Date: ${DATE}" \
-#     -H "Authorization: AWS ${CACHE_AWS_ACCESS_KEY_ID}:${SIGNATURE}" \
-#     https://${CACHE_BUCKET}.s3.amazonaws.com/${KEY}
-#   tar -xzf "$CACHE_FILENAME"
-#   rm "$CACHE_FILENAME"
+  ~/bin/aws s3 cp "s3://${CACHE_BUCKET}/${KEY}" - | tar -xz
 else
   echo "No cache found" >&2
   exit 1
