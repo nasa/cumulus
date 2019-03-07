982532bde37d66b730852f7f8a23e3397d8ec031
Marc Huffnagle <mhuffnagle@mbp-mhuffnag.edn.ecs.nasa.gov>
2019-03-07 14:09:36 -0500

    Reduce output size

--- travis-ci/build-cache.sh
+++ travis-ci/build-cache.sh
@@ -20,12 +20,13 @@ KEY="travis-ci-cache/${CACHE_FILENAME}"
 echo "Creating cache"
 npm ci
 # npm run bootstrap-no-build
-./node_modules/.bin/lerna --loglevel silly --no-progress bootstrap --ignore-scripts -- --loglevel silly
+# ./node_modules/.bin/lerna --loglevel silly --no-progress bootstrap --ignore-scripts -- --loglevel silly
+./node_modules/.bin/lerna --no-progress bootstrap --ignore-scripts
 
 for p in $(git ls-files | grep package.json); do
   NODE_MODULES_DIR="$(dirname $p)/node_modules"
   if [ -d "$NODE_MODULES_DIR" ]; then
-    tar -rvf "${MD5SUM}.tar" "$(dirname $p)/node_modules"
+    tar -rf "${MD5SUM}.tar" "$(dirname $p)/node_modules"
   fi
 done
 
