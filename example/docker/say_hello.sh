208f67b06939f56b824a6ef277d6c563f8028157
Aimee Barciauskas <aimee@developmentseed.org>
2019-03-10 17:25:30 -0400

    Add container overrides for hello world task

--- example/docker/say_hello.sh
+++ example/docker/say_hello.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 echo Waking up from nap...
-# TODO(aimee): Make this configurable (and pass in values in workflow payload)
-stress --cpu 2 -m 4 --vm-bytes 1024M --timeout 120
+stress --cpu $1 -m $2 --vm-bytes ${3}M --timeout $4
 echo Hello World
