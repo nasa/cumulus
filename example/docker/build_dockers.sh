3cc133a089650a7f7a7701eb792d4da07c2673ba
Aimee Barciauskas <aimee@developmentseed.org>
2019-03-09 23:17:58 -0500

    Modify docker for taking up memory

--- example/docker/build_dockers.sh
+++ example/docker/build_dockers.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 export DOCKER_TAG=hello_world
 export AWS_ACCOUNT_ID=$(cat ../app/.env | grep AWS_ACCOUNT_ID | cut -d'=' -f 2) 
-export AWS_REGION=us-east-1
-
-$(aws ecr get-login --no-include-email --region $AWS_REGION)
 
 docker build -t ${DOCKER_TAG} -f Dockerfile.hello_world .
 docker tag ${DOCKER_TAG}:latest ${AWS_ACCOUNT_ID}.dkr.ecr.$AWS_REGION.amazonaws.com/${DOCKER_TAG}:latest
--- example/docker/say_hello.sh
+++ example/docker/say_hello.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo Waking up from nap...
-stress --cpu  1 --vm-bytes 256M --timeout 120
+stress --cpu 2 -m 4 --vm-bytes 1024M --timeout 120
 echo Hello World
