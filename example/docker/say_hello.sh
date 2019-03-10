559c791e50e14e6928e12e08f94af57184ea59b3
Aimee Barciauskas <aimee@developmentseed.org>
2019-03-09 23:21:20 -0500

    Add TODO

--- example/docker/say_hello.sh
+++ example/docker/say_hello.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 echo Waking up from nap...
+# TODO(aimee): Make this configurable (and pass in values in workflow payload)
 stress --cpu 2 -m 4 --vm-bytes 1024M --timeout 120
 echo Hello World
