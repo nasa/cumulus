df58ec026863335a3003812de0b7e016a21f8905
Menno Van Diermen <mvandiermen@contractor.usgs.gov>
2019-03-15 12:14:47 -0500

    files-to-granule task w/ tests

--- tasks/files-to-granule/index.js
+++ tasks/files-to-granule/index.js
@@ -1,1 +1,84 @@
 'use strict';
+
+const get = require('lodash.get');
+const path = require('path');
+
+const { getGranuleId } = require('@cumulus/cmrjs');
+const { parseS3Uri } = require('@cumulus/common/aws');
+const cumulusMessageAdapter = require('@cumulus/cumulus-message-adapter-js');
+
+/**
+ * Helper to turn an s3URI into a fileobject
+ * @param {string} s3URI s3://mybucket/myprefix/myobject.
+ * @returns {Object} file object
+ */
+function fileObjectFromS3URI(s3URI) {
+  const uriParsed = parseS3Uri(s3URI);
+  return {
+    name: path.basename(s3URI),
+    bucket: uriParsed.Bucket,
+    filename: s3URI,
+    fileStagingDir: path.dirname(uriParsed.Key)
+  };
+}
+
+/**
+ * Takes the files from input and granules and merges them into an object where
+ * each file is associated with it's granuleId.
+ *
+ * @param {Array} inputFiles - list of s3 files to add to the inputgranules
+ * @param {Array} inputGranules - an array of the granules
+ * @param {string} regex - regex needed to extract granuleId from filenames
+ * @returns {Object} an object that contains lists of each granules' files
+ *                   attatched by their granuleId
+ */
+function mergeInputFilesWithInputGranules(inputFiles, inputGranules, regex) {
+  const granulesHash = {};
+  const filesFromInputGranules = {};
+
+  // create hash list of the granules
+  // and a hash list of files
+  inputGranules.forEach((g) => {
+    granulesHash[g.granuleId] = g;
+    g.files.forEach((f) => {
+      filesFromInputGranules[f.filename] = g.granuleId;
+    });
+  });
+
+  // add input files to corresponding granules
+  // the process involve getting granuleId of each file
+  // match it against the granuleObj and adding the new files to the
+  // file list
+  inputFiles.forEach((f) => {
+    if (f && !filesFromInputGranules[f]) {
+      const granuleId = getGranuleId(f, regex);
+      granulesHash[granuleId].files.push(fileObjectFromS3URI(f));
+    }
+  });
+
+  return granulesHash;
+}
+
+function convertFileURIArrayToGranuleObjectArray(event) {
+  const granuleIdExtractionRegex = get(event.config, 'granuleIdExtraction', '(.*)');
+  const inputGranules = get(event.config, 'input_granules', {});
+  const inputFileList = get(event, 'input', []);
+
+  const allGranules = mergeInputFilesWithInputGranules(
+    inputFileList, inputGranules, granuleIdExtractionRegex
+  );
+
+  return {
+    granules: Object.keys(allGranules).map((k) => allGranules[k])
+  };
+}
+
+exports.filesToGranules = convertFileURIArrayToGranuleObjectArray;
+
+function handler(event, context, callback) {
+  cumulusMessageAdapter.runCumulusTask(
+    convertFileURIArrayToGranuleObjectArray, event, context, callback
+  );
+}
+
+exports.handler = handler;
--- tasks/files-to-granule/tests/test-index.js
+++ tasks/files-to-granule/tests/test-index.js
@@ -1,1 +1,27 @@
 'use strict';
+
+const fs = require('fs');
+const path = require('path');
+const test = require('ava');
+const { promisify } = require('util');
+
+const { filesToGranules } = require('..');
+const readFile = promisify(fs.readFile);
+
+async function loadDataJSON(filename) {
+  const payloadPath = path.join(__dirname, 'data', filename);
+  const rawPayload = await readFile(payloadPath, 'utf8');
+  return JSON.parse(rawPayload);
+}
+
+test.beforeEach(async (t) => {
+  t.context.payload = await loadDataJSON('payload.json');
+  t.context.output = await loadDataJSON('output.json');
+});
+
+test('files-to-granules transforms files array to granules object', (t) => {
+  const event = t.context.payload;
+  const expectedOutput = t.context.output;
+  const output = filesToGranules(event);
+  t.deepEqual(output, expectedOutput);
+});
