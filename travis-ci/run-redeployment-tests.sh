d863ea97b37645aecd4bfb9256d08925ef46474c
Matt Savoie <savoie@nsidc.org>
2019-03-18 12:34:50 -0600

    Missed another.

--- travis-ci/run-redeployment-tests.sh
+++ travis-ci/run-redeployment-tests.sh
@@ -2,13 +2,13 @@
 
 set -e
 
-. ./travis-ci/set-env-vars.sh
-
 if [ "$USE_NPM_PACKAGES" = "true" ]; then
   (cd example && rm -rf node_modules && npm install)
 else
   npm install
   npm run bootstrap
 fi
+
+. ./travis-ci/set-env-vars.sh
 
 (cd example && npm run redeploy-test)
