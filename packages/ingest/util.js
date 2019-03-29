15c935376b5af8d7675955e6622d2e98fdc13634
Menno Van Diermen <mvandiermen@contractor.usgs.gov>
2019-03-29 11:30:24 -0500

    remove leading slash from provider path expectations, allow both forms

---
+++ packages/ingest/util.js
@@ -0,0 +1,20 @@
+'use strict';
+
+/**
+ * Ensure provider path conforms to expectations.
+ *
+ * @param {string} provPath - provider path
+ * @returns {string} path, updated to conform if necessary.
+ */
+function conformProviderPath(provPath) {
+  if (provPath) {
+    if (provPath[0] === '/') {
+      return provPath.substr(1);
+    }
+  }
+  return '';
+}
+
+module.exports = {
+  conformProviderPath
+};
