6d078e429623b74e0c5dcdd76833200f9cbe9413
Lauren Frederick <lpfreder@mbp-lpfreder.edn.ecs.nasa.gov>
2019-04-04 14:28:49 -0400

    Revert back to original

--- travis-ci/deploy-to-npm.sh
+++ travis-ci/deploy-to-npm.sh
@@ -9,6 +9,7 @@ NPM_TAG=$(node ./travis-ci/npm-tag.js);
 echo "Publishing packages to NPM with version=${VERSION} and tag=${NPM_TAG}"
 lerna publish \
   --skip-git \
+  --repo-version "$VERSION" \
   --yes \
   --force-publish=* \
   --npm-tag="$NPM_TAG"
