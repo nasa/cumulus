4fc74bf80d891a6e8116cf2fb4e9e88fb6631c94
Lauren Frederick <lpfreder@mbp-lpfreder.edn.ecs.nasa.gov>
2019-04-12 09:40:23 -0400

    Remove reindex functionality from CLI

--- packages/api/bin/cli.js
+++ packages/api/bin/cli.js
@@ -18,69 +18,6 @@ program
   .usage('TYPE COMMAND [options]');
 
 program
-  .command('reindex')
-  .description('Reindex elasticsearch index to a new destination index')
-  .option('-a, --index-alias <indexAlias>', 'AWS Elasticsearch index alias', defaultIndexAlias)
-  .option('--host <host>', 'AWS Elasticsearch host', null)
-  .option('-s, --source-index <sourceIndex>', 'Index to reindex', null)
-  .option('-d, --dest-index <destIndex>',
-    'Name of the destination index, should not be an existing index. Will default to an index named with today\'s date',
-    null)
-  .action((cmd) => {
-    const missingOptions = cliUtils.findMissingOptions(cmd, ['host']);
-    if (missingOptions.length === 0) {
-      es.reindex(
-        cmd.host,
-        cmd.sourceIndex,
-        cmd.destIndex,
-        cmd.indexAlias
-      ).then((response) => console.log(`Reindex successful: ${JSON.stringify(response)}`))
-        .catch((err) => console.error(`Error reindexing: ${err.message}`));
-    }
-    else {
-      cliUtils.displayMissingOptionsMessage(missingOptions);
-    }
-  });
-
-program
-  .command('status')
-  .description('Get the status of the reindex tasks for the given host')
-  .option('--host <host>', 'AWS Elasticsearch host', null)
-  .action((cmd) => {
-    const missingOptions = cliUtils.findMissingOptions(cmd, ['host']);
-    if (missingOptions.length === 0) {
-      es.getStatus(cmd.host)
-        .then((tasks) => console.log(JSON.stringify(tasks)))
-        .catch((err) => console.error(`Error getting status: ${err.message}`));
-    }
-    else {
-      cliUtils.displayMissingOptionsMessage(missingOptions);
-    }
-  });
-
-program
-  .command('complete-reindex')
-  .description('Switch to using the new index (destination index) instead of the source index.')
-  .option('-a, --index-alias <indexAlias>', 'AWS Elasticsearch index alias', 'cumulus-alias')
-  .option('--host <host>', 'AWS Elasticsearch host', null)
-  .option('-s, --source-index <sourceIndex>', 'Index to switch from and no longer used', null)
-  .option('-d, --dest-index <destIndex>', 'Index to be aliased and used as the elasticsearch index for Cumulus', null)
-  .action((cmd) => {
-    const missingOptions = cliUtils.findMissingOptions(cmd, ['host']);
-    if (missingOptions.length === 0) {
-      es.completeReindex(
-        cmd.host,
-        cmd.sourceIndex,
-        cmd.destIndex,
-        cmd.indexAlias
-      ).catch((err) => console.error(`Error: ${err.message}`));
-    }
-    else {
-      cliUtils.displayMissingOptionsMessage(missingOptions);
-    }
-  });
-
-program
   .command('migrate')
   .option('--stack <stack>', 'AWS CloudFormation stack name')
   .option('--migrationVersion <version>', 'Migration version to run')
