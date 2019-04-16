3dbf6c6f301212b24371b23c92e66504f96652d3
Jonathan Kovarik <kovarik@nsidc.org>
2019-04-16 16:02:01 -0600

    Move npm update to bamboo CI, comment out audit

--- bamboo/audit.sh
+++ bamboo/audit.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 ln -s /dev/stdout ./lerna-debug.log
-npm install -g npm # install latest npm version
 npm install --no-audit
 npm run install-locks
-npm run audit
+# npm run audit # Disabling for dev due to ongoing audit issue.
