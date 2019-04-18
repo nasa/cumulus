c742da5c8ac39be5a09af6901ff80ca1acad4009
Jonathan Kovarik <kovarik@nsidc.org>
2019-04-18 11:39:04 -0600

    Cumulus 1203 (#936)
    * Fix IAM template bug
    
    * Add/update override integration tests
    
    * Minor refactor, fix linting
    
    * Temporarily remove audit from travis.yml
    
    * Update CHANGELOG
    
    * Minor refactor: leave pass-through config load yml method
    
    * revert audit step removal
    
    * Fix import order
    
    * Fix errant missing commit

--- example/spec/standalone/redeployment/TemplateOverrideDeploySpec.js
+++ example/spec/standalone/redeployment/TemplateOverrideDeploySpec.js
@@ -1,19 +1,50 @@
 'use strict';
 
 const fs = require('fs-extra');
-const { aws: { cf } } = require('@cumulus/common');
 
 const {
   loadConfig,
-  redeploy
+  runKes
 } = require('../../helpers/testUtils');
-const config = loadConfig();
 
-describe('When an override template is in the application directory', () => {
-  let stackDescription;
+const { loadYmlFile } = require('../../helpers/configUtils');
+
+describe('When an iam override template is in the IAM directory ', () => {
+  let config;
+  let cloudFormation;
   beforeAll(async () => {
-    await redeploy(config);
-    // Redeploy with override file in test directory
+    config = loadConfig('iam');
+    // Compile with override file in test directory
+    try {
+      await fs.mkdir('test_iam_override');
+      await fs.copy('spec/standalone/redeployment/iam_override_template.yml', 'test_iam_override/cloudformation.template.yml');
+      let fileList = await fs.readdir('iam');
+      fileList = fileList.filter((x) => x !== 'build');
+      const promiseList = fileList.map((filename) => fs.copy(`iam/${filename}`, `test_iam_override/${filename}`));
+      await Promise.all(promiseList);
+      await runKes(config, {
+        kesCommand: 'compile',
+        kesClass: 'node_modules/@cumulus/deployment/iam/kes.js',
+        kesFolder: 'test_iam_override',
+        template: 'node_modules/@cumulus/deployment/iam'
+      });
+      cloudFormation = loadYmlFile('test_iam_override/cloudformation.yml');
+    } finally {
+      await fs.remove('test_iam_override');
+    }
+  });
+  it('added a parameter', () => {
+    expect(cloudFormation.Description).toEqual('Overridden IAM Stack Description');
+  });
+});
+
+
+describe('When an application override template is in the application directory', () => {
+  let config;
+  let cloudFormation;
+  beforeAll(async () => {
+    config = loadConfig();
+    // Compile with override file in test directory
     try {
       await fs.mkdir('test_app_override');
       await fs.copy('spec/standalone/redeployment/override_template.yml', 'test_app_override/cloudformation.template.yml');
@@ -21,18 +52,17 @@ describe('When an override template is in the application directory', () => {
       fileList = fileList.filter((x) => x !== 'build');
       const promiseList = fileList.map((filename) => fs.copy(`app/${filename}`, `test_app_override/${filename}`));
       await Promise.all(promiseList);
-      await redeploy(config, {
+      await runKes(config, {
+        kesCommand: 'compile',
         kesClass: 'node_modules/@cumulus/deployment/app/kes.js',
         kesFolder: 'test_app_override'
       });
-      stackDescription = await cf().describeStacks({ StackName: config.stackName }).promise();
+      cloudFormation = loadYmlFile('test_app_override/cloudformation.yml');
     } finally {
       await fs.remove('test_app_override');
     }
   });
-  afterAll(() => redeploy(config));
-
   it('has overridden the stack description', () => {
-    expect(stackDescription.Stacks[0].Description).toEqual('Overridden Stack Description');
+    expect(cloudFormation.Description).toEqual('Overridden Stack Description');
   });
 });
