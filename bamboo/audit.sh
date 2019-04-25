bbf67db84c4e32e3bf413b035c618d18d2d0103a
Jonathan Kovarik <kovarik@nsidc.org>
2019-04-25 15:34:13 -0600

    Clean up script

--- bamboo/audit.sh
+++ bamboo/audit.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
-if [[ ! $(git log --pretty='format:%Creset%s' -1) =~ '[skip-audit]' && ! $(git describe --exact-match HEAD 2>/dev/null |sed -n '1p') =~ ^v[0-9]+.* ]];  then
-npm install --no-audit;
-npm run install-locks;
-npm run audit;
+commit_message_contains_skip_audit=false
+commit_is_version_tag=false
+
+if [[ $(git log --pretty='format:%Creset%s' -1) =~ '[skip-audit]' ]]; then
+  commit_message_contains_skip_audit=true;
+fi
+if [[ $(git describe --exact-match HEAD 2>/dev/null |sed -n '1p') =~ ^v[0-9]+.* ]]; then
+  commit_is_version_tag=true;
+fi
+
+if [[ $commit_message_contains_skip_audit = false && $commit_is_version_tag = false ]]; then
+  npm install --no-audit;
+  npm run install-locks;
+  npm run audit;
 fi
