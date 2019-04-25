b60831b1556d0d80b32f9a9000b07e32992d6c91
Jonathan Kovarik <kovarik@nsidc.org>
2019-04-25 14:41:11 -0600

    Add audit check conditionals.

--- bamboo/audit.sh
+++ bamboo/audit.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-npm install --no-audit
-npm run install-locks
-npm run audit
+if [[ ! $(git log --pretty='format:%Creset%s' -1) =~ '[skip-audit]' && ! $(git describe --exact-match HEAD 2>/dev/null |sed -n '1p') =~ ^v[0-9]+.* ]];  then
+npm install --no-audit;
+npm run install-locks;
+npm run audit;
+fi
