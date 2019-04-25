e19d3e366c4085d96974ebc06710a96c09b8b5e1
Jonathan Kovarik <kovarik@nsidc.org>
2019-04-25 16:10:06 -0600

    Improve variable names

--- bamboo/audit.sh
+++ bamboo/audit.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-commit_message_contains_skip_audit=false
-commit_is_version_tag=false
+commit_message_contains_skip_audit_flag=false
+commit_matches_version_tag=false
 
 if [[ $(git log --pretty='format:%Creset%s' -1) =~ '[skip-audit]' ]]; then
-  commit_message_contains_skip_audit=true;
+  commit_message_contains_skip_audit_flag=true;
 fi
 if [[ $(git describe --exact-match HEAD 2>/dev/null |sed -n '1p') =~ ^v[0-9]+.* ]]; then
-  commit_is_version_tag=true;
+  commit_matches_version_tag=true;
 fi
 
-if [[ $commit_message_contains_skip_audit = false && $commit_is_version_tag = false ]]; then
+if [[ $commit_message_contains_skip_audit_flag = false && $commit_matches_version_tag = false ]]; then
   npm install --no-audit;
   npm run install-locks;
   npm run audit;
