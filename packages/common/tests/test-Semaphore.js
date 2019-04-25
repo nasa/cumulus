83e749301e3687b706ee20a67558d0a28e6e5216
Mark Boyd <markdavidboyd@gmail.com>
2019-04-25 15:23:07 -0400

    update semaphore unit tests

--- packages/common/tests/test-Semaphore.js
+++ packages/common/tests/test-Semaphore.js
@@ -17,21 +17,25 @@ test.before(async () => {
   await manager.createTable();
 });
 
+test.beforeEach(async (t) => {
+  t.context.semaphore = new Semaphore(
+    dynamodbDocClient(),
+    process.env.semaphoreTable
+  );
+  t.context.key = randomId('key');
+});
+
 test.after.always(async () => {
   await manager.deleteTable();
 });
 
-test('Can add up to the max', async (t) => {
-  const semaphore = new Semaphore(
-    dynamodbDocClient(),
-    process.env.semaphoreTable
-  );
-  const key = randomId('key');
+test('Semaphore.add() can increase the count up to the maximum', async (t) => {
+  const { semaphore, key } = t.context;
 
   try {
     await Promise.all([
-      semaphore.up(key),
-      semaphore.up(key)
+      semaphore.add(key, 1, 2),
+      semaphore.add(key, 1, 2)
     ]);
   } catch (err) {
     console.log(err);
@@ -41,12 +45,38 @@ test('Can add up to the max', async (t) => {
   t.pass();
 });
 
-test('Cannot add more than max', async (t) => {
-  const semaphore = new Semaphore(
-    dynamodbDocClient(),
-    process.env.semaphoreTable
-  );
-  const key = randomId('key');
+test('Semaphore.add() cannot increment the count beyond the maximum', async (t) => {
+  const { semaphore, key } = t.context;
+
+  try {
+    await Promise.all([
+      semaphore.add(key, 1, 1),
+      semaphore.add(key, 1, 1)
+    ]);
+    t.fail('expected error to be thrown');
+  } catch (err) {
+    t.pass();
+  }
+});
+
+test('Semaphore.up() can increment the count to the maximum', async (t) => {
+  const { semaphore, key } = t.context;
+
+  try {
+    await Promise.all([
+      semaphore.up(key, 2),
+      semaphore.up(key, 2)
+    ]);
+  } catch (err) {
+    console.log(err);
+    t.fail();
+  }
+
+  t.pass();
+});
+
+test('Semaphore.up() cannot increment the count beyond the maximum', async (t) => {
+  const { semaphore, key } = t.context;
 
   try {
     await Promise.all([
@@ -54,7 +84,7 @@ test('Cannot add more than max', async (t) => {
       semaphore.up(key, 2),
       semaphore.up(key, 2)
     ]);
-    t.fail('expected error');
+    t.fail('expected error to be thrown');
   } catch (err) {
     t.pass();
   }
