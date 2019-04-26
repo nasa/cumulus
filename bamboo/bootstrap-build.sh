07b888a0767c997ec8f74cbaedc051162de7e6fa
Jonathan Kovarik <kovarik@nsidc.org>
2019-04-25 23:16:37 -0600

    Give bamboo write permissions to global node_modules

--- bamboo/bootstrap-build.sh
+++ bamboo/bootstrap-build.sh
@@ -2,5 +2,6 @@
 if [[ ! -z $CI_UID ]]; then
   groupadd -g $CI_UID bamboo
   useradd --gid bamboo --create-home --uid $CI_UID bamboo
+  chown -R bamboo:bamboo /usr/local/lib/node_modules
 fi
 tail -f /dev/null
