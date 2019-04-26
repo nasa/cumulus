237632d1c6c6d6687b26e41769c0f7863d0ecac3
Jonathan Kovarik <kovarik@nsidc.org>
2019-04-26 01:32:51 -0600

    DRY up docker executions

--- bamboo/bootstrap-unit-tests.sh
+++ bamboo/bootstrap-unit-tests.sh
@@ -6,6 +6,8 @@ export SSH_USERS=user:$(id -u):$(id -u)
 ## Set container_id for docker-compose to use to identify the compose stack per planKey
 container_id=${bamboo_planKey,,}
 container_id=${container_id/-/}
+container_id=bamboo
+docker_command='docker exec -t ${container_id}\_build_env_1 /bin/bash -c'
 
 docker ps -a
 
@@ -23,10 +25,10 @@ while ! docker container inspect ${container_id}\_build_env_1; do
 done
 
 ## Setup the build env container once it's started
-docker exec -t ${container_id}\_build_env_1 /bin/bash -c 'npm install --error --no-progress -g nyc; cd /source/cumulus; npm install --error  --no-progress; npm run bootstrap-quiet'
+$docker_command 'npm install --error --no-progress -g nyc; cd /source/cumulus; npm install --error  --no-progress; npm run bootstrap-quiet'
 
 # Wait for the FTP server to be available
-while ! docker exec -t ${container_id}\_build_env_1 /bin/bash -c 'curl --connect-timeout 5 -sS -o /dev/null ftp://testuser:testpass@127.0.0.1/README.md'; do
+while ! $docker_command  'curl --connect-timeout 5 -sS -o /dev/null ftp://testuser:testpass@127.0.0.1/README.md'; do
   echo 'Waiting for FTP to start'
   docker ps -a
   sleep 2
@@ -34,7 +36,7 @@ done
 echo 'FTP service is available'
 
 # Wait for the HTTP server to be available
-while ! docker exec -t ${container_id}\_build_env_1 /bin/bash -c 'curl --connect-timeout 5 -sS -o /dev/null http://127.0.0.1:3030/README.md'; do
+while ! $docker_command  'curl --connect-timeout 5 -sS -o /dev/null http://127.0.0.1:3030/README.md'; do
   echo 'Waiting for HTTP to start'
   docker ps -a
   sleep 2
@@ -45,7 +47,7 @@ echo 'HTTP service is available'
 chmod 0400 ../packages/test-data/keys/ssh_client_rsa_key
 
 # Wait for the SFTP server to be available
-while ! docker exec -t ${container_id}\_build_env_1 /bin/bash -c 'sftp\
+while ! $docker_command 'sftp\
   -P 2222\
   -i /source/cumulus/packages/test-data/keys/ssh_client_rsa_key\
   -o "ConnectTimeout=5"\
@@ -60,25 +62,25 @@ done
 echo 'SFTP service is available'
 
 # Wait for the Elasticsearch service to be available
-while ! docker exec -t ${container_id}\_build_env_1 /bin/bash -c 'nc -z 127.0.0.1 9200'; do
+while ! $docker_command  'nc -z 127.0.0.1 9200'; do
   echo 'Waiting for Elasticsearch to start'
   docker ps -a
   sleep 2
 done
 echo 'Elasticsearch service is started'
 
-while ! docker exec -t ${container_id}\_build_env_1 /bin/bash -c 'curl --connect-timeout 5 -sS http://127.0.0.1:9200/_cluster/health | grep green > /dev/null 2>&1'; do
+while ! $docker_command 'curl --connect-timeout 5 -sS http://127.0.0.1:9200/_cluster/health | grep green > /dev/null 2>&1'; do
   echo 'Waiting for Elasticsearch status to be green'
   sleep 2
 done
 echo 'Elasticsearch status is green'
 
 # Update Elasticsearch config to stop complaining about running out of disk space
-docker exec -t ${container_id}\_build_env_1 /bin/bash -c 'curl -XPUT "http://127.0.0.1:9200/_cluster/settings" -d @/source/cumulus/bamboo/elasticsearch.config'
+$docker_command 'curl -XPUT "http://127.0.0.1:9200/_cluster/settings" -d @/source/cumulus/bamboo/elasticsearch.config'
 
 
 # Lambda seems to be the last service that's started up by Localstack
-while ! docker exec -t ${container_id}\_build_env_1 /bin/bash -c 'nc -z 127.0.0.1 4574'; do
+while ! $docker_command 'nc -z 127.0.0.1 4574'; do
   echo 'Waiting for Localstack Lambda service to start'
   docker ps -a
   sleep 2
