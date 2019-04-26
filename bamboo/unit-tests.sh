913f6e6fcaddebe55bf23872d2d136c8a72d1eb4
Jonathan Kovarik <kovarik@nsidc.org>
2019-04-25 21:02:34 -0600

    Fix container name bug

--- bamboo/unit-tests.sh
+++ bamboo/unit-tests.sh
@@ -7,4 +7,4 @@ docker ps -a ## Show running containers for output logs
 # Run unit tests, excluding integration/api tests
 docker exec -i ${container_id}\_build_env_1 /bin/bash -c 'cd /source/cumulus; nyc ./node_modules/.bin/lerna run test --ignore @cumulus/api --ignore cumulus-integration-tests'
 # Run api tests
-docker exec -i ${container_id}\_env_1 /bin/bash -c 'cd /source/cumulus/packages/api; npm run test-coverage'
+docker exec -i ${container_id}\_build_env_1 /bin/bash -c 'cd /source/cumulus/packages/api; npm run test-coverage'
