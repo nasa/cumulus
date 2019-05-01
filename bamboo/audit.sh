3bf6c349585716d892c7721080f4a098be4199ba
Jonathan Kovarik <kovarik@nsidc.org>
2019-05-01 11:25:26 -0600

    Cumulus 1156 (#959)
    * Add basic build script
    
    * Add log tag so we know the script is running
    
    * Update logging output on the script
    
    * Remove build logging lines
    
    * Add audit script
    
    * Fix perms
    
    * Update audit step
    
    * Move npm update to bamboo CI, comment out audit
    
    * Add unit test compose stack to bamboo
    
    * Add unit test stack/run configuration
    
    * Fix CI UID issue
    
    * Fix bad docker-compose.yml
    
    * Add wait logic in unit test setup script
    
    * Fix config mistake
    
    * Add debugging
    
    * Fix debugging
    
    * Fix source path
    
    * Fixup - no really, fix the path.
    
    * Add ports to build container
    
    * Add additional FTP ports
    
    * Fix credentials path
    
    * Fix pathing
    
    * Add sftp diagnostic output
    
    * Update output
    
    * Skip dep install until sshd is sorted
    
    * Add basic build script
    
    * Add lint/audit scripts
    
    * Purposefully create lint failure for testing
    
    * Add sshd/container updates
    
    * Add plankey to build environment
    
    * Add bamboo directory, update compose to utilize internal network, add
    build scripts
    
    * Remove commented debug line
    
    * Fix eslint
    
    * Use bamboo build key
    
    * Update bamboo env variable
    
    * Fix bootstrap bug
    
    * Downcase bamboo_planKey
    
    * Set script to bin/bash
    
    * Hoist planKey modifications, update all scripts
    
    * Fix container_id
    
    * Fix file permissions on cleanup script
    
    * Add docker status output prior to trying to bring up the stack
    
    * Tidy up
    
    * Add correct container_id
    
    * Comment out bootstrap for debugging
    
    * Debug sftp
    
    * More debugging changes
    
    * re-export ssh users
    
    * Take sshd out of debug mode
    
    * Un-debug docker-compose, add docker ps output for each retry
    
    * Add docker ps to *all* wait loops
    
    * Fix container name bug
    
    * Make lerna/npm slightly less quiet
    
    * Update comments/etc
    
    * Cumulus 1156 seperate container (#962)
    
    * Move all 'up' testing into the build_env container
    
    This has the advantage of avoiding port conflicts entirely on the
    host.
    
    Unfortunately it also makes it less useful for local dev.....
    
    * Remove errant debug statement
    
    * DRY up docker executions
    
    * Remove debugging statement
    
    * Use double quotes for command assignment
    
    * CUMULUS-1157 Add audit check conditionals (#960)
    
    * Add audit check conditionals.
    
    * Clean up script
    
    * Improve variable names

--- bamboo/audit.sh
+++ bamboo/audit.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
-npm install --no-audit
-npm run install-locks
-npm run audit
+commit_message_contains_skip_audit_flag=false
+commit_matches_version_tag=false
+
+if [[ $(git log --pretty='format:%Creset%s' -1) =~ '[skip-audit]' ]]; then
+  commit_message_contains_skip_audit_flag=true;
+fi
+if [[ $(git describe --exact-match HEAD 2>/dev/null |sed -n '1p') =~ ^v[0-9]+.* ]]; then
+  commit_matches_version_tag=true;
+fi
+
+if [[ $commit_message_contains_skip_audit_flag = false && $commit_matches_version_tag = false ]]; then
+  npm install --no-audit;
+  npm run install-locks;
+  npm run audit;
+fi
---
+++ bamboo/unit-tests.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -e
+container_id=${bamboo_planKey,,}
+container_id=${container_id/-/}
+docker ps -a ## Show running containers for output logs
+
+# Run unit tests, excluding integration/api tests
+docker exec -i ${container_id}\_build_env_1 /bin/bash -c 'cd /source/cumulus; nyc ./node_modules/.bin/lerna run test --ignore @cumulus/api --ignore cumulus-integration-tests'
+# Run api tests
+docker exec -i ${container_id}\_build_env_1 /bin/bash -c 'cd /source/cumulus/packages/api; npm run test-coverage'
