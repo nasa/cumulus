d41ca3a68aaabcdc6bcbc46c4ba1e277835e3225
Jonathan Kovarik <kovarik@nsidc.org>
2019-05-02 23:11:47 -0600

    debugging bamboo

--- example/scripts/tests-parallel.sh
+++ example/scripts/tests-parallel.sh
@@ -9,22 +9,21 @@ echo Running parallel integration tests
 DOT_PID="$!"
 
 TESTS=$(find spec/parallel -type f -name '*spec.js' -or -name '*Spec.js')
-
+TESTS="spec/parallel/ingest/ingestFromPdrSpec.js" #debugging
 testOutputDir=scripts/test_output
 
 rm -r -f $testOutputDir
 mkdir -p $testOutputDir
 
 ./node_modules/.bin/parallel -j 1 sh scripts/run_test.sh  $testOutputDir ::: $TESTS
-
+echo "getting results"
 result=$?
-
 echo parallel tests complete: $result suite failures
 
 echo "Echoing TestOutputDirFiles"
 # print test output to console
 for testFile in $testOutputDir/*; do
-  cat $testFile
+cat $testFile
 done
 
 echo "Removing testOutputDir"
