7f4ee05b4686c46f9bf751fa3e210a27e4f71fc2
Jonathan Kovarik <kovarik@nsidc.org>
2019-05-06 20:58:47 -0600

    Tidy up env variable script

--- bamboo/set-integration-test-env-variables.sh
+++ bamboo/set-integration-test-env-variables.sh
@@ -1,66 +1,35 @@
-#!/bin/sh
+#!/bin/bash
 
-# Set the environment variables for a bamboo build
-# based on the NGAP_ENV environment variable
-
-if [ -z $NGAP_ENV ]; then
-  NGAP_ENV=SANDBOX
-fi
-
-declare -a array=(
+declare -a param_list=(
   "bamboo_SECRET_AWS_ACCESS_KEY_ID"
   "bamboo_SECRET_AWS_SECRET_ACCESS_KEY"
   "bamboo_SECRET_AWS_DEFAULT_REGION"
   "bamboo_SECRET_AWS_ACCOUNT_ID"
-  "$bamboo_SECRET_VPC_ID"
-  "$bamboo_SECRET_AWS_SUBNET"
-  "$bamboo_SECRET_PROVIDER_HOST"
-  "$bamboo_SECRET_PROVIDER_HTTP_PORT"
-  "$bamboo_SECRET_PROVIDER_FTP_PORT"
-  "$bamboo_SECRET_VPC_CIDR_IP"
-  "$bamboo_AWS_REGION"
-  "$bamboo_CMR_PASSWORD"
-  "$bamboo_CMR_USERNAME"
-  "$bamboo_TOKEN_SECRET"
+  "bamboo_SECRET_VPC_ID"
+  "bamboo_SECRET_AWS_SUBNET"
+  "bamboo_SECRET_PROVIDER_HOST"
+  "bamboo_SECRET_PROVIDER_HTTP_PORT"
+  "bamboo_SECRET_PROVIDER_FTP_PORT"
+  "bamboo_SECRET_VPC_CIDR_IP"
+  "bamboo_AWS_REGION"
+  "bamboo_CMR_PASSWORD"
+  "bamboo_CMR_USERNAME"
+  "bamboo_SECRET_TOKEN_SECRET"
   "bamboo_SECRET_EARTHDATA_USERNAME"
   "bamboo_SECRET_EARTHDATA_PASSWORD"
   "bamboo_SECRET_EARTHDATA_CLIENT_ID"
-
-
-
+  "bamboo_SECRET_EARTHDATA_CLIENT_PASSWORD"
 )
+regex='bamboo(_SECRET)?_(.*)'
 
-echo Setting variables for environment: $NGAP_ENV
-
-if [ $NGAP_ENV = "SIT" ]; then
-  export AWS_ACCESS_KEY_ID="$SIT_AWS_ACCESS_KEY_ID"
-  export AWS_SECRET_ACCESS_KEY="$SIT_AWS_SECRET_ACCESS_KEY"
-  export AWS_DEFAULT_REGION="$INTEGRATION_AWS_DEFAULT_REGION"
-  export AWS_ACCOUNT_ID="$SIT_AWS_ACCOUNT_ID"
-  export VPC_ID="$SIT_VPC_ID"
-  export AWS_SUBNET="$SIT_AWS_SUBNET"
-  export PROVIDER_HOST="$SIT_PROVIDER_HOST"
-  export VPC_CIDR_IP="$SIT_VPC_CIDR_IP"
-else
-  export AWS_ACCESS_KEY_ID="$bamboo_SECRET_AWS_ACCESS_KEY_ID"
-  export AWS_SECRET_ACCESS_KEY="$bamboo_SECRET_AWS_SECRET_ACCESS_KEY"
-  export AWS_DEFAULT_REGION="$bamboo_SECRET_AWS_DEFAULT_REGION"
-  export AWS_ACCOUNT_ID="$bamboo_SECRET_AWS_ACCOUNT_ID"
-  export VPC_ID="$bamboo_SECRET_VPC_ID"
-  export AWS_SUBNET="$bamboo_SECRET_AWS_SUBNET"
-  export PROVIDER_HOST="$bamboo_SECRET_PROVIDER_HOST"
-  export PROVIDER_HTTP_PORT="$bamboo_SECRET_PROVIDER_HTTP_PORT"
-  export PROVIDER_FTP_PORT="$bamboo_SECRET_PROVIDER_FTP_PORT"
-  export VPC_CIDR_IP="$bamboo_SECRET_VPC_CIDR_IP"
-  export AWS_REGION="$bamboo_AWS_REGION"
-  export CMR_PASSWORD="$bamboo_CMR_PASSWORD"
-  export CMR_USERNAME="$bamboo_CMR_USERNAME"
-  export TOKEN_SECRET="$bamboo_TOKEN_SECRET"
-  export EARTHDATA_USERNAME="$bamboo_SECRET_EARTHDATA_USERNAME"
-  export EARTHDATA_PASSWORD="$bamboo_SECRET_EARTHDATA_PASSWORD"
-  export EARTHDATA_CLIENT_ID="$bamboo_SECRET_EARTHDATA_CLIENT_ID"
-  export EARTHDATA_CLIENT_PASSWORD="$bamboo_SECRET_EARTHDATA_CLIENT_PASSWORD"
-fi
+## Strip 'bamboo_SECRET_' from secret keys
+## Translate bamboo_ keys to expected
+## stack keys
+for key in ${param_list[@]}; do
+  [[ $key =~ bamboo(_SECRET)?_(.*) ]]
+  update_key=${BASH_REMATCH[2]}
+  export $update_key=${!key}
+done
 
 if [ -z "$DEPLOYMENT" ]; then
   DEPLOYMENT=$(node ./bamboo/select-stack.js)
