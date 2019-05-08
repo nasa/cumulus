2189b80815db0ed29228ec5968f74f7ecf610bfb
Jonathan Kovarik <kovarik@nsidc.org>
2019-05-08 14:17:22 -0600

    Add counter to stack lock timeout

--- bamboo/bootstrap-integration-tests.sh
+++ bamboo/bootstrap-integration-tests.sh
@@ -21,13 +21,18 @@ echo "Locking stack for deployment $DEPLOYMENT"
   LOCK_EXISTS_STATUS=$?
   echo "Locking status $LOCK_EXISTS_STATUS"
 
+  COUNTER=0;
   while [[ $LOCK_EXISTS_STATUS = 100 ]]; do
+    if [[ $COUNTER -gt 2 ]]; then
+      exit 1;
+    fi
     echo "Another build is using the ${DEPLOYMENT} stack."
     sleep 30
+    ((counter++))
     /bin/bash -c "node ./scripts/lock-stack.js true $DEPLOYMENT"
     LOCK_EXISTS_STATUS=$?
   done
-  if [[ $LOCK_EXIST_STATUS > 0 ]]; then
+  if [[ $LOCK_EXIST_STATUS -gt 0 ]]; then
     exit 1;
   fi
   set -e
