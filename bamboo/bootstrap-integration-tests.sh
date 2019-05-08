e495aad827e9b2ddb9bf8100a946f9c0bf4ac808
Jonathan Kovarik <kovarik@nsidc.org>
2019-05-07 22:21:35 -0600

    Fix typo

--- bamboo/bootstrap-integration-tests.sh
+++ bamboo/bootstrap-integration-tests.sh
@@ -3,7 +3,7 @@ set -e
 npm config set unsafe-perm true
 npm install
 
-./bamboo/set-integration-test-env-variables.sh
+. ./bamboo/set-integration-test-env-variables.sh
 if [[ $PULL_REQUEST = "false" ]]; then
   echo "******Skipping integration tests as this commit is not a PR"
   exit 0
