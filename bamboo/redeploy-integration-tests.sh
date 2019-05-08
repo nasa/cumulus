17770ddc6b8627d156b30f19628b7e2ce14951fd
Jonathan Kovarik <kovarik@nsidc.org>
2019-05-08 08:42:09 -0600

    Remove PR logic from redeploy tests

--- bamboo/redeploy-integration-tests.sh
+++ bamboo/redeploy-integration-tests.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 set -e
 . ./bamboo/set-bamboo-env-variables.sh
-if [[ $PULL_REQUEST = "false" ]]; then
-  echo "******Skipping integration tests as this commit is not a PR"
-  exit 0
-fi
-
 if [[ $BRANCH == master || $VERSION_TAG || COMMIT_MESSAGE =~ '[run-redeploy-tests]' ]]; then
   (cd example && npm run redeploy-test)
 else
