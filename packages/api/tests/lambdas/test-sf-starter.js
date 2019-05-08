bc72c46e42276083e8879fc33701bd1f0df8fe63
Marc Huffnagle <marc@element84.com>
2019-05-08 16:21:11 -0400

    Break API lambdas into smaller builds

--- packages/api/tests/lambdas/test-sf-starter.js
+++ packages/api/tests/lambdas/test-sf-starter.js
@@ -3,7 +3,7 @@
 const rewire = require('rewire');
 const test = require('ava');
 
-const handler = rewire('../../lambdas/sf-starter');
+const sfStarter = rewire('../../lambdas/sf-starter');
 class stubConsumer {
   async consume() {
     return 9;
@@ -18,12 +18,12 @@ const ruleInput = {
 
 test.serial('throws error when queueUrl is undefined', (t) => {
   const errCb = (err, _data) => t.is(err.message, 'queueUrl is missing');
-  handler(ruleInput, {}, errCb);
+  sfStarter.handler(ruleInput, {}, errCb);
 });
 
 test.serial('calls cb with number of messages received', async (t) => {
   ruleInput.queueUrl = 'queue';
-  handler.__set__('Consumer', stubConsumer);
+  sfStarter.__set__('Consumer', stubConsumer);
   const cb = (_err, data) => t.is(data, 9);
-  handler(ruleInput, {}, cb);
+  sfStarter.handler(ruleInput, {}, cb);
 });
