0ca1ddc8f83f21a2ff828d537e1e2a19b725c649
Jonathan Kovarik <kovarik@nsidc.org>
2019-05-13 11:18:43 -0600

    Cumulus 1160 add integration tests (#972)
    * Initial translation of travis integration tests
    
    * Update env vars script call
    
    * Fix env setup script references
    
    * Fix root perms npm install
    
    * Fix bootstrap script
    
    * Fix npm configuration
    
    * Reconfigure deployment
    
    * Fix cmr configuration
    
    * Change test config
    
    * Set non-default port values
    
    * Add parallel debug ouput/set parallel to 1
    
    * debugging bamboo
    
    * Wrap parallel commands in a shell
    
    * Fix STDIN bug in parallel (╯°□°）╯︵ ┻━┻
    
    * Remove shell, set parallel to 6
    
    * Undebug
    
    * Remove env override
    
    * Remove newline from test execution
    
    * Add integration tests to script
    
    * Tidy up env variable script
    
    * Remove debug output
    
    * Add test output
    
    * Remove stack override
    
    * Add port of integration test cleanup
    
    * Add logic to skip redeploy if branch
    
    Will run only if:
    
    * Branch is master
    * VERSION_TAG is set
    * COMMIT_MESSAGE matches '[run redeploy tests]'
    
    * Fix script flag
    
    * Update env var script name
    
    * fixup
    
    * Add script to detect if a current SHA is the head of a PR
    
    * Set cleanup to run bash
    
    * fix env variable script
    
    * Add github token to env variable translation
    
    * Minor refactor [run-redeploy-tests]
    
    * Test commit [run-redeploy-tests]
    
    * Remove test commit [run-redeploy-tests]
    
    * Add logic to skip integration test if not pull request
    
    * Add logic to skip integration test if not pull request
    
    * Add debugging output
    
    * Fix typo
    
    * Debug lock status
    
    * ""
    
    * Set integration tests to only run on PR.
    
    This is probably part of CUMULUS-1155
    
    * Remove PR logic from redeploy tests
    
    * Remove abort conditional, this will be addressed in CUMULUS-1155
    
    * Fix stack locking bug
    
    * Set lock-stack to emit '100' on failure
    
    * Fix integration test bootstrapping
    
    * Minor bugfix
    
    * Fix shell bug
    
    * ""
    
    * Add counter to stack lock timeout
    
    * Fix minor bug
    
    * ""
    
    * Add lock timeout notice
    
    * Remove unneeded subshell
    
    * Minor refactor/fix
    
    * Change integration stack to jk2 for feature branch
    
    * Make stack lock timeout variable
    
    * Remove content from CUMULUS-1155
    
    * Remove octokit graphql module
    
    * Remove unneeded block
    
    * Fix lint error
    
    * Add comments
    
    * update comment
    
    * Set retry count for audit to 20

---
+++ bamboo/set-bamboo-env-variables.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+set -e
+
+# Bamboo envs are prefixed with bamboo_SECRET to avoid being printed
+declare -a param_list=(
+  "bamboo_SECRET_AWS_ACCESS_KEY_ID"
+  "bamboo_SECRET_AWS_SECRET_ACCESS_KEY"
+  "bamboo_SECRET_AWS_DEFAULT_REGION"
+  "bamboo_SECRET_AWS_ACCOUNT_ID"
+  "bamboo_SECRET_VPC_ID"
+  "bamboo_SECRET_AWS_SUBNET"
+  "bamboo_SECRET_GITHUB_TOKEN"
+  "bamboo_SECRET_PROVIDER_HOST"
+  "bamboo_SECRET_PROVIDER_HTTP_PORT"
+  "bamboo_SECRET_PROVIDER_FTP_PORT"
+  "bamboo_SECRET_VPC_CIDR_IP"
+  "bamboo_AWS_REGION"
+  "bamboo_CMR_PASSWORD"
+  "bamboo_CMR_USERNAME"
+  "bamboo_SECRET_TOKEN_SECRET"
+  "bamboo_SECRET_EARTHDATA_USERNAME"
+  "bamboo_SECRET_EARTHDATA_PASSWORD"
+  "bamboo_SECRET_EARTHDATA_CLIENT_ID"
+  "bamboo_SECRET_EARTHDATA_CLIENT_PASSWORD"
+)
+regex='bamboo(_SECRET)?_(.*)'
+
+## Strip 'bamboo_SECRET_' from secret keys
+## Translate bamboo_ keys to expected stack keys
+for key in ${param_list[@]}; do
+  [[ $key =~ bamboo(_SECRET)?_(.*) ]]
+  update_key=${BASH_REMATCH[2]}
+  export $update_key=${!key}
+done
+
+export COMMIT_MSG=$(git log --pretty='format:%Creset%s' -1)
+export GIT_SHA=$(git rev-parse HEAD)
+echo GIT_SHA is $GIT_SHA
+
+if [[ $(git describe --exact-match HEAD 2>/dev/null |sed -n '1p') =~ ^v[0-9]+.* ]]; then
+  export VERSION_TAG=true
+fi
+echo "Version Tag: $VERSION_TAG"
+
+# Timeout is 40 minutes
+if [ -z $TIMEOUT_PERIODS ]; then
+  TIMEOUT_PERIODS=80
+fi
+
+if [ -z "$DEPLOYMENT" ]; then
+  DEPLOYMENT=$(node ./bamboo/select-stack.js)
+  echo deployment "$DEPLOYMENT"
+  if [ "$DEPLOYMENT" = "none" ]; then
+    echo "Unable to determine integration stack" >&2
+    exit 1
+  fi
+fi
+export DEPLOYMENT
