8890f4dbca1b7b932a91f7516e4711f59a3ea70d
Jonathan Kovarik <kovarik@nsidc.org>
2019-05-16 11:54:18 -0600

    Fix typo

--- bamboo/lint.sh
+++ bamboo/lint.sh
@@ -2,7 +2,7 @@
 # This script is intented to run following bootstrap_lint_audit.sh
 source .bamboo_env_vars || true
 if [[ $GIT_PR != true && BRANCH != master ]]; then
-  echo >&2"******Branch HEAD is not a github PR, and this isn't master, skipping bootstrap/deploy step"
+  echo >&2 "******Branch HEAD is not a github PR, and this isn't master, skipping bootstrap/deploy step"
   exit 0
 fi
 npm install
