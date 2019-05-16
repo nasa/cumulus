3eba3682958cf78f2cb398e41e14330dc953b62a
Jonathan Kovarik <kovarik@nsidc.org>
2019-05-16 11:51:50 -0600

    Add master logic to lint/bootstrap lint_audit step

--- bamboo/bootstrap_lint_audit.sh
+++ bamboo/bootstrap_lint_audit.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # This script runs before lint.sh, audit.sh in the agent container
 source .bamboo_env_vars || true
-if [[ $GIT_PR != true ]]; then
+if [[ $GIT_PR != true && BRANCH != master ]]; then
   >&2 echo "******Branch HEAD is not a github PR, and this isn't a redeployment build, skipping bootstrap/deploy step"
   exit 0
 fi
--- bamboo/lint.sh
+++ bamboo/lint.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 # This script is intented to run following bootstrap_lint_audit.sh
 source .bamboo_env_vars || true
-if [[ $GIT_PR != true ]]; then
-  echo >&2"******Branch HEAD is not a github PR, and this isn't a redeployment build, skipping bootstrap/deploy step"
+if [[ $GIT_PR != true && BRANCH != master ]]; then
+  echo >&2"******Branch HEAD is not a github PR, and this isn't master, skipping bootstrap/deploy step"
   exit 0
 fi
-
 npm install
 npm run bootstrap-no-build
 npm run lint
