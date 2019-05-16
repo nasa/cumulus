884eb997d9af3f0f0221474f1b8035fcb397d151
Jonathan Kovarik <kovarik@nsidc.org>
2019-05-16 12:45:21 -0600

    Fix integration test pass error output

--- bamboo/integration-tests.sh
+++ bamboo/integration-tests.sh
@@ -3,7 +3,7 @@ set -e
 
 source .bamboo_env_vars || true
 if [[ $GIT_PR != true && BRANCH != master ]]; then
-  echo "******Branch HEAD is not a github PR or master, skipping step"
+  >&2 echo "******Branch HEAD is not a github PR or master, skipping step"
   exit 0
 fi
 . ./bamboo/set-bamboo-env-variables.sh
