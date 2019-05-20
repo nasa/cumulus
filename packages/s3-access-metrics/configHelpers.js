e4f68721b00fb35d30b780d97b8d4ab43e5d9a19
Marc <yjpa7145@users.noreply.github.com>
2019-05-20 14:15:00 -0400

    Update packages/s3-access-metrics/configHelpers.js
    Co-Authored-By: Menno Van Diermen <mvandiermen@contractor.usgs.gov>

--- packages/s3-access-metrics/configHelpers.js
+++ packages/s3-access-metrics/configHelpers.js
@@ -75,7 +75,7 @@ module.exports.prefix = configValueOrThrow('prefix', validateIsString);
 module.exports.stack = configValueOrThrow('stack', validateIsString);
 
 // Return the configured subnetIds or false
-module.exports.subnetIds = (_severless, config) => {
+module.exports.subnetIds = (_serverless, config) => {
   if (isNil(config.subnetIds)) return false;
 
   if (isNil(config.vpcId)) throw new Error('Both vpcId and subnetIds must be set');
