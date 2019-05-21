25c2ea529df36956cddda7972936954a765b5265
Jonathan Kovarik <kovarik@nsidc.org>
2019-05-21 12:49:58 -0600

    Update context

--- bamboo/set-status.sh
+++ bamboo/set-status.sh
@@ -6,7 +6,7 @@ source .bamboo_env_vars || true
 ### Post status to github.  Requires set-bamboo-env-varaibles to have been set.
 curl -H\
 "Authorization: token $GITHUB_TOKEN"\
- -d "{\"state\":\"$1\", \"target_url\": \"$2\", \"description\": \"$3\", \"context\": \"continuous-integration/earthdata-bamboo\"}"\
+ -d "{\"state\":\"$1\", \"target_url\": \"$2\", \"description\": \"$3\", \"context\": \"earthdata-bamboo\"}"\
  -H "Content-Type: application/json"\
  -X POST\
  https://api.github.com/repos/nasa/cumulus/statuses/$GIT_SHA
