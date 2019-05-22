7baa23d80ebe45717bc0ac64207cb9b821b8cf00
Jonathan Kovarik <kovarik@nsidc.org>
2019-05-22 17:14:47 -0600

    Add npm install to integration test run

--- bamboo/integration-tests.sh
+++ bamboo/integration-tests.sh
@@ -3,4 +3,5 @@ set -e
 . ./bamboo/abort-if-not-pr-or-redeployment.sh
 . ./bamboo/abort-if-skip-integration-tests.sh
 . ./bamboo/set-bamboo-env-variables.sh
-(cd example && npm test)
+
+(cd example && npm install && npm test)
