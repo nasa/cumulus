7654e23d9972470a63ffd913cbc52a5001504ead
Jonathan Kovarik <kovarik@nsidc.org>
2019-05-22 15:45:44 -0600

    Update 'abort' language to clarify aborting if not a valid PR, not *all* PRs

--- bamboo/abort-if-not-pr-or-redeployment.sh
+++ bamboo/abort-if-not-pr-or-redeployment.sh
@@ -3,6 +3,6 @@ set -e
 
 source .bamboo_env_vars || true
 if [[ $GIT_PR != true && $RUN_REDEPLOYMENT != true ]]; then
-  >&2 echo "******Branch HEAD is not a github PR, and this isn't a redeployment build, skipping step"
+  >&2 echo "******Branch HEAD is not a github PR targeting a protected branch, and this isn't a redeployment build, skipping step"
   exit 0
 fi
