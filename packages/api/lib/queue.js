a1c145cb7ed26425b98734c25dadab283806956b
Mark Boyd <markdavidboyd@gmail.com>
2019-05-22 18:13:23 -0400

    refactor lambdas to use queue helpers

--- packages/api/lib/queue.js
+++ packages/api/lib/queue.js
@@ -2,13 +2,29 @@ const get = require('lodash.get');
 const { isNil } = require('@cumulus/common/util');
 
 /**
+ * Get the queue name from a workflow message.
+ *
+ * @param {Object} message - A workflow message object
+ * @returns {string} - A queue name
+ */
+const getQueueName = (message) => get(message, 'cumulus_meta.queueName');
+
+/**
+ * Get the maximum executions for a queue.
+ *
+ * @param {Object} message - A workflow message object
+ * @returns {number} - Count of the aximum executions for the queue
+ */
+const getMaximumExecutions = (message, queueName) => get(message, `meta.queueExecutionLimits.${queueName}`);
+
+/**
  * Determine if message specifies a queue name.
  *
  * @param {Object} message - A workflow message object
  * @returns {boolean} - True if message has a queue name.
  */
 const hasQueueName = (message) => {
-  const queueName = get(message, 'cumulus_meta.queueName');
+  const queueName = getQueueName(message);
   return !isNil(queueName);
 }
 
@@ -19,12 +35,14 @@ const hasQueueName = (message) => {
  * @returns {boolean} - True if queue has an execution limit.
  */
 const hasExecutionLimit = (message) => {
-  const queueName = get(message, 'cumulus_meta.queueName');
-  const executionLimit  = get(message, `meta.queueExecutionLimits.${queueName}`);
+  const queueName = getQueueName(message);
+  const executionLimit = getMaximumExecutions(message, queueName);
   return !isNil(executionLimit);
 }
 
 module.exports = {
+  getQueueName,
+  getMaximumExecutions,
   hasQueueName,
   hasExecutionLimit
 };
