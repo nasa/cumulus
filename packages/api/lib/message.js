c817656652ce7b11a6091d28430b61f9742c4035
Mark Boyd <markdavidboyd@gmail.com>
2019-05-23 15:22:41 -0400

    refactor message parsing functions

--- packages/api/lib/message.js
+++ packages/api/lib/message.js
@@ -7,15 +7,27 @@ const { isNil } = require('@cumulus/common/util');
  * @param {Object} message - A workflow message object
  * @returns {string} - A queue name
  */
-const getQueueName = (message) => get(message, 'cumulus_meta.queueName');
+const getQueueName = (message) => {
+  const queueName = get(message, 'cumulus_meta.queueName');
+  if (isNil(queueName)) {
+    throw new Error('cumulus_meta.queueName not set in message');
+  }
+  return queueName;
+}
 
 /**
  * Get the maximum executions for a queue.
  *
  * @param {Object} message - A workflow message object
- * @returns {number} - Count of the aximum executions for the queue
+ * @returns {number} - Count of the maximum executions for the queue
  */
-const getMaximumExecutions = (message, queueName) => get(message, `meta.queueExecutionLimits.${queueName}`);
+const getMaximumExecutions = (message, queueName) => {
+  const maxExecutions = get(message, `meta.queueExecutionLimits.${queueName}`);
+  if (isNil(maxExecutions)) {
+    throw new Error(`Could not determine maximum executions for queue ${queueName}`);
+  }
+  return maxExecutions;
+}
 
 /**
  * Determine if message specifies a queue name.
@@ -24,7 +36,12 @@ const getMaximumExecutions = (message, queueName) => get(message, `meta.queueExe
  * @returns {boolean} - True if message has a queue name.
  */
 const hasQueueName = (message) => {
-  const queueName = getQueueName(message);
+  let queueName;
+  try {
+    queueName = getQueueName(message);
+  } catch (err) {
+    return false;
+  }
   return !isNil(queueName);
 }
 
@@ -35,8 +52,13 @@ const hasQueueName = (message) => {
  * @returns {boolean} - True if queue has an execution limit.
  */
 const hasExecutionLimit = (message) => {
-  const queueName = getQueueName(message);
-  const executionLimit = getMaximumExecutions(message, queueName);
+  let executionLimit;
+  try {
+    const queueName = getQueueName(message);
+    executionLimit = getMaximumExecutions(message, queueName);
+  } catch (err) {
+    return false;
+  }
   return !isNil(executionLimit);
 }
 
