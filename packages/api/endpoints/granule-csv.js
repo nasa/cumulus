4ccc3ff5181569536fd0a1c8f15218cc955bd8ba
Katherine Kelly <katherine.a.kelly@nasa.gov>
2019-05-23 13:08:06 -0400

    adding granule csv endpoint

---
+++ packages/api/endpoints/granule-csv.js
@@ -0,0 +1,50 @@
+'use strict';
+
+const { Parser } = require('json2csv');
+const router = require('express-promise-router')();
+
+const { Granule } = require('../models');
+
+/**
+ * Builds a CSV file of all granules in the Cumulus DB
+ *
+ * @param {Object} req - express request object
+ * @param {Object} res - express response object
+ * @returns {Object} the csv file of granules
+ */
+async function list(req, res) {
+  const granuleModel = new Granule();
+  const allDbGranules = await granuleModel.scan();
+  const fields = ['granuleUr', 'collectionId', 'startDateTime', 'endDateTime'];
+  const granuleArray = [];
+  allDbGranules.Items.forEach((granule) => {
+    const granuleUr = granule.granuleId;
+    const collectionId = granule.collectionId;
+    // Only granules in cmr will only have beginningDateTime/endingDateTime
+    const startDate = granule.beginningDateTime || '';
+    const endDate = granule.endingDateTime || '';
+
+    const granuleObject = {
+      'granuleUr': granuleUr,
+      'collectionId': collectionId,
+      'startDateTime': startDate,
+      'endDateTime': endDate
+    };
+    granuleArray.push(granuleObject);
+  });
+  let csv;
+  try {
+    const parser = new Parser({ fields });
+    csv = parser.parse(granuleArray);
+    console.log(csv);
+
+  } catch (error) {
+    console.log(error);
+  }
+
+  return res.send(csv);
+}
+
+router.get('/', list);
+
+module.exports = router;
