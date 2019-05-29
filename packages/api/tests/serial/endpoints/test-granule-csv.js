23e3d006263abf53aa1aa6e2c335bb1a04e75098
Katherine Kelly <katherine.a.kelly@nasa.gov>
2019-05-29 14:58:01 -0400

    adding to tests

--- packages/api/tests/serial/endpoints/test-granule-csv.js
+++ packages/api/tests/serial/endpoints/test-granule-csv.js
@@ -10,16 +10,18 @@ const { Search } = require('../../../es/search');
 const bootstrap = require('../../../lambdas/bootstrap');
 const models = require('../../../models');
 const indexer = require('../../../es/indexer');
+const assertions = require('../../../lib/assertions');
 
 const {
+  fakeAccessTokenFactory,
   fakeGranuleFactoryV2,
   createFakeJwtAuthToken
 } = require('../../../lib/testUtils');
+const { createJwtToken } = require('../../../lib/token');
 const { app } = require('../../../app');
 
 
 process.env.AccessTokensTable = randomId('token');
-// process.env.CollectionsTable = randomId('collection');
 process.env.GranulesTable = randomId('granules');
 process.env.UsersTable = randomId('users');
 process.env.stackName = randomId('stackname');
@@ -35,6 +37,7 @@ let accessTokenModel;
 let granuleModel;
 let accessToken;
 let userModel;
+let fakeGranules;
 
 test.before(async () => {
   esIndex = randomId('esindex');
@@ -61,16 +64,14 @@ test.before(async () => {
   await accessTokenModel.createTable();
 
   accessToken = await createFakeJwtAuthToken({ accessTokenModel, userModel });
-});
 
-test.beforeEach(async (t) => {
   // create fake granule records
-  t.context.fakeGranules = [
-    fakeGranuleFactoryV2({ status: 'completed' }),
-    fakeGranuleFactoryV2({ status: 'failed' })
+  fakeGranules = [
+    fakeGranuleFactoryV2({ status: 'completed', beginningDateTime: '177204' , endingDateTime: '132948'}),
+    fakeGranuleFactoryV2({ status: 'failed', beginningDateTime: '177205' , endingDateTime: '132949' })
   ];
 
-  await Promise.all(t.context.fakeGranules.map((granule) =>
+  await Promise.all(fakeGranules.map((granule) =>
     granuleModel.create(granule)
       .then((record) => indexer.indexGranule(esClient, record, esIndex))));
 });
@@ -83,6 +84,39 @@ test.after.always(async () => {
   await aws.recursivelyDeleteS3Bucket(process.env.system_bucket);
 });
 
+test.serial('GET without an Authorization header returns an Authorization Missing response', async (t) => {
+  const response = await request(app)
+    .get('/granule-csv')
+    .set('Accept', 'application/json')
+    .expect(401);
+
+  assertions.isAuthorizationMissingResponse(t, response);
+});
+
+test.serial('GET with an invalid access token returns an unauthorized response', async (t) => {
+  const response = await request(app)
+    .get('/granule-csv')
+    .set('Accept', 'application/json')
+    .set('Authorization', 'Bearer ThisIsAnInvalidAuthorizationToken')
+    .expect(403);
+
+  assertions.isInvalidAccessTokenResponse(t, response);
+});
+
+test.serial('GET with an unauthorized user returns an unauthorized response', async (t) => {
+  const accessTokenRecord = fakeAccessTokenFactory();
+  await accessTokenModel.create(accessTokenRecord);
+  const jwtToken = createJwtToken(accessTokenRecord);
+
+  const response = await request(app)
+    .get('/granule-csv')
+    .set('Accept', 'application/json')
+    .set('Authorization', `Bearer ${jwtToken}`)
+    .expect(401);
+
+  assertions.isUnauthorizedUserResponse(t, response);
+});
+
 test('GET returns a cvs file of all granules', async (t) => {
   const response = await request(app)
     .get('/granule-csv')
@@ -90,10 +124,16 @@ test('GET returns a cvs file of all granules', async (t) => {
     .set('Authorization', `Bearer ${accessToken}`)
     .expect(200);
 
-  const granuleIds = t.context.fakeGranules.map((g) => g.granuleId);
+  const lines = response.text.split('\n');
   t.is(response.status, 200);
-  t.true(response.text.includes('"granuleUr","collectionId","createdAt","startDateTime","endDateTime"'));
-  granuleIds.forEach((gId) => {
-    t.true(response.text.includes(gId));
+  t.is(lines[0], '"granuleUr","collectionId","createdAt","startDateTime","endDateTime"');
+  fakeGranules.forEach((g) => {
+    const line = lines.filter((l) => l.includes(g.granuleId))[0];
+    const createdDate = new Date(g.createdAt);
+    t.true(line.includes(g.granuleId));
+    t.true(line.includes(g.collectionId));
+    t.true(line.includes(g.beginningDateTime));
+    t.true(line.includes(g.endingDateTime));
+    t.true(line.includes(createdDate.toISOString()));
   });
 });
