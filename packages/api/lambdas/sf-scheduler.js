542695bded84a93ae198f6789b00f72fffb0bd93
ifestus <ifestus@pm.me>
2019-05-29 16:46:23 -0500

    async/await-ify sf-scheduler and add unit tests

--- packages/api/lambdas/sf-scheduler.js
+++ packages/api/lambdas/sf-scheduler.js
@@ -7,6 +7,45 @@ const { getS3Object, parseS3Uri } = require('@cumulus/common/aws');
 const { SQS } = require('@cumulus/ingest/aws');
 const { Provider, Collection } = require('../models');
 
+const buildCumulusMeta = (queueName) => ({
+    execution_name: uuidv4(),
+    queueName
+});
+
+function getProvider(provider) {
+  if (provider) {
+    const p = new Provider();
+    return p.get({ id: provider });
+  }
+  return {};
+}
+
+function getCollection(collection) {
+  if (collection) {
+    const c = new Collection();
+    return c.get({ name: collection.name, version: collection.version });
+  }
+  return {};
+}
+
+function buildMessage(event, baseMessage) {
+  const provider = get(event, 'provider', null);
+  const meta = get(event, 'meta', {});
+  const cumulusMeta = get(event, 'cumulus_meta', {});
+  const collection = get(event, 'collection', null);
+  const payload = get(event, 'payload', {});
+  const queueName = get(event, 'queueName', 'startSF');
+
+  return {
+    ...baseMessage,
+    provider: getProvider(provider),
+    collection: getCollection(collection),
+    meta: merge(baseMessage.meta, meta),
+    payload,
+    cumulus_meta: merge(buildCumulusMeta(queueName), cumulusMeta)
+  };
+}
+
 /**
  * Builds a cumulus-compatible message and adds it to the queue specified
  * by meta.queueName. This queue will then start a stepfunction for the
@@ -16,18 +55,18 @@ const { Provider, Collection } = require('../models');
  * @param   {Object} context - lambda context
  * @param   {function} cb    - lambda callback
  */
-function schedule(event, context, cb) {
+async function schedule(event, context, cb) {
   const template = get(event, 'template');
-  const provider = get(event, 'provider', null);
-  const meta = get(event, 'meta', {});
-  const cumulusMeta = get(event, 'cumulus_meta', {});
-  const collection = get(event, 'collection', null);
-  const payload = get(event, 'payload', {});
-  const queueName = get(event, 'queueName', 'startSF');
-  let message;
 
   const parsed = parseS3Uri(template);
-  getS3Object(parsed.Bucket, parsed.Key)
+  const data = await getS3Object(parsed.Bucket, parsed.Key);
+
+  const message = buildMessage(event, JSON.parse(data.Body));
+
+  const queueName = message.cumulus_meta.queueName;
+  await SQS.sendMessage(message.meta.queues[queueName], message);
+
+  /*getS3Object(parsed.Bucket, parsed.Key)
     .then((data) => {
       message = JSON.parse(data.Body);
       message.meta.provider = {};
@@ -64,6 +103,7 @@ function schedule(event, context, cb) {
     })
     .then((r) => cb(null, r))
     .catch((e) => cb(e));
+    */
 }
 
 module.exports = { schedule };
--- packages/api/tests/lambdas/sf-scheduler.js
+++ packages/api/tests/lambdas/sf-scheduler.js
@@ -1,19 +1,62 @@
 'use strict';
 
 const rewire = require('rewire');
+const sinon = require('sinon');
 const test = require('ava');
 
+const { SQS } = require('@cumulus/ingest/aws');
 const schedule = rewire('../../lambdas/sf-scheduler');
 
-const queueName = 'dont talk to me';
-const scheduleInput = {
-  queueName
+const queueName = 'batman';
+const keyedItem = 'robin';
+const fakeResponse = {
+  meta: {
+    queues: {
+    }
+  }
+};
+fakeResponse.meta.queues[queueName] = keyedItem;
+
+const Bucket = 'my-bucket';
+const Key = 'my-key'
+const restoreParseS3Uri = schedule.__set__('parseS3Uri', () => ({ Bucket, Key }));
+const restoreGetS3Object = schedule.__set__('getS3Object', () => Promise.resolve({
+  Body: JSON.stringify(fakeResponse)
+}));
+
+const scheduleEventTemplate = {
+  meta: {},
+  cumulusMeta: {},
+  payload: {},
+  template: 's3://somewhere/nice'
 };
 
-test.serial('Sends a message to SQS with queueName if queueName is defined', (t) => {
-  schedule(scheduleInput);
+const sqsSpy = sinon.stub(SQS, 'sendMessage').returns(true);
+
+test.afterEach(() => {
+  sqsSpy.resetHistory();
+})
+
+test.after.always(() => {
+  restoreParseS3Uri();
+  restoreGetS3Object();
+
+  sqsSpy.restore();
+})
+
+test.only('Sends a message to SQS with queueName if queueName is defined', async (t) => {
+  const scheduleInput = { ...scheduleEventTemplate, queueName };
+  await schedule.schedule(scheduleInput);
+
+  t.is(sqsSpy.calledOnce, true);
+
+  const sendMessageArgs = sqsSpy.getCall(0).args;
+  console.log(sendMessageArgs[0], keyedItem);
 });
 
 test.serial('Sends a message to SQS with startSF if queueName is not defined', (t) => {
-  schedule(scheduleInput);
+  const scheduleInput = { ...scheduleEventTemplate }
+  schedule.schedule(scheduleInput);
+
+  t.is(sqsSpy.calledOnce, true);
 });
