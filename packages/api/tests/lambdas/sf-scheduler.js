da75be0f81027a837d0dd8fa4b874564d3197c9e
ifestus <ifestus@pm.me>
2019-05-31 15:02:03 -0500

    add explicit assertions that cumulus_meta.queueName is set in sf-scheduler tests

--- packages/api/tests/lambdas/sf-scheduler.js
+++ packages/api/tests/lambdas/sf-scheduler.js
@@ -9,11 +9,12 @@ const schedule = rewire('../../lambdas/sf-scheduler');
 
 const queueName = 'userDefinedQueueName';
 const keyedItem = 'userDefinedQueueUrl';
+const defaultQueueName = 'startSF';
 const fakeMessageResponse = {
   meta: {
     queues: {
       [queueName]: keyedItem,
-      startSF: 'startSFQueueUrl'
+      [defaultQueueName]: 'startSFQueueUrl'
     }
   }
 };
@@ -45,8 +46,9 @@ test.serial('Sends a message to SQS with queueName if queueName is defined', asy
 
   t.is(sqsStub.calledOnce, true);
 
-  const [targetQueueUrl] = sqsStub.getCall(0).args;
+  const [targetQueueUrl, targetMessage] = sqsStub.getCall(0).args;
   t.is(targetQueueUrl, fakeMessageResponse.meta.queues[queueName]);
+  t.is(targetMessage.cumulus_meta.queueName, queueName);
 });
 
 test.serial('Sends a message to SQS with startSF if queueName is not defined', async (t) => {
@@ -55,6 +57,7 @@ test.serial('Sends a message to SQS with startSF if queueName is not defined', a
 
   t.is(sqsStub.calledOnce, true);
 
-  const [targetQueueUrl] = sqsStub.getCall(0).args;
+  const [targetQueueUrl, targetMessage] = sqsStub.getCall(0).args;
   t.is(targetQueueUrl, fakeMessageResponse.meta.queues.startSF);
+  t.is(targetMessage.cumulus_meta.queueName, defaultQueueName);
 });
