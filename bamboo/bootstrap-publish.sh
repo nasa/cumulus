03f2f4d5cbf6f83673ef6d746541e1d086c81fc7
Jonathan Kovarik <kovarik@nsidc.org>
2019-06-03 16:17:52 -0600

    Fix script conditionals, update config values

--- bamboo/bootstrap-publish.sh
+++ bamboo/bootstrap-publish.sh
@@ -2,9 +2,12 @@
 set -e
 
 . ./bamboo/set-bamboo-env-variables.sh
-if [[ -z $DEPLOYMENT ]]; then
+if [[ ! -z $DEPLOYMENT ]]; then
   npm install -g npm
   npm install
+  rm -rf website/build
+  npm run docs-install
+  npm run docs-build
   npm run bootstrap-no-build
   apt-get update; apt-get install jq
   echo "//registry.npmjs.org/:_authToken=${NPM_TOKEN}" > ~/.npmrc
