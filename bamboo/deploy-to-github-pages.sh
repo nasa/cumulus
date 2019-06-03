3c7ee2355c1d6e80c0aa53d6d9d2ce6734b646dc
Jonathan Kovarik <kovarik@nsidc.org>
2019-06-03 16:30:18 -0600

    Use "PUBLISH_FLAG" instead of deployment for publish trigger

--- bamboo/deploy-to-github-pages.sh
+++ bamboo/deploy-to-github-pages.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-if [[ ! -z $DEPLOYMENT ]]; then
+if [[ $PUBLISH_FLAG == true ]]; then
 git clone --depth=50 --branch=gh-pages "https://${GITHUB_USER}:${GITHUB_TOKEN}@github.com/nasa/cumulus.git" gh-pages
 (
   set -e
