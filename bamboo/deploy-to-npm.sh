8cd05f5ce7c8773d2583c68788235456dd229cac
Jonathan Kovarik <kovarik@nsidc.org>
2019-06-04 15:47:43 -0600

    Remove commented out/defanging comments

--- bamboo/deploy-to-npm.sh
+++ bamboo/deploy-to-npm.sh
@@ -6,10 +6,7 @@ if [[ $PUBLISH_FLAG == true ]]; then
   export VERSION=$(jq --raw-output .version lerna.json)
   export NPM_TAG=$(node ./bamboo/npm-tag.js);
 
-  echo NPM tag is $NPM_TAG
-
   echo "Publishing packages to NPM with version=${VERSION} and tag=${NPM_TAG}"
-  echo "Would run
   lerna publish \
     ${VERSION} \
     --no-git-tag-version \
@@ -18,5 +15,4 @@ if [[ $PUBLISH_FLAG == true ]]; then
     --force-publish=* \
     --dist-tag=${NPM_TAG} \
     --exact
-  "
 fi
