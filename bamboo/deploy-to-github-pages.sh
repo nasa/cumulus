81e5021d47f0a7f2b56b0b87b77df8d054053ab5
Jonathan Kovarik <kovarik@nsidc.org>
2019-06-04 13:10:39 -0600

    Update github pages deploy script to utilize test branch

--- bamboo/deploy-to-github-pages.sh
+++ bamboo/deploy-to-github-pages.sh
@@ -3,7 +3,7 @@ set -e
 . ./bamboo/set-bamboo-env-variables.sh
 
 if [[ $PUBLISH_FLAG == true ]]; then
-git clone --depth=50 --branch=gh-pages "https://${GITHUB_USER}:${GITHUB_TOKEN}@github.com/nasa/cumulus.git" gh-pages
+git clone --depth=50 --branch=gh-pages-test "https://${GITHUB_USER}:${GITHUB_TOKEN}@github.com/nasa/cumulus.git" gh-pages
 (
   set -e
   cd gh-pages
@@ -12,6 +12,6 @@ git clone --depth=50 --branch=gh-pages "https://${GITHUB_USER}:${GITHUB_TOKEN}@g
   git add .
   git commit -m "Automated build in Bamboo CI"
   echo "Push disabled as we don't want to use bamboo to publish, yet"
-  #git push
+  git push
 )
 fi
