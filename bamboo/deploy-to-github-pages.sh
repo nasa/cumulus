0ceeb03e1e5ccc5bf7432b5ae9dc658b3181a345
Jonathan Kovarik <kovarik@nsidc.org>
2019-06-04 15:12:34 -0600

    re-comment pages push

--- bamboo/deploy-to-github-pages.sh
+++ bamboo/deploy-to-github-pages.sh
@@ -11,6 +11,6 @@ git clone --depth=50 --branch=gh-pages "https://${GITHUB_USER}:${GITHUB_TOKEN}@g
   rsync -av ../website/build/Cumulus/ .
   git add .
   git commit -m "Automated build in Bamboo CI"
-  git push
+  # it push
 )
 fi
