d894e75d922669215ebe0baa046f668f8c4486d5
Jonathan Kovarik <kovarik@nsidc.org>
2019-06-04 15:03:47 -0600

    Remove debugging statements

--- bamboo/abort-if-not-pr-or-redeployment.sh
+++ bamboo/abort-if-not-pr-or-redeployment.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 set -e
 
-echo "PWD is $(pwd)"
-ls -ltra
-printenv
 source .bamboo_env_vars || true
-echo "Run redeployment is $RUN_REDEPLOYMENT"
 if [[ $GIT_PR != true && $RUN_REDEPLOYMENT != true ]]; then
   >&2 echo "******Branch HEAD is not a github PR targeting a protected branch, and this isn't a redeployment build, skipping step"
   exit 0
