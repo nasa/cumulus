41c8941b4a062b424c96e4e5ae72b5010a1be3c6
Jonathan Kovarik <kovarik@nsidc.org>
2019-06-03 20:43:37 -0600

    Add logging statement

--- bamboo/abort-if-not-pr-or-redeployment.sh
+++ bamboo/abort-if-not-pr-or-redeployment.sh
@@ -2,6 +2,7 @@
 set -e
 
 source .bamboo_env_vars || true
+echo "Run redeployment is $RUN_REDEPLOYMENT"
 if [[ $GIT_PR != true && $RUN_REDEPLOYMENT != true ]]; then
   >&2 echo "******Branch HEAD is not a github PR targeting a protected branch, and this isn't a redeployment build, skipping step"
   exit 0
