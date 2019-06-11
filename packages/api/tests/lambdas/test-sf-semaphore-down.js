41cf2e638418431d81b918a1261571b2ed303c42
Marc <yjpa7145@users.noreply.github.com>
2019-06-11 14:16:08 -0400

    Remove jobs task (#1044)
    * Remove jobs task
    
    Also, upgrade @cumulus/api to use ava 2.1.0
    
    * Fix bug in api test command
    
    * Remove and deprecate code
    
    * Trigger CI

--- packages/api/tests/lambdas/test-sf-semaphore-down.js
+++ packages/api/tests/lambdas/test-sf-semaphore-down.js
@@ -146,24 +146,26 @@ test('sfSemaphoreDown lambda does nothing for an event with no message', async (
 });
 
 test('sfSemaphoreDown lambda throws error when attempting to decrement empty semaphore', async (t) => {
-  const queueName = randomId('low');
-
-  await t.throws(handleSemaphoreDecrementTask(
-    createCloudwatchEventMessage({
-      status: 'COMPLETED',
-      queueName
-    })
-  ));
+  await t.throwsAsync(
+    () => handleSemaphoreDecrementTask(
+      createCloudwatchEventMessage({
+        status: 'COMPLETED',
+        queueName: randomId('low')
+      })
+    )
+  );
 });
 
 test('sfSemaphoreDown lambda throws error for invalid event message', async (t) => {
-  await t.throws(handleSemaphoreDecrementTask({
-    source: sfEventSource,
-    detail: {
-      status: 'COMPLETED',
-      output: 'invalid message'
-    }
-  }));
+  await t.throwsAsync(
+    () => handleSemaphoreDecrementTask({
+      source: sfEventSource,
+      detail: {
+        status: 'COMPLETED',
+        output: 'invalid message'
+      }
+    })
+  );
 });
 
 test('sfSemaphoreDown lambda decrements semaphore for completed event message', async (t) => {
