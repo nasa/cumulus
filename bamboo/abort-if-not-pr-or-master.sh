607006cce4deac7908e537c146a6005e9080a083
Jonathan Kovarik <kovarik@nsidc.org>
2019-06-13 10:03:32 -0600

    Fix abort script typo

--- bamboo/abort-if-not-pr-or-master.sh
+++ bamboo/abort-if-not-pr-or-master.sh
@@ -2,7 +2,7 @@
 set -e
 
 source .bamboo_env_vars || true
-if [[ $GIT_PR != true && BRANCH != master ]]; then
+if [[ $GIT_PR != true && $BRANCH != master ]]; then
   >&2 echo "******Branch HEAD is not a github PR targeting a protected bran
   ch, and this isn't the master branch, skipping step"
   exit 0
