f555cefb1c233b757981b538c7a5fcae02baa956
Jonathan Kovarik <kovarik@nsidc.org>
2019-06-14 16:30:17 -0600

    Un-stub listing in 'update-provider', and query specific provider
    I'm not sure why we were querying the full list here, and perhaps more
    importantly why we were stubbing the search primitive -- that seems
    like 'not what we want' unless I'm missing something.

--- packages/api/tests/endpoints/providers/update-provider.js
+++ packages/api/tests/endpoints/providers/update-provider.js
@@ -4,6 +4,7 @@ const test = require('ava');
 const request = require('supertest');
 const sinon = require('sinon');
 const { randomString } = require('@cumulus/common/test-utils');
+const pick = require('lodash.pick');
 
 const bootstrap = require('../../../lambdas/bootstrap');
 const models = require('../../../models');
@@ -100,21 +101,15 @@ test.serial('PUT updates an existing provider and returns it in listing', async 
     .set('Authorization', `Bearer ${jwtAuthToken}`)
     .expect(200);
 
-  t.plan(2);
-  const stub = sinon.stub(Search.prototype, 'query').resolves({
-    results: [updatedProvider]
-  });
-
+  t.plan(1);
   const response = await request(app)
-    .get('/providers')
+    .get(`/providers/${t.context.testProvider.id}`)
     .set('Accept', 'application/json')
     .set('Authorization', `Bearer ${jwtAuthToken}`)
     .expect(200);
-
-  const { results } = response.body;
-  stub.restore();
-  t.is(results.length, 1);
-  t.deepEqual(results[0], updatedProvider);
+  let results = response.body;
+  results = pick(results, Object.keys(results).filter(key => !['createdAt', 'updatedAt'].includes(key)));
+  t.deepEqual(results, updatedProvider);
 });
 
 test('PUT without an Authorization header returns an Authorization Missing response and does not update an existing provider', async (t) => {
