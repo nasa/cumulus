379cfcbedc3169634f73bfddae35eca08cb6ccba
Mark Boyd <markdavidboyd@gmail.com>
2019-06-19 18:38:15 -0400

    simplify message queueing for startSFSpec

--- example/spec/parallel/startSF/startSfSpec.js
+++ example/spec/parallel/startSF/startSfSpec.js
@@ -4,32 +4,19 @@ const {
   lambda,
   sfn,
   sqs,
-  s3PutObject,
-  deleteS3Object,
   dynamodbDocClient
 } = require('@cumulus/common/aws');
 const StepFunctions = require('@cumulus/common/StepFunctions');
-const {
-  addCollections,
-  deleteCollections
-} = require('@cumulus/integration-tests');
-const {
-  deleteRule,
-  postRule,
-  rerunRule
-} = require('@cumulus/integration-tests/api/rules');
 
 const {
   loadConfig,
   createTimestampedTestId,
-  timestampedName,
-  createTestSuffix
+  timestampedName
 } = require('../../helpers/testUtils');
 
 const config = loadConfig();
 
 const testName = createTimestampedTestId(config.stackName, 'testStartSf');
-const testSuffix = createTestSuffix(testName);
 
 const passSfRoleArn = `arn:aws:iam::${config.awsAccountId}:role/${config.stackName}-steprole`;
 
@@ -76,14 +63,53 @@ const waitPassSfParams = {
   roleArn: passSfRoleArn
 };
 
-const sfStarterName = `${config.stackName}-sqs2sf`;
+async function sendStartSfMessages({
+  numOfMessages,
+  queueMaxExecutions,
+  queueName,
+  queueUrl,
+  workflowArn
+}) {
+  const message = {
+    cumulus_meta: {
+      queueName,
+      state_machine: workflowArn
+    },
+    meta: {
+      queues: {
+        [queueName]: queueUrl
+      }
+    }
+  };
 
-function generateStartSfMessages(num, workflowArn) {
-  const arr = [];
-  for (let i = 0; i < num; i += 1) {
-    arr.push({ cumulus_meta: { state_machine: workflowArn }, payload: `message #${i}` });
+  if (queueMaxExecutions) {
+    message.meta.queueExecutionLimits = {
+      [queueName]: queueMaxExecutions
+    };
   }
-  return arr;
+
+  const messages = new Array(numOfMessages)
+    .fill()
+    .map(() => ({
+      cumulus_meta: {
+        queueName,
+        state_machine: workflowArn
+      },
+      meta: {
+        queues: {
+          [queueName]: queueUrl
+        },
+        queueExecutionLimits: {
+          [queueName]: queueMaxExecutions
+        }
+      }
+    }));
+  return Promise.all(
+    messages.map(
+      (msg) =>
+        sqs().sendMessage({ QueueUrl: queueUrl, MessageBody: JSON.stringify(msg) }).promise()
+    )
+  );
 }
 
 describe('the sf-starter lambda function', () => {
@@ -93,18 +119,21 @@ describe('the sf-starter lambda function', () => {
   });
 
   describe('when provided a queue', () => {
+    const sfStarterName = `${config.stackName}-sqs2sf`;
     const initialMessageCount = 30;
     const testMessageLimit = 25;
     let qAttrParams;
     let messagesConsumed;
     let passSfArn;
+    let queueName;
     let queueUrl;
 
     beforeAll(async () => {
-      console.log(`queue name: ${testName}Queue`);
+      queueName = `${testName}Queue`;
+      console.log(`queue name: ${queueName}`);
 
       const { QueueUrl } = await sqs().createQueue({
-        QueueName: `${testName}Queue`
+        QueueName: queueName
       }).promise();
       queueUrl = QueueUrl;
 
@@ -116,11 +145,12 @@ describe('the sf-starter lambda function', () => {
       const { stateMachineArn } = await sfn().createStateMachine(passSfParams).promise();
       passSfArn = stateMachineArn;
 
-      const msgs = generateStartSfMessages(initialMessageCount, passSfArn);
-      await Promise.all(
-        msgs.map((msg) =>
-          sqs().sendMessage({ QueueUrl: queueUrl, MessageBody: JSON.stringify(msg) }).promise())
-      );
+      await sendStartSfMessages({
+        numOfMessages: initialMessageCount,
+        queueName,
+        queueUrl,
+        workflowArn: passSfArn
+      });
     });
 
     afterAll(async () => {
@@ -139,7 +169,7 @@ describe('the sf-starter lambda function', () => {
     });
 
     it('consumes the messages', async () => {
-      const response = await lambda().invoke({
+      const { Payload } = await lambda().invoke({
         FunctionName: sfStarterName,
         InvocationType: 'RequestResponse',
         Payload: JSON.stringify({
@@ -147,10 +177,6 @@ describe('the sf-starter lambda function', () => {
           messageLimit: testMessageLimit
         })
       }).promise();
-      console.log('time', new Date());
-      console.log('response', response);
-      const { Payload } = response;
-      console.log(`request ID: ${response.$response.requestId}`);
       messagesConsumed = parseInt(Payload, 10);
       expect(messagesConsumed).toBeGreaterThan(0);
     });
@@ -165,6 +191,7 @@ describe('the sf-starter lambda function', () => {
     it('to trigger workflows', async () => {
       console.log(`Pass SF arn: ${passSfArn}`);
       const { executions } = await StepFunctions.listExecutions({ stateMachineArn: passSfArn });
+      console.log(`Pass SF executions length: ${executions.length}`);
       expect(executions.length).toBe(messagesConsumed);
     });
   });
@@ -172,8 +199,6 @@ describe('the sf-starter lambda function', () => {
   describe('when provided a queue with a maximum number of executions', () => {
     let maxQueueUrl;
     let maxQueueName;
-    let templateKey;
-    let templateUri;
     let messagesConsumed;
     let waitPassSfArn;
 
@@ -181,15 +206,6 @@ describe('the sf-starter lambda function', () => {
     const numRuleMessages = 10;
     const numGranuleMessages = 10;
     const totalNumMessages = numRuleMessages + numGranuleMessages;
-
-    const collectionsDir = './data/collections/s3_MOD09GQ_006';
-    const collection = {
-      name: `MOD09GQ${testSuffix}`,
-      dataType: `MOD09GQ${testSuffix}`,
-      version: '006'
-    };
-
-    const ruleName = timestampedName('waitPassRule');
 
     beforeAll(async () => {
       maxQueueName = `${testName}MaxQueue`;
@@ -200,73 +216,23 @@ describe('the sf-starter lambda function', () => {
       }).promise();
       maxQueueUrl = QueueUrl;
 
-      templateKey = `${config.stackName}/workflows/${waitPassSfName}.json`;
-      templateUri = `s3://${config.bucket}/${templateKey}`;
-
       const { stateMachineArn } = await sfn().createStateMachine(waitPassSfParams).promise();
       waitPassSfArn = stateMachineArn;
 
-      await s3PutObject({
-        Bucket: config.bucket,
-        Key: templateKey,
-        Body: JSON.stringify({
-          cumulus_meta: {
-            state_machine: waitPassSfArn
-          },
-          meta: {
-            queues: {
-              [maxQueueName]: maxQueueUrl
-            },
-            queueExecutionLimits: {
-              [maxQueueName]: queueMaxExecutions
-            }
-          }
-        })
+      await sendStartSfMessages({
+        numOfMessages: totalNumMessages,
+        queueMaxExecutions,
+        queueName: maxQueueName,
+        queueUrl: maxQueueUrl,
+        workflowArn: waitPassSfArn
       });
-
-      await Promise.all([
-        postRule({
-          prefix: config.stackName,
-          rule: {
-            name: ruleName,
-            workflow: waitPassSfName,
-            collection: {
-              name: collection.name,
-              version: collection.version
-            },
-            state: 'ENABLED',
-            rule: {
-              type: 'onetime'
-            },
-            queueName: maxQueueName
-          }
-        }),
-        addCollections(config.stackName, config.bucket, collectionsDir, testSuffix)
-      ]);
-
-      const runRules = new Array(numRuleMessages)
-        .fill()
-        .map(() => rerunRule({
-          prefix: config.stackName,
-          ruleName
-        }));
-
-      await Promise.all(runRules);
     });
 
     afterAll(async () => {
-      // Have to delete rule before associated collection
-      await deleteRule({
-        prefix: config.stackName,
-        ruleName
-      });
-
       await Promise.all([
-        deleteS3Object(config.bucket, templateKey),
         sqs().deleteQueue({
           QueueUrl: maxQueueUrl
         }).promise(),
-        deleteCollections(config.stackName, config.bucket, [collection]),
         sfn().deleteStateMachine({ stateMachineArn: waitPassSfArn }).promise(),
         dynamodbDocClient().delete({
           TableName: `${config.stackName}-SemaphoresTable`,
@@ -275,60 +241,6 @@ describe('the sf-starter lambda function', () => {
           }
         }).promise()
       ]);
-    });
-
-    it('queue-granules returns the correct amount of queued executions', async () => {
-      // Create granule records
-      const granules = new Array(numGranuleMessages)
-        .fill()
-        .map((_, index) => ({
-          granuleId: `granule${index}`,
-          dataType: collection.dataType,
-          version: collection.version,
-          files: []
-        }));
-
-      const { Payload } = await lambda().invoke({
-        FunctionName: `${config.stackName}-QueueGranules`,
-        InvocationType: 'RequestResponse',
-        Payload: JSON.stringify({
-          cumulus_meta: {
-            message_source: 'local',
-            execution_name: 'test-execution',
-            task: 'QueueGranules'
-          },
-          meta: {
-            stack: config.stackName,
-            buckets: {
-              internal: {
-                name: config.bucket
-              }
-            },
-            templates: {
-              [waitPassSfName]: templateUri
-            },
-            provider: {},
-            queues: {
-              [maxQueueName]: maxQueueUrl
-            }
-          },
-          workflow_config: {
-            QueueGranules: {
-              stackName: '{{$.meta.stack}}',
-              queueUrl: `{{$.meta.queues.${maxQueueName}}}`,
-              granuleIngestMessageTemplateUri: `{{$.meta.templates.${waitPassSfName}}}`,
-              provider: '{{$.meta.provider}}',
-              internalBucket: '{{$.meta.buckets.internal.name}}'
-            }
-          },
-          payload: {
-            granules: granules
-          }
-        })
-      }).promise();
-
-      const { payload } = JSON.parse(Payload);
-      expect(payload.running.length).toEqual(numGranuleMessages);
     });
 
     it('consumes the right amount of messages', async () => {
