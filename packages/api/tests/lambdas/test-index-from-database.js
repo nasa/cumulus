5ddef122a49163644e4201218d5faadcee162d7a
Lauren Frederick <lpfreder@mbp-lpfreder.edn.ecs.nasa.gov>
2019-06-20 13:59:51 -0400

    Implementing indexing from

--- packages/api/tests/lambdas/test-index-from-database.js
+++ packages/api/tests/lambdas/test-index-from-database.js
@@ -7,7 +7,9 @@ const indexFromDatabase = require('../../lambdas/index-from-database');
 
 const models = require('../../models');
 const {
-  fakeExecutionFactory
+  fakeCollectionFactoryV2,
+  fakeGranuleFactoryV2,
+  fakeExecutionFactoryV2
 } = require('../../lib/testUtils');
 const bootstrap = require('../../lambdas/bootstrap');
 const indexer = require('../../es/indexer');
@@ -17,7 +19,31 @@ const { Search } = require('../../es/search');
 let esClient;
 let esIndex;
 const fakeExecutions = [];
+
 process.env.ExecutionsTable = randomString();
+process.env.CollectionsTable = randomString();
+
+const executionModel = new models.Execution();
+const collectionModel = new models.Collection();
+
+async function addFakeData(numItems, factory, model) {
+  const items = [];
+
+  /* eslint-disable no-await-in-loop */
+  for (let i = 0; i < numItems; i += 1) {
+    const item = factory();
+    items.push(item);
+    await model.create(item);
+  }
+  /* eslint-enable no-await-in-loop */
+
+  return items;
+}
+
+function searchEs(type) {
+  const executionQuery = new Search({}, type, esIndex);
+  return executionQuery.query();
+}
 
 test.before(async () => {
   esIndex = randomString();
@@ -26,16 +52,35 @@ test.before(async () => {
 
   // add fake elasticsearch index
   await bootstrap.bootstrapElasticSearch('fakehost', esIndex);
+
+  await executionModel.createTable();
+  await collectionModel.createTable();
+});
+
+test.after.always(async () => {
+  await esClient.indices.delete({ index: esIndex });
+
+  await executionModel.deleteTable();
 });
 
 test('index executions', async (t) => {
-  const executionModel = new models.Execution();
-  await executionModel.createTable();
+  const numItems = 10;
 
-  fakeExecutions.push(fakeExecutionFactory('completed'));
-  fakeExecutions.push(fakeExecutionFactory('failed', 'workflow2'));
-  await Promise.all(fakeExecutions.map((i) => executionModel.create(i)
-    .then((record) => indexer.indexExecution(esClient, record, esIndex))));
+  const fakeData = await Promise.all([
+    addFakeData(numItems, fakeExecutionFactoryV2, executionModel)
+  ]);
 
-  await indexFromDatabase.testIndex();
+  await indexFromDatabase.indexFromDatabase(esIndex);
+
+  const searchResults = await Promise.all([
+    searchEs('execution')
+  ]);
+
+  searchResults.map((res) => t.is(res.meta.count, numItems));
+
+  searchResults.map((res, index) =>
+    t.deepEqual(
+      res.results.map((r) => delete r.timestamp),
+      fakeData[index].map((r) => delete r.timestamp)
+    ));
 });
