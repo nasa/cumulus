8c687994e5f52d93dba227c20b6b9e4a9ca17c9a
Jenny Liu <jenny.h.liu@nasa.gov>
2019-07-03 16:38:26 -0400

    check ingest report fields not empty

--- example/spec/parallel/emsReport/emsIngestDistributionReportSpec.js
+++ example/spec/parallel/emsReport/emsIngestDistributionReportSpec.js
@@ -247,6 +247,8 @@ describe('The EMS report', () => {
           const records = reportRecords.filter((record) =>
             record.startsWith(ingestedGranuleIds[0]) || record.startsWith(ingestedGranuleIds[1]));
           expect(records.length).toEqual(2);
+          records.forEach((record) =>
+            expect(record.split('|&|').find((field) => field.length === 0)).toBeFalsy());
         }
         if (report.reportType === 'delete') {
           const records = reportRecords.filter((record) =>
