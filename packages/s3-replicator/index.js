9f10d704b47b88f7d0e79cb78d4fa8ffc1110d45
Menno Van Diermen <mvandiermen@contractor.usgs.gov>
2019-07-03 13:14:34 -0500

    CUMULUS-1337: lint replicator

--- packages/s3-replicator/index.js
+++ packages/s3-replicator/index.js
@@ -2,22 +2,22 @@
 const AWS = require('aws-sdk');
 const path = require('path');
 
-async function handler(event, context) {
-    const s3 = new AWS.S3();
-    // assuming one record per event
-    const eventType = event.Records[0].eventName;
-    if (!eventType.startsWith('ObjectCreated:')) return null;
-    
-    const srcBucket = event.Records[0].s3.bucket.name;
-    const srcKey = event.Records[0].s3.object.key;
-    return s3.copyObject({
-        CopySource: `${srcBucket}/${srcKey}`,
-        Bucket: process.env.TARGET_BUCKET,
-        Key: `${process.env.TARGET_PREFIX}/${path.basename(srcKey)}`,
-        ACL: 'bucket-owner-full-control'
-    }).promise();
+async function handler(event, _context) {
+  const s3 = new AWS.S3();
+  // assuming one record per event
+  const eventType = event.Records[0].eventName;
+  if (!eventType.startsWith('ObjectCreated:')) return null;
+
+  const srcBucket = event.Records[0].s3.bucket.name;
+  const srcKey = event.Records[0].s3.object.key;
+  return s3.copyObject({
+    CopySource: `${srcBucket}/${srcKey}`,
+    Bucket: process.env.TARGET_BUCKET,
+    Key: `${process.env.TARGET_PREFIX}/${path.basename(srcKey)}`,
+    ACL: 'bucket-owner-full-control'
+  }).promise();
 }
 
 module.exports = {
-    handler
-}
+  handler
+};
