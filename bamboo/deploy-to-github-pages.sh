da5111767cf0fb8d211630cbb4be97752bc4673d
Jonathan Kovarik <kovarik@nsidc.org>
2019-07-18 16:24:41 -0600

    Add bamboo from master, remove travis-ci for backport

--- bamboo/deploy-to-github-pages.sh
+++ bamboo/deploy-to-github-pages.sh
@@ -1,18 +1,16 @@
-#!/bin/sh
+#!/bin/bash
+set -ex
+. ./bamboo/set-bamboo-env-variables.sh
+. ./bamboo/abort-if-not-publish.sh
 
-set -e
-
-rm -rf website/build
-npm run docs-install
-npm run docs-build
 
 git clone --depth=50 --branch=gh-pages "https://${GITHUB_USER}:${GITHUB_TOKEN}@github.com/nasa/cumulus.git" gh-pages
 (
-  set -e
+  set -ex
   cd gh-pages
   rm -rf *
   rsync -av ../website/build/Cumulus/ .
   git add .
-  git commit -m "Automated build in Travis CI"
+  git commit -m "Automated build in Bamboo CI"
   git push
 )
