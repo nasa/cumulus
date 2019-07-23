6db69ad163170d28408f82db749ad841ddc4d74f
Mark Boyd <markdavidboyd@gmail.com>
2019-07-23 15:39:29 -0400

    refactor report executions lambda code

--- packages/report-executions/index.js
+++ packages/report-executions/index.js
@@ -1,34 +1,66 @@
-const path = require('path');
-const AWS = require('aws-sdk');
 const get = require('lodash.get');
+const has = require('lodash.has');
 
-const { Execution } = require('packages/api/models');
+const { Execution } = require('@cumulus/api/models');
 
-async function handlePayload(event) {
-  // const source = get(event, 'EventSource');
-  // if (source !== 'aws:sns') {
-  //   return;
-  // }
+/**
+ * Get message from SNS record.
+ *
+ * @param {Object} record - Record from SNS event
+ * @returns {Object} - Message object from SNS record
+ */
+const getSnsMessage = (record) => JSON.parse(get(record, 'Sns.Message', '{}'));
 
-  const snsMessage = get(event, 'Sns.Message');
-  const payload = JSON.parse(snsMessage);
+/**
+ * Determine if SNS message is an execution message.
+ *
+ * @param {Object} message - Message from SNS record
+ * @returns {boolean} - true if message is an execution message
+ */
+const isExecutionMessage = (message) => has(message, 'meta.status');
 
-  let executionPromise;
+/**
+ * Create/update execution record from SNS message.
+ *
+ * @param {Object} message - An execution message
+ * @returns {Promise} - Promise from execution create/update record operation
+ */
+async function handleExecutionMessage(message) {
   const executionModel = new Execution();
-  if (['failed', 'completed'].includes(payload.meta.status)) {
-    executionPromise = executionModel.updatexecutionModelExecutionFromSns(payload);
-  } else {
-    executionPromise = executionModel.createExecutionFromSns(payload);
+
+  if (['failed', 'completed'].includes(message.meta.status)) {
+    return executionModel.updateExecutionFromSns(message);
   }
 
-  return executionPromise;
+  return executionModel.createExecutionFromSns(message);
 }
 
+/**
+ * Filter and map SNS records to get report execution tasks.
+ *
+ * @param {Object} event - Incoming event from SNS
+ * @returns {Array<Promise>} - Array of promises for report execution tasks.
+ */
+function getReportExecutionTasks(event) {
+  const records = get(event, 'Records', []);
+  return records
+    .map(getSnsMessage)
+    .filter(isExecutionMessage)
+    .map(handleExecutionMessage);
+}
+
+/**
+ * Lambda handler for reportExecutions Lambda
+ *
+ * @param {Object} event - Incoming event from SNS
+ * @returns {Promise}
+ */
 async function handler(event) {
-  const records = get(event, 'Records');
-  return Promise.all(records.map(handlePayload));
+  const tasks = getReportExecutionTasks(event);
+  return Promise.all(tasks);
 }
 
 module.exports = {
+  getReportExecutionTasks,
   handler
 };
--- packages/report-executions/test/test-index.js
+++ packages/report-executions/test/test-index.js
@@ -1,4 +1,48 @@
 const test = require('ava');
 
-const { handler } = require('..');
+const { getReportExecutionTasks } = require('..');
 
+const createExecutionSnsMessage = (status) => ({
+  Sns: {
+    Message: JSON.stringify({
+      meta: {
+        status
+      }
+    })
+  }
+});
+
+test('getReportExecutionTasks returns no tasks for non-execution messages', (t) => {
+  let messages = getReportExecutionTasks([{}]);
+  t.is(messages.length, 0);
+
+  messages = getReportExecutionTasks([{
+    Sns: {}
+  }]);
+  t.is(messages.length, 0);
+
+  messages = getReportExecutionTasks([{
+    Sns: {
+      Message: 'message'
+    }
+  }]);
+  t.is(messages.length, 0);
+
+  messages = getReportExecutionTasks([{
+    Sns: {
+      Message: JSON.stringify({
+        meta: {}
+      })
+    }
+  }]);
+  t.is(messages.length, 0);
+});
+
+test('getReportExecutionTasks returns correct number of tasks', (t) => {
+  const tasks = getReportExecutionTasks([
+    createExecutionSnsMessage('completed'),
+    createExecutionSnsMessage('failed'),
+    { }
+  ]);
+  t.is(tasks.length, 2);
+});
