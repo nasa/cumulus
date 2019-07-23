e11213ea2a99df45d43a5166be654faff1bffed1
Mark Boyd <markdavidboyd@gmail.com>
2019-07-23 16:28:45 -0400

    fix unit tests

--- packages/report-executions/test/test-index.js
+++ packages/report-executions/test/test-index.js
@@ -5,6 +5,10 @@ const { getReportExecutionTasks } = require('..');
 const createExecutionSnsMessage = (status) => ({
   Sns: {
     Message: JSON.stringify({
+      cumulus_meta: {
+        state_machine: 'fake-state-machine',
+        execution_name: 'fake-execution-name'
+      },
       meta: {
         status
       }
@@ -12,37 +16,49 @@ const createExecutionSnsMessage = (status) => ({
   }
 });
 
+test.before(async () => {
+  process.env.ExecutionsTable = 'test-executionsTable';
+});
+
 test('getReportExecutionTasks returns no tasks for non-execution messages', (t) => {
   let messages = getReportExecutionTasks([{}]);
   t.is(messages.length, 0);
 
   messages = getReportExecutionTasks([{
-    Sns: {}
+    Records: [{
+      Sns: {}
+    }]
   }]);
   t.is(messages.length, 0);
 
   messages = getReportExecutionTasks([{
-    Sns: {
-      Message: 'message'
-    }
+    Records: [{
+      Sns: {
+        Message: 'message'
+      }
+    }]
   }]);
   t.is(messages.length, 0);
 
   messages = getReportExecutionTasks([{
-    Sns: {
-      Message: JSON.stringify({
-        meta: {}
-      })
-    }
+    Records: [{
+      Sns: {
+        Message: JSON.stringify({
+          meta: {}
+        })
+      }
+    }]
   }]);
   t.is(messages.length, 0);
 });
 
 test('getReportExecutionTasks returns correct number of tasks', (t) => {
-  const tasks = getReportExecutionTasks([
-    createExecutionSnsMessage('completed'),
-    createExecutionSnsMessage('failed'),
-    { }
-  ]);
+  const tasks = getReportExecutionTasks({
+    Records: [
+      createExecutionSnsMessage('completed'),
+      createExecutionSnsMessage('failed'),
+      { }
+    ]
+  });
   t.is(tasks.length, 2);
 });
