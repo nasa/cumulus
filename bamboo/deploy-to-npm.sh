7d8369749e892e114f03c0b8ee2cd255e67d3544
Jonathan Kovarik <kovarik@nsidc.org>
2019-07-23 15:54:59 -0600

    Temp disable publish to get bamboo going.   [skip-audit]

--- bamboo/deploy-to-npm.sh
+++ bamboo/deploy-to-npm.sh
@@ -6,13 +6,14 @@ set -ex
 export VERSION=$(jq --raw-output .version lerna.json)
 export NPM_TAG=$(node ./bamboo/npm-tag.js);
 
-echo "Publishing packages to NPM with version=${VERSION} and tag=${NPM_TAG}"
-npx lerna publish \
-  ${VERSION} \
-  --no-git-tag-version \
-  --no-push \
-  --yes \
-  --force-publish=* \
-  --dist-tag=${NPM_TAG} \
-  --exact
+echo "Disabled because publish already occurred"
+# echo "Publishing packages to NPM with version=${VERSION} and tag=${NPM_TAG}"
+# npx lerna publish \
+#   ${VERSION} \
+#   --no-git-tag-version \
+#   --no-push \
+#   --yes \
+#   --force-publish=* \
+#   --dist-tag=${NPM_TAG} \
+#   --exact
 
