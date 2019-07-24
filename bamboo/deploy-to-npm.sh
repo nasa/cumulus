9ba560b117b36a4cadb50729bdc6e848553de383
Jonathan Kovarik <kovarik@nsidc.org>
2019-07-24 17:32:37 -0600

    Fix publish container perms issue

--- bamboo/deploy-to-npm.sh
+++ bamboo/deploy-to-npm.sh
@@ -7,7 +7,7 @@ export VERSION=$(jq --raw-output .version lerna.json)
 export NPM_TAG=$(node ./bamboo/npm-tag.js);
 
 echo "Publishing packages to NPM with version=${VERSION} and tag=${NPM_TAG}"
-lerna publish \
+npm_config_unsafe_perm=true; npx lerna publish \
   ${VERSION} \
   --no-git-tag-version \
   --no-push \
