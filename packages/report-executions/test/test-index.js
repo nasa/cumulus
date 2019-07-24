ad6e7b7c320f3a2ff1d37eb2f11796bc8fdf3431
Mark Boyd <markdavidboyd@gmail.com>
2019-07-24 15:08:36 -0400

    add more unit tests

--- packages/report-executions/test/test-index.js
+++ packages/report-executions/test/test-index.js
@@ -2,39 +2,57 @@
 
 const test = require('ava');
 
-const { randomId } = require('@cumulus/common/test-utils');
 const { Execution } = require('@cumulus/api/models');
+const { getExecutionArn } = require('@cumulus/common/aws');
+const { randomId, randomString } = require('@cumulus/common/test-utils');
+const { RecordDoesNotExist } = require('@cumulus/common/errors');
 
 const { getReportExecutionMessages, handler } = require('..');
+
+const createExecutionMessage = ({
+  status,
+  stateMachine,
+  executionName,
+  startTime
+}) => ({
+  cumulus_meta: {
+    state_machine: stateMachine,
+    execution_name: executionName,
+    workflow_start_time: startTime
+  },
+  meta: {
+    status
+  }
+});
 
 const createExecutionSnsMessage = ({
   status,
   stateMachine,
-  executionName
+  executionName,
+  startTime
 }) => ({
   Sns: {
-    Message: JSON.stringify({
-      cumulus_meta: {
-        state_machine: stateMachine,
-        execution_name: executionName
-      },
-      meta: {
-        status
-      }
-    })
+    Message: JSON.stringify(
+      createExecutionMessage({
+        status,
+        stateMachine,
+        executionName,
+        startTime
+      })
+    )
   }
 });
 
-let executionModel;
+let executionsModel;
 
 test.before(async () => {
   process.env.ExecutionsTable = randomId('executionsTable');
-  executionModel = new Execution();
-  await executionModel.createTable();
+  executionsModel = new Execution();
+  await executionsModel.createTable();
 });
 
 test.after.always(async () => {
-  await executionModel.deleteTable();
+  await executionsModel.deleteTable();
 });
 
 test('getReportExecutionMessages returns no tasks for non-execution messages', (t) => {
@@ -91,22 +109,120 @@ test('getReportExecutionMessages returns correct number of tasks', (t) => {
   t.is(tasks.length, 2);
 });
 
-test.skip('handler correctly creates execution record', async (t) => {
-  const stateMachine = 'stateMachine';
-  const executionName = 'execution1';
-  const arn = `${stateMachine}:${executionName}`;
-
-  const response = await executionsModel.exists({ arn });
-  debugger;
+test('handler correctly creates execution record', async (t) => {
+  const stateMachine = randomId('stateMachine');
+  const executionName = randomString();
+  const arn = getExecutionArn(stateMachine, executionName);
 
   await handler({
     Records: [
       createExecutionSnsMessage({
         stateMachine,
-        executionName
+        executionName,
+        status: 'running'
       })
     ]
   });
 
   t.true(await executionsModel.exists({ arn }));
+});
+
+test('handler throws error for updated to non-existent execution', async (t) => {
+  const stateMachine = randomId('stateMachine');
+  const executionName = randomString();
+
+  await t.throwsAsync(
+    () => handler({
+      Records: [
+        createExecutionSnsMessage({
+          stateMachine,
+          executionName,
+          status: 'completed'
+        })
+      ]
+    }),
+    { instanceOf: RecordDoesNotExist }
+  );
+});
+
+test('handler correctly updates completed execution record', async (t) => {
+  const stateMachine = randomId('stateMachine');
+  const executionName = randomString();
+  const arn = getExecutionArn(stateMachine, executionName);
+
+  const startTime = Date.now();
+  const status = 'completed';
+
+  await executionsModel.createExecutionFromSns(
+    createExecutionMessage({
+      stateMachine,
+      executionName,
+      status: 'running',
+      startTime
+    })
+  );
+  const originalExecution = await executionsModel.get({ arn });
+
+  await handler({
+    Records: [
+      createExecutionSnsMessage({
+        stateMachine,
+        executionName,
+        startTime,
+        status
+      })
+    ]
+  });
+
+  const updatedExecution = await executionsModel.get({ arn });
+  const expectedResponse = {
+    ...originalExecution,
+    status,
+    duration: updatedExecution.duration,
+    timestamp: updatedExecution.timestamp,
+    updatedAt: updatedExecution.updatedAt
+  };
+
+  t.deepEqual(updatedExecution, expectedResponse);
+});
+
+test('handler correctly updates failed execution record', async (t) => {
+  const stateMachine = randomId('stateMachine');
+  const executionName = randomString();
+  const arn = getExecutionArn(stateMachine, executionName);
+
+  const startTime = Date.now();
+  const status = 'failed';
+
+  await executionsModel.createExecutionFromSns(
+    createExecutionMessage({
+      stateMachine,
+      executionName,
+      status: 'running',
+      startTime
+    })
+  );
+  const originalExecution = await executionsModel.get({ arn });
+
+  await handler({
+    Records: [
+      createExecutionSnsMessage({
+        stateMachine,
+        executionName,
+        startTime,
+        status
+      })
+    ]
+  });
+
+  const updatedExecution = await executionsModel.get({ arn });
+  const expectedResponse = {
+    ...originalExecution,
+    status,
+    duration: updatedExecution.duration,
+    timestamp: updatedExecution.timestamp,
+    updatedAt: updatedExecution.updatedAt
+  };
+
+  t.deepEqual(updatedExecution, expectedResponse);
 });
