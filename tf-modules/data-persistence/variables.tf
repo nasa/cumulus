0b82d0ab05aba3ef0714a7c34b2e5defaf226a29
Mark Boyd <markdavidboyd@gmail.com>
2019-08-01 16:14:26 -0400

    add ES access policy

--- tf-modules/data-persistence/variables.tf
+++ tf-modules/data-persistence/variables.tf
@@ -6,15 +6,20 @@ variable "prefix" {
 
 # Optional
 
+variable "include_elasticsearch" {
+  type    = bool
+  default = true
+}
+
 variable "elasticsearch_config" {
-  type    = object({
+  type = object({
     domain_name = string
     instance_type = string
     version = string
     volume_size = number
   })
   default = {
-    domain_name = null
+    domain_name = "es"
     instance_type = "t2.small.elasticsearch"
     version = "5.3"
     volume_size = 5
@@ -33,6 +38,11 @@ variable "enable_point_in_time_tables" {
     "RulesTable",
     "UsersTable"
   ]
+}
+
+variable "es_role_arns" {
+  type    = list(string)
+  default = []
 }
 
 variable "subnet_ids" {
