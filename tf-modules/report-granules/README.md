039554b2c4844d682a31832b7b7b07fa8c9e0ce2
Menno Van Diermen <mvandiermen@contractor.usgs.gov>
2019-08-05 16:10:58 -0500

    report-granules documentation

---
+++ tf-modules/report-granules/README.md
@@ -0,0 +1,26 @@
+# Report Executions
+
+This package includes a Lambda function to process granule ingest notifications received via SNS and store it to a database.
+
+## Deployment
+
+1. Copy the .tfvars sample file: `cp terraform.tfvars.sample terraform.tfvars`
+2. Populate the sample file with values that apply to your AWS environment (see configuration variables section below).
+3. Deploy this module: `terraform apply`
+
+NOTE: Terraform will ignore the `aws_profile` config variable if you have static credentials or environment variables set, see the [AWS Provider page](https://www.terraform.io/docs/providers/aws/index.html#authentication).
+
+## Configuration
+
+Configuration variables are shown in `terraform.tfvars.sample`, and are explained below:
+
+```text
+# Required
+granules_table        = "ExecutionsTableName"  # name of DynamoDB table to store granules data
+prefix                = "myprefix"             # prefix to use for naming created resources
+
+# Optional
+permissions_boundary  = "arn:aws:iam::1234567890:policy/YourRoleBoundary" # IAM permissions boundary
+security_groups       = ["sg-123456"]          # Security Group IDs (for Lambda)
+subnet_ids            = ["subnet-123456"]      # Subnet IDs (for Lambda)
+```
--- tf-modules/report-granules/index.js
+++ tf-modules/report-granules/index.js
@@ -9,15 +9,34 @@ const {
 } = require('@cumulus/common/sns-event');
 const Granule = require('@cumulus/api/models/granules');
 
+/**
+ * Check if message contains granules.
+ * Checks locations expected by sns2elasticsearch
+ *
+ * @param {Object} message - SNS Cumulus message object
+ * @returns {boolean} whether the message contains granules
+ */
 function containsGranules(message) {
   return (has(message, 'payload.granules') || has(message, 'meta.input_granules'));
 }
 
+/**
+ * Process Cumulus message object and create granule database records.
+ *
+ * @param {*} message - SNS Cumulus message object
+ * @returns {Promise<Array>} granule records
+ */
 async function handleGranuleMessage(message) {
   const granuleModel = new Granule();
   return granuleModel.createGranulesFromSns(message);
 }
 
+/**
+ * Return valid Cumulus SNS messages containing granule ingest notifications.
+ *
+ * @param {Object} event - SNS Notification Event
+ * @returns {Array<Object>} granule ingest notification Cumulus messages
+ */
 function getReportGranuleMessages(event) {
   const records = get(event, 'Records', []);
   return records
@@ -26,6 +45,12 @@ function getReportGranuleMessages(event) {
     .filter(containsGranules);
 }
 
+/**
+ * Lambda handler for report-granules Lambda.
+ *
+ * @param {*} event - SNS Notification Event
+ * @returns {Promise<Array>} granule records
+ */
 async function handler(event) {
   const messages = getReportGranuleMessages(event);
   return Promise.all(
