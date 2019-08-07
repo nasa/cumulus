61cc369998f09f86f3efed8f8bb47eeb4adb6072
Jenny Liu <jenny.h.liu@nasa.gov>
2019-08-07 18:34:45 -0400

    update config

--- example/spec/parallel/createReconciliationReport/CreateReconciliationReportSpec.js
+++ example/spec/parallel/createReconciliationReport/CreateReconciliationReportSpec.js
@@ -14,8 +14,7 @@ const {
   bucketsConfigJsonObject,
   constructCollectionId,
   testUtils: {
-    randomString,
-    randomStringFromRegex
+    randomString
   }
 } = require('@cumulus/common');
 
@@ -26,7 +25,6 @@ const {
   buildAndExecuteWorkflow,
   cleanupCollections,
   cleanupProviders,
-  generateCmrXml,
   granulesApi: granulesApiTestUtils,
   waitForConceptExistsOutcome,
   waitUntilGranuleStatusIs
@@ -347,7 +345,7 @@ describe('When there are granule differences and granule reconciliation is run',
       deleteFolder(config.bucket, testDataFolder),
       cleanupCollections(config.stackName, config.bucket, collectionsDir),
       cleanupProviders(config.stackName, config.bucket, providersDir, testSuffix),
-      granulesApiTestUtils.deleteGranule({ prefix: config.stackName, granuleId: dbGranuleId }),
+      granulesApiTestUtils.deleteGranule({ prefix: config.stackName, granuleId: dbGranuleId })
     ]);
 
     // need to add the cmr granule back to the table, so the granule can be removed from api
@@ -361,7 +359,6 @@ describe('When there are granule differences and granule reconciliation is run',
       granuleId: publishedGranuleId
     });
     await granulesApiTestUtils.removeFromCMR({ prefix: config.stackName, granuleId: publishedGranuleId });
-    console.log(publishedGranuleId);
     await waitForConceptExistsOutcome(JSON.parse(granuleResponse.body).cmrLink, false);
     await granulesApiTestUtils.deleteGranule({ prefix: config.stackName, granuleId: publishedGranuleId });
   });
