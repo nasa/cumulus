dfa47a1b17d4f81fdfed528198d6acabaf47b6cb
Jacob Campbell <ifestus@protonmail.com>
2019-08-10 00:27:03 -0500

    CUMULUS-1373 Dashboard reporting should work with TEA (#1135)
    * LogGroup|Subscription updates to distribution (api gateway and lambda). Also moved s3-replicator to tf-modules
    
    * tf variables for cw/lambda logging and s3 replication
    
    * update example variables to include new logging switches
    
    * update example variables to work with s3_replicator
    
    * update lint command in package.json to look for s3-replicator in tf-modules
    
    * re-activate s3access tests
    
    * s3 replicator accepts security groups now, defaults to it's own.
    
    * remove destination arn from examples
    
    * Update example/variables.tf
    
    Co-Authored-By: Matt Savoie <savoie@nsidc.org>
    
    * move s3-replicator into distribution module and update variables/descriptions
    
    * Remove log_to_shared_destination bool
    
    Use null or value in log_destination_arn instead.
    
    * update description to variables in distribution and example
    
    * s3-replicator variables into a single object variable. Update tea source to build 23. Minor qol changes to conditional resources.
    
    * upgrade to build 25 of thin egress app
    
    * disable teaS3AccessSpec because terraform config currently isnt being deployed during integration tests
    
    * move s3-replicator from distribution to a top level module. Added a build script into the s3-replicator directory for building the tf package.
    
    * update release documentation with information about s3-replicator publishing
    
    * add the map template to distribution build
    
    * update explanation of example tfvars
    
    * Adds ByLogStream to subscriptionFilters
    
    I think terraform has a bug where if you don't specify a `distribution`
    attribute, it defaults to "ByLogStream", and then forgets so that a second
    terraform apply shows you a change from "ByLogStream" => null,
    but that apply doesn't do it. so you always have a change with terraform apply
    and it never stablizes even with no changes.
    
    * Use name_prefix instead of name for secretsmanager
    
    Secrets last 7 days after you delete them, so you can easily get in a case
    where you can't create a secret because you had one less than 7 days ago.
    This just adds a random suffix to the initial creation and prevents duplicate
    secret names.
    
    * terraform fmt all the terraform files

--- example/main.tf
+++ example/main.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  region = var.region
+  region  = var.region
   profile = var.aws_profile
 }
 
@@ -13,6 +13,10 @@ module "distribution" {
 
   distribution_url = var.distribution_url
 
+  # Additional Logging Settings
+  log_api_gateway_to_cloudwatch = var.log_api_gateway_to_cloudwatch
+  log_destination_arn           = var.log_destination_arn
+
   protected_buckets = var.protected_buckets
   public_buckets    = var.public_buckets
 
@@ -22,4 +26,19 @@ module "distribution" {
 
   vpc_id     = var.vpc_id
   subnet_ids = var.subnet_ids
+}
+
+module "s3-replicator" {
+  source = "../tf-modules/s3-replicator"
+
+  prefix               = var.prefix
+  permissions_boundary = var.permissions_boundary_arn
+
+  vpc_id     = var.vpc_id
+  subnet_ids = var.subnet_ids
+
+  source_bucket = var.s3_replicator_config.source_bucket
+  source_prefix = var.s3_replicator_config.source_prefix
+  target_bucket = var.s3_replicator_config.target_bucket
+  target_prefix = var.s3_replicator_config.target_prefix
 }
