6ff7303f35ddb093bce8ed4463417ea96669d566
Lauren Frederick <lpfreder@mbp-lpfreder.edn.ecs.nasa.gov>
2019-08-29 17:56:53 -0400

    Remove deploy to npm step [skip-audit]

--- bamboo/deploy-to-npm.sh
+++ bamboo/deploy-to-npm.sh
@@ -3,17 +3,17 @@ set -ex
 . ./bamboo/set-bamboo-env-variables.sh
 . ./bamboo/abort-if-not-publish.sh
 
-export VERSION=$(jq --raw-output .version lerna.json)
-export NPM_TAG=$(node ./bamboo/npm-tag.js);
+# export VERSION=$(jq --raw-output .version lerna.json)
+# export NPM_TAG=$(node ./bamboo/npm-tag.js);
 
-echo "Publishing packages to NPM with version=${VERSION} and tag=${NPM_TAG}"
-export npm_config_unsafe_perm=true
-npx lerna publish \
-  ${VERSION} \
-  --no-git-tag-version \
-  --no-push \
-  --yes \
-  --force-publish=* \
-  --dist-tag=${NPM_TAG} \
-  --exact
+# echo "Publishing packages to NPM with version=${VERSION} and tag=${NPM_TAG}"
+# export npm_config_unsafe_perm=true
+# npx lerna publish \
+#   ${VERSION} \
+#   --no-git-tag-version \
+#   --no-push \
+#   --yes \
+#   --force-publish=* \
+#   --dist-tag=${NPM_TAG} \
+#   --exact
 
