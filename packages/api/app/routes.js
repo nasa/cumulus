89f0d2e0df94055db412ce9fc72783aa2ed5d8ef
Katherine Kelly <katherine.a.kelly@nasa.gov>
2019-09-03 13:34:10 -0400

    adding saml stuff

--- packages/api/app/routes.js
+++ packages/api/app/routes.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const router = require('express-promise-router')();
+const saml2 = require('saml2-js');
 
 const log = require('@cumulus/common/log');
 
@@ -26,6 +27,35 @@ const elasticsearch = require('../endpoints/elasticsearch');
 const ems = require('../endpoints/ems');
 const launchpadAuth = require('./launchpadAuth');
 
+// set up SP and IdP
+const sp_options = {
+  entity_id: 'https://sp.example.com/metadata.xml', // ??
+  private_key: fs.readFileSync('/Users/kakelly/Documents/Projects/serverkey.pem').toString(),
+  certificate: fs.readFileSync('/Users/kakelly/Documents/Projects/cert-file.crt').toString(),
+  assert_endpoint: 'https://cumulus-sandbox.earthdata.nasa.gov/saml/sso', // change to just /assert
+  force_authn: true,
+  // auth_context: { comparison: "exact", class_refs: ["urn:oasis:names:tc:SAML:1.0:am:password"] },
+  // nameid_format: "urn:oasis:names:tc:SAML:2.0:nameid-format:transient",
+  sign_get_request: false,
+  allow_unencrypted_assertion: true
+}
+
+// Call service provider constructor with options
+const sp = new saml2.ServiceProvider(sp_options);
+
+// Example use of service provider.
+// Call metadata to get XML metatadata used in configuration.
+const metadata = sp.create_metadata();
+
+const idp_options = {
+  sso_login_url: 'https://auth.launchpad-sbx.nasa.gov/affwebservices/public/saml2sso',
+  sso_logout_url: null,
+  certificates: [fs.readFileSync('/Users/kakelly/Documents/Projects/launchpad-sbx.crt').toString()]
+};
+const idp = new saml2.IdentityProvider(idp_options);
+
+const request_id = '12345';
+
 let token = require('../endpoints/token');
 let { ensureAuthorized } = require('./auth');
 if (process.env.FAKE_AUTH === 'true') {
@@ -36,6 +66,35 @@ if (process.env.FAKE_AUTH === 'true') {
 if (process.env.OAUTH_PROVIDER === 'launchpad') {
   ensureAuthorized = launchpadAuth.ensureAuthorized;
 }
+
+// Starting point for login
+router.get("/samlLogin", function(req, res) {
+  sp.create_login_request_url(idp, {}, function(err, login_url, request_id) {
+    if (err != null)
+      return res.send(500);
+    res.redirect(login_url);
+  });
+});
+
+// Assert endpoint for when login completes
+router.post("/saml/sso", function(req, res) { // /assert
+  var options = {request_body: req.body};
+  sp.post_assert(idp, options, function(err, saml_response) {
+    if (err != null)
+      return res.send(500);
+ 
+    // Save name_id and session_index for logout
+    // Note:  In practice these should be saved in the user session, not globally.
+    name_id = saml_response.user.name_id;
+    session_index = saml_response.user.session_index;
+
+    // json web token 1 store with the user name , for some amount of time , 1 hour. 
+    // jwt to dashboard :
+    // ^ jwt 
+ 
+    res.send("Hello #{saml_response.user.name_id}!");
+  });
+});
 
 // collections endpoints
 router.use('/collections', ensureAuthorized, collections);
