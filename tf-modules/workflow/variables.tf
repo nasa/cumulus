ab8e29b5ffb89c161b4cbe0c5f7b99bdbb902758
Marc Huffnagle <marc@element84.com>
2019-09-09 12:50:48 -0400

    Create workflow module

---
+++ tf-modules/workflow/variables.tf
@@ -0,0 +1,32 @@
+variable "name" {
+  type = string
+}
+
+variable "prefix" {
+  type = string
+}
+
+variable "state_machine_definition" {
+  type = string
+}
+
+variable "state_machine_role_arn" {
+  type = string
+}
+
+variable "sf_semaphore_down_lambda_function_arn" {
+  type = string
+}
+
+variable "sftracker_sns_topic_arn" {
+  type = string
+}
+
+variable "tags" {
+  type    = map(string)
+  default = null
+}
+
+variable "workflow_config" {
+  type = string
+}
