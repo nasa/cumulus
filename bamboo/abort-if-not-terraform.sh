47417a3ee9e00d75966317a712094067dfc20d11
Menno Van Diermen <mvandiermen@contractor.usgs.gov>
2019-09-12 09:01:55 -0500

    CUMULUS-1408 deduplicate bamboo script code

--- bamboo/abort-if-not-terraform.sh
+++ bamboo/abort-if-not-terraform.sh
@@ -2,7 +2,7 @@
 set -e
 
 #source .bamboo_env_vars || true
-if ! [[ $COMMIT_MESSAGE =~ terraform ]]; then
+if [[ $COMMIT_MESSAGE =~ skip-terraform ]]; then
   >&2 echo "******Skipping Terraform integration tests based on commit message: $COMMIT_MESSAGE"
   exit 0
 fi
