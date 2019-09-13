cf090396aaf280f03f74a7d56e13f4d777bd7067
Menno Van Diermen <mvandiermen@contractor.usgs.gov>
2019-09-13 09:43:37 -0500

    CUMULUS-1408 tf test setup

--- bamboo/integration-tests.sh
+++ bamboo/integration-tests.sh
@@ -4,4 +4,10 @@ set -ex
 . ./bamboo/abort-if-skip-integration-tests.sh
 . ./bamboo/set-bamboo-env-variables.sh
 
-cd example && npm test
+if [[ $DEPLOYMENT =~ '-tf' ]]; then
+  echo "Running migrated terraform-compatible tests."
+  . ./bamboo/integration-tests-terraform.sh
+else
+  echo "Running kes-compatible full test suite."
+  cd example && npm test
+fi
