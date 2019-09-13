07a967b3f10776258237dfbaf368f8bb72fe527f
Menno Van Diermen <mvandiermen@contractor.usgs.gov>
2019-09-13 09:38:55 -0500

    CUMULUS-1408 final PR feedback

--- bamboo/terraform-integration-tests.sh
+++ bamboo/terraform-integration-tests.sh
@@ -2,6 +2,7 @@
 set -ex
 . ./bamboo/abort-if-not-pr-or-redeployment.sh
 . ./bamboo/abort-if-skip-integration-tests.sh
+. ./bamboo/abort-if-not-terraform.sh
 . ./bamboo/set-bamboo-env-variables.sh
 
 # Eventual goal
