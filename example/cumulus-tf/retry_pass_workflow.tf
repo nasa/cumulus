cd95726bbbba210c249506d4abf2e49fd9653685
Marc Huffnagle <marc@element84.com>
2019-09-16 16:05:15 -0400

    Got retry tests passing

---
+++ example/cumulus-tf/retry_pass_workflow.tf
@@ -0,0 +1,107 @@
+module "retry_pass_workflow" {
+  source = "../../tf-modules/workflow"
+
+  prefix                                = var.prefix
+  name                                  = "RetryPassWorkflow"
+  distribution_url                      = module.cumulus.distribution_url
+  state_machine_role_arn                = module.cumulus.step_role_arn
+  sf_semaphore_down_lambda_function_arn = module.cumulus.sf_semaphore_down_lambda_function_arn
+  sftracker_sns_topic_arn               = module.cumulus.sftracker_sns_topic_arn
+  tags                                  = local.default_tags
+
+  workflow_config = <<JSON
+{
+  "StartStatus": {
+    "cumulus_message": {
+      "input": "{{$}}"
+    }
+  },
+  "HelloWorld": {
+    "fail": true,
+    "passOnRetry": true,
+    "bucket": "{{$.meta.buckets.internal.name}}",
+    "execution": "{{$.cumulus_meta.execution_name}}"
+  },
+  "StopStatus": {
+    "sfnEnd": true,
+    "stack": "{{$.meta.stack}}",
+    "bucket": "{{$.meta.buckets.internal.name}}",
+    "stateMachine": "{{$.cumulus_meta.state_machine}}",
+    "executionName": "{{$.cumulus_meta.execution_name}}",
+    "cumulus_message": {
+      "input": "{{$}}"
+    }
+  },
+  "WorkflowFailed": {}
+}
+JSON
+
+  state_machine_definition = <<JSON
+{
+  "Comment": "Tests Retry Configurations",
+  "StartAt": "StartStatus",
+  "States": {
+    "StartStatus": {
+      "Type": "Task",
+      "Resource": "${module.cumulus.sf_sns_report_task_lambda_function_arn}",
+      "Retry": [
+        {
+          "ErrorEquals": [
+            "Lambda.ServiceException",
+            "Lambda.AWSLambdaException",
+            "Lambda.SdkClientException"
+          ],
+          "IntervalSeconds": 2,
+          "MaxAttempts": 6,
+          "BackoffRate": 2
+        }
+      ],
+      "Next": "HelloWorld"
+    },
+    "HelloWorld": {
+      "Type": "Task",
+      "Resource": "${module.cumulus.hello_world_task_lambda_function_arn}",
+      "Next": "StopStatus",
+      "Retry": [
+        {
+          "ErrorEquals": [
+            "States.ALL"
+          ],
+          "IntervalSeconds": 2,
+          "MaxAttempts": 3
+        }
+      ]
+    },
+    "StopStatus": {
+      "Type": "Task",
+      "Resource": "${module.cumulus.sf_sns_report_task_lambda_function_arn}",
+      "Retry": [
+        {
+          "ErrorEquals": [
+            "Lambda.ServiceException",
+            "Lambda.AWSLambdaException",
+            "Lambda.SdkClientException"
+          ],
+          "IntervalSeconds": 2,
+          "MaxAttempts": 6,
+          "BackoffRate": 2
+        }
+      ],
+      "Catch": [
+        {
+          "ErrorEquals": [
+            "States.ALL"
+          ],
+          "Next": "WorkflowFailed"
+        }
+      ],
+      "End": true
+    },
+    "WorkflowFailed": {
+      "Type": "Fail",
+      "Cause": "Workflow failed"
+    }
+  }
+}
+JSON
+}
