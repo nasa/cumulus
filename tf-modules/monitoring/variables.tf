20a8824a01bb45476c347de535b0768ec34f14d8
Jenny Liu <jenny.h.liu@nasa.gov>
2019-09-17 09:03:44 -0400

    add ecs alarms
    object list

--- tf-modules/monitoring/variables.tf
+++ tf-modules/monitoring/variables.tf
@@ -1,8 +1,17 @@
+# Required
 
 variable "prefix" {
   type = string
 }
 
+# Optional
+
 variable "elasticsearch_alarms" {
-  type = list
+  type = list(object({ name = string, arn = string }))
+  default = []
+}
+
+variable "ecs_service_alarms" {
+  type = list(object({ name = string, arn = string }))
+  default = []
 }
