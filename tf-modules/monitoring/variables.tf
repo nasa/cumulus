c1fe76d1462d3ad018afef12428142cad894a253
Jenny Liu <jenny.h.liu@nasa.gov>
2019-09-17 09:03:44 -0400

    add ecs alarms

--- tf-modules/monitoring/variables.tf
+++ tf-modules/monitoring/variables.tf
@@ -1,8 +1,17 @@
+# Required
 
 variable "prefix" {
   type = string
 }
 
+# Optional
+
 variable "elasticsearch_alarms" {
   type = list
+  default = []
+}
+
+variable "ecs_service_alarms" {
+  type = list
+  default = []
 }
