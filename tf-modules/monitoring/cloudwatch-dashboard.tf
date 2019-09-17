96c38e1aff140ddb3730a70878cc542af0d9ddd5
Jenny Liu <jenny.h.liu@nasa.gov>
2019-09-17 09:03:44 -0400

    add ecs alarms

--- tf-modules/monitoring/cloudwatch-dashboard.tf
+++ tf-modules/monitoring/cloudwatch-dashboard.tf
@@ -13,14 +13,46 @@ resource "aws_cloudwatch_dashboard" "cloudwatch_dashboard" {
         }
       },
       {
-          "type": "text",
-          "width": 24,
-          "height": 1,
-          "properties": {
-            "markdown": "## Alarms"
+        "type": "text",
+        "width": 24,
+        "height": 1,
+        "properties": {
+          "markdown": "## Alarms"
         }
       }
       %{ for alarm in var.elasticsearch_alarms }
+      ,
+      {
+        "type":"metric",
+        "width":6,
+        "height":3,
+        "properties": {
+          "title": "${alarm.name}",
+          "annotations": {
+            "alarms": ["${alarm.arn}"]
+          },
+          "view": "singleValue"
+        }
+      }
+      %{ endfor }
+      ,
+      {
+        "type": "text",
+        "width": 24,
+        "height": 1,
+        "properties": {
+          "markdown": "# Elastic Container Service"
+        }
+      },
+      {
+        "type": "text",
+        "width": 24,
+        "height": 1,
+        "properties": {
+          "markdown": "## Alarms"
+        }
+      }
+      %{ for alarm in var.ecs_service_alarms }
       ,
       {
         "type":"metric",
