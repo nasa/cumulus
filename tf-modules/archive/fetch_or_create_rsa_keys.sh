23ae5ab5870f172f4097c10f4b965bf6e5a4fa60
Menno <mvandiermen@contractor.usgs.gov>
2019-09-20 16:03:40 -0500

    CUMULUS-1408 deploy cumulus-tf in CI (#1188)
    * CUMULUS-1408 deploy cumulus-tf in CI
    
    * use NonProd creds to deploy TF

--- tf-modules/archive/fetch_or_create_rsa_keys.sh
+++ tf-modules/archive/fetch_or_create_rsa_keys.sh
@@ -2,6 +2,10 @@
 
 set -e
 
+apt-get update
+apt-get install -y python-pip
+pip install awscli
+
 BUCKET="$1"
 STACK="$2"
 
@@ -13,12 +17,17 @@ fi
 PRIVATE_KEY="${STACK}/crypto/private.pem"
 PUBLIC_KEY="${STACK}/crypto/public.pub"
 
+echo "Checking for ${STACK} private key"
 # Create private key if it doesn't exist
 if ! aws s3api head-object --bucket "$BUCKET" --key "$PRIVATE_KEY" >/dev/null 2>&1; then
+  echo "Uploading private key for ${STACK}"
   openssl genrsa -out /dev/stdout 2048 2>/dev/null | aws s3 cp - "s3://${BUCKET}/${PRIVATE_KEY}" >/dev/null 2>&1
 fi
 
+echo "Uploading public key for ${STACK}"
 # Create and upload public key to S3
-aws s3 cp "s3://${BUCKET}/${PRIVATE_KEY}" - 2>/dev/null |\
-  openssl rsa -in /dev/stdin -outform PEM -pubout -out /dev/stdout 2>&1 |\
-  aws s3 cp - "s3://${BUCKET}/${PUBLIC_KEY}" >/dev/null 2>&1
+if ! aws s3api head-object --bucket "$BUCKET" --key "$PRIVATE_KEY" >/dev/null 2>&1; then
+  aws s3 cp "s3://${BUCKET}/${PRIVATE_KEY}" - 2>/dev/null |\
+    openssl rsa -in /dev/stdin -outform PEM -pubout -out /dev/stdout 2>&1 |\
+    aws s3 cp - "s3://${BUCKET}/${PUBLIC_KEY}" >/dev/null 2>&1
+fi
