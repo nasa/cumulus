bf7e8bbe3931d454025f71cdad174de989e54556
Menno Van Diermen <mvandiermen@contractor.usgs.gov>
2019-09-20 12:21:56 -0500

    add echos to rsa key script

--- tf-modules/archive/fetch_or_create_rsa_keys.sh
+++ tf-modules/archive/fetch_or_create_rsa_keys.sh
@@ -13,11 +13,13 @@ fi
 PRIVATE_KEY="${STACK}/crypto/private.pem"
 PUBLIC_KEY="${STACK}/crypto/public.pub"
 
+echo "Uploading private key for ${STACK}"
 # Create private key if it doesn't exist
 if ! aws s3api head-object --bucket "$BUCKET" --key "$PRIVATE_KEY" >/dev/null 2>&1; then
   openssl genrsa -out /dev/stdout 2048 2>/dev/null | aws s3 cp - "s3://${BUCKET}/${PRIVATE_KEY}" >/dev/null 2>&1
 fi
 
+echo "Uploading public key for ${STACK}"
 # Create and upload public key to S3
 aws s3 cp "s3://${BUCKET}/${PRIVATE_KEY}" - 2>/dev/null |\
   openssl rsa -in /dev/stdin -outform PEM -pubout -out /dev/stdout 2>&1 |\
