2dd78a59193359e7502a64394411e0553d9858b2
Mark Boyd <markdavidboyd@gmail.com>
2019-10-02 17:36:43 -0400

    Revert "remove sf-sns-report task"
    This reverts commit 7220a965322b236821a69597b8f073707603913d.

---
+++ tasks/sf-sns-report/index.js
@@ -0,0 +1,102 @@
+'use strict';
+
+const get = require('lodash.get');
+const isObject = require('lodash.isobject');
+const { publishReportSnsMessages } = require('@cumulus/api/lambdas/publish-reports');
+const errors = require('@cumulus/common/errors');
+const cumulusMessageAdapter = require('@cumulus/cumulus-message-adapter-js');
+
+/**
+ * Determines if there was a valid exception in the input message
+ *
+ * @param {Object} event - aws event object
+ * @returns {boolean} true if there was an exception, false otherwise
+ */
+function eventFailed(event) {
+  // event has exception
+  // and it is needed to avoid flagging cases like "exception: {}" or "exception: 'none'"
+  if (isObject(event.exception)
+    && (Object.keys(event.exception).length > 0)) return true;
+
+  // Error and error keys are not part of the cumulus message
+  // and if they appear in the message something is seriously wrong
+  if (event.Error || event.error) return true;
+
+  return false;
+}
+
+/**
+ * Builds error object based on error type
+ *
+ * @param {string} type - error type
+ * @param {string} cause - error cause
+ * @returns {Object} the error object
+ */
+function buildError(type, cause) {
+  let ErrorClass;
+
+  if (Object.keys(errors).includes(type)) ErrorClass = errors[type];
+  else if (type === 'TypeError') ErrorClass = TypeError;
+  else ErrorClass = Error;
+
+  return new ErrorClass(cause);
+}
+
+/**
+ * If the cumulus message shows that a previous step failed,
+ * this function extracts the error message from the cumulus message
+ * and fails the function with that information. This ensures that the
+ * Step Function workflow fails with the correct error info
+ *
+ * @param {Object} event - aws event object
+ * @returns {undefined} throws an error and does not return anything
+ */
+function makeLambdaFunctionFail(event) {
+  const error = event.exception || event.error;
+
+  if (error) throw buildError(error.Error, error.Cause);
+
+  throw new Error('Step Function failed for an unknown reason.');
+}
+
+/**
+ * Publishes incoming Cumulus Message in its entirety to
+ * a given SNS topic
+ *
+ * @param  {Object} event - a Cumulus Message that has been sent through the
+ * Cumulus Message Adapter. See schemas/input.json for detailed input schema.
+ * @param {Object} event.config - configuration object for the task
+ * @param {Object} event.config.sfnEnd - indicate if it's the last step of the step function
+ * @returns {Promise.<Object>} - AWS SNS response or error in case of step function
+ *  failure.
+ */
+async function publishSnsMessage(event) {
+  const config = get(event, 'config');
+  const message = get(event, 'input');
+
+  const finished = get(config, 'sfnEnd', false);
+  const failed = eventFailed(message);
+
+  await publishReportSnsMessages(message, finished, failed);
+
+  if (failed) {
+    makeLambdaFunctionFail(message);
+  }
+
+  return get(message, 'payload', {});
+}
+
+exports.publishSnsMessage = publishSnsMessage;
+
+/**
+ * Lambda handler. It broadcasts an incoming Cumulus message to SNS
+ *
+ * @param {Object} event - a Cumulus Message
+ * @param {Object} context - an AWS Lambda context object
+ * @param {Function} callback - an AWS Lambda call back
+ * @returns {undefined} - does not return a value
+ */
+function handler(event, context, callback) {
+  cumulusMessageAdapter.runCumulusTask(publishSnsMessage, event, context, callback);
+}
+exports.handler = handler;
