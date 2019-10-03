d5004416e49baab7a4c5b324c23434466f835828
Marc <yjpa7145@users.noreply.github.com>
2019-10-03 13:59:35 -0400

    Add remaining resources from spreadsheet (#1212)

---
+++ tf-modules/cumulus_ecs_service/outputs.tf
@@ -0,0 +1,3 @@
+output "service_name" {
+  value = aws_ecs_service.default.name
+}
--- tf-modules/ingest/queue-pdrs-task.tf
+++ tf-modules/ingest/queue-pdrs-task.tf
@@ -25,3 +25,17 @@ resource "aws_lambda_function" "queue_pdrs_task" {
 
   tags = merge(local.default_tags, { Project = var.prefix })
 }
+
+resource "aws_cloudwatch_log_group" "queue_pdrs_task" {
+  name              = "/aws/lambda/${aws_lambda_function.queue_pdrs_task.function_name}"
+  retention_in_days = 30
+  tags              = local.default_tags
+}
+
+resource "aws_cloudwatch_log_subscription_filter" "queue_pdrs_task" {
+  name            = "${var.prefix}-QueuePdrsLogSubscription"
+  destination_arn = var.log2elasticsearch_lambda_function_arn
+  log_group_name  = aws_cloudwatch_log_group.queue_pdrs_task.name
+  filter_pattern  = ""
+  distribution    = "ByLogStream"
+}
