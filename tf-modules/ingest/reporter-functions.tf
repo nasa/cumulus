1a20528d750d7b0bb00ffd392b20b31f89a22eaa
Menno Van Diermen <mvandiermen@contractor.usgs.gov>
2019-10-04 17:05:39 -0500

    compatibility fixes

---
+++ tf-modules/ingest/reporter-functions.tf
@@ -0,0 +1,32 @@
+module "report_executions" {
+  source = "../report-executions"
+
+  prefix = var.prefix
+  executions_table = var.dynamo_tables.executions.name
+
+  permissions_boundary = var.permissions_boundary_arn
+  subnet_ids = var.lambda_subnet_ids
+  security_groups = var.lambda_subnet_ids == null ? null : [aws_security_group.no_ingress_all_egress[0].id]
+}
+
+module "report_granules" {
+  source = "../report-granules"
+
+  prefix = var.prefix
+  granules_table = var.dynamo_tables.granules.name
+
+  permissions_boundary = var.permissions_boundary_arn
+  subnet_ids = var.lambda_subnet_ids
+  security_groups = var.lambda_subnet_ids == null ? null : [aws_security_group.no_ingress_all_egress[0].id]
+}
+
+module "report_pdrs" {
+  source = "../report-pdrs"
+
+  prefix = var.prefix
+  pdrs_table = var.dynamo_tables.pdrs.name
+
+  permissions_boundary = var.permissions_boundary_arn
+  subnet_ids = var.lambda_subnet_ids
+  security_groups = var.lambda_subnet_ids == null ? null : [aws_security_group.no_ingress_all_egress[0].id]
+}
