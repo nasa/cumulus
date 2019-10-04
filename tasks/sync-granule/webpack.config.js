52000390133f33c92dc0ea5eaa59d9c329a05b38
Mark Boyd <markdavidboyd@gmail.com>
2019-10-04 18:11:10 -0400

    experiment with webpack/sourcemap config

--- tasks/sync-granule/webpack.config.js
+++ tasks/sync-granule/webpack.config.js
@@ -1,26 +1,39 @@
 const path = require('path');
 
-let mode = 'development';
-let devtool = 'inline-source-map';
+// let mode = 'development';
+// let devtool = 'inline-source-map';
 
-if(process.env.PRODUCTION) {
-  mode = 'production';
-  devtool = 'source-map';
-}
+// if(process.env.PRODUCTION) {
+//   mode = 'production';
+//   devtool = 'source-map';
+// }
 
 module.exports = {
-  mode,
+  mode: process.env.PRODUCTION ? 'production' : 'development',
   entry: './index.js',
   output: {
     libraryTarget: 'commonjs2',
     filename: 'index.js',
     path: path.resolve(__dirname, 'dist')
   },
+  module: {
+    rules: [{
+      test: /\.js$/,
+      loader: 'babel-loader',
+      include: __dirname,
+      exclude: /node_modules/,
+      options: {
+        plugins: [
+          'source-map-support',
+        ],
+      },
+    }],
+  },
   externals: [
     'aws-sdk',
     'electron',
     {'formidable': 'url'}
   ],
-  devtool,
+  devtool: 'inline-source-map',
   target: 'node'
 };
