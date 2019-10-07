2c8e85ba26ead4d27afb5a6b338d5146ccf001b9
Marc Huffnagle <marc@element84.com>
2019-10-07 16:16:44 -0400

    Move UMMG validation to a separate module and add tests

---
+++ packages/cmr-client/UmmUtils.js
@@ -0,0 +1,53 @@
+'use strict';
+
+const get = require('lodash.get');
+const got = require('got');
+const getUrl = require('./getUrl');
+const ValidationError = require('./ValidationError');
+
+/**
+ * Find the UMM version as a decimal string.
+ * If a version cannot be found on the input object
+ * version 1.4 is assumed and returned.
+ *
+ * @param {Object} umm - UMM metadata object
+ * @returns {string} UMM version for the given object
+ */
+function ummVersion(umm) {
+  return get(umm, 'MetadataSpecification.Version', '1.4');
+}
+
+/**
+ * Posts a given XML string to the validate endpoint of CMR and throws an
+ * exception if it is not valid
+ *
+ * @param {string} ummMetadata - the UMM object
+ * @param {string} identifier - the document identifier
+ * @param {string} provider - the CMR provider
+ * @returns {Promise<undefined>}
+ */
+async function validateUMMG(ummMetadata, identifier, provider) {
+  const version = ummVersion(ummMetadata);
+
+  const { statusCode, body } = await got.post(
+    `${getUrl('validate', provider)}granule/${identifier}`,
+    {
+      json: true,
+      body: ummMetadata,
+      headers: {
+        Accept: 'application/json',
+        'Content-type': `application/vnd.nasa.cmr.umm+json;version=${version}`
+      },
+      throwHttpErrors: false
+    }
+  );
+
+  if (statusCode === 200) return;
+
+  throw new ValidationError(`Validation was not successful, CMR error message: ${JSON.stringify(body.errors)}`);
+}
+
+module.exports = {
+  ummVersion,
+  validateUMMG
+};
