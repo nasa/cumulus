0e752525fcafadd59cba99319982eb7004c5253a
Marc Huffnagle <marc@element84.com>
2019-10-07 16:16:44 -0400

    Move UMMG validation to a separate module and add tests

--- packages/cmr-client/tests/test-UmmUtils.js
+++ packages/cmr-client/tests/test-UmmUtils.js
@@ -2,12 +2,9 @@
 
 const test = require('ava');
 const nock = require('nock');
-const rewire = require('rewire');
-
-const cmr = rewire('../CMR');
-const validateUMMG = cmr.__get__('validateUMMG');
 const ValidationError = require('../ValidationError');
 const validate = require('../validate');
+const { ummVersion, validateUMMG } = require('../UmmUtils');
 
 const cmrError = 'Granule start date [2016-01-09T11:41:12.027Z] is later than granule end date [2016-01-09T11:40:45.032Z].';
 
@@ -25,14 +22,18 @@ const ummValidationError = {
   ]
 };
 
-const xmlValidationError = `
-<?xml version="1.0" encoding="UTF-8"?>
-<errors><error>
-  <path>Temporal</path>
-  <errors>
-    <error>${cmrError}</error>
-  </errors>
-</error></errors>`;
+test.before(() => {
+  nock.disableNetConnect();
+  nock.enableNetConnect('127.0.0.1');
+});
+
+test.afterEach.always(() => {
+  nock.cleanAll();
+});
+
+test.after.always(() => {
+  nock.enableNetConnect();
+});
 
 test.serial('CMR.validateUMMG UMMG validation succeeds', async (t) => {
   nock('https://cmr.uat.earthdata.nasa.gov')
@@ -62,7 +63,6 @@ test.serial('CMR.validateUMMG UMMG validation fails with error messages from CMR
   } catch (e) {
     t.true(e instanceof ValidationError);
     t.true(e.message.includes(cmrError));
-    t.pass();
   }
 });
 
@@ -82,6 +82,15 @@ test.serial('cmr-client.validate XML validation succeeds', async (t) => {
 });
 
 test.serial('cmr-client.validate XML validation fails with error messages from CMR', async (t) => {
+  const xmlValidationError = `
+<?xml version="1.0" encoding="UTF-8"?>
+<errors><error>
+  <path>Temporal</path>
+  <errors>
+    <error>${cmrError}</error>
+  </errors>
+</error></errors>`;
+
   nock('https://cmr.uat.earthdata.nasa.gov')
     .post(`/ingest/providers/${provider}/validate/granule/${granuleId}`)
     .reply(422, xmlValidationError);
@@ -94,6 +103,70 @@ test.serial('cmr-client.validate XML validation fails with error messages from C
   } catch (e) {
     t.true(e instanceof ValidationError);
     t.true(e.message.includes(cmrError));
-    t.pass();
   }
+});
+
+test.serial('validateUMMG calls post with correct metadata version when metadata version available', async (t) => {
+  const identifier = 'fakeIdentifier';
+  const metadata = {
+    restOfMetadataUpHere: 'still fake',
+    MetadataSpecification: {
+      URL: 'https://cdn.earthdata.nasa.gov/umm/granule/v1.5',
+      Name: 'UMM-G',
+      Version: '1.5'
+    }
+  };
+
+  nock('https://cmr.uat.earthdata.nasa.gov')
+    .matchHeader('Accept', 'application/json')
+    .matchHeader('Content-type', 'application/vnd.nasa.cmr.umm+json;version=1.5')
+    .post(`/ingest/providers/${provider}/validate/granule/${identifier}`)
+    .reply(200);
+
+  process.env.CMR_ENVIRONMENT = 'UAT';
+  await validateUMMG(metadata, identifier, provider);
+
+  t.true(nock.isDone());
+});
+
+test.serial('validateUMMG calls post with default version (1.4) when metadata version unavailable', async (t) => {
+  const identifier = 'fakeIdentifier';
+  const metadata = {
+    restOfMetadataUpHere: 'still fake',
+    MissingMetadataSpecification: 'nothing here'
+  };
+
+  nock('https://cmr.uat.earthdata.nasa.gov')
+    .matchHeader('Accept', 'application/json')
+    .matchHeader('Content-type', 'application/vnd.nasa.cmr.umm+json;version=1.4')
+    .post(`/ingest/providers/${provider}/validate/granule/${identifier}`)
+    .reply(200);
+
+  process.env.CMR_ENVIRONMENT = 'UAT';
+  await validateUMMG(metadata, identifier, provider);
+
+  t.true(nock.isDone());
+});
+
+
+test('ummVersion returns UMM version if found on metadata object.', (t) => {
+  const metadata = {
+    restOfMetadataUpHere: 'it is all fake',
+    MetadataSpecification: {
+      URL: 'https://cdn.earthdata.nasa.gov/umm/granule/v1.5',
+      Name: 'UMM-G',
+      Version: '1.5'
+    }
+  };
+
+  t.is(ummVersion(metadata), '1.5');
+});
+
+test('ummVersion returns default version 1.4 if object has no metadata specification.', (t) => {
+  const metadata = {
+    restOfMetadataUpHere: 'still fake',
+    MissingMetadataSpecification: 'nothing here'
+  };
+
+  t.is(ummVersion(metadata), '1.4');
 });
