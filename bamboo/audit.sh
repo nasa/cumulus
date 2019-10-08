e7762535b2b22030819813235ad8156ce5577329
Lauren Frederick <lpfreder@mbp-lpfreder.edn.ecs.nasa.gov>
2019-10-08 14:46:49 -0400

    Move flag check

--- bamboo/audit.sh
+++ bamboo/audit.sh
@@ -5,16 +5,16 @@ set -ex
 commit_message_contains_skip_audit_flag=false
 commit_matches_version_tag=false
 
-if [[ $(git log --pretty='format:%Creset%s' -1) =~ '[skip-audit]' || $SKIP_AUDIT ]]; then
+if [[ $(git log --pretty='format:%Creset%s' -1) =~ '[skip-audit]' ]]; then
   commit_message_contains_skip_audit_flag=true;
 fi
 if [[ $(git describe --exact-match HEAD 2>/dev/null |sed -n '1p') =~ ^v[0-9]+.* ]]; then
   commit_matches_version_tag=true;
 fi
 
-if [[ $commit_message_contains_skip_audit_flag = false && $commit_matches_version_tag = false ]]; then
+if [[ $commit_message_contains_skip_audit_flag = false && $commit_matches_version_tag = false && ! $SKIP_AUDIT = true ]]; then
   npm run install-locks;
   npm run audit;
 else
-  >&2 echo "******Skipping audit due to commit message/version tag being present"
+  >&2 echo "******Skipping audit due to commit message/version tag/env var being present"
 fi
