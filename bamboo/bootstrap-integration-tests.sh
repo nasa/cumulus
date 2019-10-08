b44223c9333fcd6680ccd32ff5ea636eff27b3e2
Jonathan Kovarik <jonathan.kovarik@gmail.com>
2019-10-08 12:15:04 -0600

    Fix spacing

--- bamboo/bootstrap-integration-tests.sh
+++ bamboo/bootstrap-integration-tests.sh
@@ -11,7 +11,6 @@ set -ex
 . ./bamboo/abort-if-skip-integration-tests.sh
 . ./bamboo/abort-if-terraform-branch.sh
 
-
 npm config set unsafe-perm true
 npm install
 . ./bamboo/set-bamboo-env-variables.sh
