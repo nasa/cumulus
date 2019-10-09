14586b792f5c5a28a00c0ba756e04c930ae04362
Lauren Frederick <lpfreder@mbp-lpfreder.edn.ecs.nasa.gov>
2019-10-09 07:59:30 -0400

    Remove test steps temporarily

--- bamboo/deploy-to-npm.sh
+++ bamboo/deploy-to-npm.sh
@@ -6,14 +6,14 @@ set -ex
 export VERSION=$(jq --raw-output .version lerna.json)
 export NPM_TAG=$(node ./bamboo/npm-tag.js);
 
-echo "Publishing packages to NPM with version=${VERSION} and tag=${NPM_TAG}"
-export npm_config_unsafe_perm=true
-npx lerna publish \
-  ${VERSION} \
-  --no-git-tag-version \
-  --no-push \
-  --yes \
-  --force-publish=* \
-  --dist-tag=${NPM_TAG} \
-  --exact
+# echo "Publishing packages to NPM with version=${VERSION} and tag=${NPM_TAG}"
+# export npm_config_unsafe_perm=true
+# npx lerna publish \
+#   ${VERSION} \
+#   --no-git-tag-version \
+#   --no-push \
+#   --yes \
+#   --force-publish=* \
+#   --dist-tag=${NPM_TAG} \
+#   --exact
 
