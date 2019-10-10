7cf56ef9d0a46afd87830b828654cc7b62fe251f
Jonathan Kovarik <jonathan.kovarik@gmail.com>
2019-10-10 15:31:15 -0600

    disable audit

--- bamboo/audit.sh
+++ bamboo/audit.sh
@@ -12,9 +12,3 @@ if [[ $(git describe --exact-match HEAD 2>/dev/null |sed -n '1p') =~ ^v[0-9]+.* 
   commit_matches_version_tag=true;
 fi
 
-if [[ $commit_message_contains_skip_audit_flag = false && $commit_matches_version_tag = false ]]; then
-#  npm run install-locks;
- # npm run audit;
-else
-  >&2 echo "******Skipping audit due to commit message/version tag being present"
-fi
