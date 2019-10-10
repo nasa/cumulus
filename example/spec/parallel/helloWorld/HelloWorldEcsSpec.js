3c6e304f3bd2544f18038804ad371b51f39c05f0
Jonathan Kovarik <jonathan.kovarik@gmail.com>
2019-10-10 14:56:42 -0600

    Remove unneeded test.
    If we're running tests against TF *and* kes deployment, there's no
    need for a seperate test.  We can, instead, just run it configured
    seperately in TF and Kes deployment

--- example/spec/parallel/helloWorld/HelloWorldEcsSpec.js
+++ example/spec/parallel/helloWorld/HelloWorldEcsSpec.js
@@ -6,46 +6,6 @@ const { waitForModelStatus } = require('../../helpers/apiUtils');
 const awsConfig = loadConfig();
 const activityStep = new ActivityStep();
 
-// TODO Remove this test entirely when we are no longer using kes
-xdescribe('The Hello World workflow using ECS and kes CMA', () => {
-  let workflowExecution = null;
-  process.env.ExecutionsTable = `${awsConfig.stackName}-ExecutionsTable`;
-  const executionModel = new Execution();
-
-  beforeAll(async () => {
-    workflowExecution = await buildAndExecuteWorkflow(
-      awsConfig.stackName,
-      awsConfig.bucket,
-      'EcsKesCMAHelloWorldWorkflow'
-    );
-  });
-
-  it('executes successfully', () => {
-    expect(workflowExecution.status).toEqual('SUCCEEDED');
-  });
-
-  describe('the HelloWorld ECS', () => {
-    let activityOutput = null;
-
-    beforeAll(async () => {
-      activityOutput = await activityStep.getStepOutput(
-        workflowExecution.executionArn,
-        'EcsTaskHelloWorld'
-      );
-    });
-
-    it('output is Hello World', () => {
-      expect(activityOutput.payload).toEqual({ hello: 'Hello World' });
-    });
-  });
-
-  describe('the sf-sns-report task has published a sns message and', () => {
-    it('the execution record is added to DynamoDB', async () => {
-      const record = await executionModel.get({ arn: workflowExecution.executionArn });
-      expect(record.status).toEqual('completed');
-    });
-  });
-});
 
 describe('The Hello World workflow using ECS and CMA Layers', () => {
   let workflowExecution = null;
