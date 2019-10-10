0f9cf53cd73dc55e03c1da508d0818c2d9f15af6
Jonathan Kovarik <jonathan.kovarik@gmail.com>
2019-10-10 13:22:54 -0600

    Update CI configuration to work with main TF branch

--- bamboo/integration-tests.sh
+++ bamboo/integration-tests.sh
@@ -6,10 +6,4 @@ set -ex
 . ./bamboo/abort-if-terraform-branch.sh
 
 
-if [[ $DEPLOYMENT =~ '-tf' ]]; then
-  echo "Running migrated terraform-compatible tests."
-  . ./bamboo/integration-tests-terraform.sh
-else
-  echo "Running kes-compatible full test suite."
-  cd example && npm test
-fi
+cd example && npm test
