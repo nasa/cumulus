ad8ba74d9a23215858f5376372c039675021eb13
Jonathan Kovarik <jonathan.kovarik@gmail.com>
2019-10-10 15:03:22 -0600

    re-enable terraform integration tests

--- bamboo/integration-tests.sh
+++ bamboo/integration-tests.sh
@@ -3,7 +3,6 @@ set -ex
 . ./bamboo/abort-if-not-pr-or-redeployment.sh
 . ./bamboo/abort-if-skip-integration-tests.sh
 . ./bamboo/set-bamboo-env-variables.sh
-. ./bamboo/abort-if-terraform-branch.sh
 
 
 cd example && npm test
