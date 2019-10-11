05a73c313103d3b53e7ba58db45a7a1782273011
Mark Boyd <markdavidboyd@gmail.com>
2019-10-11 15:04:09 -0400

    update code comments

--- tasks/sf-sns-report/index.js
+++ tasks/sf-sns-report/index.js
@@ -5,15 +5,11 @@ const { publishReportSnsMessages } = require('@cumulus/api/lambdas/publish-repor
 const cumulusMessageAdapter = require('@cumulus/cumulus-message-adapter-js');
 
 /**
- * Publishes incoming Cumulus Message in its entirety to
- * a given SNS topic
+ * Publishes ingest notifications based on the Cumulus execution message.
  *
- * @param  {Object} event - a Cumulus Message that has been sent through the
- * Cumulus Message Adapter. See schemas/input.json for detailed input schema.
- * @param {Object} event.config - configuration object for the task
- * @param {Object} event.config.sfnEnd - indicate if it's the last step of the step function
- * @returns {Promise.<Object>} - AWS SNS response or error in case of step function
- *  failure.
+ * @param  {Object} event - a Cumulus execution message that has been sent through the
+ * Cumulus Message Adapter.
+ * @returns {Promise<Object>} - Payload object from the Cumulus message
  */
 async function publishSnsMessage(event) {
   const message = get(event, 'input');
@@ -28,14 +24,15 @@ async function publishSnsMessage(event) {
 exports.publishSnsMessage = publishSnsMessage;
 
 /**
- * Lambda handler. It broadcasts an incoming Cumulus message to SNS
+ * Lambda handler.
  *
  * @param {Object} event - a Cumulus Message
  * @param {Object} context - an AWS Lambda context object
- * @param {Function} callback - an AWS Lambda call back
+ * @param {Function} callback - an AWS Lambda callback
  * @returns {undefined} - does not return a value
  */
 function handler(event, context, callback) {
   cumulusMessageAdapter.runCumulusTask(publishSnsMessage, event, context, callback);
 }
+
 exports.handler = handler;
