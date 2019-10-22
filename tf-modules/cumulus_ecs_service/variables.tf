7f5d487a8f6c98fce22201674c37845f9f8bb645
Marc Huffnagle <marc@element84.com>
2019-10-22 14:58:12 -0400

    Remove Cumulus-defined service autoscaling

--- tf-modules/cumulus_ecs_service/variables.tf
+++ tf-modules/cumulus_ecs_service/variables.tf
@@ -21,16 +21,7 @@ variable "prefix" {
   type = string
 }
 
-variable "scaling_role_arn" {
-  type = string
-}
-
 # Optional
-
-variable "activity_arn" {
-  type    = string
-  default = null
-}
 
 variable "alarms" {
   type    = map(object({ comparison_operator = string, metric_name = string, threshold = number }))
@@ -52,29 +43,14 @@ variable "desired_count" {
   default = 0
 }
 
-variable "enable_autoscaling" {
-  type    = bool
-  default = false
-}
-
 variable "environment" {
   type    = map(string)
   default = {}
 }
 
-variable "max_capacity" {
-  type    = number
-  default = 2
-}
-
 variable "memory_reservation" {
   type    = number
   default = 256
-}
-
-variable "min_capacity" {
-  type    = number
-  default = 1
 }
 
 variable "network_mode" {
@@ -85,26 +61,6 @@ variable "network_mode" {
 variable "privileged" {
   type    = bool
   default = false
-}
-
-variable "scale_in_activity_schedule_time" {
-  type    = number
-  default = 5000
-}
-
-variable "scale_in_adjustment_percent" {
-  type    = number
-  default = -5
-}
-
-variable "scale_out_adjustment_percent" {
-  type    = number
-  default = 10
-}
-
-variable "scale_out_activity_schedule_time" {
-  type    = number
-  default = 10000
 }
 
 variable "tags" {
