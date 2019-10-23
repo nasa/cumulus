0a9696bec457e122aa6bdcae1de023abb4b48be9
Mark Boyd <markdavidboyd@gmail.com>
2019-10-23 12:53:43 -0400

    update webpack config to use file-loader for JPG files

--- tasks/test-processing/webpack.config.js
+++ tasks/test-processing/webpack.config.js
@@ -27,6 +27,14 @@ module.exports = {
           },
         ],
       },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+          },
+        ],
+      }
     ],
   },
   devtool: 'inline-source-map',
