31c69788f6632fed95f841cf33db587155e30bd0
Jonathan Kovarik <jonathan.kovarik@gmail.com>
2019-10-24 10:32:00 -0600

    Update release script to build modules

--- bamboo/create-release.sh
+++ bamboo/create-release.sh
@@ -15,7 +15,14 @@ export VERSION=$(jq --raw-output .version lerna.json)
    -X POST\
    https://api.github.com/repos/jkovarik/cumulus/releases |grep \"url\" |grep releases |sed -e 's/.*\(https.*\)\"\,/\1/'| sed -e 's/api/uploads/')
 
+## Build TF modules that require source building
+
+$(cd tf-modules/distribution && ./bin/build-tf-module.sh)
+$(cd tf-modules/s3-replicator && ./bin/build-tf-module.sh)
+
 ## Create zipfile
 zip -r -x \*node_modules\* -o terraform-cumulus-$VERSION.zip tf-modules tasks packages
+
+### Release package
 echo $RELEASE_URL
 curl -X POST -H "Authorization: token $TOKEN" --data-binary "@terraform-cumulus-$VERSION.zip" -H "Content-type: application/octet-stream" $RELEASE_URL/assets?name=terraform-cumulus-$VERSION.zip
