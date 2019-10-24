e4ee2e0b24aceb362f7b5198dd10a6a6378c7b62
Jonathan Kovarik <jonathan.kovarik@gmail.com>
2019-10-24 13:35:49 -0600

    Remove versions from filenames

--- bamboo/create-release.sh
+++ bamboo/create-release.sh
@@ -17,16 +17,16 @@ export VERSION=$(jq --raw-output .version lerna.json)
 
 ## Build TF modules that require source building
 
-(cd tf-modules/distribution && ./bin/build-tf-module.sh && cp ./dist/terraform-aws-cumulus-distribution.zip ../../terraform-aws-cumulus-distribution-$VERSION.zip)
-(cd tf-modules/s3-replicator && ./bin/build-tf-module.sh && cp ./dist/terraform-aws-cumulus-s3-replicator.zip ../../terraform-aws-cumulus-s3-replicator-$VERSION.zip)
+(cd tf-modules/distribution && ./bin/build-tf-module.sh && cp ./dist/terraform-aws-cumulus-distribution.zip ../../terraform-aws-cumulus-distribution.zip)
+(cd tf-modules/s3-replicator && ./bin/build-tf-module.sh && cp ./dist/terraform-aws-cumulus-s3-replicator.zip ../../terraform-aws-cumulus-s3-replicator.zip)
 
 ## Create zipfile
-zip -r -x \*node_modules\* -o terraform-aws-cumulus-$VERSION.zip tf-modules tasks packages
-(cd ./tf-modules/workflow; zip -r -x \*node_modules\* -o ../../terraform-aws-cumulus-workflow-$VERSION.zip .)
+zip -r -x \*node_modules\* -o terraform-aws-cumulus.zip tf-modules tasks packages
+(cd ./tf-modules/workflow; zip -r -x \*node_modules\* -o ../../terraform-aws-cumulus-workflow.zip .)
 
 ### Release package
 echo $RELEASE_URL
-curl -X POST -H "Authorization: token $TOKEN" --data-binary "@terraform-aws-cumulus-$VERSION.zip" -H "Content-type: application/octet-stream" $RELEASE_URL/assets?name=terraform-aws-cumulus-$VERSION.zip
-curl -X POST -H "Authorization: token $TOKEN" --data-binary "@terraform-aws-cumulus-workflow-$VERSION.zip" -H "Content-type: application/octet-stream" $RELEASE_URL/assets?name=terraform-aws-cumulus-workflow-$VERSION.zip
-curl -X POST -H "Authorization: token $TOKEN" --data-binary "@terraform-aws-cumulus-s3-replicator-$VERSION.zip" -H "Content-type: application/octet-stream" $RELEASE_URL/assets?name=terraform-aws-cumulus-s3-replicator-$VERSION.zip
-curl -X POST -H "Authorization: token $TOKEN" --data-binary "@terraform-aws-cumulus-distribution-$VERSION.zip" -H "Content-type: application/octet-stream" $RELEASE_URL/assets?name=terraform-aws-cumulus-distribution-$VERSION.zip
+curl -X POST -H "Authorization: token $TOKEN" --data-binary "@terraform-aws-cumulus.zip" -H "Content-type: application/octet-stream" $RELEASE_URL/assets?name=terraform-aws-cumulus.zip
+curl -X POST -H "Authorization: token $TOKEN" --data-binary "@terraform-aws-cumulus-workflow.zip" -H "Content-type: application/octet-stream" $RELEASE_URL/assets?name=terraform-aws-cumulus-workflow.zip
+curl -X POST -H "Authorization: token $TOKEN" --data-binary "@terraform-aws-cumulus-s3-replicator.zip" -H "Content-type: application/octet-stream" $RELEASE_URL/assets?name=terraform-aws-cumulus-s3-replicator.zip
+curl -X POST -H "Authorization: token $TOKEN" --data-binary "@terraform-aws-cumulus-distribution.zip" -H "Content-type: application/octet-stream" $RELEASE_URL/assets?name=terraform-aws-cumulus-distribution.zip
