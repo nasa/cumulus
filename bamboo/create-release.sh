51d87f0fba833b6b805b4c86abc1a00f1b23e671
Jonathan Kovarik <jonathan.kovarik@gmail.com>
2019-10-24 14:14:59 -0600

    Add -FS to all zip commands

--- bamboo/create-release.sh
+++ bamboo/create-release.sh
@@ -21,9 +21,9 @@ export VERSION=$(jq --raw-output .version lerna.json)
 (cd tf-modules/s3-replicator && ./bin/build-tf-module.sh && cp ./dist/terraform-aws-cumulus-s3-replicator.zip ../../terraform-aws-cumulus-s3-replicator.zip)
 
 ## Create zipfiles
-zip -r -x \*node_modules\* -o terraform-aws-cumulus.zip tf-modules tasks/**/dist/** packages/**/dist/**
-(cd ./tf-modules/workflow; zip -r -x \*node_modules\* -o ../../terraform-aws-cumulus-workflow.zip .)
-(cd ./tf-modules/cumulus_ecs_service; zip -r -x \*node_modules\* -o ../../terraform-aws-cumulus-ecs-service.zip .)
+zip -FS -r -x \*node_modules\* -o terraform-aws-cumulus.zip tf-modules tasks/**/dist/** packages/**/dist/**
+(cd ./tf-modules/workflow; zip -FS -r -x \*node_modules\* -o ../../terraform-aws-cumulus-workflow.zip .)
+(cd ./tf-modules/cumulus_ecs_service; zip -FS -r -x \*node_modules\* -o ../../terraform-aws-cumulus-ecs-service.zip .)
 
 ### Release package
 echo $RELEASE_URL
