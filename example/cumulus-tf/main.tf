fed3e69d489483319086b7b9394e19f82989456d
Lauren Frederick <lpfreder@mbp-lpfreder.edn.ecs.nasa.gov>
2019-11-04 15:04:41 -0500

    Move description

--- example/cumulus-tf/main.tf
+++ example/cumulus-tf/main.tf
@@ -115,7 +115,6 @@ resource "aws_lambda_permission" "sns_s3_test" {
 
 module "s3_access_test_lambda" {
   source                     = "./modules/s3_access_test"
-  description                = "Lambda for integration testing direct S3 access"
 
   prefix                     = var.prefix
   lambda_processing_role_arn = module.cumulus.lambda_processing_role_arn
--- example/cumulus-tf/modules/s3_access_test/main.tf
+++ example/cumulus-tf/modules/s3_access_test/main.tf
@@ -10,6 +10,7 @@ provider "aws" {
 
 resource "aws_lambda_function" "s3_acccess_test" {
   function_name    = "${var.prefix}-s3AccessTest"
+  description      = "Lambda for integration testing direct S3 access"
   filename         = "${path.module}/../../../lambdas/s3AccessTest/lambda.zip"
   source_code_hash = filebase64sha256("${path.module}/../../../lambdas/s3AccessTest/lambda.zip")
   handler          = "index.handler"
