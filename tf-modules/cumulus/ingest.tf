709a1048a41387d0bcbbcabc7c736d743cf9f2c2
Mark Boyd <markdavidboyd@gmail.com>
2019-11-11 15:43:38 -0500

    pass custom_queues var from cumulus module through to ingest module

--- tf-modules/cumulus/ingest.tf
+++ tf-modules/cumulus/ingest.tf
@@ -40,6 +40,7 @@ module "ingest" {
   log2elasticsearch_lambda_function_arn = module.archive.log2elasticsearch_lambda_function_arn
 
   queue_execution_limits = var.queue_execution_limits
+  custom_queues = var.custom_queues
 
   report_executions_sns_topic_arn = module.archive.report_executions_sns_topic_arn
   report_granules_sns_topic_arn   = module.archive.report_granules_sns_topic_arn
