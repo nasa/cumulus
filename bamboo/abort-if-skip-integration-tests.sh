9c5d3dcecaa0f2d6eba888e31d3f6607abd6c783
Jonathan Kovarik <kovarik@nsidc.org>
2019-11-14 13:04:40 -0700

    Add skip integration env var (#1339)
    * Add skip integration env var
    
    * Fix int test flags
    
    * Fix abort ordering

--- bamboo/abort-if-skip-integration-tests.sh
+++ bamboo/abort-if-skip-integration-tests.sh
@@ -2,7 +2,7 @@
 set -e
 
 source .bamboo_env_vars || true
-if [[ $COMMIT_MESSAGE =~ skip-integration-tests ]]; then
+if [[ $COMMIT_MESSAGE =~ skip-integration-tests || $SKIP_INTEGRATION_TESTS == true ]]; then
   >&2 echo "******Skipping integration tests based on commit message: $COMMIT_MESSAGE"
   exit 0
 fi
