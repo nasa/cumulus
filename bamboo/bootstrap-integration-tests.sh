b4d37d17d441884c1f8e9355e8d29f7d79f53643
Jonathan Kovarik <jonathan.kovarik@gmail.com>
2019-11-14 20:25:25 -0700

    Update pathing for post-release deployment script

--- bamboo/bootstrap-integration-tests.sh
+++ bamboo/bootstrap-integration-tests.sh
@@ -17,10 +17,21 @@ npm install
 
 if [[ $USE_TERRAFORM_ZIPS == true ]]; then
   echo "***Deploying stack with deployment packages"
+
+  ## Update cumulus-tf
+
   cd example/cumulus-tf
-  sed -i 's/source = \"..\/..\//source = \"https:\/\/github.com\/nasa\/cumulus\/releases\/download\/$VERSION_FLAG/terraform-aws-cumulus.zip\/\//g' *.tf
+  # Update to use workflow module
+  sed -i "s/source = \"..\/..\/tf-modules\/workflow/source = \"https:\/\/github.com\/nasa\/cumulus\/releases\/download\/$VERSION_FLAG\/terraform-aws-cumulus-workflow.zip\/\//g" *.tf
+  # Update to use ecs service module
+  sed -i "s/source = \"..\/..\/tf-modules\/cumulus_ecs_service/source = \"https:\/\/github.com\/nasa\/cumulus\/releases\/download\/$VERSION_FLAG\/terraform-aws-cumulus-ecs-service.zip\/\//g" *.tf
+  # Update to use cumulus core module
+  sed -i "s/source = \"..\/..\/tf-modules\/cumulus/source = \"https:\/\/github.com\/nasa\/cumulus\/releases\/download\/$VERSION_FLAG\/terraform-aws-cumulus.zip\/\/tf-modules\/cumulus/g" *.tf
+
+  ## Update data-persistence
+
   cd ../data-persistence-tf
-  sed -i 's/source = \"..\/..\//source = \"https:\/\/github.com\/nasa\/cumulus\/releases\/download\/$VERSION_FLAG/terraform-aws-cumulus.zip\/\//g' *.tf
+  sed -i "s/source = \"..\/..\/tf-modules\/data-persistence/source = \"https:\/\/github.com\/nasa\/cumulus\/releases\/download\/$VERSION_FLAG\/terraform-aws-cumulus.zip\/\/tf-modules\/data-persistence/g" *.tf
   cd ../../
 else
   echo "***Deploying stack with built source"
