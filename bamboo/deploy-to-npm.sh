6710f502122cc375a75c4e08ba53171ab5dd1719
Jonathan Kovarik <jonathan.kovarik@gmail.com>
2019-11-15 20:28:56 -0700

    Add example lambda to release

--- bamboo/deploy-to-npm.sh
+++ bamboo/deploy-to-npm.sh
@@ -8,13 +8,14 @@ export NPM_TAG=$(node ./bamboo/npm-tag.js);
 
 echo "Publishing packages to NPM with version=${VERSION} and tag=${NPM_TAG}"
 export npm_config_unsafe_perm=true
-npx lerna publish \
-  ${VERSION} \
-  --no-git-tag-version \
-  --no-push \
-  --yes \
-  --force-publish=* \
-  --dist-tag=${NPM_TAG} \
-  --exact
+npx lerna run prepare
+#npx lerna publish \
+#  ${VERSION} \
+#  --no-git-tag-version \
+#  --no-push \
+#  --yes \
+#  --force-publish=* \
+#  --dist-tag=${NPM_TAG} \
+#  --exact#
 
 . ./bamboo/create-release.sh
