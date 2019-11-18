f60a8f73ef13d178a328a84ea5e453a8c88237b0
Jonathan Kovarik <jonathan.kovarik@gmail.com>
2019-11-18 06:59:22 -0700

    Un-comment deploy script

--- bamboo/deploy-to-npm.sh
+++ bamboo/deploy-to-npm.sh
@@ -8,14 +8,13 @@ export NPM_TAG=$(node ./bamboo/npm-tag.js);
 
 echo "Publishing packages to NPM with version=${VERSION} and tag=${NPM_TAG}"
 export npm_config_unsafe_perm=true
-npx lerna run prepare
-#npx lerna publish \
-#  ${VERSION} \
-#  --no-git-tag-version \
-#  --no-push \
-#  --yes \
-#  --force-publish=* \
-#  --dist-tag=${NPM_TAG} \
-#  --exact#
+npx lerna publish \
+  ${VERSION} \
+  --no-git-tag-version \
+  --no-push \
+  --yes \
+  --force-publish=* \
+  --dist-tag=${NPM_TAG} \
+  --exact#
 
 . ./bamboo/create-release.sh
