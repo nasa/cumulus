4f2811dafa3fe9fdcaa305386e7bbcfeb6bc42a7
Mark Boyd <markdavidboyd@gmail.com>
2019-11-25 12:59:30 -0500

    simplify tests

--- packages/common/tests/test-sfnStep.js
+++ packages/common/tests/test-sfnStep.js
@@ -10,142 +10,47 @@ const {
   getStepExitedEvent,
   getTaskExitedEventOutput
 } = require('../sfnStep');
-const { randomId, randomNumber } = require('../test-utils');
 
-const failedStepOutput = {
-  error: 'Error',
-  cause: '{\"errorMessage\":\"{\\\"errorMessage\\\":\\\"CMR Error: Invalid username or password, please retry.\\\",\\\"errorType\\\":\\\"Error\\\",\\\"stackTrace\\\":[\\\"updateToken (/var/task/index.js:437:41)\\\",\\\"<anonymous>\\\",\\\"process._tickDomainCallback (internal/process/next_tick.js:228:7)\\\"]}\",\"errorType\":\"Error\",\"stackTrace\":[\"buildError (/var/task/index.js:20549:10)\",\"makeLambdaFunctionFail (/var/task/index.js:20564:20)\",\"publishSnsMessage (/var/task/index.js:20618:5)\",\"<anonymous>\",\"process._tickDomainCallback (internal/process/next_tick.js:228:7)\"]}'
-};
-
-const failedStepException = {
-  error: 'Error',
-  cause: '{\"errorMessage\":\"{\\\"errorMessage\\\":\\\"CMR Error: Invalid username or password, please retry.\\\",\\\"errorType\\\":\\\"Error\\\",\\\"stackTrace\\\":[\\\"updateToken (/var/task/index.js:437:41)\\\",\\\"<anonymous>\\\",\\\"process._tickDomainCallback (internal/process/next_tick.js:228:7)\\\"]}\",\"errorType\":\"Error\",\"stackTrace\":[\"buildError (/var/task/index.js:20549:10)\",\"makeLambdaFunctionFail (/var/task/index.js:20564:20)\",\"publishSnsMessage (/var/task/index.js:20618:5)\",\"<anonymous>\",\"process._tickDomainCallback (internal/process/next_tick.js:228:7)\"]}'
-};
-
-const createFakeExecutionHistory = ({
-  message,
-  completedStepId = randomNumber(),
-  stepName = randomId('stepName'),
-  stepType = 'lambda',
-  timestamp = Date.now(),
-  failed = true,
-  finalEventType = 'taskExit'
-}) => {
-  const isLambdaHistory = stepType === 'lambda';
-  const scheduledDetailsKey = isLambdaHistory
-    ? 'lambdaFunctionScheduledEventDetails'
-    : 'activityScheduledEventDetails';
-
-  let completedDetailsKey;
-  let completionEventType;
-  if (failed) {
-    completedDetailsKey = isLambdaHistory ? 'lambdaFunctionFailedEventDetails' : 'activityFailedEventDetails';
-    completionEventType = isLambdaHistory ? 'LambdaFunctionFailed' : 'ActivityFailed';
-  } else {
-    completedDetailsKey = isLambdaHistory ? 'lambdaFunctionSucceededEventDetails' : 'activitySucceededEventDetails';
-    completionEventType = isLambdaHistory ? 'LambdaFunctionSucceeded' : 'ActivitySucceeded';
-  }
-
+test('getStepExitedEvent returns falsy if task exited event cannot be found', (t) => {
   const events = [
     {
-      timestamp,
-      type: isLambdaHistory ? 'LambdaFunctionScheduled' : 'ActivityScheduled',
-      [scheduledDetailsKey]: {
-        resource: stepName,
-        input: JSON.stringify(message)
+      type: 'LambdafunctionFailed',
+      id: 1,
+      lambdaFunctionFailedEventDetails: {}
+    },
+    {
+      type: 'TaskStateExited',
+      id: 2,
+      previousEventId: 1,
+      stateExitedEventDetails: {
+        output: {}
       }
-    },
-    {
-      timestamp: timestamp + 100,
-      type: isLambdaHistory ? 'LambdaFunctionStarted' : 'ActivityStarted'
-    },
-    {
-      timestamp: timestamp + 200,
-      type: completionEventType,
-      id: completedStepId,
-      [completedDetailsKey]: failed ? failedStepOutput : {}
     }
   ];
 
-  if (finalEventType === 'taskExit') {
-    const additionalOutput = failed
-      ? { exception: failedStepException }
-      : {};
-    events.push({
-      timestamp: timestamp + 200,
-      type: 'TaskStateExited',
-      id: completedStepId + 1,
-      previousEventId: completedStepId,
-      stateExitedEventDetails: {
-        name: stepName,
-        output: JSON.stringify({
-          ...message,
-          ...additionalOutput
-        })
-      }
-    });
-  } else if (finalEventType === 'executionFail') {
-    events.push({
-      timestamp: timestamp + 200,
-      type: 'ExecutionFailed',
-      id: completedStepId + 1,
-      previousEventId: completedStepId,
-      executionFailedEventDetails: failedStepException
-    });
-  }
-
-  return {
-    events
-  };
-};
-
-const createCumulusMessage = ({
-  cMetaParams = {},
-  payload = {}
-} = {}) => ({
-  cumulus_meta: {
-    execution_name: randomId('execution'),
-    state_machine: randomId('ingest-'),
-    ...cMetaParams
-  },
-  payload
-});
-
-test.beforeEach((t) => {
-  t.context.completedStepId = randomNumber();
-  t.context.invalidFailedStepId = randomNumber();
-  // Ensure that the invalid failed step ID does not match an event ID
-  // in the execution history
-  while (t.context.completedStepId === t.context.invalidFailedStepId) {
-    t.context.invalidFailedStepId = randomNumber();
-  }
-});
-
-test('getStepExitedEvent returns falsy if task exited event cannot be found', (t) => {
-  const { completedStepId, invalidFailedStepId } = t.context;
-
-  const message = createCumulusMessage();
-  const fakeExecutionHistory = createFakeExecutionHistory({
-    message,
-    failed: false,
-    completedStepId
-  });
-  t.falsy(getStepExitedEvent(fakeExecutionHistory.events, { id: invalidFailedStepId }));
+  t.falsy(getStepExitedEvent(events, { id: 3 }));
 });
 
 test('getStepExitedEvent returns correct task exited event', (t) => {
-  const { completedStepId } = t.context;
-
-  const message = createCumulusMessage();
-  const fakeExecutionHistory = createFakeExecutionHistory({
-    message,
-    failed: false,
-    completedStepId
-  });
+  const events = [
+    {
+      type: 'LambdafunctionFailed',
+      id: 1,
+      lambdaFunctionFailedEventDetails: {}
+    },
+    {
+      type: 'TaskStateExited',
+      id: 2,
+      previousEventId: 1,
+      stateExitedEventDetails: {
+        output: {}
+      }
+    }
+  ];
 
   const taskExitedEvent = getStepExitedEvent(
-    fakeExecutionHistory.events,
-    { id: completedStepId }
+    events,
+    { id: 1 }
   );
   t.truthy(taskExitedEvent);
   t.is(taskExitedEvent.type, 'TaskStateExited');
