59fcb6bbb4fbfce9774beb7d66e458eaacdfc4ea
Marc <yjpa7145@users.noreply.github.com>
2019-12-06 09:21:17 -0500

    Make deployment of the S3 credentials endpoint optional (#1365)
    * Remove STSCredentialsLambda env var from ApiEndpoints lambda
    
    * Make deployment of the S3 credentials endpoint optional
    
    * Revert unintended change
    
    * Remove extra new line
    
    * Make sts_credentials_lambda_function_arn optional in the Cumulus module

--- tf-modules/cumulus/distribution.tf
+++ tf-modules/cumulus/distribution.tf
@@ -1,6 +1,7 @@
 module "distribution" {
   source = "../distribution"
 
+  deploy_s3_credentials_endpoint      = var.deploy_distribution_s3_credentials_endpoint
   api_gateway_stage                   = var.distribution_api_gateway_stage
   distribution_url                    = var.distribution_url
   log_api_gateway_to_cloudwatch       = var.log_api_gateway_to_cloudwatch
--- tf-modules/distribution/main.tf
+++ tf-modules/distribution/main.tf
@@ -49,6 +49,8 @@ module "thin_egress_app" {
 data "aws_caller_identity" "current" {}
 
 resource "aws_dynamodb_table" "access_tokens" {
+  count = var.deploy_s3_credentials_endpoint ? 1 : 0
+
   name         = "${var.prefix}-s3-credentials-access-tokens"
   billing_mode = "PAY_PER_REQUEST"
   hash_key     = "accessToken"
@@ -62,6 +64,8 @@ resource "aws_dynamodb_table" "access_tokens" {
 }
 
 data "aws_iam_policy_document" "assume_lambda_role" {
+  count = var.deploy_s3_credentials_endpoint ? 1 : 0
+
   statement {
     principals {
       type        = "Service"
@@ -72,14 +76,18 @@ data "aws_iam_policy_document" "assume_lambda_role" {
 }
 
 resource "aws_iam_role" "s3_credentials_lambda" {
+  count = var.deploy_s3_credentials_endpoint ? 1 : 0
+
   name                 = "${var.prefix}-S3CredentialsLambda"
-  assume_role_policy   = data.aws_iam_policy_document.assume_lambda_role.json
+  assume_role_policy   = data.aws_iam_policy_document.assume_lambda_role[0].json
   permissions_boundary = var.permissions_boundary_arn
   # TODO Re-enable once IAM permissions have been fixed
   # tags                 = local.default_tags
 }
 
 data "aws_iam_policy_document" "s3_credentials_lambda" {
+  count = var.deploy_s3_credentials_endpoint ? 1 : 0
+
   statement {
     actions   = ["lambda:InvokeFunction"]
     resources = [var.sts_credentials_lambda_function_arn]
@@ -90,7 +98,7 @@ data "aws_iam_policy_document" "s3_credentials_lambda" {
       "dynamodb:GetItem",
       "dynamodb:PutItem"
     ]
-    resources = [aws_dynamodb_table.access_tokens.arn]
+    resources = [aws_dynamodb_table.access_tokens[0].arn]
   }
 
   statement {
@@ -112,12 +120,14 @@ data "aws_iam_policy_document" "s3_credentials_lambda" {
 }
 
 resource "aws_iam_role_policy" "s3_credentials_lambda" {
-  policy = data.aws_iam_policy_document.s3_credentials_lambda.json
-  role   = aws_iam_role.s3_credentials_lambda.id
+  count = var.deploy_s3_credentials_endpoint ? 1 : 0
+
+  policy = data.aws_iam_policy_document.s3_credentials_lambda[0].json
+  role   = aws_iam_role.s3_credentials_lambda[0].id
 }
 
 resource "aws_security_group" "s3_credentials_lambda" {
-  count = var.vpc_id == null ? 0 : 1
+  count = (var.deploy_s3_credentials_endpoint && var.vpc_id != null) ? 1 : 0
 
   vpc_id = var.vpc_id
 
@@ -131,18 +141,20 @@ resource "aws_security_group" "s3_credentials_lambda" {
 }
 
 resource "aws_lambda_function" "s3_credentials" {
+  count            = var.deploy_s3_credentials_endpoint ? 1 : 0
+
   function_name    = "${var.prefix}-s3-credentials-endpoint"
   filename         = "${path.module}/dist/src.zip"
   source_code_hash = filebase64sha256("${path.module}/dist/src.zip")
   handler          = "index.handler"
-  role             = aws_iam_role.s3_credentials_lambda.arn
+  role             = aws_iam_role.s3_credentials_lambda[0].arn
   runtime          = "nodejs8.10"
   timeout          = 10
   memory_size      = 320
 
   vpc_config {
     subnet_ids         = var.subnet_ids
-    security_group_ids = var.subnet_ids == null ? null : [aws_security_group.s3_credentials_lambda[0].id]
+    security_group_ids = (var.deploy_s3_credentials_endpoint && var.vpc_id != null) ? [aws_security_group.s3_credentials_lambda[0].id] : null
   }
 
   environment {
@@ -153,74 +165,92 @@ resource "aws_lambda_function" "s3_credentials" {
       EARTHDATA_BASE_URL             = var.urs_url
       EARTHDATA_CLIENT_ID            = var.urs_client_id
       EARTHDATA_CLIENT_PASSWORD      = var.urs_client_password
-      AccessTokensTable              = aws_dynamodb_table.access_tokens.id
+      AccessTokensTable              = aws_dynamodb_table.access_tokens[0].id
       STSCredentialsLambda           = var.sts_credentials_lambda_function_arn
     }
   }
   tags = local.default_tags
 }
 
+data "aws_region" "current" {}
+
 resource "aws_lambda_permission" "lambda_permission" {
+  count            = var.deploy_s3_credentials_endpoint ? 1 : 0
+
   action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.s3_credentials.function_name
+  function_name = aws_lambda_function.s3_credentials[0].function_name
   principal     = "apigateway.amazonaws.com"
 
   # The /*/*/* part allows invocation from any stage, method and resource path
   # within API Gateway REST API.
-  source_arn = "arn:aws:execute-api:${var.region}:${data.aws_caller_identity.current.account_id}:${module.thin_egress_app.rest_api.id}/*/*/*"
+  source_arn = "arn:aws:execute-api:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:${module.thin_egress_app.rest_api.id}/*/*/*"
 }
 
 # GET /s3credentials
 resource "aws_api_gateway_resource" "s3_credentials" {
+  count = var.deploy_s3_credentials_endpoint ? 1 : 0
+
   rest_api_id = module.thin_egress_app.rest_api.id
   parent_id   = module.thin_egress_app.rest_api.root_resource_id
   path_part   = "s3credentials"
 }
 
 resource "aws_api_gateway_method" "s3_credentials" {
+  count = var.deploy_s3_credentials_endpoint ? 1 : 0
+
   rest_api_id   = module.thin_egress_app.rest_api.id
-  resource_id   = aws_api_gateway_resource.s3_credentials.id
+  resource_id   = aws_api_gateway_resource.s3_credentials[0].id
   http_method   = "GET"
   authorization = "NONE"
 }
 
 resource "aws_api_gateway_integration" "s3_credentials" {
+  count = var.deploy_s3_credentials_endpoint ? 1 : 0
+
   rest_api_id             = module.thin_egress_app.rest_api.id
-  resource_id             = aws_api_gateway_resource.s3_credentials.id
-  http_method             = aws_api_gateway_method.s3_credentials.http_method
+  resource_id             = aws_api_gateway_resource.s3_credentials[0].id
+  http_method             = aws_api_gateway_method.s3_credentials[0].http_method
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
-  uri                     = aws_lambda_function.s3_credentials.invoke_arn
+  uri                     = aws_lambda_function.s3_credentials[0].invoke_arn
 }
 
 # GET /redirect
 resource "aws_api_gateway_resource" "s3_credentials_redirect" {
+  count = var.deploy_s3_credentials_endpoint ? 1 : 0
+
   rest_api_id = module.thin_egress_app.rest_api.id
   parent_id   = module.thin_egress_app.rest_api.root_resource_id
   path_part   = "redirect"
 }
 
 resource "aws_api_gateway_method" "s3_credentials_redirect" {
+  count = var.deploy_s3_credentials_endpoint ? 1 : 0
+
   rest_api_id   = module.thin_egress_app.rest_api.id
-  resource_id   = aws_api_gateway_resource.s3_credentials_redirect.id
+  resource_id   = aws_api_gateway_resource.s3_credentials_redirect[0].id
   http_method   = "GET"
   authorization = "NONE"
 }
 
 resource "aws_api_gateway_integration" "s3_credentials_redirect" {
+  count = var.deploy_s3_credentials_endpoint ? 1 : 0
+
   rest_api_id             = module.thin_egress_app.rest_api.id
-  resource_id             = aws_api_gateway_resource.s3_credentials_redirect.id
-  http_method             = aws_api_gateway_method.s3_credentials_redirect.http_method
+  resource_id             = aws_api_gateway_resource.s3_credentials_redirect[0].id
+  http_method             = aws_api_gateway_method.s3_credentials_redirect[0].http_method
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
-  uri                     = aws_lambda_function.s3_credentials.invoke_arn
+  uri                     = aws_lambda_function.s3_credentials[0].invoke_arn
 }
 
 # API deployment
 resource "aws_api_gateway_deployment" "s3_credentials" {
+  count = var.deploy_s3_credentials_endpoint ? 1 : 0
+
   depends_on = [
-    "aws_api_gateway_integration.s3_credentials_redirect",
-    "aws_api_gateway_integration.s3_credentials"
+    aws_api_gateway_integration.s3_credentials_redirect[0],
+    aws_api_gateway_integration.s3_credentials[0]
   ]
 
   rest_api_id = module.thin_egress_app.rest_api.id
