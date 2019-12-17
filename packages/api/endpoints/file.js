907e5067dd389d31df47cb1baea1795710b5fa0d
Lauren Frederick <lpfreder@mbp-lpfreder.edn.ecs.nasa.gov>
2019-12-17 09:47:30 -0500

    prototype

---
+++ packages/api/endpoints/file.js
@@ -0,0 +1,23 @@
+'use strict';
+
+// const AWS = require('aws-sdk');
+const aws = require('@cumulus/common/aws');
+
+async function listBucketTfStateFiles(bucket) {
+  const bucketObjects = await aws.listS3ObjectsV2({ Bucket: bucket });
+
+  return bucketObjects.filter((obj) => obj.Key.includes('tfstate'));
+}
+
+async function listAllTfStateFiles() {
+  const buckets = await aws.s3().listBuckets().promise();
+
+  const bucketPromises = buckets.Buckets.map((bucket) =>
+    listBucketTfStateFiles(bucket.Name));
+
+  return Promise.all(bucketPromises);
+}
+
+module.exports = {
+  listAllTfStateFiles
+}
