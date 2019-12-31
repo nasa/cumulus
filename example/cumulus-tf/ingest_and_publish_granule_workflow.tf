ca1086e70859d77a86bd85162d14075f42da5236
Menno Van Diermen <mvandiermen@contractor.usgs.gov>
2019-12-31 11:03:07 -0600

    CUMULUS-1481 fix rogue comma

--- example/cumulus-tf/ingest_and_publish_granule_workflow.tf
+++ example/cumulus-tf/ingest_and_publish_granule_workflow.tf
@@ -219,7 +219,7 @@ module "ingest_and_publish_granule_workflow" {
             "bucket": "{$.meta.buckets.internal.name}",
             "stack": "{$.meta.stack}",
             "cmr": "{$.meta.cmr}",
-            "launchpad": "{$.meta.launchpad}",
+            "launchpad": "{$.meta.launchpad}"
           }
         }
       },
