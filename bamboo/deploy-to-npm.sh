b4a13d6c88d2fbbde94cc2d31302e4e31ba62642
Jonathan Kovarik <jonathan.kovarik@gmail.com>
2019-12-31 14:36:13 -0700

    Comment out NPM publish as step published but failed.

--- bamboo/deploy-to-npm.sh
+++ bamboo/deploy-to-npm.sh
@@ -11,15 +11,15 @@ fi
 export VERSION=$(jq --raw-output .version lerna.json)
 export NPM_TAG=$(node ./bamboo/npm-tag.js);
 
-echo "Publishing packages to NPM with version=${VERSION} and tag=${NPM_TAG}"
-export npm_config_unsafe_perm=true
-npx lerna publish \
-  ${VERSION} \
-  --no-git-tag-version \
-  --no-push \
-  --yes \
-  --force-publish=* \
-  --dist-tag=${NPM_TAG} \
-  --exact
+#echo "Publishing packages to NPM with version=${VERSION} and tag=${NPM_TAG}"
+#export npm_config_unsafe_perm=true
+#npx lerna publish \
+#  ${VERSION} \
+#  --no-git-tag-version \
+#  --no-push \
+#  --yes \
+#  --force-publish=* \
+#  --dist-tag=${NPM_TAG} \
+#  --exact
 
 . ./bamboo/create-release.sh
