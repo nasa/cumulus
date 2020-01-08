3d47fe5991a51887d686e25cc1611121b9addafa
Jonathan Kovarik <jonathan.kovarik@gmail.com>
2020-01-08 16:00:58 -0700

    re-add cleanup-integration tests.sh

--- bamboo/cleanup-integration-tests.sh
+++ bamboo/cleanup-integration-tests.sh
@@ -5,4 +5,54 @@ npm install
 . ./bamboo/set-bamboo-env-variables.sh
 . ./bamboo/abort-if-skip-integration-tests.sh
 
-echo "cleanup-integration-tests.sh is depricated"
+set +e
+(
+  set -e
+
+  cd example
+
+  # Delete the stack if it's a nightly build
+  if [ "$DEPLOYMENT" = "cumulus-nightly" ]; then
+    npm install
+    echo Delete app deployment
+
+    ./node_modules/.bin/kes cf delete \
+      --kes-folder app \
+      --region us-east-1 \
+      --stack nightly
+      --yes
+
+    echo Delete db deployment
+
+    ./node_modules/.bin/kes cf delete \
+      --kes-folder app \
+      --region us-east-1 \
+      --stack nightly-db
+      --yes
+
+    echo Delete iam deployment
+
+    ./node_modules/.bin/kes cf delete \
+      --kes-folder app \
+      --region us-east-1 \
+      --stack nightly-iam
+      --yes
+
+    echo Delete app deployment
+  else
+    rm -rf node_modules
+
+    # Needed functionality is in 1.11.3
+    # Prevents breaking on a release build when it tries to install
+    # the version that does not exist
+    # We only need the common package for the lock-stack script
+    npm install @cumulus/common@1.11.3
+  fi
+)
+RESULT=$?
+set -e
+
+echo Unlocking stack
+(cd example && node ./scripts/lock-stack.js false $DEPLOYMENT)
+
+exit $RESULT
