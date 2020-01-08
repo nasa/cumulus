fb0fc5cc9306e386fa6e579e09e9db2141cc61cc
Jonathan Kovarik <jonathan.kovarik@gmail.com>
2020-01-08 09:26:48 -0700

    fixup

--- bamboo/audit.sh
+++ bamboo/audit.sh
@@ -6,6 +6,7 @@ set -ex
 commit_message_contains_skip_audit_flag=false
 commit_matches_version_tag=false
 
+
 if [[ $(git log --pretty='format:%Creset%s' -1) =~ '[skip-audit]' ]]; then
   commit_message_contains_skip_audit_flag=true;
 fi
@@ -14,6 +15,13 @@ if [[ $(git describe --exact-match HEAD 2>/dev/null |sed -n '1p') =~ ^v[0-9]+.* 
 fi
 
 if [[ $commit_message_contains_skip_audit_flag = false && $commit_matches_version_tag = false && $SKIP_AUDIT != true ]]; then
+
+ # If flag is set, use container-cached bootstrap env
+ if [[ $USE_CACHED_BOOTSTRAP == true ]]; then
+    echo "*** Using cached bootstrap"
+    cd /cumulus/
+ fi
+
   npm run install-locks;
   npm run audit;
 else
