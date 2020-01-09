06212be7e73d2d1bd380bc710bafc77368d03a5a
Mark Boyd <markdavidboyd@gmail.com>
2020-01-09 13:12:23 -0500

    add Dynamo & StepFunctions utility classes with unit tests

---
+++ packages/aws-client/StepFunctions.js
@@ -0,0 +1,158 @@
+'use strict';
+
+/**
+ * Utility functions for working with the AWS StepFunctions API
+ * @module StepFunctions
+ *
+ * @example
+ * const StepFunctions = require('@cumulus/common/StepFunctions');
+ */
+
+const aws = require('./aws');
+
+// Utility functions
+
+const doesExecutionExist = (describeExecutionPromise) =>
+  describeExecutionPromise
+    .then(() => true)
+    .catch((err) => {
+      if (err.code === 'ExecutionDoesNotExist') return false;
+      throw err;
+    });
+
+// Exported functions
+
+/**
+ * Call StepFunctions DescribeExecution
+ *
+ * See [StepFunctions.describeExecution()](https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/StepFunctions.html#describeExecution-property)
+ * for descriptions of `params` and the return data.
+ *
+ * If a ThrottlingException is received, this function will retry using an
+ * exponential backoff.
+ *
+ * @param {Object} params
+ * @returns {Promise.<Object>}
+ *
+ * @static
+ * @kind function
+ */
+const describeExecution = aws.improveStackTrace(
+  aws.retryOnThrottlingException(
+    (params) => aws.sfn().describeExecution(params).promise()
+  )
+);
+
+/**
+ * Call StepFunctions DescribeStateMachine
+ *
+ * See [StepFunctions.describeStateMachine()](https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/StepFunctions.html#describeStateMachine-property)
+ * for descriptions of `params` and the return data.
+ *
+ * If a ThrottlingException is received, this function will retry using an
+ * exponential backoff.
+ *
+ * @param {Object} params
+ * @returns {Promise.<Object>}
+ *
+ * @static
+ * @kind function
+ */
+const describeStateMachine = aws.improveStackTrace(
+  aws.retryOnThrottlingException(
+    (params) => aws.sfn().describeStateMachine(params).promise()
+  )
+);
+
+/**
+ * Check if a Step Function Execution exists
+ *
+ * If a ThrottlingException is received, this function will retry using an
+ * exponential backoff.
+ *
+ * @param {string} executionArn - the ARN of the Step Function Execution to
+ *   check for
+ * @returns {Promise.<boolean>}
+ *
+ * @static
+ * @kind function
+ */
+const executionExists = (executionArn) =>
+  doesExecutionExist(describeExecution({ executionArn }));
+
+/**
+ * Call StepFunctions GetExecutionHistory
+ *
+ * See [StepFunctions.getExecutionHistory()](https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/StepFunctions.html#getExecutionHistory-property)
+ * for descriptions of `params` and the return data.
+ *
+ * If a ThrottlingException is received, this function will retry using an
+ * exponential backoff.
+ *
+ * @param {Object} params
+ * @returns {Promise.<Object>}
+ *
+ * @static
+ * @kind function
+ */
+const getExecutionHistory = aws.improveStackTrace(
+  aws.retryOnThrottlingException(
+    async (
+      params,
+      previousResponse = {
+        events: []
+      }
+    ) => {
+      const response = await aws.sfn().getExecutionHistory(params).promise();
+      const events = [
+        ...previousResponse.events,
+        ...response.events
+      ];
+      // If there is a nextToken, recursively call this function to get all events
+      // in the execution history.
+      if (response.nextToken) {
+        return getExecutionHistory({
+          ...params,
+          nextToken: response.nextToken
+        }, {
+          events
+        });
+      }
+      return {
+        events
+      };
+    }
+  )
+);
+
+/**
+ * Call StepFunctions ListExecutions
+ *
+ * See [StepFunctions.listExecutions()](https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/StepFunctions.html#listExecutions-property)
+ * for descriptions of `params` and the return data.
+ *
+ * If a ThrottlingException is received, this function will retry using an
+ * exponential backoff.
+ *
+ * @param {Object} params
+ * @returns {Promise.<Object>}
+ *
+ * @static
+ * @kind function
+ */
+const listExecutions = aws.improveStackTrace(
+  aws.retryOnThrottlingException(
+    (params) => aws.sfn().listExecutions(params).promise()
+  )
+);
+
+module.exports = {
+  describeExecution,
+  describeStateMachine,
+  executionExists,
+  getExecutionHistory,
+  listExecutions,
+
+  // Not part of the public API, exported for testing
+  doesExecutionExist
+};
