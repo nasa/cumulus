3f8b2018cba5aacc22f836332cb3c926b2efa091
Jonathan Kovarik <jonathan.kovarik@gmail.com>
2020-01-10 07:28:24 -0700

    Add git checkout to unit test step

--- bamboo/bootstrap-unit-tests.sh
+++ bamboo/bootstrap-unit-tests.sh
@@ -27,7 +27,9 @@ while ! docker container inspect ${container_id}\_build_env_1; do
 done
 
 ## Setup the build env container once it's started
-$docker_command "npm install --error --no-progress -g nyc; cd $UNIT_TEST_BUILD_DIR ; npm install --error  --no-progress; npm run bootstrap-no-build-quiet; npm run bootstrap-no-build-quiet"
+$docker_command "npm install --error --no-progress -g nyc; cd $UNIT_TEST_BUILD_DIR; " \
+"git fetch --all; git checkout $GIT_SHA" \
+"npm install --error  --no-progress; npm run bootstrap-no-build-quiet; npm run bootstrap-no-build-quiet"
 
 # Wait for the FTP server to be available
 while ! $docker_command  'curl --connect-timeout 5 -sS -o /dev/null ftp://testuser:testpass@127.0.0.1/README.md'; do
