55f73848dec45cd5a1654dc8be855e3052f420c6
Marc Huffnagle <marc@element84.com>
2020-01-13 13:20:52 -0500

    Simplify providerClientUtils

--- packages/ingest/providerClientUtils.js
+++ packages/ingest/providerClientUtils.js
@@ -8,40 +8,17 @@ const SftpProviderClient = require('./SftpProviderClient');
 const buildProviderClient = (providerConfig = {}) => {
   switch (providerConfig.protocol) {
   case 'ftp':
-    return new FtpProviderClient({
-      host: providerConfig.host,
-      port: providerConfig.port,
-      username: providerConfig.username,
-      password: providerConfig.password,
-      encrypted: providerConfig.encrypted,
-      userList: providerConfig.useList,
-      path: providerConfig.path
-    });
+    return new FtpProviderClient(providerConfig);
   case 'http':
   case 'https':
-    return new HttpProviderClient({
-      protocol: providerConfig.protocol,
-      host: providerConfig.host,
-      port: providerConfig.port,
-      path: providerConfig.path
-    });
+    return new HttpProviderClient(providerConfig);
   case 's3':
     return new S3ProviderClient({
       bucket: providerConfig.host,
       path: providerConfig.path
     });
   case 'sftp':
-    return new SftpProviderClient({
-      id: providerConfig.id,
-      host: providerConfig.host,
-      port: providerConfig.port,
-      username: providerConfig.username,
-      password: providerConfig.password,
-      encrypted: providerConfig.encrypted,
-      privateKey: providerConfig.privateKey,
-      cmKeyId: providerConfig.cmKeyId,
-      path: providerConfig.path
-    });
+    return new SftpProviderClient(providerConfig);
   default:
     throw new Error(`Protocol ${providerConfig.protocol} is not supported.`);
   }
