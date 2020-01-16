b534a3a704b708b5640d1f1e83e2624088dcb4a7
Jonathan Kovarik <jonathan.kovarik@gmail.com>
2020-01-16 11:10:46 -0700

    Fix nyc output execution string

--- bamboo/unit-tests.sh
+++ bamboo/unit-tests.sh
@@ -9,4 +9,4 @@ docker exec -i ${container_id}\_build_env_1 /bin/bash -c "cd $UNIT_TEST_BUILD_DI
 # Run api tests
 docker exec -i ${container_id}\_build_env_1 /bin/bash -c "cd $UNIT_TEST_BUILD_DIR/packages/api; npm run test-coverage"
 # Report combined code coverage
-docker exec -i ${container_id}\_build_env_1 /bin/bash -c "cd /source; mkdir .nyc_output; cp -a $UNIT_TEST_BUILD_DIR/.nyc_output/. /source/.nyc_output/; cp -a $BUILD_DIR/packages/api/.nyc_output/. /source/.nyc_output/; nyc report;"
+docker exec -i ${container_id}\_build_env_1 /bin/bash -c "cd /source; mkdir .nyc_output; cp -a $UNIT_TEST_BUILD_DIR/.nyc_output/. /source/.nyc_output/; cp -a $UNIT_TEST_BUILD_DIR/packages/api/.nyc_output/. /source/.nyc_output/; nyc report;"
