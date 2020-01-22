079c4eb9c189e2b62bb95bfd3331d095328378eb
Jonathan Kovarik <jonathan.kovarik@gmail.com>
2020-01-22 14:35:37 -0700

    Fix module version

--- bamboo/bootstrap-lint-audit.sh
+++ bamboo/bootstrap-lint-audit.sh
@@ -5,15 +5,15 @@ set -e
 . ./bamboo/set-bamboo-env-variables.sh
 
 
-  if [[ $USE_CACHED_BOOTSTRAP == true ]]; then ## Change into cached cumulus, pull down /cumulus ref and run there
-    echo "*** Using cached bootstrap"
-    cp .bamboo_env_vars /cumulus/
-    cd /cumulus/
-    git fetch --all
-    git checkout "$GIT_SHA"
-    rm package-lock.json || true
-  fi
+if [[ $USE_CACHED_BOOTSTRAP == true ]]; then ## Change into cached cumulus, pull down /cumulus ref and run there
+  echo "*** Using cached bootstrap"
+  cp .bamboo_env_vars /cumulus/
+  cd /cumulus/
+  git fetch --all
+  git checkout "$GIT_SHA"
+  rm package-lock.json || true
+fi
 
 npm install -g npm
-npm install
+npm install --ignore-scripts --no-package-lock
 ln -s /dev/stdout ./lerna-debug.log
