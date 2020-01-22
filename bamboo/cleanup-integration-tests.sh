5be5c0fd1465899206125609662edf1d6d9b3554
Jonathan Kovarik <jonathan.kovarik@gmail.com>
2020-01-22 07:55:58 -0700

    Update cleanup to use cache until aws-client is published

--- bamboo/cleanup-integration-tests.sh
+++ bamboo/cleanup-integration-tests.sh
@@ -9,48 +9,27 @@ set +e
   set -e
 
   cd example
-
-  # Delete the stack if it's a nightly build
-  if [ "$DEPLOYMENT" = "cumulus-nightly" ]; then
-    npm install
-    echo Delete app deployment
-
-    ./node_modules/.bin/kes cf delete \
-      --kes-folder app \
-      --region us-east-1 \
-      --stack nightly
-      --yes
-
-    echo Delete db deployment
-
-    ./node_modules/.bin/kes cf delete \
-      --kes-folder app \
-      --region us-east-1 \
-      --stack nightly-db
-      --yes
-
-    echo Delete iam deployment
-
-    ./node_modules/.bin/kes cf delete \
-      --kes-folder app \
-      --region us-east-1 \
-      --stack nightly-iam
-      --yes
-
-    echo Delete app deployment
-  else
-    rm -rf node_modules
+  rm -rf node_modules
 
     # Prevents breaking on a release build when it tries to install
     # the version that does not exist
     # We only need the common package for the lock-stack script
-    npm install @cumulus/common@1.17.0
-  fi
+    # npm install @cumulus/common@1.17.0
+    # Update post release of aws-client
 )
 RESULT=$?
 set -e
 
 echo Unlocking stack
-(npm run bootstrap-no-build && cd example && node ./scripts/lock-stack.js lock $GIT_SHA $DEPLOYMENT false)
+
+if [[ $USE_CACHED_BOOTSTRAP == true ]]; then ## Change into cached cumulus, pull down /cumulus ref and run there
+    echo "*** Using cached bootstrap"
+    cp .bamboo_env_vars /cumulus/
+    cd /cumulus/
+    git fetch --all
+    git checkout "$GIT_SHA"
+    rm package-lock.json || true
+fi
+(npm run bootstrap-no-build && npm run bootstrap-no-build && cd example && node ./scripts/lock-stack.js lock $GIT_SHA $DEPLOYMENT false)
 
 exit $RESULT
