f98f3243602e6d75ba6185a56d6eb5eb14a33e25
Jonathan Kovarik <jonathan.kovarik@gmail.com>
2020-01-24 03:55:41 -0700

    Add debug output

--- bamboo/cleanup-integration-tests.sh
+++ bamboo/cleanup-integration-tests.sh
@@ -4,18 +4,17 @@ set -ex
 . ./bamboo/set-bamboo-env-variables.sh
 . ./bamboo/abort-if-skip-integration-tests.sh
 
-cp .bamboo_env_vars /cumulus/
-cd example
 echo Unlocking stack
 
 if [[ $USE_CACHED_BOOTSTRAP == true ]]; then ## Change into cached cumulus, pull down /cumulus ref and run there
     echo "*** Using cached bootstrap"
+    cp .bamboo_env_vars /cumulus/
     cd /cumulus/
     git fetch --all
     git checkout "$GIT_SHA"
     rm package-lock.json || true
 fi
 npm install --ignore-scripts --no-package-lock
+npm --version
 (npm run bootstrap-no-build && npm run bootstrap-no-build && cd example && node ./scripts/lock-stack.js lock $GIT_SHA $DEPLOYMENT false)
-
 exit $RESULT
