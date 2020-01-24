f87135a449e75bb16f77dcab9b5fcbc124d94e09
Marc <yjpa7145@users.noreply.github.com>
2020-01-24 15:37:06 -0500

    From kbeamnsidc issue 1453 (#1443)
    * Resolves #1435 (#1436)
    
    Co-authored-by: Marc <yjpa7145@users.noreply.github.com>
    
    * Fixed #1437. Fixed #1438. (#1439)
    
    Co-authored-by: Kevin Beam <kbeam@nsidc.org>

--- tf-modules/archive/fetch_or_create_rsa_keys.sh
+++ tf-modules/archive/fetch_or_create_rsa_keys.sh
@@ -15,12 +15,13 @@ PUBLIC_KEY="${STACK}/crypto/public.pub"
 
 echo "Checking for ${STACK} private key"
 # Create private key if it doesn't exist
-if ! aws s3api head-object --bucket "$BUCKET" --key "$PRIVATE_KEY" >/dev/null 2>&1; then
+if ! aws s3api head-object --bucket "$BUCKET" --key "$PRIVATE_KEY"; then
   echo "Uploading private key for ${STACK}"
-  openssl genrsa -out /dev/stdout 2048 2>/dev/null | aws s3 cp - "s3://${BUCKET}/${PRIVATE_KEY}" >/dev/null 2>&1
+  openssl genrsa 2048 | \
+      aws s3 cp - "s3://${BUCKET}/${PRIVATE_KEY}"
 fi
 
 echo "Uploading public key for ${STACK}"
-aws s3 cp "s3://${BUCKET}/${PRIVATE_KEY}" - 2>/dev/null |\
-  openssl rsa -in /dev/stdin -outform PEM -pubout -out /dev/stdout 2>&1 |\
-  aws s3 cp - "s3://${BUCKET}/${PUBLIC_KEY}" >/dev/null 2>&1
+aws s3 cp "s3://${BUCKET}/${PRIVATE_KEY}" - |\
+  openssl rsa -outform PEM -pubout |\
+  aws s3 cp - "s3://${BUCKET}/${PUBLIC_KEY}"
