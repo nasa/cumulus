0517765dfd807fe223a4dbaafb4852ed05669dbc
Matt Savoie <savoie@nsidc.org>
2020-01-27 19:01:30 -0700

    regression in localstack bucket names.
    Looks like there was a regression in valid localstack bucketnames.

--- packages/api/tests/endpoints/test-granules.js
+++ packages/api/tests/endpoints/test-granules.js
@@ -43,7 +43,7 @@ process.env.AccessTokensTable = randomId('token');
 process.env.CollectionsTable = randomId('collection');
 process.env.GranulesTable = randomId('granules');
 process.env.stackName = randomId('stackname');
-process.env.system_bucket = randomId('system_bucket');
+process.env.system_bucket = randomId('systemBucket');
 process.env.TOKEN_SECRET = randomId('secret');
 
 // import the express app after setting the env variables
