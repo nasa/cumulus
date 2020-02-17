66a63be923c0daee485f273c742da955f8f3e3f2
Brian Tennity <brian+git@element84.com>
2020-02-17 17:46:14 -0500

    [CUMULUS-1549] Add S3 Replicator to example cumulus deployment

--- example/cumulus-tf/main.tf
+++ example/cumulus-tf/main.tf
@@ -210,3 +210,16 @@ module "s3_access_test_lambda" {
 
   tags = local.tags
 }
+
+module "s3-replicator" {
+  source = "../../tf-modules/s3-replicator"
+
+  prefix               = var.prefix
+  vpc_id               = var.vpc_id
+  subnet_ids           = var.subnet_ids
+  permissions_boundary = var.permissions_boundary_arn
+  source_bucket        = var.s3_replicator_config.source_bucket
+  source_prefix        = var.s3_replicator_config.source_prefix
+  target_bucket        = var.s3_replicator_config.target_bucket
+  target_prefix        = var.s3_replicator_config.target_prefix
+}
