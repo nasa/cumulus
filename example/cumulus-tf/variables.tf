b8ba29ef18f0fe4b7de91dea0163d90cdde506d1
Mark Boyd <markdavidboyd@gmail.com>
2020-02-19 11:51:11 -0500

    make thin_egress_jwt_secret_name var optional for example deployment

--- example/cumulus-tf/variables.tf
+++ example/cumulus-tf/variables.tf
@@ -96,11 +96,6 @@ variable "system_bucket" {
   type = string
 }
 
-variable "thin_egress_jwt_secret_name" {
-  type        = string
-  description = "Name of AWS secret where keys for the Thin Egress App JWT encode/decode are stored"
-}
-
 variable "token_secret" {
   type = string
 }
@@ -229,6 +224,12 @@ variable "archive_api_port" {
 variable "private_archive_api_gateway" {
   type = bool
   default = true
+}
+
+variable "thin_egress_jwt_secret_name" {
+  type        = string
+  description = "Name of AWS secret where keys for the Thin Egress App JWT encode/decode are stored"
+  default     = null
 }
 
 variable "metrics_es_host" {
