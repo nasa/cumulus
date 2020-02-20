5c74ac23d79e83fa6a68041900bf1333fd6c91b5
Mark Boyd <markdavidboyd@gmail.com>
2020-02-20 13:17:25 -0500

    add helpers to aws-client/CloudFormation for describing stacks and getting stack parameters

--- packages/aws-client/CloudFormation.js
+++ packages/aws-client/CloudFormation.js
@@ -1,6 +1,19 @@
 const awsServices = require('./services');
 
 /**
+ * Describes a given CloudFormation stack
+ *
+ * See https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/CloudFormation.html#describeStacks-property
+ *
+ * @param {string} stackName -  The name of the CloudFormation stack to query
+ * @returns {Array<Object>} The resources belonging to the stack
+ */
+exports.describeCfStacks = (stackName) =>
+  awsServices.cf().describeStacks({ StackName: stackName })
+    .promise()
+    .then((response) => response.Stacks[0]);
+
+/**
  * Describes the resources belonging to a given CloudFormation stack
  *
  * See https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/CloudFormation.html#describeStackResources-property
@@ -12,3 +25,24 @@ exports.describeCfStackResources = (stackName) =>
   awsServices.cf().describeStackResources({ StackName: stackName })
     .promise()
     .then((response) => response.StackResources);
+
+/**
+ * Get parameter values for the given CloudFormation stack
+ *
+ * @param {string} stackName
+ *   The name of the CloudFormation stack to query
+ * @param {Array<string>} parameterKeys
+ *   Key names for the stack parameters that you want to return
+ * @returns {Object} Object keyed by parameter names
+ */
+exports.getCfStackParameterValues = (stackName, parameterKeys = []) =>
+  exports.describeCfStacks(stackName)
+    .then((response) => {
+      const parameters = {};
+      parameterKeys.forEach((parameterKey) => {
+        const { ParameterValue } = response.Parameters
+          .find((element) => element.ParameterKey === parameterKey);
+        parameters[parameterKey] = ParameterValue;
+      });
+      return parameters;
+    });
