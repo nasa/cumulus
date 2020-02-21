8f71734d00da7ce988693a5078975619cd500870
djnewman <douglas.j.newman@nasa.gov>
2020-02-21 15:54:43 -0500

    Appending URL to UMM-G with no related urls

--- tasks/hyrax-metadata-updates/index.js
+++ tasks/hyrax-metadata-updates/index.js
@@ -5,12 +5,13 @@ const log = require('@cumulus/common/log');
 const { InvalidArgument } = require('@cumulus/errors');
 
 const {
-  buildS3Uri,
   s3ObjectExists,
-  s3PutObject
+  s3PutObject,
+  deleteS3Object
 } = require('@cumulus/aws-client/S3');
 
 const get = require('lodash.get');
+const _ = require('lodash/core');
 
 const {
   isCMRFile,
@@ -64,6 +65,52 @@ async function throwErrorIfConfigured(event) {
 }
 
 /**
+ * generateAddress
+ *
+ * @param {Object} env - the environment retrieved from configuration
+ * @throws {InvalidArgument} if the env is not valid
+ * @returns {string} - the corresponding OPeNDAP address
+ */
+function generateAddress(env) {
+  const validEnvs = ['prod', 'uat', 'sit'];
+  let envSubstition = env;
+  if (validEnvs.includes(env)) {
+    envSubstition = (env === 'prod' ? '' : `${env}.`);
+  }
+  else {
+    // Throw an exception if it is not a valid environment
+    throw new InvalidArgument(`Environment ${env} is not a valid environment.`);
+  }
+  return (`https://opendap.${envSubstition}earthdata.nasa.gov`);
+}
+
+/**
+ * generatePath
+ *
+ * @param {Object} event - the event
+ * @throws {Object} invalidArgumentException - if the env is not valid
+ * @returns {string} - the OPeNDAP path
+ */
+function generatePath(event) {
+  const config = event.config;
+  const providerId = get(config, 'provider');
+  // Check if providerId is defined
+  if (_.isUndefined(providerId)) {
+    throw new InvalidArgument('Provider not supplied in configuration. Unable to construct path');
+  }
+  const entryTitle = get(config, 'entryTitle');
+  // Check if entryTitle is defined
+  if (_.isUndefined(entryTitle)) {
+    throw new InvalidArgument('Entry Title not supplied in configuration. Unable to construct path');
+  }
+  // TODO retrieve source metadata file from event
+  const metadata = ''
+  const nativeId = 'GLDAS_CLSM025_D.2.0:GLDAS_CLSM025_D.A20141230.020.nc4';
+  const path = `providers/${providerId}/collections/${entryTitle}/granules/${nativeId}`;
+
+  return path;
+}
+/**
  * Do the work
  *
  * @param {Object} event - input from the message adapter
@@ -72,11 +119,9 @@ async function throwErrorIfConfigured(event) {
 async function updateMetadata(event) {
   await throwErrorIfConfigured(event);
 
-  var url = require('url');
-
-  const host = generateHost(get(event.config, 'environment', 'prod'));
+  const address = generateAddress(get(event.config, 'environment', 'prod'));
   const path = generatePath(event);
-  var q = new URL(host + '/' + path);
+  const q = new URL(`${address}/${path}`);
 
   return {
     result: q.href
@@ -84,60 +129,14 @@ async function updateMetadata(event) {
 }
 
 /**
- * generateAddress
- *
- * @param {Object} env - the environment retrieved from configuration
- * @throws {InvalidArgument} if the env is not valid
- * @returns {String} - the corresponding OPeNDAP address
- */
-function generateHost(env) {
-  var validEnvs = ['prod', 'uat', 'sit'];
-  if (validEnvs.includes(env)) {
-    env = (env == 'prod' ? '' : env + '.');
-  }
-  else {
-    // Throw an exception if it is not a valid environment
-    throw new InvalidArgument(`Environment ${env} is not a valid environment.`);
-  }
-  return ('https://opendap.' + env + 'earthdata.nasa.gov');
-}
-
-/**
- * generatePath
- *
- * @param {Object} event - the event
- * @throws {Object} invalidArgumentException - if the env is not valid
- * @returns {String} - the OPeNDAP path
- */
-function generatePath(event) {
-  const config = event.config;
-  const providerId = get(config, 'provider');
-  // Check if providerId is defined
-  if (typeof providerId === 'undefined') {
-    throw new InvalidArgument(`Provider not supplied in configuration. Unable to construct path`);
-  }
-  const entryTitle = get(config, 'entryTitle');
-  // Check if entryTitle is defined
-  if (typeof entryTitle === 'undefined') {
-    throw new InvalidArgument(`Entry Title not supplied in configuration. Unable to construct path`);
-  }
-  // TODO retrieve native ID from source metadata file.
-  const metadata = ''
-  const nativeId = 'GLDAS_CLSM025_D.2.0:GLDAS_CLSM025_D.A20141230.020.nc4'
-  const path = 'providers/' + providerId + '/collections/' + entryTitle + '/granules/' + nativeId;
-
-  return path;
-}
-
-/**
  * generateNativeId
  *
- * @param {String} metadataContents - the metadata
- * @returns {String} - the native id
+ * @param {string} metadata - the metadata
+ * @returns {string} - the native id
  */
 function getNativeId(metadata) {
-  
-  var metadataObject = null;
+
+  let metadataObject = null;
   try {
     // Is this UMM-G or ECHO10?
     metadataObject = JSON.parse(metadata);
@@ -147,13 +146,41 @@ function getNativeId(metadata) {
   } catch (e) {
     // ECHO10: Granule/DataGranule/ProducerGranuleId
     const parseString = require('xml2js').parseString;
-    
-    var nativeId = null;
-    parseString(metadata, function (err, result) {
-       nativeId = result.Granule.DataGranule[0].ProducerGranuleId[0];
+
+    let nativeId = null;
+    parseString(metadata, (_err, result) => {
+      nativeId = result.Granule.GranuleUR[0];
     });
     return nativeId;
   }
+}
+
+/**
+ * addHyraxUrl
+ *
+ * @param {String} metadata - the orginal metadata
+ * @param {URL} hyraxUrl - the hyrax url
+ * @returns {String} - the updated metadata containing a Hyrax URL
+ */
+function addHyraxUrl(metadata, hyraxUrl) {
+
+  let metadataObject = null;
+  try {
+    // Is this UMM-G or ECHO10?
+    metadataObject = JSON.parse(metadata);
+    // UMM-G: meta/native-id
+    if (_.isUndefined(metadataObject.umm.RelatedUrls)) {
+      metadataObject.umm.RelatedUrls = [];
+    }
+    const url = {
+      URL: hyraxUrl,
+      Type: 'GET DATA',
+      Subtype: 'OPENDAP DATA',
+      Description: 'OPeNDAP request URL'
+    };
+    metadataObject.umm.RelatedUrls.push(url);
+  } catch (e) {}
+  return JSON.stringify(metadataObject, null, 2);
 }
 
 /**
@@ -170,6 +197,7 @@ function handler(event, context, callback) {
 
 exports.handler = handler;
 exports.updateMetadata = updateMetadata; // exported to support testing
-exports.generateHost = generateHost; // exported to support testing
+exports.generateAddress = generateAddress; // exported to support testing
 exports.generatePath = generatePath; // exported to support testing
 exports.getNativeId = getNativeId; // exported to support testing
+exports.addHyraxUrl = addHyraxUrl; // exported to support testing
