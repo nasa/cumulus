b4dbeed0f8608919db10d386593d325ce618dea2
djnewman <douglas.j.newman@nasa.gov>
2020-02-21 15:22:34 -0500

    Appending URL to UMM-G

--- tasks/hyrax-metadata-updates/tests/test-index.js
+++ tasks/hyrax-metadata-updates/tests/test-index.js
@@ -6,7 +6,7 @@ const HyraxMetadataUpdate = require('..');
 
 test('Test return prod OPeNDAP host when no environment value supplied', async (t) => {
 
-  const data = await HyraxMetadataUpdate.generateHost('prod');
+  const data = await HyraxMetadataUpdate.generateAddress('prod');
 
   t.is(data, 'https://opendap.earthdata.nasa.gov');
 });
@@ -25,13 +25,13 @@ test('Test return prod OPeNDAP url when prod environment value supplied', async 
 });
 
 test('Test return sit OPeNDAP host when sit environment value supplied', async (t) => {
-  const data = await HyraxMetadataUpdate.generateHost('sit');
+  const data = await HyraxMetadataUpdate.generateAddress('sit');
 
   t.is(data, 'https://opendap.sit.earthdata.nasa.gov');
 });
 
 test('Test return uat OPeNDAP host when uat environment value supplied', async (t) => {
-  const data = await HyraxMetadataUpdate.generateHost('uat');
+  const data = await HyraxMetadataUpdate.generateAddress('uat');
 
   t.is(data, 'https://opendap.uat.earthdata.nasa.gov');
 });
@@ -39,7 +39,7 @@ test('Test return uat OPeNDAP host when uat environment value supplied', async (
 test('Test return error when invalid environment supplied for host generation', async (t) => {
 
   const error = await t.throws(
-    () => HyraxMetadataUpdate.generateHost('foo')
+    () => HyraxMetadataUpdate.generateAddress('foo')
   );
 
   t.is(error.message, 'Environment foo is not a valid environment.');
@@ -97,5 +97,38 @@ test('Test native id extraction from ECHO10', async (t) => {
 
   const result = await HyraxMetadataUpdate.getNativeId(data);
 
-  t.is(result, 'GLDAS_CLSM025_D.A20141230.020.nc4');
+  t.is(result, 'GLDAS_CLSM025_D.2.0:GLDAS_CLSM025_D.A20141230.020.nc4');
 });
+
+test('Test adding OPeNDAP URL to UMM-G file', async (t) => {
+  const data = fs.readFileSync('tests/data/umm-gin.json', 'utf8');
+  const expected = fs.readFileSync('tests/data/umm-gout.json', 'utf8');
+  const expectedObject = JSON.parse(expected);
+  const result = await HyraxMetadataUpdate.addHyraxUrl(data, 'https://opendap.earthdata.nasa.gov/providers/GES_DISC/collections/GLDAS%20Catchment%20Land%20Surface%20Model%20L4%20daily%200.25%20x%200.25%20degree%20V2.0%20(GLDAS_CLSM025_D)%20at%20GES%20DISC/granules/GLDAS_CLSM025_D.2.0:GLDAS_CLSM025_D.A20141230.020.nc4');
+
+  t.is(result, JSON.stringify(expectedObject, null, 2));
+});
+
+/* test('Test adding OPeNDAP URL to ECHO10 file', async (t) => {
+  const data = fs.readFileSync('tests/data/echo10in.xml', 'utf8');
+
+  const result = await HyraxMetadataUpdate.addHyraxUrl(data);
+
+  t.is(result, fs.readFileSync('tests/data/echo10out.xml', 'utf8'));
+}); */
+
+/* test('Test adding OPeNDAP URL to UMM-G file with no related urls', async (t) => {
+  const data = fs.readFileSync('tests/data/echo10in.xml', 'utf8');
+
+  const result = await HyraxMetadataUpdate.addHyraxUrl(data);
+
+  t.is(result, 'GLDAS_CLSM025_D.2.0:GLDAS_CLSM025_D.A20141230.020.nc4');
+});
+
+test('Test adding OPeNDAP URL to ECHO10 file with no OnlineResources', async (t) => {
+  const data = fs.readFileSync('tests/data/echo10in.xml', 'utf8');
+
+  const result = await HyraxMetadataUpdate.addHyraxUrl(data);
+
+  t.is(result, 'GLDAS_CLSM025_D.2.0:GLDAS_CLSM025_D.A20141230.020.nc4');
+}); */
