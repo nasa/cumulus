9e14beb52784d43bab8e5f64f9b77a7775482881
Jonathan Kovarik <jonathan.kovarik@gmail.com>
2020-03-02 08:59:26 -0700

    Minor refactor

--- packages/common/tests/test-LaunchpadApiClient.js
+++ packages/common/tests/test-LaunchpadApiClient.js
@@ -17,30 +17,29 @@ const CONFIG = {
   baseUrl: 'http://fakeurl'
 };
 
+test.beforeEach((t) => {
+  t.context.client = new LaunchpadApiRewire(CONFIG);
+})
 test.serial('createNewAuthToken calls launchpad.requestToken ', async (t) => {
-  const testLaunchpadApiClient = new LaunchpadApiRewire(CONFIG);
-  testLaunchpadApiClient.launchpadToken.requestToken = async () => ({ sm_token: 'launchpad response' });
-  const actual = await testLaunchpadApiClient.createNewAuthToken();
+  t.context.client.launchpadToken.requestToken = async () => ({ sm_token: 'launchpad response' });
+  const actual = await t.context.client.createNewAuthToken();
   t.is(actual, 'launchpad response');
 });
 
 test.serial('getTokenTimeLeft returns correct time left on token', async (t) => {
-  const testLaunchpadApiClient = new LaunchpadApiRewire(CONFIG);
-  testLaunchpadApiClient.launchpadToken.validateToken = async (_token) => ({
+  t.context.client.launchpadToken.validateToken = async (_token) => ({
     session_idleremaining: 50,
     session_maxremaining: 100
   });
-  const actual = await testLaunchpadApiClient.getTokenTimeLeft();
+  const actual = await t.context.client.getTokenTimeLeft();
   t.is(actual, 100);
 });
 
 test.serial('refreshAuthToken throws an error', async (t) => {
-  const testLaunchpadApiClient = new LaunchpadApiRewire(CONFIG);
-  await t.throwsAsync(testLaunchpadApiClient.refreshAuthToken());
+  await t.throwsAsync(t.context.client.refreshAuthToken());
 });
 
 test.serial('validateTokenExpiry returns true', async (t) => {
-  const testLaunchpadApiClient = new LaunchpadApiRewire(CONFIG);
-  const actual = await testLaunchpadApiClient.validateTokenExpiry();
+  const actual = await t.context.client.validateTokenExpiry();
   t.is(actual, true);
 });
