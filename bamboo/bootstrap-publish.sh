060dbcdbb4c691c4d3f92c860ab3fd747cbf5e44
Lauren Frederick <lpfreder@mbp-lpfreder.edn.ecs.nasa.gov>
2020-03-16 16:47:42 -0400

    Revert publish changes

--- bamboo/bootstrap-publish.sh
+++ bamboo/bootstrap-publish.sh
@@ -11,6 +11,5 @@ npm run bootstrap-no-build
 set +e; apt-get update; set -e;
 apt-get install -y jq rsync zip
 echo "//registry.npmjs.org/:_authToken=${NPM_TOKEN}" > ~/.npmrc
-npx lerna run prepare
 
 git config --global user.name "Bamboo CI"
