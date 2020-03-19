c0c17e1eaafa9d77242dcb47cdbf7d96a7bd6a72
Mark Boyd <markdavidboyd@gmail.com>
2020-03-19 16:03:32 -0400

    Removed `@cumulus/integration-tests/api/api.addCollectionApi()`

--- packages/integration-tests/api/api.js
+++ packages/integration-tests/api/api.js
@@ -233,31 +233,6 @@ async function getExecutionLogs({ prefix, executionName }) {
 }
 
 /**
- * Add a collection to Cumulus via the API
- *
- * @param {Object} params - params
- * @param {string} params.prefix - the prefix configured for the stack
- * @param {Object} params.collection - a collection object
- * @returns {Promise<Object>} - the POST confirmation from the API
- */
-async function addCollectionApi({ prefix, collection }) {
-  deprecate('@cumulus/integration-tests/api/api.addCollectionApi', '1.18.0', '@cumulus/integration-tests/api/collections.createCollection');
-  const response = await callCumulusApi({
-    prefix: prefix,
-    payload: {
-      httpMethod: 'POST',
-      resource: '/{proxy+}',
-      path: '/collections',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(collection)
-    }
-  });
-  return verifyCumulusApiResponse(response);
-}
-
-/**
  * Add a provider to Cumulus via the API
  *
  * @param {Object} params - params
@@ -467,7 +442,6 @@ module.exports = {
   getAsyncOperation,
   deletePdr,
   getExecutionLogs,
-  addCollectionApi,
   addProviderApi,
   getProviders,
   getCollections,
