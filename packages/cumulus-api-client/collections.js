1310efef4335a6e06e4ec01a110aa3227509a88e
Jonathan Kovarik <jonathan.kovarik@gmail.com>
2020-03-22 14:52:17 -0600

    Add collections docstrings

--- packages/cumulus-api-client/collections.js
+++ packages/cumulus-api-client/collections.js
@@ -2,6 +2,18 @@
 
 const { invokeApi } = require('./cumulusApiClient');
 
+
+/**
+ * POST /collections
+ *
+ * @param {Object} params              - params
+ * @param {string} params.prefix       - the prefix configured for the stack
+ * @param {Object} params.collection   - collection object to add to the database
+ * @param {Function} params.callback   - async function to invoke the api lambda
+ *                                     that takes a prefix / user payload.  Defaults
+ *                                     to cumulusApiClient.invokeApi
+ * @returns {Promise<Object>}          - the response from the callback
+ */
 const createCollection = async ({ prefix, collection, callback = invokeApi }) => callback({
   prefix,
   payload: {
@@ -13,6 +25,18 @@ const createCollection = async ({ prefix, collection, callback = invokeApi }) =>
   }
 });
 
+/**
+ * DELETE /collections/{vollectionName}/{collectionVersion}
+ *
+ * @param {Object} params                     - params
+ * @param {string} params.prefix              - the prefix configured for the stack
+ * @param {Object} params.collectionVersion   - name of collection to delete
+ * @param {Object} params.collectionName      - version of collection to delete
+ * @param {Function} params.callback          - async function to invoke the api lambda
+ *                                            that takes a prefix / user payload.  Defaults
+ *                                            to cumulusApiClient.invokeApi
+ * @returns {Promise<Object>}                 - the response from the callback
+ */
 const deleteCollection = async ({
   prefix, collectionName, collectionVersion, callback = invokeApi
 }) => callback({
@@ -24,6 +48,19 @@ const deleteCollection = async ({
   }
 });
 
+/**
+ * Get a collection from Cumulus via the API lambda
+ * GET /collections/{vollectionName}/{collectionVersion}
+ *
+ * @param {Object} params                     - params
+ * @param {string} params.prefix              - the prefix configured for the stack
+ * @param {Object} params.collectionVersion   - name of collection to get
+ * @param {Object} params.collectionName      - version of collection to get
+ * @param {Function} params.callback          - async function to invoke the api lambda
+ *                                              that takes a prefix / user payload.  Defaults
+ *                                              to cumulusApiClient.invokeApi
+ * @returns {Promise<Object>}                 - the response from the callback
+ */
 const getCollection = async ({
   prefix, collectionName, collectionVersion, callback = invokeApi
 }) => {
