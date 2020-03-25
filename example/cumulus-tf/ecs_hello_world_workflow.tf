fd47bd34779b4acb7d13fffa818afb8cee8dde7d
Jonathan Kovarik <jonathan.kovarik@gmail.com>
2020-03-25 12:56:50 -0600

    Fix ecs version

--- example/cumulus-tf/ecs_hello_world_workflow.tf
+++ example/cumulus-tf/ecs_hello_world_workflow.tf
@@ -12,7 +12,7 @@ module "hello_world_service" {
 
   cluster_arn                           = module.cumulus.ecs_cluster_arn
   desired_count                         = 1
-  image                                 = "cumuluss/cumulus-ecs-task:1.5.0"
+  image                                 = "cumuluss/cumulus-ecs-task:1.5.1"
   log2elasticsearch_lambda_function_arn = module.cumulus.log2elasticsearch_lambda_function_arn
 
   cpu                = 400
