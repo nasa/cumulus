c3fc4d2ed5ab5734f6c481d680dd1c2946bf2772
Mark Boyd <markdavidboyd@gmail.com>
2020-04-02 10:36:25 -0400

    add unit tests

--- packages/message/tests/test-executions.js
+++ packages/message/tests/test-executions.js
@@ -3,10 +3,30 @@
 const test = require('ava');
 
 const {
+  getExecutionUrlFromArn,
   getMessageExecutionArn,
   getMessageExecutionName,
-  getMessageStateMachineArn
+  getMessageStateMachineArn,
+  getStateMachineArnFromExecutionArn
 } = require('../executions');
+
+test('getExecutionUrlFromArn returns correct URL when no region environment variable is specified', (t) => {
+  t.is(
+    getExecutionUrlFromArn('fake-arn'),
+    'https://console.aws.amazon.com/states/home?region=us-east-1'
+      + '#/executions/details/fake-arn'
+  );
+})
+
+test.serial('getExecutionUrlFromArn returns correct URL when a region environment variable is specified', (t) => {
+  process.env.AWS_DEFAULT_REGION = 'fake-region';
+  t.is(
+    getExecutionUrlFromArn('fake-arn'),
+    'https://console.aws.amazon.com/states/home?region=fake-region'
+      + '#/executions/details/fake-arn'
+  );
+  delete process.env.AWS_DEFAULT_REGION;
+})
 
 test('getMessageExecutionName throws error if cumulus_meta.execution_name is missing', (t) => {
   t.throws(
@@ -42,3 +62,19 @@ test('getMessageExecutionArn returns null for an invalid message', (t) => {
   const executionArn = getMessageExecutionArn({});
   t.is(executionArn, null);
 });
+
+test('getStateMachineArnFromExecutionArn returns correct state machine ARN', (t) => {
+  t.is(
+    getStateMachineArnFromExecutionArn(
+      'arn:aws:states:us-east-1:000000000000:execution:fake-Workflow:abcd-1234-efgh-5678'
+    ),
+    'arn:aws:states:us-east-1:000000000000:stateMachine:fake-Workflow'
+  );
+})
+
+test('getStateMachineArnFromExecutionArn returns null for no input', (t) => {
+  t.is(
+    getStateMachineArnFromExecutionArn(),
+    null
+  );
+})
