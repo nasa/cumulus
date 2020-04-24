f4165c5312319d296d5b96000947ad0dea3888b8
Marc Huffnagle <marc@element84.com>
2020-04-24 13:53:35 -0400

    Migreate Kinesis to TS

---
+++ packages/aws-client/src/Kinesis.ts
@@ -0,0 +1,26 @@
+/**
+ * Utility functions for interacting with Kinesis
+ */
+import { kinesis } from './services';
+import pRetry = require('p-retry');
+
+/**
+ * Describe a Kinesis stream.
+ */
+export const describeStream = (
+  params: AWS.Kinesis.DescribeStreamInput,
+  retryOptions: pRetry.Options = { retries: 0 }
+) =>
+  pRetry(
+    async () => {
+      try {
+        return await kinesis().describeStream(params).promise();
+      } catch (err) {
+        if (err.code === 'ResourceNotFoundException') throw err;
+        throw new pRetry.AbortError(err);
+      }
+    },
+    { maxTimeout: 10000, ...retryOptions }
+  );
+
+export default describeStream;
