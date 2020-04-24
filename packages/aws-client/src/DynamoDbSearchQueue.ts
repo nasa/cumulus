42df8b9c652c05635292f66937497edf8a555553
Marc Huffnagle <marc@element84.com>
2020-04-24 13:47:53 -0400

    Migrate DynamoDbSearchQueue to TS

--- packages/aws-client/src/DynamoDbSearchQueue.ts
+++ packages/aws-client/src/DynamoDbSearchQueue.ts
@@ -1,12 +1,17 @@
-const awsServices = require('./services');
+import { dynamodbDocClient } from './services';
 
 // Class to efficiently search all of the items in a DynamoDB table, without
 // loading them all into memory at once.  Handles paging.
 class DynamoDbSearchQueue {
-  constructor(params, searchType = 'scan') {
+  private readonly dynamodbDocClient: AWS.DynamoDB.DocumentClient;
+  private readonly searchType: 'scan';
+  private readonly params: AWS.DynamoDB.DocumentClient.ScanInput;
+  private items: Array<AWS.DynamoDB.DocumentClient.AttributeMap|null>;
+
+  constructor(params: AWS.DynamoDB.DocumentClient.ScanInput, searchType: 'scan' = 'scan') {
     this.items = [];
     this.params = params;
-    this.dynamodbDocClient = awsServices.dynamodbDocClient();
+    this.dynamodbDocClient = dynamodbDocClient();
     this.searchType = searchType;
   }
 
@@ -15,8 +20,6 @@ class DynamoDbSearchQueue {
    *
    * This does not remove the object from the queue.  When there are no more
    * items in the queue, returns 'null'.
-   *
-   * @returns {Promise<Object>} - an item from the DynamoDB table
    */
   async peek() {
     if (this.items.length === 0) await this.fetchItems();
@@ -27,8 +30,6 @@ class DynamoDbSearchQueue {
    * Remove the next item from the queue
    *
    * When there are no more items in the queue, returns 'null'.
-   *
-   * @returns {Promise<Object>} - an item from the DynamoDB table
    */
   async shift() {
     if (this.items.length === 0) await this.fetchItems();
@@ -37,21 +38,18 @@ class DynamoDbSearchQueue {
 
   /**
    * Query the DynamoDB API to get the next batch of items
-   *
-   * @returns {Promise<undefined>} - resolves when the queue has been updated
-   * @private
    */
   async fetchItems() {
     let response;
     do {
       response = await this.dynamodbDocClient[this.searchType](this.params).promise(); // eslint-disable-line no-await-in-loop, max-len
       if (response.LastEvaluatedKey) this.params.ExclusiveStartKey = response.LastEvaluatedKey;
-    } while (response.Items.length === 0 && response.LastEvaluatedKey);
+    } while ((response.Items || []).length === 0 && response.LastEvaluatedKey);
 
-    this.items = response.Items;
+    this.items = (response.Items || []);
 
     if (!response.LastEvaluatedKey) this.items.push(null);
   }
 }
 
-module.exports = DynamoDbSearchQueue;
+export = DynamoDbSearchQueue;
