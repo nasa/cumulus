9f24330fe97068fb843d8e06fa8e47e4da8975d3
Marc Huffnagle <marc@element84.com>
2020-04-24 13:56:08 -0400

    Migrate Lambda to TS

---
+++ packages/aws-client/src/Lambda.ts
@@ -0,0 +1,23 @@
+import Logger = require('@cumulus/logger');
+import { lambda } from './services';
+import { inTestMode } from './test-utils';
+
+const log = new Logger({ sender: 'aws-client/Lambda' });
+
+export const invoke = async (name: string, payload: unknown, type = 'Event') => {
+  if (process.env.IS_LOCAL || inTestMode()) {
+    log.info(`Faking Lambda invocation for ${name}`);
+
+    return false;
+  }
+
+  log.info(`Invoking ${name}`);
+
+  return lambda().invoke({
+    FunctionName: name,
+    Payload: JSON.stringify(payload),
+    InvocationType: type
+  }).promise();
+};
+
+export default invoke;
