fea2ba6fa54680d45d5029711d254e2645ae66c9
Marc Huffnagle <marc@element84.com>
2020-05-13 15:37:07 -0400

    Add test using 300MB file

---
+++ example/spec/parallel/ingestGranule/IngestGranuleWithLargeFileSpec.js
@@ -0,0 +1,148 @@
+'use strict';
+
+const get = require('lodash/get');
+const pAll = require('p-all');
+const pick = require('lodash/pick');
+const { randomId } = require('@cumulus/common/test-utils');
+
+const { createCollection } = require('@cumulus/integration-tests/Collections');
+const {
+  findExecutionArn, getExecutionWithStatus
+} = require('@cumulus/integration-tests/Executions');
+const { getGranuleWithStatus } = require('@cumulus/integration-tests/Granules');
+const { createProvider } = require('@cumulus/integration-tests/Providers');
+const { createOneTimeRule } = require('@cumulus/integration-tests/Rules');
+
+const { deleteCollection } = require('@cumulus/api-client/collections');
+const { deleteGranule } = require('@cumulus/api-client/granules');
+const { deleteProvider } = require('@cumulus/api-client/providers');
+const { deleteRule } = require('@cumulus/api-client/rules');
+
+const { deleteS3Object, s3PutObject } = require('@cumulus/aws-client/S3');
+
+const { loadConfig } = require('../../helpers/testUtils');
+
+describe('The IngestGranule workflow ingesting a 300M file', () => {
+  let beforeAllFailed = false;
+  let collection;
+  let differentChecksumFilename;
+  let differentChecksumKey;
+  let ingestGranuleRule;
+  let granuleId;
+  let ingestGranuleExecution;
+  let newFileFilename;
+  let newFileKey;
+  let prefix;
+  let provider;
+  let sourceFilename;
+  let sameChecksumKey;
+  let secondIngestGranuleExecution;
+  let secondIngestGranuleRule;
+  let sourceBucket;
+
+  beforeAll(async () => {
+    try {
+      const config = await loadConfig();
+      prefix = config.stackName;
+      // TODO Need to dynamically get this bucket
+      sourceBucket = 'mth-internal';
+
+      // Create the collection
+      collection = await createCollection(
+        prefix,
+        {
+          duplicateHandling: 'version',
+          process: 'modis'
+        }
+      );
+
+      // Create the S3 provider
+      provider = await createProvider(prefix, { host: sourceBucket });
+
+      granuleId = randomId('granule-id-');
+
+      // Ingest the granule the first time
+      ingestGranuleRule = await createOneTimeRule(
+        prefix,
+        {
+          workflow: 'IngestGranule',
+          collection: pick(collection, ['name', 'version']),
+          provider: provider.id,
+          payload: {
+            testExecutionId: randomId('test-execution-'),
+            granules: [
+              {
+                granuleId,
+                dataType: collection.name,
+                version: collection.version,
+                files: [
+                  {
+                    name: '300M',
+                    path: ''
+                  }
+                ]
+              }
+            ]
+          }
+        }
+      );
+
+      // Find the execution ARN
+      const ingestGranuleExecutionArn = await findExecutionArn(
+        prefix,
+        (execution) => {
+          const executionId = get(execution, 'originalPayload.testExecutionId');
+          return executionId === ingestGranuleRule.payload.testExecutionId;
+        },
+        { timeout: 15 }
+      );
+
+      // Wait for the execution to be completed
+      ingestGranuleExecution = await getExecutionWithStatus({
+        prefix,
+        arn: ingestGranuleExecutionArn,
+        status: 'completed'
+      });
+
+      // Wait for the granule to be fully ingested
+      await getGranuleWithStatus({ prefix, granuleId, status: 'completed' });
+
+      console.log('ingestGranuleExecution:', JSON.stringify(ingestGranuleExecution, null, 2));
+    } catch (error) {
+      beforeAllFailed = true;
+      throw error;
+    }
+  });
+
+  it('succeeds', () => {
+    if (beforeAllFailed) fail('beforeAll() failed');
+    else {
+      // If the `beforeAll` succeeded then this will always be true, but it
+      // seemed sad to just say `expect.nothing()` after all that work.
+      expect(ingestGranuleExecution.status).toBe('completed');
+    }
+  });
+
+  afterAll(async () => {
+    // Must delete rules before deleting associated collection and provider
+    await pAll(
+      [
+        () => deleteRule({ prefix, ruleName: get(ingestGranuleRule, 'name') })
+      ],
+      { stopOnError: false }
+    ).catch(console.error);
+
+    await pAll(
+      [
+        () => deleteGranule({ prefix, granuleId }),
+        () => deleteProvider({ prefix, providerId: get(provider, 'id') }),
+        () => deleteCollection({
+          prefix,
+          collectionName: get(collection, 'name'),
+          collectionVersion: get(collection, 'version')
+        })
+      ],
+      { stopOnError: false }
+    ).catch(console.error);
+  });
+});
