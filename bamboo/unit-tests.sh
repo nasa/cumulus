bbb7d7fbdefb564f60f64136382ee4e9af19824c
Marc Huffnagle <marc@element84.com>
2020-05-27 14:41:35 -0400

    Test making unit tests faster [skip-integration-tests]

--- bamboo/unit-tests.sh
+++ bamboo/unit-tests.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 set -ex
 . ./bamboo/set-bamboo-env-variables.sh
-. ./bamboo/abort-if-not-pr.sh
-docker ps -a ## Show running containers for output logs
+# . ./bamboo/abort-if-not-pr.sh
+# docker ps -a ## Show running containers for output logs
 
 # Run unit tests (excluding integration/api tests)
-docker exec -i ${container_id}\_build_env_1 /bin/bash -c "cd $UNIT_TEST_BUILD_DIR; nyc ./node_modules/.bin/lerna run test --ignore @cumulus/api --ignore cumulus-integration-tests"
+docker exec -i ${container_id}\_build_env_1 /bin/bash -c "cd $UNIT_TEST_BUILD_DIR; nyc npm run test-a"
 # Run api tests
-docker exec -i ${container_id}\_build_env_1 /bin/bash -c "cd $UNIT_TEST_BUILD_DIR/packages/api; npm run test-coverage"
+docker exec -i ${container_id}\_build_env_1 /bin/bash -c "cd $UNIT_TEST_BUILD_DIR; nyc npm run test-b"
 # Report combined code coverage
 docker exec -i ${container_id}\_build_env_1 /bin/bash -c "cd /source; mkdir .nyc_output; cp -a $UNIT_TEST_BUILD_DIR/.nyc_output/. /source/.nyc_output/; cp -a $UNIT_TEST_BUILD_DIR/packages/api/.nyc_output/. /source/.nyc_output/; nyc report;"
