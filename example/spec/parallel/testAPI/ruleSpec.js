c4bca74580e5177ba1183a0c293f43f5dacc3dec
Lauren Frederick <lpfreder@mbp-lpfreder.edn.ecs.nasa.gov>
2020-05-28 16:19:14 -0400

    Rework test to output log messages

--- example/spec/parallel/testAPI/ruleSpec.js
+++ example/spec/parallel/testAPI/ruleSpec.js
@@ -223,25 +223,23 @@ describe('When I create a one-time rule via the Cumulus API', () => {
   });
 
   it('the rule is returned with the listed rules', async () => {
-    await expectAsync(
-      pWaitFor(
-        async () => {
-          const listRulesResponse = await rulesApi.listRules({ prefix });
+    await pWaitFor(
+      async () => {
+        const listRulesResponse = await rulesApi.listRules({ prefix });
 
-          const rules = JSON.parse(listRulesResponse.body).results;
-          console.log(`Rules meta: ${JSON.parse(listRulesResponse.body).meta}`);
+        const rules = JSON.parse(listRulesResponse.body).results;
+        console.log(`Rules meta: ${JSON.parse(listRulesResponse.body).meta}`);
 
-          const ruleNames = rules.map((x) => x.name);
-          console.log(`rule name: ${helloWorldRule.name}`);
-          console.log(`rules found: ${ruleNames}`);
+        const ruleNames = rules.map((x) => x.name);
+        console.log(`rule name: ${helloWorldRule.name}`);
+        console.log(`rules found: ${ruleNames}`);
 
-          return ruleNames.includes(helloWorldRule.name);
-        },
-        {
-          interval: 1000,
-          timeout: 60 * 1000
-        }
-      )
-    ).toBeResolved();
+        return ruleNames.includes(helloWorldRule.name);
+      },
+      {
+        interval: 1000,
+        timeout: 60 * 1000
+      }
+    );
   });
 });
