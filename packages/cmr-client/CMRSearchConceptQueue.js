0ff255463c1f78dfc36e3023e5bf126c16355c52
Lauren Frederick <lpfreder@mbp-lpfreder.edn.ecs.nasa.gov>
2020-05-29 12:14:15 -0400

    Update CMRSearchConceptQueue to use CMR function

--- packages/cmr-client/CMRSearchConceptQueue.js
+++ packages/cmr-client/CMRSearchConceptQueue.js
@@ -1,6 +1,5 @@
 'use strict';
 
-const searchConcept = require('./searchConcept');
 const CMR = require('./CMR');
 
 /**
@@ -28,16 +27,11 @@ class CMRSearchConceptQueue {
    * @param {string} params.cmrSettings - the CMR settings for the requests - the provider,
    * clientId, and either launchpad token or EDL username and password
    * @param {string} params.type - the type of search 'granule' or 'collection'
-   * @param {string} params.cmrEnvironment - optional, CMR environment to
-   *              use valid arguments are ['OPS', 'SIT', 'UAT']
    * @param {string} [params.searchParams={}] - the search parameters
    * @param {string} params.format - the result format
    */
   constructor(params = { searchParams: {} }) {
-    this.clientId = params.cmrSettings.clientId;
-    this.provider = params.cmrSettings.provider;
     this.type = params.type;
-    this.cmrEnvironment = params.cmrEnvironment;
     this.params = { provider_short_name: params.cmrSettings.provider, ...params.searchParams };
     this.format = params.format;
     this.items = [];
@@ -77,14 +71,10 @@ class CMRSearchConceptQueue {
    * @private
    */
   async fetchItems() {
-    const results = await searchConcept({
+    const results = await this.CMR.searchConcept({
       type: this.type,
-      cmrEnvironment: this.cmrEnvironment,
       searchParams: this.params,
-      previousResults: [],
-      headers: { 'Client-Id': this.clientId },
-      format: this.format,
-      recursive: false
+      format: this.format
     });
     this.items = results;
     this.params.page_num = (this.params.page_num) ? this.params.page_num + 1 : 1;
