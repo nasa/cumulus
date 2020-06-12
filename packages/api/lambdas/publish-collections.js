ff625da6a4d97be564e84ac86183075adee2c27d
Chuck Daniels <chuck@developmentseed.org>
2020-06-12 16:42:22 -0400

    Add unit tests

--- packages/api/lambdas/publish-collections.js
+++ packages/api/lambdas/publish-collections.js
@@ -3,45 +3,34 @@
 const attr = require('dynamodb-data-types').AttributeValue;
 const { publishSnsMessage } = require('@cumulus/aws-client/SNS');
 
+const publishTo = (arn) => (message) => publishSnsMessage(arn, message);
+const createSnsMessage = (record) => {
+  const { name, version } = attr.unwrap(record.dynamodb.OldImage);
+  const newImage = attr.unwrap(record.dynamodb.NewImage);
+
+  switch (record.eventName) {
+  case 'INSERT': return { event: 'Create', record: newImage };
+  case 'MODIFY': return { event: 'Update', record: newImage };
+  case 'REMOVE': return {
+    event: 'Delete',
+    record: { name, version },
+    deletedAt: Date.now()
+  };
+  default: return {};
+  }
+};
+
 /**
  * Publish SNS messages for collection reporting.
  *
  * @param {Object} event - A DynamoDB event
- * @returns {Promise}
+ * @returns {Promise<Array>} Promise of array of SNS publication results
  */
-const handler = async (event) => {
-  const topicArn = process.env.collection_sns_topic_arn;
-
-  const promisedPublishEvents = event.Records.map(
-    (record) => {
-      const message = {};
-      const newImage = attr.unwrap(record.dynamodb.NewImage);
-      const oldImage = attr.unwrap(record.dynamodb.OldImage);
-      switch (record.eventName) {
-      case 'INSERT':
-        message.event = 'Create';
-        message.record = newImage;
-        break;
-      case 'MODIFY':
-        message.event = 'Update';
-        message.record = newImage;
-        break;
-      case 'REMOVE':
-        message.event = 'Delete';
-        message.record = {
-          name: oldImage.name,
-          version: oldImage.version
-        };
-        message.deletedAt = Date.now();
-        break;
-      default:
-        break;
-      }
-      return publishSnsMessage(topicArn, message);
-    }
+const handler = (event) =>
+  Promise.all(
+    event.Records
+      .map(createSnsMessage)
+      .map(publishTo(process.env.collection_sns_topic_arn))
   );
-
-  await Promise.all(promisedPublishEvents);
-};
 
 module.exports = { handler };
---
+++ packages/api/tests/lambdas/test-publish-collections.js
@@ -0,0 +1,166 @@
+'use strict';
+
+const test = require('ava');
+const sinon = require('sinon');
+const { sns, sqs } = require('@cumulus/aws-client/services');
+const { randomString } = require('@cumulus/common/test-utils');
+const { handler } = require('../../lambdas/publish-collections');
+
+const randomCollection = () => ({
+  name: randomString(),
+  version: randomString()
+});
+
+test.before(async (t) => {
+  const topicName = randomString();
+  const { TopicArn } = await sns().createTopic({ Name: topicName }).promise();
+  const QueueName = randomString();
+  const { QueueUrl } = await sqs().createQueue({ QueueName }).promise();
+  const { Attributes: { QueueArn } } = await sqs().getQueueAttributes({
+    QueueUrl,
+    AttributeNames: ['QueueArn']
+  }).promise();
+  const { SubscriptionArn } = await sns().subscribe({
+    TopicArn,
+    Protocol: 'sqs',
+    Endpoint: QueueArn
+  }).promise();
+
+  await sns().confirmSubscription({
+    TopicArn,
+    Token: SubscriptionArn
+  }).promise();
+
+  process.env.collection_sns_topic_arn = TopicArn;
+  t.context = { QueueUrl, TopicArn };
+});
+
+test.after.always(async (t) => {
+  const { QueueUrl, TopicArn } = t.context;
+
+  Promise.all([
+    sqs().deleteQueue({ QueueUrl }).promise(),
+    sns().deleteTopic({ TopicArn }).promise()
+  ]);
+});
+
+test.serial('The publish-collections Lambda function takes a DynamoDB stream event with a single record and publishes a collection to SNS', async (t) => {
+  const { QueueUrl } = t.context;
+  const collection = randomCollection();
+  const event = {
+    Records: [
+      {
+        dynamodb: {
+          NewImage: {
+            name: { S: collection.name },
+            version: { S: collection.version }
+          }
+        },
+        eventName: 'MODIFY'
+      }
+    ]
+  };
+
+  await handler(event);
+
+  const { Messages } = await sqs().receiveMessage({
+    QueueUrl,
+    WaitTimeSeconds: 10
+  }).promise();
+
+  t.is(Messages.length, 1);
+
+  const message = JSON.parse(JSON.parse(Messages[0].Body).Message);
+
+  t.deepEqual(message, { event: 'Update', record: collection });
+});
+
+test.serial('The publish-collections Lambda function takes a DynamoDB stream event with a multiple records and publishes their collections to SNS', async (t) => {
+  const { QueueUrl } = t.context;
+  const collection1 = randomCollection();
+  const collection2 = randomCollection();
+  const event = {
+    Records: [
+      {
+        dynamodb: {
+          NewImage: {
+            name: { S: collection1.name },
+            version: { S: collection1.version }
+          }
+        },
+        eventName: 'INSERT'
+      },
+      {
+        dynamodb: {
+          NewImage: {
+            name: { S: collection2.name },
+            version: { S: collection2.version }
+          }
+        },
+        eventName: 'MODIFY'
+      }
+    ]
+  };
+
+  await handler(event);
+
+  const { Messages } = await sqs().receiveMessage({
+    QueueUrl,
+    MaxNumberOfMessages: 2,
+    WaitTimeSeconds: 10
+  }).promise();
+
+  t.is(Messages.length, 2);
+
+  const message1 = JSON.parse(JSON.parse(Messages[0].Body).Message);
+  const message2 = JSON.parse(JSON.parse(Messages[1].Body).Message);
+  const expectedMessages = [
+    { event: 'Create', record: collection1 },
+    { event: 'Update', record: collection2 }
+  ];
+
+  if (message1.event === 'Create') {
+    t.deepEqual([message1, message2], expectedMessages);
+  } else {
+    t.deepEqual([message2, message1], expectedMessages);
+  }
+});
+
+test.serial('The publish-collections Lambda function takes a DynamoDB stream event with a REMOVE record and adds a deletedAt to the SNS message', async (t) => {
+  const deletedAt = Date.now();
+  const stub = sinon.stub(Date, 'now').returns(deletedAt);
+  const { QueueUrl } = t.context;
+  const { name, version } = randomCollection();
+  const event = {
+    Records: [
+      {
+        dynamodb: {
+          OldImage: {
+            name: { S: name },
+            version: { S: version }
+          }
+        },
+        eventName: 'REMOVE'
+      }
+    ]
+  };
+
+  await handler(event);
+
+  const { Messages } = await sqs().receiveMessage({
+    QueueUrl,
+    WaitTimeSeconds: 10
+  }).promise();
+
+  t.is(Messages.length, 1);
+
+  const message = JSON.parse(JSON.parse(Messages[0].Body).Message);
+
+  t.deepEqual(message, {
+    event: 'Delete',
+    record: { name, version },
+    deletedAt
+  });
+
+  stub.restore();
+});
