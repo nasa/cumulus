2907f60884dc908241643fd962e478c22f4fae75
Lauren Frederick <lpfreder@mbp-lpfreder.edn.ecs.nasa.gov>
2020-06-15 10:44:17 -0400

    Fix unit test

--- packages/api-client/tests/test-asyncOperations.js
+++ packages/api-client/tests/test-asyncOperations.js
@@ -8,7 +8,7 @@ test.before(async (t) => {
   t.context.testPrefix = 'unitTestStack';
 });
 
-test.serial('getAsyncOperation calls the callback with the expected object and returns the parsed response', async (t) => {
+test('getAsyncOperation calls the callback with the expected object and returns the parsed response', async (t) => {
   const asyncOperationId = 'id-1234';
 
   const expected = {
@@ -32,7 +32,7 @@ test.serial('getAsyncOperation calls the callback with the expected object and r
       prefix: t.context.testPrefix,
       asyncOperationId
     });
-    t.deepEqual(result, { foo: 'bar' });
+    t.deepEqual(JSON.parse(result.body), { foo: 'bar' });
   } finally {
     revertCallback();
   }
