acc14fbb26a2b8d70e26032d4fa10c2e5ba7da31
Marc <yjpa7145@users.noreply.github.com>
2020-06-19 15:48:12 -0400

    Migrate common/cloudwatch-events to TS (#1718)

--- packages/common/src/cloudwatch-event.ts
+++ packages/common/src/cloudwatch-event.ts
@@ -1,7 +1,6 @@
-const get = require('lodash/get');
-
-const log = require('./log');
-const { isOneOf } = require('./util');
+import get from 'lodash/get';
+import * as log from './log';
+import { isOneOf } from './util';
 
 /**
  * Determine if Cloudwatch event is a Step Function event
@@ -9,7 +8,8 @@ const { isOneOf } = require('./util');
  * @param {Object} event - A Cloudwatch event
  * @returns {boolean} - True if event is a Step Function event
  */
-const isSfExecutionEvent = (event) => event.source === 'aws.states';
+export const isSfExecutionEvent = (event: { source?: string }) =>
+  event.source === 'aws.states';
 
 /**
  * Determine if Step Function is in a terminal state.
@@ -17,7 +17,7 @@ const isSfExecutionEvent = (event) => event.source === 'aws.states';
  * @param {Object} status - A Step Function execution status from a Cloudwatch event
  * @returns {boolean} - True if Step Function is in terminal state.
  */
-const isTerminalSfStatus = isOneOf([
+export const isTerminalSfStatus = isOneOf([
   'ABORTED',
   'SUCCEEDED',
   'FAILED',
@@ -30,7 +30,7 @@ const isTerminalSfStatus = isOneOf([
  * @param {Object} status - A Step Function execution status from a Cloudwatch event
  * @returns {boolean} - True if Step Function is in failed state.
  */
-const isFailedSfStatus = isOneOf([
+export const isFailedSfStatus = isOneOf([
   'ABORTED',
   'FAILED',
   'TIMED_OUT'
@@ -42,7 +42,13 @@ const isFailedSfStatus = isOneOf([
  * @param {Object} event - A Cloudwatch event
  * @returns {string} - Step Function execution status
  */
-const getSfEventStatus = (event) => get(event, 'detail.status');
+export const getSfEventStatus = (
+  event: {
+    detail?: {
+      status?: string
+    }
+  }
+): string | undefined => get(event, 'detail.status');
 
 /**
  * Get the Step Function output message from a Cloudwatch Event
@@ -52,7 +58,15 @@ const getSfEventStatus = (event) => get(event, 'detail.status');
  * @param {any} [defaultValue] - A default value for the message, if none exists
  * @returns {any} - Output message from Step Function
  */
-const getSfEventDetailValue = (event, field, defaultValue) => get(event, `detail.${field}`, defaultValue);
+export const getSfEventDetailValue = (
+  event: {
+    detail?: {
+      [key: string]: string
+    }
+  },
+  field: string,
+  defaultValue: string
+): string => get(event, `detail.${field}`, defaultValue);
 
 /**
  * Get the Step Function output message from a Cloudwatch Event
@@ -62,21 +76,20 @@ const getSfEventDetailValue = (event, field, defaultValue) => get(event, `detail
  * @param {string} defaultValue - defaultValue to fetch if no event message object is found
  * @returns {Object} - Message object from Step Function
  */
-const getSfEventMessageObject = (event, messageSource, defaultValue) => {
+export const getSfEventMessageObject = (
+  event: {
+    detail?: {
+      [key: string]: string
+    }
+  },
+  messageSource: string,
+  defaultValue: string
+) => {
   const message = getSfEventDetailValue(event, messageSource, defaultValue);
   try {
     return JSON.parse(message);
-  } catch (e) {
+  } catch (error) {
     log.error(`Could not parse '${message}'`);
-    return null;
+    return undefined;
   }
-};
-
-module.exports = {
-  getSfEventDetailValue,
-  getSfEventMessageObject,
-  getSfEventStatus,
-  isSfExecutionEvent,
-  isFailedSfStatus,
-  isTerminalSfStatus
 };
