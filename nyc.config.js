aafd97f9f16e9562f3d31fc6500b506fb2f6395b
Marc Huffnagle <marc@element84.com>
2020-07-02 11:54:20 -0400

    Properly handle nyc cleaning

--- nyc.config.js
+++ nyc.config.js
@@ -4,7 +4,7 @@ const path = require('path');
 
 module.exports = {
   all: true,
-  'no-clean': true,
+  clean: process.env.NYC_CLEAN !== 'false',
   silent: process.env.NYC_SILENT === 'true',
   'temp-dir': path.join(__dirname, '.nyc_output'),
   include: [
