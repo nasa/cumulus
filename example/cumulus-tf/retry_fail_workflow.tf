dabec838b96dd415ea32d4c0a32a1ac72843dc02
Mark Boyd <markdavidboyd@gmail.com>
2020-07-09 14:48:46 -0400

    CUMULUS-2033: Move state machine definitions to *.asl.json files (#1758)
    * start moving workflow definitions to separate JSON files
    
    * move rest of workflow definitions to separate JSON files
    
    * fix workflow definitino errors
    
    * fix bad JSON definition
    
    * fix workflow definitions

--- example/cumulus-tf/retry_fail_workflow.tf
+++ example/cumulus-tf/retry_fail_workflow.tf
@@ -7,35 +7,10 @@ module "retry_fail_workflow" {
   system_bucket   = var.system_bucket
   tags            = local.tags
 
-  state_machine_definition = <<JSON
-{
-  "Comment": "Tests Retries and Fail",
-  "StartAt": "HelloWorld",
-  "States": {
-    "HelloWorld": {
-      "Parameters": {
-        "cma": {
-          "event.$": "$",
-          "task_config": {
-            "fail": true
-          }
-        }
-      },
-      "Type": "Task",
-      "Resource": "${module.cumulus.hello_world_task.task_arn}",
-      "Retry": [
-        {
-          "ErrorEquals": [
-            "States.ALL"
-          ],
-          "IntervalSeconds": 2,
-          "BackoffRate": 2,
-          "MaxAttempts": 3
-        }
-      ],
-      "End": true
+  state_machine_definition = templatefile(
+    "${path.module}/retry_fail_workflow.asl.json",
+    {
+      hello_world_task_arn: module.cumulus.hello_world_task.task_arn
     }
-  }
-}
-JSON
+  )
 }
