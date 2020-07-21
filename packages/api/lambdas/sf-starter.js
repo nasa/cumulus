2e50c0dd51fa23e9220a4c0db13d0f6f518ede1b
Mark Boyd <markdavidboyd@gmail.com>
2020-07-21 11:48:30 -0400

    fix sf-starter code

--- packages/api/lambdas/sf-starter.js
+++ packages/api/lambdas/sf-starter.js
@@ -6,7 +6,7 @@ const get = require('lodash/get');
 const { sfn } = require('@cumulus/aws-client/services');
 const { parseSQSMessageBody } = require('@cumulus/aws-client/SQS');
 const {
-  getQueueArn,
+  getQueueUrl,
   getMaximumExecutions
 } = require('@cumulus/message/Queue');
 const { Consumer } = require('@cumulus/ingest/consumer');
@@ -51,10 +51,10 @@ function dispatch(message) {
 async function incrementAndDispatch(queueMessage) {
   const workflowMessage = JSON.parse(get(queueMessage, 'Body', '{}'));
 
-  const queueArn = getQueueArn(workflowMessage);
-  const maxExecutions = getMaximumExecutions(workflowMessage, queueArn);
+  const queueUrl = getQueueUrl(workflowMessage);
+  const maxExecutions = getMaximumExecutions(workflowMessage, queueUrl);
 
-  await incrementQueueSemaphore(queueArn, maxExecutions);
+  await incrementQueueSemaphore(queueUrl, maxExecutions);
 
   // If dispatch() fails, execution is not started and thus semaphore will
   // never be decremented for the above increment, so we decrement it
@@ -62,7 +62,7 @@ async function incrementAndDispatch(queueMessage) {
   try {
     return await dispatch(queueMessage);
   } catch (error) {
-    await decrementQueueSemaphore(queueArn);
+    await decrementQueueSemaphore(queueUrl);
     throw error;
   }
 }
