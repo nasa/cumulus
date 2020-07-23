b8bd0d32b7482b4751bfeb54db961eb235579408
Jonathan Kovarik <jonathan.kovarik@gmail.com>
2020-07-22 18:16:22 -0600

    Update got.get proxy to apply to the 'default' object
    Updates got.get proxy to apply the proxy to the 'default' object in
    got.get.     There's some weirdness in
    https://github.com/sindresorhus/got/blob/a3e171c4d43c8bbb236ef3a04feb25ba6fdf6c49/source/create.ts
     with aliasing the .get method that proxyquire doesn't handle.

--- packages/tea-map-cache/tests/test-tea.js
+++ packages/tea-map-cache/tests/test-tea.js
@@ -4,42 +4,48 @@ const test = require('ava');
 const proxyquire = require('proxyquire').noPreserveCache();
 
 test('getTeaBucketPath returns a mapped bucket path on an expected response from TEA', async (t) => {
-  const { getTeaBucketPath } = proxyquire('../dist/tea', {
+  const { getTeaBucketPath } = proxyquire.load('../dist/tea', {
     got: {
-      get: async () => ({ body: '["fake-bucket-redirect/path"]' })
+      default: {
+        get: async (_param) => ({ body: '["fake-bucket-redirect/path"]' })
+      }
     }
   });
 
   const actual = await getTeaBucketPath({
     bucket: 'notUsedTestValue',
-    teaEndPoint: 'notUsedTestValue'
+    teaEndPoint: 'https://127.0.0.1/'
   });
   t.is('fake-bucket-redirect/path', actual);
 });
 
 test('getTeaBucketPath throws error if multiple paths are returned', async (t) => {
-  const { getTeaBucketPath } = proxyquire('../dist/tea', {
+  const { getTeaBucketPath } = proxyquire.load('../dist/tea', {
     got: {
-      get: async () => ({ body: '["fake-bucket-redirect/path", "some-other-path"]' })
+      default: {
+        get: async (_param) => ({ body: '["fake-bucket-redirect/path", "some-other-path"]' })
+      }
     }
   });
   await t.throwsAsync(() => getTeaBucketPath({
     bucket: 'notUsedTestValue',
-    teaEndPoint: 'notUsedTestValue'
+    teaEndPoint: 'https://127.0.0.1/'
   }));
 });
 
 test('getTeaBucketPath returns empty string if TEA throws a 404', async (t) => {
-  const { getTeaBucketPath } = proxyquire('../dist/tea', {
+  const { getTeaBucketPath } = proxyquire.load('../dist/tea', {
     got: {
-      get: async () => {
-        const error = new Error('Response code 404 (Not Found)');
-        error.statusCode = 404;
-        error.name = 'HTTPError';
-        error.response = {
-          body: 'No route defined for test-bucket'
-        };
-        throw error;
+      default: {
+        get: async () => {
+          const error = new Error('Response code 404 (Not Found)');
+          error.statusCode = 404;
+          error.name = 'HTTPError';
+          error.response = {
+            body: 'No route defined for test-bucket'
+          };
+          throw error;
+        }
       }
     }
   });
@@ -48,16 +54,18 @@ test('getTeaBucketPath returns empty string if TEA throws a 404', async (t) => {
 });
 
 test('getTeaBucketPath throws an error empty string if non-bucket-search 404 is thrown', async (t) => {
-  const { getTeaBucketPath } = proxyquire('../dist/tea', {
+  const { getTeaBucketPath } = proxyquire.load('../dist/tea', {
     got: {
-      get: async () => {
-        const error = new Error('Response code 404 (Not Found)');
-        error.statusCode = 404;
-        error.name = 'HTTPError';
-        error.response = {
-          body: 'Some other error page from API gateway/etc'
-        };
-        throw error;
+      default: {
+        get: async () => {
+          const error = new Error('Response code 404 (Not Found)');
+          error.statusCode = 404;
+          error.name = 'HTTPError';
+          error.response = {
+            body: 'Some other error page from API gateway/etc'
+          };
+          throw error;
+        }
       }
     }
   });
