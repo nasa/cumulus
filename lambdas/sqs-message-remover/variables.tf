4ddf0e2f969ae00b52c0cf63fbaf74be80401915
Mark Boyd <markdavidboyd@gmail.com>
2020-08-04 11:28:42 -0400

    Move sqs-message-remover lambda out of API package (#1792)
    * initial creation of sqs-message-remover-lambda module
    
    * add unit tests
    
    * remove sqs-message-remover lambda from API package
    
    * update lambda deployment config
    
    * move sqs-message-remover lambda module to lambdas/ folder
    
    * move sqs message remover lambda deployment into ingest module
    
    * fix module outputs
    
    * add lambdas/ folder to lerna config
    
    * move index file to src/
    
    * fix reference to lambda module
    
    * fix reference to security groups
    
    * update release script to include lambda modules
    
    * fix unit tests
    
    * update npm scripts
    
    * update package versions
    
    * add ava config to package
    
    * update main entry of package
    
    * unset process.env.NODE_ENV for webpack build

---
+++ lambdas/sqs-message-remover/variables.tf
@@ -0,0 +1,41 @@
+# Required
+
+variable "prefix" {
+  description = "The unique prefix for your deployment resources"
+  type        = string
+}
+
+variable "cmr_environment" {
+  description = "Environment that should be used for CMR requests (e.g. 'UAT', 'SIT')"
+  type        = string
+}
+
+variable "system_bucket" {
+  description = "The name of the S3 bucket to be used for staging deployment files"
+  type        = string
+}
+
+variable "lambda_processing_role_arn" {
+  description = "Name of IAM role assumed when executing Lambda"
+  type = string
+}
+
+# Optional
+
+variable "lambda_subnet_ids" {
+  description = "Subnet IDs for Lambdas"
+  type        = list(string)
+  default     = null
+}
+
+variable "security_group_ids" {
+  description = "Security Group IDs for Lambdas"
+  type        = list(string)
+  default     = null
+}
+
+variable "tags" {
+  description = "Tags to be applied to Cumulus resources that support tags"
+  type        = map(string)
+  default     = {}
+}
