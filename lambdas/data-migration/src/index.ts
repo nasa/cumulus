d683c56a1c99b78225383d57bf71466139f857d0
Mark Boyd <markdavidboyd@gmail.com>
2020-08-17 12:41:27 -0400

    start writing collections data migration

--- lambdas/data-migration/src/index.ts
+++ lambdas/data-migration/src/index.ts
@@ -1,15 +1,53 @@
 import Knex from 'knex';
+import DynamoDbSearchQueue from '@cumulus/aws-client/DynamoDbSearchQueue';
 
-const knex = Knex({
-  client: 'pg',
-  connection: {
-    host: process.env.PG_HOST,
-    user: process.env.PG_USER,
-    password: process.env.PG_PASSWORD,
-    database: process.env.PG_DATABASE,
-  },
-  asyncStackTraces: true,
+export interface HandlerEvent {
+  env?: NodeJS.ProcessEnv
+}
+
+const getRequiredEnvVar = (name: string, env: NodeJS.ProcessEnv): string => {
+  const value = env?.[name];
+
+  if (value) return value;
+
+  throw new Error(`The ${name} environment variable must be set`);
+};
+
+export const migrateCollections = async (env: NodeJS.ProcessEnv) => {
+  const collectionsTable = getRequiredEnvVar('CollectionsTable', env);
+  const dbSearchQueue = new DynamoDbSearchQueue({
+    TableName: collectionsTable,
+  }, 'scan');
+  let result = await dbSearchQueue.peek();
+  while (result) {
+    console.log(result);
+    await dbSearchQueue.shift();
+    result = await dbSearchQueue.peek();
+  }
+  // return knex('collections').insert(data);
+};
+
+const getConnectionConfig = (env: NodeJS.ProcessEnv): Knex.PgConnectionConfig => ({
+  host: getRequiredEnvVar('PG_HOST', env),
+  user: getRequiredEnvVar('PG_USER', env),
+  // TODO Get this value from secrets manager
+  password: getRequiredEnvVar('PG_PASSWORD', env),
+  database: getRequiredEnvVar('PG_DATABASE', env),
 });
 
-export const createCollection = (data: object) =>
-  knex('collections').insert(data);
+export const handler = async (event: HandlerEvent): Promise<void> => {
+  const env = event?.env ?? process.env;
+
+  const knex = Knex({
+    client: 'pg',
+    connection: getConnectionConfig(env),
+    debug: env?.KNEX_DEBUG === 'true',
+    asyncStackTraces: env?.KNEX_ASYNC_STACK_TRACES === 'true',
+  });
+
+  try {
+    await migrateCollections(env);
+  } finally {
+    await knex.destroy();
+  }
+};
--- lambdas/data-migration/tests/test-index.js
+++ lambdas/data-migration/tests/test-index.js
@@ -1,9 +1,80 @@
 const test = require('ava');
-const { createCollection } = require('..');
+const cryptoRandomString = require('crypto-random-string');
 
-test('createCollection', async (t) => {
-  await t.notThrowsAsync(createCollection({
-    name: 'foo',
-    version: '2',
-  }));
+const {
+  createAndWaitForDynamoDbTable,
+  deleteAndWaitForDynamoDbTableNotExists
+} = require('@cumulus/aws-client/DynamoDb');
+const { dynamodbDocClient } = require('@cumulus/aws-client/services');
+
+const { migrateCollections } = require('..');
+
+const generateFakeCollection = (params) => ({
+  name: `${cryptoRandomString({ length: 10 })}collection`,
+  version: '0.0.0',
+  duplicateHandling: 'replace',
+  granuleId: '^MOD09GQ\\.A[\\d]{7}\.[\\S]{6}\\.006\\.[\\d]{13}$',
+  granuleIdExtraction: '(MOD09GQ\\.(.*))\\.hdf',
+  sampleFileName: 'MOD09GQ.A2017025.h21v00.006.2017034065104.hdf',
+  files: [{ regex: 'fake-regex ', name: 'file.name' }],
+  meta: { foo: 'bar', key: { value: 'test' } },
+  ...params,
+});
+
+const batchWriteItems = (tableName, items) =>
+  dynamodbDocClient().batchWrite({
+    RequestItems: { [tableName]: items },
+  }).promise();
+
+test.before(async () => {
+  process.env.CollectionsTable = cryptoRandomString({ length: 10 });
+
+  const collectionsTableHash = { name: 'name', type: 'S' };
+  const collectionsTableRange = { name: 'version', type: 'S' };
+  await createAndWaitForDynamoDbTable({
+    TableName: process.env.CollectionsTable,
+    AttributeDefinitions: [{
+      AttributeName: collectionsTableHash.name,
+      AttributeType: collectionsTableHash.type,
+    }, {
+      AttributeName: collectionsTableRange.name,
+      AttributeType: collectionsTableRange.type,
+    }],
+    KeySchema: [{
+      AttributeName: collectionsTableHash.name,
+      KeyType: 'HASH',
+    }, {
+      AttributeName: collectionsTableRange.name,
+      KeyType: 'RANGE',
+    }],
+    ProvisionedThroughput: {
+      ReadCapacityUnits: 5,
+      WriteCapacityUnits: 5,
+    },
+  });
+});
+
+test.after.always(async () => {
+  await deleteAndWaitForDynamoDbTableNotExists({
+    TableName: process.env.CollectionsTable,
+  });
+});
+
+test('migrateCollections', async (t) => {
+  const items = [{
+    PutRequest: {
+      Item: generateFakeCollection(),
+    },
+  }, {
+    PutRequest: {
+      Item: generateFakeCollection(),
+    },
+  }];
+  try {
+    await batchWriteItems(process.env.CollectionsTable, items);
+    await migrateCollections(process.env);
+    t.pass();
+  } catch (err) {
+    t.fail();
+  }
 });
