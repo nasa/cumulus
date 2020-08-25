336b0727aa5fcacafc7e6ef2cc3f43a157431c24
Jonathan Kovarik <jonathan.kovarik@gmail.com>
2020-08-25 09:21:52 -0600

    Make rds_admin_access_secret_id descriptions consistent

--- lambdas/db-provision-user-database/variables.tf
+++ lambdas/db-provision-user-database/variables.tf
@@ -24,7 +24,7 @@ variable "rds_user_password" {
 }
 
 variable "rds_admin_access_secret_id" {
-  description = "AWS Secrets Manager secret ID containing jsonified db credentials containing at least host, password, port"
+  description = "AWS Secrets Manager secret ID containing a JSON string of DB credentials (containing at least host, password, port as keys)"
   type = string
 }
 
