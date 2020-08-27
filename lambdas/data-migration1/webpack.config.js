ef1b5f55f1b4692620fa168e29a6865641d284ad
Mark Boyd <markdavidboyd@gmail.com>
2020-08-27 13:16:52 -0400

    rename data-migration and update build process to use wwebpack

---
+++ lambdas/data-migration1/webpack.config.js
@@ -0,0 +1,70 @@
+const path = require('path');
+const { IgnorePlugin } = require('webpack');
+
+// path to module root
+const root = path.resolve(__dirname);
+
+const ignoredPackages = [
+  'mssql',
+  'mssql/lib/base',
+  'mssql/package.json',
+  'mysql',
+  'mysql2',
+  'oracledb',
+  'pg-native',
+  'pg-query-stream',
+  'sqlite3',
+  'tedious'
+];
+
+module.exports = {
+  plugins: [
+    new IgnorePlugin(new RegExp(`^(${ignoredPackages.join('|')})$`)),
+  ],
+  mode: process.env.PRODUCTION ? 'production' : 'development',
+  entry: './src/index.ts',
+  output: {
+    libraryTarget: 'commonjs2',
+    filename: 'index.js',
+    devtoolModuleFilenameTemplate: (info) => {
+      const relativePath = path.relative(root, info.absoluteResourcePath)
+      return `webpack://${relativePath}`;
+    }
+  },
+  externals: [
+    'aws-sdk',
+    'electron',
+    {'formidable': 'url'}
+  ],
+  module: {
+    // rules: [
+    //   {
+    //     test: /\.js$/,
+    //     exclude: /node_modules/,
+    //     use: [
+    //       {
+    //         loader: 'babel-loader',
+    //         options: {
+    //           cacheDirectory: true
+    //         },
+    //       },
+    //     ],
+    //   },
+    // ],
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [ '.tsx', '.ts', '.js' ],
+  },
+  devtool: 'inline-source-map',
+  target: 'node',
+  optimization: {
+    nodeEnv: false
+  }
+};
