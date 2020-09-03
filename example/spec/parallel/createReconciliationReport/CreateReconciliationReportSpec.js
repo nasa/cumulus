a0db11a2508566a16a5cb4b58f8feca810240ff4
Matt Savoie <savoie@nsidc.org>
2020-09-03 16:37:35 -0600

    removes extra error logging.

--- example/spec/parallel/createReconciliationReport/CreateReconciliationReportSpec.js
+++ example/spec/parallel/createReconciliationReport/CreateReconciliationReportSpec.js
@@ -324,7 +324,6 @@ describe('When there are granule differences and granule reconciliation is run',
     } catch (error) {
       console.log(error);
       beforeAllFailed = true;
-      console.error(error);
       throw error;
     }
   });
