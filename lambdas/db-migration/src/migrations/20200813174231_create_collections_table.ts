8550c165b0e33549d6cbe659f7d5d8a0d53f2c85
Jonathan Kovarik <jonathan.kovarik@gmail.com>
2020-09-04 16:39:44 -0600

    Respond to PR feedback

--- lambdas/db-migration/src/migrations/20200813174231_create_collections_table.ts
+++ lambdas/db-migration/src/migrations/20200813174231_create_collections_table.ts
@@ -2,34 +2,60 @@ import * as Knex from 'knex';
 
 export const up = async (knex: Knex): Promise<void> =>
   knex.schema.createTable('collections', (table) => {
-    table.increments('cumulusId').primary();
-    table.text('name').comment('Collection short_name registered with the CMR').notNullable();
-    table.text('version').comment('The version registered with the CMR').notNullable();
-    table.text('sampleFileName').comment('Example filename for this collection').notNullable();
-    table.text('granuleIdValidationRegex').notNullable().comment(
-      'The regular expression used to validate the granule ID extracted from filenames '
-      + 'according to the granuleIdExtraction'
-    );
-    table.text('granuleIdExtractionRegex').comment('The regular expression used to extract the granule ID from filenames').notNullable();
-    table.jsonb('files').comment('List of collection file definitions').notNullable();
-    table.text('process').comment('Name of the docker process to be used, e.g. modis, aster');
-    table.text('url_path').comment('The folder (url) used to save granules on S3 buckets');
-    table.enum('duplicateHandling', ['error', 'replace', 'skip', 'version']).comment(
-      'Duplicate handling behavior for this collection'
-    );
-    table.boolean('reportToEms').comment('Flag to set if this granule should be reported to EMS');
-    table.boolean('ignoreFilesConfigForDiscovery').comment(
-      'When true, ignore the collection files config list for determining which files to ingest for a granule'
-      + 'When false, ingest only files that match a regex in the colletion files config list'
-    );
-    table.jsonb('meta').comment('Collection meta object');
-    table.jsonb('tags').comment('JSON encoded array of collection tags');
+    table
+      .increments('cumulusId').primary();
+    table
+      .text('name')
+      .comment('Collection short_name registered with the CMR')
+      .notNullable();
+    table
+      .text('version')
+      .comment('The version registered with the CMR')
+      .notNullable();
+    table
+      .text('sampleFileName')
+      .comment('Example filename for this collection')
+      .notNullable();
+    table.text('granuleIdValidationRegex')
+      .notNullable()
+      .comment('The regular expression used to validate the granule ID extracted from filenames according to the granuleIdExtraction');
+    table.text('granuleIdExtractionRegex')
+      .comment('The regular expression used to extract the granule ID from filenames')
+      .notNullable();
+    table
+      .jsonb('files')
+      .comment('List of collection file definitions')
+      .notNullable();
+    table
+      .text('process')
+      .comment('Name of the docker process to be used, e.g. modis, aster');
+    table
+      .text('url_path')
+      .comment('The folder (url) used to save granules on S3 buckets');
+    table
+      .enum('duplicateHandling', ['error', 'replace', 'skip', 'version'])
+      .comment(
+        'Duplicate handling behavior for this collection'
+      );
+    table
+      .boolean('reportToEms')
+      .comment('Flag to set if this granule should be reported to EMS');
+    table
+      .boolean('ignoreFilesConfigForDiscovery')
+      .comment('When true, ignore the collection files config list for determining which files to ingest for a granule. When false, ingest only files that match a regex in the colletion files config list');
+    table
+      .jsonb('meta')
+      .comment('Collection meta object');
+    table
+      .jsonb('tags')
+      .comment('JSON encoded array of collection tags');
     // adds "created_at" and "updated_at" columns automatically
-    table.timestamps(false, true);
-
+    table
+      .timestamps(false, true);
     // add unique constraints
-    table.unique(['name', 'version']);
+    table
+      .unique(['name', 'version']);
   });
 
 export const down = async (knex: Knex): Promise<void> =>
-  knex.schema.dropTable('collections');
+  knex.schema.dropTableIfExists('collections');
--- lambdas/db-migration/src/migrations/20200901060127_create_files.ts
+++ lambdas/db-migration/src/migrations/20200901060127_create_files.ts
@@ -1,23 +1,52 @@
 import * as Knex from 'knex';
 
-export const up = async (knex: Knex): Promise<void> => knex.schema
-  .createTable('files', (table) => {
-    table.bigIncrements('cumulusId').comment('Internal Cumulus ID for a file').primary();
-    table.integer('granuleCumulusId').references('cumulusId').inTable('granules').notNullable();
-    table.timestamps(false, true);
-    table.integer('fileSize').comment('Deprecated - size of file');
-    table.integer('size').comment('Size of file (bytes)');
-    table.text('bucket').comment('AWS Bucket file is archived in');
-    table.text('checksumType').comment('Type of file checksum (e.g. md5');
-    table.text('checksumValue').comment('File checksum');
-    table.text('filename');
-    table.text('fileName').comment('Source file name');
-    table.text('key').comment('AWS S3 key file is archived at');
-    table.text('name');
-    table.text('path').comment('Source file path');
-    table.text('source').comment('Full source path s3/ftp/sftp/http URI to granule');
-    table.unique(['bucket', 'key']);
+export const up = async (knex: Knex): Promise<void> =>
+  knex.schema.createTable('files', (table) => {
+    table
+      .bigIncrements('cumulusId')
+      .comment('Internal Cumulus ID for a file')
+      .primary();
+    table
+      .integer('granuleCumulusId')
+      .references('cumulusId')
+      .inTable('granules')
+      .notNullable();
+    table
+      .timestamps(false, true);
+    table
+      .integer('fileSize')
+      .comment('Deprecated - size of file');
+    table
+      .integer('size')
+      .comment('Size of file (bytes)');
+    table
+      .text('bucket')
+      .comment('AWS Bucket file is archived in');
+    table
+      .text('checksumType')
+      .comment('Type of file checksum (e.g. md5');
+    table
+      .text('checksumValue')
+      .comment('File checksum');
+    table
+      .text('filename');
+    table
+      .text('fileName')
+      .comment('Source file name');
+    table
+      .text('key')
+      .comment('AWS S3 key file is archived at');
+    table
+      .text('name');
+    table
+      .text('path')
+      .comment('Source file path');
+    table
+      .text('source')
+      .comment('Full source path s3/ftp/sftp/http URI to granule');
+    table
+      .unique(['bucket', 'key']);
   });
 
 export const down = async (knex: Knex): Promise<void> => knex.schema
-  .dropTable('files');
+  .dropTableIfExists('files');
