7748b75fefae53aab2f69d6f3ddf265927ff06f3
Marc Huffnagle <marc@element84.com>
2020-09-29 15:24:10 -0400

    Migrate index to TS

---
+++ packages/cmr-client/src/index.ts
@@ -0,0 +1,6 @@
+export { CMR } from './CMR';
+
+/* eslint-disable @typescript-eslint/no-unused-vars */
+export import CMRSearchConceptQueue = require('./CMRSearchConceptQueue');
+export import ValidationError = require('./ValidationError');
+/* eslint-enable @typescript-eslint/no-unused-vars */
