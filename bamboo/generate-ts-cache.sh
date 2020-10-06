7397c82631cccf88e927be593eea701a70fcd5d3
Mark Boyd <markdavidboyd@gmail.com>
2020-10-06 12:42:43 -0400

    add bamboo script to generate TS build cache

---
+++ bamboo/generate-ts-cache.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -ex
+
+PWD=$(pwd)
+
+# Bootstrap to generate the compiled files from TS
+npm run bootstrap-no-build
+
+# Get a list of TS compiled files and generate a cache artifact
+npm run tsc:listEmittedFiles --silent \
+  | grep TSFILE \
+  | awk '{print $2}' \
+  | sed "s,$PWD/,,g" \
+  | tar cf ts-build-cache.tgz -T -
