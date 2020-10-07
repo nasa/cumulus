0611b05ecb4a0bca5d43f29f5f32385fdbf85ea1
Mark Boyd <markdavidboyd@gmail.com>
2020-10-07 17:47:53 -0400

    testing

--- bamboo/extract-ts-build-cache.sh
+++ bamboo/extract-ts-build-cache.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -ex
 
+. ./bamboo/set-bamboo-env-variables.sh
+. ./bamboo/abort-if-not-pr.sh
+
 if [[ $USE_CACHED_BOOTSTRAP == true ]]; then
   echo "*** Using cached bootstrap build dir"
   cd /cumulus/
