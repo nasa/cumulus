ec4f78001a8868df6799ed1c07b3ac7be27bba43
Marc <yjpa7145@users.noreply.github.com>
2020-10-14 17:17:03 -0400

    Migrate FtpProviderClient to TS (#1905)
    * Migrate ingest/consumer to TS
    
    * Migrate empty provider connect end mixin to TS
    
    * Migrate S3ProviderClient to TS
    
    * Update packages/aws-client/src/SQS.ts
    
    Co-authored-by: Mark Boyd <markdavidboyd@gmail.com>
    
    * Remove EmptyProviderConnectEndMixin
    
    * Rename variables
    
    * Fix refactoring error
    
    * Use sinon stubs in test
    
    * Migrate FtpProviderClient to TS
    
    * Refactor JSFtpError
    
    * Remove commented out code
    
    * Address PR feedback
    
    * fix lint issue
    
    * fix FtpProviderClient code
    
    Co-authored-by: Mark Boyd <markdavidboyd@gmail.com>

--- packages/ingest/src/FtpProviderClient.ts
+++ packages/ingest/src/FtpProviderClient.ts
@@ -1,63 +1,98 @@
-'use strict';
+import JSFtp, { ListError } from 'jsftp';
+import { PassThrough } from 'stream';
+import isNil from 'lodash/isNil';
+import * as S3 from '@cumulus/aws-client/S3';
+import Logger from '@cumulus/logger';
+import { Socket } from 'net';
+import { recursion } from './recursion';
+import { lookupMimeType, decrypt } from './util';
+import { FtpProviderClientListItem, ProviderClientListItem } from './types';
 
-const get = require('lodash/get');
-const JSFtp = require('jsftp');
-const { PassThrough } = require('stream');
-const log = require('@cumulus/common/log');
-const omit = require('lodash/omit');
-const S3 = require('@cumulus/aws-client/S3');
-const isNil = require('lodash/isNil');
-const { recursion } = require('./recursion');
-const { lookupMimeType, decrypt } = require('./util');
+interface FtpProviderClientConstructorParams {
+  host: string;
+  port?: number;
+  useList?: boolean;
+  username?: string;
+  password?: string;
+  encrypted?: boolean;
+}
+
+function isJSFtpError(error: Error | ListError): error is ListError {
+  return (error as ListError).text !== undefined && !(error as Error).message;
+}
+
+const logger = new Logger({ sender: '@cumulus/ingest/FtpProviderClient' });
 
 class FtpProviderClient {
+  private readonly providerConfig: FtpProviderClientConstructorParams;
+  private readonly host: string;
+  private ftpClient?: JSFtp;
+  private plaintextUsername?: string;
+  private plaintextPassword?: string;
+
   // jsftp.ls is called in _list and uses 'STAT' as a default. Some FTP
   // servers return inconsistent results when using
   // 'STAT' command. We can use 'LIST' in those cases by
   // setting the variable `useList` to true
-  constructor(providerConfig) {
+  constructor(providerConfig: FtpProviderClientConstructorParams) {
     this.providerConfig = providerConfig;
     this.host = providerConfig.host;
 
-    if (get(providerConfig, 'encrypted', false) === false) {
-      this.plaintextUsername = get(providerConfig, 'username', 'anonymous');
-      this.plaintextPassword = get(providerConfig, 'password', 'password');
+    if ((providerConfig.encrypted ?? false) === false) {
+      this.plaintextUsername = providerConfig.username ?? 'anonymous';
+      this.plaintextPassword = providerConfig.password ?? 'password';
     }
   }
 
-  async getUsername() {
+  async getUsername(): Promise<string> {
     if (!this.plaintextUsername) {
+      if (!this.providerConfig.username) {
+        throw new Error('username not set');
+      }
+
       this.plaintextUsername = await decrypt(this.providerConfig.username);
+
+      if (!this.plaintextUsername) {
+        throw new Error('Unable to decrypt username');
+      }
     }
     return this.plaintextUsername;
   }
 
-  async getPassword() {
+  async getPassword(): Promise<string> {
     if (!this.plaintextPassword) {
+      if (!this.providerConfig.password) {
+        throw new Error('password not set');
+      }
+
       this.plaintextPassword = await decrypt(this.providerConfig.password);
+
+      if (!this.plaintextPassword) {
+        throw new Error('Unable to decrypt password');
+      }
     }
     return this.plaintextPassword;
   }
 
-  async buildFtpClient() {
+  async buildFtpClient(): Promise<JSFtp> {
     if (isNil(this.ftpClient)) {
       this.ftpClient = new JSFtp({
         host: this.host,
-        port: get(this.providerConfig, 'port', 21),
+        port: this.providerConfig.port ?? 21,
         user: await this.getUsername(),
         pass: await this.getPassword(),
-        useList: get(this.providerConfig, 'useList', false),
+        useList: this.providerConfig.useList ?? false,
       });
     }
     return this.ftpClient;
   }
 
-  errorHandler(rejectFn, error) {
+  errorHandler(rejectFn: (reason?: any) => void, error: Error | ListError): void {
     let normalizedError = error;
     // error.text is a product of jsftp returning an object with a `text` field to the callback's
     // `err` param, but normally javascript errors have a `message` field. We want to normalize
     // this before throwing it out of the `FtpProviderClient` because it is a quirk of jsftp.
-    if (!error.message && error.text) {
+    if (isJSFtpError(error)) {
       const message = `${error.code
         ? `FTP Code ${error.code}: ${error.text}`
         : `FTP error: ${error.text}`} This may be caused by user permissions disallowing the listing.`;
@@ -66,7 +101,7 @@ class FtpProviderClient {
     if (!isNil(this.ftpClient)) {
       this.ftpClient.destroy();
     }
-    log.error('FtpProviderClient encountered error: ', normalizedError);
+    logger.error('FtpProviderClient encountered error: ', normalizedError);
     return rejectFn(normalizedError);
   }
 
@@ -77,9 +112,9 @@ class FtpProviderClient {
    * @param {string} localPath - the full local destination file path
    * @returns {Promise.<string>} - the path that the file was saved to
    */
-  async download(remotePath, localPath) {
+  async download(remotePath: string, localPath: string): Promise<string> {
     const remoteUrl = `ftp://${this.host}/${remotePath}`;
-    log.info(`Downloading ${remoteUrl} to ${localPath}`);
+    logger.info(`Downloading ${remoteUrl} to ${localPath}`);
 
     const client = await this.buildFtpClient();
 
@@ -89,7 +124,7 @@ class FtpProviderClient {
         if (err) {
           return this.errorHandler(reject, err);
         }
-        log.info(`Finishing downloading ${remoteUrl}`);
+        logger.info(`Finishing downloading ${remoteUrl}`);
         client.destroy();
         return resolve(localPath);
       });
@@ -99,23 +134,21 @@ class FtpProviderClient {
   /**
    * List all files from a given endpoint
    * @param {string} path - path to list
-   * @param {number} _counter - recursive attempt counter
+   * @param {number} counter - recursive attempt counter
    * @returns {Promise} promise of contents
    * @private
    */
-  async _list(path, _counter = 0) {
-    let counter = _counter;
+  async _list(path: string, counter = 0): Promise<FtpProviderClientListItem[]> {
     const client = await this.buildFtpClient();
-    return new Promise((resolve, reject) => {
+    return new Promise<FtpProviderClientListItem[]>((resolve, reject) => {
       client.on('error', this.errorHandler.bind(this, reject));
-      client.ls(path, (err, data) => {
+      client.ls(path, (err: Error | ListError, data) => {
         if (err) {
-          const message = err.message || err.text;
+          const message = isJSFtpError(err) ? err.text : err.message;
           if (message && message.includes('Timed out') && counter < 3) {
-            log.error(`Connection timed out while listing ${path}. Retrying...`);
-            counter += 1;
-            return this._list(path, counter).then((r) => {
-              log.info(`${counter} retry succeeded`);
+            logger.error(`Connection timed out while listing ${path}. Retrying...`);
+            return this._list(path, counter + 1).then((r) => {
+              logger.info(`${counter + 1} retry succeeded`);
               return resolve(r);
             }).catch(this.errorHandler.bind(this, reject));
           }
@@ -123,13 +156,16 @@ class FtpProviderClient {
         }
 
         client.destroy();
-        return resolve(data.map((d) => ({
-          name: d.name,
-          path: path,
-          size: Number.parseInt(d.size, 10),
-          time: d.time,
-          type: d.type,
-        })));
+
+        return resolve(
+          data.map((d) => ({
+            name: d.name,
+            path: path,
+            size: typeof d.size === 'number' ? d.size : Number.parseInt(d.size, 10),
+            time: d.time,
+            type: d.type,
+          }))
+        );
       });
     });
   }
@@ -139,15 +175,20 @@ class FtpProviderClient {
    * @param {string} path - path to list
    * @returns {Promise}
    */
-  async list(path) {
+  async list(path: string): Promise<ProviderClientListItem[]> {
     const listFn = this._list.bind(this);
     const files = await recursion(listFn, path);
 
-    log.info(`${files.length} files were found on ${this.host}`);
+    logger.info(`${files.length} files were found on ${this.host}`);
 
     // Type 'type' field is required to support recursive file listing, but
     // should not be part of the returned result.
-    return files.map((file) => omit(file, 'type'));
+    return files.map((file) => ({
+      name: file.name,
+      path: file.path,
+      size: file.size,
+      time: file.time,
+    }));
   }
 
   /**
@@ -159,15 +200,19 @@ class FtpProviderClient {
    * @returns {Promise.<{ s3uri: string, etag: string }>} an object containing
    *    the S3 URI and ETag of the destination file
    */
-  async sync(remotePath, bucket, key) {
+  async sync(
+    remotePath: string,
+    bucket: string,
+    key: string
+  ): Promise<{s3uri: string, etag: string}> {
     const remoteUrl = `ftp://${this.host}/${remotePath}`;
     const s3uri = S3.buildS3Uri(bucket, key);
-    log.info(`Sync ${remoteUrl} to ${s3uri}`);
+    logger.info(`Sync ${remoteUrl} to ${s3uri}`);
 
     const client = await this.buildFtpClient();
 
     // get readable stream for remote file
-    const readable = await new Promise((resolve, reject) => {
+    const readable = await new Promise<Socket>((resolve, reject) => {
       client.get(remotePath, (err, socket) => {
         if (err) {
           return this.errorHandler(reject, err);
@@ -188,7 +233,7 @@ class FtpProviderClient {
 
     try {
       const { ETag: etag } = await S3.promiseS3Upload(params);
-      log.info('Uploading to s3 is complete(ftp)', s3uri);
+      logger.info('Uploading to s3 is complete(ftp)', s3uri);
 
       return { s3uri, etag };
     } finally {
@@ -196,11 +241,11 @@ class FtpProviderClient {
     }
   }
 
-  /* eslint-disable no-empty-function */
-  async connect() {}
+  /* eslint-disable @typescript-eslint/no-empty-function */
+  async connect(): Promise<void> { }
 
-  async end() {}
-  /* eslint-enable no-empty-function */
+  async end(): Promise<void> { }
+  /* eslint-enable @typescript-eslint/no-empty-function */
 }
 
-module.exports = FtpProviderClient;
+export = FtpProviderClient;
