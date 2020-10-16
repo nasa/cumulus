269b32f92dd90424d9f421168b69a42ad3a1dd45
Menno Van Diermen <mvandiermen@contractor.usgs.gov>
2020-10-16 14:18:09 -0500

    async operations migration

--- lambdas/db-migration/src/migrations/20201014105030_create_async_operations_table.ts
+++ lambdas/db-migration/src/migrations/20201014105030_create_async_operations_table.ts
@@ -9,6 +9,21 @@ export const up = async (knex: Knex): Promise<void> =>
       .uuid('id')
       .comment('Unique ID for async operation');
     table
+      .text('description')
+      .comment('description for async operation');
+    table
+      .enum('operationType', ['ES Index', 'Bulk Granules', 'Bulk Granule Delete', 'Kinesis Replay', 'Reconciliation Report'])
+      .comment('type of async operation');
+    table
+      .jsonb('output')
+      .comment('output of completed async operation');
+    table
+      .enum('status', ['RUNNING', 'SUCCEEDED', 'RUNNER_FAILED', 'TASK_FAILED'])
+      .comment('async operation status');
+    table
+      .text('taskArn')
+      .comment('async operation ECS task ARN');
+    table
       .timestamps(false, true);
     table.unique(['id']);
   });
