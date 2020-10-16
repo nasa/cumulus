b85f9e60a776be8e03028155038ddeaac576d19a
Menno Van Diermen <mvandiermen@contractor.usgs.gov>
2020-10-16 17:09:55 -0500

    set required fields to notNullable

--- lambdas/db-migration/src/migrations/20201014105030_create_async_operations_table.ts
+++ lambdas/db-migration/src/migrations/20201014105030_create_async_operations_table.ts
@@ -7,19 +7,23 @@ export const up = async (knex: Knex): Promise<void> =>
       .primary();
     table
       .uuid('id')
-      .comment('Unique ID for async operation');
+      .comment('Unique ID for async operation')
+      .notNullable();
     table
       .text('description')
-      .comment('description for async operation');
+      .comment('description for async operation')
+      .notNullable();
     table
       .enum('operationType', ['ES Index', 'Bulk Granules', 'Bulk Granule Delete', 'Kinesis Replay', 'Reconciliation Report'])
-      .comment('type of async operation');
+      .comment('type of async operation')
+      .notNullable();
     table
       .jsonb('output')
       .comment('output of completed async operation');
     table
       .enum('status', ['RUNNING', 'SUCCEEDED', 'RUNNER_FAILED', 'TASK_FAILED'])
-      .comment('async operation status');
+      .comment('async operation status')
+      .notNullable();
     table
       .text('taskArn')
       .comment('async operation ECS task ARN');
