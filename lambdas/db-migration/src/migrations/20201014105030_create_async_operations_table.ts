5b15007461708acf5bf2692c3b496be738492f44
Menno <mvandiermen@contractor.usgs.gov>
2020-10-19 16:13:25 -0500

    CUMULUS-2183: async operations table schema migration (#1942)
    * async operations migration
    
    * set required fields to notNullable

--- lambdas/db-migration/src/migrations/20201014105030_create_async_operations_table.ts
+++ lambdas/db-migration/src/migrations/20201014105030_create_async_operations_table.ts
@@ -7,7 +7,26 @@ export const up = async (knex: Knex): Promise<void> =>
       .primary();
     table
       .uuid('id')
-      .comment('Unique ID for async operation');
+      .comment('Unique ID for async operation')
+      .notNullable();
+    table
+      .text('description')
+      .comment('description for async operation')
+      .notNullable();
+    table
+      .enum('operationType', ['ES Index', 'Bulk Granules', 'Bulk Granule Delete', 'Kinesis Replay', 'Reconciliation Report'])
+      .comment('type of async operation')
+      .notNullable();
+    table
+      .jsonb('output')
+      .comment('output of completed async operation');
+    table
+      .enum('status', ['RUNNING', 'SUCCEEDED', 'RUNNER_FAILED', 'TASK_FAILED'])
+      .comment('async operation status')
+      .notNullable();
+    table
+      .text('taskArn')
+      .comment('async operation ECS task ARN');
     table
       .timestamps(false, true);
     table.unique(['id']);
