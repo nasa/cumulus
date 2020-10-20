384d887eeb10ce2f5d941404f99fdcc07b2674e7
Jennifer Tran <tranjennif@gmail.com>
2020-10-19 19:28:04 -0700

    Add update-granules-metadata and begin building out tests, remove functionality from move-granules

---
+++ tasks/update-granules-metadata/index.js
@@ -0,0 +1,130 @@
+'use strict';
+
+const cumulusMessageAdapter = require('@cumulus/cumulus-message-adapter-js');
+const get = require('lodash/get');
+const keyBy = require('lodash/keyBy');
+const mapValues = require('lodash/mapValues');
+const set = require('lodash/set');
+
+const {
+  getJsonS3Object,
+} = require('@cumulus/aws-client/S3');
+
+const { getDistributionBucketMapKey } = require('@cumulus/common/stack');
+
+const BucketsConfig = require('@cumulus/common/BucketsConfig');
+
+const {
+  isCMRFile,
+  granulesToCmrFileObjects,
+  updateCMRMetadata,
+} = require('@cumulus/cmrjs');
+
+/**
+ * Update each of the CMR files' OnlineAccessURL fields to represent the new
+ * file locations.
+ *
+ * @param {Array<Object>} cmrFiles       - array of objects that include CMR xmls uris and
+ *                                         granuleIds
+ * @param {Object} granulesObject        - an object of the granules where the key is the granuleId
+ * @param {string} cmrGranuleUrlType .   - type of granule CMR url
+ * @param {string} distEndpoint          - the api distribution endpoint
+ * @param {Object} bucketTypes           - map of bucket names to bucket types
+ * @param {Object} distributionBucketMap - mapping of bucket->distirubtion path values
+ *                                         (e.g. { bucket: distribution path })
+ * @returns {Promise<Object[]>} array of updated CMR files with etags
+ *
+ */
+async function updateEachCmrFileAccessURLs(
+  cmrFiles,
+  granulesObject,
+  cmrGranuleUrlType,
+  distEndpoint,
+  bucketTypes,
+  distributionBucketMap
+) {
+  return Promise.all(cmrFiles.map( (cmrFile) => {
+    const granuleId = cmrFile.granuleId;
+    const granule = granulesObject[granuleId];
+    return updateCMRMetadata({
+      granuleId,
+      // must be determined from granules because cmrFiles is of a different, condensed shape
+      cmrFile: granule.files.find(isCMRFile),
+      files: granule.files,
+      distEndpoint,
+      published: false,
+      bucketTypes,
+      cmrGranuleUrlType,
+      distributionBucketMap,
+    });
+  }));
+}
+
+/**
+ * Adds etag values to the specified granules' CMR files.
+ *
+ * @param {Object} granulesByGranuleId - mapping of granule IDs to granules,
+ *    each containing a list of `files`
+ * @param {Object[]} cmrFiles - array of CMR file objects with `filename` and
+ *    `etag` properties
+ * @returns {Object} granule mapping identical to input granule mapping, but
+ *    with CMR file objects updated with the `etag` values supplied via the
+ *    array of CMR file objects, matched by `filename`
+ */
+function addCmrFileEtags(granulesByGranuleId, cmrFiles) {
+  const etagsByFilename = Object.fromEntries(cmrFiles
+    .map(({ filename, etag }) => [filename, etag]));
+  const addEtag = (file) => set(file, 'etag', etagsByFilename[file.filename]);
+  const addEtags = (files) => files.map((f) => (isCMRFile(f) ? addEtag(f) : f));
+
+  return mapValues(granulesByGranuleId,
+    (granule) => ({ ...granule, files: addEtags(granule.files) }));
+}
+
+async function updateGranulesMetadata(event) {
+  const config = event.config;
+  const bucketsConfig = new BucketsConfig(config.buckets);
+  const bucketTypes = Object.fromEntries(Object.values(bucketsConfig.buckets)
+  .map(({ name, type }) => [name, type]));
+
+  const cmrGranuleUrlType = get(config, 'cmrGranuleUrlType', 'distribution');
+
+  const granules = event.input.granules;
+  const cmrFiles = granulesToCmrFileObjects(granules);
+  const granulesByGranuleId = keyBy(granules, 'granuleId');
+
+  const distributionBucketMap = await getJsonS3Object(
+    process.env.system_bucket,
+    getDistributionBucketMapKey(process.env.stackName)
+  );
+    
+  const updatedCmrFiles = await updateEachCmrFileAccessURLs(
+    cmrFiles,
+    granulesByGranuleId,
+    cmrGranuleUrlType,
+    config.distribution_endpoint,
+    bucketTypes,
+    distributionBucketMap
+  );
+  console.log(updatedCmrFiles)
+
+   // Transfer etag info to granules' CMR files
+   const result = addCmrFileEtags(granulesByGranuleId, updatedCmrFiles);
+
+   return { granules: Object.values(result) }
+}
+
+/**
+ * Lambda handler
+ *
+ * @param {Object} event      - a Cumulus Message
+ * @param {Object} context    - an AWS Lambda context
+ * @returns {Promise<Object>} - Returns output from task.
+ *                              See schemas/output.json for detailed output schema
+ */
+async function handler(event, context) {
+  return cumulusMessageAdapter.runCumulusTask(updateGranulesMetadata, event, context);
+}
+
+exports.handler = handler;
+exports.updateGranulesMetadata = updateGranulesMetadata;
---
+++ tf-modules/ingest/update-granules-metadata-task.tf
@@ -0,0 +1,33 @@
+resource "aws_lambda_function" "update_granules_metadata_task" {
+  function_name    = "${var.prefix}-UpdateGranulesMetadata"
+  filename         = "${path.module}/../../tasks/update-granules-metadata/dist/lambda.zip"
+  source_code_hash = filebase64sha256("${path.module}/../../tasks/update-granules-metadata/dist/lambda.zip")
+  handler          = "index.handler"
+  role             = var.lambda_processing_role_arn
+  runtime          = "nodejs12.x"
+  timeout          = 300
+  memory_size      = 1024
+
+  layers = [var.cumulus_message_adapter_lambda_layer_arn]
+
+  environment {
+    variables = {
+      CMR_ENVIRONMENT             = var.cmr_environment
+      stackName                   = var.prefix
+      CUMULUS_MESSAGE_ADAPTER_DIR = "/opt/"
+      system_bucket               = var.system_bucket
+    }
+  }
+
+  dynamic "vpc_config" {
+    for_each = length(var.lambda_subnet_ids) == 0 ? [] : [1]
+    content {
+      subnet_ids = var.lambda_subnet_ids
+      security_group_ids = [
+        aws_security_group.no_ingress_all_egress[0].id
+      ]
+    }
+  }
+
+  tags = var.tags
+} 
