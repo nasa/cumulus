5d8e2128205dfbdad20996d4199197a9db5dbd3b
Jonathan Kovarik <jonathan.kovarik@gmail.com>
2020-10-21 11:47:48 -0600

    Add PDRs table migration

--- lambdas/db-migration/src/migrations/20201014105054_create_pdrs_table.ts
+++ lambdas/db-migration/src/migrations/20201014105054_create_pdrs_table.ts
@@ -4,11 +4,8 @@ export const up = async (knex: Knex): Promise<void> =>
   knex.schema.createTable('pdrs', (table) => {
     table
       .increments('cumulusId')
+      .comment('Internal Cumulus ID for a PDR')
       .primary();
-    table
-      .text('name')
-      .comment('PDR name')
-      .notNullable();
     table
       .integer('collectionCumulusId')
       .references('cumulusId')
@@ -22,8 +19,36 @@ export const up = async (knex: Knex): Promise<void> =>
     table
       .integer('executionCumulusId')
       .references('cumulusId')
-      .inTable('executions')
+      .inTable('executions');
+    table
+      .enum('status', ['running', 'failed', 'completed'])
+      .comment('Status (running, failed, completed) of the PDR')
       .notNullable();
+    table
+      .float('progress')
+      .comment('PDR completion progress percentage');
+    table
+      .boolean('PANSent')
+      .comment('Boolean defining if a PAN response has been sent for this PDR');
+    table
+      .text('PANmessage')
+      .comment('PAN message text');
+    table
+      .jsonb('stats')
+      .comment('PDR stats json object');
+    table
+      .text('address');
+    table
+      .text('originalUrl');
+    table
+      .integer('duration');
+    table
+      .text('name')
+      .comment('PDR name')
+      .notNullable();
+    table
+      .timestamp('timestamp')
+      .comment('PDR timestamp');
     table
       .timestamps(false, true);
     table.unique(['name']);
