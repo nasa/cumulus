e2dc2d906adcc001087fd3edd5bc6ed54002296e
Mark Boyd <markdavidboyd@gmail.com>
2020-10-23 17:49:20 -0400

    refactor exports

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -1,5 +1,5 @@
 export { getKnexClient } from './connection';
 export { getKnexConfig, localStackConnectionEnv } from './config';
-export * as Executions from './Executions';
-export * as database from './database';
+export { doesExecutionExist } from './Executions';
+export { getDbClient, getDbTransaction } from './database';
 export { tableNames } from './tables';
