b74c78e67779b1311a56d32c451e6f3f0b74397e
Jonathan Kovarik <jonathan.kovarik@gmail.com>
2020-11-02 15:02:09 -0700

    Update to use private image for feature branch

--- tf-modules/archive/async_operation.tf
+++ tf-modules/archive/async_operation.tf
@@ -23,7 +23,7 @@ resource "aws_ecs_task_definition" "async_operation" {
         "value": "${var.rds_user_access_secret_arn}"
       }
     ],
-    "image": "cumuluss/async-operation:27",
+    "image": "jlkovarik/ecs-task-runner:37",
     "memoryReservation": 700,
     "logConfiguration": {
       "logDriver": "awslogs",
