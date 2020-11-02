435f5cb9034f45cbbd1271790c7bef7a44abdba1
Jonathan Kovarik <jonathan.kovarik@gmail.com>
2020-11-02 16:58:51 -0700

    Update async image

--- tf-modules/archive/async_operation.tf
+++ tf-modules/archive/async_operation.tf
@@ -23,7 +23,7 @@ resource "aws_ecs_task_definition" "async_operation" {
         "value": "${var.rds_user_access_secret_arn}"
       }
     ],
-    "image": "jlkovarik/ecs-task-runner:37",
+    "image": "jlkovarik/ecs-task-runner:38",
     "memoryReservation": 700,
     "logConfiguration": {
       "logDriver": "awslogs",
