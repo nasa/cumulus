759dbf3cab2f2f8dcfb6d083243ad02528db43a7
Jonathan Kovarik <jonathan.kovarik@gmail.com>
2020-11-06 12:55:35 -0700

    Add CHANGELOG entry for 2191, update task to use cumuluss project

--- tf-modules/archive/async_operation.tf
+++ tf-modules/archive/async_operation.tf
@@ -23,7 +23,7 @@ resource "aws_ecs_task_definition" "async_operation" {
         "value": "${var.rds_user_access_secret_arn}"
       }
     ],
-    "image": "jlkovarik/ecs-task-runner:41",
+    "image": "cumuluss/async-operation:28",
     "memoryReservation": 700,
     "logConfiguration": {
       "logDriver": "awslogs",
