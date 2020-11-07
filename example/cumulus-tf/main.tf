1cd69b1fe3b7a6d0dbd19b17848b173a5f29b3ca
Jennifer Tran <tranjennif@gmail.com>
2020-11-06 17:48:35 -0800

    Change tea_api_egress_log_group condition

--- example/cumulus-tf/main.tf
+++ example/cumulus-tf/main.tf
@@ -157,7 +157,7 @@ module "cumulus" {
   tea_rest_api_root_resource_id = module.thin_egress_app.rest_api.root_resource_id
   tea_internal_api_endpoint = module.thin_egress_app.internal_api_endpoint
   tea_external_api_endpoint = module.thin_egress_app.api_endpoint
-  tea_api_egress_log_group = { egress_log_group = module.thin_egress_app.egress_log_group }
+  tea_api_egress_log_group = var.log_api_gateway_to_cloudwatch ? { egress_log_group = module.thin_egress_app.egress_log_group } : null
 
   log_destination_arn = var.log_destination_arn
 
@@ -214,6 +214,7 @@ module "thin_egress_app" {
   stage_name                 = local.tea_stage_name
   urs_auth_creds_secret_name = aws_secretsmanager_secret.thin_egress_urs_creds.name
   vpc_subnet_ids             = var.lambda_subnet_ids
+  log_api_gateway_to_cloudwatch = var.log_api_gateway_to_cloudwatch
 }
 
 resource "aws_security_group" "no_ingress_all_egress" {
