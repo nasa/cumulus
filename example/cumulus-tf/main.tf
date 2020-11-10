a441a8fdabb55aa86baa4fa8064c08105793418d
Jennifer Tran <tranjennif@gmail.com>
2020-11-09 16:10:50 -0800

    Add explicit dependency

--- example/cumulus-tf/main.tf
+++ example/cumulus-tf/main.tf
@@ -216,6 +216,7 @@ module "thin_egress_app" {
 }
 
 resource "aws_cloudwatch_log_subscription_filter" "egress_api_gateway_log_subscription_filter" {
+  depends_on = [module.thin_egress_app.egress_log_group]
   count           = var.log_api_gateway_to_cloudwatch != null ? 1 : 0
   name            = "${var.prefix}-EgressApiGatewayCloudWatchLogSubscriptionToSharedDestination"
   distribution    = "ByLogStream"
