7306e8513ac867c22ff685cfd0c8dd0a70f0def2
Menno Van Diermen <mvandiermen@contractor.usgs.gov>
2020-11-10 17:55:14 -0600

    preserve output original case

--- packages/db/src/async_operations.ts
+++ packages/db/src/async_operations.ts
@@ -1,6 +1,33 @@
 import { toSnake } from 'snake-camel';
-// import { AsyncOperationRecord } from './types';
+
+interface CamelCaseAsyncOperation {
+  id: string
+  description: string
+  operationType: string
+  status: string
+  output?: object
+  taskArn?: string
+  createdAt?: Date
+  updatedAt?: Date
+}
+
+interface SnakeCaseAsyncOperation {
+  id: string
+  description: string
+  operation_type: string
+  status: string
+  output?: object
+  task_arn?: string
+  created_at?: Date
+  updated_at?: Date
+}
 
 export const translateAsyncOperationToSnakeCase = (
-  record: Object
-): Object => toSnake(record);
+  record: CamelCaseAsyncOperation
+): SnakeCaseAsyncOperation => {
+  const translatedRecord: SnakeCaseAsyncOperation = <SnakeCaseAsyncOperation>toSnake(record);
+  if(record.output !== undefined) {
+    translatedRecord.output = record.output;
+  }
+  return translatedRecord;
+};
---
+++ packages/db/tests/test-async-operations.js
@@ -0,0 +1,47 @@
+const test = require('ava');
+const { translateAsyncOperationToSnakeCase } = require('../dist/async_operations');
+
+test('translateAsyncOperationToSnakeCase converts a camelCase record to snake_case', (t) => {
+  const camelCaseAsyncOperation = {
+    id: '1234567890',
+    status: 'RUNNING',
+    taskArn: 'aws:arn:ecs:task:someTask',
+    description: 'dummy operation',
+    operationType: 'ES Index',
+  };
+
+  const expected = {
+    id: camelCaseAsyncOperation.id,
+    status: camelCaseAsyncOperation.status,
+    task_arn: camelCaseAsyncOperation.taskArn,
+    description: camelCaseAsyncOperation.description,
+    operation_type: camelCaseAsyncOperation.operationType,
+  };
+
+  t.deepEqual(translateAsyncOperationToSnakeCase(camelCaseAsyncOperation), expected);
+});
+
+test('translateAsyncOperationToSnakeCase does not convert output field to snake_case', (t) => {
+  const camelCaseAsyncOperation = {
+    id: '1234567890',
+    status: 'SUCCEEDED',
+    taskArn: 'aws:arn:ecs:task:someTask',
+    description: 'dummy operation',
+    operationType: 'ES Index',
+    output: {
+      esIndex: 'test-index',
+      operationStatus: 'complete',
+    }
+  };
+
+  const expected = {
+    id: camelCaseAsyncOperation.id,
+    status: camelCaseAsyncOperation.status,
+    task_arn: camelCaseAsyncOperation.taskArn,
+    description: camelCaseAsyncOperation.description,
+    operation_type: camelCaseAsyncOperation.operationType,
+    output: camelCaseAsyncOperation.output,
+  };
+
+  t.deepEqual(translateAsyncOperationToSnakeCase(camelCaseAsyncOperation), expected);
+});
