99cd413a02a597bb58ab7158b2cc0bc822ba63d8
Jonathan Kovarik <jonathan.kovarik@gmail.com>
2020-11-13 04:08:27 -0700

    Move error code comment to match error definition

--- packages/api/lib/errors.js
+++ packages/api/lib/errors.js
@@ -7,14 +7,14 @@ const {
   ValidationError,
 } = require('@cumulus/errors');
 
+// Postgres error codes:
+// https://www.postgresql.org/docs/10/errcodes-appendix.html
 const isPostgresValidationError = (error) => ['22', '23'].includes((error.code || '').substring(0, 2));
 
 const isBadRequestError = (error) =>
   error.name === 'SchemaValidationError'
   || error instanceof ValidationError
   || isPostgresValidationError(error);
-  // Postgres error codes:
-  // https://www.postgresql.org/docs/10/errcodes-appendix.html
 
 const TokenUnauthorizedUserError = createErrorType('TokenUnauthorizedUserError');
 const IndexExistsError = createErrorType('IndexExistsError');
