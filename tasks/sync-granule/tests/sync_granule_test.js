857ce9d6f8bb531bd34a15a5aef72917ace50c9d
Jonathan Kovarik <jonathan.kovarik@gmail.com>
2020-11-16 09:00:44 -0700

    Remove errant only

--- tasks/sync-granule/tests/sync_granule_test.js
+++ tasks/sync-granule/tests/sync_granule_test.js
@@ -175,7 +175,7 @@ test.serial('no error when collection info is not provided in the event', async 
   t.is(output.granules[0].files.length, 1);
 });
 
-test.serial.only('download Granule from FTP endpoint', async (t) => {
+test.serial('download Granule from FTP endpoint', async (t) => {
   t.context.event.config.provider = {
     id: 'MODAPS',
     protocol: 'ftp',
