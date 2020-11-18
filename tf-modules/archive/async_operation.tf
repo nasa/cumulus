81e7c4d490037312c4d51a4c7b4c7e097ba83537
Menno Van Diermen <mvandiermen@contractor.usgs.gov>
2020-11-18 13:05:23 -0600

    async-operation image v31

--- tf-modules/archive/async_operation.tf
+++ tf-modules/archive/async_operation.tf
@@ -23,7 +23,7 @@ resource "aws_ecs_task_definition" "async_operation" {
         "value": "${var.rds_user_access_secret_arn}"
       }
     ],
-    "image": "cumuluss/async-operation:30",
+    "image": "cumuluss/async-operation:31",
     "memoryReservation": 700,
     "logConfiguration": {
       "logDriver": "awslogs",
