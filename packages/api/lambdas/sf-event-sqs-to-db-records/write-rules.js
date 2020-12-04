ef3ca53a52a27a1769cc6a45e87777ffaeafc3a3
Jennifer Tran <tranjennif@gmail.com>
2020-12-03 22:14:22 -0800

    WIP Write Rules logic and tests to write to dynamodb and rds

--- packages/api/lambdas/sf-event-sqs-to-db-records/write-rules.js
+++ packages/api/lambdas/sf-event-sqs-to-db-records/write-rules.js
@@ -12,14 +12,45 @@ const {
 
 const Rule = require('../../models/rules');
 
+const buildRuleRecord = (
+  rule,
+  cumulusMessage,
+  collectionCumulusId,
+  providerCumulusId,
+  now = new Date()
+) => {
+  const name = rule.name;
+  return {
+    name,
+    collection_cumulus_id: collectionCumulusId,
+    provider_cumulus_id: providerCumulusId,
+    workflow: rule.workflow,
+    enabled: rule.state === 'ENABLED',
+    type: rule.rule.type,
+    value: rule.rule.value,
+    arn: rule.rule.arn,
+    log_event_arn: rule.rule.log_event_arn,
+    payload: rule.payload,
+    meta: rule.meta,
+    tags: rule.tags ? JSON.stringify(rule.tags) : undefined,
+    queue_url: rule.queue_rule,
+    created_at: rule.created_at,
+    updated_at: now,
+  };
+};
+
 const writeRuleViaTransaction = async ({
   rule,
+  cumulusMessage,
+  collectionCumulusId,
+  providerCumulusId,
   trx,
-}) =>
-  trx(tableNames.rules)
-    .insert({
-      name: rule.name,
-    });
+}) => {
+  const record = buildRuleRecord(rule, cumulusMessage, collectionCumulusId, providerCumulusId);
+  return trx(tableNames.rules)
+    .insert(record)
+    .returning('cumulus_id');
+};
 
 /**
  * Write a rule to DynamoDB and Postgres
@@ -27,6 +58,10 @@ const writeRuleViaTransaction = async ({
  * @param {Object} params
  * @param {Object} params.rule - An API Rule object
  * @param {Object} params.cumulusMessage - A workflow message
+ * @param {string} params.collectionCumulusId
+ *   Cumulus ID for collection referenced in workflow message, if any
+ * @param {string} params.providerCumulusId
+ *   Cumulus ID for provider referenced in workflow message, if any
  * @param {Knex} params.knex - Client to interact with Postgres database
  * @param {Object} [params.ruleModel]
  *   Optional override for the rule model writing to DynamoDB
@@ -36,15 +71,21 @@ const writeRuleViaTransaction = async ({
  */
 const writeRule = async ({
   rule,
+  cumulusMessage,
+  collectionCumulusId,
+  providerCumulusId,
   knex,
   ruleModel,
 }) =>
   knex.transaction(async (trx) => {
     await writeRuleViaTransaction({
       rule,
+      cumulusMessage,
+      collectionCumulusId,
+      providerCumulusId,
       trx,
     });
-    return ruleModel.storeRuleFromCumulusMessage(rule);
+    return ruleModel.storeRuleFromCumulusMessage({ rule });
   });
 
 /**
@@ -63,10 +104,11 @@ const writeRule = async ({
  * @returns {Promise<Object[]>}
  *  true if there are no rules on the message, otherwise
  *  results from Promise.allSettled for all rules
- * @throws {Error} - if no collection or provider is provided
  */
 const writeRules = async ({
   cumulusMessage,
+  collectionCumulusId,
+  providerCumulusId,
   knex,
   ruleModel = new Rule(),
 }) => {
@@ -78,6 +120,8 @@ const writeRules = async ({
     (rule) => writeRule({
       rule,
       cumulusMessage,
+      collectionCumulusId,
+      providerCumulusId,
       knex,
       ruleModel,
     })
--- packages/api/tests/lambdas/sf-event-sqs-to-db-records/test-write-rules.js
+++ packages/api/tests/lambdas/sf-event-sqs-to-db-records/test-write-rules.js
@@ -1,16 +1,176 @@
 'use strict';
 
+const cryptoRandomString = require('crypto-random-string');
 const test = require('ava');
+
+const {
+  localStackConnectionEnv,
+  getKnexClient,
+  tableNames,
+  doesRecordExist,
+} = require('@cumulus/db');
+const { randomString } = require('@cumulus/common/test-utils');
+
 const {
   writeRules,
 } = require('../../../lambdas/sf-event-sqs-to-db-records/write-rules');
 
+const { migrationDir } = require('../../../../../lambdas/db-migration');
 const { fakeRuleFactoryV2 } = require('../../../lib/testUtils');
 const Rule = require('../../../models/rules');
 
-test('writeRules() saves rule records to Dynamo and RDS if RDS write is enabled', async (t) => {
+test.before(async (t) => {
+  process.env.RulesTable = cryptoRandomString({ length: 10 });
+
+  const ruleModel = new Rule();
+  await ruleModel.createTable();
+  t.context.ruleModel = ruleModel;
+
+  t.context.testDbName = `writeRules${cryptoRandomString({ length: 10 })}`;
+
+  t.context.knexAdmin = await getKnexClient({ env: localStackConnectionEnv });
+  await t.context.knexAdmin.raw(`create database "${t.context.testDbName}";`);
+  await t.context.knexAdmin.raw(`grant all privileges on database "${t.context.testDbName}" to "${localStackConnectionEnv.PG_USER}"`);
+  t.context.knex = await getKnexClient({
+    env: {
+      ...localStackConnectionEnv,
+      PG_DATABASE: t.context.testDbName,
+      migrationDir,
+    },
+  });
+  await t.context.knex.migrate.latest();
 });
 
+test.beforeEach(async (t) => {
+  t.context.onetimeRule = {
+    name: cryptoRandomString({ length: 10 }),
+    workflow: randomString(),
+    provider: randomString(),
+    collection: {
+      name: randomString(),
+      version: 'my-collection-version',
+    },
+    rule: {
+      type: 'onetime',
+    },
+    state: 'ENABLED',
+    createdAt: Date.now(),
+    updatedAt: Date.now(),
+  };
+
+  t.context.kinesisRule = {
+    name: cryptoRandomString({ length: 5 }),
+    workflow: randomString(),
+    provider: cryptoRandomString({ length: 10 }),
+    collection: {
+      name: cryptoRandomString({ length: 10 }),
+      version: '0.0.0',
+    },
+    rule: {
+      type: 'kinesis',
+      value: randomString(),
+    },
+    state: 'ENABLED',
+    createdAt: Date.now(),
+    updatedAt: Date.now(),
+  };
+
+  t.context.cumulusMessage = {
+    cumulus_meta: {
+      execution_name: cryptoRandomString({ length: 5 }),
+      state_machine: cryptoRandomString({ length: 5 }),
+      workflow_start_time: Date.now(),
+    },
+    meta: {
+      collection: {
+        name: 'name',
+        version: '001',
+      },
+      provider: {
+        host: 'example-bucket',
+        protocol: 's3',
+        id: 'id',
+      },
+      status: 'completed',
+      workflow: randomString(),
+    },
+    payload: {
+      rules: [
+        t.context.onetimeRule,
+        t.context.kinesisRule,
+      ],
+    },
+  };
+
+  t.context.collection = {
+    name: cryptoRandomString({ length: 5 }),
+    version: '0.0.0',
+    sample_file_name: 'file.txt',
+    granule_id_extraction_regex: 'fake-regex',
+    granule_id_validation_regex: 'fake-regex',
+    files: JSON.stringify([{
+      regex: 'fake-regex',
+      sampleFileName: 'file.txt',
+    }]),
+  };
+
+  t.context.provider = {
+    id: `provider${cryptoRandomString({ length: 5 })}`,
+    host: 'test-bucket',
+    protocol: 's3',
+  };
+  const collectionResponse = await t.context.knex(tableNames.collections)
+    .insert(t.context.collection)
+    .returning('cumulus_id');
+
+  t.context.collectionCumulusId = collectionResponse[0];
+
+  const providerResponse = await t.context.knex(tableNames.providers)
+    .insert({
+      name: t.context.provider.id,
+      host: t.context.provider.host,
+      protocol: t.context.provider.protocol,
+    })
+    .returning('cumulus_id');
+  t.context.providerCumulusId = providerResponse[0];
+});
+
+test.after.always(async (t) => {
+  const {
+    ruleModel,
+  } = t.context;
+  await ruleModel.deleteTable();
+  await t.context.knex.destroy();
+  await t.context.knexAdmin.raw(`drop database if exists "${t.context.testDbName}"`);
+  await t.context.knexAdmin.destroy();
+});
+
+test('writeRules() saves rule records to Dynamo and RDS', async (t) => {
+  const {
+    cumulusMessage,
+    collectionCumulusId,
+    providerCumulusId,
+    knex,
+    onetimeRule,
+    kinesisRule,
+    ruleModel,
+  } = t.context;
+
+  await writeRules({
+    cumulusMessage,
+    collectionCumulusId,
+    providerCumulusId,
+    knex,
+  });
+
+  t.true(await ruleModel.exists({ name: onetimeRule.name }));
+  t.true(await ruleModel.exists({ name: kinesisRule.name }));
+  t.true(
+    await doesRecordExist({ name: onetimeRule.name }, knex, tableNames.rules)
+  );
+});
+
+/*
 test('writeRules() handles successful and failing writes independently', async (t) => {
 });
 
@@ -21,5 +181,5 @@ test.serial('writeRules() does not persist records to Dynamo or RDS if Dynamo wr
 });
 
 test.serial('writeRules() does not persist records to Dynamo or RDS if RDS write fails', async (t) => {
-
 });
+*/
