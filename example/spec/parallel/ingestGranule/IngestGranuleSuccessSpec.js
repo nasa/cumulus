5e5dc839147c0b6660d0594446e1fa9bcb4233ec
Lauren Frederick <lpfreder@mbp-lpfreder.edn.ecs.nasa.gov>
2020-12-09 16:16:25 -0500

    Fix IngestGranule test expected Opendap link

--- example/spec/parallel/ingestGranule/IngestGranuleSuccessSpec.js
+++ example/spec/parallel/ingestGranule/IngestGranuleSuccessSpec.js
@@ -246,7 +246,7 @@ describe('The S3 Ingest Granules workflow', () => {
       }
     );
 
-    opendapFilePath = `https://opendap.uat.earthdata.nasa.gov/providers/CUMULUS/collections/MODIS/Terra%20Surface%20Reflectance%20Daily%20L2G%20Global%20250m%20SIN%20Grid%20V006/granules/${granuleId}`;
+    opendapFilePath = `https://opendap.uat.earthdata.nasa.gov/providers/CUMULUS/collections/MODIS%2FTerra%20Surface%20Reflectance%20Daily%20L2G%20Global%20250m%20SIN%20Grid%20V006/granules/${granuleId}`;
   });
 
   afterAll(async () => {
