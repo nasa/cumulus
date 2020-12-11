951503a33b4fa047fb8605ccce36f53df236ba45
Nate Pauzenga <npauzenga@gmail.com>
2020-12-11 11:26:35 -0500

    CUMULUS-2188 test stubs for execution migration

---
+++ lambdas/data-migration2/tests/test-executions.js
@@ -0,0 +1,21 @@
+const test = require('ava');
+
+test.serial('migrateExecutionRecord correctly migrates execution record', async (t) => {
+
+});
+
+test.serial('migrateExecutionRecord throws error on invalid source data from Dynamo', async (t) => {
+
+});
+
+test.serial('migrateExecutionRecord handles nullable fields on source execution data', async (t) => {
+
+});
+
+test.serial('migrateExecutionRecord ignores extraneous fields from Dynamo', async (t) => {
+
+});
+
+test.serial('migrateExecutionRecord skips already migrated record', async (t) => {
+
+});
