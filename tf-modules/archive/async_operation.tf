6599760ea0e24cfbc2d82364b22b71248114593b
Matt Savoie <savoie@nsidc.org>
2020-12-16 15:24:43 -0700

    Update async-operation to use 4GB memory.
    This is the change to the Dockerfile, including the parameter being passed to the
    node command to increase the heap size.  The terraform change reflects a docker
    image already pushed to dockerhub.

--- tf-modules/archive/async_operation.tf
+++ tf-modules/archive/async_operation.tf
@@ -19,7 +19,7 @@ resource "aws_ecs_task_definition" "async_operation" {
         "value": "${data.aws_region.current.name}"
       }
     ],
-    "image": "cumuluss/async-operation:27",
+    "image": "cumuluss/async-operation:33",
     "memoryReservation": 700,
     "logConfiguration": {
       "logDriver": "awslogs",
