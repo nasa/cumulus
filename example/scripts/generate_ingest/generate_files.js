537b59e6f2e2040fbcd36d642baf9839d39b8274
Jonathan Kovarik <jonathan.kovarik@gmail.com>
2020-12-17 22:36:24 -0700

    Add data generation script to /example

---
+++ example/scripts/generate_ingest/generate_files.js
@@ -0,0 +1,42 @@
+/* eslint-disable no-console */
+
+const { s3PutObject } = require('@cumulus/aws-client/S3');
+const { randomStringFromRegex } = require('@cumulus/common/test-utils');
+
+const granuleRegExp = 'MOD09GQ\\.A[\\d]{7}\\.[\\w]{6}\\.006\\.[\\d]{13}';
+const BATCHSIZE = 100;
+
+// In batches of 100, upload granules
+const uploadTestFiles = async (Bucket, path, batches) => {
+  console.log(`Bucket is ${Bucket}`);
+  console.log(...process.argv.slice(2, 5));
+  let batchCount = 0;
+  console.log(`Running ${batches} batches`);
+  while (batchCount < batches) {
+    batchCount += 1;
+    console.log(`Uploading batch ${batchCount}`);
+    const uploadBatchPromises = new Array(BATCHSIZE).fill().map(
+      () => {
+        const granString = randomStringFromRegex(granuleRegExp);
+        return Promise.all(['hdf', 'jpg', 'hdf.met'].map((extension) => s3PutObject({
+          Bucket,
+          Key: `${path}/${granString}.${extension}`,
+          Body: 'fake_test_data',
+        })));
+      }
+    );
+    // eslint-disable-next-line no-await-in-loop
+    await Promise.all(uploadBatchPromises);
+    console.log(`Uploaded ${batchCount * BATCHSIZE} / ${BATCHSIZE * batches} granules`);
+  }
+};
+
+uploadTestFiles(...process.argv.slice(2, 5)).then(
+  (result) => {
+    console.log(`${result}`);
+    return undefined;
+  }
+).catch((error) => {
+  console.dir(error);
+  throw error;
+});
