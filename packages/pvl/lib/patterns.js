75e9495eac31aa6e67ae50dfd5f796ddad167e06
Menno Van Diermen <mvandiermen@contractor.usgs.gov>
2020-12-18 15:19:36 -0600

    fix datetime parsing to be slightly more flexible

--- packages/pvl/lib/patterns.js
+++ packages/pvl/lib/patterns.js
@@ -9,8 +9,8 @@ module.exports = {
 
   // Currently handles the subset of DateTime formats specified in the PDR ICD
   dateTimePatterns: [
-    /^\d{4}-\d{2}-\d{2}(T)?(\d{2}:\d{2}:\d{2}(Z)?)?$/,
-    /^(?:\d{2}\/){2}\d{4}(-\d{2}:\d{2}(:\d{2})?)?$/,
+    /^((\d{4}-\d{2}-\d{2})(-|T)?(\d{2}(:\d{2}){0,2}(\.\d*)?(Z)?)?)$/,
+    /^((\d{2}\/){2}\d{4}(-|T)?(\d{2}(:\d{2}){0,2}(\.\d*)?(Z)?)?)$/,
   ],
 
   textStringPatterns: [
