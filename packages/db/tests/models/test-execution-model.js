87a48727e5ecdcaab41e6151deccf748a6e92092
Mark Boyd <markdavidboyd@gmail.com>
2020-12-22 17:51:39 -0500

    update executino model tests

--- packages/db/tests/models/test-execution-model.js
+++ packages/db/tests/models/test-execution-model.js
@@ -63,3 +63,105 @@ test('ExecutionPgModel.upsert() creates new running execution', async (t) => {
     executionRecord
   );
 });
+
+test('ExecutionPgModel.upsert() updates only allowed fields for a running execution', async (t) => {
+  const {
+    knex,
+    executionPgModel,
+    executionRecord,
+  } = t.context;
+
+  executionRecord.status = 'running';
+  executionRecord.workflow_name = 'workflow-1';
+  executionRecord.url = 'url-1';
+  await executionPgModel.create(knex, executionRecord);
+
+  const updatedRecord = {
+    ...executionRecord,
+    created_at: new Date(),
+    updated_at: new Date(),
+    timestamp: new Date(),
+    original_payload: {
+      foo: 'bar',
+    },
+    workflow_name: 'workflow-2',
+    url: 'url-2',
+  };
+
+  await executionPgModel.upsert(knex, updatedRecord);
+
+  t.like(
+    await executionPgModel.get(knex, { arn: executionRecord.arn }),
+    {
+      ...updatedRecord,
+      workflow_name: 'workflow-1',
+      url: 'url-1',
+    }
+  );
+});
+
+test('ExecutionPgModel.upsert() creates new completed execution', async (t) => {
+  const {
+    knex,
+    executionPgModel,
+    executionRecord,
+  } = t.context;
+
+  executionRecord.status = 'completed';
+
+  await executionPgModel.upsert(knex, executionRecord);
+
+  t.like(
+    await executionPgModel.get(knex, { arn: executionRecord.arn }),
+    executionRecord
+  );
+});
+
+test('ExecutionPgModel.upsert() updates a completed execution', async (t) => {
+  const {
+    knex,
+    executionPgModel,
+    executionRecord,
+  } = t.context;
+
+  executionRecord.status = 'completed';
+  executionRecord.final_payload = {
+    key1: 'value',
+  };
+  await executionPgModel.create(knex, executionRecord);
+
+  const updatedRecord = {
+    ...executionRecord,
+    final_payload: {
+      key2: 'value',
+    },
+  };
+  await executionPgModel.upsert(knex, updatedRecord);
+
+  t.like(
+    await executionPgModel.get(knex, { arn: executionRecord.arn }),
+    updatedRecord
+  );
+});
+
+test('ExecutionPgModel.upsert() will not allow a running execution to replace a completed execution', async (t) => {
+  const {
+    knex,
+    executionPgModel,
+    executionRecord,
+  } = t.context;
+
+  executionRecord.status = 'completed';
+  await executionPgModel.create(knex, executionRecord);
+
+  const updatedRecord = {
+    ...executionRecord,
+    status: 'running',
+  };
+  await executionPgModel.upsert(knex, updatedRecord);
+
+  t.like(
+    await executionPgModel.get(knex, { arn: executionRecord.arn }),
+    executionRecord
+  );
+});
