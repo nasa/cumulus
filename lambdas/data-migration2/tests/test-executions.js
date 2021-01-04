38384a5ad191f0d708e7fb540e5d892c70fc67b0
Nate Pauzenga <npauzenga@gmail.com>
2021-01-04 16:20:05 -0500

    CUMULUS-2188 - tests and logic for fetching an executions collection and async operation

--- lambdas/data-migration2/tests/test-executions.js
+++ lambdas/data-migration2/tests/test-executions.js
@@ -1,9 +1,17 @@
 const omit = require('lodash/omit');
 const test = require('ava');
 const cryptoRandomString = require('crypto-random-string');
+const uuid = require('uuid/v4');
 
 const Execution = require('@cumulus/api/models/executions');
-const { fakeExecutionFactoryV2 } = require('@cumulus/api/lib/testUtils');
+const AsyncOperation = require('@cumulus/api/models/async-operation');
+const Collection = require('@cumulus/api/models/collections');
+
+const {
+  fakeExecutionFactoryV2,
+  fakeCollectionFactory,
+  fakeAsyncOperationFactory,
+} = require('@cumulus/api/lib/testUtils');
 const {
   createBucket,
   recursivelyDeleteS3Bucket,
@@ -14,10 +22,14 @@ const { migrationDir } = require('../../db-migration');
 
 const {
   migrateExecutionRecord,
-  migrateExecutions,
 } = require('../dist/lambda/executions');
+// TODO should we not pull these from another Lambda?
+const { migrateAsyncOperationRecord } = require('../../data-migration1/dist/lambda/async-operations');
+const { migrateCollectionRecord } = require('../../data-migration1/dist/lambda/collections');
 
-const executionOmitList = ['createdAt', 'updatedAt', 'finalPayload', 'originalPayload', 'parentArn', 'type', 'execution', 'name'];
+const executionOmitList = [
+  'createdAt', 'updatedAt', 'finalPayload', 'originalPayload', 'parentArn', 'type', 'execution', 'name', 'collectionId', 'asyncOperationId',
+];
 
 const testDbName = `data_migration_2_${cryptoRandomString({ length: 10 })}`;
 const testDbUser = 'postgres';
@@ -25,12 +37,21 @@ const testDbUser = 'postgres';
 process.env.stackName = cryptoRandomString({ length: 10 });
 process.env.system_bucket = cryptoRandomString({ length: 10 });
 process.env.ExecutionsTable = cryptoRandomString({ length: 10 });
+process.env.AsyncOperationsTable = cryptoRandomString({ length: 10 });
+process.env.CollectionsTable = cryptoRandomString({ length: 10 });
 
 const executionsModel = new Execution();
+const asyncOperationsModel = new AsyncOperation({
+  stackName: process.env.stackName,
+  systemBucket: process.env.system_bucket,
+});
+const collectionsModel = new Collection();
 
 test.before(async (t) => {
   await createBucket(process.env.system_bucket);
   await executionsModel.createTable();
+  await asyncOperationsModel.createTable();
+  await collectionsModel.createTable();
 
   t.context.knexAdmin = await getKnexClient({
     env: {
@@ -51,27 +72,48 @@ test.before(async (t) => {
 
   await t.context.knex.migrate.latest();
 
-  t.context.existingExecution = await executionsModel.create(fakeExecutionFactoryV2());
+  // This will be the top-level execution (no parent execution)
+  t.context.existingExecution = await executionsModel.create(fakeExecutionFactoryV2({ parentArn: undefined }));
+  t.context.existingCollection = await collectionsModel.create(fakeCollectionFactory());
+  t.context.existingAsyncOperation = await asyncOperationsModel.create(
+    fakeAsyncOperationFactory({
+      id: uuid(),
+      output: '{ "output": "test" }',
+    })
+  );
 });
 
 test.afterEach.always(async (t) => {
   await t.context.knex('executions').del();
+  await t.context.knex('collections').del();
+  await t.context.knex('async_operations').del();
 });
 
 test.after.always(async (t) => {
   await executionsModel.deleteTable();
+  await asyncOperationsModel.deleteTable();
+  await collectionsModel.deleteTable();
   await recursivelyDeleteS3Bucket(process.env.system_bucket);
   await t.context.knex.destroy();
   await t.context.knexAdmin.raw(`drop database if exists "${testDbName}"`);
   await t.context.knexAdmin.destroy();
 });
 
-test('migrateExecutionRecord correctly migrates execution record', async (t) => {
-  const { existingExecution } = t.context;
+test.serial('migrateExecutionRecord correctly migrates execution record', async (t) => {
+  const {
+    existingExecution,
+    existingCollection,
+    existingAsyncOperation,
+  } = t.context;
+
+  // migrate existing async operation and collection
+  await migrateAsyncOperationRecord(existingAsyncOperation, t.context.knex);
+  await migrateCollectionRecord(existingCollection, t.context.knex);
 
   // migrate the existing dynamo execution to postgres so
   // we can use it as the parent for the next execution
   await migrateExecutionRecord(existingExecution, t.context.knex);
+
   const existingPostgresExecution = await t.context.knex.queryBuilder()
     .select()
     .table('executions')
@@ -79,9 +121,15 @@ test('migrateExecutionRecord correctly migrates execution record', async (t) => 
     .first();
 
   // Create new Dynamo execution to be migrated to postgres
-  const newExecution = fakeExecutionFactoryV2({ parentArn: existingExecution.arn });
+  const newExecution = fakeExecutionFactoryV2({
+    parentArn: existingExecution.arn,
+    // TODO double-check that this is right. The IDs here are always in this format?
+    collectionId: `${existingCollection.name}___${existingCollection.version}`,
+    asyncOperationId: existingAsyncOperation.id,
+  });
 
   await migrateExecutionRecord(newExecution, t.context.knex);
+
   const createdRecord = await t.context.knex.queryBuilder()
     .select()
     .table('executions')
@@ -93,8 +141,8 @@ test('migrateExecutionRecord correctly migrates execution record', async (t) => 
     omit(
       {
         ...newExecution,
-        async_operation_cumulus_id: null,
-        collection_cumulus_id: null,
+        async_operation_cumulus_id: 1,
+        collection_cumulus_id: 1,
         cumulus_version: null,
         url: null,
         parent_cumulus_id: existingPostgresExecution.cumulus_id,
@@ -110,18 +158,65 @@ test('migrateExecutionRecord correctly migrates execution record', async (t) => 
   );
 });
 
-test('migrateExecutionRecord throws error on invalid source data from Dynamo', async (t) => {
+test.serial('migrateExecutionRecord throws error on invalid source data from Dynamo', async (t) => {
+  const newExecution = fakeExecutionFactoryV2();
+
+  // make source record invalid
+  delete newExecution.arn;
+
+  await t.throwsAsync(migrateExecutionRecord(newExecution, t.context.knex));
+});
+
+test.serial('migrateExecutionRecord handles nullable fields on source execution data', async (t) => {
+  const newExecution = fakeExecutionFactoryV2();
+
+  // // remove nullable fields
+  delete newExecution.asyncOperationId;
+  delete newExecution.collectionId;
+  delete newExecution.tasks;
+  delete newExecution.error;
+  delete newExecution.duration;
+  delete newExecution.originalPayload;
+  delete newExecution.finalPayload;
+  delete newExecution.timestamp;
+
+  await migrateExecutionRecord(newExecution, t.context.knex);
+
+  const createdRecord = await t.context.knex.queryBuilder()
+    .select()
+    .table('executions')
+    .where({ arn: newExecution.arn })
+    .first();
+
+  t.deepEqual(
+    omit(createdRecord, ['cumulus_id']),
+    omit(
+      {
+        ...newExecution,
+        async_operation_cumulus_id: null,
+        collection_cumulus_id: null,
+        cumulus_version: null,
+        url: null,
+        duration: null,
+        error: null,
+        parent_cumulus_id: null,
+        workflow_name: newExecution.name,
+        original_payload: null,
+        final_payload: null,
+        created_at: new Date(newExecution.createdAt),
+        updated_at: new Date(newExecution.updatedAt),
+        timestamp: null,
+        tasks: null,
+      },
+      executionOmitList
+    )
+  );
+});
+
+test.serial('migrateExecutionRecord ignores extraneous fields from Dynamo', async (t) => {
 
 });
 
-test('migrateExecutionRecord handles nullable fields on source execution data', async (t) => {
-
-});
-
-test('migrateExecutionRecord ignores extraneous fields from Dynamo', async (t) => {
-
-});
-
-test('migrateExecutionRecord skips already migrated record', async (t) => {
+test.serial('migrateExecutionRecord skips already migrated record', async (t) => {
 
 });
--- packages/db/src/executions.ts
+++ packages/db/src/executions.ts
@@ -2,8 +2,11 @@ import Knex from 'knex';
 
 import { ExecutionRecord } from '@cumulus/types/api/executions';
 import { PostgresExecution } from './types';
+// TODO move to common location
 const {
   getParentExecutionCumulusId,
+  getAsyncOperationCumulusId,
+  getCollectionCumulusId,
 } = require('../../api/lambdas/sf-event-sqs-to-db-records/utils');
 
 /**
@@ -22,10 +25,7 @@ export const translateApiExecutionToPostgresExecution = async (
   // Map old record to new schema.
   const translatedRecord: PostgresExecution = {
     async_operation_cumulus_id: (
-      dynamoRecord.asyncOperationId ? Number(dynamoRecord.asyncOperationId) : undefined
-    ),
-    collection_cumulus_id: (
-      dynamoRecord.collectionId ? Number(dynamoRecord.collectionId) : undefined
+      dynamoRecord.asyncOperationId ? await getAsyncOperationCumulusId(dynamoRecord.asyncOperationId, knex) : undefined
     ),
     status: dynamoRecord.status,
     tasks: JSON.stringify(dynamoRecord.tasks),
@@ -34,9 +34,18 @@ export const translateApiExecutionToPostgresExecution = async (
     duration: dynamoRecord.duration,
     original_payload: JSON.stringify(dynamoRecord.originalPayload),
     final_payload: JSON.stringify(dynamoRecord.finalPayload),
-    timestamp: new Date(Number(dynamoRecord.timestamp)),
     workflow_name: dynamoRecord.name,
   };
+
+  if (dynamoRecord.timestamp) {
+    translatedRecord.timestamp = new Date(Number(dynamoRecord.timestamp));
+  }
+
+  if (dynamoRecord.collectionId) {
+    // TODO is there a helper for this?
+    const collectionNameVersionArray = dynamoRecord.collectionId.split('___');
+    translatedRecord.collection_cumulus_id = await getCollectionCumulusId({ name: collectionNameVersionArray[0], version: collectionNameVersionArray[1] }, knex);
+  }
 
   // If we have a parentArn, try a lookup in Postgres. If there's a match, set the parent_cumulus_id
   if (dynamoRecord.parentArn !== undefined) {
