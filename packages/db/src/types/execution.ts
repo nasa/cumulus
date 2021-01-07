e5fc497038dbd4be43e1806bcfe93766832b3dd5
Nate Pauzenga <npauzenga@gmail.com>
2021-01-07 14:54:52 -0500

    CUMULUS-2188 update type definition location and translation .ts to new locations per new conventions

--- packages/db/src/types/execution.ts
+++ packages/db/src/types/execution.ts
@@ -1,10 +1,20 @@
+import { ExecutionRecordStatus } from '@cumulus/types/api/executions';
+
 export interface PostgresExecution {
   arn: string,
-  status: string,
   async_operation_cumulus_id?: number,
   collection_cumulus_id?: number,
   parent_cumulus_id?: number,
   cumulus_version?: string,
+  url?: string,
+  status?: ExecutionRecordStatus,
+  tasks?: Object, // TODO need specific type?
+  error?: Object, // TODO need specific type?
+  workflow_name?: string,
+  duration?: number,
+  original_payload?: Object,
+  final_payload?: Object
+  timestamp?: Date,
   created_at?: Date,
   updated_at?: Date,
 }
