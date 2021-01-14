43a43c1c860116a25274556173ce41e56d83d65e
Jonathan Kovarik <jonathan.kovarik@gmail.com>
2021-01-14 09:08:30 -0700

    Alpha sort updated fields

--- lambdas/data-migration1/main.tf
+++ lambdas/data-migration1/main.tf
@@ -43,9 +43,9 @@ data "aws_iam_policy_document" "data_migration1" {
       "dynamodb:Scan"
     ]
     resources = [
+      var.dynamo_tables.async_operations.arn,
       var.dynamo_tables.collections.arn,
       var.dynamo_tables.providers.arn,
-      var.dynamo_tables.async_operations.arn,
       var.dynamo_tables.rules.arn
     ]
   }
@@ -100,13 +100,13 @@ resource "aws_lambda_function" "data_migration1" {
 
   environment {
     variables = {
-      databaseCredentialSecretArn = var.rds_user_access_secret_arn
-      RulesTable = var.dynamo_tables.rules.name
       AsyncOperationsTable = var.dynamo_tables.async_operations.name
       CollectionsTable = var.dynamo_tables.collections.name
-      ProvidersTable = var.dynamo_tables.providers.name
+      databaseCredentialSecretArn = var.rds_user_access_secret_arn
       dbHeartBeat = var.rds_connection_heartbeat
       provider_kms_key_id = var.provider_kms_key_id
+      ProvidersTable = var.dynamo_tables.providers.name
+      RulesTable = var.dynamo_tables.rules.name
     }
   }
 
