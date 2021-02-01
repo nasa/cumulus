148c88cd8a72ffe83eea136d75a7b208a0ffca8d
Danielle Peters <dopeter1@mbp-dopeter1.edn.ecs.nasa.gov>
2021-02-01 15:25:58 -0500

    Change workflows to workflow

--- tf-modules/ingest/queue-workflow-task.tf
+++ tf-modules/ingest/queue-workflow-task.tf
@@ -1,7 +1,7 @@
-resource "aws_lambda_function" "queue_workflows_task" {
-  function_name    = "${var.prefix}-QueueWorkflows"
-  filename         = "${path.module}/../../tasks/queue-workflows/dist/lambda.zip"
-  source_code_hash = filebase64sha256("${path.module}/../../tasks/queue-workflows/dist/lambda.zip")
+resource "aws_lambda_function" "queue_workflow_task" {
+  function_name    = "${var.prefix}-QueueWorkflow"
+  filename         = "${path.module}/../../tasks/queue-workflow/dist/lambda.zip"
+  source_code_hash = filebase64sha256("${path.module}/../../tasks/queue-workflow/dist/lambda.zip")
   handler          = "index.handler"
   role             = var.lambda_processing_role_arn
   runtime          = "nodejs12.x"
@@ -31,8 +31,8 @@ resource "aws_lambda_function" "queue_workflows_task" {
   tags = var.tags
 }
 
-resource "aws_cloudwatch_log_group" "queue_workflows_task" {
-  name              = "/aws/lambda/${aws_lambda_function.queue_workflows_task.function_name}"
+resource "aws_cloudwatch_log_group" "queue_workflow_task" {
+  name              = "/aws/lambda/${aws_lambda_function.queue_workflow_task.function_name}"
   retention_in_days = 30
   tags              = var.tags
 }
