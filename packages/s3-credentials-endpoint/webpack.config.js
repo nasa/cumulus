865a765b379a16b5e6491e7cee05878cde12388f
Matt Savoie <savoie@nsidc.org>
2021-02-04 14:29:11 -0700

    Adds new endpoint to distribution module at /s3credentialsREADME (#2072)

--- packages/s3-credentials-endpoint/webpack.config.js
+++ packages/s3-credentials-endpoint/webpack.config.js
@@ -1,5 +1,6 @@
 'use strict';
 
+const CopyPlugin = require('copy-webpack-plugin');
 const path = require('path');
 // path to module root
 const root = path.resolve(__dirname);
@@ -7,18 +8,29 @@ const root = path.resolve(__dirname);
 module.exports = {
   mode: process.env.PRODUCTION ? 'production' : 'development',
   entry: './index.js',
+  plugins: [
+    // templates to include html for readme
+    new CopyPlugin({
+      patterns: [
+        {
+          from: 'instructions',
+          to: 'instructions',
+        },
+      ],
+    }),
+  ],
   output: {
     libraryTarget: 'commonjs2',
     filename: 'index.js',
     devtoolModuleFilenameTemplate: (info) => {
-      const relativePath = path.relative(root, info.absoluteResourcePath)
+      const relativePath = path.relative(root, info.absoluteResourcePath);
       return `webpack://${relativePath}`;
-    }
+    },
   },
   externals: [
     'aws-sdk',
     'electron',
-    { formidable: 'url' }
+    { formidable: 'url' },
   ],
   module: {
     rules: [
@@ -29,7 +41,7 @@ module.exports = {
           {
             loader: 'babel-loader',
             options: {
-              cacheDirectory: true
+              cacheDirectory: true,
             },
           },
         ],
@@ -38,13 +50,16 @@ module.exports = {
   },
   devtool: 'inline-source-map',
   target: 'node',
+  node: {
+    __dirname: false,
+  },
   // https://github.com/webpack/webpack/issues/196#issuecomment-620227719
   stats: {
     warningsFilter: [
-      /critical dependency:/i
+      /critical dependency:/i,
     ],
   },
   optimization: {
-    nodeEnv: false
-  }
+    nodeEnv: false,
+  },
 };
