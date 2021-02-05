79c6d2a42cdcfc5ed854c2abe0e7f962fb3b2b05
Jonathan Kovarik <jonathan.kovarik@gmail.com>
2021-02-04 17:12:59 -0700

    Minor comment edit

--- example/spec/parallel/discoverGranules/DiscoverGranulesS3SuccessSpec.js
+++ example/spec/parallel/discoverGranules/DiscoverGranulesS3SuccessSpec.js
@@ -40,7 +40,7 @@ describe('The DiscoverGranules workflow', () => {
 
     const testId = randomString();
 
-    // Create the provide
+    // Create the provider
     provider = await loadProvider({
       filename: './data/providers/s3/s3_provider.json',
       postfix: testId,
