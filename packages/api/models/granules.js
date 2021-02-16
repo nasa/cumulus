6133ec5635d8a6b65b6755d02b2d7fc57da9eeec
Nate Pauzenga <npauzenga@gmail.com>
2021-02-16 12:41:06 -0500

    CUMULUS-2309 remove unused requires

--- packages/api/models/granules.js
+++ packages/api/models/granules.js
@@ -1,12 +1,10 @@
 'use strict';
 
 const cloneDeep = require('lodash/cloneDeep');
-const get = require('lodash/get');
 const isArray = require('lodash/isArray');
 const isString = require('lodash/isString');
 const partial = require('lodash/partial');
 const path = require('path');
-const pMap = require('p-map');
 
 const awsClients = require('@cumulus/aws-client/services');
 const Lambda = require('@cumulus/aws-client/Lambda');
