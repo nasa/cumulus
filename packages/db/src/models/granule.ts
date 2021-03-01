45849468019bb6425a67b5af00fce95ec53fca9a
Mark Boyd <markdavidboyd@gmail.com>
2021-03-01 15:15:50 -0500

    revert change to make execution cumulus ID optional to granulePgModel.upsert()

--- packages/db/src/models/granule.ts
+++ packages/db/src/models/granule.ts
@@ -17,32 +17,29 @@ export default class GranulePgModel extends BasePgModel<PostgresGranule, Postgre
   async upsert(
     knexOrTrx: Knex | Knex.Transaction,
     granule: PostgresGranule,
-    executionCumulusId?: number,
+    executionCumulusId: number,
     granulesExecutionsPgModel = new GranulesExecutionsPgModel()
   ) {
     if (granule.status === 'running') {
-      const upsertQuery = knexOrTrx(this.tableName)
+      return knexOrTrx(this.tableName)
         .insert(granule)
         .onConflict(['granule_id', 'collection_cumulus_id'])
         .merge({
           status: granule.status,
           timestamp: granule.timestamp,
           updated_at: granule.updated_at,
-        });
-      if (executionCumulusId) {
+        })
         // Only do the upsert if there IS NOT already a record associating
         // the granule to this execution. If there IS already a record
         // linking this granule to this execution, then this upsert query
         // will not affect any rows.
-        upsertQuery.whereNotExists(
+        .whereNotExists(
           granulesExecutionsPgModel.search(
             knexOrTrx,
             { execution_cumulus_id: executionCumulusId }
           )
-        );
-      }
-      upsertQuery.returning('cumulus_id');
-      return upsertQuery;
+        )
+        .returning('cumulus_id');
     }
     return knexOrTrx(this.tableName)
       .insert(granule)
