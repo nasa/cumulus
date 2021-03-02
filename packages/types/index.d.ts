fe07d273c189aacfab72b71b62fb83d75db20dba
Mark Boyd <markdavidboyd@gmail.com>
2021-03-02 12:05:46 -0500

    update grnaule API type

--- packages/types/index.d.ts
+++ packages/types/index.d.ts
@@ -2,4 +2,6 @@ export type DuplicateHandling = 'error' | 'replace' | 'skip' | 'version';
 
 export * as Message from './message';
 
+export * from './api/files';
+export * from './api/granules';
 export * from './api/providers';
