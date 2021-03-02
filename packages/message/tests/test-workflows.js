a553a52ca56e1edf9c7868d1983399088ce5c05a
Jonathan Kovarik <jonathan.kovarik@gmail.com>
2021-03-02 14:39:45 -0700

    Add missing unit test

---
+++ packages/message/tests/test-workflows.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const test = require('ava');
+
+const { getMessageWorkflowStartTime } = require('../workflows');
+
+test('getCreatedAt returns the workflow start time', (t) => {
+  const testMessage = {
+    cumulus_meta: {
+      workflow_start_time: 123456,
+    },
+  };
+  const result = getMessageWorkflowStartTime(testMessage);
+  t.is(result, 123456);
+});
+
+test('getMessageGranules throws when a start time is not present in the message', (t) => {
+  const testMessage = {};
+  t.throws(() => getMessageWorkflowStartTime(testMessage));
+});
