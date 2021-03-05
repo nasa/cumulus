db3561fd37feaa68df34c7f95982eb68f8754a82
Jonathan Kovarik <kovarik@nsidc.org>
2021-03-05 15:18:50 -0700

    Update packages/api/tests/models/granules/test-granule-reporting.js
    Co-authored-by: Mark Boyd <markdavidboyd@gmail.com>

--- packages/api/tests/models/granules/test-granule-reporting.js
+++ packages/api/tests/models/granules/test-granule-reporting.js
@@ -246,7 +246,7 @@ test('_validateAndStoreGranuleRecord() will not allow a final status for an olde
   t.is(fetchedItem.status, 'running');
 });
 
-test('_validateAndStoreGranuleRecord() will not allow an older final status to replace a final status for a newer execution ', async (t) => {
+test('_validateAndStoreGranuleRecord() will not allow a final status for an older execution to replace a final status for a newer execution ', async (t) => {
   const { granuleModel } = t.context;
 
   const timeVal = Date.now();
