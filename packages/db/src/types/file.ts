30bc79652791ef586b8b050866e2465f158c4061
Nate Pauzenga <npauzenga@gmail.com>
2021-03-05 10:39:21 -0500

    CUMULUS-2385 update required keys on TS type definition

--- packages/db/src/types/file.ts
+++ packages/db/src/types/file.ts
@@ -1,11 +1,11 @@
 export interface PostgresFile {
   granule_cumulus_id: number
-  bucket?: string
+  bucket: string
   checksum_type?: string
   checksum_value?: string
   file_name?: string
   file_size?: number
-  key?: string
+  key: string
   path?: string
   source?: string
 }
