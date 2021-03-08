9418a5dcaa607fd35e91fdac05d37b6ef573e17f
Nate Pauzenga <npauzenga@gmail.com>
2021-03-08 10:58:02 -0500

    CUMULUS-2385 if no file bucket/key provided, assign empty string during dynamo to PG migration

--- packages/db/src/translate/file.ts
+++ packages/db/src/translate/file.ts
@@ -5,12 +5,12 @@ import { PostgresFile } from '../types/file';
 export const translateApiFiletoPostgresFile = (
   file: ApiFile
 ): Omit<PostgresFile, 'granule_cumulus_id'> => ({
-  bucket: file.bucket,
+  bucket: file.bucket || '',
   checksum_type: file.checksumType,
   checksum_value: file.checksum,
   file_name: file.fileName,
   file_size: file.size,
-  key: file.key,
+  key: file.key || '',
   path: file.path,
   source: file.source,
 });
