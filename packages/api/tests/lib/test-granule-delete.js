80097dbb32342deba330fd19619bfa7735a5c838
Jonathan Kovarik <jonathan.kovarik@gmail.com>
2021-03-09 21:41:29 -0700

    Fix errant spacing

--- packages/api/tests/lib/test-granule-delete.js
+++ packages/api/tests/lib/test-granule-delete.js
@@ -146,7 +146,7 @@ test.serial('deleteGranuleAndFiles() removes granule and files from Postgres, Dy
   // Check Dynamo and RDS. The granule should have been removed from both.
   t.false(await granuleModel.exists({ granuleId: newDynamoGranule.granuleId }));
   t.false(await granulePgModel.exists(t.context.knex, { granule_id: newPgGranule.granule_id }));
-  
+
   // Verify files were deleted from S3 and Postgres
   await Promise.all(
     files.map(async (file) => {
