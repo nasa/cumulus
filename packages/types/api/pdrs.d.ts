1b3cd115b7c1c067cdc4f67c82cfccea19049f7c
Jonathan Kovarik <jonathan.kovarik@gmail.com>
2021-03-25 08:03:14 -0600

    Add missing types file

---
+++ packages/types/api/pdrs.d.ts
@@ -0,0 +1,24 @@
+export type pdrStatus = 'running' | 'failed' | 'completed'
+
+export interface ApiPdr {
+  pdrName: string,
+  provider: string,
+  collectionId: string,
+  status: pdrStatus,
+  createdAt?: number,
+  progress?: number,
+  execution?: string,
+  PANSent?: boolean,
+  PANmessage?: string,
+  stats?: {
+    total?: number,
+    completed?: number,
+    failed?: number,
+    processing?: number,
+  },
+  address?: string,
+  originalUrl?: string,
+  timestamp?: number,
+  duration?: number,
+  updatedAt?: number,
+}
