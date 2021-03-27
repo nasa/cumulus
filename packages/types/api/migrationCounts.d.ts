8270eae02c674f0c8a77ec6f63db6b1db4df9f76
Jonathan Kovarik <jonathan.kovarik@gmail.com>
2021-03-26 21:41:50 -0600

    Fix Payload type

--- packages/types/api/migrationCounts.d.ts
+++ packages/types/api/migrationCounts.d.ts
@@ -1,7 +1,7 @@
 export interface MigrationCountsPayload {
-  reportBucket: string,
-  reportPath: string,
-  cutoffSeconds: number,
-  dbConcurrency: number,
-  dbMaxPool: number
+  reportBucket?: string,
+  reportPath?: string,
+  cutoffSeconds?: number,
+  dbConcurrency?: number,
+  dbMaxPool?: number
 }
