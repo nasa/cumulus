f4b859bc7c71afc14694338970f6d72669c7ca9a
Jonathan Kovarik <jonathan.kovarik@gmail.com>
2021-04-09 14:23:03 -0600

    Revert db migration name changes, remove name collisions for migration lambda

--- lambdas/db-migration/main.tf
+++ lambdas/db-migration/main.tf
@@ -42,7 +42,7 @@ data "aws_iam_policy_document" "db_migration" {
 }
 
 resource "aws_iam_role_policy" "db_migration" {
-  name   = "${var.prefix}_db_migration"
+  name   = "${var.prefix}_postgres_db_migration"
   role   = aws_iam_role.db_migration.id
   policy = data.aws_iam_policy_document.db_migration.json
 }
@@ -64,7 +64,7 @@ resource "aws_security_group" "db_migration" {
 }
 
 resource "aws_lambda_function" "db_migration" {
-  function_name    = "${var.prefix}-db-migration"
+  function_name    = "${var.prefix}-postgres-db-migration"
   filename         = local.lambda_path
   source_code_hash = filebase64sha256(local.lambda_path)
   handler          = "index.handler"
