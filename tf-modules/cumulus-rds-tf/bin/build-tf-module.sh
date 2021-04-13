6030a38a5a5258e5e598b3388011b3ea5dff50e3
Jonathan Kovarik <jonathan.kovarik@gmail.com>
2021-04-13 14:03:18 -0600

    Remove garbage directory creation

--- tf-modules/cumulus-rds-tf/bin/build-tf-module.sh
+++ tf-modules/cumulus-rds-tf/bin/build-tf-module.sh
@@ -4,7 +4,7 @@ set -e
 # This is meant to be run from the tf-modules/cumulus-rds-tf directory as part of the bamboo/create-release.sh script
 (cd ../../lambdas/db-provision-user-database && npm run package)
 
-mkdir -p dist/provision_database
+mkdir -p dist
 
 zip dist/terraform-aws-cumulus-rds.zip \
   *.tf \
