c3d8bb7a39f7b6a9b4974b152f86fcadfdfc8230
jennyhliu <34660846+jennyhliu@users.noreply.github.com>
2021-04-14 13:51:14 -0400

    CUMULUS-2348: Add Granule Recovery status to granules endpoint (#2167)
    * CUMULUS-2348:update granules endpoints to take getRecoveryStatus parameter
    
    * add orca unit test, notFound when orca is not installed
    
    * log error when orca endpoint status is not 200
    
    * log error
    
    * fix orca test lint
    
    * fix getGranule unit test, add change log
    
    * @cumulus/api@7.2.1-alpha.0
    
    * response should have statusCode
    
    * add include_orca to cumulus-sit
    
    * Update CHANGELOG.md
    
    * add @cumulus/api-client to api package

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,6 +6,14 @@ The format is based on [Keep a Changelog](http://keepachangelog.com/en/1.0.0/).
 
 ## [Unreleased]
 
+### Added
+- **CUMULUS-2348**
+  - The `@cumulus/api` `/granules` and `/granules/{granuleId}` endpoints now take `getRecoveryStatus` parameter
+  to include recoveryStatus in result granule(s)
+  - The `@cumulus/api-client.granules.getGranule` function takes a `query` parameter which can be used to
+  request additional granule information.
+  - Published `@cumulus/api@7.2.1-alpha.0` for dashboard testing
+
 ## [v8.0.0] 2021-04-08
 
 ### BREAKING CHANGES
---
+++ packages/api/tests/lib/test-orca.js
@@ -0,0 +1,142 @@
+'use strict';
+
+const test = require('ava');
+const proxyquire = require('proxyquire');
+const sinon = require('sinon');
+const { randomId } = require('@cumulus/common/test-utils');
+const { fakeGranuleFactoryV2 } = require('../../lib/testUtils');
+
+const sandbox = sinon.createSandbox();
+const fakeListRequests = sandbox.stub();
+
+const orca = proxyquire('../../lib/orca', {
+  '@cumulus/api-client/orca': { listRequests: fakeListRequests },
+});
+
+const recoveryRequestFactory = (options) => (
+  {
+    granule_id: options.granuleId || randomId('granuleId'),
+    object_key: randomId('objectKey'),
+    job_type: 'restore',
+    job_status: options.status || 'inprogress',
+  });
+
+test.afterEach.always(() => {
+  sandbox.reset();
+});
+
+test.after.always(() => {
+  sandbox.restore();
+});
+
+test.serial(
+  'getOrcaRecoveryStatusByGranuleId returns undefined status when orca endpoint returns error',
+  async (t) => {
+    const granuleId = randomId('granId');
+    fakeListRequests.resolves({
+      statusCode: 400,
+      error: 'Bad Request',
+      message: 'Function not found: prefix_request_status, please check if orca is deployed',
+    });
+    const status = await orca.getOrcaRecoveryStatusByGranuleId(granuleId);
+    t.is(status, undefined);
+  }
+);
+
+test.serial(
+  'getOrcaRecoveryStatusByGranuleId returns undefined status when recovery request for granule is not found',
+  async (t) => {
+    const granuleId = randomId('granId');
+    const recoveryRequests = [];
+    fakeListRequests.resolves({
+      statusCode: 200,
+      body: JSON.stringify(recoveryRequests),
+    });
+    const status = await orca.getOrcaRecoveryStatusByGranuleId(granuleId);
+    t.is(status, undefined);
+  }
+);
+
+test.serial(
+  'getOrcaRecoveryStatusByGranuleId returns running status when files are still in progress',
+  async (t) => {
+    const granuleId = randomId('granId');
+    const recoveryRequests = [
+      recoveryRequestFactory({ granuleId, status: 'complete' }),
+      recoveryRequestFactory({ granuleId, status: 'inprogress' }),
+      recoveryRequestFactory({ granuleId, status: 'error' }),
+    ];
+    fakeListRequests.resolves({
+      statusCode: 200,
+      body: JSON.stringify(recoveryRequests),
+    });
+    const status = await orca.getOrcaRecoveryStatusByGranuleId(granuleId);
+    t.is(status, 'running');
+  }
+);
+
+test.serial(
+  'getOrcaRecoveryStatusByGranuleId returns running status when files are complete',
+  async (t) => {
+    const granuleId = randomId('granId');
+    const recoveryRequests = [
+      recoveryRequestFactory({ granuleId, status: 'complete' }),
+      recoveryRequestFactory({ granuleId, status: 'complete' }),
+    ];
+    fakeListRequests.resolves({
+      statusCode: 200,
+      body: JSON.stringify(recoveryRequests),
+    });
+    const status = await orca.getOrcaRecoveryStatusByGranuleId(granuleId);
+    t.is(status, 'completed');
+  }
+);
+
+test.serial(
+  'getOrcaRecoveryStatusByGranuleId returns running status when file restore has error',
+  async (t) => {
+    const granuleId = randomId('granId');
+    const recoveryRequests = [
+      recoveryRequestFactory({ granuleId, status: 'complete' }),
+      recoveryRequestFactory({ granuleId, status: 'error' }),
+    ];
+    fakeListRequests.resolves({
+      statusCode: 200,
+      body: JSON.stringify(recoveryRequests),
+    });
+    const status = await orca.getOrcaRecoveryStatusByGranuleId(granuleId);
+    t.is(status, 'failed');
+  }
+);
+
+test.serial(
+  'addOrcaRecoveryStatus adds recovery status to granules',
+  async (t) => {
+    const granuleIds = [randomId('granId'), randomId('granId')];
+    const inputResponse = {
+      results: [
+        fakeGranuleFactoryV2({ granuleId: granuleIds[0] }),
+        fakeGranuleFactoryV2({ granuleId: granuleIds[1] })],
+    };
+    const recoveryRequestsGranule1 = [
+      recoveryRequestFactory({ granuleId: granuleIds[0], status: 'inprogress' }),
+      recoveryRequestFactory({ granuleId: granuleIds[0], status: 'inprogress' }),
+    ];
+
+    const recoveryRequestsGranule2 = [
+      recoveryRequestFactory({ granuleId: granuleIds[1], status: 'complete' }),
+      recoveryRequestFactory({ granuleId: granuleIds[1], status: 'error' }),
+    ];
+
+    fakeListRequests.onCall(0)
+      .returns({ statusCode: 200, body: JSON.stringify(recoveryRequestsGranule1) });
+    fakeListRequests.onCall(1)
+      .returns({ statusCode: 200, body: JSON.stringify(recoveryRequestsGranule2) });
+
+    const updatedResponse = await orca.addOrcaRecoveryStatus(inputResponse);
+    const granules = updatedResponse.results;
+    t.is(granules.length, 2);
+    t.is(granules[0].recoveryStatus, 'running');
+    t.is(granules[1].recoveryStatus, 'failed');
+  }
+);
