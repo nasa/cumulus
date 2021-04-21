54544cb48d8937dae00a79301bd382354964a4f0
Menno Van Diermen <mvandiermen@contractor.usgs.gov>
2021-04-21 10:37:03 -0500

    process dead letters in batches

--- packages/api/lambdas/process-s3-dead-letter-archive.js
+++ packages/api/lambdas/process-s3-dead-letter-archive.js
@@ -2,7 +2,8 @@
 
 const log = require('@cumulus/common/log');
 
-const { getJsonS3Object, listS3ObjectsV2, deleteS3Object } = require('@cumulus/aws-client/S3');
+const { s3 } = require('@cumulus/aws-client/services');
+const { getJsonS3Object, deleteS3Object } = require('@cumulus/aws-client/S3');
 const { getKnexClient } = require('@cumulus/db');
 const { getMessageExecutionName } = require('@cumulus/message/Executions');
 
@@ -14,24 +15,29 @@ async function processDeadLetterArchive({
   path = `${process.env.stackName}/dead-letter-archive/sqs/`,
   writeRecordsFunction = writeRecords,
 }) {
-  // TODO: figure out a way to cap this so we don't read a billion objects into memory
-  const deadLetterObjects = await listS3ObjectsV2({
-    Bucket: bucket,
-    Prefix: path,
-  });
-  return Promise.allSettled(deadLetterObjects.map(
-    async (deadLetterObject) => {
-      const cumulusMessage = await getJsonS3Object(bucket, deadLetterObject.Key);
-      try {
-        await writeRecordsFunction({ cumulusMessage, knex });
-        return deleteS3Object(bucket, deadLetterObject.Key);
-      } catch (err) {
-        const executionName = getMessageExecutionName(cumulusMessage);
-        log.error(`Failed to write records from cumulusMessage for execution ${executionName}, reason: `, err);
-        throw err;
+  let listObjectsResponse;
+  /* eslint-disable no-await-in-loop */
+  do {
+    listObjectsResponse = await s3().listObjectsV2({
+      Bucket: bucket,
+      Prefix: path,
+    }).promise();
+    const deadLetterObjects = listObjectsResponse.Contents;
+    await Promise.allSettled(deadLetterObjects.map(
+      async (deadLetterObject) => {
+        const cumulusMessage = await getJsonS3Object(bucket, deadLetterObject.Key);
+        try {
+          await writeRecordsFunction({ cumulusMessage, knex });
+          return deleteS3Object(bucket, deadLetterObject.Key);
+        } catch (error) {
+          const executionName = getMessageExecutionName(cumulusMessage);
+          log.error(`Failed to write records from cumulusMessage for execution ${executionName}, reason: `, error);
+          throw error;
+        }
       }
-    }
-  ));
+    ));
+  } while (listObjectsResponse.isTruncated);
+  /* eslint-enable no-await-in-loop */
 }
 
 /**
