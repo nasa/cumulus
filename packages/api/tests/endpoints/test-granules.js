febe7c9f84b4b9833b2dfee481b34556f8390a95
Jonathan Kovarik <jonathan.kovarik@gmail.com>
2021-04-26 15:30:56 -0600

    Improve test title

--- packages/api/tests/endpoints/test-granules.js
+++ packages/api/tests/endpoints/test-granules.js
@@ -952,7 +952,7 @@ test.serial('move a granule with no .cmr.xml file', async (t) => {
   );
 });
 
-test.serial('when a granule fails to move a file correctly records the expected granule files in postgres and dynamo', async (t) => {
+test.serial('When a move granule request fails to move a file correctly, it records the expected granule files in postgres and dynamo', async (t) => {
   const bucket = process.env.system_bucket;
   const secondBucket = randomId('second');
   const thirdBucket = randomId('third');
