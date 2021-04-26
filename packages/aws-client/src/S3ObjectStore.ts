02354d56d386f2373a93cb9961ce0aec58cc2899
Danielle Peters <danielle@element84.com>
2021-04-26 15:05:53 -0400

    Add S3ObjectStore to aws-client

---
+++ packages/aws-client/src/S3ObjectStore.ts
@@ -0,0 +1,79 @@
+import aws from 'aws-sdk';
+import * as querystring from 'querystring';
+import { URL } from 'url';
+import { s3 } from './services';
+import { headObject, parseS3Uri } from './S3';
+
+/**
+ * Class to use when interacting with S3
+ *
+ */
+export class S3ObjectStore {
+  s3: aws.S3;
+
+  /**
+   * Builds and returns an S3 object store
+   */
+  constructor() {
+    this.s3 = s3();
+  }
+
+  /**
+   * Returns an HTTPS URL that can be used to perform a GET on the given object
+   * store URL
+   *
+   * @param {string} objectUrl - the URL of the object to sign
+   * @param {string} params - an optional mapping of parameter key/values to put in the URL
+   * @returns {Promise<string>} a signed URL
+   * @throws TypeError - if the URL is not a recognized protocol or cannot be parsed
+   */
+  async signGetObject(objectUrl: string, params: { [key: string]: string }): Promise<string> {
+    const url = new URL(objectUrl);
+    if (url.protocol.toLowerCase() !== 's3:') {
+      throw new TypeError(`Invalid S3 URL: ${objectUrl}`);
+    }
+
+    const { Bucket, Key } = parseS3Uri(objectUrl);
+
+    // Verifies that the object exists, or throws NotFound
+    await headObject(Bucket, Key);
+    const req = this.s3.getObject({ Bucket, Key });
+
+    if (params && req.on) {
+      (req.on('build', () => { req.httpRequest.path += `?${querystring.stringify(params)}`; }));
+    }
+    // TypeScript doesn't recognize that req has a presign method.  It does.
+    const result = await (req as any).presign();
+    return result;
+  }
+}
+
+/**
+ * Returns a class to interact with the object store appropriate for
+ * the provided protocol, or null if no such store exists.
+ *
+ * @param {string} protocol - the protocol used in object store URLs.  This may be a full URL, in
+ *   which case the protocol will be read from the front of the URL.
+ * @returns {S3ObjectStore} an object store for interacting with the given protocol
+ */
+export function objectStoreForProtocol(protocol?: string): S3ObjectStore|undefined {
+  if (!protocol) {
+    return undefined;
+  }
+  // Make sure the protocol is lowercase and does not end in a colon (as URL parsing produces)
+  const normalizedProtocol = protocol.toLowerCase().split(':')[0];
+  if (normalizedProtocol === 's3') {
+    return new S3ObjectStore();
+  }
+  return undefined;
+}
+
+/**
+ * Returns the default object store for this instance of Harmony.  Allows requesting an
+ * object store without first knowing a protocol.
+ *
+ * @returns {S3ObjectStore} the default object store for Harmony.
+ */
+export function defaultObjectStore(): S3ObjectStore {
+  return new S3ObjectStore();
+}
---
+++ packages/aws-client/tests/test-S3ObjectStore.js
@@ -0,0 +1,59 @@
+const test = require('ava');
+const sinon = require('sinon');
+const { defaultObjectStore, objectStoreForProtocol, S3ObjectStore } = require('../S3ObjectStore');
+const S3 = require('../S3');
+
+test('objectStoreForProtocol returns null when no protocol is supplied', (t) => {
+  t.is(objectStoreForProtocol(), undefined);
+});
+
+test('objectStoreForProtocol returns null when an unrecognized protocol is supplied', (t) => {
+  t.is(objectStoreForProtocol('azure'), undefined);
+});
+
+test('objectStoreForProtocol returns an S3ObjectStore when "s3" is supplied as the protocol', (t) => {
+  t.true(objectStoreForProtocol('s3') instanceof S3ObjectStore);
+});
+
+test('objectStoreForProtocol ignores trailing colons on the protocol', (t) => {
+  t.true(objectStoreForProtocol('s3:') instanceof S3ObjectStore);
+});
+
+test('defaultObjectStore returns an S3 object store', (t) => {
+  t.true(defaultObjectStore() instanceof S3ObjectStore);
+});
+
+test.serial('S3ObjectStore.signGetObject throws NotFound from S3.headObject when object does not exist', async (t) => {
+  const store = new S3ObjectStore();
+  const headObjectResponse = { Metadata: { foo: 'bar' }, ContentType: 'image/png' };
+  const headObjectStub = sinon.stub(S3, 'headObject').returns({ promise: () => headObjectResponse });
+  await store.signGetObject('s3://example-bucket/example/path.txt', { 'A-userid': 'joe' });
+  t.true(headObjectStub.calledOnce);
+  t.teardown(() => {
+    headObjectStub.restore();
+  });
+});
+
+test.serial('S3ObjectStore.signGetObject calls s3.headObject to make sure the object exists', async (t) => {
+  const store = new S3ObjectStore();
+  const headObjectResponse = { Metadata: { foo: 'bar' }, ContentType: 'image/png' };
+  const headObjectStub = sinon.stub(S3, 'headObject').returns({ promise: () => headObjectResponse });
+  await store.signGetObject('s3://example-bucket/example/path.txt', { 'A-userid': 'joe' });
+  t.true(headObjectStub.calledOnce);
+  t.teardown(() => {
+    headObjectStub.restore();
+  });
+});
+
+test.serial('S3ObjectStore.signGetObject calls s3.getObject in order to call presign', async (t) => {
+  const store = new S3ObjectStore();
+  const headObjectResponse = { Metadata: { foo: 'bar' }, ContentType: 'image/png' };
+  const headObjectStub = sinon.stub(S3, 'headObject').returns({ promise: () => headObjectResponse });
+  const getObjectStub = sinon.stub(store.s3, 'getObject').returns({ presign: () => 'http://example.com/signed' });
+  await store.signGetObject('s3://example-bucket/example/path.txt', { 'A-userid': 'joe' });
+  t.true(getObjectStub.calledOnce);
+  t.teardown(() => {
+    headObjectStub.restore();
+    getObjectStub.restore();
+  });
+});
