44f376fa267cbdaad6f511435013b18f1664e6e2
Mark Boyd <markdavidboyd@gmail.com>
2021-04-28 09:27:59 -0400

    remove unused imports

--- packages/db/src/database.ts
+++ packages/db/src/database.ts
@@ -1,7 +1,1 @@
-import Knex from 'knex';
-
-import { RecordDoesNotExist } from '@cumulus/errors';
-
-import { tableNames } from './tables';
-
 export const isRecordDefined = <T>(record: T) => record !== undefined;
