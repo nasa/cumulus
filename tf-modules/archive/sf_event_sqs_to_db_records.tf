ef2e2a0f81ee4141c88dd6cfe66d000130704cf5
Menno <mvandiermen@contractor.usgs.gov>
2021-05-03 10:05:11 -0500

    update version env to 9.0.0

--- tf-modules/archive/sf_event_sqs_to_db_records.tf
+++ tf-modules/archive/sf_event_sqs_to_db_records.tf
@@ -177,7 +177,7 @@ resource "aws_lambda_function" "sf_event_sqs_to_db_records" {
       PdrsTable       = var.dynamo_tables.pdrs.name
       DeadLetterQueue = aws_sqs_queue.sf_event_sqs_to_db_records_dead_letter_queue.id
       databaseCredentialSecretArn = var.rds_user_access_secret_arn
-      RDS_DEPLOYMENT_CUMULUS_VERSION = "8.0.0"
+      RDS_DEPLOYMENT_CUMULUS_VERSION = "9.0.0"
     }
   }
 
@@ -230,5 +230,4 @@ resource "aws_lambda_function" "write_db_dlq_records_to_s3" {
 
   tags = var.tags
 }
-
 
