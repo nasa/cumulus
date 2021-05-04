d5a00012c04be7e2b60c20de864a9e2e2a4ec6f9
Menno Van Diermen <mvandiermen@contractor.usgs.gov>
2021-05-04 14:20:45 -0500

    unwrap cumulus messages before saving to S3

--- packages/api/lambdas/write-db-dlq-records-to-s3.js
+++ packages/api/lambdas/write-db-dlq-records-to-s3.js
@@ -8,6 +8,30 @@ const { s3PutObject } = require('@cumulus/aws-client/S3');
 const { parseSQSMessageBody } = require('@cumulus/aws-client/SQS');
 const { getMessageExecutionName } = require('@cumulus/message/Executions');
 
+/**
+ * Unwrap dead letter Cumulus message, which may be wrapped in a
+ * States cloudwatch event, which is wrapped in an SQS message.
+ *
+ * @param {Object} messageBody - received SQS message
+ * @returns {Object} the cumulus message or nearest available object
+ */
+function unwrapDeadLetterCumulusMessage(messageBody) {
+  if (messageBody.cumulus_meta !== undefined) return messageBody;
+  if (messageBody.body !== undefined || messageBody.Body !== undefined) {
+    // SQS message case
+    const unwrappedMessageBody = parseSQSMessageBody(messageBody);
+    return unwrapDeadLetterCumulusMessage(unwrappedMessageBody);
+  }
+  if (messageBody.detail !== undefined) {
+    // AWS States event case
+    const { input, output } = messageBody.detail;
+    const unwrappedMessageBody = JSON.parse((output !== undefined ? output : input));
+    return unwrapDeadLetterCumulusMessage(unwrappedMessageBody);
+  }
+  // indeterminate, possibly malformed case
+  return messageBody;
+}
+
 function determineExecutionName(cumulusMessageObject) {
   try {
     return getMessageExecutionName(cumulusMessageObject);
@@ -22,7 +46,8 @@ async function handler(event) {
   if (!process.env.stackName) throw new Error('Could not determine archive path as stackName env var is undefined.');
   const sqsMessages = get(event, 'Records', []);
   await Promise.all(sqsMessages.map(async (sqsMessage) => {
-    const cumulusMessageObject = parseSQSMessageBody(sqsMessage);
+    const messageBody = parseSQSMessageBody(sqsMessage);
+    const cumulusMessageObject = unwrapDeadLetterCumulusMessage(messageBody);
     const executionName = determineExecutionName(cumulusMessageObject);
     // version messages with UUID as workflows can produce multiple messages that may all fail.
     const s3Identifier = `${executionName}-${uuidv4()}`;
@@ -37,4 +62,5 @@ async function handler(event) {
 module.exports = {
   determineExecutionName,
   handler,
+  unwrapDeadLetterCumulusMessage,
 };
