df8b64cef513a52649d796e7db37b29a308af070
Mark Boyd <markdavidboyd@gmail.com>
2021-05-18 10:22:43 -0400

    fix all unit tests in es-client package

--- packages/es-client/tests/test-queries.js
+++ packages/es-client/tests/test-queries.js
@@ -7,15 +7,26 @@ const { randomId } = require('@cumulus/common/test-utils');
 
 const indexer = require('../indexer');
 const { Search } = require('../search');
-const { fakeGranuleFactoryV2 } = require('../../lib/testUtils');
 const { bootstrapElasticSearch } = require('../bootstrap');
 
 const collectionIds = [randomId('collectionId-abc'), randomId('collectionId-efg')];
 const granules = [
-  fakeGranuleFactoryV2({ collectionId: collectionIds[0] }),
-  fakeGranuleFactoryV2({ collectionId: collectionIds[1], granuleId: randomId('granprefix123') }),
-  fakeGranuleFactoryV2({ collectionId: collectionIds[1], granuleId: randomId('granprefix'), status: 'failed' }),
-  fakeGranuleFactoryV2({ collectionId: collectionIds[0], status: 'failed' }),
+  {
+    collectionId: collectionIds[0],
+    granuleId: randomId('granule'),
+    status: 'completed',
+  },
+  {
+    collectionId: collectionIds[1],
+    granuleId: randomId('granprefix123'),
+    status: 'completed',
+  },
+  { collectionId: collectionIds[1], granuleId: randomId('granprefix'), status: 'failed' },
+  {
+    granuleId: randomId('granule'),
+    collectionId: collectionIds[0],
+    status: 'failed',
+  },
 ];
 const reconciliationReports = [
   { type: 'Inventory' },
