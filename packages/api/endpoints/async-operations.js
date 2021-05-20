f04125a6bc0d2f8172a71b7817ef8bfa6f696084
Mark Boyd <markdavidboyd@gmail.com>
2021-05-20 14:15:34 -0400

    add DELETE endpoint for async operations and add unit tests

--- packages/api/endpoints/async-operations.js
+++ packages/api/endpoints/async-operations.js
@@ -3,6 +3,13 @@
 const router = require('express-promise-router')();
 const pick = require('lodash/pick');
 
+const {
+  AsyncOperationPgModel,
+  getKnexClient,
+} = require('@cumulus/db');
+const {
+  RecordDoesNotExist,
+} = require('@cumulus/errors');
 const { Search } = require('../es/search');
 const { AsyncOperation: AsyncOperationModel } = require('../models');
 
@@ -42,7 +49,54 @@ async function getAsyncOperation(req, res) {
   return res.send(pick(asyncOperation, ['id', 'status', 'taskArn', 'description', 'operationType', 'output']));
 }
 
+/**
+ * Delete an async operation
+ *
+ * @param {Object} req - express request object
+ * @param {Object} res - express response object
+ * @returns {Promise<Object>} the promise of express response object
+ */
+async function deleteAsyncOperation(req, res) {
+  const {
+    asyncOperationModel = new AsyncOperationModel({
+      stackName: process.env.stackName,
+      systemBucket: process.env.system_bucket,
+      tableName: process.env.AsyncOperationsTable,
+    }),
+    asyncOperationPgModel = new AsyncOperationPgModel(),
+    knex = await getKnexClient(),
+  } = req.testContext || {};
+
+  const { id } = req.params;
+
+  let existingAsyncOperation;
+  try {
+    existingAsyncOperation = await asyncOperationModel.get({ id });
+  } catch (error) {
+    if (error instanceof RecordDoesNotExist) {
+      return res.boom.notFound('No record found');
+    }
+    throw error;
+  }
+
+  try {
+    await knex.transaction(async (trx) => {
+      await asyncOperationPgModel.delete(trx, { id });
+      await asyncOperationModel.delete({ id });
+    });
+    return res.send({ message: 'Record deleted' });
+  } catch (error) {
+    // Delete is idempotent, so there may not be a DynamoDB
+    // record to recreate
+    if (existingAsyncOperation) {
+      await asyncOperationModel.create(existingAsyncOperation);
+    }
+    throw error;
+  }
+}
+
 router.get('/', list);
 router.get('/:id', getAsyncOperation);
+router.delete('/:id', deleteAsyncOperation);
 
 module.exports = router;
