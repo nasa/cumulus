6f98cdabcba883cce8803df70dc23863cf3ca0d5
Jonathan Kovarik <kovarik@nsidc.org>
2021-05-21 01:09:15 -0600

    Add buckets to granule file fixtures
    The granule files also need a bucket to avoid the logspam

--- example/spec/parallel/ingestGranule/IngestGranuleFailureSpec.js
+++ example/spec/parallel/ingestGranule/IngestGranuleFailureSpec.js
@@ -75,6 +75,11 @@ describe('The Ingest Granule failure workflow', () => {
       // update test data filepaths
       inputPayload = await setupTestGranuleForIngest(config.bucket, inputPayloadJson, granuleRegex, testSuffix, testDataFolder);
 
+      // Add bucket to files to prevent files write failure
+      inputPayload.granules[0].files.forEach((file) => {
+        file.bucket = 'non-existent-bucket';
+      });
+
       // add a non-existent file to input payload to cause lambda error
       const nonexistentFile = {
         path: 'non-existent-path',
