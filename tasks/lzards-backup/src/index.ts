cf12f173b0f38727342af66bd6ea54aeeb701d93
Menno Van Diermen <mvandiermen@contractor.usgs.gov>
2021-05-26 10:27:45 -0500

    desperate, insane logging

--- tasks/lzards-backup/src/index.ts
+++ tasks/lzards-backup/src/index.ts
@@ -39,7 +39,9 @@ export const generateDistributionUrl = async (params: {
   Key: string,
   distributionEndpoint?: string,
 }) => {
+  console.log('hereL1');
   const distributionBucketMap = await fetchDistributionBucketMap();
+  console.log('hereL2');
   return constructDistributionUrl(
     params.Bucket,
     params.Key,
@@ -54,17 +56,20 @@ export const generateDirectS3Url = async (params: {
   Key: string,
   usePassedCredentials?: boolean
 }) => {
+  console.log('hereK1');
   const { roleCreds, Key, Bucket, usePassedCredentials } = params;
   const region = process.env.AWS_REGION || 'us-east-1';
   const secretAccessKey = roleCreds?.Credentials?.SecretAccessKey;
   const sessionToken = roleCreds?.Credentials?.SessionToken;
   const accessKeyId = roleCreds?.Credentials?.AccessKeyId;
-
+  console.log('hereK2');
   const s3AccessTimeoutSeconds = (
     process.env.lzards_s3_link_timeout || S3_LINK_EXPIRY_SECONDS_DEFAULT
   );
   let s3;
+  console.log('hereK3');
   if (!inTestMode() || usePassedCredentials) {
+    console.log('hereK4');
     const s3Config = {
       signatureVersion: 'v4',
       secretAccessKey,
@@ -74,6 +79,7 @@ export const generateDirectS3Url = async (params: {
     };
     s3 = new AWS.S3(s3Config);
   } else {
+    console.log('hereK5');
     coreS3().config.update({ signatureVersion: 'v4' });
     s3 = coreS3();
   }
@@ -89,6 +95,7 @@ export const generateAccessUrl = async (params: {
     distributionEndpoint?: string,
   },
 }) => {
+  console.log('hereJ1');
   const {
     Bucket,
     Key,
@@ -98,7 +105,7 @@ export const generateAccessUrl = async (params: {
       distributionEndpoint,
     },
   } = params;
-
+  console.log('hereJ2');
   try {
     switch ((urlType || 's3')) {
       case 's3': return await generateDirectS3Url({ roleCreds, Bucket, Key });
@@ -106,6 +113,7 @@ export const generateAccessUrl = async (params: {
       default: throw new InvalidUrlTypeError(`${urlType} is not a recognized type for access URL generation`);
     }
   } catch (error) {
+    console.log('hereJ3');
     log.error(`${urlType} access URL generation failed for s3://${Bucket}/${Key}: ${error}`);
     throw error;
   }
@@ -115,6 +123,7 @@ export const setLzardsChecksumQueryType = (
   file: MessageGranuleFilesObject,
   granuleId: string
 ) => {
+  console.log('hereI1');
   if (file.checksumType === 'md5') {
     return { expectedMd5Hash: file.checksum };
   }
@@ -132,6 +141,7 @@ export const postRequestToLzards = async (params: {
   file: MessageGranuleFilesObject,
   granuleId: string,
 }) => {
+  console.log('hereH1');
   const {
     accessUrl,
     authToken,
@@ -142,9 +152,9 @@ export const postRequestToLzards = async (params: {
 
   const provider = getRequiredEnvVar('lzards_provider');
   const lzardsApiUrl = getRequiredEnvVar('lzards_api');
-
+  console.log('hereH2');
   const checksumConfig = setLzardsChecksumQueryType(file, granuleId);
-
+  console.log('hereH3');
   return await got.post(lzardsApiUrl,
     {
       json: {
@@ -176,6 +186,7 @@ export const makeBackupFileRequest = async (params: {
   lzardsPostMethod?: typeof postRequestToLzards,
   generateAccessUrlMethod?: typeof generateAccessUrl,
 }): Promise<makeBackupFileRequestResult> => {
+  console.log('hereG1');
   const {
     collectionId,
     backupConfig,
@@ -185,15 +196,17 @@ export const makeBackupFileRequest = async (params: {
     lzardsPostMethod = postRequestToLzards,
     generateAccessUrlMethod = generateAccessUrl,
   } = params;
-
+  console.log('hereG2');
   try {
     const { Key, Bucket } = parseS3Uri(file.filename);
     log.info(`${granuleId}: posting backup request to LZARDS: ${file.filename}`);
+    console.log('hereG3');
     const accessUrl = await generateAccessUrlMethod({
       Bucket,
       Key,
       urlConfig: backupConfig,
     });
+    console.log('hereG4');
     const { statusCode, body } = await lzardsPostMethod({
       accessUrl,
       authToken,
@@ -201,12 +214,15 @@ export const makeBackupFileRequest = async (params: {
       file,
       granuleId,
     });
+    console.log('hereG5');
     if (statusCode !== 201) {
       log.error(`${granuleId}: Request failed - LZARDS api returned ${statusCode}: ${JSON.stringify(body)}`);
       return { statusCode, granuleId, filename: file.filename, body, status: 'FAILED' };
     }
+    console.log('hereG6');
     return { statusCode, granuleId, filename: file.filename, body, status: 'COMPLETED' };
   } catch (error) {
+    console.log('hereG7');
     log.error(`${granuleId}: LZARDS request failed: ${error}, response: ${JSON.stringify(error.response)}`);
     return {
       granuleId,
@@ -221,16 +237,20 @@ export const shouldBackupFile = (
   fileName: string,
   collectionConfig: CollectionRecord
 ): boolean => {
+  console.log('hereF1');
   const collectionFiles = collectionConfig?.files || [];
   const matchingConfig = collectionFiles.filter(
     ({ regex }) => fileName.match(regex)
   );
+  console.log('hereF2');
   if (matchingConfig.length > 1) {
     const errString = `Multiple files matched configured regexp for ${JSON.stringify(collectionConfig)},${fileName}`;
     log.error(errString);
     throw new CollectionInvalidRegexpError(errString);
   }
+  console.log('hereF3');
   if (matchingConfig[0]?.lzards?.backup) return true;
+  console.log('hereF4');
   return false;
 };
 
@@ -239,11 +259,14 @@ export const getGranuleCollection = async (params: {
   collectionVersion: string,
   stackPrefix?: string
 }): Promise<CollectionRecord> => {
+  console.log('hereE1');
   const prefix = params.stackPrefix || getRequiredEnvVar('stackName');
   const { collectionName, collectionVersion } = params;
+  console.log('hereE2');
   if (!collectionName && !collectionVersion) {
     throw new CollectionNotDefinedError('Collection Name and Version not defined');
   }
+  console.log('hereE3');
   return await getCollection({
     prefix,
     collectionName,
@@ -260,19 +283,24 @@ export const backupGranule = async (params: {
     distributionEndpoint?: string,
   },
 }) => {
+  console.log('hereD1');
   const { granule, backupConfig } = params;
   log.info(`${granule.granuleId}: Backup called on granule: ${JSON.stringify(granule)}`);
+  console.log('hereD2');
   try {
     const granuleCollection = await getGranuleCollection({
       collectionName: granule.dataType,
       collectionVersion: granule.version,
     });
+    console.log('hereD3');
     const collectionId = constructCollectionId(granule.dataType, granule.version);
+    console.log('hereD4');
     const backupFiles = granule.files.filter(
       (file) => shouldBackupFile(file.name, granuleCollection)
     );
-
+    console.log('hereD5');
     log.info(`${JSON.stringify(granule)}: Backing up ${JSON.stringify(backupFiles)}`);
+    console.log('hereD6');
     return Promise.all(backupFiles.map((file) => makeBackupFileRequest({
       backupConfig,
       file,
@@ -280,56 +308,68 @@ export const backupGranule = async (params: {
       granuleId: granule.granuleId,
     })));
   } catch (error) {
+    console.log('hereD7');
     if (error.name === 'CollectionNotDefinedError') {
       log.error(`${granule.granuleId}: Granule did not have a properly defined collection and version, or refer to a collection that does not exist in the database`);
       log.error(`${granule.granuleId}: Granule (${granule.granuleId}) will not be backed up.`);
     }
+    console.log('hereD8');
     error.message = `${granule.granuleId}: ${error.message}`;
     throw error;
   }
 };
 
 export const generateAccessCredentials = async () => {
+  console.log('hereC1');
   const params = {
     RoleArn: getRequiredEnvVar('backup_role_arn'),
     DurationSeconds: CREDS_EXPIRY_SECONDS,
     RoleSessionName: `${Date.now()}`,
   };
+  console.log('hereC2');
   const roleCreds = await sts().assumeRole(params).promise();
+  console.log('hereC3');
   return roleCreds as AWS.STS.AssumeRoleResponse;
 };
 
 export const getAuthToken = async () => {
+  console.log('hereB1');
   const api = getRequiredEnvVar('launchpad_api');
+  console.log('hereB2');
   const passphrase = await getSecretString(getRequiredEnvVar('launchpad_passphrase_secret_name'));
+  console.log('hereB3');
   if (!passphrase) {
     throw new GetAuthTokenError('The value stored in "launchpad_passphrase_secret_name" must be defined');
   }
+  console.log('hereB4');
   const certificate = getRequiredEnvVar('launchpad_certificate');
+  console.log('hereB5');
   const token = await getLaunchpadToken({
     api, passphrase, certificate,
   });
+  console.log('hereB6');
   return token;
 };
 
 export const backupGranulesToLzards = async (event: HandlerEvent) => {
+  console.log('hereA1');
   // Given an array of granules, submit each file for backup.
   log.warn(`Running backup on ${JSON.stringify(event)}`);
   const roleCreds = await generateAccessCredentials();
   const authToken = await getAuthToken();
-
+  console.log('hereA2');
   const backupConfig = {
     ...event.config,
     roleCreds,
     authToken,
   };
-
+  console.log('hereA3');
   const backupPromises = (event.input.granules.map(
     (granule) => backupGranule({ granule, backupConfig })
   ));
-
+  console.log('hereA4');
   const backupResults = await Promise.allSettled(backupPromises);
-
+  console.log('hereA5');
   // If there are uncaught exceptions, we want to fail the task.
   if (backupResults.some((result) => result.status === 'rejected')) {
     log.error('Some LZARDS backup results failed due to internal failure');
@@ -337,9 +377,11 @@ export const backupGranulesToLzards = async (event: HandlerEvent) => {
     log.error(`Full output: ${JSON.stringify(backupResults)}`);
     throw new Error(`${JSON.stringify(backupResults)}`);
   }
+  console.log('hereA6');
   const filteredResults = backupResults.filter(
     (result) => isFulfilledPromise(result)
   ) as PromiseFulfilledResult<makeBackupFileRequestResult[]>[];
+  console.log('hereA7');
   return {
     backupResults: filteredResults.map((result) => result.value).flat(),
     granules: event.input.granules,
