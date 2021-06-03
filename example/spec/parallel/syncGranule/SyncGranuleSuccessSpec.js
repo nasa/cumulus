127db0f3ca859ab807d7c721ec908fd4a708652b
Jennifer Tran <tranjennif@gmail.com>
2021-06-03 15:54:47 -0700

    Clean up remaining granules in SyncGranuleSuccessSpec

--- example/spec/parallel/syncGranule/SyncGranuleSuccessSpec.js
+++ example/spec/parallel/syncGranule/SyncGranuleSuccessSpec.js
@@ -191,6 +191,15 @@ describe('The Sync Granules workflow', () => {
       }));
     });
 
+    afterAll(async () => {
+      await Promise.all(lambdaOutput.payload.granules.map(
+        (granule) => granulesApiTestUtils.deleteGranule({
+          prefix: config.stackName,
+          granuleId: granule.granuleId,
+        })
+      ));
+    });
+
     it('receives payload with file objects updated to include file staging location', () => {
       const thisExpectedPayload = {
         ...expectedPayload,
@@ -271,6 +280,13 @@ describe('The Sync Granules workflow', () => {
         granuleId: inputPayload.granules[0].granuleId,
       });
       reingestResponse = JSON.parse(reingestGranuleResponse.body);
+    });
+
+    afterAll(async () => {
+      await granulesApiTestUtils.deleteGranule({
+        prefix: config.stackName,
+        granuleId: granule.granuleId,
+      });
     });
 
     it('executes successfully', () => {
