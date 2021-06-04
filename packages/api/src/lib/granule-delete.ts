58f67eca54dcebb06ff19da303219ff7c9a7193b
Jennifer Tran <tranjennif@gmail.com>
2021-06-03 22:39:49 -0700

    Fix typo in granule-delete

--- packages/api/src/lib/granule-delete.ts
+++ packages/api/src/lib/granule-delete.ts
@@ -62,7 +62,7 @@ const deleteGranuleAndFiles = async ({
   granulePgModel: GranulePgModel,
   granuleModelClient: typeof Granule
 }) => {
-  log.info(`Deleting PG granule ${pgGranule} mapped to dynamoGranule ${dynamoGranule}`);
+  logger.info(`Deleting PG granule ${pgGranule} mapped to dynamoGranule ${dynamoGranule}`);
   if (pgGranule === undefined) {
     // Delete only the Dynamo Granule and S3 Files
     await _deleteS3Files(dynamoGranule.files);
