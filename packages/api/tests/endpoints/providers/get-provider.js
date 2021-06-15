3d872411267b523210c4e4a3a4c59efc3b28e3fc
Jonathan Kovarik <jonathan.kovarik@gmail.com>
2021-06-15 11:37:08 -0600

    Update/provide RDS units for provider gets :bell:

--- packages/api/tests/endpoints/providers/get-provider.js
+++ packages/api/tests/endpoints/providers/get-provider.js
@@ -2,6 +2,8 @@
 
 const test = require('ava');
 const request = require('supertest');
+const cryptoRandomString = require('crypto-random-string');
+
 const { s3 } = require('@cumulus/aws-client/services');
 const {
   recursivelyDeleteS3Bucket,
@@ -9,11 +11,20 @@ const {
 const { randomString } = require('@cumulus/common/test-utils');
 const { bootstrapElasticSearch } = require('@cumulus/es-client/bootstrap');
 const { Search } = require('@cumulus/es-client/search');
+const { migrationDir } = require('../../../../../lambdas/db-migration');
+
+const {
+  ProviderPgModel,
+  destroyLocalTestDb,
+  fakeProviderRecordFactory,
+  generateLocalTestDb,
+  localStackConnectionEnv,
+  translateApiProviderToPostgresProvider,
+} = require('@cumulus/db');
 
 const models = require('../../../models');
 const {
   createFakeJwtAuthToken,
-  fakeProviderFactory,
   setAuthorizedOAuthUsers,
 } = require('../../../lib/testUtils');
 const assertions = require('../../../lib/assertions');
@@ -23,9 +34,6 @@ process.env.stackName = randomString();
 process.env.system_bucket = randomString();
 process.env.TOKEN_SECRET = randomString();
 
-// import the express app after setting the env variables
-const { app } = require('../../../app');
-
 let providerModel;
 const esIndex = randomString();
 let esClient;
@@ -33,7 +41,8 @@ let esClient;
 let jwtAuthToken;
 let accessTokenModel;
 
-test.before(async () => {
+test.before(async (t) => {
+  t.context.testDbName = `test_executions_${cryptoRandomString({ length: 10 })}`;
   await s3().createBucket({ Bucket: process.env.system_bucket }).promise();
 
   const esAlias = randomString();
@@ -53,22 +62,40 @@ test.before(async () => {
   jwtAuthToken = await createFakeJwtAuthToken({ accessTokenModel, username });
 
   esClient = await Search.es('fakehost');
+  const { knex, knexAdmin } = await generateLocalTestDb(t.context.testDbName, migrationDir);
+  t.context.knex = knex;
+  t.context.knexAdmin = knexAdmin;
+  process.env = {
+    ...process.env,
+    ...localStackConnectionEnv,
+    PG_DATABASE: t.context.testDbName,
+  };
+
+  // eslint-disable-next-line global-require
+  const { app } = require('../../../app');
+  t.context.app = app;
 });
 
 test.beforeEach(async (t) => {
-  t.context.testProvider = fakeProviderFactory();
-  await providerModel.create(t.context.testProvider);
+  t.context.testProvider = fakeProviderRecordFactory();
+  const providerPgModel = new ProviderPgModel();
+  await providerPgModel.create(t.context.knex, t.context.testProvider);
 });
 
-test.after.always(async () => {
+test.after.always(async (t) => {
   await recursivelyDeleteS3Bucket(process.env.system_bucket);
   await providerModel.deleteTable();
   await accessTokenModel.deleteTable();
   await esClient.indices.delete({ index: esIndex });
+  await destroyLocalTestDb({
+    knex: t.context.knex,
+    knexAdmin: t.context.knexAdmin,
+    testDbName: t.context.testDbName,
+  });
 });
 
 test('CUMULUS-911 GET with pathParameters and without an Authorization header returns an Authorization Missing response', async (t) => {
-  const response = await request(app)
+  const response = await request(t.context.app)
     .get('/providers/asdf')
     .set('Accept', 'application/json')
     .expect(401);
@@ -77,7 +104,7 @@ test('CUMULUS-911 GET with pathParameters and without an Authorization header re
 });
 
 test('CUMULUS-912 GET with pathParameters and with an invalid access token returns an unauthorized response', async (t) => {
-  const response = await request(app)
+  const response = await request(t.context.app)
     .get('/providers/asdf')
     .set('Accept', 'application/json')
     .set('Authorization', 'Bearer ThisIsAnInvalidAuthorizationToken')
@@ -89,22 +116,22 @@ test('CUMULUS-912 GET with pathParameters and with an invalid access token retur
 test.todo('CUMULUS-912 GET with pathParameters and with an unauthorized user returns an unauthorized response');
 
 test('GET returns an existing provider', async (t) => {
-  const response = await request(app)
-    .get(`/providers/${t.context.testProvider.id}`)
+  const response = await request(t.context.app)
+    .get(`/providers/${t.context.testProvider.name}`)
     .set('Accept', 'application/json')
     .set('Authorization', `Bearer ${jwtAuthToken}`)
     .expect(200);
 
-  t.is(response.body.id, t.context.testProvider.id);
+  t.like(await translateApiProviderToPostgresProvider(response.body), t.context.testProvider);
 });
 
 test('GET returns not found for a missing provider', async (t) => {
-  const response = await request(app)
+  const response = await request(t.context.app)
     .get('/providers/missing-provider-id')
     .set('Accept', 'application/json')
     .set('Authorization', `Bearer ${jwtAuthToken}`)
     .expect(404);
 
   t.is(response.body.error, 'Not Found');
-  t.is(response.body.message, 'Provider not found.');
+  t.is(response.body.message, 'Provider "missing-provider-id" not found.');
 });
