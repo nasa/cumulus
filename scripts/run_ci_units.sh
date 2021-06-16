98c30e6d3958f6a34c1880ae3a4ec85b93365a51
Jonathan Kovarik <jonathan.kovarik@gmail.com>
2021-06-16 15:48:01 -0600

    Add script output to running CI

--- scripts/run_ci_units.sh
+++ scripts/run_ci_units.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+touch ./test_output.txt
+tail -f ./test_output.txt &
 npx lerna run --ignore @cumulus/cumulus-integration-tests --concurrency 1 test > ./test_output.txt 2>&1
 RESULT=$?
-cat ./test_output.txt
 printf '\n\n\n*****TEST FAILURES:\n'
 grep '✖' ./test_output.txt
 exit $RESULT
