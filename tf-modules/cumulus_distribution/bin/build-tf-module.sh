dbcda92e4fbff3ef9a9c70103429dbbc1d2bc778
Nate Pauzenga <npauzenga@gmail.com>
2021-06-18 13:45:51 -0400

    include lambda.zip in distribution package

--- tf-modules/cumulus_distribution/bin/build-tf-module.sh
+++ tf-modules/cumulus_distribution/bin/build-tf-module.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 
 set -e
+# This is meant to be run from the tf-modules/cumulus_distribution directory as part of the bamboo/create-release.sh script
+(cd ../../packages/api && npm run package)
+
+mkdir -p dist
+
+cp ../../packages/api/dist/distribution/lambda.zip lambda.zip
 
 zip dist/terraform-aws-cumulus-distribution.zip \
-  *.tf
+  *.tf \
+  lambda.zip
