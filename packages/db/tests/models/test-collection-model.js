5c6e090e005b30e7f25604eff8520a54eebc6fb8
Jonathan Kovarik <jonathan.kovarik@gmail.com>
2021-06-21 15:36:41 -0600

    Update collection model object with updated_at range search

--- packages/db/tests/models/test-collection-model.js
+++ packages/db/tests/models/test-collection-model.js
@@ -1,5 +1,8 @@
 const test = require('ava');
+const times = require('lodash/times');
 const cryptoRandomString = require('crypto-random-string');
+
+const { removeNilProperties } = require('@cumulus/common/util');
 
 const {
   CollectionPgModel,
@@ -34,7 +37,7 @@ test.after.always(async (t) => {
   });
 });
 
-test('CollectionPgModel.upsert() creates new collection', async (t) => {
+test.serial('CollectionPgModel.upsert() creates new collection', async (t) => {
   const {
     knex,
     collectionPgModel,
@@ -52,7 +55,7 @@ test('CollectionPgModel.upsert() creates new collection', async (t) => {
   );
 });
 
-test('CollectionPgModel.upsert() overwrites a collection record', async (t) => {
+test.serial('CollectionPgModel.upsert() overwrites a collection record', async (t) => {
   const {
     knex,
     collectionPgModel,
@@ -77,5 +80,145 @@ test('CollectionPgModel.upsert() overwrites a collection record', async (t) => {
       ...updatedCollection,
       files: JSON.parse(updatedCollection.files),
     }
+  );
+});
+
+test('Collection.searchWithUpdatedAtRange() returns an array of records if no date range specified', async (t) => {
+  const {
+    knex,
+    collectionPgModel,
+  } = t.context;
+
+  const collectionName = cryptoRandomString({ length: 10 });
+  const records = times(3, (i) => fakeCollectionRecordFactory({
+    name: collectionName,
+    version: i,
+  }));
+  await Promise.all(records.map((r) => collectionPgModel.create(knex, r)));
+
+  const searchResponse = await collectionPgModel.searchWithUpdatedAtRange(
+    knex,
+    { name: collectionName },
+    {}
+  );
+
+  t.is(searchResponse.length, 3);
+});
+
+test('Collection.searchWithUpdatedAtRange() returns a filtered array of records if a date range is specified', async (t) => {
+  const {
+    knex,
+    collectionPgModel,
+  } = t.context;
+
+  const collectionName = cryptoRandomString({ length: 10 });
+  const records = times(3, (i) => fakeCollectionRecordFactory({
+    name: collectionName,
+    version: i,
+  }));
+
+  const dateValue = 5000;
+  const searchRecord = fakeCollectionRecordFactory({
+    name: collectionName,
+    version: '4',
+    updated_at: new Date(dateValue),
+  });
+  records.push(searchRecord);
+
+  await Promise.all(records.map((r) => collectionPgModel.create(knex, r)));
+
+  const searchResponse = await collectionPgModel.searchWithUpdatedAtRange(
+    knex,
+    {
+      name: collectionName,
+    },
+    {
+      updatedAtFrom: new Date(dateValue - 1),
+      updatedAtTo: new Date(dateValue + 1),
+    }
+  );
+
+  t.is(searchResponse.length, 1);
+  t.like(
+    removeNilProperties(searchResponse[0]),
+    { ...searchRecord, ...{ files: JSON.parse(searchRecord.files) } }
+  );
+});
+
+test('Collection.searchWithUpdatedAtRange() returns a filtered array of records if only updatedAtTo is specified', async (t) => {
+  const {
+    knex,
+    collectionPgModel,
+  } = t.context;
+
+  const dateValue = 5000;
+  const collectionName = cryptoRandomString({ length: 10 });
+  const records = times(3, (i) => fakeCollectionRecordFactory({
+    name: collectionName,
+    version: i,
+  }));
+
+  const searchRecord = fakeCollectionRecordFactory({
+    name: collectionName,
+    updated_at: new Date(dateValue),
+    version: '4',
+  });
+  records.push(searchRecord);
+
+  await Promise.all(records.map((r) => collectionPgModel.create(knex, r)));
+
+  const searchResponse = await collectionPgModel.searchWithUpdatedAtRange(
+    knex,
+    {
+      name: collectionName,
+    },
+    {
+      updatedAtTo: new Date(dateValue + 1),
+    }
+  );
+
+  t.is(searchResponse.length, 1);
+  t.like(
+    removeNilProperties(searchResponse[0]),
+    { ...searchRecord, ...{ files: JSON.parse(searchRecord.files) } }
+  );
+});
+
+test('Collection.searchWithUpdatedAtRange() returns a filtered array of records if only updatedAtFrom is specified', async (t) => {
+  const {
+    knex,
+    collectionPgModel,
+  } = t.context;
+
+  const collectionName = cryptoRandomString({ length: 10 });
+  const records = times(3, (i) => fakeCollectionRecordFactory({
+    name: collectionName,
+    version: i,
+  }));
+
+  const nowDateValue = new Date().valueOf();
+  const searchRecord = fakeCollectionRecordFactory({
+    updated_at: new Date(nowDateValue),
+    name: collectionName,
+    version: '4',
+  });
+  records.push(searchRecord);
+
+  await Promise.all(records.map((r) => collectionPgModel.create(knex, r)));
+
+  const searchResponse = await collectionPgModel.searchWithUpdatedAtRange(
+    knex,
+    {
+      name: collectionName,
+    },
+    {
+      updatedAtFrom: new Date(nowDateValue - 1),
+    }
+  );
+
+  t.is(searchResponse.length, 1);
+  t.like(
+    removeNilProperties(searchResponse[0]),
+    { ...searchRecord, ...{ files: JSON.parse(searchRecord.files) } }
   );
 });
