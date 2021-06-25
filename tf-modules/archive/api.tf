2360e6f10bc8cdb5e5c2448c4f2ad07fcf369ba1
Menno Van Diermen <mvandiermen@contractor.usgs.gov>
2021-06-25 09:56:16 -0500

    set api reserved concurrency to 5

--- tf-modules/archive/api.tf
+++ tf-modules/archive/api.tf
@@ -162,6 +162,8 @@ resource "aws_lambda_function" "api" {
   memory_size = 960
   tags        = var.tags
 
+  reserved_concurrent_executions = 5
+
   dynamic "vpc_config" {
     for_each = length(var.lambda_subnet_ids) == 0 ? [] : [1]
     content {
