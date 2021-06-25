c816620ece08b6f75a84ef0a9b2fbe7ee4e9eada
Mark Boyd <markdavidboyd@gmail.com>
2021-06-25 10:28:12 -0400

    add helper for kinesis replay requests

---
+++ packages/api-client/tests/test-replays.js
@@ -0,0 +1,36 @@
+'use strict';
+
+const test = require('ava');
+
+const replaysApi = require('../replays');
+
+test('postKinesisReplays calls the callback with the expected object', async (t) => {
+  const payload = {
+    type: 'kinesis',
+    kinesisStream: 'fake-stream',
+    endTimestamp: Date.now(),
+    startTimestamp: Date.now(),
+  };
+  const expected = {
+    prefix: 'replaysPrefix',
+    payload: {
+      httpMethod: 'POST',
+      resource: '/{proxy+}',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      path: '/replays',
+      body: JSON.stringify(payload),
+    },
+    expectedStatusCode: 202,
+  };
+
+  const callback = (configObject) => {
+    t.deepEqual(expected, configObject);
+  };
+  await t.notThrowsAsync(replaysApi.postKinesisReplays({
+    prefix: expected.prefix,
+    payload,
+    callback,
+  }));
+});
