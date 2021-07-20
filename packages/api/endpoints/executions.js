326964eb42194cb386216a3d6874c480ac96f0c8
Anthony Ortega <jose.a.ortega@nasa.gov>
2021-07-20 09:01:24 -0500

    CUMULUS-2460 Added knex instance to call of translatePostgresExecutionToApiExecution()

--- packages/api/endpoints/executions.js
+++ packages/api/endpoints/executions.js
@@ -108,7 +108,7 @@ async function searchByGranules(req, res) {
     .searchByCumulusIds(knex, executionCumulusIds, { limit, offset });
 
   const apiExecutions = await Promise.all(executions
-    .map((execution) => translatePostgresExecutionToApiExecution(execution)));
+    .map((execution) => translatePostgresExecutionToApiExecution(execution, knex)));
 
   return res.send(apiExecutions);
 }
