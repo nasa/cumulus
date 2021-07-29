944f2c80f656ad7cd5007643bf3125e01b0aa6f1
Jonathan Kovarik <kovarik@nsidc.org>
2021-07-29 13:06:40 -0600

    CUMULUS-2528 -- Update core to use knex/tarn internal database retry.  Remove dbHeartBeat (#2385)
    * Update core to use knex/tarn internal database retry.  Remove dbHeartBeat
    
    * Remove doc reference
    
    * Tidy up
    
    * Fix unit timings
    
    * Minor test update
    
    * fixup
    
    * Update exception test to run serially
    
    * Update test to fail on port connection instead of password, as knex
    doesn't note PW changes with live connections.
    
    * Update CHANGELOG.md
    
    Co-authored-by: Menno <mvandiermen@contractor.usgs.gov>
    
    * Update CHANGELOG.md
    
    Co-authored-by: Menno <mvandiermen@contractor.usgs.gov>
    
    * Update CHANGELOG.md
    
    Co-authored-by: Menno <mvandiermen@contractor.usgs.gov>
    
    * Address PR feedback
    
    * Address PR feedback
    
    * Alpha sort env vars in response to PR feedback
    
    * Add newline/PR feedback
    
    * Update CHANGELOG.md
    
    Co-authored-by: Menno <mvandiermen@contractor.usgs.gov>
    
    * Remove duped env vars
    
    * Cleanup/remove extra heartbeat references
    
    * Update variable description
    
    * Refactor test/respond to PR feedback
    
    Co-authored-by: Menno <mvandiermen@contractor.usgs.gov>

--- packages/db/src/config.ts
+++ packages/db/src/config.ts
@@ -136,12 +136,17 @@ export const getKnexConfig = async ({
       // ts-ignore as https://github.com/knex/knex/blob/master/types/index.d.ts#L1886
       // is improperly typed.
       //@ts-ignore
-      createTimeoutMillis: Number.parseInt(env.createTimeoutMillis ?? '60000', 10),
+      acquireTimeoutMillis: Number.parseInt(env.acquireTimeoutMillis ?? '90000', 10),
+      createRetryIntervalMillis: Number.parseInt(env.createRetryIntervalMillis ?? '30000', 10),
+      createTimeoutMillis: Number.parseInt(env.createTimeoutMillis ?? '20000', 10),
+      destroyTimeoutMillis: Number.parseInt(env.destroyTimeoutMillis ?? '5000', 10),
+      reapIntervalMillis: Number.parseInt(env.reapIntervalMillis ?? '1000', 10),
+      propagateCreateError: false,
     },
   };
 
-  knexConfig.acquireConnectionTimeout = env.knexAcquireConnectionTimeout
-    ? Number(env.knexAcquireConnectionTimeout)
+  knexConfig.acquireConnectionTimeout = env.acquireTimeoutMillis
+    ? Number(env.acquireTimeoutMillis + 1000)
     : 60000;
 
   if (env.migrationDir) {
