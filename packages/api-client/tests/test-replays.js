91f7265710337d171fa13f54a4cf28107a70f388
Jennifer Tran <12633533+botanical@users.noreply.github.com>
2021-08-03 20:29:15 -0700

    CUMULUS-2373 - Replay archived S3 messages (#2373)
    Co-authored-by: Jennifer Tran <tranjennif@gmail.com>

--- packages/api-client/tests/test-replays.js
+++ packages/api-client/tests/test-replays.js
@@ -34,3 +34,34 @@ test('postKinesisReplays calls the callback with the expected object', async (t)
     callback,
   }));
 });
+
+test('replaySqsMessages calls the callback with the expected object and returns the parsed response', async (t) => {
+  const prefix = 'unitTestStack';
+  const queueName = 'myQueue';
+  const payload = {
+    queueName,
+  };
+
+  const expected = {
+    prefix,
+    payload: {
+      httpMethod: 'POST',
+      resource: '/{proxy+}',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      path: '/replays/sqs',
+      body: JSON.stringify(payload),
+    },
+    expectedStatusCode: 202,
+  };
+  const callback = (configObject) => {
+    t.deepEqual(expected, configObject);
+  };
+
+  await t.notThrowsAsync(replaysApi.replaySqsMessages({
+    prefix,
+    payload,
+    callback,
+  }));
+});
