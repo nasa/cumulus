4a6073677d8422c66fe50dc0c10707467e790868
Nate Pauzenga <npauzenga@gmail.com>
2021-08-13 15:48:21 -0400

    temporarily remove method

--- packages/db/src/lib/execution.ts
+++ packages/db/src/lib/execution.ts
@@ -5,22 +5,3 @@ import { tableNames } from '../tables';
 export interface arnRecord {
   arn: string;
 }
-
-export const getExecutionArnsByGranuleCumulusId = (
-  knexOrTransaction: Knex | Knex.Transaction,
-  granuleCumulusId: Number
-): Promise<arnRecord[]> =>
-  knexOrTransaction(tableNames.executions)
-    .select('arn')
-    .where(`${tableNames.granules}.cumulus_id`, granuleCumulusId)
-    .join(
-      tableNames.granulesExecutions,
-      `${tableNames.executions}.cumulus_id`,
-      `${tableNames.granulesExecutions}.execution_cumulus_id`
-    )
-    .join(
-      tableNames.granules,
-      `${tableNames.granules}.cumulus_id`,
-      `${tableNames.granulesExecutions}.granule_cumulus_id`
-    )
-    .orderBy(`${tableNames.executions}.timestamp`, 'desc');
