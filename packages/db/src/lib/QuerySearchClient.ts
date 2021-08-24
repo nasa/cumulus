83024e85262a9f6ed532011eee786754e449549f
Mark Boyd <markdavidboyd@gmail.com>
2021-08-24 15:42:08 -0400

    update jsdoc

--- packages/db/src/lib/QuerySearchClient.ts
+++ packages/db/src/lib/QuerySearchClient.ts
@@ -2,6 +2,10 @@ import Knex from 'knex';
 
 import { BaseRecord } from '../types/base';
 
+/**
+ * Class to handle fetching results for an arbitrary PostgreSQL query and
+ * paging through them.
+ */
 class QuerySearchClient<RecordType extends BaseRecord> {
   readonly query: Knex.QueryBuilder;
   readonly limit: number;
@@ -19,9 +23,9 @@ class QuerySearchClient<RecordType extends BaseRecord> {
   }
 
   /**
-   * Return next items from a set of results (if any).
+   * Query the PostgreSQL database for the given offset/limit to get a set of results.
    *
-   * @returns {Promise<RecordType[]>}
+   * @returns {Promise<RecordType[]>} - set of records from PostgreSQL table
    * @throws
    */
   private async fetchRecords() {
@@ -33,11 +37,23 @@ class QuerySearchClient<RecordType extends BaseRecord> {
     this.offset += this.limit;
   }
 
+  /**
+   * View the next item in the results
+   *
+   * This does not remove the object from the queue.
+   *
+   * @returns {Promise<RecordType>} - record from PostgreSQL table
+   */
   async peek() {
     if (this.records.length === 0) await this.fetchRecords();
     return this.records[0];
   }
 
+  /**
+   * Remove and return the next item in the results
+   *
+   * @returns {Promise<RecordType>} - record from PostgreSQL table
+   */
   async shift() {
     if (this.records.length === 0) await this.fetchRecords();
     return this.records.shift();
