45d8979197c95ba9706607d35da3d56e08e0034b
Mark Boyd <markdavidboyd@gmail.com>
2021-08-24 15:47:50 -0400

    update jsdoc

--- packages/db/src/lib/file.ts
+++ packages/db/src/lib/file.ts
@@ -4,6 +4,21 @@ import { tableNames } from '../tables';
 import { PostgresFileRecord } from '../types/file';
 import { PostgresGranuleRecord } from '../types/granule';
 
+/**
+ * Helper to build a query that returns records from the files table
+ * with data joined in from the granules table.
+ *
+ * @param {Object} params
+ * @param {Knex} params.knex - Knex client object
+ * @param {Partial<PostgresFileRecord>} params.searchParams
+ *   Query search parameters for files table
+ * @param {Array<string>} params.sortColumns
+ *   Columns to sort results by
+ * @param {Array<string>} params.granuleColumns
+ *   Columns to return from granules table
+ * @param {number} [params.limit] - Limit on number of results to return. Optional.
+ * @returns {Promise<Object>} - A Knex query builder object
+ */
 export const getFilesAndGranuleInfoQuery = ({
   knex,
   searchParams,
@@ -20,10 +35,9 @@ export const getFilesAndGranuleInfoQuery = ({
   const query = knex(tableNames.files)
     .select(`${tableNames.files}.*`)
     .select(granuleColumns.map((column) => `${tableNames.granules}.${column}`))
-    .join(
+    .innerJoin(
       tableNames.granules,
       `${tableNames.files}.granule_cumulus_id`,
-      '=',
       `${tableNames.granules}.cumulus_id`
     )
     .where(searchParams)
