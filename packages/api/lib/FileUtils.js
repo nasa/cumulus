e6b96203df40954b24ec89014c0b1f60a800a142
Matt Savoie <savoie@colorado.edu>
2021-08-30 12:09:29 -0600

    Simplifies buildDatabaseFiles with a default value.

--- packages/api/lib/FileUtils.js
+++ packages/api/lib/FileUtils.js
@@ -126,15 +126,9 @@ const cleanDatabaseFile = (file) =>
     removeNilProperties,
   ])(file);
 
-const buildDatabaseFiles = async ({ s3, providerURL, files }) => {
-  if (files === undefined) {
-    log.warn('No files available to buildDatabaseFiles');
-    return [];
-  }
-  return await Promise.all(
-    files.map(partial(buildDatabaseFile, s3, providerURL))
-  ).then((newFiles) => newFiles.map(cleanDatabaseFile));
-};
+const buildDatabaseFiles = async ({ s3, providerURL, files = [] }) => await Promise.all(
+  files.map(partial(buildDatabaseFile, s3, providerURL))
+).then((newFiles) => newFiles.map(cleanDatabaseFile));
 
 module.exports = {
   setSource,
