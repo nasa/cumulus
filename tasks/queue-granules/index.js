653d0ba1df2f6f9f57457c7c154877f27a4a3171
Danielle Peters <danielle@element84.com>
2021-09-03 14:31:34 -0400

    Call updateStatus in QueueGranules task

--- tasks/queue-granules/index.js
+++ tasks/queue-granules/index.js
@@ -5,7 +5,10 @@ const pMap = require('p-map');
 const cumulusMessageAdapter = require('@cumulus/cumulus-message-adapter-js');
 const { enqueueGranuleIngestMessage } = require('@cumulus/ingest/queue');
 const { buildExecutionArn } = require('@cumulus/message/Executions');
-const { providers: providersApi } = require('@cumulus/api-client');
+const {
+  providers: providersApi,
+  granules: granulesApi,
+} = require('@cumulus/api-client');
 const CollectionConfigStore = require('@cumulus/collection-config-store');
 
 async function fetchGranuleProvider(prefix, providerId) {
@@ -18,12 +21,12 @@ async function fetchGranuleProvider(prefix, providerId) {
 }
 
 /**
-* See schemas/input.json and schemas/config.json for detailed event description
-*
-* @param {Object} event - Lambda event object
-* @returns {Promise} - see schemas/output.json for detailed output schema
-*   that is passed to the next task in the workflow
-**/
+ * See schemas/input.json and schemas/config.json for detailed event description
+ *
+ * @param {Object} event - Lambda event object
+ * @returns {Promise} - see schemas/output.json for detailed output schema
+ *   that is passed to the next task in the workflow
+ **/
 async function queueGranules(event) {
   const granules = event.input.granules || [];
 
@@ -33,13 +36,17 @@ async function queueGranules(event) {
   );
 
   const arn = buildExecutionArn(
-    get(event, 'cumulus_config.state_machine'), get(event, 'cumulus_config.execution_name')
+    get(event, 'cumulus_config.state_machine'),
+    get(event, 'cumulus_config.execution_name')
   );
 
   const executionArns = await pMap(
     granules,
     async (granule) => {
-      const collectionConfig = await collectionConfigStore.get(granule.dataType, granule.version);
+      const collectionConfig = await collectionConfigStore.get(
+        granule.dataType,
+        granule.version
+      );
       return enqueueGranuleIngestMessage({
         granule,
         queueUrl: event.config.queueUrl,
@@ -54,7 +61,14 @@ async function queueGranules(event) {
         systemBucket: event.config.internalBucket,
         executionNamePrefix: event.config.executionNamePrefix,
         additionalCustomMeta: event.config.childWorkflowMeta,
-      });
+      }).then(
+        async () =>
+          await granulesApi.updateGranuleStatus({
+            prefix: event.config.stackName,
+            granuleId: granule.granuleId,
+            status: 'queued',
+          })
+      );
     },
     { concurrency: get(event, 'config.concurrency', 3) }
   );
@@ -74,6 +88,10 @@ exports.queueGranules = queueGranules;
  *                              See schemas/output.json for detailed output schema
  */
 async function handler(event, context) {
-  return await cumulusMessageAdapter.runCumulusTask(queueGranules, event, context);
+  return await cumulusMessageAdapter.runCumulusTask(
+    queueGranules,
+    event,
+    context
+  );
 }
 exports.handler = handler;
