6ba7ef694150c5f93b131712773486bf86ad72c9
Danielle Peters <danielle@element84.com>
2021-09-08 16:48:00 -0400

    Add queued to GranuleStatus

--- packages/types/api/granules.d.ts
+++ packages/types/api/granules.d.ts
@@ -1,6 +1,6 @@
 export type GranuleId = string;
 
-export type GranuleStatus = 'completed' | 'failed' | 'running';
+export type GranuleStatus = 'completed' | 'failed' | 'running' | 'queued';
 
 export interface ApiGranule {
   status: GranuleStatus
