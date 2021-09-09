9a0463493eaeb48a6391f6d95bf66436a11f8b08
Danielle Peters <danielle@element84.com>
2021-09-09 12:28:16 -0400

    Update write granules and docs

--- packages/api/models/granules.js
+++ packages/api/models/granules.js
@@ -302,7 +302,7 @@ class Granule extends Manager {
    * @param {number} [params.timeToPreprocess] -  seconds
    * @param {integer} [params.productVolume] - sum of the files sizes in bytes
    * @param {number} [params.duration] - seconds
-   * @param {GranuleStatus} params.status - ['running','failed','completed']
+   * @param {GranuleStatus} params.status - ['running','failed','completed','queued']
    * @param {number} params.workflowStartTime
    * @param {Array<ApiFile>} params.files - files associated with the granule.
    * @param {Object} [params.error] = {} - workflow error that may have occurred.
