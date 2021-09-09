f4f1f9be07e7c8132073d0b0806ebcd6ca6b6d6b
Danielle Peters <danielle@element84.com>
2021-09-09 14:55:17 -0400

    More migration updates

--- packages/db/src/migrations/20210909133428_queued_granule_status.ts
+++ packages/db/src/migrations/20210909133428_queued_granule_status.ts
@@ -1,5 +1,19 @@
 import * as Knex from 'knex';
-import { formatAlterTableEnumSql } from '../lib/utils';
+
+const formatAlterTableEnumSql = (
+  tableName: string,
+  columnName: string,
+  enums: Array<string>
+) => {
+  const constraintName = `${tableName}_${columnName}_check`;
+  return [
+    `ALTER TABLE ${tableName}`,
+    `DROP CONSTRAINT IF EXISTS ${constraintName};`,
+    `ALTER TABLE ${tableName} ADD CONSTRAINT ${constraintName} CHECK (${columnName} = ANY (ARRAY['${enums.join(
+      "'::text, '"
+    )}'::text]));`,
+  ].join('\n');
+};
 
 export const up = async (knex: Knex): Promise<void> => {
   await knex.raw(formatAlterTableEnumSql('granules', 'status', [
