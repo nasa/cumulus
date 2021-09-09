56232c6290f8772108688ac1a9985e7a7a50607b
Danielle Peters <danielle@element84.com>
2021-09-09 14:42:15 -0400

    Fix migrations and lint

--- packages/db/src/migrations/20210727104740_alter_async_operations_table_operation_type.ts
+++ packages/db/src/migrations/20210727104740_alter_async_operations_table_operation_type.ts
@@ -1,19 +1,5 @@
 import * as Knex from 'knex';
-
-const formatAlterTableEnumSql = (
-  tableName: string,
-  columnName: string,
-  enums: Array<string>
-) => {
-  const constraintName = `${tableName}_${columnName}_check`;
-  return [
-    `ALTER TABLE ${tableName}`,
-    `DROP CONSTRAINT IF EXISTS ${constraintName};`,
-    `ALTER TABLE ${tableName} ADD CONSTRAINT ${constraintName} CHECK (${columnName} = ANY (ARRAY['${enums.join(
-      "'::text, '"
-    )}'::text]));`,
-  ].join('\n');
-};
+import { formatAlterTableEnumSql } from '../lib/utils';
 
 export const up = async (knex: Knex): Promise<void> => {
   await knex.raw(formatAlterTableEnumSql('async_operations', 'operation_type', [
--- packages/db/src/migrations/20210909133428_queued_granule_status.ts
+++ packages/db/src/migrations/20210909133428_queued_granule_status.ts
@@ -1,18 +1,19 @@
 import * as Knex from 'knex';
+import { formatAlterTableEnumSql } from '../lib/utils';
 
-export const up = async (knex: Knex): Promise<void> =>
-  await knex.schema.alterTable('granules', (table) => {
-    table
-      .enum('status', ['running', 'completed', 'failed', 'queued'])
-      .comment('Ingest status of the granule')
-      .notNullable();
-  });
+export const up = async (knex: Knex): Promise<void> => {
+  await knex.raw(formatAlterTableEnumSql('granules', 'status', [
+    'running',
+    'completed',
+    'failed',
+    'queued',
+  ]));
+};
 
 export const down = async (knex: Knex): Promise<void> => {
-  await knex.schema.alterTable('granules', (table) => {
-    table
-      .enum('status', ['running', 'completed', 'failed'])
-      .comment('Ingest status of the granule')
-      .notNullable();
-  });
+  await knex.raw(formatAlterTableEnumSql('granules', 'status', [
+    'running',
+    'completed',
+    'failed',
+  ]));
 };
