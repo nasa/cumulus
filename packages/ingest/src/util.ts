fba6bedd5aa8c18138d0b51320d65c66f6da661b
Mark Boyd <markdavidboyd@gmail.com>
2021-09-10 17:39:45 -0400

    add logging of KMS decrypt error

--- packages/ingest/src/util.ts
+++ packages/ingest/src/util.ts
@@ -2,12 +2,17 @@ import mime from 'mime-types';
 import * as KMS from '@cumulus/aws-client/KMS';
 import { S3KeyPairProvider } from '@cumulus/common/key-pair-provider';
 
+import Logger from '@cumulus/logger';
+
+const logger = new Logger({ sender: '@cumulus/ingest/utils' });
+
 export const decrypt = async (
   ciphertext: string
 ): Promise<string | undefined> => {
   try {
     return await KMS.decryptBase64String(ciphertext);
-  } catch (_) {
+  } catch (error) {
+    logger.error('Could not decrypt secret with KMS', error);
     return S3KeyPairProvider.decrypt(ciphertext);
   }
 };
