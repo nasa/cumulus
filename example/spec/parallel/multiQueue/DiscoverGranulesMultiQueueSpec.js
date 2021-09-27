a5469de5a25a663e45d9dbe61db71ec8f90a1003
Danielle Peters <danielle@element84.com>
2021-09-27 16:48:36 -0400

    Check for completed or queued

--- example/spec/parallel/multiQueue/DiscoverGranulesMultiQueueSpec.js
+++ example/spec/parallel/multiQueue/DiscoverGranulesMultiQueueSpec.js
@@ -212,7 +212,7 @@ describe('The DiscoverGranules workflow', () => {
       const granules = await Promise.all(testGranuleIds.map((granuleId) => waitForApiStatus(
         getGranule,
         { prefix: stackName, granuleId },
-        'completed'
+        ['completed', 'queued']
       )));
       granules.forEach((g) => {
         expect(g).toBeDefined();
