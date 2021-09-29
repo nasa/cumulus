d9192bb134ea1ffab317ee1367edfea1b8f49c4e
Mark Boyd <markdavidboyd@gmail.com>
2021-09-29 13:39:29 -0400

    remove unused helper

--- packages/api/lib/reconciliationReport.js
+++ packages/api/lib/reconciliationReport.js
@@ -147,34 +147,6 @@ function convertToDBGranuleSearchParams(params) {
 }
 
 /**
- *
- * @param {Object} params - request params to convert to database params
- * @returns {Object} object of desired parameters formated for database granule search
- */
-function convertToDBScanGranuleSearchParams(params) {
-  const {
-    collectionIds: collectionId,
-    granuleIds: granuleIdsParam,
-    providers,
-    status,
-    startTimestamp,
-    endTimestamp,
-  } = params;
-  const granuleId = (granuleIdsParam && (granuleIdsParam.length === 1))
-    ? granuleIdsParam[0] : granuleIdsParam;
-
-  const searchParams = {
-    updatedAt__from: dateToValue(startTimestamp),
-    updatedAt__to: dateToValue(endTimestamp),
-    collectionId,
-    granuleId,
-    providerNames: providers,
-    status,
-  };
-  return removeNilProperties(searchParams);
-}
-
-/**
  * create initial report header
  *
  * @param {Object} recReportParams - params
@@ -259,7 +231,6 @@ module.exports = {
   cmrGranuleSearchParams,
   convertToDBCollectionSearchObject,
   convertToDBGranuleSearchParams,
-  convertToDBScanGranuleSearchParams,
   convertToESCollectionSearchParams,
   convertToESGranuleSearchParams,
   filterCMRCollections,
