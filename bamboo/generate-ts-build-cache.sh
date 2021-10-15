ba8dbf1598e84e9b40bc2bc5dc983a5576a20ee3
Mark Boyd <markdavidboyd@gmail.com>
2021-10-15 15:50:22 -0400

    update TS cache builder script

--- bamboo/generate-ts-build-cache.sh
+++ bamboo/generate-ts-build-cache.sh
@@ -21,12 +21,12 @@ fi
 npm run ci:bootstrap-no-scripts
 
 # Get a list of TS compiled files
-npm run tsc:listEmittedFiles --silent | grep TSFILE | awk '{print $2}' | sed "s,$CURRENT_WORKING_DIR/,,g" >> .ts-build-cache-files
+npm run tsc:listEmittedFiles | grep TSFILE | awk '{print $2}' | sed "s,$CURRENT_WORKING_DIR/,,g" >> .ts-build-cache-files
 cat .ts-build-cache-files
 
 # Generate TS build cache artifact
-tar cf $TS_BUILD_CACHE_FILE -T .ts-build-cache-files
+tar cf "$TS_BUILD_CACHE_FILE" -T .ts-build-cache-files
 
 if [[ $USE_CACHED_BOOTSTRAP == true ]]; then
-  cp $TS_BUILD_CACHE_FILE "$NONCACHE_WORKING_DIR"
+  cp "$TS_BUILD_CACHE_FILE" "$NONCACHE_WORKING_DIR"
 fi
