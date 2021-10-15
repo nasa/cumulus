5c5c3f4cef489e3566b850bc617bdeb42b09498c
Matt Savoie <savoie@colorado.edu>
2021-10-15 11:20:21 -0600

    Adds a queued granule in the discover-granules skip duplicate handling path.

--- example/spec/parallel/discoverGranules/DiscoverGranulesDuplicateHandlingSkipSpec.js
+++ example/spec/parallel/discoverGranules/DiscoverGranulesDuplicateHandlingSkipSpec.js
@@ -9,18 +9,19 @@ const { createCollection } = require('@cumulus/integration-tests/Collections');
 const {
   findExecutionArn, getExecutionWithStatus,
 } = require('@cumulus/integration-tests/Executions');
+const { constructCollectionId } = require('@cumulus/message/Collections');
 const { getGranuleWithStatus } = require('@cumulus/integration-tests/Granules');
 const { createProvider } = require('@cumulus/integration-tests/Providers');
 const { createOneTimeRule } = require('@cumulus/integration-tests/Rules');
 
 const { deleteCollection } = require('@cumulus/api-client/collections');
-const { deleteGranule, getGranule } = require('@cumulus/api-client/granules');
+const { createGranule, deleteGranule, getGranule } = require('@cumulus/api-client/granules');
 const { deleteProvider } = require('@cumulus/api-client/providers');
 const { deleteRule } = require('@cumulus/api-client/rules');
 const { deleteExecution } = require('@cumulus/api-client/executions');
-
+const { removeNilProperties } = require('@cumulus/common/util');
 const { deleteS3Object, s3PutObject } = require('@cumulus/aws-client/S3');
-
+const { fakeGranuleFactoryV2 } = require('@cumulus/api/lib/testUtils');
 const { loadConfig } = require('../../helpers/testUtils');
 const { waitForApiStatus } = require('../../helpers/apiUtils');
 
@@ -36,6 +37,8 @@ describe('The DiscoverGranules workflow with one existing granule, one new granu
   let newGranuleKey;
   let prefix;
   let provider;
+  let queuedGranuleId;
+  let queuedGranuleKey;
   let sourceBucket;
   let discoverGranulesExecutionArn;
   let ingestGranuleExecutionArn;
@@ -60,6 +63,34 @@ describe('The DiscoverGranules workflow with one existing granule, one new granu
 
       // Create the S3 provider
       provider = await createProvider(prefix, { host: sourceBucket });
+
+      // Stage and Create a queued Granule in S3
+      queuedGranuleId = randomId('queued-granule-');
+      queuedGranuleKey = `${sourcePath}/${queuedGranuleId}.txt`;
+      await s3PutObject({
+        Bucket: sourceBucket,
+        Key: queuedGranuleKey,
+        Body: 'asdf-queued',
+      });
+
+      const collectionId = constructCollectionId(collection.name, collection.version);
+      const randomQueuedGranuleRecord = removeNilProperties(fakeGranuleFactoryV2({
+        collectionId,
+        granuleId: queuedGranuleId,
+        published: false,
+        dataType: undefined,
+        version: undefined,
+        execution: undefined,
+        files: [{
+          bucket: sourceBucket,
+          fileName: `${sourceBucket}/${queuedGranuleKey}`,
+          key: queuedGranuleKey,
+        }],
+      }));
+      await createGranule({
+        prefix,
+        body: randomQueuedGranuleRecord,
+      });
 
       // Stage the existing granule file to S3
       existingGranuleId = randomId('existing-granule-');
@@ -220,6 +251,7 @@ describe('The DiscoverGranules workflow with one existing granule, one new granu
       [
         () => deleteGranule({ prefix, granuleId: existingGranuleId }),
         () => deleteGranule({ prefix, granuleId: newGranuleId }),
+        () => deleteGranule({ prefix, granuleId: queuedGranuleId }),
       ],
       { stopOnError: false }
     ).catch(console.error);
@@ -228,6 +260,7 @@ describe('The DiscoverGranules workflow with one existing granule, one new granu
       [
         () => deleteS3Object(sourceBucket, existingGranuleKey),
         () => deleteS3Object(sourceBucket, newGranuleKey),
+        () => deleteS3Object(sourceBucket, queuedGranuleKey),
         () => deleteProvider({ prefix, providerId: get(provider, 'id') }),
         () => deleteCollection({
           prefix,
