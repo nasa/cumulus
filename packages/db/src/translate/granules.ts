badbb38f0865f914283e4dc36089abe6b9ad6bf8
Anthony Ortega <jose.a.ortega@nasa.gov>
2021-10-21 09:29:36 -0500

    CUMULUS-2584: addressed PR feedback

--- packages/db/src/translate/granules.ts
+++ packages/db/src/translate/granules.ts
@@ -23,11 +23,10 @@ import { PostgresCollectionRecord } from '../types/collection';
  * @param {PostgresCollectionRecord} params.collectionPgRecord - Collection from Postgres
  * @param {Knex | Knex.Transaction} params.knexOrTransaction
  *   Knex client for reading from RDS database
- * @param {Object} params.collectionPgModel - Instance of the collection database model
- * @param {Object} params.pdrPgModel - Instance of the pdr database model
- * @param {Object} params.providerPgModel - Instance of the provider database model
- * @param {Object} params.filePgModel - Instance of the file database model
- * @param {Object} params.executionPgModel - Instance of the execution database model
+ * @param {Object} [params.collectionPgModel] - Instance of the collection database model
+ * @param {Object} [params.pdrPgModel] - Instance of the pdr database model
+ * @param {Object} [params.providerPgModel] - Instance of the provider database model
+ * @param {Object} [params.filePgModel] - Instance of the file database model
  * @returns {Object} An API Granule with associated Files
  */
 export const translatePostgresGranuleToApiGranule = async ({
@@ -42,10 +41,10 @@ export const translatePostgresGranuleToApiGranule = async ({
   granulePgRecord: PostgresGranuleRecord,
   collectionPgRecord: PostgresCollectionRecord,
   knexOrTransaction: Knex | Knex.Transaction,
-  collectionPgModel: CollectionPgModel,
-  pdrPgModel: PdrPgModel,
-  providerPgModel: ProviderPgModel,
-  filePgModel: FilePgModel,
+  collectionPgModel?: CollectionPgModel,
+  pdrPgModel?: PdrPgModel,
+  providerPgModel?: ProviderPgModel,
+  filePgModel?: FilePgModel,
 }): Promise<ApiGranule> => {
   const collection = collectionPgRecord || await collectionPgModel.get(
     knexOrTransaction, { cumulus_id: granulePgRecord.collection_cumulus_id }
