c4232cb9790dc8f5e0eb5d1c458b967ffaad4258
Jenny Liu <jenny.h.liu@nasa.gov>
2021-12-07 17:31:59 -0500

    add rds_security_group_id to orca

--- example/cumulus-tf/orca.tf
+++ example/cumulus-tf/orca.tf
@@ -6,14 +6,8 @@ data "aws_secretsmanager_secret_version" "rds_admin_credentials" {
   secret_id = data.aws_secretsmanager_secret.rds_admin_credentials.id
 }
 
-data "aws_vpc_endpoint" "vpc_endpoint_id" {
-  vpc_id       = var.vpc_id
-  service_name = "com.amazonaws.us-east-1.execute-api"
-}
-
 locals {
   rds_admin_login = jsondecode(data.aws_secretsmanager_secret_version.rds_admin_credentials.secret_string)
-  vpc_endpoint_id = data.aws_vpc_endpoint.vpc_endpoint_id.id
 }
 
 # ORCA Module
@@ -29,6 +23,7 @@ module "orca" {
   lambda_subnet_ids        = var.lambda_subnet_ids
   permissions_boundary_arn = var.permissions_boundary_arn
   prefix                   = var.prefix
+  rds_security_group_id    = local.rds_security_group
   system_bucket            = var.system_bucket
   vpc_id                   = var.vpc_id
   workflow_config          = module.cumulus.workflow_config
@@ -40,15 +35,14 @@ module "orca" {
   ## ORCA Variables
   ## --------------------------
   ## REQUIRED
-  orca_default_bucket  = var.orca_default_bucket
   db_admin_password    = local.rds_admin_login.password
   db_host_endpoint     = local.rds_admin_login.host
   db_user_password     = var.orca_db_user_password
+  orca_default_bucket  = var.orca_default_bucket
 
   ## OPTIONAL
   db_admin_username                                    = local.rds_admin_login.username
   default_multipart_chunksize_mb                       = var.default_s3_multipart_chunksize_mb
-  vpc_endpoint_id                                      = local.vpc_endpoint_id
   orca_ingest_lambda_memory_size                       = 2240
   orca_ingest_lambda_timeout                           = 720
   orca_recovery_buckets                                = []
@@ -63,4 +57,5 @@ module "orca" {
   sqs_maximum_message_size                             = 262144
   staged_recovery_queue_message_retention_time_seconds = 432000
   status_update_queue_message_retention_time_seconds   = 777600
+  vpc_endpoint_id                                      = null
 }
