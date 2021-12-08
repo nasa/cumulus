ea94a211ee41cdb4a3e3104283711297311de998
Mark Boyd <markdavidboyd@gmail.com>
2021-12-08 17:29:00 -0500

    add data sources for easier deploy

--- example/rds-cluster-tf/main.tf
+++ example/rds-cluster-tf/main.tf
@@ -12,13 +12,31 @@ provider "aws" {
   profile = var.aws_profile
 }
 
+data "aws_vpc" "application_vpc" {
+  count = var.vpc_id == null ? 1 : 0
+  tags = {
+    Name = var.vpc_tag_name
+  }
+}
+
+data "aws_subnet_ids" "subnet_ids" {
+  count = var.subnets == null ? 1 : 0
+  vpc_id = var.vpc_id != null ? var.vpc_id : data.aws_vpc.application_vpc[0].id
+
+  filter {
+    name   = "tag:Name"
+    values = [var.subnets_tag_name]
+  }
+}
+
 module "rds_cluster" {
   source              = "../../tf-modules/cumulus-rds-tf"
+  prefix              = var.prefix
   db_admin_username   = var.db_admin_username
   db_admin_password   = var.db_admin_password
   region              = var.region
-  vpc_id              = var.vpc_id
-  subnets             = var.subnets
+  vpc_id              = var.vpc_id != null ? var.vpc_id : data.aws_vpc.application_vpc[0].id
+  subnets             = var.subnets != null ? var.subnets : data.aws_subnet_ids.subnet_ids[0].ids
   engine_version      = var.engine_version
   deletion_protection = true
   cluster_identifier  = var.cluster_identifier
