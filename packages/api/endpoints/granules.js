d16a9add1ccbf309be5c9268a13d7a273ce9770f
Jennifer Tran <tranjennif@gmail.com>
2021-12-08 19:52:59 -0800

    Add ES_HOST environment variable to async operation call in bulkOperations

--- packages/api/endpoints/granules.js
+++ packages/api/endpoints/granules.js
@@ -471,6 +471,7 @@ async function bulkOperations(req, res) {
       payload,
       type: 'BULK_GRANULE',
       envVars: {
+        ES_HOST: process.env.ES_HOST,
         GranulesTable: process.env.GranulesTable,
         granule_sns_topic_arn: process.env.granule_sns_topic_arn,
         system_bucket: process.env.system_bucket,
