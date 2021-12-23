7394a5bb496eb6e2e79df0017027ef1ae75e7924
Menno <mvandiermen@contractor.usgs.gov>
2021-12-23 14:51:50 -0600

    support SHA algo types with hyphens

--- packages/checksum/src/checksum.ts
+++ packages/checksum/src/checksum.ts
@@ -2,6 +2,23 @@ import * as cksum from 'cksum';
 import * as crypto from 'crypto';
 import { Readable, TransformOptions } from 'stream';
 
+export function normalizeHashAlgorithm(algorithm: string): string {
+  switch(algorithm) {
+    case 'SHA-1':
+      return 'SHA1';
+    case 'SHA-2':
+      return 'SHA2';
+    case 'SHA-256':
+      return 'SHA256';
+    case 'SHA-384':
+      return 'SHA384';
+    case 'SHA-512':
+      return 'SHA512';
+    default:
+      return algorithm;
+  }
+}
+
 // Calculate the cksum of a readable stream
 async function getCksumFromStream(stream: Readable): Promise<string> {
   return await new Promise((resolve, reject) =>
@@ -16,8 +33,9 @@ async function getChecksumFromStream(
   stream: Readable,
   options: TransformOptions = {}
 ): Promise<string> {
+  const normalizedAlgorithm: string = normalizeHashAlgorithm(algorithm);
   return await new Promise((resolve, reject) => {
-    const hash = crypto.createHash(algorithm, options);
+    const hash = crypto.createHash(normalizedAlgorithm, options);
     stream.on('error', reject);
     stream.on('data', (chunk) => hash.update(chunk));
     stream.on('end', () => resolve(hash.digest('hex')));
