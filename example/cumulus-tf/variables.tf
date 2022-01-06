a23e3bfa23cdac8a786b8e55b4a41168e4829036
Jonathan Kovarik <jonathan.kovarik@gmail.com>
2022-01-06 12:37:05 -0700

    Update image versions

--- example/cumulus-tf/variables.tf
+++ example/cumulus-tf/variables.tf
@@ -344,7 +344,7 @@ variable "async_operation_image_version" {
 variable "cumulus_process_activity_version" {
     description = "docker image version to use for python processing service"
     type = string
-    default = "1"
+    default = "2"
 }
 
 variable "ecs_task_image_version" {
@@ -356,7 +356,7 @@ variable "ecs_task_image_version" {
 variable "cumulus_test_ingest_image_version" {
     description = "docker image version to use for python test ingest processing service"
     type = string
-    default = "13"
+    default = "14"
 }
 variable "ecs_custom_sg_ids" {
   description = "User defined security groups to add to the Core ECS cluster"
