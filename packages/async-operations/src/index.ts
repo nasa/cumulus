642b7f4fa90a8e2d154d2aa1074cb4e9f0d86dc0
Mark Boyd <markdavidboyd@gmail.com>
2022-01-21 17:30:14 -0500

    remove comment

--- packages/async-operations/src/index.ts
+++ packages/async-operations/src/index.ts
@@ -1,4 +1,2 @@
-// import { startAsyncOperation } from './async_operations';
-// module.exports = { startAsyncOperation };
 import * as asyncOperations from './async_operations';
 module.exports = asyncOperations;
