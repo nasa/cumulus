70bb3d4ca15b369b632547ea467b2c2050c49e30
Jonathan Kovarik <jonathan.kovarik@gmail.com>
2022-02-04 09:11:13 -0700

    Update deployment configuration to work (#2701)

--- example/cumulus-tf/orca.tf
+++ example/cumulus-tf/orca.tf
@@ -13,7 +13,7 @@ locals {
 # ORCA Module
 module "orca" {
   count = 1
-  source = "https://github.com/nasa/cumulus-orca/releases/download/v4.0.0-Beta3/cumulus-orca-terraform.zip"
+  source = "https://github.com/nasa/cumulus-orca/releases/download/v4.0.0/cumulus-orca-terraform.zip"
 
   ## --------------------------
   ## Cumulus Variables
