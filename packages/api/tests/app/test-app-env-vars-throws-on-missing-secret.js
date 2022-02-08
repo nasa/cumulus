a8b79cdccb4b554c540de94c1555c638a8505d23
Jonathan Kovarik <jonathan.kovarik@gmail.com>
2022-02-08 12:28:15 -0700

    Update packages/api/tests/app/test-app-env-vars-throws-on-missing-secret.js
    Co-authored-by: Mark Boyd <markdavidboyd@gmail.com>

--- packages/api/tests/app/test-app-env-vars-throws-on-missing-secret.js
+++ packages/api/tests/app/test-app-env-vars-throws-on-missing-secret.js
@@ -3,7 +3,7 @@ const { MissingRequiredEnvVarError } = require('@cumulus/errors');
 
 process.env.dynamoTableNamesParameterName = 'fake-param-name';
 
-test('handler sets environment variables based on configured secretsManager secret', async (t) => {
+test('handler throws error if environment variable for secret ID is missing', async (t) => {
   process.env.INIT_ENV_VARS_FUNCTION_TEST = true;
   const dynamoTableNames = {
     DynamoTableName: 'prefix-dynamoTableName',
