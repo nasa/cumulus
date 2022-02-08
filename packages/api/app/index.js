595714ce4fc995f3b71fe4c19b35f3c73685d19f
Jonathan Kovarik <jonathan.kovarik@gmail.com>
2022-02-08 13:24:21 -0700

    Pull database table names from s3, add logging

--- packages/api/app/index.js
+++ packages/api/app/index.js
@@ -11,9 +11,9 @@ const morgan = require('morgan');
 const awsServerlessExpress = require('aws-serverless-express');
 const awsServerlessExpressMiddleware = require('aws-serverless-express/middleware');
 
-const { services } = require('@cumulus/aws-client');
 const { getRequiredEnvVar } = require('@cumulus/common/env');
 const { inTestMode } = require('@cumulus/common/test-utils');
+const { getJsonS3Object } = require('@cumulus/aws-client/S3');
 const { MissingRequiredEnvVar } = require('@cumulus/errors');
 const { secretsManager } = require('@cumulus/aws-client/services');
 const Logger = require('@cumulus/logger');
@@ -98,14 +98,15 @@ const server = awsServerlessExpress.createServer(app);
 const handler = async (event, context) => {
   await initEnvVars; // Wait for environment vars to resolve from initEnvVarsFunction
   const dynamoTableNamesParameterName = getRequiredEnvVar('dynamoTableNamesParameterName');
+  log.info('Starting API handler');
   if (!dynamoTableNamesParameterName) {
     throw new MissingRequiredEnvVar('dynamoTableNamesParameterName environment variable is required for API Lambda');
   }
-
-  const ssmClient = context.ssmClient || services.systemsManager();
-  const dynamoTableNamesParameter = await ssmClient.getParameter({
-    Name: dynamoTableNamesParameterName,
-  }).promise();
+  log.info('Getting dynamo table names from S3');
+  const dynamoTableNamesParameter = await getJsonS3Object(
+    process.env.system_bucket,
+    `${process.env.stackName}/api/dynamo_table_names.json`
+  );
   const dynamoTableNames = JSON.parse(dynamoTableNamesParameter.Parameter.Value);
   // Set Dynamo table names as environment variables for Lambda
   Object.keys(dynamoTableNames).forEach((tableEnvVarName) => {
@@ -118,6 +119,7 @@ const handler = async (event, context) => {
     ...event,
     queryStringParameters: event.multiValueQueryStringParameters || event.queryStringParameters,
   };
+  log.info('Running servelessExpress.proxy');
   // see https://github.com/vendia/serverless-express/issues/297
   return new Promise((resolve, reject) => {
     awsServerlessExpress.proxy(
