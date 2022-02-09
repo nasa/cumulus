87cc79a138b5adc5dc6801cfd93b6544ad659bdd
Jonathan Kovarik <jonathan.kovarik@gmail.com>
2022-02-09 08:19:13 -0700

    Fix CNM lambda versions

--- example/cumulus-tf/cnm_response_task.tf
+++ example/cumulus-tf/cnm_response_task.tf
@@ -3,7 +3,7 @@ resource "null_resource" "get_cnmResponse" {
     always_run = timestamp()
   }
   provisioner "local-exec" {
-    command = "curl -s -L -o cnmResponse.zip https://github.com/podaac/cumulus-cnm-response-task/releases/download/v2.0.2/cnmResponse-2.0.2.zip"
+    command = "curl -s -L -o cnmResponse.zip https://github.com/podaac/cumulus-cnm-response-task/releases/download/v1.4.4/cnmResponse-1.4.4.zip"
   }
 }
 
