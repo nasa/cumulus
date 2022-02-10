a1f3b2af0447c777be369f9d6dd7a14a713719be
Jonathan Kovarik <jonathan.kovarik@gmail.com>
2022-02-10 10:38:13 -0700

    Tidy up

--- packages/api/tests/app/test-missing-dynamo-env-var.js
+++ packages/api/tests/app/test-missing-dynamo-env-var.js
@@ -1,11 +1,9 @@
 const test = require('ava');
-const S3 = require('@cumulus/aws-client/S3');
-const { randomString } = require('@cumulus/common/test-utils');
 
 const { MissingRequiredEnvVar } = require('@cumulus/errors');
 
-
 test.serial('index throws error if environment variable for Dynamo tables parameter name is missing', async (t) => {
+  // eslint-disable-next-line global-require
   const { handler } = require('../../app');
   await t.throwsAsync(
     handler(),
