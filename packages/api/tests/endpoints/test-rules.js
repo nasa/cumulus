cef0f4bce9a2683df4f78449c1746aeb863e35de
Mark Boyd <markdavidboyd@gmail.com>
2022-02-16 17:32:42 -0500

    add unit test for SQS rule

--- packages/api/tests/endpoints/test-rules.js
+++ packages/api/tests/endpoints/test-rules.js
@@ -21,6 +21,7 @@ const {
   translateApiRuleToPostgresRule,
 } = require('@cumulus/db');
 const S3 = require('@cumulus/aws-client/S3');
+const { createQueue } = require('@cumulus/aws-client/SQS');
 const { constructCollectionId } = require('@cumulus/message/Collections');
 const { Search } = require('@cumulus/es-client/search');
 const indexer = require('@cumulus/es-client/indexer');
@@ -1015,6 +1016,78 @@ test.serial('put() creates the same updated Kinesis rule in Dynamo/PostgreSQL/El
   });
 });
 
+test.serial.only('put() creates the same SQS rule in Dynamo/PostgreSQL/Elasticsearch', async (t) => {
+  const {
+    pgProvider,
+    pgCollection,
+  } = t.context;
+
+  const queue = await createQueue(randomId('queue'));
+
+  const rule = {
+    name: randomId('rule'),
+    state: 'ENABLED',
+    rule: {
+      type: 'sqs',
+      value: queue,
+    },
+    collection: {
+      name: pgCollection.name,
+      version: pgCollection.version,
+    },
+    provider: pgProvider.name,
+  };
+
+  const expressRequest = {
+    params: {
+      name: rule.name,
+    },
+    body: rule,
+  };
+
+  const response = buildFakeExpressResponse();
+
+  await put(expressRequest, response);
+
+  const updatedRule = await ruleModel.get({ name: rule.name });
+  // const updatedPgRule = await t.context.rulePgModel
+  //   .get(t.context.testKnex, { name: rule.name });
+  // const updatedEsRule = await t.context.esRulesClient.get(
+  //   rule.name
+  // );
+
+  const expectedMeta = {
+    visibilityTimeout: 10,
+    retries: 3,
+  };
+  t.deepEqual(updatedRule, {
+    ...rule,
+    updatedAt: updatedRule.updatedAt,
+    meta: expectedMeta,
+  });
+  // t.deepEqual(
+  //   updatedEsRule,
+  //   {
+  //     ...rule,
+  //     updatedAt: updatedEsRule.updatedAt,
+  //     timestamp: updatedEsRule.timestamp,
+  //     rule: {
+  //       ...fakeKinesisSources2,
+  //       type: 'kinesis',
+  //       value: kinesisArn2,
+  //     },
+  //   }
+  // );
+  // t.deepEqual(updatedPgRule, {
+  //   ...originalPgRecord,
+  //   updated_at: updatedPgRule.updated_at,
+  //   type: 'kinesis',
+  //   value: kinesisArn2,
+  //   arn: fakeKinesisSources2.arn,
+  //   log_event_arn: fakeKinesisSources2.logEventArn,
+  // });
+});
+
 test('PUT returns 404 for non-existent rule', async (t) => {
   const name = 'new_make_coffee';
   const response = await request(app)
@@ -1065,7 +1138,7 @@ test('put() does not write to PostgreSQL/Elasticsearch if writing to Dynamo fail
     },
     create: () => Promise.resolve(originalDynamoRule),
     createRuleTrigger: () => Promise.resolve(originalDynamoRule),
-    updateRuleTrigger: () => Promise.resolve(originalDynamoRule),
+    ruleTrigger: () => Promise.resolve(originalDynamoRule),
   };
 
   const updatedRule = {
