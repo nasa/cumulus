3f73275350ef41d6f5a88ddf873de22a80cb9205
Jonathan Kovarik <jonathan.kovarik@gmail.com>
2022-03-03 09:01:04 -0700

    CUMULUS-2854: Update queue-granules to correctly set granule createdAt stamp (#2863)
    Co-authored-by: Matt Savoie <savoie@colorado.edu>
    Co-authored-by: Jonathan Kovarik <jonathan.kovarik@gmail.com>

---
+++ example/cumulus-tf/passthrough_workflow.tf
@@ -0,0 +1,16 @@
+module "passthrough_workflow" {
+  source = "../../tf-modules/workflow"
+
+  prefix          = var.prefix
+  name            = "Passthrough"
+  workflow_config = module.cumulus.workflow_config
+  system_bucket   = var.system_bucket
+  tags            = local.tags
+
+  state_machine_definition = templatefile(
+    "${path.module}/passthrough_workflow.asl.json",
+    {
+      hello_world_task_arn: module.cumulus.hello_world_task.task_arn
+    }
+  )
+}
