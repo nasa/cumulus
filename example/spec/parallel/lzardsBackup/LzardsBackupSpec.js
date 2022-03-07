2e6bbfa0b017eaef2fbef1df35244316d48e1b71
Anthony Ortega <jose.a.ortega@nasa.gov>
2022-03-07 16:46:36 -0600

    CUMULUS-2787: updating to troubleshoot integration test  that is passing locally

--- example/spec/parallel/lzardsBackup/LzardsBackupSpec.js
+++ example/spec/parallel/lzardsBackup/LzardsBackupSpec.js
@@ -158,6 +158,8 @@ describe('The Lzards Backup Task ', () => {
 
         const payload = JSON.parse(lzardsApiGetOutput.Payload);
 
+        console.log(`lzardsGetPayload: ${JSON.stringify(payload)}`);
+
         expect(lzardsApiGetOutput.FunctionError).toBe(undefined);
         expect(payload.count).toBe(1);
         expect(payload.items[0].metadata.granuleId).toBe('FakeGranule2');
@@ -181,6 +183,8 @@ describe('The Lzards Backup Task ', () => {
         );
 
         const payload = JSON.parse(lzardsApiGetOutput.Payload);
+
+        console.log(`lzardsGetPayload: ${JSON.stringify(payload)}`);
 
         expect(lzardsApiGetOutput.FunctionError).toBe(undefined);
         expect(payload.count).toBe(0);
