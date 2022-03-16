610cb84a01b628670752f001bf83cf405b0bfde9
Mark Boyd <markdavidboyd@gmail.com>
2022-03-16 14:51:26 -0400

    remove commented code

--- packages/aws-client/src/client.ts
+++ packages/aws-client/src/client.ts
@@ -3,14 +3,7 @@ import { inTestMode, testAwsClient } from './test-utils';
 
 import { AWSClientTypes } from './types';
 
-const noop = () => {}; // eslint-disable-line lodash/prefer-noop
-
 export const getRegion = () => process.env.AWS_DEFAULT_REGION || process.env.AWS_REGION || 'us-east-1';
-
-// Workaround upload hangs. See: https://github.com/andrewrk/node-s3-client/issues/74
-// @ts-ignore - AWS.util is not part of the public API and may break
-AWS.util.update(AWS.S3.prototype, { addExpect100Continue: noop });
-AWS.config.setPromisesDependency(Promise);
 
 const memoize = <T>(fn: (options?: object) => T): (options?: object) => T => {
   let memo: T;
@@ -47,11 +40,6 @@ export const awsClient = <T extends AWSClientTypes>(
   if (version) options.apiVersion = version;
 
   if (inTestMode()) {
-    // @ts-ignore - serviceIdentifier is not part of the public API and may break at any time
-    // if (AWS.DynamoDB.DocumentClient.serviceIdentifier === undefined) {
-    //   // @ts-ignore - serviceIdentifier is not part of the public API and may break at any time
-    //   AWS.DynamoDB.DocumentClient.serviceIdentifier = 'dynamodb';
-    // }
     return memoize((o) => testAwsClient(Service, Object.assign(options, o)));
   }
   return memoize((o) => new Service(Object.assign(options, o)));
