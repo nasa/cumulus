ea94d54f88c4b323302c0e3135cbf3f463259f17
Mark Boyd <markdavidboyd@gmail.com>
2022-03-25 15:07:41 -0400

    fix unit test

--- packages/api/tests/endpoints/test-distribution.js
+++ packages/api/tests/endpoints/test-distribution.js
@@ -110,7 +110,7 @@ test.before(async () => {
   const fileKey = randomId('key');
 
   const fileLocation = `${protectedBucket}/${fileKey}`;
-  const s3Endpoint = getLocalstackEndpoint('s3');
+  const s3Endpoint = getLocalstackEndpoint('S3');
 
   const getAccessTokenResponse = fakeAccessTokenFactory();
   const getUserInfoResponse = { foo: 'bar', username: getAccessTokenResponse.username };
