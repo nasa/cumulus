137a7afba75461f289344399eed84732c7cbeb72
Jonathan Kovarik <jonathan.kovarik@gmail.com>
2022-04-20 16:15:57 -0600

    Decomplexify cleanup script

--- bamboo/cleanup-integration-tests.sh
+++ bamboo/cleanup-integration-tests.sh
@@ -8,19 +8,10 @@ set -ex
 . ./bamboo/abort-if-skip-integration-tests.sh
 
 echo Unlocking stack
+echo "*** Using cached bootstrap"
+cp .bamboo_env_vars /cumulus/
+cd /cumulus/
+git fetch --all
+git checkout "$GIT_SHA"
 
-if [[ $USE_CACHED_BOOTSTRAP == true ]]; then ## Change into cached cumulus, pull down /cumulus ref and run there
-    echo "*** Using cached bootstrap"
-    cp .bamboo_env_vars /cumulus/
-    cd /cumulus/
-    git fetch --all
-    git checkout "$GIT_SHA"
-fi
-npm install
-npm --version
-
-## This is needed to ensure lock-stack has the expected dependencies
-npx lerna bootstrap --scope @cumulus/cumulus-integration-tests --scope @cumulus/aws-client --scope @cumulus/common --scope @cumulus/errors --scope @cumulus/logger
-
-# (npm run bootstrap-no-build || true) && npm run bootstrap-no-build && cd example && node ./scripts/lock-stack.js lock $GIT_SHA $DEPLOYMENT false
 cd /cumulus/example && node ./scripts/lock-stack.js lock "$GIT_SHA" "$DEPLOYMENT" false
