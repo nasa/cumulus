66e9c229674cc8c7614ee7f0b8664166c888ced1
Jonathan Kovarik <jonathan.kovarik@gmail.com>
2022-04-21 06:23:14 -0600

    Re-add cached bootstrap, minor updates

--- bamboo/cleanup-integration-tests.sh
+++ bamboo/cleanup-integration-tests.sh
@@ -7,11 +7,20 @@ set -ex
 . ./bamboo/abort-if-not-pr-or-redeployment.sh
 . ./bamboo/abort-if-skip-integration-tests.sh
 
+cp .bamboo_env_vars /noncache/cumulus/
 echo Unlocking stack
-echo "*** Using cached bootstrap"
-cp .bamboo_env_vars /cumulus/
-cd /cumulus/
-git fetch --all
-git checkout "$GIT_SHA"
 
-cd /cumulus/example && node ./scripts/lock-stack.js lock "$GIT_SHA" "$DEPLOYMENT" false
+if [[ $USE_CACHED_BOOTSTRAP == true ]]; then ## Change into cached cumulus, pull down /cumulus ref and run there
+    echo "*** Using cached bootstrap"
+    cp .bamboo_env_vars /cumulus/
+    cd /cumulus/
+    git fetch --all
+    git checkout "$GIT_SHA"
+fi
+npm install
+npm --version
+
+## This is needed to ensure lock-stack has the expected dependencies
+npx lerna bootstrap --scope @cumulus/cumulus-integration-tests --scope @cumulus/aws-client --scope @cumulus/common --scope @cumulus/errors --scope @cumulus/logger
+
+cd example && node ./scripts/lock-stack.js lock "$GIT_SHA" "$DEPLOYMENT" false
