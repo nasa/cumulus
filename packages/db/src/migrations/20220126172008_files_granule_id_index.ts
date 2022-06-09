ae375976b5dc4278c5e68854eda2393b1b59c822
Jonathan Kovarik <jonathan.kovarik@gmail.com>
2022-06-09 07:59:40 -0600

    Include missing/mangled commit

--- packages/db/src/migrations/20220126172008_files_granule_id_index.ts
+++ packages/db/src/migrations/20220126172008_files_granule_id_index.ts
@@ -1,13 +1,14 @@
 import { Knex } from 'knex';
 
-export const up = async (knex: Knex): Promise<void> => {
+// eslint-disable-next-line arrow-body-style
+export const up = async (): Promise<void> => {
   // **Note** - this migration was removed due to production deployment timeouts.
   // A new ticket was added (CUMULUS-2962) to implement this instead.  To ensure all deployed
   // stacks reach the same migration state, this migration was updated to *not* add the index and
   // the next migration in sequence was added to drop the index *if it exists*.
-  await knex.schema.table('files', (table) => {
+  /* await knex.schema.table('files', (table) => {
     table.index('granule_cumulus_id');
-  });
+  }); */
   return Promise.resolve();
 };
 
