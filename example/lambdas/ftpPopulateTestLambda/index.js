37764cfb1ca7a46cbf1476d37f0cb2a57adba612
Vitor Henckel <vitor@henckel.com.br>
2022-07-28 13:04:04 -0300

    fix: removing foreach method and returning with map

--- example/lambdas/ftpPopulateTestLambda/index.js
+++ example/lambdas/ftpPopulateTestLambda/index.js
@@ -1,16 +1,16 @@
-"use strict";
+'use strict';
 
-const { randomStringFromRegex } = require("@cumulus/common/test-utils");
-const Logger = require("@cumulus/logger");
+const { randomStringFromRegex } = require('@cumulus/common/test-utils');
+const Logger = require('@cumulus/logger');
 
-const { fetchFakeProviderIp } = require("@cumulus/common/fake-provider");
-const replace = require("lodash/replace");
-const JSFtp = require("jsftp");
-const path = require("path");
-const fs = require("fs");
+const { fetchFakeProviderIp } = require('@cumulus/common/fake-provider');
+const replace = require('lodash/replace');
+const JSFtp = require('jsftp');
+const path = require('path');
+const fs = require('fs');
 
 const log = new Logger({
-  sender: "@cumulus/example/lambdas/ftpPopulateTestLambda",
+  sender: '@cumulus/example/lambdas/ftpPopulateTestLambda',
 });
 
 const updateAndUploadTestFileToFtpHost = (params) => {
@@ -25,7 +25,7 @@ const updateAndUploadTestFileToFtpHost = (params) => {
   let data = fs.readFileSync(file);
   if (replacements.length > 0) {
     replacements.forEach((replacement) => {
-      data = replace(data, new RegExp(replacement.old, "g"), replacement.new);
+      data = replace(data, new RegExp(replacement.old, 'g'), replacement.new);
     });
   }
   let key = path.basename(file);
@@ -42,8 +42,8 @@ const updateAndUploadTestFileToFtpHost = (params) => {
   try {
     // Upload buffer to FTP site
     return new Promise((resolve, reject) => {
-      ftp.on("error", reject);
-      ftp.raw("mkd", prefix, () => {
+      ftp.on('error', reject);
+      ftp.raw('mkd', prefix, () => {
         ftp.put(data, `${prefix}/${key}`, (suberr) => {
           if (suberr) {
             reject(suberr);
@@ -54,25 +54,25 @@ const updateAndUploadTestFileToFtpHost = (params) => {
       });
     });
   } catch (error) {
-    log.info("Error on updateAndUploadTestFileToFtpHost", error);
+    log.info('Error on updateAndUploadTestFileToFtpHost', error);
     throw error;
   }
 };
 
 const uploadFtpGranuleDataForDiscovery = async ({ prefix }) => {
-  log.info("Starting lambda");
+  log.info('Starting lambda');
   const ftpData = [
-    "./granules/MOD09GQ.A2016358.h13v04.006.2016360104606.hdf.met",
-    "./granules/MOD09GQ.A2016358.h13v04.006.2016360104606.hdf",
-    "./granules/MOD09GQ.A2016358.h13v04.006.2016360104606_ndvi.jpg",
+    './granules/MOD09GQ.A2016358.h13v04.006.2016360104606.hdf.met',
+    './granules/MOD09GQ.A2016358.h13v04.006.2016360104606.hdf',
+    './granules/MOD09GQ.A2016358.h13v04.006.2016360104606_ndvi.jpg',
   ];
-  const granuleRegex = "^MOD09GQ\\.A[\\d]{7}\\.[\\w]{6}\\.006\\.[\\d]{13}$";
+  const granuleRegex = '^MOD09GQ\\.A[\\d]{7}\\.[\\w]{6}\\.006\\.[\\d]{13}$';
   const newGranuleId = randomStringFromRegex(granuleRegex);
 
   const hostConfig = {
     host: await fetchFakeProviderIp(),
-    user: "testuser",
-    pass: "testpass",
+    user: 'testuser',
+    pass: 'testpass',
   };
 
   log.info(`Hostconfig is ${JSON.stringify(hostConfig)}`);
@@ -85,11 +85,11 @@ const uploadFtpGranuleDataForDiscovery = async ({ prefix }) => {
           file,
           hostConfig,
           prefix,
-          targetReplacementRegex: "MOD09GQ.A2016358.h13v04.006.2016360104606",
+          targetReplacementRegex: 'MOD09GQ.A2016358.h13v04.006.2016360104606',
           targetReplacementString: newGranuleId,
         });
       } catch (error) {
-        log.info("Error!", error);
+        log.info('Error!', error);
         throw error;
       }
       return objectKey;
@@ -102,15 +102,15 @@ const uploadFtpGranuleDataForDiscovery = async ({ prefix }) => {
 const deleteFtpGranule = async (filePath) => {
   const hostConfig = {
     host: await fetchFakeProviderIp(),
-    user: "testuser",
-    pass: "testpass",
+    user: 'testuser',
+    pass: 'testpass',
   };
   const ftp = new JSFtp(hostConfig);
 
   // Delete files from FTP volume
   return new Promise((resolve, reject) => {
-    ftp.on("error", reject);
-    ftp.raw("dele", filePath, (err) => {
+    ftp.on('error', reject);
+    ftp.raw('dele', filePath, (err) => {
       ftp.destroy(filePath);
       if (err) {
         log.info(`Failed to delete file ${filePath}`, err);
@@ -131,7 +131,7 @@ const deleteFtpGranules = async ({ filePaths }) => {
 
 const handler = async (event, _context) => {
   log.info(event);
-  if (event.command === "delete") {
+  if (event.command === 'delete') {
     return await deleteFtpGranules(event);
   }
   return await uploadFtpGranuleDataForDiscovery(event);
