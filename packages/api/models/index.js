76a289dcc0ab5ea0b1d994dd77ee74fd0366c5a7
Jennifer Tran <12633533+botanical@users.noreply.github.com>
2022-08-22 16:18:26 -0400

    Remove provider model

--- packages/api/models/index.js
+++ packages/api/models/index.js
@@ -3,8 +3,6 @@
 const AccessToken = require('./access-tokens');
 const AsyncOperation = require('./async-operation');
 const Manager = require('./base');
-
-const Provider = require('./providers');
 const ReconciliationReport = require('./reconciliation-reports');
 const Rule = require('./rules');
 const Execution = require('./executions');
@@ -12,7 +10,6 @@ const Execution = require('./executions');
 module.exports = {
   AccessToken,
   AsyncOperation,
-  Provider,
   ReconciliationReport,
   Rule,
   Manager,
