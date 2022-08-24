89fe3d9953b1930ea168f84adff62348a8c0a349
Nate Pauzenga <npauzenga@gmail.com>
2022-08-24 17:08:56 -0400

    Remove unnecessary type cast

--- packages/db/src/translate/granules.ts
+++ packages/db/src/translate/granules.ts
@@ -179,7 +179,7 @@ export const translateApiGranuleToPostgresGranule = async (
     updated_at: new Date(dynamoRecord.updatedAt),
   };
 
-  return <PostgresGranule>removeNilProperties(granuleRecord);
+  return removeNilProperties(granuleRecord);
 };
 
 /**
