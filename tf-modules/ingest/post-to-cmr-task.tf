ecca345dae834ddb2aec5a004957903d6b032e87
Nagavenkata Nageswaran <nnageswa@mbp-nnageswa.edn.ecs.nasa.gov>
2022-08-24 16:20:19 -0400

    Fixed spacing

--- tf-modules/ingest/post-to-cmr-task.tf
+++ tf-modules/ingest/post-to-cmr-task.tf
@@ -5,8 +5,8 @@ resource "aws_lambda_function" "post_to_cmr_task" {
   handler          = "index.handler"
   role             = var.lambda_processing_role_arn
   runtime          = "nodejs14.x"
-  timeout          = lookup(var.lambda_timeouts, "post_to_cmr_task_timeout", 300)
-  memory_size      = lookup(var.lambda_memory_sizes, "post_to_cmr_task_memory_size", 256)
+  timeout          = lookup (var.lambda_timeouts, "post_to_cmr_task_timeout", 300)
+  memory_size      = lookup (var.lambda_memory_sizes, "post_to_cmr_task_memory_size", 256)
 
   layers = [var.cumulus_message_adapter_lambda_layer_version_arn]
 
