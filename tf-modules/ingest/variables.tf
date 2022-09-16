1940133d1436b1bf923ae88b6f6dae58c39112ef
Nagavenkata Nageswaran <nnageswa@mbp-nnageswa.edn.ecs.nasa.gov>
2022-09-16 17:22:57 -0400

    needed to add variable to definition

--- tf-modules/ingest/variables.tf
+++ tf-modules/ingest/variables.tf
@@ -148,6 +148,12 @@ variable "tags" {
   default     = {}
 }
 
+variable "lambda_memory_sizes" {
+  description = "Configurable map of memory sizes for ingest task lambdas in the form <lambda_identifier>_memory_size: <memory_size>"
+  type = map(string)
+  default = {}
+}
+
 variable "lambda_timeouts" {
   description = "Configurable map of timeouts for ingest task lambdas in the form <lambda_identifier>_timeout: <timeout>"
   type = map(string)
