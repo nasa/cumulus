67f49080832f458b91b27d250e6a3b59d0a4e62c
Jonathan Kovarik <jonathan.kovarik@gmail.com>
2022-09-28 08:34:49 -0600

    Update PDR spec tests to properly remove PDR after related granule

--- example/spec/parallel/ingestGranule/IngestGranulePythonProcessingSuccessSpec.js
+++ example/spec/parallel/ingestGranule/IngestGranulePythonProcessingSuccessSpec.js
@@ -124,16 +124,16 @@ describe('The TestPythonProcessing workflow', () => {
 
   afterAll(async () => {
     // clean up stack state added by test
-    await apiTestUtils.deletePdr({
-      prefix: config.stackName,
-      pdr: pdrFilename,
-    });
     await deleteGranule({
       prefix: config.stackName,
       granuleId: inputPayload.granules[0].granuleId,
     });
     await deleteProvider({ prefix: config.stackName, providerId: provider.id });
     await deleteExecution({ prefix: config.stackName, executionArn: workflowExecutionArn });
+    await apiTestUtils.deletePdr({
+      prefix: config.stackName,
+      pdr: pdrFilename,
+    });
     await Promise.all([
       deleteFolder(config.bucket, testDataFolder),
       deleteCollection({
