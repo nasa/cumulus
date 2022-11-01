9dbbcf95558dc30031bc8f122a3fabe103780868
Nate Pauzenga <npauzenga@gmail.com>
2022-11-01 10:49:14 -0400

    update to hopefully fix compilation error

--- packages/aws-client/src/S3.ts
+++ packages/aws-client/src/S3.ts
@@ -1011,7 +1011,7 @@ export const multipartCopyObject = async (
     destinationBucket: string,
     destinationKey: string,
     sourceObject?: HeadObjectOutput,
-    ACL?: ObjectCannedACL,
+    ACL?: ObjectCannedACL | string,
     copyTags?: boolean,
     chunkSize?: number
   }
@@ -1034,7 +1034,7 @@ export const multipartCopyObject = async (
     sourceKey,
     destinationBucket,
     destinationKey,
-    ACL,
+    ACL: <ObjectCannedACL>ACL,
     copyTags,
     contentType: sourceObject.ContentType,
   });
