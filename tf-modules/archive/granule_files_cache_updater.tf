41d99e74e85cbea68d9af8fac7f06496b926cf5d
Naga Nages <66387215+Nnaga1@users.noreply.github.com>
2022-11-03 16:40:23 -0400

    Updated Changelog

--- tf-modules/archive/granule_files_cache_updater.tf
+++ tf-modules/archive/granule_files_cache_updater.tf
@@ -86,7 +86,7 @@ resource "aws_lambda_function" "granule_files_cache_updater" {
 
 resource "aws_cloudwatch_log_group" "granule_files_cache_updater_logs" {
   name              = "/aws/lambda/${aws_lambda_function.granule_files_cache_updater.function_name}"
-  retention_in_days = 14
+  retention_in_days = 30
   tags              = var.tags
 }
 
