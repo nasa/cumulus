dcc1ffca396a077255ef28dfe6049ca5d2c3b417
Marc Huffnagle <marc@element84.com>
2023-01-26 17:10:47 -0500

    Fix sqs-message-remove test broken by LocalStack upgrade

--- lambdas/sqs-message-remover/tests/test-index.js
+++ lambdas/sqs-message-remover/tests/test-index.js
@@ -235,7 +235,7 @@ test('sqsMessageRemover lambda updates message visibilityTimeout when workflow f
   t.is(numberOfMessages.numberOfMessagesAvailable, 0);
   t.is(numberOfMessages.numberOfMessagesNotVisible, 1);
 
-  await delay(5 * 1000);
+  await delay(6 * 1000);
   numberOfMessages = await getSqsQueueMessageCounts(sqsQueues.queueUrl);
   t.is(numberOfMessages.numberOfMessagesAvailable, 1);
   t.is(numberOfMessages.numberOfMessagesNotVisible, 0);
