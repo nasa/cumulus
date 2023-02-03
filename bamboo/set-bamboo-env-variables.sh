4c30651c0f69f6a0900542e4e2a02e1bc7f0afb8
Naga Nages <nnagavenkata@yahoo.com>
2023-02-03 14:43:24 -0500

    fixing CI setup

--- bamboo/set-bamboo-env-variables.sh
+++ bamboo/set-bamboo-env-variables.sh
@@ -66,7 +66,8 @@ for key in "${param_list[@]}"; do
 done
 
 ## Get the current git SHA
-git config --global --add safe.directory "${bamboo.working.directory}"/source/cumulus
+export WORKING_DIRECTORY=${bamboo.working.directory}
+git config --global --add safe.directory ${WORKING_DIRECTORY}/source/cumulus
 GIT_SHA=$(git rev-parse HEAD)
 export GIT_SHA
 
