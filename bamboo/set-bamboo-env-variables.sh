e316ffcda3e59640734ab1f460a388a184c3924f
Naga Nages <nnagavenkata@yahoo.com>
2023-02-03 10:01:41 -0500

    git bamboo fix (test)

--- bamboo/set-bamboo-env-variables.sh
+++ bamboo/set-bamboo-env-variables.sh
@@ -66,6 +66,7 @@ for key in "${param_list[@]}"; do
 done
 
 ## Get the current git SHA
+git config --global --add safe.directory ${bamboo.working.directory}/source/cumulus
 GIT_SHA=$(git rev-parse HEAD)
 export GIT_SHA
 
