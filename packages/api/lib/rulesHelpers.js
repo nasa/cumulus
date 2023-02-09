4142ec602deeab6786a5e3cf9b4b27a458823163
Nate Pauzenga <npauzenga@gmail.com>
2023-02-09 13:49:17 -0500

    Update packages/api/lib/rulesHelpers.js
    Co-authored-by: Marc <marc@element84.com>

--- packages/api/lib/rulesHelpers.js
+++ packages/api/lib/rulesHelpers.js
@@ -14,7 +14,7 @@ const { sqsQueueExists } = require('@cumulus/aws-client/SQS');
 const { invoke } = require('@cumulus/aws-client/Lambda');
 const { RulePgModel } = require('@cumulus/db');
 const { ValidationError } = require('@cumulus/errors');
-const { getRequiredEnvVar } = require('@cumulus/common/env.js');
+const { getRequiredEnvVar } = require('@cumulus/common/env');
 
 const { listRules } = require('@cumulus/api-client/rules');
 const { removeNilProperties } = require('@cumulus/common/util');
