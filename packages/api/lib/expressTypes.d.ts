69391ff74e475f4b41fa9d6048ef5194b89fea9e
Jonathan Kovarik <jonathan.kovarik@gmail.com>
2023-02-09 22:49:37 -0700

    Remove unneeded type exports

--- packages/api/lib/expressTypes.d.ts
+++
@@ -1,5 +0,0 @@
-import Boom from 'boom';
-
-export type ExpressRequest = import('express').Request;
-export type ExpressResponse = import('express').Response;
-export type ExpressNextFunction = import('express').NextFunction;
