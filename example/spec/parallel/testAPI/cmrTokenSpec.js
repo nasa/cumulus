2f1cf09b70474d6823a5eebcb42fe62d17e99a6d
jennyhliu <jenny.h.liu@nasa.gov>
2023-03-01 06:59:50 -0500

    fix integration test

--- example/spec/parallel/testAPI/cmrTokenSpec.js
+++ example/spec/parallel/testAPI/cmrTokenSpec.js
@@ -1,6 +1,8 @@
 'use strict';
 
-const { CMR, EarthdataLogin } = require('@cumulus/cmr-client');
+const { CMR } = require('@cumulus/cmr-client');
+// EarthdataLogin is not part of the public cmr-client API
+const { EarthdataLogin } = require('@cumulus/cmr-client/EarthdataLogin');
 const { loadConfig } = require('../../helpers/testUtils');
 const { setDistributionApiEnvVars } = require('../../helpers/apiUtils');
 
