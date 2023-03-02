a184b2d0fdbe7a587167928f5dd8e8625777c6d0
Naga Nages <nnagavenkata@yahoo.com>
2023-03-02 11:42:24 -0500

    fixing safe directory

--- bamboo/use-working-directory.sh
+++ bamboo/use-working-directory.sh
@@ -2,6 +2,8 @@
 ## script moves from <current> at <GIT_SHA> to /uncached/cumulus and checks out <GIT_SHA>
 set -ex
 
+git config --global --add safe.directory $bamboo_working_directory/source/cumulus
+
 GIT_SHA=$(git rev-parse HEAD)
 export GIT_SHA
 cd /uncached/cumulus
