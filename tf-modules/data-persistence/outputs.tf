dfb13b8be5149ea369a4df7ff9a008a628daa286
Nate Pauzenga <npauzenga@gmail.com>
2023-03-02 15:11:32 -0500

    remove granule dynamo table ref

--- tf-modules/data-persistence/outputs.tf
+++ tf-modules/data-persistence/outputs.tf
@@ -12,10 +12,6 @@ output "dynamo_tables" {
       name = aws_dynamodb_table.collections_table.name
       arn  = aws_dynamodb_table.collections_table.arn
     }
-    granules = {
-      name = aws_dynamodb_table.granules_table.name
-      arn  = aws_dynamodb_table.granules_table.arn
-    }
     pdrs = {
       name = aws_dynamodb_table.pdrs_table.name
       arn  = aws_dynamodb_table.pdrs_table.arn
