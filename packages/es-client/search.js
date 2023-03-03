03c671093b1874118abb5a0d9495cbd57c974c28
Charles Huang <charles.huang@nasa.gov>
2023-03-03 16:25:47 -0500

    CUMULUS-3008: remove missed bracket

--- packages/es-client/search.js
+++ packages/es-client/search.js
@@ -137,7 +137,6 @@ class BaseSearch {
     this.frm = (page - 1) * this.size;
     this.page = Number.parseInt((params.skip) ? params.skip : page, 10);
     this.index = index || defaultIndexAlias;
-    }
   }
 
   _buildSearch() {
