0c5d02796210362e5e53bc6d309fb26d7c03bdd0
moduyebo <michael.a.oduyebo@nasa.gov>
2023-03-06 11:07:48 -0500

    empty commit

--- tf-modules/archive/ingest-reporting.tf
+++ tf-modules/archive/ingest-reporting.tf
@@ -1,26 +1,22 @@
 # Report executions
-
 resource "aws_sns_topic" "report_executions_topic" {
   name = "${var.prefix}-report-executions-topic"
   tags = var.tags
 }
 
 # Report granules
-
 resource "aws_sns_topic" "report_granules_topic" {
   name = "${var.prefix}-report-granules-topic"
   tags = var.tags
 }
 
 # Report PDRs
-
 resource "aws_sns_topic" "report_pdrs_topic" {
   name = "${var.prefix}-report-pdrs-topic"
   tags = var.tags
 }
 
 # Report collections
-
 resource "aws_sns_topic" "report_collections_topic" {
   name = "${var.prefix}-report-collections-topic"
   tags = var.tags
