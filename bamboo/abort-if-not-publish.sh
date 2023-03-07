ed07f6221ec5cd78e6dab75bac22de24dadb055a
jennyhliu <jenny.h.liu@nasa.gov>
2023-03-07 13:40:22 -0500

    add CUMULUS-2997 to build doc

--- bamboo/abort-if-not-publish.sh
+++ bamboo/abort-if-not-publish.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-if [[ $BRANCH != master && $PUBLISH_FLAG != true ]]; then
+if [[ $BRANCH != master && $BRANCH != CUMULUS-2997 && $PUBLISH_FLAG != true ]]; then
   >&2 echo "******PUBLISH_FLAG or master branch not detected, skipping doc publish"
   exit 0
 fi
