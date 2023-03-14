89c5c30d21436b0af31fead445c280276f661823
jennyhliu <34660846+jennyhliu@users.noreply.github.com>
2023-03-14 13:36:13 -0400

    CUMULUS-3165: Update example/cumulus-tf/orca.tf to orca v6.0.3 (#3300)
    * upgrade to orca v6.0.3
    
    * fix lambda.zip huge size issue

--- example/lambdas/python-reference-task/package.sh
+++ example/lambdas/python-reference-task/package.sh
@@ -20,7 +20,7 @@ cp ./*.py ./dist/
 
 cd ./dist || exit 1
 
-find . -type f -print0 | xargs -0 node ../../../../bin/zip.js lambda.zip $(ls | grep -v lambda.zip)
+node ../../../../bin/zip.js lambda.zip $(ls | grep -v lambda.zip)
 
 cd .. || exit 1
 
