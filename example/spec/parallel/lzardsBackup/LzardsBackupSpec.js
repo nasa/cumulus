5ed07abd06e3c4bf6f2279a1aa5c32fb0e60aff7
Jennifer Tran <12633533+botanical@users.noreply.github.com>
2023-03-15 20:24:08 -0400

    Update LzardsBackupSpec to consolidate spec to use a single granule with multiple files of different checksum types

--- example/spec/parallel/lzardsBackup/LzardsBackupSpec.js
+++ example/spec/parallel/lzardsBackup/LzardsBackupSpec.js
@@ -40,6 +40,10 @@ describe('The Lzards Backup Task ', () => {
       ingestPath = `${prefix}/lzardsBackupSpec`;
       await putFile(ingestBucket, `${ingestPath}/testGranule.dat`, path.join(__dirname, 'test_data', 'testGranule.dat'));
       await putFile(ingestBucket, `${ingestPath}/testGranule.jpg`, path.join(__dirname, 'test_data', 'testGranule.jpg'));
+      await putFile(ingestBucket, `${ingestPath}/testGranule2.dat`, path.join(__dirname, 'test_data', 'testGranule2.dat'));
+      await putFile(ingestBucket, `${ingestPath}/testGranule2.jpg`, path.join(__dirname, 'test_data', 'testGranule2.jpg'));
+      await putFile(ingestBucket, `${ingestPath}/testGranule3.dat`, path.join(__dirname, 'test_data', 'testGranule3.dat'));
+      await putFile(ingestBucket, `${ingestPath}/testGranule3.jpg`, path.join(__dirname, 'test_data', 'testGranule3.jpg'));
       FunctionName = `${prefix}-LzardsBackup`;
       lzardsApiGetFunctionName = `${prefix}-LzardsApiClientTest`;
       functionConfig = await lambda().getFunctionConfiguration({
@@ -68,6 +72,101 @@ describe('The Lzards Backup Task ', () => {
         }
       );
 
+      const Payload = JSON.stringify(
+        {
+          cma: {
+            ReplaceConfig: {
+              Path: '$.payload',
+              TargetPath: '$.payload',
+            },
+            task_config: {
+              cumulus_message: {
+                outputs: [
+                  {
+                    source: '{$.originalPayload}',
+                    destination: '{$.payload}',
+                  },
+                  {
+                    source: '{$.backupResults}',
+                    destination: '{$.meta.backupStatus}',
+                  },
+                ],
+              },
+            },
+            event: {
+              cumulus_meta: {
+                system_bucket: config.bucket,
+              },
+              meta: {
+                buckets: config.buckets,
+                collection,
+                stack: config.stackName,
+              },
+              payload: {
+                granules: [
+                  {
+                    granuleId,
+                    dataType: collection.name,
+                    version: collection.version,
+                    provider,
+                    createdAt: tenMinutesAgo,
+                    files: [
+                      {
+                        fileName: 'testGranule.jpg',
+                        bucket: ingestBucket,
+                        key: `${ingestPath}/testGranule.jpg`,
+                        checksumType: 'md5',
+                        checksum: '5799f9560b232baf54337d334179caa0',
+                      },
+                      {
+                        fileName: 'testGranule.dat',
+                        bucket: ingestBucket,
+                        key: `${ingestPath}/testGranule.dat`,
+                        checksumType: 'md5',
+                        checksum: '39a870a194a787550b6b5d1f49629236',
+                      },
+                      {
+                        fileName: 'testGranule2.jpg',
+                        bucket: ingestBucket,
+                        key: `${ingestPath}/testGranule2.jpg`,
+                        checksumType: 'sha256',
+                        checksum: 'cf27948e5b84c8b3254162a77193ae02e4971da6313ddffaf075c45d7ca03fce',
+                      },
+                      {
+                        fileName: 'testGranule2.dat',
+                        bucket: ingestBucket,
+                        key: `${ingestPath}/testGranule2.dat`,
+                        checksumType: 'sha256',
+                        checksum: 'cf27948e5b84c8b3254162a77193ae02e4971da6313ddffaf075c45d7ca03fce',
+                      },
+                      {
+                        fileName: 'testGranule3.jpg',
+                        bucket: ingestBucket,
+                        key: `${ingestPath}/testGranule3.jpg`,
+                        checksumType: 'sha512',
+                        checksum: 'ceabc00b6c6d0b58c8dfa8e398808e217e893b01e4bf617e043d18d8680275285ad8dcd2aff88916d49a115ad76f8af5966f75cef481ab4764355254655fac2b',
+                      },
+                      {
+                        fileName: 'testGranule3.dat',
+                        bucket: ingestBucket,
+                        key: `${ingestPath}/testGranule3.dat`,
+                        checksumType: 'sha512',
+                        checksum: 'ceabc00b6c6d0b58c8dfa8e398808e217e893b01e4bf617e043d18d8680275285ad8dcd2aff88916d49a115ad76f8af5966f75cef481ab4764355254655fac2b',
+                      },
+                    ],
+                  },
+                ],
+              },
+            },
+          },
+        }
+      );
+
+      lzardsBackupOutput = await pTimeout(
+        lambda().invoke({ FunctionName, Payload }).promise(),
+        (functionConfig.Timeout + 10) * 1000
+      );
+
       console.log(`generated collection: ${JSON.stringify(collection)}`);
     } catch (error) {
       beforeAllFailed = true;
@@ -75,156 +174,22 @@ describe('The Lzards Backup Task ', () => {
     }
   });
 
-  const createPayloadWithChecksumType = (checksumType, checksum1, checksum2) => JSON.stringify(
-    {
-      cma: {
-        ReplaceConfig: {
-          Path: '$.payload',
-          TargetPath: '$.payload',
-        },
-        task_config: {
-          cumulus_message: {
-            outputs: [
-              {
-                source: '{$.originalPayload}',
-                destination: '{$.payload}',
-              },
-              {
-                source: '{$.backupResults}',
-                destination: '{$.meta.backupStatus}',
-              },
-            ],
-          },
-        },
-        event: {
-          cumulus_meta: {
-            system_bucket: config.bucket,
-          },
-          meta: {
-            buckets: config.buckets,
-            collection,
-            stack: config.stackName,
-          },
-          payload: {
-            granules: [
-              {
-                granuleId,
-                dataType: collection.name,
-                version: collection.version,
-                provider,
-                createdAt: tenMinutesAgo,
-                files: [
-                  {
-                    fileName: 'testGranule.jpg',
-                    bucket: ingestBucket,
-                    key: `${ingestPath}/testGranule.jpg`,
-                    checksumType: checksumType,
-                    checksum: checksum1,
-                  },
-                  {
-                    fileName: 'testGranule.dat',
-                    bucket: ingestBucket,
-                    key: `${ingestPath}/testGranule.dat`,
-                    checksumType: checksumType,
-                    checksum: checksum2,
-                  },
-                ],
-              },
-            ],
-          },
-        },
-      },
+  it('succeeds', () => {
+    if (beforeAllFailed) fail('beforeAll() failed');
+    else {
+      expect(lzardsBackupOutput.FunctionError).toBe(undefined);
     }
-  );
-
-  describe('With an md5 checksum', () => {
-    beforeAll(async () => {
-      const checksum1 = '5799f9560b232baf54337d334179caa0';
-      const checksum2 = '39a870a194a787550b6b5d1f49629236';
-      const payloadWithMd5Checksum = createPayloadWithChecksumType('md5', checksum1, checksum2);
-
-      lzardsBackupOutput = await pTimeout(
-        lambda().invoke({ FunctionName, Payload: payloadWithMd5Checksum }).promise(),
-        (functionConfig.Timeout + 10) * 1000
-      );
-    });
-    it('succeeds', () => {
-      if (beforeAllFailed) fail('beforeAll() failed');
-      else {
-        expect(lzardsBackupOutput.FunctionError).toBe(undefined);
-      }
-    });
-
-    it('has the expected backup information', () => {
-      const backupStatus = JSON.parse(lzardsBackupOutput.Payload).meta.backupStatus;
-      console.log(`backupStatus: ${JSON.stringify(backupStatus)}`);
-      expect(backupStatus[0].status).toBe('COMPLETED');
-      expect(backupStatus[0].statusCode).toBe(201);
-      expect(backupStatus[0].granuleId).toBe(granuleId);
-      expect(backupStatus[0].provider).toBe(provider);
-      expect(backupStatus[0].createdAt).toBe(tenMinutesAgo);
-      expect(backupStatus[0].collectionId).toBe(constructCollectionId(collection.name, collection.version));
-    });
   });
 
-  describe('With an sha256 checksum', () => {
-    beforeAll(async () => {
-      const checksum1 = '6fafafa6384f939c983d04ad0ffa2dec4ffac22849b3fc7a22f1c6063acc0db3';
-      const checksum2 = '5bb9ddb36633012eb2ec971b0b9fca3ed71878cb7c64252208f4426ec19eeb65';
-      const payloadWithSha256Checksum = createPayloadWithChecksumType('sha256', checksum1, checksum2);
-
-      lzardsBackupOutput = await pTimeout(
-        lambda().invoke({ FunctionName, Payload: payloadWithSha256Checksum }).promise(),
-        (functionConfig.Timeout + 10) * 1000
-      );
-    });
-    it('succeeds', () => {
-      if (beforeAllFailed) fail('beforeAll() failed');
-      else {
-        expect(lzardsBackupOutput.FunctionError).toBe(undefined);
-      }
-    });
-
-    it('has the expected backup information', () => {
-      const backupStatus = JSON.parse(lzardsBackupOutput.Payload).meta.backupStatus;
-      console.log(`backupStatus: ${JSON.stringify(backupStatus)}`);
-      expect(backupStatus[0].status).toBe('COMPLETED');
-      expect(backupStatus[0].statusCode).toBe(201);
-      expect(backupStatus[0].granuleId).toBe(granuleId);
-      expect(backupStatus[0].provider).toBe(provider);
-      expect(backupStatus[0].createdAt).toBe(tenMinutesAgo);
-      expect(backupStatus[0].collectionId).toBe(constructCollectionId(collection.name, collection.version));
-    });
-  });
-
-  describe('With an sha512 checksum', () => {
-    beforeAll(async () => {
-      const checksum1 = '386bff951dbf6c7329c9cfccfe65b29f13d9c58e39dd56d65357150ba18124af96506cf4752f70ac14f0513b032bc5ef6953eb0eedb64c55a5641420633ba1a0';
-      const checksum2 = '383502dcb509bbc9f3cad73202397e4f8db08ef6ec09e5dd498fe735d2d0c68b6ea3b27c3bfdd494d1fed7ce9e3786af77d0e126a4faa834ee9e86e998ff19a7';
-      const payloadWithSha512Checksum = createPayloadWithChecksumType('sha512', checksum1, checksum2);
-
-      lzardsBackupOutput = await pTimeout(
-        lambda().invoke({ FunctionName, Payload: payloadWithSha512Checksum }).promise(),
-        (functionConfig.Timeout + 10) * 1000
-      );
-    });
-    it('succeeds', () => {
-      if (beforeAllFailed) fail('beforeAll() failed');
-      else {
-        expect(lzardsBackupOutput.FunctionError).toBe(undefined);
-      }
-    });
-
-    it('has the expected backup information', () => {
-      const backupStatus = JSON.parse(lzardsBackupOutput.Payload).meta.backupStatus;
-      console.log(`backupStatus: ${JSON.stringify(backupStatus)}`);
-      expect(backupStatus[0].status).toBe('COMPLETED');
-      expect(backupStatus[0].statusCode).toBe(201);
-      expect(backupStatus[0].granuleId).toBe(granuleId);
-      expect(backupStatus[0].provider).toBe(provider);
-      expect(backupStatus[0].createdAt).toBe(tenMinutesAgo);
-      expect(backupStatus[0].collectionId).toBe(constructCollectionId(collection.name, collection.version));
-    });
+  it('has the expected backup information', () => {
+    const backupStatus = JSON.parse(lzardsBackupOutput.Payload).meta.backupStatus;
+    console.log(`backupStatus: ${JSON.stringify(backupStatus)}`);
+    expect(backupStatus[0].status).toBe('COMPLETED');
+    expect(backupStatus[0].statusCode).toBe(201);
+    expect(backupStatus[0].granuleId).toBe(granuleId);
+    expect(backupStatus[0].provider).toBe(provider);
+    expect(backupStatus[0].createdAt).toBe(tenMinutesAgo);
+    expect(backupStatus[0].collectionId).toBe(constructCollectionId(collection.name, collection.version));
   });
 
   describe('The Lzards API Client', () => {
