98edd626c4a99fdf4fe12f2e248238cd88e5d3f5
Jim Cole <jim.cole@colorado.edu>
2023-03-24 10:23:20 -0600

    Add support for generating and writing the PAN locally

--- tasks/send-pan/index.js
+++ tasks/send-pan/index.js
@@ -1,15 +1,16 @@
 'use strict';
 
+const pdrs = require('@cumulus/api/models/pdrs');
 const cumulusMessageAdapter = require('@cumulus/cumulus-message-adapter-js');
-const path = require('path');
 const fs = require('fs');
+const path = require('path');
 const { buildUploaderClient } = require('./uploader');
 
 /**
  * Return Input payload
  *
- * @param {Object} event - input from the message adapter
- * @returns {Object} sample JSON object
+ * @param {object} event - input from the message adapter
+ * @returns {object} sample JSON object
  */
 async function sendPAN(event) {
   const config = event.config;
@@ -18,25 +19,32 @@ async function sendPAN(event) {
   const panName = config.pdrName.replace('.pdr', '.pan');
   const uploadPath = path.join(remoteDir, panName);
 
-  // TODO - replace with PAN generation
-  const localPath = '/tmp/test.pan';
-  fs.writeFile(localPath, 'Hello world!', (err) => {
+  const pan = pdrs.generatePAN();
+
+  const localPath = `/tmp/${panName}`;
+  fs.writeFile(localPath, pan, (err) => {
     if (err) {
-      console.error(err);
+      throw new Error(`Unable to write file ${localPath}: ${err.message}`);
     }
   });
 
   const providerClient = buildUploaderClient(provider);
   await providerClient.upload({ localPath, uploadPath });
 
+  fs.unlink(localPath, (err) => {
+    if (err) {
+      throw new Error(`Unable to unlink file ${localPath}: ${err.message}.`);
+    }
+  });
+
   return event;
 }
 /**
  * Lambda handler
  *
- * @param {Object} event      - a Cumulus Message
- * @param {Object} context    - an AWS Lambda context
- * @returns {Promise<Object>} - sample JSON object
+ * @param {object} event      - a Cumulus Message
+ * @param {object} context    - an AWS Lambda context
+ * @returns {Promise<object>} - sample JSON object
  */
 async function handler(event, context) {
   return await cumulusMessageAdapter.runCumulusTask(sendPAN, event, context);
