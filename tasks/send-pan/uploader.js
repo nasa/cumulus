42b326ed87d44aa2181eb5a15029de77c753c475
Jim Cole <jim.cole@colorado.edu>
2023-03-24 10:22:28 -0600

    Cleanup ESLint warnings

--- tasks/send-pan/uploader.js
+++ tasks/send-pan/uploader.js
@@ -10,66 +10,85 @@ const stream = require('node:stream');
 const got = require('got');
 const fs = require('fs');
 
+/**
+ * Upload PAN file via FTP
+ */
+async function ftpUploader() {
+//  return {};
+}
+
+/**
+ * Upload PAN via HTTP or HTTPS
+ *
+ * @param {object} params - provider configuration
+ */
+async function httpUploader(params) {
+  const { localPath, uploadPath } = params;
+  await this.setUpGotOptions();
+  await this.downloadTLSCertificate();
+  const options = {
+    protocol: 'http',
+    host: this.host,
+    port: this.port,
+    path: uploadPath,
+    method: 'POST',
+  };
+
+  log.info(params);
+
+  const remoteUrl = buildURL(options);
+  log.info(`Uploading ${localPath} to ${remoteUrl}`);
+  got.stream.options = options;
+  await promisify(pipeline)(
+    fs.createReadStream(localPath),
+    await got.stream.post(remoteUrl),
+    new stream.PassThrough()
+  );
+
+  log.info(`Finishing uploading ${localPath} to ${remoteUrl}`);
+
+  return localPath;
+}
+
+/**
+ * Upload PAN via S3
+ *
+ * @param {object} _params - provider bucket
+ */
+async function s3Uploader(_params) {
+//  return {};
+}
+
+/**
+ * Upload PAN via SFTP
+ *
+ * @param {object} _params - provider configuration
+ */
+async function sftpUploader(_params) {
+//  return {};
+}
+
 const buildUploaderClient = (providerConfig = {}) => {
-    switch (providerConfig.protocol) {
-        case 'ftp':
-            FtpProviderClient.prototype.upload = ftp_uploader
-            uploader_client = new FtpProviderClient(providerConfig);
-            return uploader_client
-        case 'http':
-        case 'https':
-            HttpProviderClient.prototype.upload = http_uploader
-            return new HttpProviderClient(providerConfig);
-        case 's3':
-            S3ProviderClient.prototype.upload = s3_uploader
-            return new S3ProviderClient({ bucket: providerConfig.host });
-        case 'sftp':
-            SftpProviderClient.prototype.upload = sftp_uploader
-            return new SftpProviderClient(providerConfig);
-        default:
-            throw new Error(`Protocol ${providerConfig.protocol} is not supported.`);
-    }
+  switch (providerConfig.protocol) {
+  case 'ftp': {
+    FtpProviderClient.prototype.upload = ftpUploader;
+    const uploaderClient = new FtpProviderClient(providerConfig);
+    return uploaderClient;
+  }
+  case 'http':
+  case 'https':
+    HttpProviderClient.prototype.upload = httpUploader;
+    return new HttpProviderClient(providerConfig);
+  case 's3':
+    S3ProviderClient.prototype.upload = s3Uploader;
+    return new S3ProviderClient({ bucket: providerConfig.host });
+  case 'sftp':
+    SftpProviderClient.prototype.upload = sftpUploader;
+    return new SftpProviderClient(providerConfig);
+  default:
+    throw new Error(`Protocol ${providerConfig.protocol} is not supported.`);
+  }
 };
-
-async function ftp_uploader() {
-    return {}
-}
-
-async function http_uploader(params) {
-    const { localPath, uploadPath } = params;
-    await this.setUpGotOptions();
-    await this.downloadTLSCertificate();
-    const options = {
-        protocol: 'http',
-        host: this.host,
-        port: this.port,
-        path: uploadPath,
-        method: 'POST',
-    }
-    log.info(params)
-    let remoteUrl = buildURL(options);
-    log.info(`Uploading ${localPath} to ${remoteUrl}`);
-    got.stream.options = options;
-    await promisify(pipeline)(
-        fs.createReadStream(localPath),
-        await got.stream.post(remoteUrl),
-        new stream.PassThrough()
-    );
-
-    log.info(`Finishing uploading ${localPath} to ${remoteUrl}`);
-
-    return localPath;
-}
-
-
-async function s3_uploader(params) {
-    return {}
-}
-
-async function sftp_uploader(params) {
-    return {}
-}
-
 module.exports = {
-    buildUploaderClient
+  buildUploaderClient,
 };
