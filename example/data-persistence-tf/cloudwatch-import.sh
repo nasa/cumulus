357759cf696cbb2a69f9ac74b87dc75da7a8397f
Naga Nages <nnagavenkata@yahoo.com>
2023-04-13 14:54:02 -0400

    finalized script

--- example/data-persistence-tf/cloudwatch-import.sh
+++ example/data-persistence-tf/cloudwatch-import.sh
@@ -1,3 +1,10 @@
-# SCRIPT FOR IMPORTING CLOUDWATCH LOG GROUPS TO TERRAFORM STATE (DATA PERSISTENVCE)
-terraform import module.data_persistence.module.db_migration.aws_cloudwatch_log_group.db_migration "/aws/lambda/${prefix}-postgres-db-migration"
-terraform import module.provision_database.aws_cloudwatch_log_group.provision_database "/aws/lambda/${prefix}-ProvisionPostgresDatabase"
+# SCRIPT FOR IMPORTING CLOUDWATCH LOG GROUPS TO TERRAFORM STATE (DATA PERSISTENCE TF)
+#!/bin/zsh
+set -e
+
+echo -n "Enter the prefix used for your terraform deployment and press [ENTER]:"
+read prefix
+terraform init
+
+terraform import module.data_persistence.module.db_migration.aws_cloudwatch_log_group.db_migration "/aws/lambda/$prefix-postgres-db-migration"
+terraform import module.provision_database.aws_cloudwatch_log_group.provision_database "/aws/lambda/$prefix-ProvisionPostgresDatabase"
