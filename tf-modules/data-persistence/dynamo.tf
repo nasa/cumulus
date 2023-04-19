4eb73ff428ebfdf1581c0e1037ac703295f0edb8
Nate Pauzenga <npauzenga@gmail.com>
2023-04-19 15:54:42 -0400

    [CUMULUS-3226] Remove refs to async operations dynamo table (#3331)
    * Remove refs to async operations dynamo table
    
    * add changelog entry
    
    * Remove additional async operation table refs
    
    * Update process_dead_letter_archive role

--- tf-modules/data-persistence/dynamo.tf
+++ tf-modules/data-persistence/dynamo.tf
@@ -2,7 +2,6 @@ locals {
   enable_point_in_time_table_names = [for x in var.enable_point_in_time_tables : "${var.prefix}-${x}"]
   table_names = {
     access_tokens_table          = "${var.prefix}-AccessTokensTable"
-    async_operations_table       = "${var.prefix}-AsyncOperationsTable"
     reconciliation_reports_table = "${var.prefix}-ReconciliationReportsTable"
     semaphores_table             = "${var.prefix}-SemaphoresTable"
   }
@@ -25,30 +24,6 @@ resource "aws_dynamodb_table" "access_tokens_table" {
 
   point_in_time_recovery {
     enabled = contains(local.enable_point_in_time_table_names, local.table_names.access_tokens_table)
-  }
-
-  lifecycle {
-    prevent_destroy = true
-    ignore_changes = [ name ]
-  }
-
-  tags = var.tags
-}
-
-resource "aws_dynamodb_table" "async_operations_table" {
-  name         = local.table_names.async_operations_table
-  billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "id"
-  stream_enabled   = true
-  stream_view_type = "NEW_AND_OLD_IMAGES"
-
-  attribute {
-    name = "id"
-    type = "S"
-  }
-
-  point_in_time_recovery {
-    enabled = contains(local.enable_point_in_time_table_names, local.table_names.async_operations_table)
   }
 
   lifecycle {
