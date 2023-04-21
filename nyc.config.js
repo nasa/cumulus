d952dbd8442d456146911ab1444caebb287ce56c
ecarton <ethan.j.carton@nasa.gov>
2023-04-21 16:17:40 -0400

    move nyc env variables to nyc config

--- nyc.config.js
+++ nyc.config.js
@@ -4,7 +4,7 @@ const path = require('path');
 
 module.exports = {
   all: true,
-  clean: process.env.NYC_CLEAN !== 'false',
+  clean: process.env.NYC_CLEAN !== 'true',
   silent: process.env.NYC_SILENT === 'true',
   'cache-dir': path.join(__dirname, 'node_modules', '.cache', 'nyc'),
   'temp-dir': path.join(__dirname, '.nyc_output'),
