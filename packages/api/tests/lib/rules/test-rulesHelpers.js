5671a95a957e448988b6c706f5bd2966472f48c2
Jonathan Kovarik <jonathan.kovarik@gmail.com>
2023-04-24 15:14:28 -0600

    merge /lib/test-sns-rules.js into lib/rules/test-rulesHelpers.js

--- packages/api/tests/lib/rules/test-rulesHelpers.js
+++ packages/api/tests/lib/rules/test-rulesHelpers.js
@@ -10,11 +10,16 @@ const { recursivelyDeleteS3Bucket } = require('@cumulus/aws-client/S3');
 const { sqsQueueExists } = require('@cumulus/aws-client/SQS');
 const { randomId, randomString } = require('@cumulus/common/test-utils');
 const {
+  CollectionPgModel,
   destroyLocalTestDb,
+  fakeCollectionRecordFactory,
+  fakeProviderRecordFactory,
   generateLocalTestDb,
   localStackConnectionEnv,
   migrationDir,
+  ProviderPgModel,
   RulePgModel,
+  translateApiRuleToPostgresRule,
   translateApiRuleToPostgresRuleRaw,
 } = require('@cumulus/db');
 
@@ -36,6 +41,8 @@ const rulesHelpers = proxyquire('../../../lib/rulesHelpers', {
     handleScheduleEvent: (payload) => payload,
   },
 });
+
+const { ResourceNotFoundError, resourceNotFoundInfo } = require('../../../lib/errors');
 
 const testDbName = randomString(12);
 
@@ -109,10 +116,20 @@ test.before(async (t) => {
   t.context.testKnex = knex;
   t.context.testKnexAdmin = knexAdmin;
   t.context.rulePgModel = new RulePgModel();
+  t.context.providerPgModel = new ProviderPgModel();
+  t.context.collectionPgModel = new CollectionPgModel();
 });
 
-test.afterEach.always(() => {
+test.beforeEach(async (t) => {
+  t.context.sandbox = sinon.createSandbox();
+  const topic = await awsServices.sns().createTopic({ Name: randomId('sns') }).promise();
+  t.context.snsTopicArn = topic.TopicArn;
+});
+
+test.afterEach.always(async (t) => {
   listRulesStub.reset();
+  t.context.sandbox.restore();
+  await awsServices.sns().deleteTopic({ TopicArn: t.context.snsTopicArn }).promise();
 });
 
 test.after.always(async (t) => {
@@ -964,4 +981,200 @@ test.serial('checkForSnsSubscriptions returns the correct status of a Rule\'s su
   t.true(response.existingSubscriptionArn.includes(topic1.TopicArn));
 
   t.teardown(() => rulePgModel.delete(testKnex, newPgRule));
+});
+
+test.serial('disabling an SNS rule removes the event source mapping', async (t) => {
+  const {
+    snsTopicArn,
+    testKnex,
+  } = t.context;
+
+  const item = fakeRuleFactoryV2({
+    workflow,
+    rule: {
+      type: 'sns',
+      value: snsTopicArn,
+    },
+    state: 'ENABLED',
+  });
+
+  const ruleWithTrigger = await rulesHelpers.createRuleTrigger(item);
+
+  t.is(ruleWithTrigger.rule.value, snsTopicArn);
+  t.truthy(ruleWithTrigger.rule.arn);
+  t.is(ruleWithTrigger.state, 'ENABLED');
+
+  const updates = { ...ruleWithTrigger, state: 'DISABLED' };
+  const ruleWithUpdatedTrigger = await rulesHelpers.updateRuleTrigger(
+    ruleWithTrigger,
+    updates,
+    testKnex
+  );
+
+  t.is(ruleWithUpdatedTrigger.name, ruleWithTrigger.name);
+  t.is(ruleWithUpdatedTrigger.state, 'DISABLED');
+  t.is(ruleWithUpdatedTrigger.rule.type, ruleWithTrigger.rule.type);
+  t.is(ruleWithUpdatedTrigger.rule.value, ruleWithTrigger.rule.value);
+  t.falsy(ruleWithUpdatedTrigger.rule.arn);
+});
+
+test.serial('deleting an SNS rule updates the event source mapping', async (t) => {
+  const {
+    snsTopicArn,
+    testKnex,
+  } = t.context;
+
+  const unsubscribeSpy = sinon.spy(awsServices.sns(), 'unsubscribe');
+
+  const item = fakeRuleFactoryV2({
+    workflow,
+    rule: {
+      type: 'sns',
+      value: snsTopicArn,
+    },
+    state: 'ENABLED',
+  });
+
+  const ruleWithTrigger = await rulesHelpers.createRuleTrigger(item);
+
+  t.is(ruleWithTrigger.rule.value, snsTopicArn);
+
+  await rulesHelpers.deleteRuleResources(testKnex, ruleWithTrigger);
+
+  t.true(unsubscribeSpy.called);
+  t.true(unsubscribeSpy.calledWith({
+    SubscriptionArn: ruleWithTrigger.rule.arn,
+  }));
+
+  t.teardown(() => {
+    unsubscribeSpy.restore();
+  });
+});
+
+test.serial('Multiple rules using same SNS topic can be created and deleted', async (t) => {
+  const {
+    collectionPgModel,
+    providerPgModel,
+    testKnex,
+    rulePgModel,
+  } = t.context;
+  const testPgProvider = fakeProviderRecordFactory();
+  await providerPgModel.create(
+    testKnex,
+    testPgProvider,
+    '*'
+  );
+
+  const testPgCollection1 = fakeCollectionRecordFactory({
+    name: randomId('collection-'),
+    version: 'v1',
+  });
+  const testPgCollection2 = fakeCollectionRecordFactory({
+    name: randomId('collection-'),
+    version: 'v2',
+  });
+  await Promise.all([
+    collectionPgModel.create(
+      testKnex,
+      testPgCollection1,
+      '*'
+    ),
+    collectionPgModel.create(
+      testKnex,
+      testPgCollection2,
+      '*'
+    ),
+  ]);
+  const unsubscribeSpy = sinon.spy(awsServices.sns(), 'unsubscribe');
+  const { TopicArn } = await awsServices.sns().createTopic({
+    Name: randomId('topic'),
+  }).promise();
+
+  const ruleWithTrigger = await rulesHelpers.createRuleTrigger(fakeRuleFactoryV2({
+    name: randomId('rule1'),
+    rule: {
+      type: 'sns',
+      value: TopicArn,
+    },
+    workflow,
+    state: 'ENABLED',
+    collection: {
+      name: testPgCollection1.name,
+      version: testPgCollection1.version,
+    },
+    provider: testPgProvider.name,
+  }));
+  const ruleWithTrigger2 = await rulesHelpers.createRuleTrigger(fakeRuleFactoryV2({
+    name: randomId('rule2'),
+    rule: {
+      type: 'sns',
+      value: TopicArn,
+    },
+    workflow,
+    state: 'ENABLED',
+    collection: {
+      name: testPgCollection2.name,
+      version: testPgCollection2.version,
+    },
+    provider: testPgProvider.name,
+  }));
+
+  const pgRule = await translateApiRuleToPostgresRule(ruleWithTrigger, testKnex);
+  const pgRule2 = await translateApiRuleToPostgresRule(ruleWithTrigger2, testKnex);
+
+  const [rule1] = await rulePgModel.create(testKnex, pgRule);
+  const [rule2] = await rulePgModel.create(testKnex, pgRule2);
+
+  // rules share the same subscription
+  t.is(rule1.arn, rule2.arn);
+
+  // Have to delete rules serially otherwise all rules still exist
+  // when logic to check for shared source mapping is evaluated
+
+  await rulesHelpers.deleteSnsTrigger(testKnex, ruleWithTrigger);
+  await rulePgModel.delete(testKnex, rule1);
+
+  await t.notThrowsAsync(rulesHelpers.deleteSnsTrigger(testKnex, ruleWithTrigger2));
+  await rulePgModel.delete(testKnex, rule2);
+
+  // Ensure that cleanup for SNS rule subscription was actually called
+  t.true(unsubscribeSpy.called);
+  t.true(unsubscribeSpy.calledWith({
+    SubscriptionArn: ruleWithTrigger.rule.arn,
+  }));
+
+  t.teardown(async () => {
+    unsubscribeSpy.restore();
+    await awsServices.sns().deleteTopic({
+      TopicArn,
+    });
+  });
+});
+
+test.serial('deleteSnsTrigger throws more detailed ResourceNotFoundError', async (t) => {
+  const errorMessage = 'Resource is not found in resource policy.';
+  const error = new Error(errorMessage);
+  error.code = 'ResourceNotFoundException';
+  const { snsTopicArn } = t.context;
+  const lambdaStub = sinon.stub(awsServices.lambda(), 'removePermission').throws(error);
+
+  const ruleWithTrigger = await rulesHelpers.createRuleTrigger(fakeRuleFactoryV2({
+    rule: {
+      type: 'sns',
+      value: snsTopicArn,
+    },
+    workflow,
+    state: 'ENABLED',
+  }));
+
+  await t.throwsAsync(
+    rulesHelpers.deleteSnsTrigger(t.context.testKnex, ruleWithTrigger), {
+      instanceOf: ResourceNotFoundError,
+      message: `${errorMessage} ${resourceNotFoundInfo}`,
+    }
+  );
+
+  t.teardown(() => {
+    lambdaStub.restore();
+  });
 });
