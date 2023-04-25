79089b659f3dc513db7d0bebc03473aa1e2a4fa7
ecarton <ethan.j.carton@nasa.gov>
2023-04-25 09:54:12 -0400

    turn on checks

--- nyc.config.js
+++ nyc.config.js
@@ -8,7 +8,7 @@ module.exports = {
   silent: process.env.NYC_SILENT === 'true',
   'cache-dir': path.join(__dirname, 'node_modules', '.cache', 'nyc'),
   'temp-dir': path.join(__dirname, '.nyc_output'),
-  // 'check-coverage': true,
+  'check-coverage': true,
   lines: 75,
   functions: 69,
   branches: 70,
