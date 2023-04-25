b6dbbe068a30a7e8d84808d531b09231d97b4f4b
Naga Nages <66387215+Nnaga1@users.noreply.github.com>
2023-04-25 17:19:35 -0400

    CUMULUS-3121/3120 v15.1.0 backport (#3346)
    * backport PR
    
    * finalizing docs and changing remaining groups
    
    * PR feedback + testing changes
    
    * PR feedback
    
    * reverting change
    
    * Revert "update changelog"
    
    This reverts commit ae4627c39d75b5e59befdcd7a950c013fec1667a.
    
    * reverting changes
    
    * PR feedback
    
    * removing EgressLambda from doc
    
    * Update xml2js 0.4.22->0.5 strict (#3330) (#3339)
    
    * Update xml2js 0.4.22->0.5 strict
    
    * Address GHSA-776f-qx25-q3cc/update allow list
    
    * Update CHANGELOG
    
    * Update package pins to 0.5.0 for xmljs
    
    Co-authored-by: Jonathan Kovarik <kovarik@nsidc.org>
    
    * PR feedback
    
    * PR feedback
    
    * PR feedback
    
    * fixing documentation linting
    
    * PR feedback
    
    * PR feedback
    
    * PR feedback
    
    * adding variables to tf-modules/workflow
    
    * PR feedback
    
    * PR feedback
    
    * reverting previous change
    
    ---------
    
    Co-authored-by: jennyhliu <34660846+jennyhliu@users.noreply.github.com>
    Co-authored-by: Jonathan Kovarik <kovarik@nsidc.org>

--- example/cumulus-tf/cumulus_distribution.tf
+++ example/cumulus-tf/cumulus_distribution.tf
@@ -40,4 +40,6 @@ module "cumulus_distribution" {
   system_bucket            = var.system_bucket
   tags                     = local.tags
   vpc_id                   = local.vpc_id
+  default_log_retention_days       = var.default_log_retention_days
+  cloudwatch_log_retention_periods = var.cloudwatch_log_retention_periods
 }
