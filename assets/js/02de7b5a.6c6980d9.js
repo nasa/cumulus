e2507add4ef5fe5cda7564418e79587d6aa9b39f
Bamboo CI <root@ci-agent-ops14.earthdata.nasa.gov>
2023-04-26 20:34:18 +0000

    Automated build in Bamboo CI

--- assets/js/02de7b5a.6c6980d9.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunk_cumulus_website=self.webpackChunk_cumulus_website||[]).push([[76240],{39908:t=>{t.exports=JSON.parse('{"title":"Getting Started","slug":"/category/getting-started","permalink":"/cumulus/docs/next/category/getting-started","navigation":{"next":{"title":"Introduction","permalink":"/cumulus/docs/next/"}}}')}}]);
