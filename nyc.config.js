a3f4af4489cf6c5b345758bfa3e05dac5494cacb
ecarton <ethan.j.carton@nasa.gov>
2023-05-05 14:38:53 -0400

    prepare for env setting

--- nyc.config.js
+++ nyc.config.js
@@ -1,14 +1,10 @@
 'use strict';
 
 const path = require('path');
-
 module.exports = {
   all: true,
   clean: process.env.NYC_CLEAN !== 'true',
   silent: process.env.NYC_SILENT === 'true',
   'cache-dir': path.join(__dirname, 'node_modules', '.cache', 'nyc'),
-  lines: 89,
-  functions: 75,
-  branches: 80,
-  statements: 88,
-};
+  "check-coverage": process.env.FAIL_ON_COVERAGE == undefined || process.env.FAIL_ON_COVERAGE == "true"
+}
