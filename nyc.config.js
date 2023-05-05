a677713d7e5b54ae82a68cc37697fd79fc3c6a01
ecarton <ethan.j.carton@nasa.gov>
2023-05-05 15:38:34 -0400

    linter error fixes

--- nyc.config.js
+++ nyc.config.js
@@ -6,5 +6,5 @@ module.exports = {
   clean: process.env.NYC_CLEAN !== 'true',
   silent: process.env.NYC_SILENT === 'true',
   'cache-dir': path.join(__dirname, 'node_modules', '.cache', 'nyc'),
-  "check-coverage": process.env.FAIL_ON_COVERAGE == undefined || process.env.FAIL_ON_COVERAGE == "true"
-}
+  'check-coverage': process.env.FAIL_ON_COVERAGE === undefined || process.env.FAIL_ON_COVERAGE === 'true',
+};
