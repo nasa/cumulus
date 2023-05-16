f707e83bd8ece6bd3ab39166ca6b2b1a1f104e4d
nasamoduyebo <michael.a.oduyebo@nasa.gov>
2023-05-16 10:20:05 -0400

    eslint errors

--- example/spec/parallel/ingestGranule/IngestGranuleWithDuplicateHandlingVersionSpec.js
+++ example/spec/parallel/ingestGranule/IngestGranuleWithDuplicateHandlingVersionSpec.js
@@ -23,8 +23,8 @@ const { deleteRule } = require('@cumulus/api-client/rules');
 
 const { deleteS3Object, s3PutObject } = require('@cumulus/aws-client/S3');
 
-const { loadConfig } = require('../../helpers/testUtils');
 const { constructCollectionId } = require('@cumulus/message/Collections');
+const { loadConfig } = require('../../helpers/testUtils');
 
 describe('The IngestGranule workflow with DuplicateHandling="version" and a granule re-ingested with one new file, one unchanged existing file, and one modified file', () => {
   let beforeAllError;
@@ -142,7 +142,9 @@ describe('The IngestGranule workflow with DuplicateHandling="version" and a gran
       });
 
       // Wait for the granule to be fully ingested
-      await getGranuleWithStatus({ prefix, granuleId, collectionId:constructCollectionId(collection.name, collection.version),
+      await getGranuleWithStatus({ prefix,
+        granuleId,
+        collectionId: constructCollectionId(collection.name, collection.version),
         status: 'completed' });
 
       // Modify the contents of the updated granule file
@@ -257,7 +259,7 @@ describe('The IngestGranule workflow with DuplicateHandling="version" and a gran
       { stopOnError: false }
     ).catch(console.error);
 
-    await deleteGranule({ prefix, granuleId, collectionId:constructCollectionId(collection.name, collection.version) });
+    await deleteGranule({ prefix, granuleId, collectionId: constructCollectionId(collection.name, collection.version) });
     await Promise.all([
       deleteExecution({ prefix: config.stackName, executionArn: ingestGranuleExecution2Arn }),
       deleteExecution({ prefix: config.stackName, executionArn: ingestGranuleExecution1Arn }),
