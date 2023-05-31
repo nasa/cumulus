05a74c918623755c9be8a896aa7c0a343fee5e41
ecarton <ethan.j.carton@nasa.gov>
2023-05-31 17:25:28 -0400

    small cleanup of experimental change leftover space

--- bamboo/bootstrap-tf-deployment.sh
+++ bamboo/bootstrap-tf-deployment.sh
@@ -44,7 +44,7 @@ fi
 
 # Deploy data-persistence-tf via terraform
 echo "Deploying Cumulus data-persistence module to $DEPLOYMENT"
-../terraform apply\
+../terraform apply \
   -auto-approve \
   -input=false \
   -var-file="../deployments/data-persistence/$BASE_VAR_FILE" \
