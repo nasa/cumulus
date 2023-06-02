b40d51a22ac943a17772582e9ef67d09013db0a9
ecarton <ethan.j.carton@nasa.gov>
2023-06-02 12:29:13 -0400

    Revert "Merge branch 'master' into ecarton-tf-files"
    This reverts commit aaf5ed07dc2d2402e9df12706fc85e0241ab4404.

--- example/scripts/tests-parallel.sh
+++ example/scripts/tests-parallel.sh
@@ -10,12 +10,11 @@ DOT_PID="$!"
 
 TESTS=$(find spec/parallel -type f -name '*spec.js' -or -name '*Spec.js')
 testOutputDir=scripts/test_output
-testTimeout=1200
 
 rm -r -f $testOutputDir
 mkdir -p $testOutputDir
 
-echo "" | ../node_modules/.bin/parallel -j "${INTEGRATION_CONCURRENCY:=0}" sh scripts/run_test.sh  $testOutputDir $testTimeout ::: $TESTS
+echo "" | ../node_modules/.bin/parallel -j "${INTEGRATION_CONCURRENCY:=0}" --timeout 1200 sh scripts/run_test.sh  $testOutputDir ::: $TESTS
 result=$?
 echo parallel tests complete: $result suite failures
 
