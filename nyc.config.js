9bb1b81dc4332a0c7008953efde1209180ac3509
ecarton <ethan.j.carton@nasa.gov>
2023-06-21 14:47:43 -0400

    update to give printout in cicd and not use "all" flag

--- nyc.config.js
+++ nyc.config.js
@@ -2,10 +2,9 @@
 
 const path = require('path');
 module.exports = {
-  all: true,
   clean: process.env.NYC_CLEAN !== 'true',
   silent: process.env.NYC_SILENT === 'true',
-  reporter: 'html',
+  reporter: ['html', 'text'],
   'cache-dir': path.join(__dirname, 'node_modules', '.cache', 'nyc'),
   'check-coverage': process.env.FAIL_ON_COVERAGE === undefined || process.env.FAIL_ON_COVERAGE === 'true',
 };
