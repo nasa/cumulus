be4851d96e5e192f0565ed2aa0fd821f402bda19
Naga Nages <nnagavenkata@yahoo.com>
2023-07-21 11:24:14 -0400

    changing updatedAt value

--- tasks/queue-granules/tests/index.js
+++ tasks/queue-granules/tests/index.js
@@ -1087,7 +1087,7 @@ test.serial('queueGranules throws an error when the updatedAt field is not an In
       granuleId: randomString(), files: [], updatedAt: '12/25/2022', collectionId: 'ABC___001',
     },
     {
-      granuleId: randomString(), files: [], updatedAt: 1553053438767.3, collectionId: 'ABC___001',
+      granuleId: randomString(), files: [], updatedAt: 1553053438767.378196, collectionId: 'ABC___001',
     },
   ];
 
