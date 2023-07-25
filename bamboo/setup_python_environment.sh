f1ec8b8ed1b6d9b7b5a82a8f22422311170e11a6
ecarton <ethan.j.carton@nasa.gov>
2023-07-25 16:59:00 -0400

    working on setting up proper version

--- bamboo/setup_python_environment.sh
+++ bamboo/setup_python_environment.sh
@@ -1,4 +1,9 @@
+PYTHON_VERSION=$(cat .python-version)
 
-apt install wget
-
-apt install python3.10
+export DEBIAN_FRONTEND=noninteractive
+export TZ-Etc/UTC
+apt update -y
+apt install software-properties-common -y
+add-apt-repository ppa:deadsnakes/ppa -y
+apt install python3.10 python3-pip -y
+update-alternatives --install /usr/bin/python python /usr/bin/python3.10 1 &&     update-alternatives --install /usr/bin/pip pip /usr/bin/pip3 1
