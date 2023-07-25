6080ed3b9eeb0dc6f695914427bfd3d1b4df319b
ecarton <ethan.j.carton@nasa.gov>
2023-07-25 13:31:54 -0400

    install locally declared pyversion in lint

--- bamboo/bootstrap-lint-audit.sh
+++ bamboo/bootstrap-lint-audit.sh
@@ -5,7 +5,7 @@ set -e
 . ./bamboo/use-working-directory.sh
 . ./bamboo/set-bamboo-env-variables.sh
 . ./bamboo/abort-if-not-pr.sh
-
+pyenv install
 if [[ $USE_CACHED_BOOTSTRAP == true ]]; then ## Change into cached cumulus, pull down /cumulus ref and run there
   echo "*** Using cached bootstrap"
   cp .bamboo_env_vars /cumulus/
