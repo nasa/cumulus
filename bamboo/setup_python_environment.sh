1ba3ec5b0bf1cf7b08218b401b5b4a5ab8d1b042
ecarton <ethan.j.carton@nasa.gov>
2023-07-26 13:33:56 -0400

    working on python3.10 installation

--- bamboo/setup_python_environment.sh
+++ bamboo/setup_python_environment.sh
@@ -1,26 +1,23 @@
 PYTHON_VERSION=$(cat .python-version)
 
-export DEBIAN_FRONTEND=noninteractive
-export TZ=Etc/UTC
-apt update -y
-apt-get install -y \
-  libbz2-dev \
-  libsqlite3-dev \
-  llvm \
-  libncurses5-dev \
-  libncursesw5-dev \
-  tk-dev \
-  liblzma-dev \
-  git \
-  curl
+wget -c https://www.python.org/ftp/python/$PYTHON_VERSION/Python-$PYTHON_VERSION.tar.xz
+tar -Jxf Python-3.11.0.tar.xz
+cd Python-$PYTHON_VERSION
+./configure --enable-optimizations
+make -j4 && sudo make altinstall
+update-alternatives --set python /usr/local/bin/python$PYTHON_VERSION
 
-PROJ=pyenv-installer
-SCRIPT_URL=https://github.com/pyenv/$PROJ/raw/master/bin/$PROJ
-curl -L $SCRIPT_URL | bash
-# setup
-export PATH="~/.pyenv/bin:$PATH"
-eval "$(pyenv init -)"
-eval "$(pyenv virtualenv-init -)"
+update-alternatives --set python3 /usr/local/bin/python$PYTHON_VERSION
+#TODO handle this interaction with base python
+
+# PROJ=pyenv-installer
+# SCRIPT_URL=https://github.com/pyenv/$PROJ/raw/master/bin/$PROJ
+# curl -L $SCRIPT_URL | bash
+# # setup
+# export PYENV_ROOT="$HOME/.pyenv"
+# export PATH="$PYENV_ROOT/bin:$PYENV_ROOT/shims:$PATH"
+# eval "$(pyenv init -)"
+# eval "$(pyenv virtualenv-init -)"
 # apt install software-properties-common -y
 # add-apt-repository ppa:deadsnakes/ppa -y
 # apt install python3.10 python3-pip -y
