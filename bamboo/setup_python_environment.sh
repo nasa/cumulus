5b713e7ca4558e411a369649bfb20df917d54385
ecarton <ethan.j.carton@nasa.gov>
2023-07-26 15:21:55 -0400

    version patch and instal

--- bamboo/setup_python_environment.sh
+++ bamboo/setup_python_environment.sh
@@ -1,13 +1,14 @@
 PYTHON_VERSION=$(cat .python-version)
-
-wget -c https://www.python.org/ftp/python/$PYTHON_VERSION/Python-$PYTHON_VERSION.tgz
-tar -zxf Python-$PYTHON_VERSION.tgz
-cd Python-$PYTHON_VERSION
+PYTHON_VERSION_PATCH=9
+wget -c https://www.python.org/ftp/python/$PYTHON_VERSION.$PYTHON_VERSION_PATCH/Python-$PYTHON_VERSION.$PYTHON_VERSION_PATCH.tgz
+tar -zxf Python-$PYTHON_VERSION.$PYTHON_VERSION_PATCH.tgz
+cd Python-$PYTHON_VERSION.$PYTHON_VERSION_PATCH
 ./configure --enable-optimizations
 make -j4 && make altinstall
+update-alternatives --install /usr/bin/python python /usr/local/bin/python$PYTHON_VERSION 1
 update-alternatives --set python /usr/local/bin/python$PYTHON_VERSION
-
-update-alternatives --set python3 /usr/local/bin/python$PYTHON_VERSION
+# update-alternatives --install /usr/bin/python3 python3 /usr/local/bin/python$PYTHON_VERSION 1
+# update-alternatives --set python3 /usr/local/bin/python$PYTHON_VERSION
 #TODO handle this interaction with base python
 
 # PROJ=pyenv-installer
