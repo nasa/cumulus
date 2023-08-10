65ccae4b54cd6b8d5e5e3f373365b98e4b901f4d
Naga Nages <nnagavenkata@yahoo.com>
2023-08-10 16:37:05 -0400

    attempting to fix security group issue

--- bamboo/bootstrap-tf-deployment.sh
+++ bamboo/bootstrap-tf-deployment.sh
@@ -71,6 +71,7 @@ echo "terraform {
 ../terraform init \
   -input=false
 
+../terraform force-unlock c057d81a-f050-6271-12ce-4b3507357e29  \
 ../terraform import module.orca.module.orca.module.orca_lambdas.module.lambda_security_group.aws_security_group.vpc-postgres-ingress-all-egress nnaga-ci-tf-vpc-ingress-all-egress \
 
 # Deploy cumulus-tf via terraform
