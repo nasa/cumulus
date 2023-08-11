bb268bed08744ddb69ea720b56ae893591a06585
Naga Nages <nnagavenkata@yahoo.com>
2023-08-11 11:35:07 -0400

    resetting CI

--- bamboo/bootstrap-tf-deployment.sh
+++ bamboo/bootstrap-tf-deployment.sh
@@ -74,6 +74,10 @@ echo "terraform {
 #../terraform force-unlock -force c057d81a-f050-6271-12ce-4b3507357e29
 ../terraform import module.orca.module.orca.module.orca_lambdas.module.lambda_security_group.aws_security_group.vpc-postgres-ingress-all-egress nnaga-ci-tf-vpc-ingress-all-egress \
   -auto-approve \
+  -var "module.orca.module.orca.module.orca_lambdas.module.lambda_security_group.aws_security_group.vpc-postgres-ingress-all-egress"
+  -var "nnaga-ci-tf-vpc-ingress-all-egress"
+  -var "ADDR=module.orca.module.orca.module.orca_lambdas.module.lambda_security_group.aws_security_group.vpc-postgres-ingress-all-egress"
+  -var "ID=nnaga-ci-tf-vpc-ingress-all-egress"
   -var "cmr_username=$CMR_USERNAME" \
   -var "cmr_password=$CMR_PASSWORD" \
   -var "cmr_client_id=cumulus-core-$DEPLOYMENT" \
