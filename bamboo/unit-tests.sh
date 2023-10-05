078c715fb61b72420b7b2813970d4dea3a203aa0
ecarton <ethan.j.carton@nasa.gov>
2023-10-05 14:55:12 -0400

    actually call the merge part of coverage

--- bamboo/unit-tests.sh
+++ bamboo/unit-tests.sh
@@ -9,5 +9,5 @@ docker ps -a ## Show running containers for output logs
 
 docker exec -i ${container_id}-build_env-1 /bin/bash -c "cd $UNIT_TEST_BUILD_DIR && npm run db:local:reset"
 docker exec -i ${container_id}-build_env-1 /bin/bash -c "cd $UNIT_TEST_BUILD_DIR && npm run test:coverage"
-docker exec -i ${container_id}-build_env-1 /bin/bash -c "cd $UNIT_TEST_BUILD_DIR && npm run coverage -- --noRerun"
+docker exec -i ${container_id}-build_env-1 /bin/bash -c "cd $UNIT_TEST_BUILD_DIR && npm run coverage -- --merge --noRerun"
 docker exec -i ${container_id}-build_env-1 /bin/bash -c "cd $UNIT_TEST_BUILD_DIR && nyc report"
