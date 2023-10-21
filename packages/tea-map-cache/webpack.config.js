16e06e32644a4e910cf8de844615dc646d967a76
jennyhliu <jenny.h.liu@nasa.gov>
2023-10-20 22:42:33 -0400

    tea-map-cache webpack

---
+++ packages/tea-map-cache/webpack.config.js
@@ -0,0 +1,17 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'production',
+  entry: './dist/src/index.js',
+  output: {
+    libraryTarget: 'commonjs2',
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index.js'
+  },
+  externals: ['aws-sdk'],
+  target: 'node',
+  devtool: 'eval-cheap-module-source-map',
+  optimization: {
+    nodeEnv: false
+  }
+};
