9c07c02f7aa31800a7118c39e69becfdf103a214
Roger Kwarteng <kwartrosa@gmail.com>
2023-11-09 12:48:39 -0500

    update

--- packages/api/tests/endpoints/test-rules.js
+++ packages/api/tests/endpoints/test-rules.js
@@ -3021,7 +3021,7 @@ test.serial('Creating, deleting, and creating a rule with the same name succeeds
   );
   t.like(esRecord, translatedPgRecord);
 
- 
+  
   t.true(await t.context.rulePgModel.exists(t.context.testKnex1, { name: originalPgRecord.name }));
 
   const deleteResponse = await request(app)
