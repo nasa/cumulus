49d1eab284e8e26262cee80f872826f4cf42f6af
Naga Nages <nnagavenkata@yahoo.com>
2023-11-10 00:13:04 -0500

    attempt to fix rules/kinesis-consumer unit tests

--- packages/api/tests/endpoints/test-rules.js
+++ packages/api/tests/endpoints/test-rules.js
@@ -949,17 +949,17 @@ test.serial('PATCH nullifies expected fields for existing rule in all datastores
 test.serial('PATCH sets SNS rule to "disabled" and removes source mapping ARN', async (t) => {
   const snsStub = sinon.stub(awsServices, 'sns')
     .returns({
-      listSubscriptionsByTopic: () => ({
-        promise: () => Promise.resolve({
+      listSubscriptionsByTopic: () => (
+        Promise.resolve({
           Subscriptions: [{
             Endpoint: process.env.messageConsumer,
             SubscriptionArn: randomString(),
           }],
-        }),
-      }),
-      unsubscribe: () => ({
-        promise: () => Promise.resolve(),
-      }),
+        })
+      ),
+      unsubscribe: () => (
+        Promise.resolve()
+      ),
     });
   const lambdaStub = sinon.stub(awsServices, 'lambda')
     .returns({
