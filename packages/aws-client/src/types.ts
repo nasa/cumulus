049207565398770f55655fc259cef15fa1d4c272
Nate Pauzenga <npauzenga@gmail.com>
2023-11-13 14:11:28 -0500

    Update CloudWatchEvents to aws v3

--- packages/aws-client/src/types.ts
+++ packages/aws-client/src/types.ts
@@ -1,10 +1,11 @@
 import * as AWS from 'aws-sdk';
 
 import { APIGatewayClient } from '@aws-sdk/client-api-gateway';
+import { CloudWatchEvents } from '@aws-sdk/client-cloudwatch-events';
 import { DynamoDBStreamsClient } from '@aws-sdk/client-dynamodb-streams';
 import { DynamoDB, DynamoDBClient } from '@aws-sdk/client-dynamodb';
 import { S3 } from '@aws-sdk/client-s3';
 import { Lambda } from '@aws-sdk/client-lambda';
 
-export type AWSClientTypes = APIGatewayClient | DynamoDB
+export type AWSClientTypes = APIGatewayClient | CloudWatchEvents | DynamoDB
 | DynamoDBClient | DynamoDBStreamsClient | S3 | Lambda | AWS.Service | AWS.DynamoDB.DocumentClient;
