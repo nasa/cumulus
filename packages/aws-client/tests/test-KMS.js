a67f6c1f6ad5c3cb2f1537472162802a9be016d7
Naga Nages <nnagavenkata@yahoo.com>
2023-11-21 15:22:20 -0500

    fixing calls

--- packages/aws-client/tests/test-KMS.js
+++ packages/aws-client/tests/test-KMS.js
@@ -1,11 +1,12 @@
 'use strict';
 
 const test = require('ava');
+const { CreateKeyCommand, DescribeKeyCommand, DecryptCommand, EncryptCommand } = require('@aws-sdk/client-kms');
 const { kms } = require('../services');
 const KMS = require('../KMS');
 
 test.before(async (t) => {
-  const createKeyResponse = await kms().createKey({});
+  const createKeyResponse = await kms().send(new CreateKeyCommand({}));
   t.context.KeyId = createKeyResponse.KeyMetadata.KeyId;
 });
 
@@ -13,26 +14,23 @@ test('createKey() creates a key', async (t) => {
   const createKeyResponse = await KMS.createKey();
 
   await t.notThrowsAsync(
-    kms().describeKey({
-      KeyId: createKeyResponse.KeyMetadata.KeyId,
-    })
+    kms().send(new DescribeKeyCommand({ KeyId: createKeyResponse.KeyMetadata.KeyId }))
   );
 });
 
 test('encrypt() properly encrypts a value', async (t) => {
   const ciphertext = await KMS.encrypt(t.context.KeyId, 'asdf');
 
-  const plaintext = await kms().decrypt({
+  const plaintext = await kms().send(new DecryptCommand({
     CiphertextBlob: Buffer.from(ciphertext, 'base64'),
-  }).then(({ Plaintext }) => Plaintext.toString());
+  })).then(({ Plaintext }) => Plaintext.toString());
 
   t.is(plaintext, 'asdf');
 });
 
 test('decryptBase64String() properly decrypts a value', async (t) => {
   const { KeyId } = t.context;
-
-  const ciphertext = await kms().encrypt({ KeyId, Plaintext: 'asdf' })
+  const ciphertext = await kms().send(new EncryptCommand({ KeyId, Plaintext: 'asdf' }))
     .then(({ CiphertextBlob }) => CiphertextBlob.toString('base64'));
 
   const plaintext = await KMS.decryptBase64String(ciphertext);
