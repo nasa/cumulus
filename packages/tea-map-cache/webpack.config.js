d03803dbef452b662d1d66417663fd71d22cbc9f
Jonathan Kovarik <jonathan.kovarik@gmail.com>
2024-01-27 04:42:45 -0700

    Set webpack configuration to *not* chunk globally

--- packages/tea-map-cache/webpack.config.js
+++ packages/tea-map-cache/webpack.config.js
@@ -4,6 +4,8 @@ module.exports = {
   mode: 'production',
   entry: './dist/src/index.js',
   output: {
+    chunkFormat: false,
+    chunkFormat: false,
     libraryTarget: 'commonjs2',
     path: path.resolve(__dirname, 'dist'),
     filename: 'index.js'
