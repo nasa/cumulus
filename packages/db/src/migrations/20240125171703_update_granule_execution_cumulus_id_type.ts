0af181177185a2ff3dbaa0c90e7355849be06db4
jennyhliu <jenny.h.liu@nasa.gov>
2024-01-30 22:23:24 -0500

    update parent_cumulus_id bigint

--- packages/db/src/migrations/20240125171703_update_granule_execution_cumulus_id_type.ts
+++ packages/db/src/migrations/20240125171703_update_granule_execution_cumulus_id_type.ts
@@ -2,7 +2,7 @@ import { Knex } from 'knex';
 
 export const up = async (knex: Knex): Promise<void> => {
   await knex.raw('ALTER TABLE files ALTER COLUMN granule_cumulus_id TYPE BIGINT');
-  await knex.raw('ALTER TABLE executions ALTER COLUMN cumulus_id TYPE BIGINT');
+  await knex.raw('ALTER TABLE executions ALTER COLUMN cumulus_id TYPE BIGINT, ALTER COLUMN parent_cumulus_id TYPE BIGINT');
   await knex.raw('ALTER TABLE granules_executions ALTER COLUMN granule_cumulus_id TYPE BIGINT, ALTER COLUMN execution_cumulus_id TYPE BIGINT');
 };
 
