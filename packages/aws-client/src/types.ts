6d0cbe237ec0f6237448ae57987e778eacff1c54
kwartrosa <34353779+kwartrosa@users.noreply.github.com>
2024-02-12 14:06:22 -0500

    Update types.ts

--- packages/aws-client/src/types.ts
+++ packages/aws-client/src/types.ts
@@ -12,7 +12,6 @@ import { S3 } from '@aws-sdk/client-s3';
 import { ECS } from '@aws-sdk/client-ecs';
 import { SNS } from '@aws-sdk/client-sns';
 import { SQS } from '@aws-sdk/client-sqs';
-
 export type AWSClientTypes = 
     APIGatewayClient |
     DynamoDB |
