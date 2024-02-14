29ac6094663f9cf3692dade57dcd0aae4c24c6e2
Nate Pauzenga <npauzenga@gmail.com>
2024-02-14 09:42:46 -0500

    remove separate coverage execution

--- bamboo/unit-tests.sh
+++ bamboo/unit-tests.sh
@@ -9,4 +9,4 @@ docker ps -a ## Show running containers for output logs
 
 docker exec -i ${container_id}-build_env-1 /bin/bash -c "cd $UNIT_TEST_BUILD_DIR && npm run db:local:reset"
 docker exec -i ${container_id}-build_env-1 /bin/bash -c "cd $UNIT_TEST_BUILD_DIR && npm run test"
-docker exec -i ${container_id}-build_env-1 /bin/bash -c "cd $UNIT_TEST_BUILD_DIR && npm run coverage -- --noRerun"
+# docker exec -i ${container_id}-build_env-1 /bin/bash -c "cd $UNIT_TEST_BUILD_DIR && npm run coverage -- --noRerun"
