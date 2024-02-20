09a9862939227a6b870f2858aa26335532772353
Nate Pauzenga <npauzenga@gmail.com>
2024-02-20 12:27:46 -0500

    add coverage back to unit test runs

--- bamboo/unit-tests.sh
+++ bamboo/unit-tests.sh
@@ -8,6 +8,6 @@ set -ex
 docker ps -a ## Show running containers for output logs
 
 docker exec -i ${container_id}-build_env-1 /bin/bash -c "cd $UNIT_TEST_BUILD_DIR && npm run db:local:reset"
-docker exec -i ${container_id}-build_env-1 /bin/bash -c "cd $UNIT_TEST_BUILD_DIR && npm run test"
+docker exec -i ${container_id}-build_env-1 /bin/bash -c "cd $UNIT_TEST_BUILD_DIR && npm run test:coverage"
 docker cp ${container_id}-build_env-1:${UNIT_TEST_BUILD_DIR}/testoutput.txt /tmp/testoutput.txt
 docker exec -i ${container_id}-build_env-1 /bin/bash -c "cd $UNIT_TEST_BUILD_DIR && npm run coverage -- --noRerun"
