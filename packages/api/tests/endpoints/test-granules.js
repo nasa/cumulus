1c8debdfe26a9403ab6d3ca5df914f54c5b3f99e
Nate Pauzenga <npauzenga@gmail.com>
2024-02-20 12:26:44 -0500

    cleanup

--- packages/api/tests/endpoints/test-granules.js
+++ packages/api/tests/endpoints/test-granules.js
@@ -3867,11 +3867,8 @@ test.serial('default paginates correctly with search_after', async (t) => {
   t.is(newResults.length, 1);
   t.is(newMeta.page, 2);
   t.truthy(newMeta.searchContext);
-  console.log(`new results::: ${JSON.stringify(newResults)}`);
-  console.log(`granuleIds:::: ${JSON.stringify(granuleIds)}`);
-  console.log(`default paginates granuleIds: ${JSON.stringify(granuleIds)}, results: ${results[0].granuleId}, ${newResults[0].granuleId}`);
   t.true(granuleIds.includes(results[0].granuleId));
-  t.true(granuleIds.includes(newResults[0].granuleId)); // THIS fails in CI, value is not true
+  t.true(granuleIds.includes(newResults[0].granuleId));
   t.not(results[0].granuleId, newResults[0].granuleId);
   t.not(meta.searchContext === newMeta.searchContext);
 });
