c38e4533adc82fc4f402e6ce76ad599957615c40
ecarton <ethan.j.carton@nasa.gov>
2024-02-21 17:17:00 -0500

    declare type guard in typescript

--- packages/common/src/lambda.ts
+++ packages/common/src/lambda.ts
@@ -1,12 +1,14 @@
 //@ts-check
+import { EventBridgeEvent } from 'aws-lambda';
+export type StepFunctionEventBridgeEvent = EventBridgeEvent<'Step Functions Execution Status Change', { [key: string]: string }>;
 
 /**
  * Bare check for EventBridge shape
  *
  * @param {{ [key: string]: any }} event
- * @returns {message is EventBridgeEvent}
+ * @returns {event is EventBridgeEvent}
  */
-export const isEventBridgeEvent = (event: Object): boolean => (
+export const isEventBridgeEvent = (event: Object): event is StepFunctionEventBridgeEvent => (
   event instanceof Object
   && 'detail' in event
 );
