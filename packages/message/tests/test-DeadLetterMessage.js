1560321dc56695d3e9003b00f75c9b1cf0ffc3c3
ecarton <ethan.j.carton@nasa.gov>
2024-03-05 20:50:23 -0500

    fixing sqs vs dlq overlap in testing

--- packages/message/tests/test-DeadLetterMessage.js
+++ packages/message/tests/test-DeadLetterMessage.js
@@ -116,13 +116,7 @@ test('unwrapDeadLetterCumulusMessage returns an non-unwrappable message', async 
 test('isDLQRecordLike correctly filters for DLQ record shaped objects', (t) => {
   t.false(isDLQRecordLike('aaa')); // must be an object
   t.false(isDLQRecordLike({ a: 'b' })); // object must contain a body
-  t.true(isDLQRecordLike({ body: 'a' }));
-  t.true(isDLQRecordLike({ Body: 'a' })); // must accept body or Body
-
-  /* isDLQRecordLike first checks if object isSQSRecordLike,
-  * the above are direct replicas of those tests for surety
-  */
-
   t.false(isDLQRecordLike({ body: '{a: "b"}' })); // object must contain an error attribute
   t.true(isDLQRecordLike({ body: '{a: "b"}', error: 'a' }));
+  t.true(isDLQRecordLike({ Body: '{a: "b"}', error: 'a' }));
 });
