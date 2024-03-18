050ac8312b24dd5670de781e7683354b4eef4e4f
ecarton <ethan.j.carton@nasa.gov>
2024-03-18 09:53:49 -0400

    linting and docstrings

--- tasks/update-dla-format/main.ts
+++ tasks/update-dla-format/main.ts
@@ -1,54 +1,65 @@
-import { zip } from 'lodash';
+import zip from 'lodash/zip';
 import minimist from 'minimist';
 import path from 'path';
 import {
   getJsonS3Object,
   putJsonS3Object,
   listS3Objects,
+  s3ObjectExists,
 } from '@cumulus/aws-client/S3';
-import {
-  PutObjectCommandOutput,
-} from '@aws-sdk/client-s3';
 import { hoistCumulusMessageDetails } from '@cumulus/message/DeadLetterMessage';
-const getEnvironmentVariable = (variable: string): string => {
+
+/**
+ * Check for an Env variable and throw if it's not present
+ */
+export const getEnvironmentVariable = (variable: string): string => {
   if (!process.env[variable]) {
     throw new Error(`Environment variable "${variable}" is not set.`);
   }
   return process.env[variable] as string;
 };
 
-const verifyRequiredEnvironmentVariables = () => {
-  [
-    'DEPLOYMENT',
-    'INTERNAL_BUCKET',
-  ].forEach((x) => {
-    getEnvironmentVariable(x);
-  });
-};
-
-export const manipulateTrailingSlash = (str: string, shouldHave: boolean): string => {
-  if (str === '') {
-    return str;
+/**
+ * Ensure that a string has or does not have a trailing slash as appropriate
+ * the strings '' and '/' are special cases that should return '' always
+ * because we're handling 'directories' in S3, expect a user to give '/' and mean 'the whole bucket'
+ * @param S3Path a string meant to represent a path within an S3 bucket
+ * @param shouldHave should it end with a '/'
+ * @returns massaged S3Path
+ */
+export const manipulateTrailingSlash = (S3Path: string, shouldHave: boolean): string => {
+  if (S3Path === '' || S3Path === '/') {
+    return '';
   }
-  const has = str.endsWith('/');
+  const has = S3Path.endsWith('/');
   if (has && shouldHave) {
-    return str;
+    return S3Path;
   }
   if (!has && !shouldHave) {
-    return str;
+    return S3Path;
   }
   if (!has && shouldHave) {
-    return `${str}/`;
+    return `${S3Path}/`;
   }
   if (has && !shouldHave) {
-    let out = str.slice(0, -1);
+    let out = S3Path.slice(0, -1);
     while (out.endsWith('/')) {
       out = out.slice(0, -1);
     }
     return out;
   }
-  throw new Error('how did you get here? this shouldnt be possible');
+  /* this is just to satisfy typescript, it shouldn't be possible to get here */
+  return S3Path;
 };
+
+/**
+ * Parse the S3 'directory' referenced by a prefix.
+ * @param prefix
+ * @returns
+ *  - '' if prefix is '' or '/'
+ *  - parent directory to prefix if prefix doesn't refer to a directory itself
+ *  - prefix in 'directory' form (trailing slash) if it refers to a directory
+ */
 export const parseS3Directory = async (prefix: string): Promise<string> => {
   let prefixForm = prefix;
   /* expect user might point us at '/' expecting that to mean 'everything' */
@@ -65,10 +76,10 @@ export const parseS3Directory = async (prefix: string): Promise<string> => {
     const testObjects = await listS3Objects(bucket, prefixForm);
     keys = testObjects.map((obj) => obj.Key);
     if (keys.length === 0) {
-      throw new Error(`cannot find contents of bucket ${bucket} under prefix '${prefixForm}'`);
+      throw new Error(`cannot find contents of bucket ${bucket} under prefix "${prefixForm}"`);
     }
   } catch (error) {
-    throw new Error(`cannot find contents of bucket ${bucket} under prefix '${prefixForm}'`);
+    throw new Error(`cannot find contents of bucket ${bucket} under prefix "${prefixForm}"`);
   }
 
   keys.forEach((key) => {
@@ -88,24 +99,68 @@ export const parseS3Directory = async (prefix: string): Promise<string> => {
   if (directoryIsValid) {
     return directoryForm;
   }
-  
-  throw new Error(`cannot find contents of bucket ${bucket} under prefix ${prefix}`);
+  throw new Error(`Couldn't make sense of contents of bucket ${bucket} under prefix "${prefixForm}"`);
 };
 
+/**
+ * handle given targetPath, either parsing standard from prefix if targetPath is undefined
+ * or massaging targetPath into a 'Directory' if a
+ * @param targetPath given target to parse or undefined if none given
+ * @param prefix only relevant if targetPath is unspecified
+ * @returns Promise<string>
+ */
+export const parseTargetPath = async (
+  targetPath: string | undefined,
+  prefix: string
+): Promise<string> => {
+  if (targetPath !== undefined) {
+    return manipulateTrailingSlash(targetPath, true);
+  }
+  const sourceDir = await parseS3Directory(prefix);
+  const defaultTarget = `${manipulateTrailingSlash(sourceDir, false)}_updated_dla/`;
+  console.log(
+    `no targetPath given, defaulting targetPath to ${defaultTarget}`
+  );
+  return defaultTarget;
+};
+
+/**
+ * pull S3 Object from sourcePath, update it to new DLA structure and push it to targetPath
+ * noop if skip is true and an object already exists at targetPath
+ * @param bucket
+ * @param sourcePath
+ * @param targetPath
+ * @param skip skip if targetPath already exists
+ * @returns whether the logic was actually run (not skipped)
+ */
 export const updateDLAFile = async (
   bucket: string,
   sourcePath: string,
-  targetPath: string
-): Promise<PutObjectCommandOutput> => {
+  targetPath: string,
+  skip: boolean = false
+): Promise<boolean> => {
+  if (skip && await s3ObjectExists({ Bucket: bucket, Key: targetPath })) {
+    return false;
+  }
   const dlaObject = await getJsonS3Object(bucket, sourcePath);
   const hoisted = await hoistCumulusMessageDetails(dlaObject);
-  return await putJsonS3Object(bucket, targetPath, hoisted);
+  await putJsonS3Object(bucket, targetPath, hoisted);
+  return true;
 };
 
+/**
+ * update a batch of DLA files under prefix and push them to the targetDirectory
+ * skip files that appear to already have been processed if skip is set to true
+ * @param bucket
+ * @param targetDirectory
+ * @param prefix
+ * @param skip skip files that are already present at the target directory, default false
+ */
 export const updateDLABatch = async (
   bucket: string,
   targetDirectory: string,
-  prefix: string
+  prefix: string,
+  skip: boolean = false
 ) => {
   const sourceDir = await parseS3Directory(prefix);
   const subObjects = await listS3Objects(bucket, prefix);
@@ -121,38 +176,20 @@ export const updateDLABatch = async (
   );
 
   const zipped: Array<[string, string]> = zip(validKeys, targetPaths) as Array<[string, string]>;
-  await Promise.all(zipped.map((pathPair) => updateDLAFile(bucket, pathPair[0], pathPair[1])));
+  return await Promise.all(
+    zipped.map((pathPair) => updateDLAFile(
+      bucket, pathPair[0], pathPair[1], skip
+    ))
+  );
 };
 
-const parseTargetPath = async (
+interface UpdateDLAArgs {
+  prefix: string,
   targetPath: string,
-  prefix: string
-): Promise<string> => {
-  const sourceDir = await parseS3Directory(prefix);
-  if (!targetPath) {
-    const defaultTarget = `${manipulateTrailingSlash(sourceDir, false)}_updated_dla/`;
-    console.log(
-      `no targetPath given, defaulting targetPath to ${defaultTarget}`
-    );
-    return defaultTarget;
-  }
-  const targetDir = manipulateTrailingSlash(targetPath, true);
-  if (targetDir === sourceDir) {
-    console.log(
-      'writing output to input bucket'
-    );
-  }
-  return targetDir;
-};
+  skip: boolean,
+}
 
-const main = async () => {
-  verifyRequiredEnvironmentVariables();
-
-  const internalBucket = getEnvironmentVariable('INTERNAL_BUCKET');
-  const deployment = getEnvironmentVariable('DEPLOYMENT');
-  if (!internalBucket) {
-    throw new Error('a');
-  }
+export const processArgs = async (): Promise<UpdateDLAArgs> => {
   const args = minimist(
     process.argv,
     {
@@ -163,17 +200,42 @@ const main = async () => {
         k: 'prefix',
         path: 'prefix',
         target: 'targetPath',
+        'skip-existing': 'skip',
       },
       default: {
         prefix: undefined,
         targetPath: undefined,
+        skip: false,
       },
     }
   );
-  const prefix = args.prefix || `${deployment}/dead-letter-archive/sqs/`;
-  const targetDir = await parseTargetPath(args.targetPath, args.prefix);
+  const prefix = args.prefix || `${getEnvironmentVariable('DEPLOYMENT')}/dead-letter-archive/sqs/`;
+  return {
+    prefix,
+    skip: args.skip,
+    targetPath: await parseTargetPath(args.targetPath, prefix),
+  };
+};
 
-  updateDLABatch(internalBucket, targetDir, prefix);
+/**
+ * Main function to update dla structure
+ * expects environment variable INTERNAL_BUCKET
+ * expects environment variable DEPLOYMENT *if* prefix is not specified (for parsing the default)
+ * parses args to get prefix, targetPath and skip
+ * massages args for useability
+ * pulls all files from the INTERNAL_BUCKET beneath the given prefix
+ * massages them to the updated DLA format
+ * pushes them to the targetPath with a similar directory structure
+ */
+const main = async () => {
+  const internalBucket = getEnvironmentVariable('INTERNAL_BUCKET');
+  const {
+    targetPath,
+    prefix,
+    skip,
+  } = await processArgs();
+
+  await updateDLABatch(internalBucket, targetPath, prefix, skip);
 };
 
 if (require.main === module) {
