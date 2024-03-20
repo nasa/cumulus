0d7a6e58ba67d4264bd4d53f8e86417019bd06ee
ecarton <ethan.j.carton@nasa.gov>
2024-03-20 09:52:52 -0400

    trying to track down difference

--- bamboo/unit-tests.sh
+++ bamboo/unit-tests.sh
@@ -6,7 +6,7 @@ set -ex
 . ./bamboo/abort-if-skip-unit-tests.sh
 
 docker ps -a ## Show running containers for output logs
-docker exec -i ${container_id}-build_env-1 /bin/bash -c "cd $UNIT_TEST_BUILD_DIR && npm run bootstrap"
+docker exec -i ${container_id}-build_env-1 /bin/bash -c "cd $UNIT_TEST_BUILD_DIR && npm run ci:bootstrap-no-scripts-quiet"
 docker exec -i ${container_id}-build_env-1 /bin/bash -c "cd $UNIT_TEST_BUILD_DIR && npm run db:local:reset"
 docker exec -i ${container_id}-build_env-1 /bin/bash -c "cd $UNIT_TEST_BUILD_DIR && npm run test:coverage"
 docker exec -i ${container_id}-build_env-1 /bin/bash -c "cd $UNIT_TEST_BUILD_DIR && npm run coverage -- --noRerun"
