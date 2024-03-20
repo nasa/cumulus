81693f3bc491e7b6de72e881424a23a28dc988f0
ecarton <ethan.j.carton@nasa.gov>
2024-03-20 13:53:32 -0400

    use hoist as declared in DeadLetterMessage.ts

--- packages/api/lambdas/write-db-dlq-records-to-s3.js
+++ packages/api/lambdas/write-db-dlq-records-to-s3.js
@@ -5,135 +5,16 @@
 const get = require('lodash/get');
 const uuidv4 = require('uuid/v4');
 
-const log = require('@cumulus/common/log');
-const { isEventBridgeEvent } = require('@cumulus/aws-client/Lambda');
 const { s3PutObject } = require('@cumulus/aws-client/S3');
-const { parseSQSMessageBody, isSQSRecordLike } = require('@cumulus/aws-client/SQS');
-const { unwrapDeadLetterCumulusMessage, isDLQRecordLike } = require('@cumulus/message/DeadLetterMessage');
-const { getCumulusMessageFromExecutionEvent } = require('@cumulus/message/StepFunctions');
-const { constructCollectionId } = require('@cumulus/message/Collections');
-const { isMessageWithProvider, getMessageProviderId } = require('@cumulus/message/Providers');
+const { isSQSRecordLike } = require('@cumulus/aws-client/SQS');
+const {
+  unwrapDeadLetterCumulusMessage,
+  hoistCumulusMessageDetails,
+} = require('@cumulus/message/DeadLetterMessage');
 /**
  *
- * @typedef {import('@cumulus/types/message').CumulusMessage} CumulusMessage
- * @typedef {import('@cumulus/types').MessageGranule} MessageGranule
- * @typedef {import('@cumulus/types/message').Meta} Meta
- * @typedef {{granules: Array<MessageGranule>}} PayloadWithGranules
  * @typedef {import('aws-lambda').SQSRecord} SQSRecord
- * @typedef {import('@cumulus/types/api/dead_letters').DLQRecord} DLQRecord
- * @typedef {import('@cumulus/types/api/dead_letters').DLARecord} DLARecord
- * @typedef {import('aws-lambda').EventBridgeEvent} EventBridgeEvent
  */
-
-/**
- * @param {unknown} payload
- * @returns {payload is PayloadWithGranules}
- */
-function payloadHasGranules(payload) {
-  return (
-    payload instanceof Object
-    && 'granules' in payload
-    && Array.isArray(payload.granules)
-  );
-}
-/**
- * @param {CumulusMessage} message
- * @returns {string | null}
- */
-function extractCollectionId(message) {
-  const collectionName = message?.meta?.collection?.name || null;
-  const collectionVersion = message?.meta?.collection?.version || null;
-  if (collectionName && collectionVersion) {
-    return constructCollectionId(collectionName, collectionVersion);
-  }
-  return null;
-}
-/**
- * @param {CumulusMessage} message
- * @returns {Array<string | null> | null}
- */
-function extractGranules(message) {
-  if (payloadHasGranules(message.payload)) {
-    return message.payload.granules.map((granule) => granule?.granuleId || null);
-  }
-  return null;
-}
-
-/**
- * Reformat dlqRecord to add key attributes at top level.
- *
- * @param {SQSRecord} dlqRecord - event bridge event as defined in aws-lambda
- * @returns {Promise<DLARecord>} - message packaged with
- * metadata or null where metadata not found
- * {
- *   error: <errorString | null>
- *   time: <timestamp(utc) | null>
- *   status: <status | null>
- *   providerId: <id | null>
- *   collectionId: <collectionName___collectionId | null>
- *   granules: <[granuleIds, ...] | []>
- *   executionArn: <executionArn | null>
- *   stateMachineArn: <stateMachineArn | null>
- *   ...originalAttributes
- * }
- */
-async function hoistCumulusMessageDetails(dlqRecord) {
-  let error = null;
-  let executionArn = null;
-  let stateMachineArn = null;
-  let status = null;
-  let time = null;
-  let collectionId = null;
-  let granules = null;
-  let providerId = null;
-
-  /* @type {any} */
-  let messageBody;
-  messageBody = dlqRecord;
-  /* de-nest sqs records of unknown depth */
-  while (isSQSRecordLike(messageBody)) {
-    /* capture outermost recorded error */
-    if (isDLQRecordLike(messageBody) && !error) {
-      error = messageBody.error || null;
-    }
-    messageBody = parseSQSMessageBody(messageBody);
-  }
-
-  if (isEventBridgeEvent(messageBody)) {
-    executionArn = messageBody?.detail?.executionArn || null;
-    stateMachineArn = messageBody?.detail?.stateMachineArn || null;
-    status = messageBody?.detail?.status || null;
-    time = messageBody?.time || null;
-    let cumulusMessage;
-    try {
-      cumulusMessage = await getCumulusMessageFromExecutionEvent(messageBody);
-    } catch (error_) {
-      cumulusMessage = undefined;
-      log.error(`could not parse details from DLQ message body due to ${error_}`);
-    }
-    if (cumulusMessage) {
-      collectionId = extractCollectionId(cumulusMessage);
-      granules = extractGranules(cumulusMessage);
-      if (isMessageWithProvider(cumulusMessage)) {
-        providerId = getMessageProviderId(cumulusMessage) || null;
-      }
-    }
-  } else {
-    log.error('could not parse details from DLQ message body, expected EventBridgeEvent');
-  }
-
-  return {
-    ...dlqRecord,
-    collectionId,
-    providerId,
-    granules,
-    executionArn,
-    stateMachineArn,
-    status,
-    time,
-    error,
-  };
-}
 
 /**
  * Lambda handler for saving DLQ reports to DLA in s3
