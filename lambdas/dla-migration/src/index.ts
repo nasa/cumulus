f6fed6517495abc14da6283347ed0c68d168acdf
ecarton <ethan.j.carton@nasa.gov>
2024-04-01 13:14:54 -0400

    docs and cleanup according to PR review

--- lambdas/dla-migration/src/index.ts
+++ lambdas/dla-migration/src/index.ts
@@ -24,15 +24,15 @@ const logger = new Logger({ sender: '@cumulus/dla-migration-lambda' });
 /**
  * Manipulate target filepath to put file is YYYY-MM-DD sub-folder
  *
- * @param targetPath
+ * @param filePath - filePath to be massaged with date identifier
  * @param message - DLARecord message from which timestamp can be extracted
  * @returns targetPath with lowest 'folder' added in form YYYY-MM-DD
  */
-export const addDateIdentifierToPath = (targetPath: string, message: DLARecord): string => (
+const addDateIdentifierToPath = (filePath: string, message: DLARecord): string => (
   path.join(
-    path.dirname(targetPath),
+    path.dirname(filePath),
     extractDateString(message),
-    path.basename(targetPath)
+    path.basename(filePath)
   )
 );
 
@@ -40,50 +40,47 @@ export const addDateIdentifierToPath = (targetPath: string, message: DLARecord):
  * pull S3 Object from sourcePath, update it to new DLA structure and push it to targetPath
  * noop if skip is true and an object already exists at targetPath
  *
- * @param bucket
- * @param sourcePath
- * @param targetPath
- * @param skip - skip if targetPath already exists
- * @returns whether the logic was actually run (not skipped)
+ * @param bucket - bucket to operate within
+ * @param sourcePath - path to source object
+ * @returns whether the update succeeded or not
  */
 export const updateDLAFile = async (
   bucket: string,
   sourcePath: string
 ): Promise<boolean> => {
-  logger.info(`About to process ${sourcePath}`);
-  const dlaObject = await getJsonS3Object(bucket, sourcePath);
-  const hoisted = await hoistCumulusMessageDetails(dlaObject);
-  const massagedTargetPath = addDateIdentifierToPath(sourcePath, hoisted);
+  try {
+    logger.info(`About to process ${sourcePath}`);
+    const dlaObject = await getJsonS3Object(bucket, sourcePath);
+    const hoisted = await hoistCumulusMessageDetails(dlaObject);
+    const massagedTargetPath = addDateIdentifierToPath(sourcePath, hoisted);
 
-  await putJsonS3Object(bucket, massagedTargetPath, hoisted);
-  logger.info(`Migrated file from bucket ${bucket}/${sourcePath} to ${massagedTargetPath}`);
+    await putJsonS3Object(bucket, massagedTargetPath, hoisted);
+    logger.info(`Migrated file from bucket ${bucket}/${sourcePath} to ${massagedTargetPath}`);
 
-  await deleteS3Object(bucket, sourcePath);
-  logger.info(`Deleted file ${bucket}/${sourcePath}`);
-  return true;
+    await deleteS3Object(bucket, sourcePath);
+    logger.info(`Deleted file ${bucket}/${sourcePath}`);
+    return true;
+  } catch (error) {
+    logger.error(`failed to process ${sourcePath} due to error: ${error}`);
+    return false;
+  }
 };
 
 /**
  * update a batch of DLA files under prefix and push them to the targetDirectory
  * skip files that appear to already have been processed if skip is set to true
  *
- * @param bucket
- * @param targetDirectory
- * @param prefix
+ * @param bucket - bucket to operate within
+ * @param directory - s3 'directory' within which to process DLA files
  * @param skip - skip files that are already present at the target directory, default false
  */
 export const updateDLABatch = async (
   bucket: string,
-  sourceDirectory: string
+  directory: string
 ): Promise<Array<boolean>> => {
   const out = [];
-  let sourceDir;
-  if (sourceDirectory.endsWith('/')) {
-    sourceDir = sourceDirectory;
-  } else {
-    sourceDir = `${sourceDirectory}/`;
-  }
-  const lastIndexOfDlaPathSeparator = sourceDir.lastIndexOf('/');
+  const sourceDir = directory.endsWith('/') ? directory : `${directory}/`;
+  const lastIndexOfDlaPathSeparator = sourceDir.length - 1;
   for await (
     const objectBatch of listS3ObjectsV2Batch({ Bucket: bucket, Prefix: sourceDir })
   ) {
@@ -108,7 +105,12 @@ export interface HandlerEvent {
 export interface HandlerOutput {
   migrated: number
 }
-
+/**
+ * lambda event handler for updating dla to new format
+ *
+ * @param event - can declare dlaPath to override default `${stackName}/dead-letter-archive/sqs/`
+ * @returns number of successful files updated
+ */
 export const handler = async (event: HandlerEvent): Promise<HandlerOutput> => {
   if (!process.env.system_bucket) throw new Error('System bucket env var is required.');
   if (!process.env.stackName) throw new Error('Could not determine archive path as stackName env var is undefined.');
