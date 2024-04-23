3617c57e728bb496a469d90c02c98110dbc8ce16
etcart <amberhosen@gmail.com>
2024-04-23 11:45:07 -0400

    cleanup comments

--- packages/api/lambdas/process-s3-dead-letter-archive.js
+++ packages/api/lambdas/process-s3-dead-letter-archive.js
@@ -30,13 +30,11 @@ const generateNewArchiveKeyForFailedMessage = (
   stackName = process.env.stackName,
   failedMessage = {}
 ) => {
-  // if(!oldKey.includes('dead-letter-archive/sqs/')) {
-  //   return getDLAKey(stackName, failedMessage).replace('dead-letter-archive/sqs/', 'dead-letter-archive/failed-sqs/');
-  // }
-  if (oldKey.includes('dead-letter-archive/sqs/')) {
-    return oldKey.replace('dead-letter-archive/sqs/', 'dead-letter-archive/failed-sqs/');
+  if (!oldKey.includes('dead-letter-archive/sqs/')) {
+    //edge case if oldKey does not conform to expected pattern
+    return getDLAKey(stackName, failedMessage).replace('dead-letter-archive/sqs/', 'dead-letter-archive/failed-sqs/');
   }
-  // return getDLAKey(stackName, failedMessage).replace('dead-letter-archive/sqs/', 'dead-letter-archive/failed-sqs/');
+  return oldKey.replace('dead-letter-archive/sqs/', 'dead-letter-archive/failed-sqs/');
 };
 
 /**
