5daa9936b1d5e7b0cc5dfa349d50fcfd2f25174a
etcart <amberhosen@gmail.com>
2024-04-24 14:33:50 -0400

    does this fix my error?

--- example/spec/parallel/dbRecords/DeadLetterArchiveProcessingSpec.js
+++ example/spec/parallel/dbRecords/DeadLetterArchiveProcessingSpec.js
@@ -288,7 +288,7 @@ describe('A dead letter record archive processing operation', () => {
       // Unsuccessfully processed dead letters should be deleted from old location
       expect(await s3ObjectExists({ Bucket: systemBucket, Key: failingMessageKey })).toBeFalse();
 
-      newArchiveKey = generateNewArchiveKeyForFailedMessage(failingMessageKey, process.env.stackName, failingMessage);
+      newArchiveKey = generateNewArchiveKeyForFailedMessage(failingMessageKey, stackName, failingMessage);
       expect(await s3ObjectExists({ Bucket: systemBucket, Key: newArchiveKey })).toBeTrue();
       // Check that the JSON object is not empty and contains at least one key
       expect(Object.keys(await getJsonS3Object(systemBucket, newArchiveKey)).length).toBeGreaterThan(0);
