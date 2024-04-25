5aa92f65399dc8b8b74f291ddbd24ed19956fc41
Jonathan Kovarik <jonathan.kovarik@gmail.com>
2024-04-25 15:28:33 -0600

    Add docstring to importGot

--- packages/common/importGot.js
+++ packages/common/importGot.js
@@ -1,3 +1,17 @@
+/**
+ * Asynchronously imports the 'got' module.
+ *
+ * This function uses dynamic imports to import the ESM-only 'got' module at runtime.
+ * This should be used for importing the non-compat module in to CommonJS TS/webpacked
+ * modules.
+ *
+ * @returns {Promise<Function>} A promise that resolves to the 'got' function.
+ * @throws {Error} If an error occurs while importing the module.
+ */
+const importGot = async () => {
+  const { default: got } = await import('got');
+  return got;
+}
 const importGot = async () => {
   const { default: got } = await import('got');
   return got;
