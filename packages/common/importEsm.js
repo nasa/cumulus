d023e8ae536bd5e3a8f375ad93e06152d4b093ee
Jonathan Kovarik <kovarik@nsidc.org>
2024-05-08 09:02:57 -0600

    Update importEsm.js
    Remove inadvertent comment update

--- packages/common/importEsm.js
+++ packages/common/importEsm.js
@@ -5,10 +5,6 @@
  * This should be used for importing the non-compat module in to CommonJS TS/webpacked
  * modules.
  *
- * The method currently memoizes the import as under certain load conditions
- * this import results in a segfault.   See https://github.com/nodejs/node/issues/35889 and the entire
- * tree of related isseus for more detials.
- *
  * @returns {Promise<Function>} A promise that resolves to the 'got' function.
  * @throws {Error} If an error occurs while importing the module.
  */
