c1393a3fa6a6e79a9fc4310b21e963fe9eff56dc
etcart <amberhosen@gmail.com>
2024-05-14 12:53:43 -0400

    fix copypasta

--- bamboo/unit-tests.sh
+++ bamboo/unit-tests.sh
@@ -9,7 +9,7 @@ docker ps -a ## Show running containers for output logs
 
 docker exec -i ${container_id}-build_env-1 /bin/bash -c "cd $UNIT_TEST_BUILD_DIR && npm run db:local:reset"
 docker exec -i ${container_id}-build_env-1 /bin/bash -c "cd $UNIT_TEST_BUILD_DIR && npm run test:ci"
-if [ -n "$(ls -A your/dir 2>/dev/null)" ]
+if [ -n "$(ls -A ./unit-logs/@cumulus 2>/dev/null)" ]
 then 
 docker exec -i ${container_id}-build_env-1 /bin/bash -c "cd $UNIT_TEST_BUILD_DIR && aws s3 sync unit-logs/@cumulus/ s3://test-error-outputs/$(git rev-parse --abbrev-ref HEAD)/$(date +%DT%H.%M.%S)/"
 fi
