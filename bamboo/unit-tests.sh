22cc243905feceec7f6b2dfcd0f7c636563cd87f
etcart <amberhosen@gmail.com>
2024-05-16 16:24:14 -0400

    pass in aws credentials from system

--- bamboo/unit-tests.sh
+++ bamboo/unit-tests.sh
@@ -8,4 +8,8 @@ set -ex
 docker ps -a ## Show running containers for output logs
 
 docker exec -i ${container_id}-build_env-1 /bin/bash -c "cd $UNIT_TEST_BUILD_DIR && npm run db:local:reset"
-docker exec -i ${container_id}-build_env-1 /bin/bash -c "cd $UNIT_TEST_BUILD_DIR && ./scripts/run_ci_unit_coverage.sh"
+docker exec -i ${container_id}-build_env-1 \
+  -e AWS_ACCESS_KEY_ID=$(aws configure get aws_access_key_id)\
+  -e AWS_SECRET_ACCESS_KEY=$(aws configure get aws_secret_access_key)\
+  -e AWS_REGION=$(aws configure get region || echo $AWS_DEFAULT_REGION)\
+  /bin/bash -c "cd $UNIT_TEST_BUILD_DIR && ./scripts/run_ci_unit_coverage.sh"
