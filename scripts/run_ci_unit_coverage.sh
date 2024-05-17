45f9e3055df2e012c51a03c0993e9537bef60ae5
etcart <amberhosen@gmail.com>
2024-05-17 09:41:29 -0400

    refactor to get aws credentials into imgage

--- scripts/run_ci_unit_coverage.sh
+++ scripts/run_ci_unit_coverage.sh
@@ -9,6 +9,10 @@ npm run test:ci > ./test_output.txt 2>&1
 RESULT=$?
 
 kill -9 $TAIL_PID
+if [ -n "$(ls -A ./unit-logs/@cumulus 2>/dev/null)" ]
+then 
+    aws s3 sync unit-logs/@cumulus/ s3://unit-test-error-logs/$(git rev-parse --abbrev-ref HEAD)/$(date +%Y-%m-%dT%H.%M.%S)/
+fi
 if [ $RESULT ]
 then
     printf '\n\n\n*****TEST FAILURES:\n'
