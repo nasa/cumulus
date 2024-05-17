9a131ea4cfef3ad4b12c3aa8a342577a063d685b
etcart <amberhosen@gmail.com>
2024-05-17 09:44:14 -0400

    remove the aws push from unit-tests

--- bamboo/unit-tests.sh
+++ bamboo/unit-tests.sh
@@ -10,9 +10,5 @@ docker ps -a ## Show running containers for output logs
 docker exec -i ${container_id}-build_env-1 /bin/bash -c "cd $UNIT_TEST_BUILD_DIR && npm run db:local:reset"
 docker exec -i ${container_id}-build_env-1 \
   /bin/bash -c "cd $UNIT_TEST_BUILD_DIR && \
-  ./scripts/run_ci_unit_coverage.sh"
+  ./scripts/run_ci_unit_coverage.sh" \
 
-if [ -n "$(ls -A ./unit-logs/@cumulus 2>/dev/null)" ]
-then 
-    aws s3 sync unit-logs/@cumulus/ s3://unit-test-error-logs/$(git rev-parse --abbrev-ref HEAD)/$(date +%Y-%m-%dT%H.%M.%S)/
-fi
