da463e0b979d81e5505311ddf9c9b851d76e41c3
etcart <amberhosen@gmail.com>
2024-05-17 10:58:23 -0400

    get aws tow ork

--- bamboo/unit-tests.sh
+++ bamboo/unit-tests.sh
@@ -12,3 +12,7 @@ docker exec -i ${container_id}-build_env-1 \
   /bin/bash -c "cd $UNIT_TEST_BUILD_DIR && \
   ./scripts/run_ci_unit_coverage.sh" \
 
+if [ -n "$(ls -A ./unit-logs/@cumulus 2>/dev/null)" ]
+then 
+    aws s3 sync unit-logs/@cumulus/ s3://unit-test-error-logs/$(git rev-parse --abbrev-ref HEAD)/$(date +%Y-%m-%dT%H.%M.%S)/
+fi
