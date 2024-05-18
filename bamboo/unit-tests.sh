fd95a2107f26b4026415738d7cfd9efc90155c77
etcart <amberhosen@gmail.com>
2024-05-17 22:02:32 -0400

    no typpo backslack

--- bamboo/unit-tests.sh
+++ bamboo/unit-tests.sh
@@ -10,7 +10,7 @@ docker ps -a ## Show running containers for output logs
 docker exec -i ${container_id}-build_env-1 /bin/bash -c "cd $UNIT_TEST_BUILD_DIR && npm run db:local:reset"
 docker exec -i ${container_id}-build_env-1 \
   /bin/bash -c "cd $UNIT_TEST_BUILD_DIR && \
-  ./scripts/run_ci_unit_coverage.sh" \
+  ./scripts/run_ci_unit_coverage.sh"
 echo "what follows should be an ls of the unit-logs/cumulus directory"
 ls ./unit-logs/@cumulus/
 if [ -n "$(ls -A ./unit-logs/@cumulus 2>/dev/null)" ]
