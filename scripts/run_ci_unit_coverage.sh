fc0da5ec07b12f4c9547e4c9fed04010afc38686
etcart <amberhosen@gmail.com>
2024-05-18 21:03:45 -0400

    will this solve my problem

--- scripts/run_ci_unit_coverage.sh
+++ scripts/run_ci_unit_coverage.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 export UNIT_TEST_BUILD_DIR=$(pwd);
-touch ./test_output.txt
-tail -f ./test_output.txt &
-TAIL_PID=$!
+# touch ./test_output.txt
+# tail -f ./test_output.txt &
+# TAIL_PID=$!
 
-npm run test:ci > ./test_output.txt 2>&1
+npm run test:ci
 
 RESULT=$?
 
-kill -9 $TAIL_PID
+# kill -9 $TAIL_PID
 
-if [[ $RESULT != 0 ]]
-then
-    printf '\n\n\n*****TEST FAILURES:\n'
-    grep '✘' ./test_output.txt
-    exit $RESULT
-fi
+# if [[ $RESULT != 0 ]]
+# then
+#     printf '\n\n\n*****TEST FAILURES:\n'
+#     grep '✘' ./test_output.txt
+#     exit $RESULT
+# fi
 npm run coverage -- --noRerun
 exit $RESULT
