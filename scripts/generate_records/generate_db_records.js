61d84bda480381874fe65ec2cc6f091a983df7e7
etcart <amberhosen@gmail.com>
2024-06-07 11:59:32 -0400

    typehints and typing improvements

--- scripts/generate_records/generate_db_records.js
+++ scripts/generate_records/generate_db_records.js
@@ -1,3 +1,4 @@
+// @ts-check
 /* eslint-disable node/no-extraneous-require */
 /* eslint-disable no-await-in-loop */
 const pMap = require('p-map');
@@ -28,8 +29,23 @@ const log = new Logger({
   sender: '@cumulus/generate_records',
 });
 
-process.env.DISABLE_PG_SSL = true;
-const collectionsDir = 'resources/collections/';
+/**
+ * @typedef {import('@cumulus/db').PostgresFile} PostgresFile
+ * @typedef {import('@cumulus/db').PostgresGranule} PostgresGranule
+ * @typedef {{
+ *   geModel: GranulesExecutionsPgModel,
+ *   executionModel: ExecutionPgModel,
+ *   granuleModel: GranulePgModel,
+ *   fileModel: FilePgModel
+ * }} ModelSet
+ * @typedef {{
+ *   name: string,
+ *   version: string,
+ *   suffix: string,
+ * }} CollectionDetails
+ */
+
+process.env.DISABLE_PG_SSL = 'true';
 
 const createTimestampedTestId = (stackName, testName) =>
   `${stackName}-${testName}-${Date.now()}`;
@@ -38,7 +54,7 @@ function* yieldCollectionDetails(total, repeatable = true) {
   for (let i = 0; i < total; i += 1) {
     let suffix;
     if (repeatable) {
-      suffix = `_test_generator${i.toString().padStart(2, '0')}`;
+      suffix = `_test_${i.toString().padStart(3, '0')}`;
     } else {
       suffix = `_${cryptoRandomString({ length: 5 }).toUpperCase()}`;
     }
@@ -49,14 +65,22 @@ function* yieldCollectionDetails(total, repeatable = true) {
     };
   }
 }
+
+/**
+ * add collection through cumulus-api call
+ *
+ * @param {string} stackName
+ * @param {string} bucket
+ * @param {string} collectionSuffix - append to collection name for uniqueness
+ * @returns {Promise<void>}
+ */
 const addCollection = async (stackName, bucket, collectionSuffix) => {
   const testId = createTimestampedTestId(stackName, 'IngestGranuleSuccess');
-  log.info('pushing up collection with suffix', collectionSuffix);
   try {
     await addCollections(
       stackName,
       bucket,
-      collectionsDir,
+      `${__dirname}/resources/collections/`,
       collectionSuffix,
       testId,
       'replace'
@@ -68,9 +92,17 @@ const addCollection = async (stackName, bucket, collectionSuffix) => {
     throw error;
   }
 };
+
+/**
+ * add provider through cumulus-api call
+ *
+ * @param {string} stackName
+ * @param {string} bucket
+ * @returns {Promise<string>}
+ */
 const addProvider = async (stackName, bucket) => {
   const providerId = 's3_provider_test';
-  const providerJson = JSON.parse(fs.readFileSync('resources/s3_provider.json', 'utf8'));
+  const providerJson = JSON.parse(fs.readFileSync(`${__dirname}/resources/s3_provider.json`, 'utf8'));
   const providerData = {
     ...providerJson,
     id: providerId,
@@ -89,34 +121,66 @@ const addProvider = async (stackName, bucket) => {
   }
   return providerId;
 };
-
+/**
+ * upload files corresponding to granule with granuleCumulusId
+ *
+ * @param {object} knex
+ * @param {number} granuleCumulusId
+ * @param {number} fileCount
+ * @param {ModelSet} models - set of PGmodels including fileModel
+ * @returns {Promise<number>}
+ */
 const uploadFiles = async (knex, granuleCumulusId, fileCount, models) => {
   const fileModel = models.fileModel;
+  let uploaded = 0;
   for (let i = 0; i < fileCount; i += 1) {
-    const file = fakeFileRecordFactory({
+    const file = /** @type {PostgresFile} */(fakeFileRecordFactory({
       granule_cumulus_id: granuleCumulusId,
-    });
+    }));
     try {
       await fileModel.upsert(knex, file);
+      uploaded += 1;
     } catch (error) {
       log.error(`failed up upload file: ${error}`);
     }
   }
+  return uploaded;
 };
+/**
+ * upload executions corresponding to collection with collectionCumulusId
+ *
+ * @param {object} knex
+ * @param {number} collectionCumulusId
+ * @param {number} executionCount
+ * @param {ModelSet} models - set of PGmodels including executionModel
+ * @returns {Promise<Array<number>>} - cumulusId for each successfully uploaded execution
+ */
 const uploadExecutions = async (knex, collectionCumulusId, executionCount, models) => {
-  const executions = [];
+  const executionCumulusIds = [];
   const executionModel = models.executionModel;
   for (let i = 0; i < executionCount; i += 1) {
     const execution = fakeExecutionRecordFactory({ collection_cumulus_id: collectionCumulusId });
     try {
       const [executionOutput] = await executionModel.upsert(knex, execution);
-      executions.push(executionOutput);
+      executionCumulusIds.push(executionOutput.cumulus_id);
     } catch (error) {
       log.error(`failed up upload execution: ${error}`);
     }
   }
-  return executions;
+  return executionCumulusIds;
 };
+
+/**
+ * upload granules corresponding to collection with collectionCumulusId
+ *
+ * @param {object} knex
+ * @param {number} collectionCumulusId
+ * @param {number} providerCumulusId
+ * @param {number} granuleCount
+ * @param {number} filesPerGranule
+ * @param {ModelSet} models - set of PGmodels including granuleModel
+ * @returns {Promise<Array<number>>} - cumulusId for each successfully uploaded granule
+ */
 const uploadGranules = async (
   knex,
   collectionCumulusId,
@@ -125,84 +189,130 @@ const uploadGranules = async (
   filesPerGranule,
   models
 ) => {
-  const granules = [];
+  const granuleCumulusIds = [];
   const granuleModel = models.granuleModel;
   for (let i = 0; i < granuleCount; i += 1) {
-    const granule = fakeGranuleRecordFactory({
+    const granule = /** @type {PostgresGranule} */(fakeGranuleRecordFactory({
       collection_cumulus_id: collectionCumulusId,
       provider_cumulus_id: providerCumulusId,
       status: 'completed',
-    });
+    }));
     try {
       const [granuleOutput] = await granuleModel.upsert({
         knexOrTrx: knex,
         granule,
+        writeConstraints: true,
       });
-      granules.push(granuleOutput);
+      granuleCumulusIds.push(granuleOutput.cumulus_id);
       uploadFiles(knex, granuleOutput.cumulus_id, filesPerGranule, models);
     } catch (error) {
       log.error(`failed up upload granule: ${error}`);
     }
   }
-  return granules;
+  return granuleCumulusIds;
 };
 
-const uploadGranuleExecutions = async (knex, granules, executions, models) => {
+/**
+ * upload granuleExecutions corresponding to each pair
+ * within list of granuleCumulusIds and executionCumulusIds
+ *
+ * @param {object} knex
+ * @param {Array<number>} granuleCumulusIds
+ * @param {Array<number>} executionCumulusIds
+ * @param {ModelSet} models - set of PGmodels including geModel
+ * @returns {Promise<number>} - number of granuleExecutions uploaded
+ */
+const uploadGranuleExecutions = async (knex, granuleCumulusIds, executionCumulusIds, models) => {
   const GEmodel = models.geModel;
-  for (let i = 0; i < granules.length; i += 1) {
-    for (let j = 0; j < executions.length; j += 1) {
+  let uploaded = 0;
+  for (let i = 0; i < granuleCumulusIds.length; i += 1) {
+    for (let j = 0; j < executionCumulusIds.length; j += 1) {
       try {
         await GEmodel.upsert(
           knex,
           {
-            granule_cumulus_id: granules[i].cumulus_id,
-            execution_cumulus_id: executions[j].cumulus_id,
+            granule_cumulus_id: granuleCumulusIds[i],
+            execution_cumulus_id: executionCumulusIds[j],
           }
         );
+        uploaded += 1;
       } catch (error) {
         log.error(`failed up upload granuleExecution: ${error}`);
       }
     }
   }
+  return uploaded;
 };
 
-const uploadDataBunch = async ({
+/**
+ * upload a batch of granules and executions
+ * along with files per granule and granuleExecutions
+ *
+ * @typedef {{
+ *   knex: object,
+ *   collectionCumulusId: number,
+ *   providerCumulusId: number,
+ *   filesPerGranule: number
+ *   granulesPerBatch: number,
+ *   executionsPerBatch: number,
+ *   models: ModelSet,
+ * }} BatchParams
+ *
+ * @param {BatchParams} params
+ * @returns {Promise<void>}
+ */
+const uploadDataBatch = async ({
   knex,
   collectionCumulusId,
   providerCumulusId,
-  files,
-  batchGranules,
-  batchExecutions,
+  filesPerGranule,
+  granulesPerBatch,
+  executionsPerBatch,
   models,
 }) => {
-  const granules = await uploadGranules(
+  const granuleCumulusIds = await uploadGranules(
     knex,
     collectionCumulusId,
     providerCumulusId,
-    batchGranules,
-    files,
+    granulesPerBatch,
+    filesPerGranule,
     models
   );
-  const executions = await uploadExecutions(
+  const executionCumulusIds = await uploadExecutions(
     knex,
     collectionCumulusId,
-    batchExecutions,
+    executionsPerBatch,
     models
   );
-  await uploadGranuleExecutions(knex, granules, executions, models);
+  await uploadGranuleExecutions(knex, granuleCumulusIds, executionCumulusIds, models);
 };
+
+/**
+ * upload a batch of granules and executions
+ * along with files per granule and granuleExecutions
+ *
+ * @param {string} providerId
+ * @param {CollectionDetails} collection
+ * @param {number} granules
+ * @param {number} filesPerGranule
+ * @param {number} granulesPerBatch
+ * @param {number} executionsPerBatch
+ * @param {number} concurrency
+ * @param {boolean} variance
+ * @returns {Promise<void>}
+ */
 
 const uploadDBGranules = async (
   providerId,
   collection,
   granules,
-  files,
-  executionsPerBatch,
+  filesPerGranule,
   granulesPerBatch,
+  executionsPerBatch,
   concurrency,
   variance
 ) => {
-  process.env.dbMaxPool = concurrency;
+  process.env.dbMaxPool = concurrency.toString();
   const knex = await getKnexClient();
 
   const collectionPgModel = new CollectionPgModel();
@@ -222,7 +332,7 @@ const uploadDBGranules = async (
     fileModel: new FilePgModel(),
   };
   const fakeIterable = {};
-  fakeIterable[Symbol.iterator] = function* fakeYielder() {
+  fakeIterable[Symbol.iterator] = function* detailGenerator() {
     let batchGranules = 1;
     for (let i = 0; i < granules; i += batchGranules) {
       console.log(i);
@@ -232,16 +342,17 @@ const uploadDBGranules = async (
         knex,
         collectionCumulusId,
         providerCumulusId,
-        files,
+        filesPerGranule,
         batchGranules,
         batchExecutions,
         models,
       };
     }
   };
+
   await pMap(
-    fakeIterable,
-    uploadDataBunch,
+    /** @type {Iterable<BatchParams>} */(fakeIterable),
+    uploadDataBatch,
     { concurrency }
   );
 };
@@ -304,8 +415,8 @@ const parseArgs = () => {
   return {
     granules: Number.parseInt(granulesK, 10) * 1000,
     files: Number.parseInt(files, 10),
-    granulesPerBatch: Number.parseInt(granulesPerBatch, 10),
-    executionsPerBatch: Number.parseInt(executionsPerBatch, 10),
+    granulesPerBatch: granulesPerBatch,
+    executionsPerBatch: executionsPerBatch,
     collections: Number.parseInt(collections, 10),
     concurrency: Number.parseInt(concurrency, 10),
     variance,
@@ -316,8 +427,8 @@ const main = async () => {
   const {
     granules,
     files,
-    executionsPerBatch,
     granulesPerBatch,
+    executionsPerBatch,
     collections,
     variance,
     concurrency,
@@ -325,7 +436,7 @@ const main = async () => {
 
   const stackName = getRequiredEnvVar('DEPLOYMENT');
   const internalBucket = getRequiredEnvVar('INTERNAL_BUCKET');
-  const providerId = await addProvider(stackName, internalBucket, 'a');
+  const providerId = await addProvider(stackName, internalBucket);
   for (const collection of yieldCollectionDetails(collections, true)) {
     await addCollection(stackName, internalBucket, collection.suffix);
     await uploadDBGranules(
@@ -333,8 +444,8 @@ const main = async () => {
       collection,
       granules,
       files,
-      executionsPerBatch,
       granulesPerBatch,
+      executionsPerBatch,
       concurrency,
       variance
     );
@@ -350,3 +461,9 @@ if (require.main === module) {
     throw error;
   });
 }
+
+module.exports = {
+  yieldCollectionDetails,
+  addCollection,
+  uploadExecutions,
+};
