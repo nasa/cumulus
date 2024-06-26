11899cb3e4bfa52c28c83d6d2ec2b7dee7b75b0d
etcart <amberhosen@gmail.com>
2024-06-26 11:35:15 -0400

    test param passthroughs

--- scripts/generate_records/tests/test-db_record_loaders.js
+++ scripts/generate_records/tests/test-db_record_loaders.js
@@ -7,6 +7,8 @@ const {
   GranulePgModel,
   FilePgModel,
   GranulesExecutionsPgModel,
+  CollectionPgModel,
+  ProviderPgModel,
 } = require('@cumulus/db');
 const { randomId } = require('@cumulus/common/test-utils');
 const { randomInt } = require('crypto');
@@ -20,7 +22,7 @@ const {
   loadRule,
 } = require('../db_record_loaders');
 test.before(async (t) => {
-  t.context.testDbName = randomId('generate_records');
+  t.context.testDbName = randomId('db_record_loaders');
   const { knex, knexAdmin } = await generateLocalTestDb(t.context.testDbName, migrationDir);
 
   t.context.knex = knex;
@@ -46,7 +48,19 @@ test('loadExecutions() uploads executions', async (t) => {
   );
   t.is(executions.length, 3);
   await Promise.all(executions.map(async (execution) => {
-    await executionModel.exists(t.context.knex, { cumulus_id: execution });
+    t.true(await executionModel.exists(t.context.knex, { cumulus_id: execution }));
+  }));
+  const paramedExecutions = await loadExecutions(
+    t.context.knex,
+    t.context.collectionCumulusId,
+    2,
+    executionModel,
+    { original_payload: { a: 'b' }, final_payload: { c: 'd' } },
+  )
+  await Promise.all(paramedExecutions.map(async (execution) => {
+    const exec = await executionModel.get(t.context.knex, { cumulus_id: execution });
+    t.deepEqual(exec.final_payload, { c: 'd' });
+    t.deepEqual(exec.original_payload, { a: 'b' });
   }));
 });
 
@@ -69,11 +83,12 @@ test('loadGranules() uploads granules', async (t) => {
     t.context.collectionCumulusId,
     t.context.providerCumulusId,
     5,
-    granuleModel
+    granuleModel,
+    { duration: 5 }
   );
   t.is(granules.length, 5);
   await Promise.all(granules.map(async (granule) => {
-    t.true(await granuleModel.exists(t.context.knex, { cumulus_id: granule }));
+    t.is((await granuleModel.get(t.context.knex, { cumulus_id: granule })).duration, 5);
   }));
 });
 
@@ -95,6 +110,16 @@ test('loadFiles() uploadsFiles', async (t) => {
   );
   await Promise.all(filesUploaded.map(async (file) => {
     t.true(await fileModel.exists(t.context.knex, { cumulus_id: file }));
+  }));
+  const paramFilesUploaded = await loadFiles(
+    t.context.knex,
+    granuleCumulusId,
+    12,
+    fileModel,
+    { bucket: 'a' }
+  );
+  await Promise.all(paramFilesUploaded.map(async (file) => {
+    t.is((await fileModel.get(t.context.knex, { cumulus_id: file })).bucket, 'a');
   }));
 });
 
@@ -125,21 +150,29 @@ test('loadGranulesExecutions() uploads GranulesExecutions', async (t) => {
     t.true(await geModel.exists(t.context.knex, granuleExecution));
   }));
 });
-test('loadCollection() adds collections', async (t) => {
+test.only('loadCollection() adds collections', async (t) => {
   //this test is largely redundant to all the above tests that demand this works to function
   for (const collectionNumber of new Array(12)) {
     const numberOfFiles = randomInt(5);
     // eslint-disable-next-line no-await-in-loop
     await loadCollection(t.context.knex, numberOfFiles, collectionNumber);
   }
-  await loadCollection(t.context.knex, 12);
-  t.pass();
+  const collectionModel = new CollectionPgModel();
+  const randomizedCollectionId = await loadCollection(t.context.knex, 12);
+  t.true((await collectionModel.exists(t.context.knex, { cumulus_id: randomizedCollectionId })));
+  
+  const parameterizedCollectionId = await loadCollection(t.context.knex, 12, 1, { duplicate_handling: 'replace' });
+
+  t.is((await collectionModel.get(t.context.knex, { cumulus_id: parameterizedCollectionId })).duplicate_handling, 'replace');
 });
 
 test('loadProvider() adds a provider', async (t) => {
   //this test is largely redundant to all the above tests that demand this works to function
-  await loadProvider(t.context.knex);
-  t.pass();
+  const providerId = await loadProvider(t.context.knex, { name: 'abcd' });
+
+  const providerModel = new ProviderPgModel();
+  t.is((await providerModel.get(t.context.knex, { cumulus_id: providerId })).name, 'abcd')
+
 });
 test('loadRule() adds a rule and accepts undefined collection/provider ids', async (t) => {
   await loadRule(
