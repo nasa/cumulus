58f0d08592169a78647a3f69a286244028f6ee36
Paul Pilone <paul@element84.com>
2024-07-02 14:08:33 -0400

    WIP: Bumps default cumulus-ecs-task version to 2.1.0

--- example/cumulus-tf/variables.tf
+++ example/cumulus-tf/variables.tf
@@ -362,7 +362,7 @@ variable "cumulus_process_activity_version" {
 variable "ecs_task_image_version" {
   description = "docker image version to use for Cumulus hello world task"
     type = string
-    default = "2.1.0-alpha"
+    default = "2.1.0"
 }
 
 variable "cumulus_test_ingest_image_version" {
