964dff03a18271bf686f9d70014f1a8504dfd424
etcart <amberhosen@gmail.com>
2024-07-10 15:48:57 -0400

    fix tests for new paradigm

--- packages/api/tests/lambdas/test-clean_pg_executions.js
+++ packages/api/tests/lambdas/test-clean_pg_executions.js
@@ -14,9 +14,8 @@ const {
 } = require('@cumulus/db');
 const { randomId } = require('@cumulus/common/test-utils');
 const {
-  getExpirablePayloadRecords,
   cleanupExpiredPGExecutionPayloads,
-  getExpirationDates,
+  getExpirationDate,
 } = require('../../lambdas/cleanExecutions');
 
 test.beforeEach(async (t) => {
@@ -34,112 +33,13 @@ test.afterEach.always(async (t) => {
   });
 });
 
-test('getExpirablePayloadRecords()', async (t) => {
-  const executions = [
-    fakeExecutionRecordFactory({
-      updated_at: moment().subtract(10, 'days').toDate(),
-      final_payload: '{"a": "b"}',
-    }),
-    fakeExecutionRecordFactory({
-      updated_at: moment().subtract(5, 'days').toDate(),
-      final_payload: '{"a": "b"}',
-    }),
-    fakeExecutionRecordFactory({
-      updated_at: moment().subtract(15, 'days').toDate(),
-      final_payload: '{"a": "b"}',
-    }),
-    fakeExecutionRecordFactory({
-      updated_at: moment().subtract(2, 'days').toDate(),
-      final_payload: '{"a": "b"}',
-    }),
-    fakeExecutionRecordFactory({
-      updated_at: moment().subtract(23, 'days').toDate(),
-    }),
-    fakeExecutionRecordFactory({
-      updated_at: moment().subtract(10, 'days').toDate(),
-    }),
-    fakeExecutionRecordFactory({
-      updated_at: moment().subtract(5, 'days').toDate(),
-      original_payload: '{"a": "b"}',
-    }),
-    fakeExecutionRecordFactory({
-      updated_at: moment().subtract(15, 'days').toDate(),
-      original_payload: '{"a": "b"}',
-    }),
-    fakeExecutionRecordFactory({
-      updated_at: moment().subtract(2, 'days').toDate(),
-      final_payload: '{"a": "b"}',
-    }),
-    fakeExecutionRecordFactory({
-      updated_at: moment().subtract(23, 'days').toDate(),
-      final_payload: '{"a": "b"}',
-    }),
-    fakeExecutionRecordFactory({
-      updated_at: moment().subtract(10, 'days').toDate(),
-      original_payload: '{"a": "b"}',
-    }),
-    fakeExecutionRecordFactory({
-      updated_at: moment().subtract(5, 'days').toDate(),
-    }),
-    fakeExecutionRecordFactory({
-      updated_at: moment().subtract(15, 'days').toDate(),
-      final_payload: '{"a": "b"}',
-    }),
-    fakeExecutionRecordFactory({
-      updated_at: moment().subtract(2, 'days').toDate(),
-      final_payload: '{"a": "b"}',
-    }),
-  ];
-  const model = new ExecutionPgModel();
-  const inserted = await model.insert(t.context.knex, executions, '*');
-  let expirable = await getExpirablePayloadRecords(t.context.knex, moment().subtract(10, 'days').toDate(), 100);
-  let expected = [
-    inserted[0],
-    inserted[2],
-    inserted[7],
-    inserted[9],
-    inserted[10],
-    inserted[12],
-  ];
-
-  t.deepEqual(expected, expirable);
-  expirable = await getExpirablePayloadRecords(t.context.knex, moment().subtract(0, 'days').toDate(), 100);
-  expected = [
-    inserted[0],
-    inserted[1],
-    inserted[2],
-    inserted[3],
-    inserted[6],
-    inserted[7],
-    inserted[8],
-    inserted[9],
-    inserted[10],
-    inserted[12],
-    inserted[13],
-  ];
-  t.deepEqual(expected, expirable);
-
-  expirable = await getExpirablePayloadRecords(t.context.knex, moment().subtract(3, 'days').toDate(), 100);
-  expected = [
-    inserted[0],
-    inserted[1],
-    inserted[2],
-    inserted[6],
-    inserted[7],
-    inserted[9],
-    inserted[10],
-    inserted[12],
-  ];
-  t.deepEqual(expected, expirable);
-});
 const pgPayloadsEmpty = (entry) => !entry.final_payload && !entry.orginal_payload;
 
-test('cleanupExpiredPGExecutionPayloads() cleans up expired payloads', async (t) => {
+test.serial('cleanupExpiredPGExecutionPayloads() cleans up expired running payloads', async (t) => {
   const env = clone(process.env);
   process.env = localStackConnectionEnv;
   process.env.PG_DATABASE = t.context.testDbName;
-  const completeExpirationDays = 3;
-  const nonCompleteExpirationDays = 5;
+  const expirationDays = 3;
 
   const records = [];
   for (let i = 0; i < 10; i += 1) {
@@ -153,7 +53,7 @@ test('cleanupExpiredPGExecutionPayloads() cleans up expired payloads', async (t)
       updated_at: moment().subtract(i, 'days').toDate(),
       final_payload: '{"a": "b"}',
       original_payload: '{"b": "c"}',
-      status: 'failed',
+      status: 'running',
     }));
   }
   const knex = await getKnexClient();
@@ -161,8 +61,106 @@ test('cleanupExpiredPGExecutionPayloads() cleans up expired payloads', async (t)
   const cumulusIds = await model.insert(knex, records, 'cumulus_id');
 
   await cleanupExpiredPGExecutionPayloads(
-    completeExpirationDays,
-    nonCompleteExpirationDays,
+    expirationDays,
+    true,
+    false
+  );
+
+  const massagedPgExecutions = await Promise.all(
+    cumulusIds.map(
+      async (cumulusId) => await model.get(knex, cumulusId)
+    )
+  );
+
+  const expiration = getExpirationDate(expirationDays);
+
+  massagedPgExecutions.forEach((massagedExecution) => {
+    if (massagedExecution.updated_at < expiration && massagedExecution.status === 'running') {
+      t.true(pgPayloadsEmpty(massagedExecution));
+    } else {
+      t.false(pgPayloadsEmpty(massagedExecution));
+    }
+  });
+  process.env = env;
+});
+
+
+test.serial('cleanupExpiredPGExecutionPayloads() cleans up expired non running payloads', async (t) => {
+  const env = clone(process.env);
+  process.env = localStackConnectionEnv;
+  process.env.PG_DATABASE = t.context.testDbName;
+  const expirationDays = 6;
+
+  const records = [];
+  for (let i = 0; i < 10; i += 1) {
+    records.push(fakeExecutionRecordFactory({
+      updated_at: moment().subtract(i, 'days').toDate(),
+      final_payload: '{"a": "b"}',
+      original_payload: '{"b": "c"}',
+      status: 'completed',
+    }));
+    records.push(fakeExecutionRecordFactory({
+      updated_at: moment().subtract(i, 'days').toDate(),
+      final_payload: '{"a": "b"}',
+      original_payload: '{"b": "c"}',
+      status: 'running',
+    }));
+  }
+  const knex = await getKnexClient();
+  const model = new ExecutionPgModel();
+  const cumulusIds = await model.insert(knex, records, 'cumulus_id');
+
+  await cleanupExpiredPGExecutionPayloads(
+    expirationDays,
+    false,
+    true
+  );
+
+  const massagedPgExecutions = await Promise.all(
+    cumulusIds.map(
+      async (cumulusId) => await model.get(knex, cumulusId)
+    )
+  );
+
+  const expiration = getExpirationDate(expirationDays);
+  massagedPgExecutions.forEach((massagedExecution) => {
+    if (massagedExecution.updated_at < expiration && massagedExecution.status !== 'running') {
+      t.true(pgPayloadsEmpty(massagedExecution));
+    } else {
+      t.false(pgPayloadsEmpty(massagedExecution));
+    }
+  });
+  process.env = env;
+});
+
+
+test.serial('cleanupExpiredPGExecutionPayloads() cleans up expired payloads', async (t) => {
+  const env = clone(process.env);
+  process.env = localStackConnectionEnv;
+  process.env.PG_DATABASE = t.context.testDbName;
+  const expirationDays = 6;
+
+  const records = [];
+  for (let i = 0; i < 10; i += 1) {
+    records.push(fakeExecutionRecordFactory({
+      updated_at: moment().subtract(i, 'days').toDate(),
+      final_payload: '{"a": "b"}',
+      original_payload: '{"b": "c"}',
+      status: 'completed',
+    }));
+    records.push(fakeExecutionRecordFactory({
+      updated_at: moment().subtract(i, 'days').toDate(),
+      final_payload: '{"a": "b"}',
+      original_payload: '{"b": "c"}',
+      status: 'running',
+    }));
+  }
+  const knex = await getKnexClient();
+  const model = new ExecutionPgModel();
+  const cumulusIds = await model.insert(knex, records, 'cumulus_id');
+
+  await cleanupExpiredPGExecutionPayloads(
+    expirationDays,
     true,
     true
   );
@@ -173,20 +171,9 @@ test('cleanupExpiredPGExecutionPayloads() cleans up expired payloads', async (t)
     )
   );
 
-  const {
-    completeExpiration,
-    nonCompleteExpiration,
-  } = getExpirationDates(
-    completeExpirationDays,
-    nonCompleteExpirationDays,
-    true,
-    true
-  );
-
+  const expiration = getExpirationDate(expirationDays);
   massagedPgExecutions.forEach((massagedExecution) => {
-    if (massagedExecution.updated_at < completeExpiration && massagedExecution.status === 'completed') {
-      t.true(pgPayloadsEmpty(massagedExecution));
-    } else if (massagedExecution.updated_at < nonCompleteExpiration && massagedExecution.status !== 'completed') {
+    if (massagedExecution.updated_at < expiration) {
       t.true(pgPayloadsEmpty(massagedExecution));
     } else {
       t.false(pgPayloadsEmpty(massagedExecution));
