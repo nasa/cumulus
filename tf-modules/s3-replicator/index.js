0476e8188d04d7fec2f339db914ed38edc36fb63
Nate Pauzenga <npauzenga@gmail.com>
2024-07-17 16:22:24 -0400

    Add regex and param

--- tf-modules/s3-replicator/index.js
+++ tf-modules/s3-replicator/index.js
@@ -4,22 +4,21 @@ const { S3 } = require('@aws-sdk/client-s3');
 const path = require('path');
 
 function _getLogDate(objectKey) {
-  return objectKey.match(/(\d{4}-\d{2}-\d{2})/g);
+  const date = objectKey.match(/(\d{4}-\d{2}-\d{2})/g);
+  return Date.parse([date]);
 }
 
 async function handler(event, _context) {
   const s3 = new S3();
-  // TODO placeholder, needs to be a param or some way for user to configure
-  const skipEarlierThanDate = '2023-10-01'
+  const skipEarlierThanDate = Date.parse(process.env.earlierThanDate);
 
   return await Promise.all(event.Records.map(async (rec) => {
     const eventType = rec.eventName;
     if (!eventType.startsWith('ObjectCreated:')) return null;
     const srcBucket = rec.s3.bucket.name;
     const srcKey = rec.s3.object.key;
-    const objectDate = _getLogDate(srcKey)[0];
+    const objectDate = _getLogDate(srcKey);
 
-    // TODO probably moment date comparisons, not strings
     if (objectDate > skipEarlierThanDate) {
       return await s3.copyObject({
         CopySource: `${srcBucket}/${srcKey}`,
