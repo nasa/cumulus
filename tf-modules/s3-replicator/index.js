5e6669889b47cf0cc3a5ca402df031d551561082
Nate Pauzenga <npauzenga@gmail.com>
2024-07-17 12:30:19 -0400

    Initial update to filter based on date

--- tf-modules/s3-replicator/index.js
+++ tf-modules/s3-replicator/index.js
@@ -3,19 +3,33 @@
 const { S3 } = require('@aws-sdk/client-s3');
 const path = require('path');
 
+function _getLogDate(objectKey) {
+  return objectKey.match(/(\d{4}-\d{2}-\d{2})/g);
+}
+
 async function handler(event, _context) {
   const s3 = new S3();
+  // TODO placeholder, needs to be a param or some way for user to configure
+  const skipEarlierThanDate = '2023-10-01'
+
   return await Promise.all(event.Records.map(async (rec) => {
     const eventType = rec.eventName;
     if (!eventType.startsWith('ObjectCreated:')) return null;
     const srcBucket = rec.s3.bucket.name;
     const srcKey = rec.s3.object.key;
-    return await s3.copyObject({
-      CopySource: `${srcBucket}/${srcKey}`,
-      Bucket: process.env.TARGET_BUCKET,
-      Key: `${process.env.TARGET_PREFIX}/${path.basename(srcKey)}`,
-      ACL: 'bucket-owner-full-control',
-    });
+    const objectDate = _getLogDate(srcKey)[0];
+
+    // TODO probably moment date comparisons, not strings
+    if (objectDate > skipEarlierThanDate) {
+      return await s3.copyObject({
+        CopySource: `${srcBucket}/${srcKey}`,
+        Bucket: process.env.TARGET_BUCKET,
+        Key: `${process.env.TARGET_PREFIX}/${path.basename(srcKey)}`,
+        ACL: 'bucket-owner-full-control',
+      });
+    }
+
+    return null;
   }));
 }
 
--- tf-modules/s3-replicator/test/test-index.js
+++ tf-modules/s3-replicator/test/test-index.js
@@ -12,3 +12,24 @@ test('handler returns immediately on non-create event', async (t) => {
   const output = await handler(event, {});
   t.deepEqual(output, [null]);
 });
+
+test('handler skips records created before a given date', async (t) => {
+  const event = {
+    Records: [
+      {
+        eventName: 'ObjectCreated:*',
+        s3: {
+          bucket: {
+            name: "bucket-name",
+          },
+          object: {
+            key: 'test-cumulus-prod/ems-distribution/s3-server-access-logs/2023-09-29-22-29-57-D1FC2703AD3288C8'
+          }
+        }
+      },
+
+    ],
+  };
+  const output = await handler(event, {});
+  t.deepEqual(output, [null]);
+});
