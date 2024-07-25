cb7877a5e8f88faefb33a9c4912c11b469fcba70
jennyhliu <jenny.h.liu@nasa.gov>
2024-07-25 16:44:28 -0400

    add more index

--- packages/db/src/migrations/20240715101230_add_indexes.sql
+++ packages/db/src/migrations/20240715101230_add_indexes.sql
@@ -1,6 +1,10 @@
 SELECT CURRENT_TIMESTAMP;
 CREATE INDEX CONCURRENTLY IF NOT EXISTS async_operations_updated_at_index ON async_operations(updated_at);
 SELECT CURRENT_TIMESTAMP;
+CREATE INDEX CONCURRENTLY IF NOT EXISTS async_operations_status_operation_type_cumulus_id_index ON async_operations(status, operation_type, cumulus_id);
+SELECT CURRENT_TIMESTAMP;
+
+SELECT CURRENT_TIMESTAMP;
 CREATE INDEX CONCURRENTLY IF NOT EXISTS collections_updated_at_index ON collections(updated_at);
 SELECT CURRENT_TIMESTAMP;
 CREATE INDEX CONCURRENTLY IF NOT EXISTS executions_updated_at_index ON executions(updated_at);
@@ -23,6 +27,13 @@ SELECT CURRENT_TIMESTAMP;
 SELECT CURRENT_TIMESTAMP;
 CREATE INDEX CONCURRENTLY IF NOT EXISTS pdrs_updated_at_index ON pdrs(updated_at);
 SELECT CURRENT_TIMESTAMP;
+CREATE INDEX CONCURRENTLY IF NOT EXISTS pdrs_status_provider_collection_cumulus_id_index ON pdrs(status, provider_cumulus_id, collection_cumulus_id, cumulus_id);
+SELECT CURRENT_TIMESTAMP;
+CREATE INDEX CONCURRENTLY IF NOT EXISTS pdrs_execution_cumulus_id_index ON pdrs(execution_cumulus_id);
+SELECT CURRENT_TIMESTAMP;
+CREATE INDEX CONCURRENTLY IF NOT EXISTS pdrs_coll_status_cumulus_id_index ON pdrs(collection_cumulus_id, status, cumulus_id);
+SELECT CURRENT_TIMESTAMP;
+
 CREATE INDEX CONCURRENTLY IF NOT EXISTS providers_updated_at_index ON providers(updated_at);
 SELECT CURRENT_TIMESTAMP;
 CREATE INDEX CONCURRENTLY IF NOT EXISTS rules_updated_at_index ON rules(updated_at);
