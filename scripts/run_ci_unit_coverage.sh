4cd872aa75271585204c39fe5d960592f2863cdc
etcart <amberhosen@gmail.com>
2024-08-20 13:31:47 -0400

    why don't api tests seem to be runnig

--- scripts/run_ci_unit_coverage.sh
+++ scripts/run_ci_unit_coverage.sh
@@ -6,8 +6,6 @@ TAIL_PID=$!
 
 npm --prefix=packages/api run test:ci > ./test_output.txt
 
-npm run test:ci -- --ignore @cumulus/api > ./test_output.txt
-
 RESULT=$?
 # make sure tail has gotten everything out
 sleep 2
@@ -19,5 +17,24 @@ then
     grep '✘' ./test_output.txt
     exit $RESULT
 fi
+
+# tail -f ./test_output.txt &
+# TAIL_PID=$!
+
+
+# npm run test:ci -- --ignore @cumulus/api > ./test_output.txt
+
+# RESULT=$?
+# # make sure tail has gotten everything out
+# sleep 2
+# kill -9 $TAIL_PID
+
+# if [[ $RESULT != 0 ]]
+# then
+#     printf '\n\n\n*****TEST FAILURES:\n'
+#     grep '✘' ./test_output.txt
+#     exit $RESULT
+# fi
+
 npm run coverage -- --noRerun
 exit $RESULT
