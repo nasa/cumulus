1bef841dd52a8eea4d422eaf67be7bb1a419b5f9
etcart <amberhosen@gmail.com>
2024-08-21 10:12:19 -0400

    try to handle prefix by cd to get around pipefail

--- scripts/run_ci_unit_coverage.sh
+++ scripts/run_ci_unit_coverage.sh
@@ -3,9 +3,9 @@ export UNIT_TEST_BUILD_DIR=$(pwd);
 touch ./test_output.txt
 tail -f ./test_output.txt &
 TAIL_PID=$!
-
-npm --prefix=packages/api run test:ci > ./test_output.txt
-
+cd packages/api
+npm run test:ci > ./test_output.txt
+cd $UNIT_TEST_BUILD_DIR
 RESULT=$?
 # make sure tail has gotten everything out
 sleep 2
@@ -18,23 +18,23 @@ then
     exit $RESULT
 fi
 
-# tail -f ./test_output.txt &
-# TAIL_PID=$!
+tail -f ./test_output.txt &
+TAIL_PID=$!
 
 
-# npm run test:ci -- --ignore @cumulus/api > ./test_output.txt
+npm run test:ci -- --ignore @cumulus/api > ./test_output.txt
 
-# RESULT=$?
-# # make sure tail has gotten everything out
-# sleep 2
-# kill -9 $TAIL_PID
+RESULT=$?
+# make sure tail has gotten everything out
+sleep 2
+kill -9 $TAIL_PID
 
-# if [[ $RESULT != 0 ]]
-# then
-#     printf '\n\n\n*****TEST FAILURES:\n'
-#     grep '✘' ./test_output.txt
-#     exit $RESULT
-# fi
+if [[ $RESULT != 0 ]]
+then
+    printf '\n\n\n*****TEST FAILURES:\n'
+    grep '✘' ./test_output.txt
+    exit $RESULT
+fi
 
 npm run coverage -- --noRerun
 exit $RESULT
--- scripts/run_package_ci_unit.sh
+++ scripts/run_package_ci_unit.sh
@@ -1,5 +1,5 @@
 
-# set -o pipefail
+set -o pipefail
 mkdir -p ${CUMULUS_UNIT_TEST_DATA}/unit-logs/@cumulus/
 touch ${CUMULUS_UNIT_TEST_DATA}/unit-logs/$(jq -r '.name' package.json).log
 npm run test:coverage 2>&1 | tee ${CUMULUS_UNIT_TEST_DATA}/unit-logs/$(jq -r '.name' package.json).log && \
