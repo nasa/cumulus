7de7cf92eb0e169b1069ec733c351704fee73ddb
etcart <amberhosen@gmail.com>
2024-08-21 11:23:01 -0400

    fr tho what's happening here?

--- scripts/run_ci_unit_coverage.sh
+++ scripts/run_ci_unit_coverage.sh
@@ -4,19 +4,19 @@ touch ./test_output.txt
 tail -f ./test_output.txt &
 TAIL_PID=$!
 cd packages/api
-npm run test:ci > ./test_output.txt
+npm run test:ci
 cd $UNIT_TEST_BUILD_DIR
-RESULT=$?
-# make sure tail has gotten everything out
-sleep 2
-kill -9 $TAIL_PID
+# RESULT=$?
+# # make sure tail has gotten everything out
+# sleep 2
+# kill -9 $TAIL_PID
 
-if [[ $RESULT != 0 ]]
-then
-    printf '\n\n\n*****TEST FAILURES:\n'
-    grep '✘' ./test_output.txt
-    exit $RESULT
-fi
+# if [[ $RESULT != 0 ]]
+# then
+#     printf '\n\n\n*****TEST FAILURES:\n'
+#     grep '✘' ./test_output.txt
+#     exit $RESULT
+# fi
 
 # tail -f ./test_output.txt &
 # TAIL_PID=$!
