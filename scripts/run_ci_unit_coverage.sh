7013bb42dc6a11e417753ad790f1a0f77d5d4810
etcart <amberhosen@gmail.com>
2024-08-21 12:18:27 -0400

    api tests are running separately confirmed

--- scripts/run_ci_unit_coverage.sh
+++ scripts/run_ci_unit_coverage.sh
@@ -4,37 +4,38 @@ touch ./test_output.txt
 tail -f ./test_output.txt &
 TAIL_PID=$!
 cd packages/api
-npm run test:ci
+npm run test:ci > ${UNIT_TEST_BUILD_DIR}/test_output.txt
+RESULT=$?
+
 cd $UNIT_TEST_BUILD_DIR
-# RESULT=$?
-# # make sure tail has gotten everything out
-# sleep 2
-# kill -9 $TAIL_PID
+# make sure tail has gotten everything out
+sleep 2
+kill -9 $TAIL_PID
 
-# if [[ $RESULT != 0 ]]
-# then
-#     printf '\n\n\n*****TEST FAILURES:\n'
-#     grep '✘' ./test_output.txt
-#     exit $RESULT
-# fi
+if [[ $RESULT != 0 ]]
+then
+    printf '\n\n\n*****TEST FAILURES:\n'
+    grep '✘' ${UNIT_TEST_BUILD_DIR}/test_output.txt
+    exit $RESULT
+fi
 
-# tail -f ./test_output.txt &
-# TAIL_PID=$!
+tail -f ./test_output.txt &
+TAIL_PID=$!
 
 
-# npm run test:ci -- --ignore @cumulus/api > ./test_output.txt
+npm run test:ci -- --ignore @cumulus/api > ${UNIT_TEST_BUILD_DIR}/test_output.txt
 
-# RESULT=$?
-# # make sure tail has gotten everything out
-# sleep 2
-# kill -9 $TAIL_PID
+RESULT=$?
+# make sure tail has gotten everything out
+sleep 2
+kill -9 $TAIL_PID
 
-# if [[ $RESULT != 0 ]]
-# then
-#     printf '\n\n\n*****TEST FAILURES:\n'
-#     grep '✘' ./test_output.txt
-#     exit $RESULT
-# fi
+if [[ $RESULT != 0 ]]
+then
+    printf '\n\n\n*****TEST FAILURES:\n'
+    grep '✘' ${UNIT_TEST_BUILD_DIR}/test_output.txt
+    exit $RESULT
+fi
 
 npm run coverage -- --noRerun
 exit $RESULT
