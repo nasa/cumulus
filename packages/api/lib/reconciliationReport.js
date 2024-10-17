1af2d1cb8f1b70ca2926b47cd2e923a5b4dd1503
Jonathan Kovarik <Jkovarik@users.noreply.github.com>
2024-10-17 09:28:35 -0600

    Seperate typedefs into seperate file

--- packages/api/lib/reconciliationReport.js
+++ packages/api/lib/reconciliationReport.js
@@ -11,30 +11,12 @@ const Logger = require('@cumulus/logger');
 
 const log = new Logger({ sender: '@api/lambdas/create-reconciliation-report' });
 
-/** @typedef {import('../lib/types').RecReportParams } RecReportParams */
 /**
+ * @typedef {import('../lib/types').RecReportParams } RecReportParams
  * @typedef {import('../lib/types').EnhancedNormalizedRecReportParams }
  * EnhancedNormalizedRecReportParams
- */
-
-/** @typedef {import('../lib/types').NormalizedRecReportParams } NormalizedRecReportParams */
-
-/**
- * @typedef {Object} ReportHeader
- * @property {string | undefined} collectionId - The collection ID.
- * @property {string | string[] | undefined} collectionIds - The collection IDs.
- * @property {string | undefined} createEndTime - The end time of the report creation.
- * @property {string} createStartTime - The start time of the report creation.
- * @property {string | undefined} error - Any error that occurred.
- * @property {string | undefined} granuleId - The granule ID.
- * @property {string | string[] | undefined} granuleIds - The granule IDs.
- * @property {string | string[] | undefined} provider - The provider.
- * @property {string | string[] | undefined} providers - The providers.
- * @property {string | undefined} location - The location.
- * @property {string | undefined} reportEndTime - The end time of the report.
- * @property {string | undefined} reportStartTime - The start time of the report.
- * @property {string} reportType - The type of the report.
- * @property {string} status - The status of the report.
+ * @typedef {import('../lib/types').NormalizedRecReportParams } NormalizedRecReportParams
+ * @typedef {import('./reconciliationReport-types').ReportHeader } ReportHeader
  */
 
 /**
