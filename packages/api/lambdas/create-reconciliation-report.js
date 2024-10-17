b7bd336235ae686c3dba12c220f1ed2d89906596
Jonathan Kovarik <Jkovarik@users.noreply.github.com>
2024-10-17 08:39:00 -0600

    Split defs out into seperate file

--- packages/api/lambdas/create-reconciliation-report.js
+++ packages/api/lambdas/create-reconciliation-report.js
@@ -50,6 +50,7 @@ const log = new Logger({ sender: '@api/lambdas/create-reconciliation-report' });
 const isDataBucket = (bucketConfig) => ['private', 'public', 'protected'].includes(bucketConfig.type);
 
 // Typescript annotations
+
 /**
  * @typedef {typeof process.env } ProcessEnv
  * @typedef {import('knex').Knex} Knex
@@ -66,62 +67,15 @@ const isDataBucket = (bucketConfig) => ['private', 'public', 'protected'].includ
  * ReconciliationReportType
  * @typedef {import('@cumulus/types/api/files').ApiFile} ApiFile
  * @typedef {import('@cumulus/db').PostgresGranuleRecord} PostgresGranuleRecord
- */
-
-/**
- * @typedef {Object} Env
- * @property {string} [CONCURRENCY] - The concurrency level for processing.
- * @property {string} [ES_INDEX] - The Elasticsearch index.
- * @property {string} [AWS_REGION] - The AWS region.
- * @property {string} [AWS_ACCESS_KEY_ID] - The AWS access key ID.
- * @property {string} [AWS_SECRET_ACCESS_KEY] - The AWS secret access key.
- * @property {string} [AWS_SESSION_TOKEN] - The AWS session token.
- * @property {string} [NODE_ENV] - The Node.js environment (e.g., 'development', 'production').
- * @property {string} [DATABASE_URL] - The database connection URL.
- * @property {string} [key] string - Any other environment variable as a string.
- */
-
-/**
- * @typedef {Object} CMRCollectionItem
- * @property {Object} umm - The UMM (Unified Metadata Model) object for the granule.
- * @property {string} umm.ShortName - The short name of the collection.
- * @property {string} umm.Version - The version of the collection.
- * @property {Array<Object>} umm.RelatedUrls - The related URLs for the granule.
- */
-
-/**
- * @typedef {Object} CMRItem
- * @property {Object} umm - The UMM (Unified Metadata Model) object for the granule.
- * @property {string} umm.GranuleUR - The unique identifier for the granule in CMR.
- * @property {Object} umm.CollectionReference - The collection reference object.
- * @property {string} umm.CollectionReference.ShortName - The short name of the collection.
- * @property {string} umm.CollectionReference.Version - The version of the collection.
- * @property {Array<Object>} umm.RelatedUrls - The related URLs for the granule.
- */
-
-/**
- * @typedef {Object} FilesReport
- * @property {number} okCount
- * @property {ApiFile[]} onlyInCumulus
- * @property {ApiFile[]} onlyInCmr
+ * @typedef {import('./create-reconciliation-report-types').Env } Env
+ * @typedef {import('./create-reconciliation-report-types').CMRCollectionItem } CMRCollectionItem
+ * @typedef {import('./create-reconciliation-report-types').CMRItem } CMRItem
+ * @typedef {import('./create-reconciliation-report-types').FilesReport } FilesReport
+ * @typedef {import('./create-reconciliation-report-types').GranulesReport } GranulesReport
+ * @typedef {import('./create-reconciliation-report-types').FilesInCumulus } FilesInCumulus
  *
  */
 
-/**
- * @typedef {Object} GranulesReport
- * @property {number} okCount - The count of OK granules.
- * @property {Array<{GranuleUR: string, ShortName: string, Version: string}>} onlyInCmr
- * - The list of granules only in Cumulus.
- * @property {Array<{granuleId: string, collectionId: string}>} onlyInCumulus
- */
-
-/**
-  * @typedef {Object} FilesInCumulus
-  * @property {number} okCount
-  * @property {Object<string, number>} okCountByGranule
-  * @property {string[]} onlyInS3
-  * @property {Object[]} onlyInDb
-  */
 /**
  *
  * @param {string} reportType - reconciliation report type
