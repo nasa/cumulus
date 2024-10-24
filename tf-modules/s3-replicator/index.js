4c2c7c40f05725f5c51bbff52ef0bb721bbdcab0
jennyhliu <jenny.h.liu@nasa.gov>
2024-10-24 10:27:30 -0400

    CUMULUS-3876:Fix S3 Replicator cross region bucket writes

--- tf-modules/s3-replicator/index.js
+++ tf-modules/s3-replicator/index.js
@@ -1,16 +1,18 @@
 'use strict';
 
-const { S3 } = require('@aws-sdk/client-s3');
 const path = require('path');
+const { s3 } = require('@cumulus/aws-client/services');
 
 async function handler(event, _context) {
-  const s3 = new S3();
+  const targetRegion = process.env.TARGET_REGION;
+  const serviceOptions = targetRegion ? { region: targetRegion } : {};
+  const s3Client = s3(serviceOptions);
   return await Promise.all(event.Records.map(async (rec) => {
     const eventType = rec.eventName;
     if (!eventType.startsWith('ObjectCreated:')) return null;
     const srcBucket = rec.s3.bucket.name;
     const srcKey = rec.s3.object.key;
-    return await s3.copyObject({
+    return await s3Client.copyObject({
       CopySource: `${srcBucket}/${srcKey}`,
       Bucket: process.env.TARGET_BUCKET,
       Key: `${process.env.TARGET_PREFIX}/${path.basename(srcKey)}`,
