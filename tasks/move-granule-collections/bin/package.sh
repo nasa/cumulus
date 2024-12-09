4a6bb014b18e8390df75667aa00080cab09fac8e
etcart <amberhosen@gmail.com>
2024-12-09 09:59:58 -0500

    different package structure to satisfy scriptless bootstrap

---
+++ tasks/move-granule-collections/bin/package.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+set -e
+
+rm -rf webpack
+
+npm run webpack
+
+cp -R schemas webpack
+
+(
+  set -e
+
+  cd webpack
+  rm -f lambda.zip && node ../../../../bin/zip.js lambda.zip index.js schemas
+)
