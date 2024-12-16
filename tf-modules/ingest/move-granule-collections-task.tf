e1172b24341606417732e5e91c61a73e218a6343
etcart <amberhosen@gmail.com>
2024-12-16 15:28:11 -0500

    try to parse bucket perms

--- tf-modules/ingest/move-granule-collections-task.tf
+++ tf-modules/ingest/move-granule-collections-task.tf
@@ -100,6 +100,15 @@ data "aws_iam_policy_document" "move_granule_collections_task_policy" {
     ]
     resources = [ "arn:aws:s3:::${var.system_bucket}/*"]
   }
+  statement {
+    actions = [
+      "s3:AbortMultipartUpload",
+      "s3:GetObject*",
+      "s3:PutObject*",
+      "s3:ListMultipartUploadParts",
+    ]
+    resources = [ "arn:aws:s3:::${var.buckets}/*"]
+  }
 
   statement {
     actions = ["secretsmanager:GetSecretValue"]
