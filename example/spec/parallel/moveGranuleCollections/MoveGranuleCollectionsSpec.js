207550073ea76d953b8223e7c92299b87dbcb061
etcart <amberhosen@gmail.com>
2024-12-19 23:58:48 -0500

    linting fixes and test for presence in cumulus according to api

--- example/spec/parallel/moveGranuleCollections/MoveGranuleCollectionsSpec.js
+++ example/spec/parallel/moveGranuleCollections/MoveGranuleCollectionsSpec.js
@@ -10,22 +10,139 @@ const { waitForListObjectsV2ResultCount } = require('@cumulus/integration-tests'
 const {
   granules,
   collections,
-  files,
 } = require('@cumulus/api-client');
 
 const path = require('path');
 const fs = require('fs');
 const { v4: uuidv4 } = require('uuid');
 const { loadConfig } = require('../../helpers/testUtils');
+const { constructCollectionId } = require('../../../../packages/message/Collections');
 describe('when moveGranulesCollection is called', () => {
   let stackName;
-  let knex
+  const sourceUrlPrefix = `source_path/${uuidv4()}`;
+  const targetUrlPrefix = `target_path/${uuidv4()}`;
+  const originalCollection = {
+    files: [
+      {
+        regex: '^MOD11A1\\.A[\\d]{7}\\.[\\S]{6}\\.006.[\\d]{13}\\.hdf$',
+        sampleFileName: 'MOD11A1.A2017200.h19v04.006.2017201090724.hdf',
+        bucket: 'protected',
+      },
+      {
+        regex: '^BROWSE\\.MOD11A1\\.A[\\d]{7}\\.[\\S]{6}\\.006.[\\d]{13}\\.hdf$',
+        sampleFileName: 'BROWSE.MOD11A1.A2017200.h19v04.006.2017201090724.hdf',
+        bucket: 'private',
+      },
+      {
+        regex: '^MOD11A1\\.A[\\d]{7}\\.[\\S]{6}\\.006.[\\d]{13}\\.hdf\\.met$',
+        sampleFileName: 'MOD11A1.A2017200.h19v04.006.2017201090724.hdf.met',
+        bucket: 'private',
+      },
+      {
+        regex: '^MOD11A1\\.A[\\d]{7}\\.[\\S]{6}\\.006.[\\d]{13}\\.cmr\\.xml$',
+        sampleFileName: 'MOD11A1.A2017200.h19v04.006.2017201090724.cmr.xml',
+        bucket: 'protected',
+      },
+      {
+        regex: '^MOD11A1\\.A[\\d]{7}\\.[\\S]{6}\\.006.[\\d]{13}_2\\.jpg$',
+        sampleFileName: 'MOD11A1.A2017200.h19v04.006.2017201090724_2.jpg',
+        bucket: 'public',
+      },
+      {
+        regex: '^MOD11A1\\.A[\\d]{7}\\.[\\S]{6}\\.006.[\\d]{13}_1\\.jpg$',
+        sampleFileName: 'MOD11A1.A2017200.h19v04.006.2017201090724_1.jpg',
+        bucket: 'private',
+      },
+    ],
+    url_path: targetUrlPrefix,
+    name: 'MOD11A1',
+    granuleIdExtraction: '(MOD11A1\\.(.*))\\.hdf',
+    granuleId: '^MOD11A1\\.A[\\d]{7}\\.[\\S]{6}\\.006.[\\d]{13}$',
+    dataType: 'MOD11A1',
+    process: 'modis',
+    version: '006',
+    sampleFileName: 'MOD11A1.A2017200.h19v04.006.2017201090724.hdf',
+    id: 'MOD11A1',
+  };
+  const targetCollection = {
+    files: [
+      {
+        regex: '^MOD11A1\\.A[\\d]{7}\\.[\\S]{6}\\.006.[\\d]{13}\\.hdf$',
+        sampleFileName: 'MOD11A1.A2017200.h19v04.006.2017201090724.hdf',
+        bucket: 'protected',
+      },
+      {
+        regex: '^BROWSE\\.MOD11A1\\.A[\\d]{7}\\.[\\S]{6}\\.006.[\\d]{13}\\.hdf$',
+        sampleFileName: 'BROWSE.MOD11A1.A2017200.h19v04.006.2017201090724.hdf',
+        bucket: 'private',
+      },
+      {
+        regex: '^MOD11A1\\.A[\\d]{7}\\.[\\S]{6}\\.006.[\\d]{13}\\.hdf\\.met$',
+        sampleFileName: 'MOD11A1.A2017200.h19v04.006.2017201090724.hdf.met',
+        bucket: 'private',
+      },
+      {
+        regex: '^MOD11A1\\.A[\\d]{7}\\.[\\S]{6}\\.006.[\\d]{13}\\.cmr\\.xml$',
+        sampleFileName: 'MOD11A1.A2017200.h19v04.006.2017201090724.cmr.xml',
+        bucket: 'public',
+      },
+      {
+        regex: '^MOD11A1\\.A[\\d]{7}\\.[\\S]{6}\\.006.[\\d]{13}_2\\.jpg$',
+        sampleFileName: 'MOD11A1.A2017200.h19v04.006.2017201090724_2.jpg',
+        bucket: 'public',
+      },
+      {
+        regex: '^MOD11A1\\.A[\\d]{7}\\.[\\S]{6}\\.006.[\\d]{13}_1\\.jpg$',
+        sampleFileName: 'MOD11A1.A2017200.h19v04.006.2017201090724_1.jpg',
+        bucket: 'public',
+        url_path: `${targetUrlPrefix}/jpg/example2/`,
+      },
+    ],
+    url_path: targetUrlPrefix,
+    name: 'MOD11A2',
+    granuleIdExtraction: '(MOD11A1\\.(.*))\\.hdf',
+    granuleId: '^MOD11A1\\.A[\\d]{7}\\.[\\S]{6}\\.006.[\\d]{13}$',
+    dataType: 'MOD11A2',
+    process: 'modis',
+    version: '006',
+    sampleFileName: 'MOD11A1.A2017200.h19v04.006.2017201090724.hdf',
+    id: 'MOD11A2',
+  };
+  const processGranule = {
+    status: 'completed',
+    collectionId: 'MOD11A1___006',
+    granuleId: 'MOD11A1.A2017200.h19v04.006.2017201090724',
+    files: [
+      {
+        key: `${sourceUrlPrefix}/MOD11A1.A2017200.h19v04.006.2017201090724.hdf`,
+        bucket: 'cumulus-test-sandbox-protected',
+        type: 'data',
+        fileName: 'MOD11A1.A2017200.h19v04.006.2017201090724.hdf',
+      },
+      {
+        key: `${sourceUrlPrefix}/MOD11A1.A2017200.h19v04.006.2017201090724_1.jpg`,
+        bucket: 'cumulus-test-sandbox-private',
+        type: 'browse',
+        fileName: 'MOD11A1.A2017200.h19v04.006.2017201090724_1.jpg',
+      },
+      {
+        key: `${sourceUrlPrefix}/MOD11A1.A2017200.h19v04.006.2017201090724_2.jpg`,
+        bucket: 'cumulus-test-sandbox-public',
+        type: 'browse',
+        fileName: 'MOD11A1.A2017200.h19v04.006.2017201090724_2.jpg',
+      },
+      {
+        key: `${sourceUrlPrefix}/MOD11A1.A2017200.h19v04.006.2017201090724.cmr.xml`,
+        bucket: 'cumulus-test-sandbox-protected',
+        type: 'metadata',
+        fileName: 'MOD11A1.A2017200.h19v04.006.2017201090724.cmr.xml',
+      },
+    ],
+  };
   // let systemBucket;
   beforeAll(async () => {
     const config = await loadConfig();
     stackName = config.stackName;
-    
-    // systemBucket = config.bucket;
   });
 
   describe('under normal circumstances', () => {
@@ -38,73 +155,28 @@ describe('when moveGranulesCollection is called', () => {
       )));
     });
     beforeAll(async () => {
-      const sourceUrlPrefix = `source_path/${uuidv4()}`;
-      const targetUrlPrefix = `target_path/${uuidv4()}`;
       finalFiles = [
         {
           bucket: 'cumulus-test-sandbox-protected',
-          prefix: `${targetUrlPrefix}/MOD11A1.A2017200.h19v04.006.2017201090724.hdf`,
+          key: `${targetUrlPrefix}/MOD11A1.A2017200.h19v04.006.2017201090724.hdf`,
         },
         {
           bucket: 'cumulus-test-sandbox-public',
-          prefix: `${targetUrlPrefix}/jpg/example2/MOD11A1.A2017200.h19v04.006.2017201090724_1.jpg`,
+          key: `${targetUrlPrefix}/jpg/example2/MOD11A1.A2017200.h19v04.006.2017201090724_1.jpg`,
         },
         {
           bucket: 'cumulus-test-sandbox-public',
-          prefix: `${targetUrlPrefix}/MOD11A1.A2017200.h19v04.006.2017201090724_2.jpg`,
+          key: `${targetUrlPrefix}/MOD11A1.A2017200.h19v04.006.2017201090724_2.jpg`,
         },
         {
           bucket: 'cumulus-test-sandbox-public',
-          prefix: `${targetUrlPrefix}/MOD11A1.A2017200.h19v04.006.2017201090724.cmr.xml`,
+          key: `${targetUrlPrefix}/MOD11A1.A2017200.h19v04.006.2017201090724.cmr.xml`,
         },
       ];
 
       const payload = {
         meta: {
-          collection: {
-            files: [
-              {
-                regex: '^MOD11A1\\.A[\\d]{7}\\.[\\S]{6}\\.006.[\\d]{13}\\.hdf$',
-                sampleFileName: 'MOD11A1.A2017200.h19v04.006.2017201090724.hdf',
-                bucket: 'protected',
-              },
-              {
-                regex: '^BROWSE\\.MOD11A1\\.A[\\d]{7}\\.[\\S]{6}\\.006.[\\d]{13}\\.hdf$',
-                sampleFileName: 'BROWSE.MOD11A1.A2017200.h19v04.006.2017201090724.hdf',
-                bucket: 'private',
-              },
-              {
-                regex: '^MOD11A1\\.A[\\d]{7}\\.[\\S]{6}\\.006.[\\d]{13}\\.hdf\\.met$',
-                sampleFileName: 'MOD11A1.A2017200.h19v04.006.2017201090724.hdf.met',
-                bucket: 'private',
-              },
-              {
-                regex: '^MOD11A1\\.A[\\d]{7}\\.[\\S]{6}\\.006.[\\d]{13}\\.cmr\\.xml$',
-                sampleFileName: 'MOD11A1.A2017200.h19v04.006.2017201090724.cmr.xml',
-                bucket: 'public',
-              },
-              {
-                regex: '^MOD11A1\\.A[\\d]{7}\\.[\\S]{6}\\.006.[\\d]{13}_2\\.jpg$',
-                sampleFileName: 'MOD11A1.A2017200.h19v04.006.2017201090724_2.jpg',
-                bucket: 'public',
-              },
-              {
-                regex: '^MOD11A1\\.A[\\d]{7}\\.[\\S]{6}\\.006.[\\d]{13}_1\\.jpg$',
-                sampleFileName: 'MOD11A1.A2017200.h19v04.006.2017201090724_1.jpg',
-                bucket: 'public',
-                url_path: `${targetUrlPrefix}/jpg/example2/`,
-              },
-            ],
-            url_path: targetUrlPrefix,
-            name: 'MOD11A2',
-            granuleIdExtraction: '(MOD11A1\\.(.*))\\.hdf',
-            granuleId: '^MOD11A1\\.A[\\d]{7}\\.[\\S]{6}\\.006.[\\d]{13}$',
-            dataType: 'MOD11A2',
-            process: 'modis',
-            version: '006',
-            sampleFileName: 'MOD11A1.A2017200.h19v04.006.2017201090724.hdf',
-            id: 'MOD11A2',
-          },
+          collection: targetCollection,
           buckets: {
             internal: {
               type: 'cumulus-test-sandbox-internal',
@@ -130,113 +202,43 @@ describe('when moveGranulesCollection is called', () => {
         },
         input: {
           granules: [
-            {
-              status: 'completed',
-              collectionId: 'MOD11A1___006',
-              granuleId: 'MOD11A1.A2017200.h19v04.006.2017201090724',
-              files: [
-                {
-                  key: `${sourceUrlPrefix}/MOD11A1.A2017200.h19v04.006.2017201090724.hdf`,
-                  bucket: 'cumulus-test-sandbox-protected',
-                  type: 'data',
-                  fileName: 'MOD11A1.A2017200.h19v04.006.2017201090724.hdf',
-                },
-                {
-                  key: `${sourceUrlPrefix}/MOD11A1.A2017200.h19v04.006.2017201090724_1.jpg`,
-                  bucket: 'cumulus-test-sandbox-private',
-                  type: 'browse',
-                  fileName: 'MOD11A1.A2017200.h19v04.006.2017201090724_1.jpg',
-                },
-                {
-                  key: `${sourceUrlPrefix}/MOD11A1.A2017200.h19v04.006.2017201090724_2.jpg`,
-                  bucket: 'cumulus-test-sandbox-public',
-                  type: 'browse',
-                  fileName: 'MOD11A1.A2017200.h19v04.006.2017201090724_2.jpg',
-                },
-                {
-                  key: `${sourceUrlPrefix}/MOD11A1.A2017200.h19v04.006.2017201090724.cmr.xml`,
-                  bucket: 'cumulus-test-sandbox-protected',
-                  type: 'metadata',
-                  fileName: 'MOD11A1.A2017200.h19v04.006.2017201090724.cmr.xml',
-                },
-              ],
-            },
+            processGranule,
           ],
         },
       };
-      const originalCollection = {
-        files: [
-          {
-            regex: '^MOD11A1\\.A[\\d]{7}\\.[\\S]{6}\\.006.[\\d]{13}\\.hdf$',
-            sampleFileName: 'MOD11A1.A2017200.h19v04.006.2017201090724.hdf',
-            bucket: 'protected',
-          },
-          {
-            regex: '^BROWSE\\.MOD11A1\\.A[\\d]{7}\\.[\\S]{6}\\.006.[\\d]{13}\\.hdf$',
-            sampleFileName: 'BROWSE.MOD11A1.A2017200.h19v04.006.2017201090724.hdf',
-            bucket: 'private',
-          },
-          {
-            regex: '^MOD11A1\\.A[\\d]{7}\\.[\\S]{6}\\.006.[\\d]{13}\\.hdf\\.met$',
-            sampleFileName: 'MOD11A1.A2017200.h19v04.006.2017201090724.hdf.met',
-            bucket: 'private',
-          },
-          {
-            regex: '^MOD11A1\\.A[\\d]{7}\\.[\\S]{6}\\.006.[\\d]{13}\\.cmr\\.xml$',
-            sampleFileName: 'MOD11A1.A2017200.h19v04.006.2017201090724.cmr.xml',
-            bucket: 'protected',
-          },
-          {
-            regex: '^MOD11A1\\.A[\\d]{7}\\.[\\S]{6}\\.006.[\\d]{13}_2\\.jpg$',
-            sampleFileName: 'MOD11A1.A2017200.h19v04.006.2017201090724_2.jpg',
-            bucket: 'public',
-          },
-          {
-            regex: '^MOD11A1\\.A[\\d]{7}\\.[\\S]{6}\\.006.[\\d]{13}_1\\.jpg$',
-            sampleFileName: 'MOD11A1.A2017200.h19v04.006.2017201090724_1.jpg',
-            bucket: 'private',
-          },
-        ],
-        url_path: targetUrlPrefix,
-        name: 'MOD11A1',
-        granuleIdExtraction: '(MOD11A1\\.(.*))\\.hdf',
-        granuleId: '^MOD11A1\\.A[\\d]{7}\\.[\\S]{6}\\.006.[\\d]{13}$',
-        dataType: 'MOD11A1',
-        process: 'modis',
-        version: '006',
-        sampleFileName: 'MOD11A1.A2017200.h19v04.006.2017201090724.hdf',
-        id: 'MOD11A1',
-      }
-      //upload to pg
+      //upload to cumulus
       try {
         try {
           await collections.createCollection({
             prefix: stackName,
             collection: originalCollection,
-          })
-        } catch {}
+          });
+        } catch {
+          console.log(`collection ${constructCollectionId(originalCollection.name, originalCollection.version)} already exists`);
+        }
         try {
           await collections.createCollection({
             prefix: stackName,
-            collection: payload.meta.collection,
-          })
-        } catch {}
-        Promise.all(payload.input.granules.map(async (granule) => {
-          try {
-            await granules.createGranule({
-              prefix: stackName,
-              body: granule,
-            })
-          } catch {}
-        }))
-        await Promise.all(payload.input.granules[0].files.map(async (file) => {
+            collection: targetCollection,
+          });
+        } catch {
+          console.log(`collection ${constructCollectionId(targetCollection.name, targetCollection.version)} already exists`);
+        }
+        try {
+          await granules.createGranule({
+            prefix: stackName,
+            body: processGranule,
+          });
+        } catch {
+          console.log(`granule ${processGranule.granuleId} already exists`);
+        }
+        await Promise.all(processGranule.files.map(async (file) => {
           let body;
           if (file.type === 'metadata') {
             body = fs.createReadStream(path.join(__dirname, 'data/meta.xml'));
           } else {
             body = file.key;
           }
-          console.log('about to use file', file)
           await promiseS3Upload({
             params: {
               Bucket: file.bucket,
@@ -266,7 +268,8 @@ describe('when moveGranulesCollection is called', () => {
 
         await Promise.all(finalFiles.map((file) => expectAsync(
           waitForListObjectsV2ResultCount({
-            ...file,
+            bucket: file.bucket,
+            prefix: file.key,
             desiredCount: 1,
             interval: 5 * 1000,
             timeout: 30 * 1000,
@@ -280,15 +283,14 @@ describe('when moveGranulesCollection is called', () => {
     it('moves the granule data in s3', () => {
       if (beforeAllFailed) fail('beforeAllFailed');
     });
-    it('updates the granule data in postgres', () => {
+    it('updates the granule data in cumulus', async () => {
       if (beforeAllFailed) fail('beforeAllFailed');
-      // const knex = await getKnexClient();
-      // const granuleModel = new GranulePgModel();
-      // const finalPgGranule = await granuleModel.get(knex, {
-      //   cumulus_id: pgRecords.granules[0].cumulus_id,
-      // });
-      // t.true(finalPgGranule.granule_id === pgRecords.granules[0].granule_id);
-      // t.true(finalPgGranule.collection_cumulus_id === pgRecords.targetCollection.cumulus_id);
+      const cumulusGranule = await granules.getGranule({
+        prefix: stackName,
+        granuleId: processGranule.granuleId,
+      });
+      expect(cumulusGranule.granuleId).toEqual(processGranule.granuleId);
+      expect(cumulusGranule.collectionId).toEqual(constructCollectionId(targetCollection.name, targetCollection.version));
     });
   });
 });
