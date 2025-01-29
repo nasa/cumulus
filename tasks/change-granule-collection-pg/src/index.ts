8f86897a3160c5541396fc61af211fbfa6c3b32c
etcart <amberhosen@gmail.com>
2025-01-29 11:44:05 -0500

    more cleanup

--- tasks/change-granule-collection-pg/src/index.ts
+++ tasks/change-granule-collection-pg/src/index.ts
@@ -13,7 +13,6 @@ import { BucketsConfigObject } from '@cumulus/common/types';
 import { bulkPatch, bulkPatchGranuleCollection, getGranule } from '@cumulus/api-client/granules';
 import { getRequiredEnvVar } from '@cumulus/common/env';
 
-const MB = 1024 * 1024;
 
 interface EventConfig {
   targetCollection: CollectionRecord
@@ -28,10 +27,6 @@ interface EventConfig {
     version: string,
   },
   buckets: BucketsConfigObject,
-  s3MultipartChunksizeMb?: number,
-  distribution_endpoint: string,
-  cmrGranuleUrlType: string,
-  invalidBehavior: string,
 }
 
 interface MoveGranuleCollectionsEvent {
@@ -51,7 +46,6 @@ function getConcurrency() {
 }
 
 async function moveGranulesInCumulusDatastores(
-  sourceGranules: Array<ApiGranuleRecord>,
   targetGranules: Array<ApiGranuleRecord>,
   sourceCollectionId: string,
   targetCollectionId: string
@@ -72,70 +66,33 @@ async function moveGranulesInCumulusDatastores(
   await bulkPatchGranuleCollection({
     prefix: getRequiredEnvVar('stackName'),
     body: {
-      apiGranules: sourceGranules,
+      apiGranules: updatedBodyGranules,
       collectionId: targetCollectionId,
       esConcurrency: getConcurrency(),
     },
   });
 }
 
-/**
- * Move all files in a collection of granules from staging location to final location,
- * and update granule files to include renamed files if any.
- */
-async function moveFilesForAllGranules({
-  sourceGranules,
-  targetGranules,
-  sourceCollectionId,
-  targetCollectionId,
-}: {
-  sourceGranules: Array<ApiGranuleRecord>,
-  targetGranules: Array<ApiGranuleRecord>,
-  sourceCollectionId: string,
-  targetCollectionId: string,
-}): Promise<void> {
-  // update postgres (or other cumulus datastores if applicable)
-  // because postgres might be our source of ground truth in the future, it must be updated *last*
-  await moveGranulesInCumulusDatastores(
-    sourceGranules,
-    targetGranules,
-    sourceCollectionId,
-    targetCollectionId,
-  );
-}
-
 async function moveGranules(event: MoveGranuleCollectionsEvent): Promise<Object> {
   const config = event.config;
-  const s3MultipartChunksizeMb = config.s3MultipartChunksizeMb
-    ? config.s3MultipartChunksizeMb : Number(process.env.default_s3_multipart_chunksize_mb);
-
   const granuleMetadataFileExtension: string = get(
     config,
     'collection.meta.granuleMetadataFileExtension'
   );
 
-  log.debug(`moveGranules config: s3MultipartChunksizeMb: ${s3MultipartChunksizeMb}, `
-    + `granuleMetadataFileExtension ${granuleMetadataFileExtension}, `
-    + `granuleIds ${event.input.granules}, `
-    + `meta ${event.config}`);
+  log.debug(`moveGranules config: s3MultipartChunksizeMb, `
+    + `granuleMetadataFileExtension ${granuleMetadataFileExtension}, `);
 
   const targetGranules = event.input.granules;
-  const sourceGranules = await Promise.all(targetGranules.map(async (granule) => await getGranule({
-    prefix: getRequiredEnvVar('stack_name'),
-    granuleId: granule.granuleId
-  })))
 
-  // const updatedCmrMetadata = updateCMRMetadata()
-  // Move files from staging location to final location
-  await moveFilesForAllGranules({
-    sourceGranules,
+  await moveGranulesInCumulusDatastores(
     targetGranules,
-    sourceCollectionId: constructCollectionId(config.collection.name, config.collection.version),
-    targetCollectionId: constructCollectionId(
+    constructCollectionId(config.collection.name, config.collection.version),
+    constructCollectionId(
       config.targetCollection.name,
       config.targetCollection.version
     ),
-  });
+  );
 
   return {
     granules: targetGranules,
