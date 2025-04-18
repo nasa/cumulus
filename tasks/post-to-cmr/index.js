70586849b2406d391e7be3a265140cdbb9827a82
etcart <amberhosen@gmail.com>
2025-04-18 11:50:24 -0400

    un-fake the cmr interface again

--- tasks/post-to-cmr/index.js
+++ tasks/post-to-cmr/index.js
@@ -102,11 +102,11 @@ function checkForMetadata(granules, cmrFiles) {
  */
 async function removeGranuleFromCmr({ granules, cmrSettings, concurrency }) {
   const granulesToUnpublish = granules.filter((granule) => granule.published || !!granule.cmrLink);
-  // await pMap(
-  //   granulesToUnpublish,
-  //   (granule) => removeFromCMR(granule.granuleId, cmrSettings),
-  //   { concurrency }
-  // );
+  await pMap(
+    granulesToUnpublish,
+    (granule) => removeFromCMR(granule.granuleId, cmrSettings),
+    { concurrency }
+  );
 
   if (granulesToUnpublish.length > 0) {
     log.info(`Removing ${granulesToUnpublish.length} out of ${granules.length} granules from CMR for republishing`);
@@ -161,12 +161,11 @@ async function postToCMR(event) {
   const startTime = Date.now();
 
   // post all meta files to CMR
-  // const results = await pMap(
-  //   updatedCMRFiles,
-  //   (cmrFile) => publish2CMR(cmrFile, cmrSettings, cmrRevisionId),
-  //   { concurrency }
-  // );
-  const results = {};
+  const results = await pMap(
+    updatedCMRFiles,
+    (cmrFile) => publish2CMR(cmrFile, cmrSettings, cmrRevisionId),
+    { concurrency }
+  );
   const endTime = Date.now();
   const outputGranules = buildOutput(
     results,
