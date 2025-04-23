f007b37554331f1e80770103567e242d1fbdbac6
etcart <amberhosen@gmail.com>
2025-04-23 16:58:27 -0400

    exclude coudflare:sockets

--- lambdas/db-migration/webpack.config.js
+++ lambdas/db-migration/webpack.config.js
@@ -3,6 +3,7 @@ const { IgnorePlugin } = require('webpack');
 const CopyPlugin = require('copy-webpack-plugin');
 
 const ignoredPackages = [
+  'cloudflare:sockets',
   'better-sqlite3',
   'mssql',
   'mssql/lib/base',
@@ -14,7 +15,6 @@ const ignoredPackages = [
   'pg-query-stream',
   'sqlite3',
   'tedious',
-  'cloudflare:sockets'
 ];
 
 module.exports = {
