58dfc83e1b6c96c8fa8d3b39047f52a4dbcf335c
Jonathan Kovarik <Jkovarik@users.noreply.github.com>
2025-05-13 08:31:07 -0600

    Seperate out workflows

--- example/cumulus-tf/discover_and_queue_pdrs_unique_workflow.tf
+++ example/cumulus-tf/discover_and_queue_pdrs_unique_workflow.tf
@@ -1,8 +1,8 @@
-module "discover_and_queue_pdrs_workflow" {
+module "discover_and_queue_pdrs_unique_workflow" {
   source = "../../tf-modules/workflow"
 
   prefix          = var.prefix
-  name            = "DiscoverAndQueuePdrs"
+  name            = "DiscoverAndQueuePdrsUnique"
   workflow_config = module.cumulus.workflow_config
   system_bucket   = var.system_bucket
   tags            = local.tags
@@ -11,7 +11,7 @@ module "discover_and_queue_pdrs_workflow" {
     "${path.module}/discover_and_queue_pdrs_workflow.asl.json",
     {
       discover_pdrs_task_arn: module.cumulus.discover_pdrs_task.task_arn,
-      parse_pdr_workflow_name: module.parse_pdr_workflow.name,
+      parse_pdr_workflow_name: module.parse_pdr_workflow_unique.name,
       queue_pdrs_task_arn: module.cumulus.queue_pdrs_task.task_arn,
       start_sf_queue_url: module.cumulus.start_sf_queue_url
     }
