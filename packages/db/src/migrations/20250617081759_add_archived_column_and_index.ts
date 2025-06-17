8f538252cce8e640f1bf4f4aed68401e47616025
etcart <amberhosen@gmail.com>
2025-06-17 12:10:18 -0400

    spaces not tabs

--- packages/db/src/migrations/20250617081759_add_archived_column_and_index.ts
+++ packages/db/src/migrations/20250617081759_add_archived_column_and_index.ts
@@ -1,27 +1,27 @@
 import { Knex } from 'knex';
 
 export const up = async (knex: Knex): Promise<void> => {
-	await knex.schema.table('granules', (table) => {
-		table.boolean('archived')
-			.comment('granule has been "archived"')
-	});
-	await knex.schema.table('executions', (table) => {
-		table.boolean('archived')
-			.comment('execution has been "archived"')
-	});
-	await knex.raw('CREATE INDEX CONCURRENTLY IF NOT EXISTS executions_archived_index ON executions (archived)');
+  await knex.schema.table('granules', (table) => {
+    table.boolean('archived')
+      .comment('granule has been "archived"')
+  });
+  await knex.schema.table('executions', (table) => {
+    table.boolean('archived')
+      .comment('execution has been "archived"')
+  });
+  await knex.raw('CREATE INDEX CONCURRENTLY IF NOT EXISTS executions_archived_index ON executions (archived)');
 };
 
 export const down = async (knex: Knex): Promise<void> => {
-	await knex.raw('DROP INDEX CONCURRENTLY IF EXISTS granules_archived_index');
-	await knex.raw('DROP INDEX CONCURRENTLY IF EXISTS executions_archived_index');
-	await knex.schema.table('granules', (table) => {
-		table.dropColumn('archived');
-	});
-	await knex.schema.table('executions', (table) => {
-		table.dropColumn('archived');
-	});
+  await knex.raw('DROP INDEX CONCURRENTLY IF EXISTS granules_archived_index');
+  await knex.raw('DROP INDEX CONCURRENTLY IF EXISTS executions_archived_index');
+  await knex.schema.table('granules', (table) => {
+    table.dropColumn('archived');
+  });
+  await knex.schema.table('executions', (table) => {
+    table.dropColumn('archived');
+  });
 };
 exports.config = {
-	transaction: false,
+  transaction: false,
 };
