076416d5f736646c4ead6ba703357600f907ba80
etcart <amberhosen@gmail.com>
2025-06-17 12:12:39 -0400

    linting

--- packages/db/src/migrations/20250617081759_add_archived_column_and_index.ts
+++ packages/db/src/migrations/20250617081759_add_archived_column_and_index.ts
@@ -3,11 +3,11 @@ import { Knex } from 'knex';
 export const up = async (knex: Knex): Promise<void> => {
   await knex.schema.table('granules', (table) => {
     table.boolean('archived')
-      .comment('granule has been "archived"')
+      .comment('granule has been "archived"');
   });
   await knex.schema.table('executions', (table) => {
     table.boolean('archived')
-      .comment('execution has been "archived"')
+      .comment('execution has been "archived"');
   });
   await knex.raw('CREATE INDEX CONCURRENTLY IF NOT EXISTS executions_archived_index ON executions (archived)');
 };
