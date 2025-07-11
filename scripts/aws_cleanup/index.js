793725eb376b75afee2555db669a01cefa092dd9
etcart <amberhosen@gmail.com>
2025-07-11 13:29:36 -0400

    tests

--- scripts/aws_cleanup/index.js
+++ scripts/aws_cleanup/index.js
@@ -5,28 +5,27 @@ const { ec2 } = require('@cumulus/aws-client/services');
 const shouldBeCleanedUp = (instanceObject, todayFunc) => {
   const timeoutKey = process.env.timeout_key || 'Rotate By';
   return instanceObject.Tags.reduce((ret, tag) => {
-    if (tag.Key === timeoutKey && moment(tag.Value) < todayFunc()) {
+
+    if (tag.Key && tag.Value && tag.Key === timeoutKey && moment(tag.Value) < todayFunc()) {
       return true;
     }
     return ret;
   }, false);
 };
 
-const getInstancesToClean = async (client) => {
-  const describeResponse = await client.describeInstances();
-  const instances = describeResponse.Reservations.flatMap((reservation) => reservation.Instances);
-  return instances.filter((instance) =>
-    shouldBeCleanedUp(instance, moment)
-  ).map((instance) => instance.InstanceId);
+const getInstancesToClean = async (describeFunc, todayFunc) => {
+  const describeResponse = await describeFunc();
+  const instances = describeResponse.Reservations?.flatMap((reservation) => reservation?.Instances);
+  if (!instances) {
+    return [];
+  }
+  return instances.filter((instance) => instance && shouldBeCleanedUp(instance, todayFunc))
+    .map((instance) => instance.InstanceId);
 };
 
-const handler = async () => {
-  const client = ec2();
-  const toClean = await getInstancesToClean(client);
-  if (toClean.length) {
-    const termination = await client.terminateInstances({
-      InstanceIds: toClean,
-    });
+const terminateInstances = async(toClean, terminateFunc) => {
+  if (toClean.length > 0) {
+    const termination = await terminateFunc();
     return {
       statusCode: 200,
       message: `termination completed with response ${JSON.stringify(termination)}`,
@@ -34,21 +33,19 @@ const handler = async () => {
   }
   return {
     statusCode: 200,
-    message: `termination completed with no instances out of date`,
+    message: 'termination completed with no instances out of date',
   };
+}
+
+const handler = async () => {
+  const client = ec2();
+  const toClean = await getInstancesToClean(client.describeInstances, moment);
+  return terminateInstances(toClean, (toClean) => client.terminateInstances({InstanceIds: toClean}));
 };
 
 module.exports = {
   shouldBeCleanedUp,
+  getInstancesToClean,
+  terminateInstances,
   handler
-}
-
-if (require.main === module) {
-  handler(
-  ).then(
-    (ret) => ret
-  ).catch((error) => {
-    console.log(`failed: ${error}`);
-    throw error;
-  });
-}
+};
--- scripts/aws_cleanup/tests/test-index.js
+++ scripts/aws_cleanup/tests/test-index.js
@@ -1,6 +1,6 @@
 const test = require('ava');
 const moment = require('moment');
-const { shouldBeCleanedUp, handler } = require('..')
+const { shouldBeCleanedUp, getInstancesToClean, terminateInstances } = require('..');
 
 test('shouldBeCleanedUp returns false if date is not passed', (t) => {
   t.false(shouldBeCleanedUp(
@@ -43,7 +43,7 @@ test.serial('shouldBeCleanedUp respects "timeout_key" variable', (t) => {
     },
     () => moment('2001-02-03')
   ));
-  process.env['timeout_key'] = 'Other Stuff';
+  process.env.timeout_key = 'Other Stuff';
   t.true(shouldBeCleanedUp(
     {
       Tags: [
@@ -60,7 +60,7 @@ test.serial('shouldBeCleanedUp respects "timeout_key" variable', (t) => {
     () => moment('2001-02-03')
   ));
 
-  delete process.env['timeout_key'];
+  delete process.env.timeout_key;
 });
 
 test('shouldBeCleanedUp returns true if date is passed', (t) => {
@@ -112,3 +112,175 @@ test('shouldBeCleanedUp returns false if there is no expiration date key', (t) =
     () => moment('2001-02-03')
   ));
 });
+
+
+test('getInstancesToClean returns a list of expired InstanceIds', async (t) => {
+  const instances = await getInstancesToClean(
+    () => ({
+      Reservations: [
+        {
+          Instances: [
+            {
+              InstanceId: '1',
+              Tags: [
+                {
+                  Key: 'Rotate By',
+                  Value: '2001-03-05',
+                },
+                {
+                  Key: 'abc',
+                  Value: 'abcd',
+                }
+              ]
+            },
+            {
+              InstanceId: '2',
+              Tags: [
+                {
+                  Key: 'Rotate By',
+                  Value: '2001-01-05',
+                }
+              ]
+            }
+          ]
+        },
+        {
+          Instances: [
+            {
+              InstanceId: '3',
+              Tags: []
+            },
+            {
+              InstanceId: '4',
+              Tags: [
+                {
+                  Key: 'Rotate By',
+                  Value: '2001-01-05',
+                }
+              ]
+            }
+          ]
+        }
+      ]
+    }),
+    () => moment('2001-02-03')
+  );
+  t.deepEqual(instances, ['2', '4'])
+});
+
+
+test('getInstancesToClean gracefully handles mangled objects', async (t) => {
+  const instances1 = await getInstancesToClean(
+    () => ({
+      abc: [
+        {
+          Instances: [
+            {
+              InstanceId: '1',
+              Tags: [
+                {
+                  Key: 'Rotate By',
+                  Value: '2001-03-05',
+                },
+                {
+                  Key: 'abc',
+                  Value: 'abcd',
+                }
+              ]
+            },
+            {
+              InstanceId: '2',
+              Tags: [
+                {
+                  Key: 'Rotate By',
+                  Value: '2001-01-05',
+                }
+              ]
+            }
+          ]
+        },
+      ]
+    }),
+    () => moment('2001-02-03')
+  );
+  t.deepEqual(instances1, []);
+  const instances2 = await getInstancesToClean(
+    () => ({
+      Reservations: [
+        {
+          acbc: [
+            {
+              InstanceId: '1',
+              Tags: [
+                {
+                  Key: 'Rotate By',
+                  Value: '2001-03-05',
+                },
+                {
+                  Key: 'abc',
+                  Value: 'abcd',
+                }
+              ]
+            },
+            {
+              InstanceId: '2',
+              Tags: [
+                {
+                  Key: 'Rotate By',
+                  Value: '2001-01-05',
+                }
+              ]
+            }
+          ]
+        },
+      ]
+    }),
+    () => moment('2001-02-03')
+  );
+  t.deepEqual(instances2, [])
+  const instances3 = await getInstancesToClean(
+    () => ({
+      Reservations: [
+        {
+          Instances: [
+            {
+              InstanceId: '1',
+              Tags: [
+                {
+                  av: 'Rotate By',
+                  Value: '2001-03-05',
+                },
+                {
+                  Key: 'abc',
+                  Value: 'abcd',
+                }
+              ]
+            },
+            {
+              InstanceId: '2',
+              Tags: [
+                {
+                  Key: 'Rotate By',
+                  Value: '2001-01-05',
+                }
+              ]
+            }
+          ]
+        },
+      ]
+    }),
+    () => moment('2001-02-03')
+  );
+  t.deepEqual(instances3, ['2'])
+});
+
+test('terminateInstances runs on valid list of instanceIds', async (t) => {
+  t.deepEqual(
+    await terminateInstances([], () => {}),
+    { statusCode: 200, message: 'termination completed with no instances out of date'}
+  );
+  t.like(
+    await terminateInstances(['abc'], () => {}),
+    { statusCode: 200 }
+  );
+})
