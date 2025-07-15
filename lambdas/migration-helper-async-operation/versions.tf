2d6a11a175994e69e716acef84bb81d21e1ce86e
Jonathan Kovarik <Jkovarik@users.noreply.github.com>
2025-07-15 09:42:46 -0600

    Update AWS provider bounds

--- lambdas/migration-helper-async-operation/versions.tf
+++ lambdas/migration-helper-async-operation/versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     aws = {
       source = "hashicorp/aws"
-      version = ">= 5.100"
+      version = ">= 5.100, < 6.0.0"
     }
   }
   required_version = ">= 1.12"
