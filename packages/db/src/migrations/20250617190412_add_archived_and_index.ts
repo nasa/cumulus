defdf298d5350a46e9c59038afef57f7c5b88c13
etcart <amberhosen@gmail.com>
2025-08-06 13:45:17 -0400

    archived notNullable

--- packages/db/src/migrations/20250617190412_add_archived_and_index.ts
+++ packages/db/src/migrations/20250617190412_add_archived_and_index.ts
@@ -5,7 +5,8 @@ export const up = async (knex: Knex): Promise<void> => {
     await knex.schema.table('granules', (table) => {
       table.boolean('archived')
         .comment('granule has been "archived"')
-        .defaultTo('false');
+        .defaultTo('false')
+        .notNullable();
     });
   }
 
@@ -13,7 +14,8 @@ export const up = async (knex: Knex): Promise<void> => {
     await knex.schema.table('executions', (table) => {
       table.boolean('archived')
         .comment('execution has been "archived"')
-        .defaultTo('false');
+        .defaultTo('false')
+        .notNullable();
     });
   }
   await knex.raw('CREATE INDEX CONCURRENTLY IF NOT EXISTS executions_archived_index ON executions (archived)');
