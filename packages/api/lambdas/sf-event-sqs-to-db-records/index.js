570c6e9410ee27e8c662a71082df17cab6686c24
jennyhliu <jenny.h.liu@nasa.gov>
2025-08-26 10:17:55 -0400

    check executionCumulusId && !shouldWriteGranuleRecords

--- packages/api/lambdas/sf-event-sqs-to-db-records/index.js
+++ packages/api/lambdas/sf-event-sqs-to-db-records/index.js
@@ -207,7 +207,7 @@ const writeRecords = async ({
     });
   }
 
-  if (shouldWriteExecutionRecords && !shouldWriteGranuleRecords && executionCumulusId) {
+  if (executionCumulusId && !shouldWriteGranuleRecords) {
     return writeGranuleExecutionAssociationsFromMessage({
       cumulusMessage,
       executionCumulusId,
