68ea5e197b0849587f82ff6555cd4b88838f8315
jennyhliu <jenny.h.liu@nasa.gov>
2025-09-11 10:59:12 -0400

    remove some style change

--- example/cumulus-tf/variables.tf
+++ example/cumulus-tf/variables.tf
@@ -26,8 +26,8 @@ variable "bucket_map_key" {
 }
 
 variable "cumulus_message_adapter_lambda_layer_version_arn" {
-  description = "Layer version ARN of the Lambda layer for the Cumulus Message Adapter"
   type        = string
+  description = "Layer version ARN of the Lambda layer for the Cumulus Message Adapter"
 }
 
 variable "cmr_oauth_provider" {
@@ -36,18 +36,18 @@ variable "cmr_oauth_provider" {
 }
 
 variable "csdap_client_id" {
-  description = "The csdap client id"
   type        = string
+  description = "The csdap client id"
 }
 
 variable "csdap_client_password" {
-  description = "The csdap client password"
   type        = string
+  description = "The csdap client password"
 }
 
 variable "csdap_host_url" {
-  description = "The csdap host url"
   type        = string
+  description = "The csdap host url"
 }
 
 variable "launchpad_api" {
@@ -107,9 +107,9 @@ variable "data_persistence_remote_state_config" {
 }
 
 variable "s3_replicator_config" {
-  description = "Configuration for the s3-replicator module. Items with prefix of source_prefix in the source_bucket will be replicated to the target_bucket with target_prefix."
   type        = object({ source_bucket = string, source_prefix = string, target_bucket = string, target_prefix = string, target_region = optional(string) })
   default     = null
+  description = "Configuration for the s3-replicator module. Items with prefix of source_prefix in the source_bucket will be replicated to the target_bucket with target_prefix."
 }
 
 variable "prefix" {
@@ -160,21 +160,21 @@ variable "vpc_id" {
 }
 
 variable "api_gateway_stage" {
-  description = "The archive API Gateway stage to create"
   type        = string
   default     = "dev"
+  description = "The archive API Gateway stage to create"
 }
 
 variable "ftp_host_configuration_bucket" {
-  description = "Bucket containing ftp test host configuration"
   type = string
   default = "cumulus-test-sandbox-internal"
+  description = "Bucket containing ftp test host configuration"
 }
 
 variable "api_reserved_concurrency" {
-  description = "Archive API Lambda reserved concurrency"
   type = number
   default = 5
+  description = "Archive API Lambda reserved concurrency"
 }
 
 variable "buckets" {
@@ -189,9 +189,9 @@ variable "cmr_search_client_config" {
 }
 
 variable "cumulus_distribution_url" {
-  description = "The url of cumulus distribution API Gateway endpoint"
   type        = string
   default     = null
+  description = "The url of cumulus distribution API Gateway endpoint"
 }
 
 variable "default_s3_multipart_chunksize_mb" {
@@ -241,15 +241,15 @@ variable "lambda_subnet_ids" {
 }
 
 variable "log_api_gateway_to_cloudwatch" {
-  description = "Enable logging of API Gateway activity to CloudWatch."
   type        = bool
   default     = false
+  description = "Enable logging of API Gateway activity to CloudWatch."
 }
 
 variable "log_destination_arn" {
-  description = "Remote kinesis/destination arn for delivering logs."
   type        = string
   default     = null
+  description = "Remote kinesis/destination arn for delivering logs."
 }
 
 variable "archive_api_port" {
@@ -258,9 +258,9 @@ variable "archive_api_port" {
 }
 
 variable "archive_api_url" {
-  description = "If not specified, the value of the Backend (Archive) API Gateway endpoint is used"
   type        = string
   default     = null
+  description = "If not specified, the value of the Backend (Archive) API Gateway endpoint is used"
 }
 
 variable "private_archive_api_gateway" {
@@ -276,8 +276,8 @@ variable "thin_egress_jwt_secret_name" {
 
 variable "metrics_es_host" {
   description = "Domain name (not URL) of the Cloud Metrics API."
-  type        = string
-  default     = null
+  type    = string
+  default = null
 }
 
 variable "metrics_es_password" {
@@ -302,15 +302,15 @@ variable "tags" {
 }
 
 variable "pdr_node_name_provider_bucket" {
+  type = string
   description = "The name of the common bucket used as an S3 provider for PDR NODE_NAME tests"
-  type        = string
-  default     = "cumulus-sandbox-pdr-node-name-provider"
+  default = "cumulus-sandbox-pdr-node-name-provider"
 }
 
 variable "rds_connection_timing_configuration" {
   description = "Cumulus rds connection timeout retry timing object -- these values map to knex.js's internal use of  https://github.com/vincit/tarn.js/ for connection acquisition"
-  type        = map(number)
-  default     = {
+  type = map(number)
+  default = {
       acquireTimeoutMillis: 90000
       createRetryIntervalMillis: 30000,
       createTimeoutMillis: 20000,
@@ -326,81 +326,81 @@ variable "rds_admin_access_secret_arn" {
 
 variable "async_operation_image_version" {
   description = "docker image version to use for Cumulus async operations tasks"
-  type        = string
-  default     = "53"
+  type = string
+  default = "53"
 }
 
 variable "cumulus_process_activity_version" {
     description = "docker image version to use for python processing service"
-    type        = string
-    default     = "4"
+    type = string
+    default = "4"
 }
 
 variable "ecs_task_image_version" {
   description = "docker image version to use for Cumulus hello world task"
-    type      = string
-    default   = "2.1.0"
+    type = string
+    default = "2.1.0"
 }
 
 variable "cumulus_test_ingest_image_version" {
     description = "docker image version to use for python test ingest processing service"
-    type        = string
-    default     = "17"
+    type = string
+    default = "17"
 }
 variable "ecs_custom_sg_ids" {
   description = "User defined security groups to add to the Core ECS cluster"
-  type        = list(string)
-  default     = []
+  type = list(string)
+  default = []
 }
 
 ## ORCA Variables Definitions
 
 variable "orca_db_user_password" {
   description = "Password for RDS orca database user authentication"
-  type        = string
+  type = string
 }
 
 variable "orca_default_bucket" {
-  description = "Default ORCA S3 Glacier bucket to use."
   type        = string
+  description = "Default ORCA S3 Glacier bucket to use."
 }
 
 variable "orca_dlq_subscription_email" {
-  description = "The email to notify users when messages are received in dead letter SQS queue due to orca restore failure."
   type        = string
-  default     = "test@email.com"
+  description = "The email to notify users when messages are received in dead letter SQS queue due to orca restore failure."
+  default = "test@email.com"
 }
 
 variable "lambda_timeouts" {
   description = "Configurable map of timeouts for lambdas"
-  type        = map(number)
-  default     = {
-    cleanExecutions          = 400 # archive
-    DistributionApiEndpoints = 400 # cumulus_distribution
-    s3-credentials-endpoint  = 400 # distribution
-    HelloWorld               = 400 # ingest
-    s3-replicator            = 400 # s3-replicator
-    TeaCache                 = 400 # tea-map-cache
+  type = map(number)
+  default = {
+    cleanExecutions = 400           # archive
+    DistributionApiEndpoints = 400  # cumulus_distribution
+    s3-credentials-endpoint  = 400  # distribution
+    HelloWorld = 400                # ingest
+    s3-replicator = 400             # s3-replicator
+    TeaCache = 400                  # tea-map-cache
   }
 }
 
 variable "lambda_memory_sizes" {
   description = "Configurable map of memory sizes for lambdas"
-  type        = map(number)
-  default     = {
-    cleanExecutions          = 512 # archive
-    DistributionApiEndpoints = 512 # cumulus_distribution
-    HelloWorld               = 512 # ingest
-    s3-credentials-endpoint  = 512 # distribution
-    s3-replicator            = 512 # s3-replicator
-    TeaCache                 = 512 # tea-map-cache
+  type = map(number)
+  default = {
+    cleanExecutions = 512           # archive
+    DistributionApiEndpoints = 512  # cumulus_distribution
+    HelloWorld = 512                # ingest
+    s3-credentials-endpoint  = 512  # distribution
+    s3-replicator = 512             # s3-replicator
+    TeaCache = 512                  # tea-map-cache
   }
 }
 
 variable "optional_dynamo_tables" {
+  type = map(object({ name = string, arn = string }))
+  default = {}
   description = "A map of objects with the `arn` and `name` of every additional DynamoDB table your Cumulus deployment can reference."
-  type        = map(object({ name = string, arn = string }))
-  default     = {}
 }
 
 variable "cmr_custom_host" {
@@ -417,64 +417,64 @@ variable "deploy_cumulus_distribution" {
 
 variable "vpc_tag_name" {
   description = "Tag name to use for looking up VPC"
-  type        = string
-  default     = "Application VPC"
+  type = string
+  default = "Application VPC"
 }
 
 variable "subnets_tag_name" {
   description = "Tag name to use for looking up VPC subnets"
-  type        = string
-  default     = "Private application us-east-1a *"
+  type = string
+  default = "Private application us-east-1a *"
 }
 
 variable "cloudwatch_log_retention_periods" {
+  type = map(number)
   description = "retention periods for the respective cloudwatch log group, these values will be used instead of default retention days"
-  type        = map(number)
-  default     = {
+  default = {
     thin-egress-app-EgressLambda = 7
-    ApiEndpoints                 = 7
-    AsyncOperationEcsLogs        = 7
-    DiscoverPdrs                 = 7
-    DistributionApiEndpoints     = 7
-    EcsLogs                      = 7
-    granuleFilesCacheUpdater     = 7
-    HyraxMetadataUpdates         = 7
-    ParsePdr                     = 7
-    PostToCmr                    = 7
-    PrivateApiLambda             = 7
-    publishExecutions            = 7
-    publishGranules              = 7
-    QueuePdrs                    = 7
-    QueueWorkflow                = 7
-    replaySqsMessages            = 7
-    SyncGranule                  = 7
-    UpdateCmrAccessConstraints   = 7
+    ApiEndpoints = 7
+    AsyncOperationEcsLogs = 7
+    DiscoverPdrs = 7
+    DistributionApiEndpoints = 7
+    EcsLogs = 7
+    granuleFilesCacheUpdater = 7
+    HyraxMetadataUpdates = 7
+    ParsePdr = 7
+    PostToCmr = 7
+    PrivateApiLambda = 7
+    publishExecutions = 7
+    publishGranules = 7
+    QueuePdrs = 7
+    QueueWorkflow = 7
+    replaySqsMessages = 7
+    SyncGranule = 7
+    UpdateCmrAccessConstraints = 7
   }
 }
 
 variable "default_log_retention_days" {
+  type = number
+  default = 14
   description = "default value that user chooses for their log retention periods"
-  type        = number
-  default     = 14
 }
 
 variable "report_sns_topic_subscriber_arns" {
-  type    = list
+  type = list
   default = null
 }
 
 ## Dead Letter Recovery Configuration
 
 variable "dead_letter_recovery_cpu" {
+  type = number
+  default = 256
   description = "The amount of CPU units to reserve for the dead letter recovery Async Operation Fargate Task"
-  type        = number
-  default     = 256
 }
 
 variable "dead_letter_recovery_memory" {
+  type = number
+  default = 1024
   description = "The amount of memory in MB to reserve for the dead letter recovery Async Operation Fargate Task"
-  type        = number
-  default     = 1024
 }
 
 variable "workflow_configurations" {
