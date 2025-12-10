531bb1e044e0672e6bab3fc5f837e3c884167fb8
Austin Tisdale <austin.d.tisdale@nasa.gov>
2025-12-10 13:02:44 -0500

    Add pdr-cleanup and initialize task dir

---
+++ tasks/pdr-cleanup/pdr-cleanup.py
@@ -0,0 +1,57 @@
+import boto3
+import json
+import logging
+import os
+from datetime import datetime
+from run_cumulus_task import run_cumulus_task
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+"""
+Handler to archive PDRs
+"""
+def cleanup_pdr(event, context):
+    logger.info('## EVENT OBJ \n' + json.dumps(event))
+
+    provider = event['config']['provider']
+    pdr = event['input']['pdr']
+
+    if (len(event['input']['failed']) == 0):
+        move_pdr(provider, pdr)
+    else:
+        logger.info("PDR failed to ingest all granules successfully, NOT archiving")
+        raise Exception(
+            "PDR failed to ingest all granules successfully\n"
+            f"Ingest Granule workflow failure count: {len(event['input']['failed'])}")
+    return event['input']
+
+"""
+Move PDR to location <provider.host>/<PDRs>/
+"""
+def move_pdr(provider: dict, pdr: dict):
+    curr_date = datetime.now().strftime('%Y.%m.%d')
+    src_path = os.path.join(pdr['path'], pdr['name'])
+    dest_path = os.path.join('PDRs', pdr['path'], curr_date, pdr['name'])
+
+    s3_client = boto3.client('s3')
+    try:
+        s3_client.copy_object(
+            CopySource=os.path.join(provider['host'], src_path),
+            Bucket=provider['host'],
+            Key=dest_path
+        )
+        logger.info(f'COPIED FROM {provider["host"]}/{src_path} TO {provider["host"]}/{dest_path}')
+    except Exception as err:
+        logger.error(f'FAILED TO COPY FROM {provider["host"]}/{src_path} TO {provider["host"]}/{dest_path}')
+        raise
+
+    try:
+        s3_client.delete_object(Bucket=provider['host'], Key=src_path)
+        logger.info(f'DELETED: {provider["host"]}/{src_path}')
+    except Exception as err:
+        logger.error(err)
+        raise
+
+def handler(event, context):
+    return run_cumulus_task(cleanup_pdr, event, context)
