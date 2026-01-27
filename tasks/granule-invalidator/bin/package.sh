df9f349c1414866ef1493be39c2a109c97c7ee2e
mikedorfman <michael.dorfman@colorado.edu>
2026-01-27 08:37:03 -0700

    Fixed package.sh script to reference the correct lambda tag

--- tasks/granule-invalidator/bin/package.sh
+++ tasks/granule-invalidator/bin/package.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 CONFIG=$(jq -r '.' build-config.json)
 RUNTIME=$(echo $CONFIG | jq -r '.runtime')
+PYTHON_VERSION=$(echo $RUNTIME | sed 's/^python//')
 ARCHITECTURE=$(echo $CONFIG | jq -r '.architecture')
 UV_VERSION=$(echo $CONFIG | jq -r '.uv_version')
 
 docker build \
     --platform linux/${ARCHITECTURE} \
     --build-arg UV_TAG=${UV_VERSION} \
-    --build-arg LAMBDA_TAG=${RUNTIME}-${ARCHITECTURE} \
+    --build-arg LAMBDA_TAG=${PYTHON_VERSION}-${ARCHITECTURE} \
     --build-arg OUTPUT_FILENAME=lambda.zip \
     --output type=local,dest=dist/ \
     --file bin/Dockerfile.package .
