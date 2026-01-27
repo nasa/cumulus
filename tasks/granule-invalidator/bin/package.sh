84dbcb200abd37ebde2f46faec9e4b9da9a21772
mikedorfman <michael.dorfman@colorado.edu>
2026-01-27 07:54:08 -0700

    Added Dockerfile to facilitate UV builds Added granule-invalidator Terraform and deployment mechanism Added the `deploy` dir to the create-release-artifacts.sh script so task Terraform is included in release

---
+++ tasks/granule-invalidator/bin/package.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+CONFIG=$(jq -r '.' build-config.json)
+RUNTIME=$(echo $CONFIG | jq -r '.runtime')
+ARCHITECTURE=$(echo $CONFIG | jq -r '.architecture')
+UV_VERSION=$(echo $CONFIG | jq -r '.uv_version')
+
+docker build \
+    --platform linux/${ARCHITECTURE} \
+    --build-arg UV_TAG=${UV_VERSION} \
+    --build-arg LAMBDA_TAG=${RUNTIME}-${ARCHITECTURE} \
+    --build-arg OUTPUT_FILENAME=lambda.zip \
+    --output type=local,dest=dist/ \
+    --file bin/Dockerfile.package .
