54d64b4376036df4680dca3ef77fb5ac89b5bb2f
mikedorfman <michael.dorfman@colorado.edu>
2026-01-27 13:39:14 -0700

    Updated package.sh to avoid docker

--- tasks/granule-invalidator/bin/package.sh
+++ tasks/granule-invalidator/bin/package.sh
@@ -1,14 +1,38 @@
 #!/bin/bash
+set -xe
 CONFIG=$(jq -r '.' build-config.json)
 RUNTIME=$(echo $CONFIG | jq -r '.runtime')
 PYTHON_VERSION=$(echo $RUNTIME | sed 's/^python//')
 ARCHITECTURE=$(echo $CONFIG | jq -r '.architecture')
-UV_VERSION=$(echo $CONFIG | jq -r '.uv_version')
 
-/usr/bin/docker build \
-    --platform linux/${ARCHITECTURE} \
-    --build-arg UV_TAG=${UV_VERSION} \
-    --build-arg LAMBDA_TAG=${PYTHON_VERSION}-${ARCHITECTURE} \
-    --build-arg OUTPUT_FILENAME=lambda.zip \
-    --output type=local,dest=dist/ \
-    --file bin/Dockerfile.package .
+mkdir -p dist/{dist,packages,final} && \
+
+uv export \
+    --frozen \
+    --no-emit-workspace \
+    --all-extras \
+    --no-dev \
+    --no-editable \
+    -o dist/requirements.txt;
+
+uv build \
+    --clear \
+    --wheel \
+    -o dist/dist;
+
+uv venv --python ${PYTHON_VERSION};
+
+uv pip install \
+    --python-platform linux \
+    --python ${PYTHON_VERSION} \
+    --requirements dist/requirements.txt \
+    --target dist/packages;
+
+uv pip install \
+    --python-platform linux \
+    --python ${PYTHON_VERSION} \
+    --target dist/packages \
+    dist/dist/*.whl;
+
+cd dist/packages;
+zip -r ../final/lambda.zip .;
