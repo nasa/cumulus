4906794198477e45a4bff68ded7bb513bba5ba5f
mikedorfman <michael.dorfman@colorado.edu>
2026-02-02 14:40:07 -0700

    Removed unnecessary quotes in select-stack string

--- bamboo/select-stack.js
+++ bamboo/select-stack.js
@@ -31,7 +31,7 @@ function determineIntegrationTestStackName(cb) {
     kkelly51: 'kk-int',
     'Katherine Kelly': 'kk-int',
     mckadesorensen: 'dms-opex-sbx-ci',
-    'mikedorfman': 'mikedorfman-ci',
+    mikedorfman: 'mikedorfman-ci',
     'Nate Pauzenga': 'np-ci',
     'Naga Nages': 'nnaga-ci',
     'Paul Pilone': 'ppilone-ci',
