46f6f3cb94eb1ab0d0186ce706b002411c7caafd
Rohan Weeden <reweeden@alaska.edu>
2026-02-02 12:40:54 -0500

    Add terraform

---
+++ tasks/cnm-response/deploy/variables.tf
@@ -0,0 +1,41 @@
+variable "default_log_retention_days" {
+  description = "The number of days to retain logs in CloudWatch"
+  type        = number
+}
+
+variable "lambda_processing_role_arn" {
+  type = string
+}
+
+variable "lambda_subnet_ids" {
+  type    = list(string)
+  default = []
+}
+
+variable "lambda_timeout" {
+  description = "Timeout value for the Lambda function in seconds"
+  type        = number
+  default     = 300
+}
+
+variable "lambda_memory_size" {
+  description = "Memory size for the Lambda function in MB"
+  type        = number
+  default     = 512
+}
+
+variable "prefix" {
+  type = string
+}
+
+variable "security_group_id" {
+  description = "Security group ID for Lambda VPC configuration"
+  type        = string
+  default     = ""
+}
+
+variable "tags" {
+  description = "Tags to be applied to managed resources"
+  type        = map(string)
+  default     = {}
+}
