f5249421139fb52f3dd96e452abd82dd0d9aa46b
mikedorfman <michael.dorfman@colorado.edu>
2026-02-03 14:29:11 -0700

    Fixed broken output path

--- tf-modules/ingest/outputs.tf
+++ tf-modules/ingest/outputs.tf
@@ -43,8 +43,8 @@ output "files_to_granules_task" {
 
 output "granule_invalidator_task" {
   value = {
-    task_arn           = module.granule_invalidator_task.outputs.lambda_function_arn
-    last_modified_date = module.granule_invalidator_task.outputs.lambda_function_last_modified
+    task_arn           = module.granule_invalidator_task.lambda_function_arn
+    last_modified_date = module.granule_invalidator_task.lambda_function_last_modified
   }
 }
 
