6a6f9b84e75d3375cdb884918b628f771f185f8d
mikedorfman <michael.dorfman@colorado.edu>
2026-02-04 11:39:16 -0700

    Tweaked TF to fix a broken reference

--- example/cumulus-tf/granule_invalidator_workflow.tf
+++ example/cumulus-tf/granule_invalidator_workflow.tf
@@ -15,7 +15,7 @@ module "granule_invalidator_workflow" {
     "${path.module}/granule_invalidator_workflow.asl.json",
     {
       cumulus_internal_api_arn : data.aws_lambda_function.private_api_lambda.arn,
-      granule_invalidator_arn : module.cumulus.granule_invalidator_task
+      granule_invalidator_arn : module.cumulus.granule_invalidator_task.task_arn
     }
   )
 }
