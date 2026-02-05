f611a0244b0926375d2276fb3d16f131e2bf8c9a
eyen <david.yen@jpl.nasa.gov>
2026-02-05 08:35:23 -0800

    package.json and package.sh

--- tasks/cnm-to-cma/bin/package.sh
+++ tasks/cnm-to-cma/bin/package.sh
@@ -1,23 +1,27 @@
 #!/usr/bin/env bash
 set -euo pipefail
-
+DIR=$1
 PYTHON_VERSION=3.13
 IMAGE="public.ecr.aws/lambda/python:${PYTHON_VERSION}"
-BUILD_DIR="build/lambda"
-ZIP_NAME="lambda.zip"
+BUILD_DIR="../build/lambda"
+ZIP_NAME="cnm2cma.zip"
 
-SRC_DIR="src"            # adjust if needed
-HANDLER_FILE="handler.py" # adjust if needed
+SRC_DIR="src/cnm2cma"                # adjust if needed
+HANDLER_FILE="cnm_to_cma.py" # adjust if needed
 
 echo "▶ Cleaning old build..."
-rm -rf build requirements.txt ${ZIP_NAME}
+rm -rf BUILD_DIR ../requirements.txt ../${ZIP_NAME}
 mkdir -p ${BUILD_DIR}
 
-echo "▶ Exporting Poetry dependencies..."
-poetry export \
-  -f requirements.txt \
-  --without-hashes \
-  -o requirements.txt
+#echo "▶ Exporting Poetry dependencies..."
+#poetry export \
+#  -f requirements.txt \
+#  --without-hashes \
+#  -o requirements.txt
+
+# use uv to generate requirements.txt
+echo "▶ Exporting uv dependencies..."
+uv export --format requirements-txt --no-dev --no-hashes -o requirements.txt
 
 echo "▶ Installing dependencies inside Lambda Docker..."
 docker run --rm \
@@ -29,13 +33,13 @@ docker run --rm \
 echo "▶ Copying project source..."
 cp -r ${SRC_DIR}/* ${BUILD_DIR}
 
-if [[ -f "${HANDLER_FILE}" ]]; then
-  cp ${HANDLER_FILE} ${BUILD_DIR}
-fi
+#if [[ -f "${HANDLER_FILE}" ]]; then
+#  cp ${HANDLER_FILE} ${BUILD_DIR}
+#fi
 
 echo "▶ Creating zip..."
-cd build
-zip -r ../${ZIP_NAME} lambda
+cd BUILD_DIR
+zip -r ../${ZIP_NAME} .
 cd ..
 
 echo "✅ Build complete: ${ZIP_NAME}"
