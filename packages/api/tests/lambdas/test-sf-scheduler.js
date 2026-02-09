178d9c1b216d4860da0ee11968b2830a42246d83
etcart <amberhosen@gmail.com>
2026-02-09 15:05:07 -0500

    shouldn't have removed test

--- packages/api/tests/lambdas/test-sf-scheduler.js
+++ packages/api/tests/lambdas/test-sf-scheduler.js
@@ -141,6 +141,26 @@ test.serial('Sends an SQS message to the custom queue URL if queueUrl is defined
   t.deepEqual(targetMessage.meta.provider, fakeProvider);
 });
 
+test.serial('Sends an SQS message to the default queue if queueUrl is not defined', async (t) => {
+  const scheduleInput = {
+    ...scheduleEventTemplate,
+    provider: fakeProvider.id,
+    collection: {
+      name: fakeCollection.name,
+      version: fakeCollection.version,
+    },
+  };
+
+  await schedule.handleScheduleEvent(scheduleInput);
+
+  t.is(sqsStub.calledOnce, true);
+
+  const [targetQueueUrl, targetMessage] = sqsStub.getCall(0).args;
+  t.is(targetQueueUrl, defaultQueueUrl);
+  t.deepEqual(targetMessage.meta.collection, fakeCollection);
+  t.deepEqual(targetMessage.meta.provider, fakeProvider);
+});
+
 test.serial('Sends an SQS message with cmrProvider as overridden by collection', async (t) => {
   const scheduleInput = {
     ...scheduleEventTemplate,
