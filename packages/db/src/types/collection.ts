8620c676876af9b54f0b3e23ffab0ddd282e37ae
etcart <amberhosen@gmail.com>
2026-02-10 13:05:15 -0500

    alphabetizing things

--- packages/db/src/types/collection.ts
+++ packages/db/src/types/collection.ts
@@ -1,27 +1,27 @@
 import { DuplicateHandling } from '@cumulus/types';
 
 export interface PostgresCollection {
-  name: string,
-  version: string,
+  cmr_provider?: string,
+  created_at?: Date,
+  duplicate_handling?: DuplicateHandling,
+  files: string,
   granule_id_validation_regex: string,
   granule_id_extraction_regex: string,
-  files: string,
-  process?: string,
-  duplicate_handling?: DuplicateHandling,
-  report_to_ems?: boolean,
-  sample_file_name: string,
-  url_path?: string,
   ignore_files_config_for_discovery?: boolean,
   meta?: object,
+  name: string,
+  process?: string,
+  report_to_ems?: boolean,
+  sample_file_name: string,
   tags?: string,
-  created_at?: Date,
   updated_at?: Date,
-  cmr_provider?: string,
+  url_path?: string,
+  version: string,
 }
 
 export interface PostgresCollectionRecord extends Omit<PostgresCollection, 'tags'> {
-  cumulus_id: number,
   created_at: Date,
+  cumulus_id: number,
   updated_at: Date,
   tags: string[],
 }
