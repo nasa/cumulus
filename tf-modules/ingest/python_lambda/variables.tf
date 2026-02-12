7eea793303962f5773c5f21bc1e28b9ac9a836cc
mikedorfman <michael.dorfman@colorado.edu>
2026-02-12 14:51:03 -0700

    Took a swag at adding an module that helps create consistent python tasks

---
+++ tf-modules/ingest/python_lambda/variables.tf
@@ -0,0 +1,78 @@
+variable "prefix" {
+  description = "The prefix for resource names"
+  type        = string
+}
+
+variable "role" {
+  description = "ARN of the IAM role for Lambda execution"
+  type        = string
+}
+
+variable "lambda_zip_path" {
+  description = "Path to the Lambda deployment package zip file"
+  type        = string
+}
+
+variable "name" {
+  description = "A name to identify this Lambda function, used in the function name and log group name"
+  type        = string
+}
+
+variable "layers" {
+  description = "ARN of the Cumulus Message Adapter Lambda layer"
+  type        = list(string)
+  default     = []
+}
+
+variable "subnet_ids" {
+  description = "List of subnet IDs for Lambda VPC configuration"
+  type        = list(string)
+  default     = []
+}
+
+variable "security_group_id" {
+  description = "Security group ID for Lambda VPC configuration"
+  type        = string
+  default     = ""
+}
+
+variable "timeout" {
+  description = "Timeout value for the Lambda function in seconds"
+  type        = number
+  default     = 900
+}
+
+variable "memory_size" {
+  description = "Memory size for the Lambda function in MB"
+  type        = number
+  default     = 4096
+}
+
+variable "environment" {
+  description = "Environment variables for the Lambda function.  This is a map that's merged with a set of defaults defined in main.tf."
+  type        = map(string)
+  default     = {}
+}
+
+variable "tags" {
+  description = "Tags to be applied to resources"
+  type        = map(string)
+  default     = {}
+}
+
+variable "default_log_retention_days" {
+  description = "The number of days to retain logs in CloudWatch"
+  type        = number
+}
+
+variable "runtime" {
+  description = "The runtime environment for the Lambda function (e.g., python3.13)"
+  type        = string
+  default     = "python3.13"
+}
+
+variable "architecture" {
+  description = "The instruction set architecture for the Lambda function (e.g., x86_64 or arm64)"
+  type        = string
+  default     = "x86_64"
+}
---
+++ tf-modules/ingest/python_lambdas.tf
@@ -0,0 +1,20 @@
+locals {
+    task_root = "${path.module}/../../tasks"
+    zip_subdir = "dist/final/lambda.zip"
+    aws_api_proxy_name = "aws-api-proxy"
+    security_group_id = length(var.lambda_subnet_ids) > 0 ? aws_security_group.no_ingress_all_egress[0].id : ""
+}
+
+module "aws_api_proxy" {
+    source = "./python_lambda"
+    prefix = var.prefix
+    role = var.lambda_processing_role_arn
+    lambda_zip_path = "${local.task_root}/${local.aws_api_proxy_name}/${local.zip_subdir}"
+    name = local.aws_api_proxy_name
+    subnet_ids = var.lambda_subnet_ids
+    security_group_id = local.security_group_id
+    timeout = lookup(var.lambda_timeouts, local.aws_api_proxy_name, 60 * 15)
+    memory_size = lookup(var.lambda_memory_sizes, local.aws_api_proxy_name, 4096)
+    default_log_retention_days = var.default_log_retention_days
+    tags = var.tags
+}
