1a630d4c0a4a14a0750ec7f75a2222436fdd5de2
mikedorfman <michael.dorfman@colorado.edu>
2026-02-25 12:19:30 -0700

    Updates per PR comments

--- tasks/aws-api-proxy/deploy/variables.tf
+++ tasks/aws-api-proxy/deploy/variables.tf
@@ -1,6 +1,14 @@
 # Specified in terraform.tfvars
-variable "lambda_processing_role_pattern" {
-  description = "Regex pattern to match IAM role name when lambda_processing_role_arn is not provided"
+
+# We may need to use this in the future if we don't have easy access to the role arn
+#variable "lambda_processing_role_pattern" {
+#  description = "Regex pattern to match IAM role name when lambda_processing_role_arn is not provided"
+#  type        = string
+#  default     = ""
+#}
+
+variable "lambda_processing_role_arn" {
+  description = "The ARN of the IAM role to use for the Lambda function."
   type        = string
 }
 
