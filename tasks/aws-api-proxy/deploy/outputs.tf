23fdc0073b0de477761ee876139b23ca16df2fb0
mikedorfman <michael.dorfman@colorado.edu>
2026-02-25 14:13:06 -0700

    Updated broken outputs

--- tasks/aws-api-proxy/deploy/outputs.tf
+++ tasks/aws-api-proxy/deploy/outputs.tf
@@ -1,9 +1,9 @@
 output "cumulus_task_lambda" {
   description = "The task lambda function"
-  value       = aws_lambda_function.cumulus_task_lambda
+  value       = module.aws_api_proxy.cumulus_task_lambda
 }
 
 output "cumulus_task_log_group_name" {
   description = "Name of the CloudWatch log group for the Lambda function"
-  value       = aws_cloudwatch_log_group.cumulus_task_log_group.name
+  value       = module.aws_api_proxy.cumulus_task_log_group_name
 }
