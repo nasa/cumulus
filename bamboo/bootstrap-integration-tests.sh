751f2f3969768f68ebf1dcfa07b9bc7de65da353
jennyhliu <jenny.h.liu@nasa.gov>
2026-03-04 09:49:23 -0500

    release integration test no file or module issues

--- bamboo/bootstrap-integration-tests.sh
+++ bamboo/bootstrap-integration-tests.sh
@@ -10,8 +10,8 @@ if [[ $USE_TERRAFORM_ZIPS == true ]]; then
   echo "***Deploying stack with deployment packages"
 
   npm install
-  ## This is needed to ensure lock-stack has the expected dependencies
-  npx lerna run build --scope @cumulus/cumulus-integration-tests --scope @cumulus/aws-client --scope @cumulus/checksum --scope @cumulus/common --scope @cumulus/errors --scope @cumulus/logger
+  npm run build:lock-stack-deps
+  npm run package:lambdas
 
   ## Update cumulus-tf
   cd example/cumulus-tf
@@ -37,8 +37,7 @@ if [[ $USE_TERRAFORM_ZIPS == true ]]; then
   ## Prepare repo lambdas
   cd ..
 
-  npm install
-  npm run package-deployment
+  npm install --no-workspaces && npm run package-deployment
   cd ..
 else
   echo "***Bootstrapping integration tests with source"
