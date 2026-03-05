30222808703a47f39f0749488cc8daab4218d135
etcart <amberhosen@gmail.com>
2026-03-05 10:22:41 -0500

    looking at index creation

--- packages/db/src/migrations/20260205224950_add_collections_cmr_provider.ts
+++ packages/db/src/migrations/20260205224950_add_collections_cmr_provider.ts
@@ -18,3 +18,7 @@ export const down = async (knex: Knex): Promise<void> => {
   }
   await knex.raw('DROP INDEX IF EXISTS collection_cmr_provider_index');
 };
+
+exports.config = {
+  transaction: false
+}
