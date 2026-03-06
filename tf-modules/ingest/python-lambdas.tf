9916ee3c77a3f0e354a8b7bc2f06516bf2e75b71
Rohan Weeden <reweeden@alaska.edu>
2026-03-06 13:41:59 -0500

    CUMULUS-4570 Cleanup corified tasks (#4268)
    * Add depends_on for lambda log groups
    
    * Rename granule invalidator lambda to match the rest of cumulus core
    tasks
    
    * Rename cnm to cma lambda to match the rest of cumulus core tasks
    
    * Refactor tasks to use shared tf module
    
    * Update CHANGELOG.md
    
    * Refactor output ordering

--- tf-modules/ingest/python-lambdas.tf
+++ tf-modules/ingest/python-lambdas.tf
@@ -1,16 +1,19 @@
 locals {
   task_root          = "${path.module}/../../tasks"
   zip_subdir         = "dist/final/lambda.zip"
-  aws_api_proxy_name = "aws-api-proxy"
+  aws_api_proxy_name = "AwsApiProxy"
   security_group_id  = length(var.lambda_subnet_ids) > 0 ? aws_security_group.no_ingress_all_egress[0].id : ""
 }
 
 module "aws_api_proxy" {
-  source                         = "../../tasks/aws-api-proxy/deploy"
-  prefix                         = var.prefix
-  lambda_processing_role_arn     = var.lambda_processing_role_arn
-  security_group_id              = local.security_group_id
-  lambda_timeout                 = lookup(var.lambda_timeouts, local.aws_api_proxy_name, 60 * 15)
-  lambda_memory_size             = lookup(var.lambda_memory_sizes, local.aws_api_proxy_name, 4096)
-  tags                           = var.tags
+  source = "../../tasks/aws-api-proxy/deploy"
+
+  prefix                     = var.prefix
+  lambda_processing_role_arn = var.lambda_processing_role_arn
+  lambda_timeout             = lookup(var.lambda_timeouts, local.aws_api_proxy_name, 60 * 15)
+  lambda_memory_size         = lookup(var.lambda_memory_sizes, local.aws_api_proxy_name, 4096)
+  security_group_id          = local.security_group_id
+  log_retention_days         = var.default_log_retention_days
+
+  tags = var.tags
 }
