a9fac4f362cb7756a5e6feb319a6db3afaf82893
etcart <amberhosen@gmail.com>
2026-03-09 11:58:16 -0400

    passthrough of cmr_provider in test, and  notNullable

--- packages/db/src/migrations/20260205224950_add_collections_cmr_provider.ts
+++ packages/db/src/migrations/20260205224950_add_collections_cmr_provider.ts
@@ -3,7 +3,8 @@ import { Knex } from 'knex';
 export const up = async (knex: Knex): Promise<void> => {
   if (!await knex.schema.hasColumn('collections', 'cmr_provider')) {
     await knex.schema.table('collections', (table) => {
-      table.text('cmr_provider') // .notNullable() optional until all pieces are updated to work with this
+      table.text('cmr_provider')
+        .notNullable()
         .comment('CMR Provider for this collection');
     });
   }
