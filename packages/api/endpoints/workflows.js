8d6ba2bd717385a7d71e9bfaf9e3076af6668327
wisdomaj <austinwisdom0816@gmail.com>
2026-03-12 19:50:47 -0400

    added ts-check and type annotations to workflow endpoints

--- packages/api/endpoints/workflows.js
+++ packages/api/endpoints/workflows.js
@@ -1,3 +1,4 @@
+// @ts-check
 'use strict';
 
 const { z } = require('zod');
@@ -8,6 +9,8 @@ const {
   getWorkflowsListKeyPrefix,
   getWorkflowFileKey,
 } = require('@cumulus/common/workflows');
+const { getRequiredEnvVar } = require('@cumulus/common/env');
+// @ts-ignore - express-promise-router types don't expose call signatures
 const router = require('express-promise-router')();
 const { zodParser } = require('../src/zod-utils');
 const { returnCustomValidationErrors } = require('../lib/endpoints');
@@ -16,6 +19,11 @@ const log = new Logger({ sender: '@cumulus/api/workflows' });
 /**
  * @typedef {import('express').Request} Request
  * @typedef {import('express').Response} Response
+ */
+
+/**
+ * @typedef {Object} Workflow
+ * @property {string} name
  */
 
 const ListWorkflowsSchema = z.object({
@@ -52,12 +60,16 @@ async function list(req, res) {
   const countOnly = query.countOnly === 'true';
   const fields = query.fields?.split(',');
 
+  const systemBucket = getRequiredEnvVar('system_bucket');
+  const stackName = getRequiredEnvVar('stackName');
+
   const workflows = await listS3ObjectsV2({
-    Bucket: process.env.system_bucket,
-    Prefix: getWorkflowsListKeyPrefix(process.env.stackName),
-  });
+    Bucket: systemBucket,
+    Prefix: getWorkflowsListKeyPrefix(stackName),
+  }) ?? [];
+  /** @type {Workflow[]} */
   let body = await Promise.all(workflows.map(
-    (obj) => getJsonS3Object(process.env.system_bucket, obj.Key)
+    (obj) => getJsonS3Object(systemBucket, /** @type {string} */ (obj.Key))
   ));
 
   // filter the body here
@@ -69,6 +81,7 @@ async function list(req, res) {
   }
 
   if (fields) {
+    // @ts-ignore - fields selection intentionally produces partial objects; sort uses String() to handle missing name
     body = body.map((workflow) =>
       Object.fromEntries(fields.map((field) => {
         if (!(field in workflow)) {
@@ -105,9 +118,11 @@ async function list(req, res) {
 async function get(req, res) {
   const name = req.params.name;
   try {
+    const systemBucket = getRequiredEnvVar('system_bucket');
+    const stackName = getRequiredEnvVar('stackName');
     const workflow = await getJsonS3Object(
-      process.env.system_bucket,
-      getWorkflowFileKey(process.env.stackName, name)
+      systemBucket,
+      getWorkflowFileKey(stackName, name)
     );
     return res.send(workflow);
   } catch (error) {
