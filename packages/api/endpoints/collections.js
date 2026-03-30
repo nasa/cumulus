bc640d12f3c3e3e5eb7e05a6dae60777ac006ed8
etcart <amberhosen@gmail.com>
2026-03-30 12:15:59 -0400

    cleanup

--- packages/api/endpoints/collections.js
+++ packages/api/endpoints/collections.js
@@ -122,9 +122,7 @@ async function post(req, res) {
 
   let translatedCollection;
   try {
-    console.log('checking colletion before translation', JSON.stringify(collection));
     const dbRecord = translateApiCollectionToPostgresCollection(collection);
-    console.log('checking dbRecord after translation', JSON.stringify(dbRecord), JSON.stringify(collection));
     try {
       await createRejectableTransaction(knex, async (trx) => {
         const [pgCollection] = await collectionPgModel.create(trx, dbRecord);
