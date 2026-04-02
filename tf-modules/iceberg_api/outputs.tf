77c54a7f47ea56c4dd878d3f41f6f1f0db83239f
Yonggang Liu <ygliuvt@gmail.com>
2026-04-01 21:24:21 -0400

    CUMULUS-4558: Remove unnecessary cumulus module outputs.

--- tf-modules/iceberg_api/outputs.tf
+++ tf-modules/iceberg_api/outputs.tf
@@ -2,8 +2,3 @@ output "iceberg_api_lb_dns_name" {
   description = "DNS name of the Iceberg API load balancer"
   value       = aws_lb.iceberg_api.dns_name
 }
-
-output "iceberg_api_lb_id" {
-  description = "ID of the Iceberg API load balancer"
-  value       = aws_lb.iceberg_api.id
-}
