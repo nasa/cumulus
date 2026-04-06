cd1e8e5f3392958aa1a45b2b698adaf7000f10db
James Norton <james.norton@nasa.gov>
2026-04-06 17:33:40 -0400

    CUMULUS-4606 iceberg replication terraform (#4319)
    * CUMULUS-4606: Add replication service
    
    * CUMULUS-4606: Clean up variable for security group
    
    * CUMULUS-4606: Rename iceberb replication directories for consistency
    
    * CUMULUS-4606: Add README and outputs for Iceberg replication module
    
    * CUMULUS-4606: Fix markdown linting error
    
    * CUMULUS-4606: Fix typo
    
    * CUMULUS-4606: Moved db creds from variables to data lookups
    
    * CUMULUS-4606: Update changelog
    
    * CUMULUS-4606: Fix terraform to read secrets arn instead of version
    
    * CUMULUS-4606: Remove default for kafka image variable
    
    * Move CHANGELOG entry to proper section
    
    * CUMULUS-4606: Minor terraform changes for clarity
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

---
+++ tf-modules/rds-iceberg-replication/iam.tf
@@ -0,0 +1,129 @@
+resource "aws_iam_role" "ecs_task_execution_role" {
+  name = "ecs-task-execution-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "ecs-tasks.amazonaws.com"
+      }
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_task_execution_role_policy" {
+  role       = aws_iam_role.ecs_task_execution_role.name
+  # AWS managed policy for basic ECS execution permissions
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+resource "aws_iam_role" "fargate_task_role" {
+  name = "fargate-task-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Principal = {
+          Service = "ecs-tasks.amazonaws.com"
+        },
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+
+  tags = var.tags
+}
+
+resource "aws_iam_policy" "s3_access_policy" {
+  name        = "fargate-s3-access-policy"
+  description = "IAM policy for Fargate task to access S3"
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Action = [
+          "s3:GetObject",
+          "s3:ListBucket",
+          "s3:PutObject",
+          "s3:DeleteObject"
+        ],
+        Resource = [
+          "arn:aws:s3:::your-bucket-name",
+          "arn:aws:s3:::your-bucket-name/*"
+        ]
+      }
+    ]
+  })
+}
+
+resource "aws_iam_policy" "glue_access_policy" {
+  name        = "fargate-glue-access-policy"
+  description = "IAM policy for Fargate task to access AWS Glue"
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Action = [
+          "glue:*"
+        ],
+        Resource = "*" # TODO: Scope down to specific ARNs if possible
+      }
+    ]
+  })
+}
+
+resource "aws_iam_policy" "rds_access_policy" {
+  name        = "fargate-rds-access-policy"
+  description = "IAM policy for Fargate task to access RDS"
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Action = [
+          "rds-db:connect"
+        ],
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "attach_s3_policy" {
+  role       = aws_iam_role.fargate_task_role.name
+  policy_arn = aws_iam_policy.s3_access_policy.arn
+}
+
+resource "aws_iam_role_policy_attachment" "attach_glue_policy" {
+  role       = aws_iam_role.fargate_task_role.name
+  policy_arn = aws_iam_policy.glue_access_policy.arn
+}
+
+resource "aws_iam_role" "ecs_infrastructure_role" {
+  name = "ecs-infrastructure-role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "ecs.amazonaws.com"  # Note: ecs, not ecs-tasks
+      }
+    }]
+  })
+  tags = var.tags
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_volumes_attachment" {
+  role       = aws_iam_role.ecs_infrastructure_role.name
+  policy_arn = data.aws_iam_policy.ECSInfrastructure.arn
+}
---
+++ tf-modules/rds-iceberg-replication/variables.tf
@@ -0,0 +1,93 @@
+variable "prefix" {
+  description = "The unique prefix for your deployment resources"
+  type        = string
+}
+
+variable "aws_db_subnet_group_prefix" {
+  description = "Prefix for RDS database cluster subnet group"
+  type        = string
+  default     = "cumulus-rds-tf-subnet"
+}
+
+variable "rds_endpoint" {
+  description = "The rw endpoint for RDS"
+  type        = string
+}
+
+variable "db_admin_username" {
+  description = "Username for RDS database administrator authentication"
+  type        = string
+  default     = "postgres"
+}
+
+variable "db_admin_password" {
+  description = "Password for RDS database administrator authentication"
+  type = string
+}
+
+variable "region" {
+  description = "Region to deploy module to"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "rds_security_group" {
+  description = "RDS access security group"
+  type        = string
+}
+
+variable "subnets" {
+  description = "Subnets for database cluster.  Requires at least 2 across multiple AZs"
+  type    = list(string)
+}
+
+variable "tags" {
+  description = "Tags to be applied to RDS cluster resources that support tags"
+  type        = map(string)
+  default     = {}
+}
+
+variable "vpc_id" {
+  description = "VPC ID for the Cumulus Deployment"
+  type        = string
+}
+
+variable "force_new_deployment" {
+  description = "Enable to force a new task deployment of the service. See https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/ecs_service#force_new_deployment"
+  type = bool
+  default = false
+}
+
+variable "cpu" {
+  description = "The number of CPU units the Amazon ECS container agent will reserve for the task"
+  type    = number
+  default = 4096 # 4 CPUs
+}
+
+variable "cpu_architecture" {
+  description = "The architecture of the cpu platform. Valid values are X86_65 and ARM64"
+  type        = string
+  default     = "ARM64"
+}
+
+variable "memory" {
+  description = "The amount of memory (in MB) that the ECS container agent reserves for a task."
+  type        = number
+  default     = 16384 # 16GB
+}
+
+variable "volume_size_in_gb" {
+  description = "Size in GB of the volume mount used to serialize kafka messages"
+  type        = number
+  default     = 20
+}
+
+variable "kafka_image" {
+  description = "Image used to start the kafka container. See https://docs.aws.amazon.com/AmazonECS/latest/APIReference/API_ContainerDefinition.html#ECS-Type-ContainerDefinition-image"
+  type = string
+}
+
+variable "connect_image" {
+  description = "Image used to start the kafka-connect container. See https://docs.aws.amazon.com/AmazonECS/latest/APIReference/API_ContainerDefinition.html#ECS-Type-ContainerDefinition-image"
+  type = string
+}
