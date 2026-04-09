a2aaa6ad7da3bff0fc117c4ec776c377e0d57010
Mason Yates <mason.t.yates@nasa.gov>
2026-04-09 11:21:54 -0400

    CSD-100

--- packages/api/app/index.js
+++ packages/api/app/index.js
@@ -15,9 +15,6 @@ const { inTestMode } = require('@cumulus/common/test-utils');
 const { secretsManager } = require('@cumulus/aws-client/services');
 const Logger = require('@cumulus/logger');
 
-// NOTE: require('./routes') and others were removed from here
-// and moved inside the handler to ensure process.env is ready first.
-
 const log = new Logger({ sender: '@api/index' });
 
 let initEnvVars;
@@ -62,9 +59,69 @@ const ensureEnvVarsInitialized = () => {
   return initEnvVars;
 };
 
-// Declare app and server globally to cache them across warm invocations
-let app;
-let server;
+// Setup express app
+const app = express();
+app.use(awsServerlessExpressMiddleware.eventContext());
+
+// logging config
+morgan.token('error_obj', (req, res) => {
+  if (res.statusCode !== 200) {
+    return res.error;
+  }
+  return undefined;
+});
+morgan.format(
+  'combined',
+  '[:date[clf]] ":method :url HTTP/:http-version"'
+  + ':status :res[content-length] ":referrer" ":user-agent" :error_obj'
+);
+
+// Config: Lazy load internal middlewares to ensure process.env is populated first
+app.use((req, res, next) => {
+  // eslint-disable-next-line global-require
+  const boom = require('../lib/expressBoom');
+  boom()(req, res, next);
+});
+
+app.use(morgan('combined'));
+app.use(cors());
+app.use(cookieParser());
+// Setting limit to 6 MB which is the AWS lambda limit
+app.use(bodyParser.urlencoded({ limit: '6mb', extended: true }));
+
+app.use((req, res, next) => {
+  // eslint-disable-next-line global-require
+  const { jsonBodyParser } = require('./middleware');
+  jsonBodyParser(req, res, next);
+});
+
+app.use(hsts({ maxAge: 31536000 }));
+
+// Lazy load internal routes to ensure process.env is populated first
+app.use('/v1', (req, res, next) => {
+  // eslint-disable-next-line global-require
+  const router = require('./routes');
+  router(req, res, next);
+});
+
+app.use('/', (req, res, next) => {
+  // eslint-disable-next-line global-require
+  const router = require('./routes');
+  router(req, res, next);
+});
+
+// global 404 response when page is not found
+app.use((_req, res) => {
+  res.boom.notFound('requested page not found');
+});
+
+// catch all error handling
+app.use((err, _req, res, _next) => {
+  res.error = JSON.stringify(err, Object.getOwnPropertyNames(err));
+  return res.boom.badImplementation('Something broke!');
+});
+
+const server = awsServerlessExpress.createServer(app);
 
 const handler = async (event, context) => {
   await ensureEnvVarsInitialized();
@@ -74,60 +131,6 @@ const handler = async (event, context) => {
   Object.keys(dynamoTableNames).forEach((tableEnvVarName) => {
     process.env[tableEnvVarName] = dynamoTableNames[tableEnvVarName];
   });
-
-  if (!server) {
-    // eslint-disable-next-line global-require
-    const router = require('./routes');
-    // eslint-disable-next-line global-require
-    const { jsonBodyParser } = require('./middleware');
-    // eslint-disable-next-line global-require
-    const boom = require('../lib/expressBoom');
-
-    app = express();
-    app.use(awsServerlessExpressMiddleware.eventContext());
-
-    // logging config
-    morgan.token('error_obj', (req, res) => {
-      if (res.statusCode !== 200) {
-        return res.error;
-      }
-      return undefined;
-    });
-    morgan.format(
-      'combined',
-      '[:date[clf]] ":method :url HTTP/:http-version"'
-      + ':status :res[content-length] ":referrer" ":user-agent" :error_obj'
-    );
-
-    // Config
-    app.use(boom());
-    app.use(morgan('combined'));
-    app.use(cors());
-    app.use(cookieParser());
-    // Setting limit to 6 MB which is the AWS lambda limit
-    app.use(bodyParser.urlencoded({ limit: '6mb', extended: true }));
-    app.use(jsonBodyParser);
-    app.use(hsts({ maxAge: 31536000 }));
-
-    // v1 routes
-    app.use('/v1', router);
-
-    // default routes
-    app.use('/', router);
-
-    // global 404 response when page is not found
-    app.use((_req, res) => {
-      res.boom.notFound('requested page not found');
-    });
-
-    // catch all error handling
-    app.use((err, _req, res, _next) => {
-      res.error = JSON.stringify(err, Object.getOwnPropertyNames(err));
-      return res.boom.badImplementation('Something broke!');
-    });
-
-    server = awsServerlessExpress.createServer(app);
-  }
 
   // workaround to support multiValueQueryStringParameters
   const modifiedEvent = {
