43b81c8eed218fdf505ff088076ae997dd2b2761
jennyhliu <jenny.h.liu@nasa.gov>
2026-04-13 22:13:31 -0400

    pdrs

---
+++ packages/db/src/migrations-bootstrap/060_pdrs.ts
@@ -0,0 +1,110 @@
+import { Knex } from 'knex';
+
+export const up = async (knex: Knex): Promise<void> => {
+  await knex.schema.createTable('pdrs', (table) => {
+    // Primary key
+    table.increments('cumulus_id').primary();
+
+    // Foreign keys
+    table.integer('collection_cumulus_id').notNullable();
+    table.integer('provider_cumulus_id').notNullable();
+    table.bigInteger('execution_cumulus_id');
+
+    // Columns
+    table.text('status').notNullable();
+    table.text('name').notNullable();
+
+    table.float('progress');
+    table.boolean('pan_sent');
+    table.text('pan_message');
+
+    table.jsonb('stats');
+
+    table.text('address');
+    table.text('original_url');
+
+    table.float('duration');
+
+    table.timestamp('timestamp', { useTz: true });
+
+    table.timestamps(false, true);
+
+    // Indexes
+    table.index(
+      ['collection_cumulus_id', 'status', 'cumulus_id'],
+      'pdrs_coll_status_cumulus_id_index'
+    );
+
+    table.index(
+      ['execution_cumulus_id'],
+      'pdrs_execution_cumulus_id_index'
+    );
+
+    table.index(
+      ['provider_cumulus_id', 'collection_cumulus_id', 'name'],
+      'pdrs_provider_collection_cumulus_id_name_index'
+    );
+
+    table.index(
+      ['status', 'provider_cumulus_id', 'collection_cumulus_id', 'cumulus_id'],
+      'pdrs_status_provider_collection_cumulus_id_index'
+    );
+
+    table.index(['updated_at'], 'pdrs_updated_at_index');
+  });
+
+  // Unique constraint
+  await knex.raw(`
+    ALTER TABLE pdrs
+    ADD CONSTRAINT pdrs_name_unique UNIQUE (name);
+  `);
+
+  // CHECK constraint
+  await knex.raw(`
+    ALTER TABLE pdrs
+    ADD CONSTRAINT pdrs_status_check
+    CHECK (status = ANY (ARRAY[
+      'running',
+      'failed',
+      'completed'
+    ]));
+  `);
+
+  // Foreign keys
+  await knex.raw(`
+    ALTER TABLE pdrs
+    ADD CONSTRAINT pdrs_collection_cumulus_id_foreign
+    FOREIGN KEY (collection_cumulus_id)
+    REFERENCES collections(cumulus_id);
+  `);
+
+  await knex.raw(`
+    ALTER TABLE pdrs
+    ADD CONSTRAINT pdrs_execution_cumulus_id_foreign
+    FOREIGN KEY (execution_cumulus_id)
+    REFERENCES executions(cumulus_id);
+  `);
+
+  await knex.raw(`
+    ALTER TABLE pdrs
+    ADD CONSTRAINT pdrs_provider_cumulus_id_foreign
+    FOREIGN KEY (provider_cumulus_id)
+    REFERENCES providers(cumulus_id);
+  `);
+
+  // Comments
+  await knex.raw(`
+    COMMENT ON COLUMN pdrs.cumulus_id IS 'Internal Cumulus ID for a PDR';
+    COMMENT ON COLUMN pdrs.status IS 'Status (running, failed, completed) of the PDR';
+    COMMENT ON COLUMN pdrs.name IS 'PDR name';
+    COMMENT ON COLUMN pdrs.progress IS 'PDR completion progress percentage';
+    COMMENT ON COLUMN pdrs.pan_sent IS 'Boolean defining if a PAN response has been sent for this PDR';
+    COMMENT ON COLUMN pdrs.pan_message IS 'PAN message text';
+    COMMENT ON COLUMN pdrs.stats IS 'PDR stats json object';
+    COMMENT ON COLUMN pdrs."timestamp" IS 'PDR timestamp';
+  `);
+};
+
+export const down = async (knex: Knex): Promise<void> => {
+  await knex.schema.dropTableIfExists('pdrs');
+};
