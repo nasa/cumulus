8a58854e3884d57238c129696f64928e4e94718e
James Norton <james.norton@nasa.gov>
2026-04-17 10:36:17 -0400

    CUMULUS-4788: Move replication for different tables into separate modules (#4403)
    * CUMULUS-4788: Move replication for different tables into separate modules
    
    * CUMULUS-4788: Update CHANGLELOG with replication split info
    
    * CUMULUS-4788: Fix linter issues for terraform code
    
    * CUMULUS-4788: Clean up terraform in response to PR comments
    
    * CUMULUS-4788: Add variables for db schema name and
    column exclude list to replication terraform code
    
    * CUMULUS-4788: Add more params for db schema and name

--- tf-modules/rds-iceberg-replication/modules/replication-service/variables.tf
+++ tf-modules/rds-iceberg-replication/modules/replication-service/variables.tf
@@ -3,6 +3,22 @@ variable "prefix" {
   type        = string
 }
 
+variable "region" {
+  description = "Region to deploy module to"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "rds_security_group" {
+  description = "RDS access security group"
+  type        = string
+}
+
+variable "task_security_group_id" {
+  description = "Security group to use for tasks"
+  type        = string
+}
+
 variable "aws_db_subnet_group_prefix" {
   description = "Prefix for RDS database cluster subnet group"
   type        = string
@@ -28,29 +44,12 @@ variable "db_admin_username" {
 
 variable "db_admin_password" {
   description = "Password for RDS database administrator authentication"
-  type = string
-}
-
-variable "region" {
-  description = "Region to deploy module to"
-  type        = string
-  default     = "us-east-1"
-}
-
-variable "rds_security_group" {
-  description = "RDS access security group"
   type        = string
 }
 
 variable "subnet" {
-  description = "Subnet for Fargate tasks"
-  type    = string
-}
-
-variable "tags" {
-  description = "Tags to be applied to RDS cluster resources that support tags"
-  type        = map(string)
-  default     = {}
+  description = "Subnet for Fargate tasks. We only use one since we are using EBS volumes which don't work across multiple AZs."
+  type        = string
 }
 
 variable "vpc_id" {
@@ -60,14 +59,14 @@ variable "vpc_id" {
 
 variable "force_new_deployment" {
   description = "Enable to force a new task deployment of the service. See https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/ecs_service#force_new_deployment"
-  type = bool
-  default = false
+  type        = bool
+  default     = false
 }
 
 variable "cpu" {
   description = "The number of CPU units the Amazon ECS container agent will reserve for the task"
-  type    = number
-  default = 4096 # 4 CPUs
+  type        = number
+  default     = 4096 # 4 CPUs
 }
 
 variable "cpu_architecture" {
@@ -90,27 +89,49 @@ variable "volume_size_in_gb" {
 
 variable "kafka_image" {
   description = "Image used to start the kafka container. See https://docs.aws.amazon.com/AmazonECS/latest/APIReference/API_ContainerDefinition.html#ECS-Type-ContainerDefinition-image"
-  type = string
+  type        = string
 }
 
 variable "connect_image" {
   description = "Image used to start the kafka-connect container. See https://docs.aws.amazon.com/AmazonECS/latest/APIReference/API_ContainerDefinition.html#ECS-Type-ContainerDefinition-image"
-  type = string
+  type        = string
 }
 
 variable "bootstrap_image" {
   description = "Image used to start the bootstrap container. See https://docs.aws.amazon.com/AmazonECS/latest/APIReference/API_ContainerDefinition.html#ECS-Type-ContainerDefinition-image"
-  type = string
+  type        = string
+}
+
+variable "tags" {
+  description = "Tags to be applied to RDS cluster resources that support tags"
+  type        = map(string)
+  default     = {}
+}
+
+variable "slot_name" {
+  description = "The name of the Postgres replication slot to be used to track changes in the dB. This will be created by the source connector if it does not exist."
+  type        = string
+}
+
+variable "table_include_list" {
+  description = "comma-separated list of dB tables to be replicated"
+  type        = string
+}
+
+variable "column_exclude_list" {
+  description = "Comma separated list of database columns that should not be replicated"
+  default     = ""
+  type        = string
 }
 
 variable "iceberg_s3_bucket" {
   description = "S3 bucket where iceberg tables are stored"
-  type = string
+  type        = string
 }
 
 variable "iceberg_namespace" {
   description = "iceberg namespace (same as glue database)"
-  type = string
+  type        = string
 }
 
 variable "pg_db" {
@@ -122,4 +143,37 @@ variable "pg_schema" {
   description = "The name of the schema in the postgres database that contains the tables"
   type = string
   default = "public"
+}
+
+variable "ecs_infrastructure_role" {
+  description = "IAM role used to provide access to EBS volumes"
+  type = object({
+    arn  = string
+    name = string
+  })
+}
+
+variable "fargate_task_role" {
+  description = "IAM role used to allow task containers to access AWS services"
+  type = object({
+    arn  = string
+    name = string
+  })
+}
+
+variable "ecs_task_execution_role" {
+  description = "IAM role used by Docker daemon and ECS container agent"
+  type = object({
+    arn  = string
+    name = string
+  })
+}
+
+variable "ecs_cluster" {
+  description = "The ECS cluster to which the replication service will belong"
+  type = object({
+    arn  = string
+    name = string
+    id   = string
+  })
 }
