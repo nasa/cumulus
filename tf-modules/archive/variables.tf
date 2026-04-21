290a9bc57569707fba94cea0965ffdd3234f5489
jennyhliu <jenny.h.liu@nasa.gov>
2026-04-21 16:00:12 -0400

    remove OpenTelemetry from feature branch

--- tf-modules/archive/variables.tf
+++ tf-modules/archive/variables.tf
@@ -397,9 +397,3 @@ variable "dead_letter_recovery_memory" {
   default     = 1024
   description = "The amount of memory in MB to reserve for the dead letter recovery Async Operation Fargate Task"
 }
-
-variable "enable_otel_tracing" {
-  description = "Enable OpenTelemetry tracing and X-Ray"
-  type        = bool
-  default     = false
-}
