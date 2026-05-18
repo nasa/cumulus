66f7cca61b971092e3e714612eaf9bd5717ffb07
etcart <amberhosen@gmail.com>
2026-05-18 10:40:42 -0400

    add cmrProvider to integration test sns outoputs

--- example/spec/parallel/testAPI/bulkDeleteSuccessSpec.js
+++ example/spec/parallel/testAPI/bulkDeleteSuccessSpec.js
@@ -301,6 +301,7 @@ describe('POST /granules/bulkDelete', () => {
         productionDateTime: message.record.productionDateTime,
         beginningDateTime: message.record.beginningDateTime,
         lastUpdateDateTime: message.record.lastUpdateDateTime,
+        cmrProvider: get(collection, 'cmrProvider')
       };
       delete expectedGranuleAfterDeletion.cmrLink;
 
