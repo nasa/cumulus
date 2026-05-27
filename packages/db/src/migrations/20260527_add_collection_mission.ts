e824fbae915af66692fb8e2e550feaffcef73d64
etcart <amberhosen@gmail.com>
2026-05-27 10:01:43 -0400

    fix table name

--- packages/db/src/migrations/20260527_add_collection_mission.ts
+++ packages/db/src/migrations/20260527_add_collection_mission.ts
@@ -1,13 +1,13 @@
 import { Knex } from 'knex';
 
 export const up = async (knex: Knex): Promise<void> => {
-  await knex.schema.alterTable('collection', (table) => {
+  await knex.schema.alterTable('collections', (table) => {
     table.text('mission').notNullable();
   })
 }
 
 export const down = async (knex: Knex): Promise<void> => {
-  await knex.schema.alterTable('collection', (table) => {
+  await knex.schema.alterTable('collections', (table) => {
     table.dropColumn('mission');
   })
 }
