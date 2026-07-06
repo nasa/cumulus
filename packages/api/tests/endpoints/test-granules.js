3a8a9a300f926a3f9598f550644fd8d443b00039
Nate Pauzenga <npauzenga@gmail.com>
2026-07-06 16:10:26 -0400

    update test to include correct bucket name

--- packages/api/tests/endpoints/test-granules.js
+++ packages/api/tests/endpoints/test-granules.js
@@ -127,7 +127,7 @@ async function setupBucketsConfig() {
   };
   const buckets = {
     protected: {
-      name: systemBucket,
+      name: systemBucket.name,
       type: 'protected',
     },
     public: {
