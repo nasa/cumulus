fccaadff9cd135ed2a07ae6c9f2d3a943772f5db
Robert Swanson <robert.swanson@nasa.gov>
2026-09-04 11:15:07 -0600

    CUMULUS-4954: unlock dev stack

--- bamboo/bootstrap-tf-deployment.sh
+++ bamboo/bootstrap-tf-deployment.sh
@@ -43,7 +43,7 @@ else
 fi
 
 # Remove RS dev lock state
-../terraform force-unlock 06d0d1bc-f3cf-eb36-1975-9cd0e05011b9 -force
+../terraform force-unlock '06d0d1bc-f3cf-eb36-1975-9cd0e05011b9' -force
 
 # Deploy data-persistence-tf via terraform
 echo "Deploying Cumulus data-persistence module to $DEPLOYMENT"
