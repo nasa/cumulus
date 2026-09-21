3b3c288ee97a580e317a15166f6e3d29061a64d5
Hailiang Zhang <hailiang.zhang@nasa.gov>
2026-09-21 16:48:12 -0400

    fix lint issue

--- packages/aws-client/tests/test-SSM.js
+++ packages/aws-client/tests/test-SSM.js
@@ -1,17 +1,20 @@
 const test = require('ava');
-const { randomId } = require('@cumulus/common/test-utils');
+const cryptoRandomString = require('crypto-random-string');
+
 const { ssm } = require('../services');
 const SSM = require('../SSM');
 
+const randomString = () => cryptoRandomString({ length: 10 });
+
 test('getParameterValue() returns the value of an SSM parameter', async (t) => {
-  const Name = randomId('parameter');
+  const Name = randomString();
   await ssm().putParameter({ Name, Type: 'String', Value: 'some-value' });
 
   t.is(await SSM.getParameterValue(Name), 'some-value');
 });
 
 test('getParameterValue() returns the raw comma-separated value of a StringList parameter', async (t) => {
-  const Name = randomId('parameter');
+  const Name = randomString();
   await ssm().putParameter({ Name, Type: 'StringList', Value: 'one,two' });
 
   t.is(await SSM.getParameterValue(Name), 'one,two');
@@ -19,7 +22,7 @@ test('getParameterValue() returns the raw comma-separated value of a StringList 
 
 test('getParameterValue() throws ParameterNotFound if the parameter does not exist', async (t) => {
   await t.throwsAsync(
-    SSM.getParameterValue(randomId('nonexistent-parameter')),
+    SSM.getParameterValue(randomString()),
     { name: 'ParameterNotFound' }
   );
 });
